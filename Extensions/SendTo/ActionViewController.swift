d082311f4ae3fdd6d69c77724de9253b5b0879b9
Stefan Arentz <stefan@arentz.ca>
2014-11-11 11:06:05 -0500

    First iteration of 1094262 - Implement the Send To UI

--- Extensions/SendTo/ActionViewController.swift
+++ Extensions/SendTo/ActionViewController.swift
@@ -1,64 +1,106 @@
-//
-//  ActionViewController.swift
-//  SendToDevice
-//
-//  Created by Stefan Arentz on 2014-11-04.
-//  Copyright (c) 2014 Mozilla. All rights reserved.
-//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
 
 import UIKit
 import MobileCoreServices
 
-class ActionViewController: UIViewController {
+let SharedContainerIdentifier = "group.org.allizom.Client" // TODO: Can we grab this from the .entitlements file instead?
 
-    @IBOutlet weak var imageView: UIImageView!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+func pushUrl(url: String, #title: String, toClient client: Client) {
+    let request = NSMutableURLRequest(URL: NSURL(string: "https://moz-syncapi.sateh.com/1.0/clients/" + client.id + "/tab")!)
+    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+    request.addValue("application/json", forHTTPHeaderField: "Accept")
+    request.HTTPMethod = "POST"
     
-        // Get the item[s] we're handling from the extension context.
+    var object = NSMutableDictionary()
+    object["url"] = url
+    object["title"] = title
+    
+    var jsonError: NSError?
+    let data = NSJSONSerialization.dataWithJSONObject(object, options: nil, error: &jsonError)
+    if data != nil {
+        request.HTTPBody = data
+    }
+    
+    let userPasswordString = "sarentz+syncapi@mozilla.com:q1w2e3r4"
+    let userPasswordData = userPasswordString.dataUsingEncoding(NSUTF8StringEncoding)
+    let base64EncodedCredential = userPasswordData!.base64EncodedStringWithOptions(nil)
+    let authString = "Basic \(base64EncodedCredential)"
+    
+    let configuration = NSURLSessionConfiguration.backgroundSessionConfigurationWithIdentifier("org.allizom.Client.SendTo")
+    configuration.HTTPAdditionalHeaders = ["Authorization" : authString]
+    configuration.sharedContainerIdentifier = SharedContainerIdentifier
+    
+    let session = NSURLSession(configuration: configuration, delegate: nil, delegateQueue: nil)
+    let task = session.dataTaskWithRequest(request)
+    task.resume()
+}
+
+class DevicesViewController: UITableViewController
+{
+    var clients: [Client] = []
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        Clients.getAll { (clients) -> Void in
+            self.clients = clients
+            self.tableView.reloadData()
+        }
+    }
+    
+    override func viewDidLoad() {
+        title = NSLocalizedString("Send To Device", comment: "")
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Cancel, target: self, action: "cancel")
+    }
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return clients.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel.text = clients[indexPath.row].name
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let client = clients[indexPath.row]
+        fetchSharedURL { (url, title, error) -> Void in
+            pushUrl(url.absoluteString!, title: "", toClient: client)
+            self.extensionContext!.completeRequestReturningItems([], completionHandler: nil);
+        }
+    }
+    
+    func cancel() {
+        self.extensionContext!.completeRequestReturningItems(nil, completionHandler: nil)
+    }
+    
+    func fetchSharedURL(completionHandler: (NSURL!, String?, NSError!) -> Void) {
+        var title : String? = nil;
         
-        // For example, look for an image and place it into an image view.
-        // Replace this with something appropriate for the type[s] your extension supports.
-        var imageFound = false
-        for item: AnyObject in self.extensionContext!.inputItems {
-            let inputItem = item as NSExtensionItem
-            for provider: AnyObject in inputItem.attachments! {
-                let itemProvider = provider as NSItemProvider
-                if itemProvider.hasItemConformingToTypeIdentifier(kUTTypeImage as NSString) {
-                    // This is an image. We'll load it, then place it in our image view.
-                    weak var weakImageView = self.imageView
-                    itemProvider.loadItemForTypeIdentifier(kUTTypeImage as NSString, options: nil, completionHandler: { (image, error) in
-                        if image != nil {
-                            NSOperationQueue.mainQueue().addOperationWithBlock {
-                                if let imageView = weakImageView {
-                                    imageView.image = image as? UIImage
-                                }
-                            }
-                        }
-                    })
-                    
-                    imageFound = true
-                    break
-                }
-            }
-            
-            if (imageFound) {
-                // We only handle one image, so stop looking for more.
-                break
+        if let inputItems : [NSExtensionItem] = self.extensionContext!.inputItems as? [NSExtensionItem] {
+            let item = inputItems[0]
+            title = item.attributedContentText?.string as String?
+            if let attachments = item.attachments as? [NSItemProvider] {
+                attachments[0].loadItemForTypeIdentifier(kUTTypeURL as NSString, options:nil, completionHandler: { (obj, error) in
+                    completionHandler(obj as? NSURL, title, error)
+                })
             }
         }
     }
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+@objc(ActionViewController)
+class ActionViewController: UINavigationController
+{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Cancel, target: self, action: "cancel")
+        pushViewController(DevicesViewController(), animated: false)
     }
-
-    @IBAction func done() {
-        // Return any edited content to the host app.
-        // This template doesn't do anything, so we just echo the passed in items.
-        self.extensionContext!.completeRequestReturningItems(self.extensionContext!.inputItems, completionHandler: nil)
-    }
-
 }
