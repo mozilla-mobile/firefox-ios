83b0fd94beef0b021af5d61be4618c79e01363af
Brian Nicholson <bnicholson@mozilla.com>
2014-12-08 17:30:40 -0800

    Bug 1108880 - Implement basic browser UI
    Adds a bare bones implementation for a minimal browser app. Includes a
    WebView, back/forward buttons, and a navigation text field.

---
+++ Client/Frontend/Browser/Browser.swift
@@ -0,0 +1,34 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import Foundation
+import WebKit
+
+class Browser {
+    private let webView = WKWebView()
+
+    var view: UIView {
+        return webView
+    }
+
+    var canGoBack: Bool {
+        return webView.canGoBack
+    }
+
+    var canGoForward: Bool {
+        return webView.canGoForward
+    }
+
+    func goBack() {
+        webView.goBack()
+    }
+
+    func goForward() {
+        webView.goForward()
+    }
+
+    func loadRequest(request: NSURLRequest) {
+        webView.loadRequest(request)
+    }
+}
---
+++ Client/Frontend/Browser/BrowserToolbar.swift
@@ -0,0 +1,112 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import Foundation
+import UIKit
+
+protocol BrowserToolbarDelegate {
+    func didClickBack()
+    func didClickForward()
+    func didEnterURL(url: NSURL)
+}
+
+class BrowserToolbar: UIToolbar, UITextFieldDelegate {
+    var browserToolbarDelegate: BrowserToolbarDelegate?
+
+    private var urlTextField: UITextField!
+
+    override init() {
+        super.init()
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        viewDidInit()
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        viewDidInit()
+    }
+
+    private func viewDidInit() {
+        let backButton = UIButton()
+        backButton.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+        backButton.setTitle("<", forState: UIControlState.Normal)
+        backButton.addTarget(self, action: "SELdidClickBack", forControlEvents: UIControlEvents.TouchUpInside)
+        let backButtonItem = UIBarButtonItem()
+        backButtonItem.customView = backButton
+
+        let forwardButton = UIButton()
+        forwardButton.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+        forwardButton.setTitle(">", forState: UIControlState.Normal)
+        forwardButton.addTarget(self, action: "SELdidClickForward", forControlEvents: UIControlEvents.TouchUpInside)
+        let forwardButtonItem = UIBarButtonItem()
+        forwardButtonItem.customView = forwardButton
+
+        urlTextField = ToolbarTextField()
+        urlTextField.keyboardType = UIKeyboardType.URL
+        urlTextField.autocorrectionType = UITextAutocorrectionType.No
+        urlTextField.layer.backgroundColor = UIColor.whiteColor().CGColor
+        urlTextField.layer.cornerRadius = 8
+        urlTextField.setContentHuggingPriority(0, forAxis: UILayoutConstraintAxis.Horizontal)
+        urlTextField.delegate = self
+        let urlButtonItem = UIBarButtonItem()
+        urlButtonItem.customView = urlTextField
+
+        let items = [forwardButtonItem, backButtonItem, urlButtonItem]
+        setItems(items, animated: true)
+
+        backButton.snp_makeConstraints { make in
+            make.left.equalTo(self)
+            make.centerY.equalTo(self)
+            make.width.height.equalTo(44)
+        }
+
+        forwardButton.snp_makeConstraints { make in
+            make.left.equalTo(backButton.snp_right)
+            make.centerY.equalTo(self)
+            make.width.height.equalTo(44)
+        }
+
+        urlTextField.snp_makeConstraints { make in
+            make.left.equalTo(forwardButton.snp_right)
+            make.centerY.equalTo(self)
+            make.right.equalTo(self).offset(-8)
+        }
+    }
+
+    func SELdidClickBack() {
+        browserToolbarDelegate?.didClickBack()
+    }
+
+    func SELdidClickForward() {
+        browserToolbarDelegate?.didClickForward()
+    }
+
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        let urlString = urlTextField.text
+        let url = NSURL(string: urlString)
+
+        if url == nil {
+            println("Error parsing URL: " + urlString)
+            return false
+        }
+
+        browserToolbarDelegate?.didEnterURL(url!)
+        return false
+    }
+}
+
+private class ToolbarTextField: UITextField {
+    override func textRectForBounds(bounds: CGRect) -> CGRect {
+        let rect = super.textRectForBounds(bounds)
+        return rect.rectByInsetting(dx: 5, dy: 5)
+    }
+
+    override func editingRectForBounds(bounds: CGRect) -> CGRect {
+        let rect = super.editingRectForBounds(bounds)
+        return rect.rectByInsetting(dx: 5, dy: 5)
+    }
+}
