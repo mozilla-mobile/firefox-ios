2045103d2028ad6abba2e7b87f8f5e8d4e9b0825
Richard Newman <rnewman@twinql.com>
2014-12-12 11:56:43 +0000

    Bug 1110688 - Comment that SecRandomCopyBytes (and thus Bytes.generateRandomBytes) do not need initialization.

--- Sync/EncryptedRecord.swift
+++ Sync/EncryptedRecord.swift
@@ -11,7 +11,10 @@ public class KeyBundle : Equatable {
     let hmacKey: NSData;
 
     public class func random() -> KeyBundle {
-        // TODO: ensure seeded random.
+        // Bytes.generateRandomBytes uses SecRandomCopyBytes, which hits /dev/random, which
+        // on iOS is populated by the OS from kernel-level sources of entropy.
+        // That should mean that we don't need to seed or initialize anything before calling
+        // this. That is probably not true on (some versions of) OS X.
         return KeyBundle(encKey: Bytes.generateRandomBytes(32), hmacKey: Bytes.generateRandomBytes(32))
     }
 
