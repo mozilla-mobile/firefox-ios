b2b4b68e9545e7df94aec4e04a240c29871a55c7
Aaron <luanjiwu5@gmail.com>
2015-01-15 23:49:13 +0800

    add license head.

--- Client/Frontend/Browser/BrowserNotification.swift
+++ Client/Frontend/Browser/BrowserNotification.swift
@@ -1,10 +1,6 @@
-//
-//  BrowserNotification.swift
-//  Client
-//
-//  Created by Aaron on 1/14/15.
-//  Copyright (c) 2015 Mozilla. All rights reserved.
-//
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import UIKit
 
