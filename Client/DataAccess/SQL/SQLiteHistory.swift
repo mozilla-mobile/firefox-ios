c5ce386158c6bec6194881bd5923a9cb763a1bac
Richard Newman <rnewman@twinql.com>
2015-01-15 18:02:57 -0800

    Bug 1122224 - Part 13: implement a SQL-based version of the History protocol.

---
+++ Client/DataAccess/SQL/SQLiteHistory.swift
@@ -0,0 +1,53 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/* The sqlite backed implementation of the history protocol */
+class SqliteHistory : History {
+    let profile: Profile
+    let db: BrowserDB
+
+    required init(profile: Profile) {
+        self.profile = profile
+        self.db = BrowserDB(profile: profile)!
+    }
+
+    func clear(filter: String?, options: HistoryOptions?, complete: (success: Bool) -> Void) {
+        let s: Site? = nil
+        var err: NSError? = nil
+        db.delete(HISTORY_TABLE, item: s, err: &err)
+        dispatch_async(dispatch_get_main_queue()) {
+            if err != nil {
+                self.debug("Clear failed: \(err!.localizedDescription)")
+            }
+            complete(success: err == nil)
+        }
+    }
+
+    func get(filter: String?, options: HistoryOptions?, complete: (data: Cursor) -> Void) {
+        let res = db.query(HISTORY_TABLE)
+        dispatch_async(dispatch_get_main_queue()) {
+            complete(data: res)
+        }
+    }
+
+    func addVisit(site: Site, options: HistoryOptions?, complete: (success: Bool) -> Void) {
+        var err: NSError? = nil
+        db.insert(HISTORY_TABLE, item: site, err: &err)
+        // TODO: Track visits in a separate table
+
+        dispatch_async(dispatch_get_main_queue()) {
+            if err != nil {
+                self.debug("Add failed: \(err!.localizedDescription)")
+            }
+            complete(success: err == nil)
+        }
+    }
+
+    private let debug_enabled = false
+    private func debug(msg: String) {
+        if debug_enabled {
+            println("HistorySqlite: " + msg)
+        }
+    }
+}
