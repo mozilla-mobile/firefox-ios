33059eaf893510c7595942b81646744d4fc5c8d2
Stefan Arentz (Mozilla) <sarentz@Copernicus.local>
2015-02-17 15:45:39 -0800

    1132014 - Implement reading list home panel

--- Client/Frontend/Home/ReaderPanel.swift
+++ Client/Frontend/Home/ReaderPanel.swift
@@ -3,51 +3,52 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import UIKit
-
 import Storage
 
-class ReaderPanel: UITableViewController {
-    var profile: Profile!
+class ReaderPanel: UITableViewController, HomePanel {
 
-    private var sites = [Site]()
-    /*
-        Site(title:"Royals Sweep Orioles to Advance to World Series", url:"http://www.nytimes.com/2014/10/16/sports/baseball/royals-keep-rolling-and-advance-to-the-world-series.html?hp&action=click&pgtype=Homepage&version=LargeMediaHeadlineSum&module=photo-spot-region&region=top-news&WT.nav=top-news&_r=0"),
-        Site(title:"How Not to Be Fooled by Odds", url:"http://www.nytimes.com/2014/10/16/upshot/how-not-to-be-fooled-by-odds.html?hp&action=click&pgtype=Homepage&version=HpSum&module=second-column-region&region=top-news&WT.nav=top-news"),
-        Site(title:"Against Rules, Amber Vinson, Dallas Worker With Ebola, Boarded Plane", url:"http://www.nytimes.com/2014/10/16/us/ebola-outbreak-texas.html?hp&action=click&pgtype=Homepage&version=LedeSum&module=first-column-region&region=top-news&WT.nav=top-news")
-    ]
-    */
+    var readingListItemsCursor: Cursor? = nil
+    weak var delegate: HomePanelDelegate? = nil
 
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
+    var profile: Profile! {
+        didSet {
+            profile.readingList.get { (cursor) -> Void in
+                self.readingListItemsCursor = (cursor.status == .Success) ? cursor : nil
+                self.tableView.reloadData()
+            }
+        }
     }
 
+    // MARK: UITableViewDataSourceDelegate
+
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return sites.count
+        if let cursor = self.readingListItemsCursor {
+            return cursor.count
+        }
+        return 0
     }
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: nil)
-
-        let site = sites[indexPath.row]
-
-        // TODO: We need better async image loading here
-        let opts = QueryOptions()
-        opts.filter = site.url
-
-        cell.textLabel?.text = site.title
-        cell.textLabel?.font = UIFont(name: "FiraSans-SemiBold", size: 13)
-        cell.textLabel?.textColor = UIAccessibilityDarkerSystemColorsEnabled() ? UIColor.blackColor() : UIColor.darkGrayColor()
-        cell.indentationWidth = 20
-
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "ReadingListCell")
+        if let cursor = self.readingListItemsCursor {
+            if let readingListItem = cursor[indexPath.row] as? ReadingListItem {
+                cell.textLabel?.text = readingListItem.title
+            }
+        }
         return cell
     }
 
-    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        let cell = tableView.cellForRowAtIndexPath(indexPath)
-        let site = sites[indexPath.row]
+    // MARK: UITableViewDelegate
 
-        let readerController = ReaderViewController(nibName: "ReaderViewController", bundle: nil)
-        readerController.urlSpec = site.url
-        presentViewController(readerController, animated: true, completion: nil)
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        if let cursor = self.readingListItemsCursor {
+            if let readingListItem = cursor[indexPath.row] as? ReadingListItem {
+                if let encodedURL = readingListItem.url.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.alphanumericCharacterSet()) {
+                    if let aboutReaderURL = NSURL(string: "about:reader?url=\(encodedURL)") {
+                        delegate?.homePanel(didSubmitURL: aboutReaderURL)
+                    }
+                }
+            }
+        }
     }
 }
