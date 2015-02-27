558cf0f184a1f5c04b2ca9d7b7b883b9e7497369
Nick Alexander <nalexander@mozilla.com>
2015-02-26 18:00:17 -0800

    Bug 1129161 - Add stub Account and Search sections.
    This lays the ground-work for a full-featured Account setting, and makes
    the expected Settings controller delegate to the inner Search Settings
    controller (which, for learning purposes, I implemented first).

--- Client/Frontend/SettingsTableViewController.swift
+++ Client/Frontend/SettingsTableViewController.swift
@@ -5,6 +5,12 @@
 import UIKit
 
 class SettingsTableViewController: UITableViewController {
+    let SectionAccount = 0
+    let SectionSearch = 1
+    let NumberOfSections = 2
+
+    var profile: Profile!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -13,5 +19,59 @@ class SettingsTableViewController: UITableViewController {
             title: NSLocalizedString("Done", comment: "Settings"),
             style: UIBarButtonItemStyle.Done,
             target: navigationController, action: "SELdone")
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell: UITableViewCell
+
+        if indexPath.section == SectionAccount {
+            cell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: nil)
+            cell.editingAccessoryType = UITableViewCellAccessoryType.DisclosureIndicator
+            cell.textLabel?.text = NSLocalizedString("Sign in", comment: "Settings")
+        } else if indexPath.section == SectionSearch {
+            cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: nil)
+            cell.editingAccessoryType = UITableViewCellAccessoryType.DisclosureIndicator
+            cell.textLabel?.text = NSLocalizedString("Search", comment: "Settings")
+        } else {
+            cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: nil)
+        }
+
+        // So that the seperator line goes all the way to the left edge.
+        cell.separatorInset = UIEdgeInsetsZero
+
+        return cell
+    }
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return NumberOfSections
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == SectionAccount {
+            return 1
+        } else if section == SectionSearch {
+            return 1
+        } else {
+            return 0
+        }
+    }
+
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == SectionAccount {
+            return nil
+        } else if section == SectionSearch {
+            return NSLocalizedString("Search", comment: "Settings")
+        } else {
+            return nil
+        }
+    }
+
+    override func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
+        if indexPath.section == SectionSearch {
+            let viewController = SearchSettingsTableViewController()
+            viewController.model = profile.searchEngines
+            navigationController?.pushViewController(viewController, animated: true)
+        }
+        return nil
     }
 }
