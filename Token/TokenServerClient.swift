098f5fcf03ae204f786ef79bbe3d00a322c9668c
Nick Alexander <nalexander@mozilla.com>
2015-03-12 16:38:00 -0700

    Bug 1142809 - Modernize TokenServerClient and tests.

--- Token/TokenServerClient.swift
+++ Token/TokenServerClient.swift
@@ -2,140 +2,108 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import Foundation
 import Alamofire
+import Deferred
+import Foundation
+import Result
 
-public let PROD_TOKEN_SERVER_ENDPOINT = "https://token.services.mozilla.com/1.0/sync/1.5";
-public let STAGE_TOKEN_SERVER_ENDPOINT = "https://token.stage.mozaws.net/1.0/sync/1.5";
+let TokenServerClientErrorDomain = "org.mozilla.token.error"
+let TokenServerClientUnknownError = NSError(domain: TokenServerClientErrorDomain, code: 999,
+    userInfo: [NSLocalizedDescriptionKey: "Invalid server response"])
 
-public let TokenServerClientErrorDomain = "org.mozilla.token.error"
+struct TokenServerToken {
+    let id: String
+    let key: String
+    let api_endpoint: String
+    let uid: Int64
+    let durationInSeconds: Int64
 
-public class TokenServerToken {
-    public let id : String;
-    public let key : String;
-    public let api_endpoint : String;
-    public let uid : UInt32;
-
-    public init(id: String, key: String, api_endpoint: String, uid: UInt32) {
+    init(id: String, key: String, api_endpoint: String, uid: Int64, durationInSeconds: Int64) {
         self.id = id
         self.key = key
         self.api_endpoint = api_endpoint
         self.uid = uid
+        self.durationInSeconds = durationInSeconds
     }
 }
 
-public class TokenServerClient {
-    public class var requestManager : Alamofire.Manager {
-        struct Static {
-            static let manager : Alamofire.Manager = Alamofire.Manager(configuration: Alamofire.Manager.sharedInstance.session.configuration)
-        }
-        Static.manager.startRequestsImmediately = false
-        return Static.manager
+class TokenServerClient {
+    let URL: NSURL
+
+    init(URL: NSURL? = nil) {
+        self.URL = URL ?? ProductionSync15Configuration().tokenServerEndpointURL
     }
 
-    public let url : String
-
-    public init(endpoint: String? = nil) {
-        self.url = endpoint ?? PROD_TOKEN_SERVER_ENDPOINT
-    }
-
-    public class func getAudienceForEndpoint(endpoint: String) -> String {
-        let url = NSURL(string: endpoint)!
-        if let port = url.port {
-            return "\(url.scheme!)://\(url.host!):\(port)"
+    class func getAudienceForURL(URL: NSURL) -> String {
+        if let port = URL.port {
+            return "\(URL.scheme!)://\(URL.host!):\(port)"
         } else {
-            return "\(url.scheme!)://\(url.host!)"
+            return "\(URL.scheme!)://\(URL.host!)"
         }
     }
 
-    private class func validateJSON(json : JSON) -> Bool {
-        return json["id"].isString &&
-            json["key"].isString &&
-            json["api_endpoint"].isString &&
-            json["uid"].isInt
+    private class func remoteErrorFromJSON(json: JSON, statusCode: Int) -> NSError? {
+        if json.isError {
+            return nil
+        }
+        if let status = json["status"].asString {
+            let userInfo = [NSLocalizedDescriptionKey: status, "status": status]
+            return NSError(domain: FxAClientErrorDomain, code: statusCode, userInfo: userInfo)
+        }
+        return nil
     }
 
-    private class func tokenFromJSON(json: JSON) -> TokenServerToken {
-        let id = json["id"].asString!
-        let key = json["key"].asString!
-        let api_endpoint = json["api_endpoint"].asString!
-        let uid = UInt32(json["uid"].asInt!)
-        return TokenServerToken(id: id, key: key, api_endpoint: api_endpoint, uid: uid)
-    }
-
-    public class Request {
-        public let queue: dispatch_queue_t
-        private let request : Alamofire.Request
-
-        private var successBlock: (TokenServerToken -> Void)?;
-
-        public init(queue: dispatch_queue_t, request: Alamofire.Request) {
-            self.queue = queue ?? dispatch_get_main_queue()
-            self.request = request
+    private class func tokenFromJSON(json: JSON) -> TokenServerToken? {
+        if json.isError {
+            return nil
         }
-
-        public func onSuccess(block: TokenServerToken -> Void) -> Self {
-            self.successBlock = block;
-            return self
-        }
-
-        public func go(errorBlock: NSError -> Void) {
-            if successBlock == nil {
-                return dispatch_async(queue, {
-                    errorBlock(NSError(domain: TokenServerClientErrorDomain, code: -1, userInfo: ["message": "no success handler"]))
-                })
-            }
-
-            request.responseJSON { (request, response, json, error) in
-                if let error = error {
-                    return dispatch_async(self.queue, {
-                        errorBlock(error)
-                    })
+        if let id = json["id"].asString {
+            if let key = json["key"].asString {
+                if let api_endpoint = json["api_endpoint"].asString {
+                    if let uid = json["uid"].asInt {
+                        if let durationInSeconds = json["duration"].asInt64 {
+                            return TokenServerToken(id: id, key: key, api_endpoint: api_endpoint, uid: Int64(uid),
+                                durationInSeconds: durationInSeconds)
+                        }
+                    }
                 }
-
-                if response == nil || json == nil {
-                    return dispatch_async(self.queue, {
-                        errorBlock(NSError(domain: TokenServerClientErrorDomain, code: -1, userInfo: ["message": "malformed JSON response"]))
-                    })
-                }
-
-                let json = JSON(json!)
-
-                let statusCode : Int = response!.statusCode
-                if  statusCode != 200 {
-                    return dispatch_async(self.queue, {
-                        errorBlock(NSError(domain: TokenServerClientErrorDomain, code: -1, userInfo: ["message": "bad response code", "code": statusCode,
-                            "body": json.toString(pretty: true)]))
-                    })
-                }
-
-                if !TokenServerClient.validateJSON(json) {
-                    return dispatch_async(self.queue, {
-                        errorBlock(NSError(domain: TokenServerClientErrorDomain, code: -1, userInfo: ["message": "invalid token server response"]))
-                    })
-                }
-
-                let token = TokenServerClient.tokenFromJSON(json)
-                return dispatch_async(self.queue, {
-                    self.successBlock!(token)
-                })
             }
         }
+        return nil
     }
 
-    public func tokenRequest(queue: dispatch_queue_t? = nil, assertion: String, clientState: String? = nil) -> Request {
-        let URL = NSURL(string: url)!
+    func token(assertion: String, clientState: String? = nil) -> Deferred<Result<TokenServerToken>> {
+        let deferred = Deferred<Result<TokenServerToken>>()
+
         var mutableURLRequest = NSMutableURLRequest(URL: URL)
         mutableURLRequest.setValue("BrowserID " + assertion, forHTTPHeaderField: "Authorization")
         if let clientState = clientState {
             mutableURLRequest.setValue(clientState, forHTTPHeaderField: "X-Client-State")
         }
 
-        let manager = TokenServerClient.requestManager
+        Alamofire.request(mutableURLRequest)
+            .validate(contentType: ["application/json"])
+            .responseJSON { (_, response, data, error) in
+                if let error = error {
+                    deferred.fill(Result(failure: error))
+                    return
+                }
 
-        let request = manager.request(mutableURLRequest)
-        request.resume()
+                if let data: AnyObject = data { // Declaring the type quiets a Swift warning about inferring AnyObject.
+                    let json = JSON(data)
+                    if let remoteError = TokenServerClient.remoteErrorFromJSON(json, statusCode: response!.statusCode) {
+                        deferred.fill(Result(failure: remoteError))
+                        return
+                    }
 
-        return Request(queue: queue ?? dispatch_get_main_queue(), request: request)
+                    if let token = TokenServerClient.tokenFromJSON(json) {
+                        deferred.fill(Result(success: token))
+                        return
+                    }
+                }
+
+                deferred.fill(Result(failure: TokenServerClientUnknownError))
+            }
+        return deferred
     }
 }
--- Token/TokenServerClientTests.swift
+++ Token/TokenServerClientTests.swift
@@ -2,126 +2,87 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+import Client
+import FxA
 import UIKit
 import XCTest
-import FxA
-import Client
 
-let TEST_TOKEN_SERVER_ENDPOINT = STAGE_TOKEN_SERVER_ENDPOINT
-let TEST_USERNAME = "testuser" // Really, testuser@mockmyid.com.
-
-class TokenServerClientTests: XCTestCase {
+// Testing client state is so delicate that I'm not going to test this.  The test below does two
+// requests; we would need a third, and a guarantee of the server state, to test this completely.
+// The rule is: if you turn up with a never-before-seen client state; you win.  If you turn up with
+// a seen-before client state, you lose.
+class TokenServerClientTests: LiveAccountTest {
     func testAudienceForEndpoint() {
+        func audienceFor(endpoint: String) -> String {
+            return TokenServerClient.getAudienceForURL(NSURL(string: endpoint)!)
+        }
+
         // Sub-domains and path components.
-        XCTAssertEqual("http://sub.test.com", TokenServerClient.getAudienceForEndpoint("http://sub.test.com"));
-        XCTAssertEqual("http://test.com", TokenServerClient.getAudienceForEndpoint("http://test.com/"));
-        XCTAssertEqual("http://test.com", TokenServerClient.getAudienceForEndpoint("http://test.com/path/component"));
-        XCTAssertEqual("http://test.com", TokenServerClient.getAudienceForEndpoint("http://test.com/path/component/"));
+        XCTAssertEqual("http://sub.test.com", audienceFor("http://sub.test.com"));
+        XCTAssertEqual("http://test.com", audienceFor("http://test.com/"));
+        XCTAssertEqual("http://test.com", audienceFor("http://test.com/path/component"));
+        XCTAssertEqual("http://test.com", audienceFor("http://test.com/path/component/"));
 
         // No port and default port.
-        XCTAssertEqual("http://test.com", TokenServerClient.getAudienceForEndpoint("http://test.com"));
-        XCTAssertEqual("http://test.com:80", TokenServerClient.getAudienceForEndpoint("http://test.com:80"));
+        XCTAssertEqual("http://test.com", audienceFor("http://test.com"));
+        XCTAssertEqual("http://test.com:80", audienceFor("http://test.com:80"));
 
-        XCTAssertEqual("https://test.com", TokenServerClient.getAudienceForEndpoint("https://test.com"));
-        XCTAssertEqual("https://test.com:443", TokenServerClient.getAudienceForEndpoint("https://test.com:443"));
+        XCTAssertEqual("https://test.com", audienceFor("https://test.com"));
+        XCTAssertEqual("https://test.com:443", audienceFor("https://test.com:443"));
 
         // Ports that are the default ports for a different scheme.
-        XCTAssertEqual("https://test.com:80", TokenServerClient.getAudienceForEndpoint("https://test.com:80"));
-        XCTAssertEqual("http://test.com:443", TokenServerClient.getAudienceForEndpoint("http://test.com:443"));
+        XCTAssertEqual("https://test.com:80", audienceFor("https://test.com:80"));
+        XCTAssertEqual("http://test.com:443", audienceFor("http://test.com:443"));
 
         // Arbitrary ports.
-        XCTAssertEqual("http://test.com:8080", TokenServerClient.getAudienceForEndpoint("http://test.com:8080"));
-        XCTAssertEqual("https://test.com:4430", TokenServerClient.getAudienceForEndpoint("https://test.com:4430"));
+        XCTAssertEqual("http://test.com:8080", audienceFor("http://test.com:8080"));
+        XCTAssertEqual("https://test.com:4430", audienceFor("https://test.com:4430"));
     }
 
-    func testSuccess() {
-        let url = TEST_TOKEN_SERVER_ENDPOINT
-        let expectation = expectationWithDescription("\(url)")
+    func testTokenSuccess() {
+        let audience = TokenServerClient.getAudienceForURL(ProductionSync15Configuration().tokenServerEndpointURL)
 
-        let keyPair = RSAKeyPair.generateKeyPairWithModulusSize(512)
-        let assertion = MockMyIDTokenFactory.defaultFactory().createAssertionWithKeyPair(keyPair, username: TEST_USERNAME, audience: TokenServerClient.getAudienceForEndpoint(url))
+        withCertificate { expectation, emailUTF8, keyPair, certificate in
+            let assertion = JSONWebTokenUtils.createAssertionWithPrivateKeyToSignWith(keyPair.privateKey,
+                certificate: certificate, audience: audience)
 
-        let client = TokenServerClient(endpoint: url)
-        client.tokenRequest(assertion: assertion).onSuccess { (token) in
-            expectation.fulfill()
-            XCTAssertNotNil(token.id)
-            XCTAssertNotNil(token.key)
-            XCTAssertNotNil(token.api_endpoint)
-            XCTAssertTrue(token.uid >= 0)
-            XCTAssertTrue(token.api_endpoint.hasSuffix(String(token.uid)))
-            }
-            .go { (error) in
+            let client = TokenServerClient()
+            client.token(assertion).upon { result in
+                if let token = result.successValue {
+                    XCTAssertNotNil(token.id)
+                    XCTAssertNotNil(token.key)
+                    XCTAssertNotNil(token.api_endpoint)
+                    XCTAssertTrue(token.uid >= 0)
+                    XCTAssertTrue(token.api_endpoint.hasSuffix(String(token.uid)))
+                } else {
+                    let error = result.failureValue as NSError
+                    XCTAssertNil(error)
+                }
                 expectation.fulfill()
-                println(error)
-                XCTFail("should have succeeded");
-        }
-
-        waitForExpectationsWithTimeout(10) { (error) in
-            XCTAssertNil(error, "\(error)")
-        }
-    }
-
-    func testFailure() {
-        let url = TEST_TOKEN_SERVER_ENDPOINT
-        let expectation = expectationWithDescription("\(url)")
-
-        let client = TokenServerClient(endpoint: url)
-        client.tokenRequest(assertion: "").onSuccess { (token) in
-            expectation.fulfill()
-            XCTFail("should have failed");
             }
-            .go { (error) in
-                expectation.fulfill()
-                let t = error.userInfo!["code"]! as Int
-                XCTAssertEqual(t, 401)
         }
-
-        waitForExpectationsWithTimeout(10) { (error) in
-            XCTAssertNil(error, "\(error)")
-        }
+        self.waitForExpectationsWithTimeout(100, handler: nil)
     }
 
-//    let TEST_CLIENT_STATE = "1";
-//    let TEST_USERNAME_FOR_CLIENT_STATE = "test3";
-//
-//    // Testing client state so delicate that I'm not going to test this.  The test below does two requests; we would need a third, and a guarantee of the server state, to test this completely.
-//    // The rule is: if you turn up with a never-before-seen client state; you win.  If you turn up with a seen-before client state, you lose.
-//    func testClientStateMatters() {
-//        let url = STAGE_TOKEN_SERVER_ENDPOINT
-//        let expectation1 = expectationWithDescription("\(url)")
-//        let expectation2 = expectationWithDescription("\(url)")
-//
-//        let keyPair = RSAKeyPair.generateKeyPairWithModulusSize(512)
-//        let assertion = MockMyIDTokenFactory.defaultFactory().createAssertionWithKeyPair(keyPair, username: TEST_USERNAME_FOR_CLIENT_STATE, audience: TokenServerClient.getAudienceForEndpoint(url))
-//
-//        var token1 : TokenServerToken!
-//        var token2 : TokenServerToken!
-//
-//        let client = TokenServerClient(endpoint: url)
-//        client.tokenRequest(assertion: assertion).onSuccess { (token) in
-//            expectation1.fulfill()
-//            token1 = token
-//            }
-//            .go { (error) in
-//                expectation1.fulfill()
-//                println(error.userInfo!["code"]);
-//                XCTFail("should have succeeded");
-//        }
-//
-//        client.tokenRequest(assertion: assertion).onSuccess { (token) in
-//            expectation2.fulfill()
-//            token2 = token
-//            }
-//            .go { (error) in
-//                expectation2.fulfill()
-//                println(error.userInfo!["code"]);
-//                XCTFail("should have succeeded");
-//        }
-//
-//        waitForExpectationsWithTimeout(10) { (error) in
-//            XCTAssertNil(error, "\(error)")
-//        }
-//
-//        XCTAssertNotEqual(token1.uid, token2.uid)
-//    }
+    func testTokenFailure() {
+        withVerifiedAccount { _, _ in
+            // Account details aren't used, but we want to skip when we're not running live tests.
+            let e = self.expectationWithDescription("")
+
+            let assertion = "BAD ASSERTION"
+
+            let client = TokenServerClient()
+            client.token(assertion).upon { result in
+                if let token = result.successValue {
+                    XCTFail("Got token: \(token)")
+                } else {
+                    let error = result.failureValue as NSError
+                    XCTAssertEqual(error.code, 401) // Bad auth.
+                    XCTAssertEqual(error.userInfo!["status"] as String, "error")
+                }
+                e.fulfill()
+            }
+        }
+        self.waitForExpectationsWithTimeout(10, handler: nil)
+    }
 }
