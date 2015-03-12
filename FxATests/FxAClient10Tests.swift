bdab32d207757a07d4c9bd4a4d72ceef01086101
Nick Alexander <nalexander@mozilla.com>
2015-03-12 13:59:48 -0700

    Bug 1139051 - Post: Only run live tests if signedInUser.json includes an email address.
    It's not easy to include a conditional resource in the test bundle, so
    we always include an empty signedInUser.json.  A developer wishing to
    test should overwrite the empty file with their own.
    
    Since there's no way to recover the password from signedInUser.json (or
    its equivalent on any platform!), this test code assumes that the test
    account's password is identical to its email address.

--- FxATests/FxAClient10Tests.swift
+++ FxATests/FxAClient10Tests.swift
@@ -8,110 +8,104 @@ import Result
 import UIKit
 import XCTest
 
-class FxAClient10Tests: XCTestCase {
+class FxAClient10Tests: LiveAccountTest {
     func testLoginSuccess() {
-        let e = self.expectationWithDescription("")
+        withVerifiedAccount { emailUTF8, quickStretchedPW in
+            let e = self.expectationWithDescription("")
 
-        let email : NSData = "testtestoo@mockmyid.com".utf8EncodedData!
-        let password : NSData = email
-        let quickStretchedPW : NSData = FxAClient10.quickStretchPW(email, password: password)
-
-        let client = FxAClient10()
-        let result = client.login(email, quickStretchedPW: quickStretchedPW, getKeys: true)
-        result.upon { result in
-            if let response = result.successValue {
-                XCTAssertNotNil(response.uid)
-                XCTAssertEqual(response.verified, true)
-                XCTAssertNotNil(response.sessionToken)
-                XCTAssertNotNil(response.keyFetchToken)
-            } else {
-                let error = result.failureValue as NSError
-                XCTAssertNil(error)
+            let client = FxAClient10()
+            let result = client.login(emailUTF8, quickStretchedPW: quickStretchedPW, getKeys: true)
+            result.upon { result in
+                if let response = result.successValue {
+                    XCTAssertNotNil(response.uid)
+                    XCTAssertEqual(response.verified, true)
+                    XCTAssertNotNil(response.sessionToken)
+                    XCTAssertNotNil(response.keyFetchToken)
+                } else {
+                    let error = result.failureValue as NSError
+                    XCTAssertNil(error)
+                }
+                e.fulfill()
             }
-            e.fulfill()
         }
         self.waitForExpectationsWithTimeout(10, handler: nil)
     }
 
     func testLoginFailure() {
-        let e = self.expectationWithDescription("")
+        withVerifiedAccount { emailUTF8, _ in
+            let e = self.expectationWithDescription("")
 
-        let email : NSData = "testtestoo@mockmyid.com".utf8EncodedData!
-        let password : NSData = "INCORRECT PASSWORD".utf8EncodedData!
-        let quickStretchedPW : NSData = FxAClient10.quickStretchPW(email, password: password)
+            let badPassword = FxAClient10.quickStretchPW(emailUTF8, password: "BAD PASSWORD".utf8EncodedData!)
 
-        let client = FxAClient10()
-        let result = client.login(email, quickStretchedPW: quickStretchedPW, getKeys: true)
-        result.upon { result in
-            if let response = result.successValue {
-                XCTFail("Got response: \(response)")
-            } else {
-                let error = result.failureValue as NSError
-                XCTAssertEqual(error.code, 103) // Incorrect password.
+            let client = FxAClient10()
+            let result = client.login(emailUTF8, quickStretchedPW: badPassword, getKeys: true)
+            result.upon { result in
+                if let response = result.successValue {
+                    XCTFail("Got response: \(response)")
+                } else {
+                    let error = result.failureValue as NSError
+                    XCTAssertEqual(error.code, 103) // Incorrect password.
+                }
+                e.fulfill()
             }
-            e.fulfill()
         }
         self.waitForExpectationsWithTimeout(10, handler: nil)
     }
 
     func testKeysSuccess() {
-        let e = self.expectationWithDescription("")
+        withVerifiedAccount { emailUTF8, quickStretchedPW in
+            let e = self.expectationWithDescription("")
 
-        let email : NSData = "testtestoo@mockmyid.com".utf8EncodedData!
-        let password : NSData = email
-        let quickStretchedPW : NSData = FxAClient10.quickStretchPW(email, password: password)
-
-        let client = FxAClient10()
-        let login: Deferred<Result<FxALoginResponse>> = client.login(email, quickStretchedPW: quickStretchedPW, getKeys: true)
-        let keys: Deferred<Result<FxAKeysResponse>> = login.bind { (result: Result<FxALoginResponse>) in
-            switch result {
-            case let .Failure(error):
-                return Deferred(value: .Failure(error))
-            case let .Success(loginResponse):
-                return client.keys(loginResponse.value.keyFetchToken)
+            let client = FxAClient10()
+            let login: Deferred<Result<FxALoginResponse>> = client.login(emailUTF8, quickStretchedPW: quickStretchedPW, getKeys: true)
+            let keys: Deferred<Result<FxAKeysResponse>> = login.bind { (result: Result<FxALoginResponse>) in
+                switch result {
+                case let .Failure(error):
+                    return Deferred(value: .Failure(error))
+                case let .Success(loginResponse):
+                    return client.keys(loginResponse.value.keyFetchToken)
+                }
             }
-        }
-        keys.upon { result in
-            if let response = result.successValue {
-                XCTAssertEqual(32, response.kA.length)
-                XCTAssertEqual(32, response.wrapkB.length)
-            } else {
-                let error = result.failureValue as NSError
-                XCTAssertNil(error)
+            keys.upon { result in
+                if let response = result.successValue {
+                    XCTAssertEqual(32, response.kA.length)
+                    XCTAssertEqual(32, response.wrapkB.length)
+                } else {
+                    let error = result.failureValue as NSError
+                    XCTAssertNil(error)
+                }
+                e.fulfill()
             }
-            e.fulfill()
         }
         self.waitForExpectationsWithTimeout(10, handler: nil)
     }
 
     func testSignSuccess() {
-        let e = self.expectationWithDescription("")
+        withVerifiedAccount { emailUTF8, quickStretchedPW in
+            let e = self.expectationWithDescription("")
 
-        let email : NSData = "testtestoo@mockmyid.com".utf8EncodedData!
-        let password : NSData = email
-        let quickStretchedPW : NSData = FxAClient10.quickStretchPW(email, password: password)
-
-        let client = FxAClient10()
-        let login: Deferred<Result<FxALoginResponse>> = client.login(email, quickStretchedPW: quickStretchedPW, getKeys: true)
-        let sign: Deferred<Result<FxASignResponse>> = login.bind { (result: Result<FxALoginResponse>) in
-            switch result {
-            case let .Failure(error):
-                return Deferred(value: .Failure(error))
-            case let .Success(loginResponse):
-                let keyPair = RSAKeyPair.generateKeyPairWithModulusSize(1024)
-                return client.sign(loginResponse.value.sessionToken, publicKey: keyPair.publicKey)
+            let client = FxAClient10()
+            let login: Deferred<Result<FxALoginResponse>> = client.login(emailUTF8, quickStretchedPW: quickStretchedPW, getKeys: true)
+            let sign: Deferred<Result<FxASignResponse>> = login.bind { (result: Result<FxALoginResponse>) in
+                switch result {
+                case let .Failure(error):
+                    return Deferred(value: .Failure(error))
+                case let .Success(loginResponse):
+                    let keyPair = RSAKeyPair.generateKeyPairWithModulusSize(1024)
+                    return client.sign(loginResponse.value.sessionToken, publicKey: keyPair.publicKey)
+                }
             }
-        }
-        sign.upon { result in
-            if let response = result.successValue {
-                XCTAssertNotNil(response.certificate)
-                // A simple test that we got a reasonable certificate back.
-                XCTAssertEqual(3, response.certificate.componentsSeparatedByString(".").count)
-            } else {
-                let error = result.failureValue as NSError
-                XCTAssertNil(error)
+            sign.upon { result in
+                if let response = result.successValue {
+                    XCTAssertNotNil(response.certificate)
+                    // A simple test that we got a reasonable certificate back.
+                    XCTAssertEqual(3, response.certificate.componentsSeparatedByString(".").count)
+                } else {
+                    let error = result.failureValue as NSError
+                    XCTAssertNil(error)
+                }
+                e.fulfill()
             }
-            e.fulfill()
         }
         self.waitForExpectationsWithTimeout(10, handler: nil)
     }
