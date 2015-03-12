77fcde9a16a3bb79717eeaaa88247b60dc388ba6
Nick Alexander <nalexander@mozilla.com>
2015-03-12 12:59:28 -0700

    No bug - Add NSData.randomOfLength.

--- Utils/HexExtensions.swift
+++ Utils/HexExtensions.swift
@@ -15,4 +15,13 @@ extension NSData {
     public var hexEncodedString: String {
         return base16Encode(self, uppercase: false)
     }
+
+    class func randomOfLength(length: UInt) -> NSData? {
+        if let data = NSMutableData(length: Int(length)) {
+            let result = SecRandomCopyBytes(kSecRandomDefault, length, UnsafeMutablePointer<UInt8>(data.mutableBytes))
+            return NSData(data: data)
+        } else {
+            return nil
+        }
+    }
 }
