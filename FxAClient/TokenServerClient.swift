188365198b823c97a33da842b6c1c2dc7ebf403e
Richard Newman <rnewman@twinql.com>
2015-03-17 12:55:27 -0700

    ThirdParty -> Shared.

--- FxAClient/TokenServerClient.swift
+++ FxAClient/TokenServerClient.swift
@@ -3,7 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import Alamofire
-import ThirdParty
+import Shared
 import Foundation
 
 let TokenServerClientErrorDomain = "org.mozilla.token.error"
