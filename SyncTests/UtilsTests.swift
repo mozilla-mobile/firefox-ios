81d9bb97d6cc68c55c9405f2864c4385f3aa2c87
Richard Newman <rnewman@twinql.com>
2015-03-20 19:39:27 -0700

    Bug 1141843 - Pre: add jsonsToStrings and mapValues utility functions. r=nalexander

---
+++ SyncTests/UtilsTests.swift
@@ -0,0 +1,35 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import Foundation
+import Shared
+import XCTest
+
+/**
+ * Test for our own utils.
+ */
+class UtilsTests: XCTestCase {
+    func testMapUtils() {
+        let m: [String: Int] = ["foo": 123, "bar": 456]
+        let f: (Int) -> Int? = { v in
+            return (v > 200) ? 999 : nil
+        }
+
+        let o = mapValues(m, f)
+        XCTAssertEqual(2, o.count)
+        XCTAssertTrue(o["foo"] == nil)
+        XCTAssertTrue(o["bar"]! == 999)
+
+        let filtered = optFilter(o)
+        XCTAssertEqual(1, filtered.count)
+        XCTAssertTrue(filtered["bar"] == 999)
+    }
+
+    func testOptFilter() {
+        let a: [Int?] = [nil, 1, nil, 2, 3, 4]
+        let b = optFilter(a)
+        XCTAssertEqual(4, b.count)
+        XCTAssertEqual([1, 2, 3, 4], b)
+    }
+}
--- Utils/Functions.swift
+++ Utils/Functions.swift
@@ -8,3 +8,42 @@
 public func optFilter<T>(a: [T?]) -> [T] {
     return a.filter { $0 != nil }.map { $0! }
 }
+
+/**
+ * Return a new map with only key-value pairs that have a non-nil value.
+ * for which the function returns non-nil. Works seamlessly with a
+ * function that has a non-optional type signature, too.
+ */
+public func optFilter<K, V>(source: [K: V?]) -> [K: V] {
+    var m = [K: V]()
+    for (k, v) in source {
+        if let v = v {
+            m[k] = v
+        }
+    }
+    return m
+}
+
+/**
+ * Map a function over the values of a map.
+ */
+public func mapValues<K, T, U>(source: [K: T], f: (T -> U)) -> [K: U] {
+    var m = [K: U]()
+    for (k, v) in source {
+        m[k] = f(v)
+    }
+    return m
+}
+
+/**
+ * Take a JSON array, returning the String elements as an array.
+ * It's usually convenient for this to accept an optional.
+ */
+public func jsonsToStrings(arr: [JSON]?) -> [String]? {
+    if let arr = arr {
+        return optFilter(arr.map { j in
+            return j.asString
+            })
+    }
+    return nil
+}
