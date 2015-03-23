20c7fed7240076e22c63892b627c95fec481ba94
Richard Newman <rnewman@twinql.com>
2015-03-22 21:39:03 -0700

    Bug 1141843 - Pre: share LiveAccountTest into SyncTests for reuse. r=nalexander

--- AccountTests/LiveAccountTest.swift
+++ AccountTests/LiveAccountTest.swift
@@ -2,9 +2,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import Shared
 import Foundation
+import Shared
 import FxA
+import Account
 import XCTest
 
 /*
