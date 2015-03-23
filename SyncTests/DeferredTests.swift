102ac95103557ad952e6fa6d4d5b4381c0ed1103
Nick Alexander <nalexander@mozilla.com>
2015-03-22 21:59:25 -0700

    Bug 1146227 - Pre: Test that multiple invocations of Deferred.upon work.

--- SyncTests/DeferredTests.swift
+++ SyncTests/DeferredTests.swift
@@ -24,4 +24,20 @@ class DeferredTests: XCTestCase {
 
         XCTAssertEqual(5, d.peek()!, "Value is filled.");
     }
+
+    func testMultipleUponBlocks() {
+        let e1 = self.expectationWithDescription("First.")
+        let e2 = self.expectationWithDescription("Second.")
+        let d = Deferred<Int>()
+        d.upon { x in
+            XCTAssertEqual(x, 5)
+            e1.fulfill()
+        }
+        d.upon { x in
+            XCTAssertEqual(x, 5)
+            e2.fulfill()
+        }
+        d.fill(5)
+        waitForExpectationsWithTimeout(10, handler: nil)
+    }
 }
