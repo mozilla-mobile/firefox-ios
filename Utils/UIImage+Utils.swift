5a3a23bad552d4138b9f328eba40592c6817a208
Wes Johnston <wjohnston@mozilla.com>
2015-03-31 12:50:38 -0700

    Bug 1147175 - Use sdwebimage for memory and disk caching of thumbnails

--- Utils/UIImage+Utils.swift
+++ Utils/UIImage+Utils.swift
@@ -16,3 +16,47 @@ extension UIImage {
         return image
     }
 }
+
+private class ThumbnailOperation : NSObject, SDWebImageOperation {
+    var cacheOperation: NSOperation?
+
+    var cancelled: Bool {
+        if let cacheOperation = cacheOperation {
+            return cacheOperation.cancelled
+        }
+        return false
+    }
+
+    func cancel() {
+        if let cacheOperation = cacheOperation {
+            cacheOperation.cancel()
+        }
+    }
+}
+
+// This is an extension to SDWebImage's api to allow passing in a cache to be used for lookup.
+public typealias CompletionBlock = (img: UIImage?, err: NSError, type: SDImageCacheType, key: String) -> Void
+extension UIImageView {
+    public func moz_getImageFromCache(url: String, cache: SDImageCache, completed: CompletionBlock) {
+        self.sd_cancelCurrentImageLoad()
+
+        let operation = ThumbnailOperation()
+        let key = SDWebThumbnails.getKey(url)
+        operation.cacheOperation = cache.queryDiskCacheForKey(key, done: { (image, cacheType) -> Void in
+            let err = NSError()
+            // If this was cancelled, don't bother notifying the caller
+            if operation.cancelled {
+                return
+            }
+
+            if let image = image {
+                self.image = image
+                self.setNeedsLayout()
+            }
+
+            completed(img: image, err: err, type: cacheType, key: url)
+        })
+
+        self.sd_setImageLoadOperation(operation, forKey: "UIImageViewImageLoad")
+    }
+}
