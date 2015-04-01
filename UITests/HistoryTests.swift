f150f3e8218e93d4cb3df3588ca450ab347f671d
Aaron Train <aaron.train@gmail.com>
2015-04-01 12:48:39 -0400

    minor header/comment alignment

--- UITests/HistoryTests.swift
+++ UITests/HistoryTests.swift
@@ -1,6 +1,6 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
-* License, v. 2.0. If a copy of the MPL was not distributed with this
-* file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import Foundation
 import WebKit
@@ -12,7 +12,7 @@ class HistoryTests: KIFTestCase {
         webRoot = SimplePageServer.start()
     }
 
-    /**
+   /**
     * Tests for listed history visits
     */
     func testHistoryUI() {
