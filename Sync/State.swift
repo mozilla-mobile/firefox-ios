2637d4dde79fd9855416f006c329086e6dde4090
Richard Newman <rnewman@twinql.com>
2015-04-12 21:50:13 -0700

    Bug 1141845 - Pre: stub out clientGUID in Scratchpad.
    This needs to be un-stubbed by the time we upload a client record.

--- Sync/State.swift
+++ Sync/State.swift
@@ -174,6 +174,8 @@ public class Scratchpad {
     // What's our client name?
     let clientName: String
 
+    let clientGUID: String = "TODOTODOTODO"
+
     // Where do we persist when told?
     let prefs: Prefs
 
