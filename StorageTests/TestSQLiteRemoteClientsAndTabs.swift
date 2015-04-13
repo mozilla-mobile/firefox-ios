1bcae413e2627c44e883ef9ab93378f5849a7652
Richard Newman <rnewman@twinql.com>
2015-04-13 16:33:39 -0700

    Review comment: move MockRemoteClientsAndTabs into tests.

--- StorageTests/TestSQLiteRemoteClientsAndTabs.swift
+++ StorageTests/TestSQLiteRemoteClientsAndTabs.swift
@@ -3,7 +3,59 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import Foundation
+import Shared
 import XCTest
+
+public class MockRemoteClientsAndTabs: RemoteClientsAndTabs {
+    public let clientsAndTabs: [ClientAndTabs]
+
+    public init() {
+        let now = NSDate.now()
+        let client1GUID = Bytes.generateGUID()
+        let client2GUID = Bytes.generateGUID()
+        let u11 = NSURL(string: "http://test.com/test1")!
+        let tab11 = RemoteTab(clientGUID: client1GUID, URL: u11, title: "Test 1", history: [], lastUsed: (now - OneMinuteInMilliseconds), icon: nil)
+
+        let u12 = NSURL(string: "http://test.com/test2")!
+        let tab12 = RemoteTab(clientGUID: client1GUID, URL: u12, title: "Test 2", history: [], lastUsed: (now - OneHourInMilliseconds), icon: nil)
+
+        let tab21 = RemoteTab(clientGUID: client2GUID, URL: u11, title: "Test 1", history: [], lastUsed: (now - OneDayInMilliseconds), icon: nil)
+
+        let u22 = NSURL(string: "http://different.com/test2")!
+        let tab22 = RemoteTab(clientGUID: client2GUID, URL: u22, title: "Different Test 2", history: [], lastUsed: now + OneHourInMilliseconds, icon: nil)
+
+        let client1 = RemoteClient(guid: client1GUID, name: "Test client 1", modified: (now - OneMinuteInMilliseconds), type: "mobile", formfactor: "largetablet", os: "iOS")
+        let client2 = RemoteClient(guid: client2GUID, name: "Test client 2", modified: (now - OneHourInMilliseconds), type: "desktop", formfactor: "laptop", os: "Darwin")
+
+        // Tabs are ordered most-recent-first.
+        self.clientsAndTabs = [ClientAndTabs(client: client1, tabs: [tab11, tab12]), ClientAndTabs(client: client2, tabs: [tab22, tab21])]
+    }
+
+    public func wipeClients() -> Deferred<Result<()>> {
+        return Deferred(value: Result(success: ()))
+    }
+
+    public func wipeTabs() -> Deferred<Result<()>> {
+        return Deferred(value: Result(success: ()))
+    }
+
+    public func insertOrUpdateClient(client: RemoteClient) -> Deferred<Result<()>> {
+        return Deferred(value: Result(success: ()))
+    }
+
+    public func insertOrUpdateTabsForClient(client: String, tabs: [RemoteTab]) -> Deferred<Result<Int>> {
+        return Deferred(value: Result(success: -1))
+    }
+
+    public func getClientsAndTabs() -> Deferred<Result<[ClientAndTabs]>> {
+        return Deferred(value: Result(success: self.clientsAndTabs))
+    }
+
+    public func getClients() -> Deferred<Result<[RemoteClient]>> {
+        return Deferred(value: Result(success: self.clientsAndTabs.map { $0.client }))
+    }
+}
+
 
 func byGUID(a: ClientAndTabs, b: ClientAndTabs) -> Bool {
     return a.client.guid < b.client.guid
