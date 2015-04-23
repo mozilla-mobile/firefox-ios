2c2d82523ce549fa447e5a598a09e7446a9d32e7
Richard Newman <rnewman@twinql.com>
2015-04-22 20:31:19 -0700

    Bug 1157553 - Pre: split Synchronizer into separate files. Add HistorySynchronizer.

---
+++ Sync/Synchronizers/Synchronizer.swift
@@ -0,0 +1,98 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import Foundation
+import Shared
+import Storage
+import XCGLogger
+
+// TODO: same comment as for SyncAuthState.swift!
+private let log = XCGLogger.defaultInstance()
+
+public typealias Success = Deferred<Result<()>>
+
+func succeed() -> Success {
+    return deferResult(())
+}
+
+/**
+ * This exists to pass in external context: e.g., the UIApplication can
+ * expose notification functionality in this way.
+ */
+public protocol SyncDelegate {
+    func displaySentTabForURL(URL: NSURL, title: String)
+    // TODO: storage.
+}
+
+// TODO: return values?
+/**
+ * A Synchronizer is (unavoidably) entirely in charge of what it does within a sync.
+ * For example, it might make incremental progress in building a local cache of remote records, never actually performing an upload or modifying local storage.
+ * It might only upload data. Etc.
+ *
+ * Eventually I envision an intent-like approach, or additional methods, to specify preferences and constraints
+ * (e.g., "do what you can in a few seconds", or "do a full sync, no matter how long it takes"), but that'll come in time.
+ *
+ * A Synchronizer is a two-stage beast. It needs to support synchronization, of course; that
+ * needs a completely configured client, which can only be obtained from Ready. But it also
+ * needs to be able to do certain things beforehand:
+ *
+ * * Wipe its collections from the server (presumably via a delegate from the state machine).
+ * * Prepare to sync from scratch ("reset") in response to a changed set of keys, syncID, or node assignment.
+ * * Wipe local storage ("wipeClient").
+ *
+ * Those imply that some kind of 'Synchronizer' exists throughout the state machine. We *could*
+ * pickle instructions for eventual delivery next time one is made and synchronized…
+ */
+public protocol Synchronizer {
+    init(scratchpad: Scratchpad, delegate: SyncDelegate, basePrefs: Prefs)
+    //func synchronize(client: Sync15StorageClient, info: InfoCollections) -> Deferred<Result<Scratchpad>>
+}
+
+public class FatalError: SyncError {
+    let message: String
+    init(message: String) {
+        self.message = message
+    }
+
+    public var description: String {
+        return self.message
+    }
+}
+
+public protocol SingleCollectionSynchronizer {
+    func remoteHasChanges(info: InfoCollections) -> Bool
+}
+
+public class BaseSingleCollectionSynchronizer: SingleCollectionSynchronizer {
+    let collection: String
+
+    let scratchpad: Scratchpad
+    let delegate: SyncDelegate
+    let prefs: Prefs
+
+    init(scratchpad: Scratchpad, delegate: SyncDelegate, basePrefs: Prefs, collection: String) {
+        self.scratchpad = scratchpad
+        self.delegate = delegate
+        self.collection = collection
+        let branchName = "synchronizer." + collection + "."
+        self.prefs = basePrefs.branch(branchName)
+
+        log.info("Synchronizer configured with prefs \(branchName).")
+    }
+
+    var lastFetched: Timestamp {
+        set(value) {
+            self.prefs.setLong(value, forKey: "lastFetched")
+        }
+
+        get {
+            return self.prefs.unsignedLongForKey("lastFetched") ?? 0
+        }
+    }
+
+    public func remoteHasChanges(info: InfoCollections) -> Bool {
+        return info.modified(self.collection) > self.lastFetched
+    }
+}
---
+++ Sync/Synchronizers/TabsSynchronizer.swift
@@ -0,0 +1,75 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import Foundation
+import Shared
+import Storage
+import XCGLogger
+
+// TODO: same comment as for SyncAuthState.swift!
+private let log = XCGLogger.defaultInstance()
+
+public class TabsSynchronizer: BaseSingleCollectionSynchronizer, Synchronizer {
+    public required init(scratchpad: Scratchpad, delegate: SyncDelegate, basePrefs: Prefs) {
+        super.init(scratchpad: scratchpad, delegate: delegate, basePrefs: basePrefs, collection: "tabs")
+    }
+
+    public func synchronizeLocalTabs(localTabs: RemoteClientsAndTabs, withServer storageClient: Sync15StorageClient, info: InfoCollections) -> Success {
+        func onResponseReceived(response: StorageResponse<[Record<TabsPayload>]>) -> Success {
+
+            func afterWipe() -> Success {
+                log.info("Fetching tabs.")
+                func doInsert(record: Record<TabsPayload>) -> Deferred<Result<(Int)>> {
+                    let remotes = record.payload.remoteTabs
+                    log.info("Inserting \(remotes.count) tabs for client \(record.id).")
+                    return localTabs.insertOrUpdateTabsForClientGUID(record.id, tabs: remotes)
+                }
+
+                // TODO: decide whether to upload ours.
+                let ourGUID = self.scratchpad.clientGUID
+                let records = response.value
+                let responseTimestamp = response.metadata.lastModifiedMilliseconds
+
+                log.debug("Got \(records.count) tab records.")
+
+                let allDone = all(records.filter({ $0.id != ourGUID }).map(doInsert))
+                return allDone.bind { (results) -> Success in
+                    if let failure = find(results, { $0.isFailure }) {
+                        return deferResult(failure.failureValue!)
+                    }
+
+                    self.lastFetched = responseTimestamp!
+                    return succeed()
+                }
+            }
+
+            // If this is a fresh start, do a wipe.
+            if self.lastFetched == 0 {
+                log.info("Last fetch was 0. Wiping tabs.")
+                return localTabs.wipeTabs()
+                    >>== afterWipe
+            }
+
+            return afterWipe()
+        }
+
+        if !self.remoteHasChanges(info) {
+            // Nothing to do.
+            // TODO: upload local tabs if they've changed or we're in a fresh start.
+            return succeed()
+        }
+
+        let keys = self.scratchpad.keys?.value
+        let encoder = RecordEncoder<TabsPayload>(decode: { TabsPayload($0) }, encode: { $0 })
+        if let encrypter = keys?.encrypter(self.collection, encoder: encoder) {
+            let tabsClient = storageClient.clientForCollection(self.collection, encrypter: encrypter)
+
+            return tabsClient.getSince(self.lastFetched)
+                >>== onResponseReceived
+        }
+
+        log.error("Couldn't make tabs factory.")
+        return deferResult(FatalError(message: "Couldn't make tabs factory."))
+    }
+}
