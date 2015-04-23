dfff8fb0f761a090085ca041264c992141b79c4f
Richard Newman <rnewman@twinql.com>
2015-04-22 20:45:55 -0700

    Bug 1163273 - Part 0: add HistoryPayload.swift.

---
+++ Sync/HistoryPayload.swift
@@ -0,0 +1,66 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import Foundation
+import Shared
+import Storage
+
+public class HistoryPayload: CleartextPayloadJSON {
+    public class func fromJSON(json: JSON) -> HistoryPayload? {
+        let p = HistoryPayload(json)
+        if p.isValid() {
+            return p
+        }
+        return nil
+    }
+
+    override public func isValid() -> Bool {
+        // We should also call super.isValid(), but that'll fail:
+        // Global is external, but doesn't have external or weak linkage!
+        // Swift compiler bug #18422804.
+        return !isError &&
+               self["histUri"].isString &&      // TODO: validate URI.
+               self["title"].isString &&
+               self["visits"].isArray
+    }
+
+    public func asPlaceWithModifiedTime(modified: Timestamp) -> RemotePlace {
+        return RemotePlace(guid: self.id, url: self.historyURI.absoluteString!, title: self.title, modified: modified)
+    }
+
+    var visits: [Visit] {
+        return optFilter(self["visits"].asArray!.map(Visit.fromJSON))
+    }
+
+    var historyURI: NSURL {
+        return self["histUri"].asString!.asURL!
+    }
+
+    var title: String {
+        return self["title"].asString!
+    }
+
+    override public func equalPayloads(obj: CleartextPayloadJSON) -> Bool {
+        if !(obj is HistoryPayload) {
+            return false;
+        }
+
+        if !super.equalPayloads(obj) {
+            return false;
+        }
+
+        let p = obj as! HistoryPayload
+        if p.title != self.title {
+            return false
+        }
+
+        if p.historyURI != self.historyURI {
+            return false
+        }
+
+        // TODO: compare visits.
+
+        return true
+    }
+}
