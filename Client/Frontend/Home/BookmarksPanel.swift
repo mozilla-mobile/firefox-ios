0023aa25c3277154b0ff2182efe921556e0a1dd2
Wes Johnston <wjohnston@mozilla.com>
2015-04-23 21:55:01 -0700

    Bug 1158020 - Allow deleting bookmarks from the UI.

--- Client/Frontend/Home/BookmarksPanel.swift
+++ Client/Frontend/Home/BookmarksPanel.swift
@@ -92,4 +92,33 @@ class BookmarksPanel: SiteTableViewController, HomePanel {
             }
         }
     }
+
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        // Intentionally blank. Required to use UITableViewRowActions
+    }
+
+    func tableView(tableView: UITableView, editActionsForRowAtIndexPath indexPath: NSIndexPath) -> [AnyObject]? {
+        let title = NSLocalizedString("Delete", tableName: "BookmarkPanel", comment: "Action button for deleting bookmarks in the bookmarks panel.")
+
+        let delete = UITableViewRowAction(style: UITableViewRowActionStyle.Default, title: title, handler: { (action, indexPath) in
+            if let bookmark = self.source?.current[indexPath.row] {
+                // Why the dispatches? Because we call success and failure on the DB
+                // queue, and so calling anything else that calls through to the DB will
+                // deadlock. This problem will go away when the bookmarks API switches to
+                // Deferred instead of using callbacks.
+                self.profile.bookmarks.remove(bookmark, success: { success in
+                    dispatch_async(dispatch_get_main_queue()) {
+                        self.source?.reloadData({ model in
+                            dispatch_async(dispatch_get_main_queue()) {
+                                self.source = model
+                                self.tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Left)
+                            }
+                        }, failure: self.onModelFailure)
+                    }
+                }, failure: self.onModelFailure)
+            }
+        })
+
+        return [delete]
+    }
 }
