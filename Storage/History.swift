7a2abed4d01adc6d0cefc8dd33b845865729ab76
Richard Newman <rnewman@twinql.com>
2015-04-27 11:10:06 -0700

    Bug 1157553 - Pre: add the beginnings of a SyncableHistory protocol.

--- Storage/History.swift
+++ Storage/History.swift
@@ -2,6 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+import Shared
+
+public typealias GUID = String
 
 /**
  * The base history protocol for front-end code.
@@ -14,4 +17,16 @@ public protocol BrowserHistory {
     func clear(complete: (success: Bool) -> Void)
     func get(options: QueryOptions?, complete: (data: Cursor) -> Void)
     func addVisit(visit: Visit, complete: (success: Bool) -> Void)
+}
+
+/**
+ * The interface that history storage needs to provide in order to be
+ * synced by a `HistorySynchronizer`.
+ */
+public protocol SyncableHistory {
+    func ensurePlaceWithURL(url: String, hasGUID guid: GUID) -> Deferred<Result<()>>
+    func changeGUID(old: GUID, new: GUID) -> Deferred<Result<()>>
+
+    func insertOrReplaceRemoteVisits(visits: [Visit], forGUID guid: GUID) -> Deferred<Result<()>>
+    func insertOrUpdatePlaceWithURL(url: String, title: String, guid: GUID) -> Deferred<Result<GUID>>
 }
