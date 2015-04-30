31587f275d115f938918b2c359699f66dbade8cf
Richard Newman <rnewman@twinql.com>
2015-04-30 10:00:25 -0700

    Bring SQLiteHistory back from the dead.

--- Storage/SQL/SQLiteHistory.swift
+++ Storage/SQL/SQLiteHistory.swift
@@ -21,17 +21,22 @@ func failOrSucceed(err: NSError?, op: String) -> Success {
     return failOrSucceed(err, op, ())
 }
 
+func getMicrosecondFrecencySQL(visitDateColumn: String, visitCountColumn: String) -> String {
+    let now = NSDate().timeIntervalSince1970
+    let age = "(\(now) - (\(visitDateColumn) / 1000)) / 86400"
+    return "\(visitCountColumn) * MAX(1, 100 * 225 / (\(age) * \(age) + 225))"
+}
+
 /**
  * The sqlite-backed implementation of the history protocol.
  */
 public class SQLiteHistory: BrowserHistory {
     let db: BrowserDB
-    private let table = JoinedHistoryVisitsTable()
     private var ignoredSchemes = ["about"]
 
     required public init(db: BrowserDB) {
         self.db = db
-        db.createOrUpdate(table)
+        db.createOrUpdate(BrowserTable())
     }
 
     public func clear() -> Success {
@@ -40,55 +45,14 @@ public class SQLiteHistory: BrowserHistory {
 
         // TODO: this should happen asynchronously.
         db.withWritableConnection(&err) { (conn, inout err: NSError?) -> Int in
-            return self.table.delete(conn, item: nil, err: &err)
+            err = conn.executeChange("DELETE FROM visits", withArgs: nil)
+            if err == nil {
+                err = conn.executeChange("DELETE FROM history", withArgs: nil)
+            }
+            return 1
         }
 
         return failOrSucceed(err, "Clear")
-    }
-
-    public class WrappedCursor : Cursor {
-        private let cursor: Cursor
-        override public var count: Int {
-            get { return cursor.count }
-        }
-
-        init(cursor: Cursor) {
-            self.cursor = cursor
-            super.init(status: cursor.status, msg: cursor.statusMessage)
-        }
-
-        // Collection iteration and access functions
-        public override subscript(index: Int) -> Any? {
-            get {
-                if let (site, visit) = cursor[index] as? (Site, Visit) {
-                    return site
-                }
-                return nil
-            }
-        }
-
-        public override func close() {
-            cursor.close()
-        }
-    }
-
-    public func get(options: QueryOptions?) -> Deferred<Result<Cursor>> {
-        var err: NSError? = nil
-        let res = db.withReadableConnection(&err) { (connection, inout err: NSError?) -> Cursor in
-            return WrappedCursor(cursor: self.table.query(connection, options: options))
-        }
-
-        if let err = err {
-            log.debug("Query failed: \(err.localizedDescription)")
-            return deferResult(DatabaseError(err: err))
-        }
-
-        if res.status != .Success {
-            log.warning("Got cursor but status != Success: \(res.statusMessage).")
-            return deferResult(DatabaseError(err: nil))
-        }
-
-        return deferResult(res)
     }
 
     private func shouldAdd(url: String) -> Bool {
@@ -103,34 +67,129 @@ public class SQLiteHistory: BrowserHistory {
         return true
     }
 
-    public func addVisit(visit: SiteVisit) -> Success {
+    private func recordVisitedSite(site: Site) -> Success {
         var err: NSError? = nil
 
         // Don't store visits to sites with about: protocols
-        if !shouldAdd(visit.site.url) {
+        if !shouldAdd(site.url) {
             return deferResult(IgnoredSiteError())
         }
 
         // TODO: at this point we need to 'shadow' the mirrored site, if the
         // remote is still authoritative.
         // For now, we just update-or-insert on our one and only table.
+        // TODO: also set modified times.
         db.withWritableConnection(&err) { (conn, inout err: NSError?) -> Int in
-            // TODO
-            return 0
+            let update = "UPDATE history SET title = ? WHERE url = ?"
+            let updateArgs: [AnyObject?]? = [site.title, site.url]
+            err = conn.executeChange(update, withArgs: updateArgs)
+            if err != nil {
+                return 0
+            }
+            if conn.numberOfRowsModified > 0 {
+                return conn.numberOfRowsModified
+            }
+
+            // Insert instead.
+            let insert = "INSERT INTO history (guid, url, title) VALUES (?, ?, ?)"
+            let insertArgs: [AnyObject?]? = [Bytes.generateGUID(), site.url, site.title]
+            err = conn.executeChange(insert, withArgs: insertArgs)
+            if err != nil {
+                return 0
+            }
+            return 1
         }
 
-        return failOrSucceed(err, "Add")
+        return failOrSucceed(err, "Record site")
     }
 
-    public func getByFrecencyWithLimit(limit: Int) -> Deferred<Result<Cursor<Site>>> {
-        return deferResult(DatabaseError(err: nil))
+    private func addLocalVisitForExistingSite(visit: SiteVisit) -> Success {
+        var err: NSError? = nil
+        db.withWritableConnection(&err) { (conn, inout err: NSError?) -> Int in
+            let insert = "INSERT INTO visits (siteID, date, type) VALUES (" +
+                         "(SELECT id FROM history WHERE url = ?), ?, ?)"
+            let realDate = NSNumber(unsignedLongLong: visit.date)
+            let insertArgs: [AnyObject?]? = [visit.site.url, realDate, visit.type.rawValue]
+            err = conn.executeChange(insert, withArgs: insertArgs)
+            if err != nil {
+                return 0
+            }
+            return 1
+        }
+
+        return failOrSucceed(err, "Record visit")
     }
 
-    public func getByFrecencyWithLimit(limit: Int, whereURLContains filter: String) -> Deferred<Result<Cursor<Site>>> {
-        return deferResult(DatabaseError(err: nil))
+    public func addLocalVisit(visit: SiteVisit) -> Success {
+        return recordVisitedSite(visit.site)
+         >>> { self.addLocalVisitForExistingSite(visit) }
     }
 
-    public func getByLastVisit(limit: Int) -> Deferred<Result<Cursor<Site>>> {
-        return deferResult(DatabaseError(err: nil))
+    public func getSitesByFrecencyWithLimit(limit: Int) -> Deferred<Result<Cursor<Site>>> {
+        let frecencySQL = getMicrosecondFrecencySQL("visitDate", "visitCount")
+        let orderBy = "ORDER BY \(frecencySQL) DESC "
+        return self.getFilteredSitesWithLimit(limit, whereURLContains: nil, orderBy: orderBy)
+    }
+
+    public func getSitesByFrecencyWithLimit(limit: Int, whereURLContains filter: String) -> Deferred<Result<Cursor<Site>>> {
+        let frecencySQL = getMicrosecondFrecencySQL("visitDate", "visitCount")
+        let orderBy = "ORDER BY \(frecencySQL) DESC "
+        return self.getFilteredSitesWithLimit(limit, whereURLContains: filter, orderBy: orderBy)
+    }
+
+    public func getSitesByLastVisit(limit: Int) -> Deferred<Result<Cursor<Site>>> {
+        let orderBy = "ORDER BY visitDate DESC "
+        return self.getFilteredSitesWithLimit(limit, whereURLContains: nil, orderBy: orderBy)
+    }
+
+    private class func basicHistoryColumnFactory(row: SDRow) -> Site {
+        let id = row["historyID"] as! Int
+        let url = row["url"] as! String
+        let title = row["title"] as! String
+        let guid = row["guid"] as! String
+
+        let site = Site(url: url, title: title)
+        site.guid = guid
+        site.id = id
+
+        if let visitDate = (row["visitDate"] as? NSNumber)?.unsignedLongLongValue {
+            site.latestVisit = Visit(date: visitDate, type: VisitType.Unknown)
+        }
+
+        return site
+    }
+
+    private func getFilteredSitesWithLimit(limit: Int, whereURLContains filter: String?, orderBy: String) -> Deferred<Result<Cursor<Site>>> {
+
+        let args: [AnyObject?]?
+        let whereClause: String
+        if let filter = filter {
+            args = ["%\(filter)%", "%\(filter)%"]
+            whereClause = " AND ((history.url LIKE ?) OR (history.title LIKE ?)) "
+        } else {
+            args = []
+            whereClause = " "
+        }
+
+        let sql =
+        "SELECT history.id AS historyID, history.url AS url, title, guid, " +
+        "max(visits.date) AS visitDate, " +
+        "count(visits.id) AS visitCount " +
+        "FROM history, visits " +
+        "WHERE history.id = visits.siteID " +
+        whereClause +
+        "GROUP BY history.id " +
+        orderBy
+        "LIMIT \(limit) "
+
+        let factory = SQLiteHistory.basicHistoryColumnFactory
+        func runQuery(conn: SQLiteDBConnection, inout err: NSError?) -> Cursor<Site> {
+            return conn.executeQuery(sql, factory: factory, withArgs: args)
+        }
+
+        var err: NSError? = nil
+        let cursor = db.withReadableConnection(&err, callback: runQuery)
+
+        return deferResult(cursor)
     }
 }
