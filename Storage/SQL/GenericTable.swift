2b0517c18c928017c3b89c5ab24e6c7b99e46c76
Richard Newman <rnewman@twinql.com>
2015-05-06 10:34:01 -0700

    Bug 1128621 - Part 3c: generic cursors in GenericTable.

--- Storage/SQL/GenericTable.swift
+++ Storage/SQL/GenericTable.swift
@@ -125,7 +125,7 @@ class GenericTable<T>: Table {
         return 0
     }
 
-    func query(db: SQLiteDBConnection, options: QueryOptions?) -> Cursor {
+    func query(db: SQLiteDBConnection, options: QueryOptions?) -> Cursor<T> {
         if var (query, args) = getQueryAndArgs(options) {
             if let factory = self.factory {
                 let c =  db.executeQuery(query, factory: factory, withArgs: args)
