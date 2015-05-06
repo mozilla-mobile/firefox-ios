c2d402e525768b53c73f989d4bea700e81beea70
Wes Johnston <wjohnston@mozilla.com>
2015-05-05 18:47:20 -0700

    Add NSString encryption functions

--- Storage/Passwords.swift
+++ Storage/Passwords.swift
@@ -161,3 +161,62 @@ public class MockPasswords : Passwords {
         complete(success: true)
     }
 }
+
+// TODO: Move this to Utils
+extension NSString {
+    private func encrypt(operation: Int, key: NSString, data: NSData) -> NSData? {
+        let keyData: NSData! = (key as NSString).dataUsingEncoding(NSUTF8StringEncoding) as NSData!
+        let keyBytes         = UnsafePointer<Void>(keyData.bytes)
+
+        let dataLength    = data.length
+        let dataBytes     = UnsafePointer<Void>(data.bytes)
+
+        let cryptData    = NSMutableData(length: Int(dataLength) + kCCBlockSizeAES128)
+        var cryptPointer = UnsafeMutablePointer<Void>(cryptData!.mutableBytes)
+        let cryptLength  = cryptData!.length
+
+        let keyLength              = kCCKeySizeAES256
+        let operation: CCOperation = UInt32(operation)
+        let algoritm:  CCAlgorithm = UInt32(kCCAlgorithmAES128)
+        let options:   CCOptions   = UInt32(kCCOptionECBMode + kCCOptionPKCS7Padding)
+
+        var numBytesEncrypted: Int = 0
+        let iv = UnsafePointer<Void>()
+
+        var cryptStatus = CCCrypt(operation,
+            algoritm,
+            options,
+            keyBytes, keyLength,
+            iv,
+            dataBytes, dataLength,
+            cryptPointer, cryptLength,
+            &numBytesEncrypted)
+
+        let success: CCCryptorStatus = Int32(kCCSuccess)
+        if cryptStatus == success {
+            return cryptData!.subdataWithRange(NSRange(location: 0, length: numBytesEncrypted))
+        }
+        
+        return nil;
+    }
+
+    func AES128EncryptWithKey(keyString: NSString) -> NSString? {
+        let data: NSData! = dataUsingEncoding(NSUTF8StringEncoding) as NSData!
+        if let encrypted = encrypt(kCCEncrypt, key: keyString, data: data) {
+            println("Encrypting \(self) as \(encrypted.base64EncodedString)")
+            return encrypted.base64EncodedString
+        }
+
+        return nil;
+    }
+
+    func AES128DecryptWithKey(keyString: NSString) -> NSString? {
+        let data: NSData! = NSData(base64EncodedString: self as String, options: NSDataBase64DecodingOptions())
+        if let decrypted = encrypt(kCCDecrypt, key: keyString, data: data) {
+            println("Decrypting \(self) as \(NSString(data: decrypted, encoding: NSUTF8StringEncoding))")
+            return NSString(data: decrypted, encoding: NSUTF8StringEncoding)
+        }
+
+        return nil;
+    }
+}
