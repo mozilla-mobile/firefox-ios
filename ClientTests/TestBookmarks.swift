c222347af5de5e8eb2281547cd08c9738afe5a56
Richard Newman <rnewman@twinql.com>
2015-05-06 09:47:45 -0700

    Check for test bookmarks in Mobile Bookmarks.

--- ClientTests/TestBookmarks.swift
+++ ClientTests/TestBookmarks.swift
@@ -15,8 +15,8 @@ class TestBookmarks : ProfileTest {
             }
 
             let expectation = self.expectationWithDescription("asynchronous request")
-            profile.bookmarks.modelForRoot({ (model: BookmarksModel) in
-                XCTAssertEqual(model.current.count, 11, "We create \(model.current.count) stub bookmarks.")
+            profile.bookmarks.modelForFolder(BookmarkRoots.MobileFolderGUID, success: { (model: BookmarksModel) in
+                XCTAssertEqual(model.current.count, 11, "We create \(model.current.count) stub bookmarks in the Mobile Bookmarks folder.")
                 let bookmark = model.current[0]
                 XCTAssertTrue(bookmark is BookmarkItem)
                 XCTAssertEqual((bookmark as! BookmarkItem).url, "http://www.example.com/0", "Example URL found.")
