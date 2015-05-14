e7b8e67f0316b5e7b1d2be4cf97ab9004ab67337
Wes Johnston <wjohnston@mozilla.com>
2015-05-14 14:50:39 -0700

    Fixup styling on tablets

--- Client/Frontend/Browser/ErrorPageHelper.swift
+++ Client/Frontend/Browser/ErrorPageHelper.swift
@@ -1,10 +1,6 @@
-//
-//  ErrorPageHelper.swift
-//  Client
-//
-//  Created by Wes Johnston on 5/13/15.
-//  Copyright (c) 2015 Mozilla. All rights reserved.
-//
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import Foundation
 import WebKit
