118ab723bd66ec4ae4ad6e17aa8d98f2b721f4a6
Wes Johnston <wjohnston@mozilla.com>
2015-05-22 10:40:28 -0700

    Use encryption for BrowserDB

--- Storage/SQL/BrowserDB.swift
+++ Storage/SQL/BrowserDB.swift
@@ -38,6 +38,17 @@ public class QueryOptions {
 }
 
 let DBCouldNotOpenErrorCode = 200
+private var secretKey: String? {
+    let key = "sqlcipher.key.browser.db"
+    if KeychainWrapper.hasValueForKey(key) {
+        return KeychainWrapper.stringForKey(key)
+    }
+
+    let Length: UInt = 256
+    let secret = Bytes.generateRandomBytes(Length).base64EncodedString
+    KeychainWrapper.setString(secret, forKey: key)
+    return secret
+}
 
 /* This is a base interface into our browser db. It holds arrays of tables and handles basic creation/updating of them. */
 // Version 1 - Basic history table.
@@ -47,8 +58,9 @@ let DBCouldNotOpenErrorCode = 200
 // Version 5 - Added the clients and the tabs tables.
 // Version 6 - Visit timestamps are now microseconds.
 // Version 7 - Eliminate most tables.
+
 public class BrowserDB {
-    private let db: SwiftData
+    private var db: SwiftData
     // XXX: Increasing this should blow away old history, since we currently don't support any upgrades.
     private let Version: Int = 7
     private let FileName = "browser.db"
@@ -64,13 +76,16 @@ public class BrowserDB {
     public init(files: FileAccessor) {
         log.debug("Initializing BrowserDB.")
         self.files = files
-        db = SwiftData(filename: files.getAndEnsureDirectory()!.stringByAppendingPathComponent(FileName))
         self.schemaTable = SchemaTable()
-        self.createOrUpdate(self.schemaTable)
-    }
 
-    var filename: String {
-        return db.filename
+        // Create and open the db file
+        let secret = secretKey
+        assert(secret != nil, "Database must be encrypted")
+        let file = files.getAndEnsureDirectory()!.stringByAppendingPathComponent(FileName)
+        db = SwiftData(filename: file, key: secret, prevKey: nil)
+
+        // Create or update will also delete and create the database if our key was incorrect.
+        self.createOrUpdate(self.schemaTable)
     }
 
     // Creates a table and writes its table info into the table-table database.
@@ -121,9 +136,10 @@ public class BrowserDB {
     // Utility for table classes. They should call this when they're initialized to force
     // creation of the table in the database.
     func createOrUpdate<T: Table>(table: T) -> Bool {
-        log.debug("Create or update \(table.name) version \(table.version).")
         var success = true
-        db.transaction({ connection -> Bool in
+        // Just trying to get a connection may fail, so we catch errors here to
+        // ensure we detect those types of failures.
+        if let err = db.transaction({ connection -> Bool in
             // If the table doesn't exist, we'll create it
             if !table.exists(connection) {
                 success = self.createTable(connection, table: table)
@@ -141,26 +157,45 @@ public class BrowserDB {
                 }
             }
 
-            // If we failed, move the file and try again. This will probably break things that are already
-            // attached and expecting a working DB, but at least we should be able to restart.
-            if !success {
-                log.debug("Couldn't create or update \(table.name).")
-                log.debug("Attempting to move \(self.FileName) to another location.")
-
-                // Note that a backup file might already exist! We append a counter to avoid this.
-                var bakCounter = 0
-                var bak: String
-                do {
-                    bak = "\(self.FileName).bak.\(++bakCounter)"
-                } while self.files.exists(bak)
-
-                success = self.files.move(self.FileName, toRelativePath: bak)
-                assert(success)
-                success = self.createTable(connection, table: table)
-            }
             return success
-        })
+        }) {
+            success = false // Err in transaction.
+        }
 
+        // If we failed, move the file and try again. This will probably break things that are already
+        // attached and expecting a working DB, but at least we should be able to restart.
+        if !success {
+            log.debug("Couldn't create or update \(table.name).")
+            backupCurrentDB()
+
+            // Now we create a brand new db connection.
+            db = SwiftData(filename: files.getAndEnsureDirectory()!.stringByAppendingPathComponent(self.FileName), key: secretKey)
+            success = true
+            if let err = db.transaction({ connection -> Bool in
+                success = self.createTable(connection, table: table)
+                return success
+            }) {
+                success = false
+            }
+        }
+
+        return success
+    }
+
+    private func backupCurrentDB() -> Bool {
+        log.debug("Attempting to move \(self.FileName) to another location.")
+
+        // Note that a backup file might already exist! We append a counter to avoid this.
+        var bakCounter = 0
+        var bak: String
+        do {
+            bak = "\(self.FileName).bak.\(++bakCounter)"
+        } while self.files.exists(bak)
+
+        // Make sure we close any open connections before moving the file.
+        db.close()
+        let success = self.files.move(self.FileName, toRelativePath: bak)
+        assert(success)
         return success
     }
 
@@ -168,7 +203,7 @@ public class BrowserDB {
 
     func withConnection<T>(#flags: SwiftData.Flags, inout err: NSError?, callback: (connection: SQLiteDBConnection, inout err: NSError?) -> T) -> T {
         var res: T!
-        db.withConnection(flags) { connection in
+        err = db.withConnection(flags) { connection in
             var err: NSError? = nil
             res = callback(connection: connection, err: &err)
             return err
@@ -184,9 +219,8 @@ public class BrowserDB {
         return withConnection(flags: SwiftData.Flags.ReadOnly, err: &err, callback: callback)
     }
 
-    func transaction(inout err: NSError?, callback: (connection: SQLiteDBConnection, inout err: NSError?) -> Bool) {
-        db.transaction { connection in
-            var err: NSError? = nil
+    func transaction(inout err: NSError?, callback: (connection: SQLiteDBConnection, inout err: NSError?) -> Bool) -> NSError? {
+        return db.transaction { connection in
             return callback(connection: connection, err: &err)
         }
     }
