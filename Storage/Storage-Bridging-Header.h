5a757bbce2d331f5c17b396517881fbf3ad4ff3c
Wes Johnston <wjohnston@mozilla.com>
2015-05-22 10:39:44 -0700

    Add encryption methods to SwiftData

--- Storage/Storage-Bridging-Header.h
+++ Storage/Storage-Bridging-Header.h
@@ -1,5 +1,6 @@
 #ifndef Client_Storage_Bridging_Header_h
 #define Client_Storage_Bridging_Header_h
+#define SQLITE_HAS_CODEC
 
 #import <Foundation/Foundation.h>
 #import "../Carthage/Checkouts/sqlcipher/sqlite3.h"
