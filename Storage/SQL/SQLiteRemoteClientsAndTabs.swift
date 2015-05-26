a5e62ccffddc7c6cfeca9a8bfc49c743b3ed23bd
Richard Newman <rnewman@twinql.com>
2015-05-26 15:55:50 -0700

    Pointer to Bug 1178690 - delete client and tabs records after account unlink.

--- Storage/SQL/SQLiteRemoteClientsAndTabs.swift
+++ Storage/SQL/SQLiteRemoteClientsAndTabs.swift
@@ -222,6 +222,7 @@ public class SQLiteRemoteClientsAndTabs: RemoteClientsAndTabs {
 
     public func onRemovedAccount() -> Success {
         log.info("Clearing clients and tabs after account removal.")
+        // TODO: Bug 1168690 - delete our client and tabs records from the server.
         return self.clear()
     }
 
