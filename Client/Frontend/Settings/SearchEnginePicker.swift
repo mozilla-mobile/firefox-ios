44123e6db763ca2e62ebb64082cda5110c4e79c4
Brian Nicholson <bnicholson@mozilla.com>
2015-06-02 17:35:48 -0700

    Bug 1170856 - Fix search image alignment in default picker

--- Client/Frontend/Settings/SearchEnginePicker.swift
+++ Client/Frontend/Settings/SearchEnginePicker.swift
@@ -28,7 +28,7 @@ class SearchEnginePicker: UITableViewController {
         let engine = engines[indexPath.item]
         let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: nil)
         cell.textLabel?.text = engine.shortName
-        cell.imageView?.image = engine.image
+        cell.imageView?.image = engine.image?.createScaled(CGSize(width: OpenSearchEngine.PreferredIconSize, height: OpenSearchEngine.PreferredIconSize))
         if engine.shortName == selectedSearchEngineName {
             cell.accessoryType = UITableViewCellAccessoryType.Checkmark
         }
