951f29834e623124b35d9a7a5e69907c534789ff
Wes Johnston <wjohnston@mozilla.com>
2015-06-04 15:37:58 -0700

    Update passwords to new login interface

--- Storage/Passwords.swift
+++ Storage/Passwords.swift
@@ -4,87 +4,127 @@
 
 import Foundation
 import WebKit
+import Shared
 
-public class Password : Equatable {
-    public let hostname: String
-    public let username: String
-    public let password: String
+/**
+ * Login is a wrapper around NSURLCredential and NSURLProtectionSpace to allow us to add extra fields where needed.
+ **/
+public protocol LoginData {
+    var credentials: NSURLCredential { get }
+    var protectionSpace: NSURLProtectionSpace { get }
+    var hostname: String { get }
+    var username: String? { get }
+    var password: String { get }
+    var httpRealm: String? { get set }
+    var formSubmitUrl: String? { get set }
+    var usernameField: String? { get set }
+    var passwordField: String? { get set }
 
-    public var httpRealm: String = ""
-    public var formSubmitUrl: String = "" {
+    func toDict() -> [String: String]
+}
+
+public protocol LoginUsageData: LoginData {
+    var timeCreated: MicrosecondTimestamp { get set }
+    var timeLastUsed: MicrosecondTimestamp { get set }
+    var timePasswordChanged: MicrosecondTimestamp { get set }
+}
+
+public protocol SyncableLoginData {
+    var guid: String? { get set }
+    var isDeleted: Bool { get set }
+}
+
+public class Login : Printable, SyncableLoginData, LoginUsageData, Equatable {
+    public let credentials: NSURLCredential
+    public let protectionSpace: NSURLProtectionSpace
+
+    public var hostname: String { return protectionSpace.host }
+    public var username: String? { return credentials.user }
+    public var password: String { return credentials.password! }
+    private var _httpRealm: String? = nil
+    public var usernameField: String? = nil
+    public var passwordField: String? = nil
+
+    public var httpRealm: String? {
+        get { return self._httpRealm ?? protectionSpace.realm }
+        set { self._httpRealm = newValue }
+    }
+
+    public var formSubmitUrl: String? = nil {
         didSet {
-            if self.formSubmitUrl == "" {
+            if self.formSubmitUrl == nil || self.formSubmitUrl!.isEmpty {
                 return
             }
 
             let url2 = NSURL(string: self.hostname)
-            let url1 = NSURL(string: self.formSubmitUrl)
+            let url1 = NSURL(string: self.formSubmitUrl!)
             if url1?.host != url2?.host {
-                formSubmitUrl = ""
+                assertionFailure("Form submit url domain doesn't match login's domain")
+                formSubmitUrl = nil
             }
         }
     }
-    public var usernameField: String = ""
-    public var passwordField: String = ""
 
-    var guid: String? = nil
-    var timeCreated = NSDate()
-    var timeLastUsed = NSDate()
-    var timePasswordChanged = NSDate()
-    var timesUsed = 0
+    // SyncableLoginData
+    public var guid: String? = nil
+    public var isDeleted: Bool = false
 
-    // var encryptedUsername: String { get }
-    // var encryptedPassword: String { get }
-    // var encType: String { get }
+    // UsageLignData
+    public var timeCreated = NSDate.nowMicroseconds()
+    public var timeLastUsed = NSDate.nowMicroseconds()
+    public var timePasswordChanged = NSDate.nowMicroseconds()
 
-    public init(hostname: String, username: String, password: String) {
-        self.hostname = hostname
-        self.username = username
-        self.password = password
+    // Printable
+    public var description: String {
+        return "Login: \(username) for \(hostname)"
     }
 
-    public init(credential: NSURLCredential?, protectionSpace: NSURLProtectionSpace) {
-        username = credential?.user ?? ""
-        password = credential?.password ?? ""
-        hostname = protectionSpace.host
-        httpRealm = protectionSpace.realm!
+    public class func createWith(hostname: String, username: String, password: String) -> LoginData {
+        return Login(hostname: hostname, username: username, password: password) as LoginData
     }
 
-    public var credential: NSURLCredential {
-        return NSURLCredential(user: username, password: password, persistence: .ForSession)
+    public class func createWith(credential: NSURLCredential, protectionSpace: NSURLProtectionSpace) -> LoginData {
+        return Login(credential: credential, protectionSpace: protectionSpace) as LoginData
     }
 
-    public var protectionSpace: NSURLProtectionSpace {
-        let url = NSURL(string: formSubmitUrl)!
-        return NSURLProtectionSpace(host: hostname, port: -1, `protocol`: url.scheme, realm: httpRealm, authenticationMethod: nil)
+    init(hostname: String, username: String, password: String) {
+        self.credentials = NSURLCredential(user: username, password: password, persistence: NSURLCredentialPersistence.None)
+        self.protectionSpace = NSURLProtectionSpace(host: hostname, port: 0, `protocol`: nil, realm: nil, authenticationMethod: nil)
+    }
+
+    init(credential: NSURLCredential, protectionSpace: NSURLProtectionSpace) {
+        self.credentials = credential
+        self.protectionSpace = protectionSpace
     }
 
     public func toDict() -> [String: String] {
-        return ["hostname": hostname,
-            "formSubmitURL": formSubmitUrl,
-            "httpReal": httpRealm,
-            "username": username,
+        return [
+            "hostname": hostname,
+            "formSubmitURL": formSubmitUrl ?? "",
+            "httpRealm": httpRealm ?? "",
+            "username": username ?? "",
             "password": password,
-            "usernameField": usernameField,
-            "passwordField": passwordField]
+            "usernameField": usernameField ?? "",
+            "passwordField": passwordField ?? ""
+        ]
     }
 
-    public class func fromScript(url: NSURL, script: [String: String]) -> Password {
-        let password = Password(hostname: getPasswordOrigin(url.absoluteString!)!, username: script["username"]!, password: script["password"]!)
+    public class func fromScript(url: NSURL, script: [String: String]) -> LoginData {
+        let login = Login(hostname: getPasswordOrigin(url.absoluteString!)!, username: script["username"]!, password: script["password"]!)
 
         if let formSubmit = script["formSubmitUrl"] {
-            password.formSubmitUrl = formSubmit
+            login.formSubmitUrl = formSubmit
         }
 
         if let passwordField = script["passwordField"] {
-            password.passwordField = passwordField
+            login.passwordField = passwordField
         }
 
         if let userField = script["usernameField"] {
-            password.usernameField = userField
+            login.usernameField = userField
         }
 
-        return password
+        return login as LoginData
     }
 
     private class func getPasswordOrigin(uriString: String, allowJS: Bool = false) -> String? {
@@ -111,51 +151,89 @@ public class Password : Equatable {
     }
 }
 
-public func ==(lhs: Password, rhs: Password) -> Bool {
-    return lhs.hostname == rhs.hostname && lhs.username == rhs.username && lhs.password == rhs.password
+public func ==(lhs: Login, rhs: Login) -> Bool {
+    return lhs.credentials == rhs.credentials && lhs.protectionSpace == rhs.protectionSpace
 }
 
-public protocol Passwords {
-    func get(options: QueryOptions, complete: (cursor: Cursor<Password>) -> Void)
-    func add(password: Password, complete: (success: Bool) -> Void)
-    func remove(password: Password, complete: (success: Bool) -> Void)
-    func removeAll(complete: (success: Bool) -> Void)
+public protocol Logins {
+    func getUsageDataForLogin(login: LoginData) -> Deferred<Result<LoginUsageData>>
+    func getLoginsForProtectionSpace(protectionSpace: NSURLProtectionSpace) -> Deferred<Result<Cursor<LoginData>>>
+    func addLogin(login: LoginData) -> Success
+    func updateLogin(login: LoginData) -> Success
+    func addVisitFor(login: LoginData) -> Success
+    func removeLogin(login: LoginData) -> Success
+    func removeAll() -> Success
 }
 
-public class MockPasswords : Passwords {
-    private var passwordsCache = [Password]()
-    private var files: FileAccessor
+public class LoginDataError: ErrorType {
+    public let description: String
+    public init(description: String) {
+        self.description = description
+    }
+}
+
+public class MockLogins : Logins {
+    private var cache = [Login]()
 
     public init(files: FileAccessor) {
-        self.files = files
-
-        let hostname = "https://m.facebook.com"
-        passwordsCache.append(Password(hostname: hostname, username: "FakeUser", password: "FakePassword"))
-        passwordsCache.append(Password(hostname: hostname, username: "Something", password: "else"))
     }
 
-    public func get(options: QueryOptions, complete: (cursor: Cursor<Password>) -> Void) {
-        dispatch_async(dispatch_get_main_queue()) { _ in
-            complete(cursor: ArrayCursor<Password>(data: self.passwordsCache))
+    public func getLoginsForProtectionSpace(protectionSpace: NSURLProtectionSpace) -> Deferred<Result<Cursor<LoginData>>> {
+        let cursor = ArrayCursor(data: cache.filter({ login in
+            return login.protectionSpace.host == protectionSpace.host
+        }).sorted({ (loginA, loginB) -> Bool in
+            return loginA.timeLastUsed > loginB.timeLastUsed
+        }).map({ login in
+            return login as LoginData
+        }))
+        return Deferred(value: Result(success: cursor))
+    }
+
+    // This method is only here for testing
+    public func getUsageDataForLogin(login: LoginData) -> Deferred<Result<LoginUsageData>> {
+        let res = cache.filter({ login in
+            return login.protectionSpace.host == login.hostname
+        }).sorted({ (loginA, loginB) -> Bool in
+            return loginA.timeLastUsed > loginB.timeLastUsed
+        })[0] as LoginUsageData
+
+        return Deferred(value: Result(success: res))
+    }
+
+    public func addLogin(login: LoginData) -> Success {
+        if let index = find(cache, login as! Login) {
+            return deferResult(LoginDataError(description: "Already in the cache"))
         }
+        cache.append(login as! Login)
+        return succeed()
     }
 
-    public func add(password: Password, complete: (success: Bool) -> Void) {
-        passwordsCache.append(password)
-        complete(success: true)
-    }
-
-    public func remove(password: Password, complete: (success: Bool) -> Void) {
-        if let index = find(passwordsCache, password) {
-            passwordsCache.removeAtIndex(index)
-            complete(success: true)
-            return
+    public func updateLogin(login: LoginData) -> Success {
+        if let index = find(cache, login as! Login) {
+            cache[index].timePasswordChanged = NSDate.nowMicroseconds()
+            return succeed()
         }
-        complete(success: false)
+        return deferResult(LoginDataError(description: "Password wasn't cached yet. Can't update"))
     }
 
-    public func removeAll(complete: (success: Bool) -> Void) {
-        passwordsCache.removeAll(keepCapacity: false)
-        complete(success: true)
+    public func addVisitFor(login: LoginData) -> Success {
+        if let index = find(cache, login as! Login) {
+            cache[index].timeLastUsed = NSDate.nowMicroseconds()
+            return succeed()
+        }
+        return deferResult(LoginDataError(description: "Password wasn't cached yet. Can't update"))
+    }
+
+    public func removeLogin(login: LoginData) -> Success {
+        if let index = find(cache, login as! Login) {
+            cache.removeAtIndex(index)
+            return succeed()
+        }
+        return deferResult(LoginDataError(description: "Can not remove a password that wasn't stored"))
+    }
+
+    public func removeAll() -> Success {
+        cache.removeAll(keepCapacity: false)
+        return succeed()
     }
 }
