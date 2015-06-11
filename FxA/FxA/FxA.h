ab8043273d78824cd5912dec4c28a9a6b66fc0c2
Richard Newman <rnewman@twinql.com>
2015-06-11 16:41:00 -0700

    Fix some compiler warnings in FxA umbrella header.

--- FxA/FxA/FxA.h
+++ FxA/FxA/FxA.h
@@ -22,4 +22,15 @@ FOUNDATION_EXPORT const unsigned char FxAVersionString[];
 #include <FxA/MockMyIDTokenFactory.h>
 #include <FxA/NSData+Base16.h>
 #include <FxA/NSData+Base32.h>
+#include <FxA/NSData+SHA.h>
+#include <FxA/NSData+Utils.h>
 #include <FxA/NSData+KeyDerivation.h>
+/*
+// These are all the ones the compiler complains are missing.
+#include "ASNUtils.h"
+#include "CHMath.h"
+#include "CHNumber.h"
+#include "CHNumber_Private.h"
+#include "CHMutableNumber.h"
+#include "CHUtils.h"
+*/
