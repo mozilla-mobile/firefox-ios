56e5b25f2f40ca1ca808a9dbe1fff0afc193953f
James Hugman <james@hugman.tv>
2015-06-19 16:53:17 +0100

    Bug 1171459 – Prevent screenshots and history of about pages.

--- Client/Frontend/Browser/BrowserViewController.swift
+++ Client/Frontend/Browser/BrowserViewController.swift
@@ -1410,7 +1410,7 @@ extension BrowserViewController: WKNavigationDelegate {
 
         tab.expireSnackbars()
 
-        if let url = webView.URL where !ErrorPageHelper.isErrorPageURL(url) {
+        if let url = webView.URL where !ErrorPageHelper.isErrorPageURL(url) && !AboutUtils.isAboutHomeURL(url) {
             let notificationCenter = NSNotificationCenter.defaultCenter()
             var info = [NSObject: AnyObject]()
             info["url"] = tab.displayURL
