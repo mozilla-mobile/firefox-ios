20ee0ed8b9229941f7709c50a91ceba9b9881077
Richard Newman <rnewman@twinql.com>
2015-07-28 15:02:58 -0700

    Bug 1188626 - Part 2: be a little more careful about FaviconFetcher response handling.

--- Client/Frontend/Home/TopSitesPanel.swift
+++ Client/Frontend/Home/TopSitesPanel.swift
@@ -398,16 +398,18 @@ private class TopSitesDataSource: NSObject, UICollectionViewDataSource {
         cell.imageView.image = nil
         cell.backgroundImage.image = nil
 
-        FaviconFetcher.getForUrl(site.url.asURL!, profile: profile) >>== { icons in
-            if (icons.count > 0) {
-                cell.imageView.sd_setImageWithURL(icons[0].url.asURL!) { (img, err, type, url) -> Void in
-                    if let img = img {
-                        cell.backgroundImage.image = img
-                        cell.image = img
-                    } else {
-                        let icon = Favicon(url: "", date: NSDate(), type: IconType.NoneFound)
-                        self.profile.favicons.addFavicon(icon, forSite: site)
-                        self.setDefaultThumbnailBackground(cell)
+        if let url = site.url.asURL {
+            FaviconFetcher.getForURL(url, profile: profile) >>== { icons in
+                if (icons.count > 0) {
+                    cell.imageView.sd_setImageWithURL(icons[0].url.asURL!) { (img, err, type, url) -> Void in
+                        if let img = img {
+                            cell.backgroundImage.image = img
+                            cell.image = img
+                        } else {
+                            let icon = Favicon(url: "", date: NSDate(), type: IconType.NoneFound)
+                            self.profile.favicons.addFavicon(icon, forSite: site)
+                            self.setDefaultThumbnailBackground(cell)
+                        }
                     }
                 }
             }
