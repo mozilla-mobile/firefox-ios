f5968cde0e1cf8d2ee099b4fa8048dcb35ace73a
Emily Toop <emilytoop@MacBook-Pro-2.local>
2015-08-03 17:00:13 +0100

    Not sure if right solution - only scroll to position in collection view if position exists

--- Client/Frontend/Browser/BrowserTrayAnimators.swift
+++ Client/Frontend/Browser/BrowserTrayAnimators.swift
@@ -103,7 +103,9 @@ private extension BrowserToTrayAnimator {
 
         // Force subview layout on the collection view so we can calculate the correct end frame for the animation
         tabTray.view.layoutSubviews()
-        tabTray.collectionView.scrollToItemAtIndexPath(NSIndexPath(forItem: bvc.tabManager.selectedIndex, inSection: 0), atScrollPosition: .CenteredVertically, animated: false)
+        if tabTray.collectionView.numberOfItemsInSection(0) > bvc.tabManager.selectedIndex {
+            tabTray.collectionView.scrollToItemAtIndexPath(NSIndexPath(forItem: bvc.tabManager.selectedIndex, inSection: 0), atScrollPosition: .CenteredVertically, animated: false)
+        }
 
         // Build a tab cell that we will use to animate the scaling of the browser to the tab
         let expandedFrame = calculateExpandedCellFrameFromBVC(bvc)
