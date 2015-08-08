6d6e6ee2068758aa170bec27a0018e926eecc81b
Richard Newman <rnewman@twinql.com>
2015-08-08 13:53:50 -0700

    Clean up defaultUserAgent.

--- Shared/UserAgent.swift
+++ Shared/UserAgent.swift
@@ -1,48 +1,55 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
-* License, v. 2.0. If a copy of the MPL was not distributed with this
-* file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import AVFoundation
 import UIKit
 
 public class UserAgent {
     public static func defaultUserAgent() -> String {
+        let defaults = NSUserDefaults.standardUserDefaults()
+
         let appVersion = NSBundle.mainBundle().objectForInfoDictionaryKey("CFBundleShortVersionString") as! String
-        
         let currentiOSVersion = UIDevice.currentDevice().systemVersion
-        let lastiOSVersion = NSUserDefaults.standardUserDefaults().stringForKey("LastDeviceSystemVersionNumber")
-        var firefoxUA = NSUserDefaults.standardUserDefaults().stringForKey("UserAgent")
-        if firefoxUA == nil
-            || lastiOSVersion != currentiOSVersion {
-                
-                let webView = UIWebView()
-                
-                NSUserDefaults.standardUserDefaults().setObject(currentiOSVersion,forKey: "LastDeviceSystemVersionNumber")
-                let userAgent = webView.stringByEvaluatingJavaScriptFromString("navigator.userAgent")!
-                
-                // Extract the WebKit version and use it as the Safari version.
-                let webKitVersionRegex = NSRegularExpression(pattern: "AppleWebKit/([^ ]+) ", options: nil, error: nil)!
-                let match = webKitVersionRegex.firstMatchInString(userAgent, options: nil, range: NSRange(location: 0, length: count(userAgent)))
-                if match == nil {
-                    println("Error: Unable to determine WebKit version")
-                    return ""
-                }
-                let webKitVersion = (userAgent as NSString).substringWithRange(match!.rangeAtIndex(1))
-                
-                // Insert "FxiOS/<version>" before the Mobile/ section.
-                let mobileRange = (userAgent as NSString).rangeOfString("Mobile/")
-                if mobileRange.location == NSNotFound {
-                    println("Error: Unable to find Mobile section")
-                    return ""
-                }
-                
-                let mutableUA = NSMutableString(string: userAgent)
-                mutableUA.insertString("FxiOS/\(appVersion) ", atIndex: mobileRange.location)
-                firefoxUA = "\(mutableUA) Safari/\(webKitVersion)"
-                
-                NSUserDefaults.standardUserDefaults().setObject(firefoxUA, forKey: "UserAgent")
+        let lastiOSVersion = defaults.stringForKey("LastDeviceSystemVersionNumber")
+
+        if let firefoxUA = defaults.stringForKey("UserAgent") {
+            if lastiOSVersion == currentiOSVersion {
+                return firefoxUA
+            }
         }
-        
-        return firefoxUA!
+
+        let webView = UIWebView()
+
+        defaults.setObject(currentiOSVersion,forKey: "LastDeviceSystemVersionNumber")
+        let userAgent = webView.stringByEvaluatingJavaScriptFromString("navigator.userAgent")!
+
+        // Extract the WebKit version and use it as the Safari version.
+        let webKitVersionRegex = NSRegularExpression(pattern: "AppleWebKit/([^ ]+) ", options: nil, error: nil)!
+        let match = webKitVersionRegex.firstMatchInString(userAgent, options: nil, range: NSRange(location: 0, length: count(userAgent)))
+
+        if match == nil {
+            println("Error: Unable to determine WebKit version in UA.")
+            return userAgent     // Fall back to Safari's.
+        }
+
+        let webKitVersion = (userAgent as NSString).substringWithRange(match!.rangeAtIndex(1))
+
+        // Insert "FxiOS/<version>" before the Mobile/ section.
+        let mobileRange = (userAgent as NSString).rangeOfString("Mobile/")
+        if mobileRange.location == NSNotFound {
+            println("Error: Unable to find Mobile section in UA.")
+            return userAgent     // Fall back to Safari's.
+        }
+
+        let mutableUA = NSMutableString(string: userAgent)
+        mutableUA.insertString("FxiOS/\(appVersion) ", atIndex: mobileRange.location)
+
+        let firefoxUA = "\(mutableUA) Safari/\(webKitVersion)"
+
+        defaults.setObject(firefoxUA, forKey: "UserAgent")
+        defaults.registerDefaults(["UserAgent": firefoxUA])      // Not strictly necessary, but v1 isn't the time to change things.
+
+        return firefoxUA
     }
 }
