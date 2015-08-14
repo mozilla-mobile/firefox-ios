d956451cdc9569c2fcb9822b54a27899e45cbe33
Richard Newman <rnewman@twinql.com>
2015-08-14 09:43:39 -0700

    Review comment: make GenericTable.insert return an optional Int.

--- Storage/SQL/SchemaTable.swift
+++ Storage/SQL/SchemaTable.swift
@@ -37,7 +37,7 @@ protocol Table: TableInfo {
  */
 protocol BaseTable: Table {
     typealias Type
-    func insert(db: SQLiteDBConnection, item: Type?, inout err: NSError?) -> Int
+    func insert(db: SQLiteDBConnection, item: Type?, inout err: NSError?) -> Int?
     func update(db: SQLiteDBConnection, item: Type?, inout err: NSError?) -> Int
     func delete(db: SQLiteDBConnection, item: Type?, inout err: NSError?) -> Int
     func query(db: SQLiteDBConnection, options: QueryOptions?) -> Cursor<Type>
