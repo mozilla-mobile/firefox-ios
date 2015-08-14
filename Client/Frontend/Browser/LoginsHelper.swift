df68ff289e87a953a66bcd86860e7ce8fd0747ad
Brian Nicholson <bnicholson@mozilla.com>
2015-08-14 14:17:52 -0700

    Bug 1194567 - Use the frame request for the Logins origin

--- Client/Frontend/Browser/LoginsHelper.swift
+++ Client/Frontend/Browser/LoginsHelper.swift
@@ -43,9 +43,13 @@ class LoginsHelper: BrowserHelper {
     }
 
     func userContentController(userContentController: WKUserContentController, didReceiveScriptMessage message: WKScriptMessage) {
+        let frameInfo = message.frameInfo
         var res = message.body as! [String: String]
         let type = res["type"]
-        if let url = browser?.url {
+
+        // We don't use the WKWebView's URL since the page can spoof the URL by using document.location
+        // right before requesting login data. See bug 1194567 for more context.
+        if let url = frameInfo.request.URL {
             if type == "request" {
                 res["username"] = ""
                 res["password"] = ""
