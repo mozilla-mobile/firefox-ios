a5b554b7073db7170de63cb6577617150803ad7f
Stephan Leroux <sleroux@mozilla.com>
2015-08-26 16:18:24 -0400

    Part 1: Added Customize Home Panels option to settings to open PanelSettingsViewController

---
+++ Client/Frontend/Settings/PanelSettingsViewController.swift
@@ -0,0 +1,12 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import UIKit
+
+/// View controller where the user can configure their home panels
+class PanelSettingsViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
