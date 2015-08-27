c4dcd6e313c3bb845ef0e63fd54f943dcc302254
Emily Toop <etoop@mozilla.com>
2015-08-27 17:06:21 +0100

    Ensure that history is always created before tabs

--- Providers/Profile.swift
+++ Providers/Profile.swift
@@ -229,6 +229,9 @@ public class BrowserProfile: Profile {
     }
 
     lazy var queue: TabQueue = {
+        if !self.dbCreated {
+            _ = self.history
+        }
         return SQLiteQueue(db: self.db)
     }()
 
