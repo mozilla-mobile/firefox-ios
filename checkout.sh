168d786dc9648554b4475a2c461cd4485a07e05a
Stephan Leroux <sleroux@mozilla.com>
2015-09-10 12:53:21 -0400

    Bug 1202793 - Part 1: Replace SVN google-breakpad-ios static lib dependency with Carthage/framework wrapped version

--- checkout.sh
+++ checkout.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env sh
-
 if which carthage >/dev/null; then
 	carthage checkout --no-use-binaries
-
-	# Checkout Breakpad from Google SVN and apply workaround for private headers/relative paths
-	svn --non-interactive checkout http://google-breakpad.googlecode.com/svn/trunk ThirdParty/google-breakpad
-	patch -N -p0 -d ThirdParty/google-breakpad <  breakpad_ios_fix.patch
 else
 	echo "\"carthage\" not found. please install with the following command:"
 	echo "\t brew update && brew install carthage"
