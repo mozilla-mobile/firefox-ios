b3957b7d197e0c6b984d113c3abc248b70283f0c
Richard Newman <rnewman@twinql.com>
2015-09-17 14:02:55 +0100

    Bug 1201104 - Part 2: stub out BookmarksModelFactory for new types.

--- Storage/SQL/SQLiteBookmarks.swift
+++ Storage/SQL/SQLiteBookmarks.swift
@@ -86,9 +86,16 @@ public class SQLiteBookmarks: BookmarksModelFactory {
             case .Folder:
                 return folderFactory(row)
             case .DynamicContainer:
-                assert(false, "Should never occur.")
+                fallthrough
             case .Separator:
+                // TODO
                 assert(false, "Separators not yet supported.")
+            case .Livemark:
+                // TODO
+                assert(false, "Livemarks not yet supported.")
+            case .Query:
+                // TODO
+                assert(false, "Queries not yet supported.")
             }
         }
 
