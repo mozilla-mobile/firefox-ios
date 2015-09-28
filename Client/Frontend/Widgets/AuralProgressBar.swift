ba112a23f84634f469891737569a82aaecafa789
Emily Toop <etoop@mozilla.com>
2015-09-28 14:29:48 -0400

    Bug 1205685 - Listen for AVAudioEngineConfigurationChangeNotification and detach then reattach mainMixerNodes when called.

--- Client/Frontend/Widgets/AuralProgressBar.swift
+++ Client/Frontend/Widgets/AuralProgressBar.swift
@@ -39,23 +39,47 @@ class AuralProgressBar {
             tickPlayer = AVAudioPlayerNode()
             tickPlayer.pan = -Float(AuralProgressBarUX.TickProgressPanSpread)
             engine.attachNode(tickPlayer)
-            engine.connect(tickPlayer, to: engine.mainMixerNode, format: nil)
 
             progressPlayer = AVAudioPlayerNode()
             progressPlayer.pan = +Float(AuralProgressBarUX.TickProgressPanSpread)
             engine.attachNode(progressPlayer)
-            engine.connect(progressPlayer, to: engine.mainMixerNode, format: nil)
+
+            connectPlayerNodes()
+
+            NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("audioEngineConfigurationDidChange:"), name: AVAudioEngineConfigurationChangeNotification, object: nil)
+        }
+
+        deinit {
+            NSNotificationCenter.defaultCenter().removeObserver(self, name: AVAudioEngineConfigurationChangeNotification, object: nil)
+        }
+
+        func connectPlayerNodes() {
+            let mainMixer = engine.mainMixerNode
+            engine.connect(tickPlayer, to: mainMixer, format: mainMixer.outputFormatForBus(0))
+            engine.connect(progressPlayer, to: mainMixer, format: tickBuffer.format)
+        }
+
+        func disconnectPlayerNodes() {
+            engine.disconnectNodeOutput(tickPlayer)
+            engine.disconnectNodeOutput(progressPlayer)
+        }
+
+        @objc func audioEngineConfigurationDidChange(notification: NSNotification) {
+            disconnectPlayerNodes()
+            connectPlayerNodes()
+
+            if !engine.running {
+                do { try engine.start() } catch _ { }
+            }
         }
 
         func start() {
             do {
                 try engine.start()
-            } catch _ {
-            }
+            } catch _ { }
             tickPlayer.play()
             progressPlayer.play()
-            tickPlayer.scheduleBuffer(tickBuffer, atTime: nil, options: AVAudioPlayerNodeBufferOptions.Loops) { () -> Void in
-            }
+            tickPlayer.scheduleBuffer(tickBuffer, atTime: nil, options: AVAudioPlayerNodeBufferOptions.Loops) { }
         }
 
         func stop() {
@@ -68,8 +92,7 @@ class AuralProgressBar {
             // using exp2 and log2 instead of exp and log as "log" clashes with XCGLogger.log
             let pitch = AuralProgressBarUX.ProgressStartFrequency * exp2(log2(AuralProgressBarUX.ProgressEndFrequency/AuralProgressBarUX.ProgressStartFrequency) * progress)
             let buffer = UI.tone(progressPlayer.outputFormatForBus(0), pitch: pitch, volume: AuralProgressBarUX.ProgressVolume, duration: AuralProgressBarUX.ProgressDuration)
-            progressPlayer.scheduleBuffer(buffer, atTime: nil, options: .Interrupts) { () -> Void in
-            }
+            progressPlayer.scheduleBuffer(buffer, atTime: nil, options: .Interrupts) { }
         }
 
         class func tone(format: AVAudioFormat, pitch: Double, volume: Double, duration: Double, period: Double? = nil) -> AVAudioPCMBuffer {
