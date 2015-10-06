5bc93aabe4ad762961e730b18a1d27399267bd32
Stephan Leroux <sleroux@mozilla.com>
2015-10-06 16:40:38 -0400

    Bug 1211925 - Part 1: Refactored ReaderModeCache to protocl and added Disk/Memory implementations

--- Client/Frontend/Reader/ReaderModeCache.swift
+++ Client/Frontend/Reader/ReaderModeCache.swift
@@ -4,7 +4,66 @@
 
 import Foundation
 
-private let ReaderModeCacheSharedInstance = ReaderModeCache()
+private let DiskReaderModeCacheSharedInstance = DiskReaderModeCache()
+private let MemoryReaderModeCacheSharedInstance = MemoryReaderModeCache()
+
+let ReaderModeCacheErrorDomain = "com.mozilla.client.readermodecache."
+enum ReaderModeCacheErrorCode: Int {
+    case NoPathsFound = 0
+}
+
+// NSObject wrapper around ReadabilityResult Swift struct for adding into the NSCache
+private class ReadabilityResultWrapper: NSObject {
+    let result: ReadabilityResult
+
+    init(readabilityResult: ReadabilityResult) {
+        self.result = readabilityResult
+        super.init()
+    }
+}
+
+protocol ReaderModeCache {
+    func put(url: NSURL, _ readabilityResult: ReadabilityResult) throws
+
+    func get(url: NSURL) throws -> ReadabilityResult
+
+    func delete(url: NSURL, error: NSErrorPointer)
+
+    func contains(url: NSURL) -> Bool
+}
+
+/// A non-persistent cache for readerized content for times when you don't want to write reader data to disk.
+/// For example, when the user is in a private tab, we want to make sure that we leave no trace on the file system
+class MemoryReaderModeCache: ReaderModeCache {
+    var cache: NSCache
+
+    init(cache: NSCache = NSCache()) {
+        self.cache = cache
+    }
+
+    class var sharedInstance: ReaderModeCache {
+        return MemoryReaderModeCacheSharedInstance
+    }
+
+    func put(url: NSURL, _ readabilityResult: ReadabilityResult) throws {
+        cache.setObject(ReadabilityResultWrapper(readabilityResult: readabilityResult), forKey: url)
+    }
+
+    func get(url: NSURL) throws -> ReadabilityResult {
+        guard let resultWrapper = cache.objectForKey(url) as? ReadabilityResultWrapper else {
+            throw NSError(domain: ReaderModeCacheErrorDomain, code: ReaderModeCacheErrorCode.NoPathsFound.rawValue, userInfo: nil)
+        }
+        return resultWrapper.result
+    }
+
+    func delete(url: NSURL, error: NSErrorPointer) {
+        cache.removeObjectForKey(url)
+    }
+
+    func contains(url: NSURL) -> Bool {
+        return cache.objectForKey(url) != nil
+    }
+}
 
 /// Really basic persistent cache to store readerized content. Has a simple hashed structure
 /// to avoid storing many items in the same directory.
@@ -12,15 +71,9 @@ private let ReaderModeCacheSharedInstance = ReaderModeCache()
 /// This currently lives in ~/Library/Caches so that the data can be pruned in case the OS needs
 /// more space. Whether that is a good idea or not is not sure. We have a bug on file to investigate
 /// and improve at a later time.
-
-let ReaderModeCacheErrorDomain = "com.mozilla.client.readermodecache."
-enum ReaderModeCacheErrorCode: Int {
-    case NoPathsFound = 0
-}
-
-class ReaderModeCache {
+class DiskReaderModeCache: ReaderModeCache {
     class var sharedInstance: ReaderModeCache {
-        return ReaderModeCacheSharedInstance
+        return DiskReaderModeCacheSharedInstance
     }
 
     func put(url: NSURL, _ readabilityResult: ReadabilityResult) throws {
