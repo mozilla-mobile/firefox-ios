896c8879ab4af973ac25796505996df8e229af48
Emily Toop <etoop@mozilla.com>
2015-10-14 14:23:15 +0100

    Adding fastlane files

---
+++ fastlane/snapshot.js
@@ -0,0 +1,9 @@
+#import "SnapshotHelper.js"
+
+var target = UIATarget.localTarget();
+var app = target.frontMostApp();
+var window = app.mainWindow();
+
+
+target.delay(3)
+captureLocalizedScreenshot("0-LandingScreen")
