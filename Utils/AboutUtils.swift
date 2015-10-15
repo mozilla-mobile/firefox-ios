aa6f2f7e6633fe3514de6a5c56f3592cbefcc19e
Emily Toop <etoop@mozilla.com>
2015-10-15 17:51:41 +0100

    Bug 1214964 - move fragment tab state discovery into AboutHomeUtils

--- Utils/AboutUtils.swift
+++ Utils/AboutUtils.swift
@@ -25,4 +25,41 @@ struct AboutUtils {
         }
         return nil
     }
+
+    static func getHomePanel(fragment: String?) -> Int {
+        guard let fragment = fragment else { return 0 }
+        let fragmentParts = fragment.componentsSeparatedByString("&")
+        if !fragmentParts.isEmpty {
+            if let panelParts = fragmentParts.first?.componentsSeparatedByString("=") {
+                if let last = panelParts.last, lastInt = Int(last) {
+                    return lastInt
+                }
+            }
+        }
+
+        return 0
+    }
+
+    static func getBookmarkFolders(fragment: String?) -> [String]? {
+        guard let fragment = fragment else { return nil }
+        let fragmentParts = fragment.componentsSeparatedByString("&")
+        if fragmentParts.count == 2 {
+            let folderParts = fragmentParts[1].componentsSeparatedByString("=")
+            if let last = folderParts.last {
+                return last.componentsSeparatedByString(",")
+            }
+        }
+
+        return nil
+    }
+
+    static func getNavigationFragment(url: NSURL?) -> (panel: Int, bookmarkFolders: [String]?) {
+        var panel = 0
+        var folder: [String]? = nil
+        
+        guard let url = url, let fragment = url.fragment else { return (panel, folder) }
+        panel = AboutUtils.getHomePanel(fragment)
+        folder = AboutUtils.getBookmarkFolders(fragment)
+        return (panel, folder)
+    }
 }
