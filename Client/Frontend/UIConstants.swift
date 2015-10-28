6f97f79f770ed9d2b2df34ec89a6db9298684067
Robin Andersen <robin@mozilla.com>
2015-10-28 16:20:57 -0700

    Update text link color
    The "Learn More" link on the Private Browsing empty state is
    desaturated compared to the PB icon, and would benefit visually from
    being the same color.

--- Client/Frontend/UIConstants.swift
+++ Client/Frontend/UIConstants.swift
@@ -14,7 +14,7 @@ public struct UIConstants {
     static let PrivateModeLocationBackgroundColor = UIColor(red: 31 / 255, green: 31 / 255, blue: 31 / 255, alpha: 1)
     static let PrivateModeLocationBorderColor = UIColor(red: 255, green: 255, blue: 255, alpha: 0.15)
     static let PrivateModeActionButtonTintColor = UIColor(red: 255, green: 255, blue: 255, alpha: 0.8)
-    static let PrivateModeTextHighlightColor = UIColor(red: 120 / 255, green: 120 / 255, blue: 165 / 255, alpha: 1)
+    static let PrivateModeTextHighlightColor = UIColor(red: 207 / 255, green: 104 / 255, blue: 255 / 255, alpha: 1)
     static let PrivateModeReaderModeBackgroundColor = UIColor(red: 89 / 255, green: 89 / 255, blue: 89 / 255, alpha: 1)
     static let PrivateModeToolbarTintColor = UIColor(red: 74 / 255, green: 74 / 255, blue: 74 / 255, alpha: 1)
 
