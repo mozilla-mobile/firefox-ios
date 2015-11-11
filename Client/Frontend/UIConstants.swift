b712acd01413ff640c57eb04aa543cac5f402100
Brian Nicholson <bnicholson@mozilla.com>
2015-11-11 15:53:29 -0800

    No bug - Remove Open In New Private Tab from TempStrings

--- Client/Frontend/UIConstants.swift
+++ Client/Frontend/UIConstants.swift
@@ -62,7 +62,6 @@ public struct UIConstants {
 
 /// Strings that will be used for features that haven't yet landed.
 private struct TempStrings {
-
     // Bug 1109675 - Request Desktop Site
     let requestDesktopSite = NSLocalizedString("Request Desktop Site", comment: "Pending feature; currently unused string! Tooltip label triggered by long pressing the refresh button.")
     let requestMobileSite = NSLocalizedString("Request Mobile Site", comment: "Pending feature; currently unused string! Tooltip label triggered by long pressing the refresh button a second time.")
@@ -73,9 +72,6 @@ private struct TempStrings {
     // Bug 1186013 - Prompt for going to clipboard URL
     let goToCopiedURL = NSLocalizedString("Go to copied URL?", comment: "Pending feature; currently unused string! Prompt message shown when browser is opened with URL on the clipboard")
     let goToCopiedURLButton = NSLocalizedString("Go", comment: "Pending feature; currently unused string! Button to browse to URL on the clipboard when browser is opened")
-
-    // Bug 1196227 - (pbmode) [Meta] Private Browsing
-    let openInNewPrivateTab = NSLocalizedString("Open In New Private Tab", tableName: "PrivateBrowsing", comment: "Context menu option for opening a link in a new private tab")
 }
 
 /// Old strings that will be removed when we kill 1.0. We need to keep them around for now for l10n export.
