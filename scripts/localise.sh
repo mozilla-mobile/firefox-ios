5183a6502ad4dadc210179ccdcd64bc58c8aaecd
Emily Toop <etoop@mozilla.com>
2015-11-18 17:20:40 +0000

    Always create virtualenv even if it already exists

--- scripts/localise.sh
+++ scripts/localise.sh
@@ -1,16 +1,11 @@
 cd ..
 echo "Activating virtualenv"
 # create/activate virtualenv
-if [ -d python-env ]; then
-  source python-env/bin/activate || exit 1
-else
-  virtualenv python-env || exit 1
-  source python-env/bin/activate || exit 1
-  # install libxml2
-  brew install libxml2 || exit 1
-  STATIC_DEPS=true pip install lxml || exit 1
-fi
-
+virtualenv python-env || exit 1
+source python-env/bin/activate || exit 1
+# install libxml2
+brew install libxml2 || exit 1
+STATIC_DEPS=true pip install lxml || exit 1
 #
 # Import locales
 #
