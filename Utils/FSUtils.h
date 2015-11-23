09827b0ad7c574bf1a965a1767c2541a0828796f
Stephan Leroux <sleroux@mozilla.com>
2015-11-23 13:26:31 -0500

    Bug 1226901 - Pre: Created FSUtils and method for getting open file descriptors

--- Utils/FSUtils.h
+++ Utils/FSUtils.h
@@ -1,13 +1,17 @@
-//
-//  FSUtils.h
-//  Client
-//
-//  Created by Steph Leroux on 2015-11-23.
-//  Copyright © 2015 Mozilla. All rights reserved.
-//
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#import <Foundation/Foundation.h>
+@import Foundation;
 
 @interface FSUtils : NSObject
+
+/**
+ *  Grabs all open file descriptions and returns them in a key-value dictionary where
+ *  the key is the descriptor # and the value being the filename.
+ *
+ *  @return Dictionary of open file descriptors.
+ */
++ (NSDictionary<NSNumber *, NSString *> * _Nonnull)openFileDescriptors;
 
 @end
