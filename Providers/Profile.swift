625becc3e60a631932ba58844deed1df480ba73e
Richard Newman <rnewman@twinql.com>
2015-12-07 21:04:20 -0800

    Bug 1201110 - Part B: rename BookmarkMirrorStorage to BookmarkBufferStorage.

--- Providers/Profile.swift
+++ Providers/Profile.swift
@@ -335,7 +335,7 @@ public class BrowserProfile: Profile {
         }
     }()
 
-    lazy var mirrorBookmarks: BookmarkMirrorStorage = {
+    lazy var mirrorBookmarks: BookmarkBufferStorage = {
         // Yeah, this is lazy. Sorry.
         return self.bookmarks as! MergedSQLiteBookmarks
     }()
