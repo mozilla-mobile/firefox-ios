a9256901d11f83bfb31aae2e99cb354762398c00
Stephan Leroux <sleroux@mozilla.com>
2015-12-09 16:03:11 -0500

    Bug 1231515 - Changed opening of domain to full url for default top sites

--- Client/Frontend/Home/TopSitesPanel.swift
+++ Client/Frontend/Home/TopSitesPanel.swift
@@ -260,8 +260,7 @@ extension TopSitesPanel: UICollectionViewDelegate {
         if let site = dataSource[indexPath.item] {
             // We're gonna call Top Sites bookmarks for now.
             let visitType = VisitType.Bookmark
-            let destination = NSURL(string: site.url) ?? NSURL(string: "about:blank")!
-            homePanelDelegate?.homePanel(self, didSelectURL: destination, visitType: visitType)
+            homePanelDelegate?.homePanel(self, didSelectURL: site.tileURL, visitType: visitType)
         }
     }
 
