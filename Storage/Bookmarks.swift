2c742a5e64fa4b028c843c1411cbddd41392adad
Richard Newman <rnewman@twinql.com>
2015-12-11 19:34:25 -0800

    Bug 1201110 - Part F: implement bookmark removal.

--- Storage/Bookmarks.swift
+++ Storage/Bookmarks.swift
@@ -333,7 +333,7 @@ public protocol BookmarksModelFactory {
     var nullModel: BookmarksModel { get }
 
     func isBookmarked(url: String) -> Deferred<Maybe<Bool>>
-    func remove(bookmark: BookmarkNode) -> Success
+    func removeByGUID(guid: GUID) -> Success
     func removeByURL(url: String) -> Success
     func clearBookmarks() -> Success
 }
@@ -561,7 +561,7 @@ public class MockMemoryBookmarksStore: BookmarksModelFactory, ShareToDestination
         return deferMaybe(DatabaseError(description: "Not implemented"))
     }
 
-    public func remove(bookmark: BookmarkNode) -> Success {
+    public func removeByGUID(guid: GUID) -> Success {
         return deferMaybe(DatabaseError(description: "Not implemented"))
     }
 
