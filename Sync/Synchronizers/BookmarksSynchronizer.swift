2d61344082e881daf48247dea98aad6b8c645785
Richard Newman <rnewman@twinql.com>
2015-12-16 16:45:40 -0800

    Hacking on defining the stages of merging.

--- Sync/Synchronizers/BookmarksSynchronizer.swift
+++ Sync/Synchronizers/BookmarksSynchronizer.swift
@@ -5,8 +5,11 @@
 import Foundation
 import Shared
 import Storage
+import XCGLogger
 
 private let log = Logger.syncLogger
+
+// MARK: - External synchronizer interface.
 
 public class BufferingBookmarksSynchronizer: TimestampedSingleCollectionSynchronizer, Synchronizer {
     public required init(scratchpad: Scratchpad, delegate: SyncDelegate, basePrefs: Prefs) {
@@ -34,6 +37,79 @@ public class BufferingBookmarksSynchronizer: TimestampedSingleCollectionSynchron
     }
 }
 
+private class MergeApplier {
+    let buffer: BookmarkBufferStorage
+    let storage: SyncableBookmarks
+    let client: Sync15CollectionClient<BookmarkBasePayload>
+    let merger: BookmarksMerger
+
+    init(buffer: BookmarkBufferStorage, storage: SyncableBookmarks, client: Sync15CollectionClient<BookmarkBasePayload>) {
+        self.buffer = buffer
+        self.storage = storage
+        self.merger = NoOpBookmarksMerger(buffer: buffer, storage: storage)
+        self.client = client
+    }
+
+    func go() -> SyncResult {
+        return self.merger.merge() >>== { result in
+            result.describe(log)
+            return result.uploadCompletion.applyToClient(self.client)
+                >>> { result.overrideCompletion.applyToStore(self.storage) }
+                >>> { result.bufferCompletion.applyToBuffer(self.buffer) }
+                >>> always(SyncStatus.Completed)
+        }
+
+    }
+}
+
+// MARK: - Self-description.
+
+protocol DescriptionDestination {
+    func write(message: String)
+}
+
+extension XCGLogger: DescriptionDestination {
+    func write(message: String) {
+        self.info(message)
+    }
+}
+
+// MARK: - Protocols to define merge results.
+
+protocol UpstreamCompletionOp {
+    func describe(log: DescriptionDestination)
+
+    // TODO: this should probably return a timestamp.
+    // The XIUS that we'll need for the upload can be captured as part of the op.
+    func applyToClient(client: Sync15CollectionClient<BookmarkBasePayload>) -> Success
+}
+
+protocol LocalOverrideCompletionOp {
+    func describe(log: DescriptionDestination)
+    func applyToStore(storage: SyncableBookmarks) -> Success
+}
+
+protocol BufferCompletionOp {
+    func describe(log: DescriptionDestination)
+    func applyToBuffer(buffer: BookmarkBufferStorage) -> Success
+}
+
+struct BookmarksMergeResult {
+    let uploadCompletion: UpstreamCompletionOp
+    let overrideCompletion: LocalOverrideCompletionOp
+    let bufferCompletion: BufferCompletionOp
+
+    func describe(log: DescriptionDestination) {
+        log.write("Merge result:")
+        self.uploadCompletion.describe(log)
+        self.overrideCompletion.describe(log)
+        self.bufferCompletion.describe(log)
+    }
+
+    static let NoOp = BookmarksMergeResult(uploadCompletion: UpstreamCompletionNoOp(), overrideCompletion: LocalOverrideCompletionNoOp(), bufferCompletion: BufferCompletionNoOp())
+}
+
+
 /**
  * The merger takes as input an existing storage state (mirror and local override),
  * a buffer of new incoming records that relate to the mirror, and performs a three-way
@@ -51,56 +127,66 @@ public class BufferingBookmarksSynchronizer: TimestampedSingleCollectionSynchron
  * It is expected that the caller will immediately apply the result in this order:
  *
  * 1. Upload the remote changes, if any. If this fails we can retry the entire process.
- * 2. Apply the local changes, if any. If this fails we will re-download the records
- *    we just uploaded, and should reach the same end state.
- * 3. Switch to the new mirror state. If this fails, we should find that our reconciled
- *    server contents apply neatly to our mirror and empty local, and we'll reach the
- *    same end state.
+ * 2(a). Apply the local changes, if any. If this fails we will re-download the records
+ *       we just uploaded, and should reach the same end state.
+ * 2(b). Switch to the new mirror state. If this fails, we should find that our reconciled
+ *       server contents apply neatly to our mirror and empty local, and we'll reach the
+ *       same end state.
+ * 3. Apply buffer changes. We only do this after the mirror has advanced; if we fail to
+ *    clean up the buffer, it'll reconcile neatly with the mirror on a subsequent try.
  * 4. Update bookkeeping timestamps. If this fails we will download uploaded records,
  *    find they match, and have no repeat merging work to do.
  *
  * The goal of merging is that the buffer is empty (because we reconciled conflicts and
  * updated the server), our local overlay is empty (because we reconciled conflicts and
  * applied our changes to the server), and the mirror matches the server.
- *
- * This implementation is coupled to SQLiteBookmarks out of convenience.
  */
-class SQLiteBookmarksMerger {
-    private let buffer: BookmarkBufferStorage
-    private let storage: SQLiteBookmarks
+protocol BookmarksMerger {
+    init(buffer: BookmarkBufferStorage, storage: SyncableBookmarks)
+    func merge() -> Deferred<Maybe<BookmarksMergeResult>>
+}
 
-    init(buffer: BookmarkBufferStorage, storage: SQLiteBookmarks) {
+// MARK: - No-op implementations of each protocol.
+
+class NoOpBookmarksMerger: BookmarksMerger {
+    let buffer: BookmarkBufferStorage
+    let storage: SyncableBookmarks
+
+    required init(buffer: BookmarkBufferStorage, storage: SyncableBookmarks) {
         self.buffer = buffer
         self.storage = storage
     }
+
+    func merge() -> Deferred<Maybe<BookmarksMergeResult>> {
+        return deferMaybe(BookmarksMergeResult.NoOp)
+    }
 }
 
-protocol DescriptionDestination {
-    func info(message: String)
+class UpstreamCompletionNoOp: UpstreamCompletionOp {
+    func describe(log: DescriptionDestination) {
+        log.write("No upstream operation.")
+    }
+    func applyToClient(client: Sync15CollectionClient<BookmarkBasePayload>) -> Success {
+        return succeed()
+    }
 }
 
-protocol LocalOverrideCompletionOp {
-    func describe(log: DescriptionDestination)
-    func applyToStore(storage: SQLiteBookmarks) -> Success
+class LocalOverrideCompletionNoOp: LocalOverrideCompletionOp {
+    func describe(log: DescriptionDestination) {
+        log.write("No local override operation.")
+    }
+    func applyToStore(storage: SyncableBookmarks) -> Success {
+        return succeed()
+    }
 }
 
-protocol BufferCompletionOp {
-    func describe(log: DescriptionDestination)
-    func applyToBuffer(buffer: BookmarkBufferStorage) -> Success
+class BufferCompletionNoOp: BufferCompletionOp {
+    func describe(log: DescriptionDestination) {
+        log.write("No buffer operation.")
+    }
+    func applyToBuffer(buffer: BookmarkBufferStorage) -> Success {
+        return succeed()
+    }
 }
 
-protocol UpstreamCompletionOp {
-    func describe(log: DescriptionDestination)
-
-    // TODO: this should probably return a timestamp.
-    // The XIUS that we'll need for the upload can be captured as part of the op.
-    func applyToClient(client: Sync15CollectionClient<BookmarkPayload>) -> Success
-}
-
-struct BookmarksMergeResult {
-    let overrideCompletion: LocalOverrideCompletionOp
-    let bufferCompletion: BufferCompletionOp
-
-}
-protocol BookmarksMerger {
-}
+// MARK: - Real implementations of each protocol.
