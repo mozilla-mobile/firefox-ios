87c36fa726683b449d66b2a1fae3634601c88302
Richard Newman <rnewman@twinql.com>
2015-12-22 22:00:36 -0800

    Bug 1201110 - Follow-up: remove clearBookmarks from the public interface.

--- Storage/Bookmarks.swift
+++ Storage/Bookmarks.swift
@@ -345,7 +345,6 @@ public protocol BookmarksModelFactory {
     func isBookmarked(url: String) -> Deferred<Maybe<Bool>>
     func removeByGUID(guid: GUID) -> Success
     func removeByURL(url: String) -> Success
-    func clearBookmarks() -> Success
 }
 
 /*
