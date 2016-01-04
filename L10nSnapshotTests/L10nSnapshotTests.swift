a4ded5ad6ce6745641e25f72bde6ad61a62a47a8
Emily Toop <etoop@mozilla.com>
2016-01-04 15:21:51 +0000

    Bug 1205015 - mpl

--- L10nSnapshotTests/L10nSnapshotTests.swift
+++ L10nSnapshotTests/L10nSnapshotTests.swift
@@ -1,10 +1,6 @@
-//
-//  L10nSnapshotTests.swift
-//  L10nSnapshotTests
-//
-//  Created by Emily Toop on 12/22/15.
-//  Copyright © 2015 Mozilla. All rights reserved.
-//
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import XCTest
 
