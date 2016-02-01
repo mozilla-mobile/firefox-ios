6ff697844fa2d080b05c5ea8498ffdd8192273ad
Stephan Leroux <sleroux@mozilla.com>
2016-02-01 17:40:39 -0500

    update to temp

--- beta_build.sh
+++ beta_build.sh
@@ -1,1 +1,1 @@
-fastlane beta version:2.0 build:4 username:sleroux@mozilla.com localise:true adjust_environment:sandbox adjust_app_token:v32c8z72qz82
+fastlane beta version:2.0 build:5 username:sleroux@mozilla.com localise:true adjust_environment:sandbox adjust_app_token:v32c8z72qz82
