4168d3ce82d2608a6a8e6ef4460ce6a38843045f
Richard Newman <rnewman@twinql.com>
2016-02-03 21:26:52 -0800

    Remove stuff from TestSQLiteBookmarks that's now in StorageTestUtils.

--- StorageTests/TestSQLiteBookmarks.swift
+++ StorageTests/TestSQLiteBookmarks.swift
@@ -19,89 +19,6 @@ private func getBrowserDB(filename: String, files: FileAccessor) -> BrowserDB? {
     return db
 }
 
-private extension BrowserDB {
-    func getGUIDs(sql: String) -> [GUID] {
-        func guidFactory(row: SDRow) -> GUID {
-            return row[0] as! GUID
-        }
-
-        guard let cursor = self.runQuery(sql, args: nil, factory: guidFactory).value.successValue else {
-            XCTFail("Unable to get cursor.")
-            return []
-        }
-        return cursor.asArray()
-    }
-
-    func getPositionsForChildrenOfParent(parent: GUID, fromTable table: String) -> [GUID: Int] {
-        let args: Args = [parent]
-        let factory: SDRow -> (GUID, Int) = {
-            return ($0["child"] as! GUID, $0["idx"] as! Int)
-        }
-        let cursor = self.runQuery("SELECT child, idx FROM \(table) WHERE parent = ?", args: args, factory: factory).value.successValue!
-        return cursor.reduce([:], combine: { (var dict, pair) in
-            if let (k, v) = pair {
-                dict[k] = v
-            }
-            return dict
-        })
-    }
-
-    func isLocallyDeleted(guid: GUID) -> Bool? {
-        let args: Args = [guid]
-        let cursor = self.runQuery("SELECT is_deleted FROM \(TableBookmarksLocal) WHERE guid = ?", args: args, factory: { $0.getBoolean("is_deleted") }).value.successValue!
-        return cursor[0]
-    }
-
-    func isOverridden(guid: GUID) -> Bool? {
-        let args: Args = [guid]
-        let cursor = self.runQuery("SELECT is_overridden FROM \(TableBookmarksMirror) WHERE guid = ?", args: args, factory: { $0.getBoolean("is_overridden") }).value.successValue!
-        return cursor[0]
-    }
-
-    func getSyncStatusForGUID(guid: GUID) -> SyncStatus? {
-        let args: Args = [guid]
-        let cursor = self.runQuery("SELECT sync_status FROM \(TableBookmarksLocal) WHERE guid = ?", args: args, factory: { $0[0] as! Int }).value.successValue!
-        if let raw = cursor[0] {
-            return SyncStatus(rawValue: raw)
-        }
-        return nil
-    }
-
-    func getRecordByURL(url: String, fromTable table: String) -> BookmarkMirrorItem {
-        let args: Args = [url]
-        return self.runQuery("SELECT * FROM \(table) WHERE bmkUri = ?", args: args, factory: BookmarkFactory.mirrorItemFactory).value.successValue![0]!
-    }
-
-    func getRecordByGUID(guid: GUID, fromTable table: String) -> BookmarkMirrorItem {
-        let args: Args = [guid]
-        return self.runQuery("SELECT * FROM \(table) WHERE guid = ?", args: args, factory: BookmarkFactory.mirrorItemFactory).value.successValue![0]!
-    }
-
-    func getChildrenOfFolder(folder: GUID) -> [GUID] {
-        let args: Args = [folder]
-        let sql =
-        "SELECT child FROM \(ViewBookmarksLocalStructureOnMirror) " +
-        "WHERE parent = ? " +
-        "ORDER BY idx ASC"
-        return self.runQuery(sql, args: args, factory: { $0[0] as! GUID }).value.successValue!.asArray()
-    }
-}
-
-// MARK: - The messy way to extend non-protocol generics.
-
-protocol Succeedable {
-    var isSuccess: Bool { get }
-}
-
-extension Maybe: Succeedable {
-}
-
-private extension Deferred where T: Succeedable {
-    func succeeded() {
-        self.value.isSuccess
-    }
-}
-
 // MARK: - Tests.
 
 class TestSQLiteBookmarks: XCTestCase {
@@ -210,7 +127,7 @@ class TestSQLiteBookmarks: XCTestCase {
             "folderCCCCCC", "bookmark2002", 0,
         ]
 
-        self.moveLocalToMirror(db)   // So we have the roots.
+        db.moveLocalToMirrorForTesting()   // So we have the roots.
         db.run([
             (sql: mirrorQuery, args: mirrorArgs),
             (sql: structureQuery, args: structureArgs),
@@ -369,7 +286,7 @@ class TestSQLiteBookmarks: XCTestCase {
         XCTAssertEqual(positionsAfterDelete[rowB.guid], 0)
 
         // Manually shuffle all of these into the mirror, as if we were fully synchronized.
-        self.moveLocalToMirror(db)
+        db.moveLocalToMirrorForTesting()
         XCTAssertEqual([], db.getGUIDs("SELECT guid FROM \(TableBookmarksLocal) ORDER BY id"))
         XCTAssertEqual([], db.getGUIDs("SELECT child FROM \(TableBookmarksLocalStructure)"))
         XCTAssertEqual(rootGUIDs + [rowB.guid], db.getGUIDs("SELECT guid FROM \(TableBookmarksMirror) ORDER BY id"))
@@ -425,34 +342,6 @@ class TestSQLiteBookmarks: XCTestCase {
         // The mirror structure is unchanged after all this.
         XCTAssertEqual(rootGUIDs + [rowB.guid], db.getGUIDs("SELECT guid FROM \(TableBookmarksMirror) ORDER BY id"))
         XCTAssertEqual([rowB.guid], db.getGUIDs("SELECT child FROM \(TableBookmarksMirrorStructure) WHERE parent = '\(BookmarkRoots.MobileFolderGUID)' ORDER BY idx"))
-    }
-
-    private func moveLocalToMirror(db: BrowserDB) {
-        // This is a risky process -- it's not the same logic that the real synchronizer uses
-        // (because I haven't written it yet), so it might end up lying. We do what we can.
-        let overrideSQL = "INSERT OR IGNORE INTO \(TableBookmarksMirror) " +
-                          "(guid, type, bmkUri, title, parentid, parentName, feedUri, siteUri, pos," +
-                          " description, tags, keyword, folderName, queryId, " +
-                          " is_overridden, server_modified, faviconID) " +
-                          "SELECT guid, type, bmkUri, title, parentid, parentName, " +
-                          "feedUri, siteUri, pos, description, tags, keyword, folderName, queryId, " +
-                          "0 AS is_overridden, \(NSDate.now()) AS server_modified, faviconID " +
-                          "FROM \(TableBookmarksLocal)"
-
-        // Copy its mirror structure.
-        let copySQL = "INSERT INTO \(TableBookmarksMirrorStructure) " +
-                      "SELECT * FROM \(TableBookmarksLocalStructure)"
-
-        // Throw away the old.
-        let deleteLocalStructureSQL = "DELETE FROM \(TableBookmarksLocalStructure)"
-        let deleteLocalSQL = "DELETE FROM \(TableBookmarksLocal)"
-
-        db.run([
-            overrideSQL,
-            copySQL,
-            deleteLocalStructureSQL,
-            deleteLocalSQL,
-        ]).succeeded()
     }
 
     /*
