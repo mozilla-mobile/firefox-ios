60daaf1b14ca241abe312a1ee41e668d32f349bc
Richard Newman <rnewman@twinql.com>
2016-02-04 17:45:08 -0800

    Profile's bookmarks is a MirrorItemSource.

--- Providers/Profile.swift
+++ Providers/Profile.swift
@@ -130,7 +130,7 @@ class BrowserProfileSyncDelegate: SyncDelegate {
  * A Profile manages access to the user's data.
  */
 protocol Profile: class {
-    var bookmarks: protocol<BookmarksModelFactory, ShareToDestination, SyncableBookmarks, LocalItemSource> { get }
+    var bookmarks: protocol<BookmarksModelFactory, ShareToDestination, SyncableBookmarks, LocalItemSource, MirrorItemSource> { get }
     // var favicons: Favicons { get }
     var prefs: Prefs { get }
     var queue: TabQueue { get }
@@ -317,7 +317,7 @@ public class BrowserProfile: Profile {
         return self.places
     }
 
-    lazy var bookmarks: protocol<BookmarksModelFactory, ShareToDestination, SyncableBookmarks, LocalItemSource> = {
+    lazy var bookmarks: protocol<BookmarksModelFactory, ShareToDestination, SyncableBookmarks, LocalItemSource, MirrorItemSource> = {
         // Make sure the rest of our tables are initialized before we try to read them!
         // This expression is for side-effects only.
         withExtendedLifetime(self.places) {
