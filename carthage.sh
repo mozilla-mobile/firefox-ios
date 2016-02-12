7e29082f06aa7f27ef9c2266bf1c934f037b95de
Emily Toop <etoop@mozilla.com>
2016-02-12 16:34:07 +0000

    update brew before fetching specific carthage version

--- carthage.sh
+++ carthage.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env sh
 CARTHAGE_VERSION=$(carthage version)
-if [[ $CARTHAGE_VERSION<0.11.0 ]]; then
+if [[ $CARTHAGE_VERSION<0.11.0 || $CARTHAGE_VERSION>0.11.0 ]]; then
 	echo "Carthage requires upgrade"
-	brew upgrade carthage
+	brew update
+	brew install https://github.com/Carthage/Carthage/releases/tag/0.11
 fi
 
 if [[ ! -e "Carthage/Checkouts/google-breakpad-ios/Breakpad.xcodeproj" ]]; then 
