68b89cf2ac8403684c2e3b9f78d8994561f6f03f
James Hugman <james@hugman.tv>
2016-02-22 19:37:48 +0000

    Bug 1250149 – Whitelist only webpages for NSUserActivity (uplift)
    This whitelists http and https schemes.
    
    In doing so, move the isWebPage() method from BVC to NSURLExtensions.

--- Client/Helpers/SpotlightHelper.swift
+++ Client/Helpers/SpotlightHelper.swift
@@ -47,7 +47,7 @@ class SpotlightHelper: NSObject {
     }
 
     func update(pageContent: [String: String], forURL url: NSURL) {
-        if AboutUtils.isAboutURL(url) || url.scheme == "about" {
+        if !url.isWebPage() {
             return
         }
 
