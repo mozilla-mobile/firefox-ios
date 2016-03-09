37ed559373892a8d74eba237ac3d4d21a09f4bb9
Stefan Arentz <stefan@arentz.ca>
2016-03-09 13:38:36 -0500

    No bug - L10N Snapshots: Print timestamp and skip ar

--- l10n-screenshots.sh
+++ l10n-screenshots.sh
@@ -14,10 +14,12 @@ mkdir l10n-screenshots
 
 for d in firefox-ios-l10n/?? firefox-ios-l10n/??? firefox-ios-l10n/??-??; do
     lang=$(basename $d)
-    echo "Snapshotting $lang"
-    mkdir "l10n-screenshots/$lang"
-    snapshot --project Client.xcodeproj --scheme L10nSnapshotTests \
-             --erase_simulator --number_of_retries 3 \
-             --devices "iPhone 4s,iPhone 5s,iPhone 6s" --languages "$lang" \
-             --output_directory "l10n-screenshots/$lang" > "l10n-screenshots/$lang/snapshot.log" 2>&1
+    if [ "$lang" != "ar" ]; then
+        echo "`date` Snapshotting $lang"
+        mkdir "l10n-screenshots/$lang"
+        snapshot --project Client.xcodeproj --scheme L10nSnapshotTests \
+           --erase_simulator --number_of_retries 3 \
+           --devices "iPhone 4s,iPhone 5s,iPhone 6s" --languages "$lang" \
+           --output_directory "l10n-screenshots/$lang" > "l10n-screenshots/$lang/snapshot.log" 2>&1
+    fi
 done
