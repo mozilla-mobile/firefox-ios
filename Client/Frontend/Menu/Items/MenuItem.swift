b7855644696bdce9f08616bd27a5d4ad40a9086b
Emily Toop <etoop@mozilla.com>
2016-03-09 12:06:50 +0000

    Bug 1254561 - Create Menu configuration             - Create structs to describe MenuItems and MenuToolbarItems             - Create MenuConfiguration file             - Add test for menu configuration

---
+++ Client/Frontend/Menu/Items/MenuItem.swift
@@ -0,0 +1,26 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import Foundation
+
+
+struct MenuItem {
+    let title: String
+    private let iconName: String
+    private let selectedIconName: String
+
+    var icon: UIImage? {
+        return UIImage(named: iconName)
+    }
+
+    var selectedIcon: UIImage? {
+        return UIImage(named: selectedIconName)
+    }
+
+    init(title: String, icon: String, selectedIcon: String) {
+        self.title = title
+        self.iconName = icon
+        self.selectedIconName = selectedIcon
+    }
+}
