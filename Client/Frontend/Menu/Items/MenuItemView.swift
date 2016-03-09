3cddf1c8c9c6d33bb2913086dbb60bcbd6d93bc1
Emily Toop <etoop@mozilla.com>
2016-03-09 18:16:49 +0000

    Bug 1254567 - Create view for displaying a MenuItem.             - Create menu view                  * create delegate and data source for menu toolbar items                  * create delegate and data source for menu items                  * adds a MenuItemView to a container view for each menu item             - Add custom Toolbar to provide rounded corners.             - Add footer view to menu view & use rounded toolbar for top right and top left             - Layout the title label to correct width

---
+++ Client/Frontend/Menu/Items/MenuItemView.swift
@@ -0,0 +1,105 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import UIKit
+
+/**
+ * creates a view that consists of an image and a title.
+ * the image view is displayed at the center top of the
+ **/
+public class MenuItemView: UIControl {
+
+    var padding: CGFloat = 5.0
+
+    private(set) public var imageView: UIImageView
+    private(set) public var titleLabel: UILabel
+
+    private var previousLocation: CGPoint?
+    public init() {
+        imageView = UIImageView()
+        titleLabel = UILabel()
+
+        super.init(frame: CGRectZero)
+
+        titleLabel.lineBreakMode = NSLineBreakMode.ByWordWrapping
+        titleLabel.numberOfLines = 2
+        titleLabel.textAlignment = NSTextAlignment.Center
+
+        self.addSubview(imageView)
+
+        self.addSubview(titleLabel)
+
+        imageView.snp_makeConstraints { make in
+            make.centerX.equalTo(self)
+            make.bottom.equalTo(self.snp_centerYWithinMargins)
+        }
+
+        titleLabel.snp_makeConstraints { make in
+            make.top.equalTo(imageView.snp_bottom).offset(padding)
+            make.centerX.equalTo(self)
+            make.bottom.equalTo(self).offset(-padding)
+        }
+    }
+
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        titleLabel.preferredMaxLayoutWidth = self.bounds.size.width / 1.5
+    }
+
+    func prepareForReuse() {
+        imageView.image = nil
+        titleLabel.text = nil
+
+        guard let recognisers = gestureRecognizers else { return }
+        for recogniser in recognisers {
+            if recogniser is UILongPressGestureRecognizer {
+                self.removeGestureRecognizer(recogniser)
+            }
+        }
+
+        self.snp_removeConstraints()
+    }
+
+    public func setTitle(title: String) {
+        titleLabel.text = title
+    }
+
+    public func setImage(image: UIImage) {
+        imageView.image = image
+    }
+
+    public func setHighlightedImage(image: UIImage) {
+        imageView.highlightedImage = image
+    }
+
+    public override func beginTrackingWithTouch(touch: UITouch, withEvent event: UIEvent?) -> Bool {
+        super.beginTrackingWithTouch(touch, withEvent: event)
+        // work out whether or not the touch happened inside this item
+        // return true if so, false otherwise
+        if !self.bounds.contains(touch.locationInView(self)) {
+            return false
+        }
+
+        imageView.highlighted = true
+        return true
+    }
+
+    public override func continueTrackingWithTouch(touch: UITouch, withEvent event: UIEvent?) -> Bool {
+        super.continueTrackingWithTouch(touch, withEvent: event)
+        if !self.bounds.contains(touch.locationInView(self)) {
+            imageView.highlighted = false
+            return false
+        }
+        return true
+    }
+
+    public override func endTrackingWithTouch(touch: UITouch?, withEvent event: UIEvent?) {
+        super.endTrackingWithTouch(touch, withEvent: event)
+        imageView.highlighted = false
+    }
+}
---
+++ Client/Frontend/Menu/MenuView.swift
@@ -0,0 +1,293 @@
+//
+//  MenuView.swift
+//  Client
+//
+//  Created by Emily Toop on 3/10/16.
+//  Copyright © 2016 Mozilla. All rights reserved.
+//
+
+import UIKit
+
+class MenuView: UIView {
+
+    var toolbar: RoundedToolbar
+    var menuItemViewContainer: UIView
+
+    var openMenuImage: UIImageView
+    var menuFooterView: UIView
+
+    var toolbarDelegate: MenuToolbarItemDelegate?
+    var toolbarDataSource: MenuToolbarDataSource?
+    var menuItemDelegate: MenuItemDelegate?
+    var menuItemDataSource: MenuItemDataSource?
+
+    var toolbarHeight: Float = 40.0 {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+
+    var menuRowHeight: Float = 65.0 {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+
+    var itemPadding: CGFloat = 10.0 {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+
+    var currentPageIndex: Int = 0 {
+        didSet {
+            needsReload = true
+            self.setNeedsLayout()
+        }
+    }
+
+    var menuFooterHeight: CGFloat = 40 {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+
+    var nextPageIndex: Int?
+
+    private var needsReload: Bool = true
+
+    private var cachedItems = [NSIndexPath: MenuItemView]()
+    private var reusableItems = [MenuItemView]()
+    private var toolbarItems = [UIBarButtonItem]()
+
+    init() {
+        toolbar = RoundedToolbar()
+        toolbar.setContentHuggingPriority(UILayoutPriorityRequired, forAxis: UILayoutConstraintAxis.Vertical)
+        menuItemViewContainer = UIView()
+        menuFooterView = UIView()
+        openMenuImage = UIImageView()
+
+        super.init(frame: CGRectZero)
+
+        toolbar.cornersToRound = [.TopLeft, .TopRight]
+        self.addSubview(toolbar)
+        toolbar.snp_makeConstraints { make in
+            make.height.equalTo(toolbarHeight)
+            make.top.left.right.equalTo(self)
+        }
+
+        self.addSubview(menuFooterView)
+        // so it always displays the colour of the background
+        menuFooterView.backgroundColor = UIColor.clearColor()
+        menuFooterView.snp_makeConstraints { make in
+            make.height.equalTo(menuFooterHeight)
+            make.left.right.equalTo(self)
+            make.bottom.equalTo(self)
+        }
+
+        menuFooterView.addSubview(openMenuImage)
+        openMenuImage.snp_makeConstraints { make in
+            make.center.equalTo(menuFooterView)
+            make.height.equalTo(menuFooterView)
+        }
+
+        self.addSubview(menuItemViewContainer)
+        menuItemViewContainer.snp_makeConstraints { make in
+            make.top.equalTo(toolbar.snp_bottom)
+            make.left.right.equalTo(self)
+            make.bottom.equalTo(menuFooterView.snp_top)
+        }
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+
+
+    override func layoutSubviews() {
+        reloadDataIfNeeded()
+        layoutToolbar()
+        layoutMenu()
+        layoutFooter()
+        super.layoutSubviews()
+    }
+
+    func reloadData() {
+        cachedItems.keys.forEach { path in
+            cachedItems[path]?.removeFromSuperview()
+        }
+        cachedItems.removeAll()
+
+        reusableItems.forEach {
+            $0.removeFromSuperview()
+        }
+        reusableItems.removeAll()
+
+        toolbarItems.removeAll()
+        toolbar.items?.removeAll()
+
+        loadToolbar()
+        loadMenu()
+
+        needsReload = false
+    }
+
+    func dequeueReusableMenuItemViewForIndexPath(indexPath: NSIndexPath) -> MenuItemView {
+        if let existingView = cachedItems[indexPath] {
+            return existingView
+        }
+
+        let view = reusableItems.last == nil ? MenuItemView() : reusableItems.removeLast()
+        cachedItems[indexPath] = view
+        return view
+    }
+
+    private func loadToolbar() {
+        guard let toolbarDataSource = toolbarDataSource else { return }
+        let numberOfToolbarItems = toolbarDataSource.numberOfToolbarItemsInMenuView(self)
+        for index in 0..<numberOfToolbarItems {
+            let toolbarButton = toolbarDataSource.menuView(self, buttonForItemAtIndex: index)
+            toolbarButton.target = self
+            toolbarButton.action = "toolbarButtonSelected:"
+            toolbarItems.append(toolbarButton)
+        }
+    }
+
+    private func loadMenu() {
+        let lastPageIndex = currentPageIndex - 1
+        if lastPageIndex >= 0 {
+            loadMenuForPageIndex(lastPageIndex)
+        }
+
+        loadMenuForPageIndex(currentPageIndex)
+
+        let nextPageIndex = currentPageIndex + 1
+        if nextPageIndex < menuItemDataSource?.numberOfPagesInMenuView(self) ?? 0 {
+            loadMenuForPageIndex(nextPageIndex)
+        }
+    }
+
+    private func loadMenuForPageIndex(pageIndex: Int) {
+        guard let itemDataSource = menuItemDataSource else { return }
+
+        let numberOfItemsForPage = itemDataSource.menuView(self, numberOfItemsForPage: pageIndex)
+        for index in 0..<numberOfItemsForPage {
+            let indexPath = NSIndexPath(forItem: index, inSection: pageIndex)
+            cachedItems[indexPath] = itemDataSource.menuView(self, viewForItemAtIndexPath: indexPath)
+        }
+
+    }
+
+    func setNeedsReload() {
+        needsReload = true
+        setNeedsLayout()
+    }
+
+    private func reloadDataIfNeeded() {
+        if needsReload {
+            reloadData()
+        }
+    }
+
+    private func layoutToolbar() {
+        var displayToolbarItems = [UIBarButtonItem]()
+        for (index, item) in toolbarItems.enumerate() {
+            displayToolbarItems.append(item)
+            if index < toolbarItems.count-1 {
+                displayToolbarItems.append(UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.FlexibleSpace, target: nil, action: nil))
+            }
+        }
+        if displayToolbarItems.count == 0 {
+            toolbar.snp_updateConstraints { make in
+                make.height.equalTo(0)
+            }
+        } else {
+            toolbar.snp_updateConstraints { make in
+                make.height.equalTo(toolbarHeight)
+            }
+            toolbar.setItems(displayToolbarItems, animated: false)
+        }
+    }
+
+
+    private func layoutMenu() {
+        // make a copy of cached items
+        var availableItems = cachedItems
+        cachedItems.removeAll()
+
+        // get views for the previous page, if there is one
+        for pageIndex in currentPageIndex-1...currentPageIndex+1 {
+            layoutPage(pageIndex, availableItems: &availableItems)
+        }
+
+        // add any unused views to reusable items
+        for item in availableItems.values {
+            item.prepareForReuse()
+            reusableItems.append(item)
+        }
+        availableItems.removeAll()
+    }
+
+    private func layoutPage(pageIndex: Int, inout availableItems: [NSIndexPath: MenuItemView]) {
+        guard let itemDataSource = menuItemDataSource else { return }
+
+        let numberOfPages = itemDataSource.numberOfPagesInMenuView(self)
+
+        if pageIndex < 0 || pageIndex >= numberOfPages { return }
+
+        let numberOfItemsForPage = itemDataSource.menuView(self, numberOfItemsForPage: pageIndex)
+        let numberOfItemsInRow = itemDataSource.numberOfItemsPerRowInMenuView(self)
+
+        let numberOfRows = ceil(CGFloat(numberOfItemsForPage) / CGFloat(numberOfItemsInRow))
+        let height = itemPadding + (CGFloat(numberOfRows) * (CGFloat(menuRowHeight) + itemPadding))
+        menuItemViewContainer.snp_updateConstraints { make in
+            make.height.equalTo(height)
+        }
+
+        for index in 0..<numberOfItemsForPage {
+            let indexPath = NSIndexPath(forItem: index, inSection: pageIndex)
+            guard let item = availableItems[indexPath] else { continue }
+            cachedItems[indexPath] = item
+            menuItemViewContainer.addSubview(item)
+
+            // now properly lay out the cells
+            let row = floor(CGFloat(index) / CGFloat(numberOfItemsInRow))
+            let columnIndex = index - (Int(row) * numberOfItemsInRow)
+            let columnMultiplier = CGFloat(columnIndex)/CGFloat(numberOfItemsInRow)
+            let rowMultiplier = CGFloat(row) / CGFloat(numberOfRows)
+            item.snp_makeConstraints { make in
+                make.height.greaterThanOrEqualTo(menuItemViewContainer.snp_height).dividedBy(numberOfRows)
+                make.width.greaterThanOrEqualTo(menuItemViewContainer.snp_width).dividedBy(numberOfItemsInRow)
+                if columnMultiplier > 0 {
+                    make.left.equalTo(menuItemViewContainer.snp_right).multipliedBy(columnMultiplier)
+                } else {
+                    make.left.equalTo(menuItemViewContainer.snp_left)
+                }
+
+                if rowMultiplier > 0 {
+                    make.top.equalTo(menuItemViewContainer.snp_bottom).multipliedBy(rowMultiplier)
+                } else {
+                    make.top.equalTo(menuItemViewContainer.snp_top)
+                }
+            }
+            availableItems.removeValueForKey(indexPath)
+        }
+
+    }
+
+    private func layoutFooter() {
+        menuFooterView.snp_updateConstraints{ make in
+            make.height.equalTo(menuFooterHeight)
+        }
+    }
+
+    @objc private func toolbarButtonSelected(sender: UIBarButtonItem) {
+        guard let selectedButtonIndex = toolbarItems.indexOf(sender) else { return }
+        toolbarDelegate?.menuView(self, didSelectItemAtIndex: selectedButtonIndex)
+    }
+    
+    func indexPathForView(itemView: MenuItemView) -> NSIndexPath? {
+        return (cachedItems as NSDictionary).allKeysForObject(itemView).first as? NSIndexPath
+    }
+}
