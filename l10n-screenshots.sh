bb56d9ab5f99c2f568b3764113c3f597ed3de974
Stefan Arentz <stefan@arentz.ca>
2016-03-09 13:40:51 -0500

    No bug - L10N Snapshots: Skip Open Summary

--- l10n-screenshots.sh
+++ l10n-screenshots.sh
@@ -18,6 +18,7 @@ for d in firefox-ios-l10n/?? firefox-ios-l10n/??? firefox-ios-l10n/??-??; do
         echo "`date` Snapshotting $lang"
         mkdir "l10n-screenshots/$lang"
         snapshot --project Client.xcodeproj --scheme L10nSnapshotTests \
+           --skip_open_summary \
            --erase_simulator --number_of_retries 3 \
            --devices "iPhone 4s,iPhone 5s,iPhone 6s" --languages "$lang" \
            --output_directory "l10n-screenshots/$lang" > "l10n-screenshots/$lang/snapshot.log" 2>&1
