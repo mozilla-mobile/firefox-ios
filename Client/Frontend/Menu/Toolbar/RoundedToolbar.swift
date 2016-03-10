cf2e41f634fdd573f252ee4b17ee580e0e618222
Emily Toop <etoop@mozilla.com>
2016-03-10 18:34:22 +0000

    Bug 1254567 - Add custom Toolbar to provide rounded corners.

---
+++ Client/Frontend/Menu/Toolbar/RoundedToolbar.swift
@@ -0,0 +1,43 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import UIKit
+
+class RoundedToolbar: UIToolbar {
+
+    private var layerBackgroundColor: UIColor?
+
+    override var backgroundColor: UIColor? {
+        get { return layerBackgroundColor }
+
+        set {
+            layerBackgroundColor = newValue
+        }
+    }
+
+    var cornersToRound: UIRectCorner = [.AllCorners]
+
+    /**
+     * The toolbar on the menu requires rounded corners on the top and bottom so we need a custom
+     * view to do this
+     */
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+        let maskPath = UIBezierPath(roundedRect: rect, byRoundingCorners: cornersToRound, cornerRadii: CGSizeMake(10.0,10.0))
+
+        // Create the shape layer and set its path
+        let maskLayer = CAShapeLayer()
+        maskLayer.frame = rect
+        maskLayer.path = maskPath.CGPath
+
+        let roundedLayer = CALayer()
+        roundedLayer.backgroundColor = layerBackgroundColor?.CGColor ?? UIColor.whiteColor().CGColor
+        roundedLayer.frame = rect
+        roundedLayer.mask = maskLayer
+
+        layer.insertSublayer(roundedLayer, atIndex: 0)
+    }
+
+
+}
