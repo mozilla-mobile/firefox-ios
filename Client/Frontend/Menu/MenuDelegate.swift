dca71870c288b27be4acfe564498a367ddb4d2ce
Emily Toop <etoop@mozilla.com>
2016-03-10 16:11:28 +0000

    Bug 1254567 - Create menu view * create delegate and data source for menu toolbar items * create delegate and data source for menu items * adds a MenuItemView to a container view for each menu item

---
+++ Client/Frontend/Menu/MenuDelegate.swift
@@ -0,0 +1,14 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import Foundation
+import UIKit
+
+protocol MenuToolbarItemDelegate {
+    func menuView(menuView: MenuView, didSelectItemAtIndex index: Int)
+}
+
+protocol MenuItemDelegate {
+    func menuView(menuView: MenuView, didSelectItemAtIndexPath indexPath: NSIndexPath)
+}
