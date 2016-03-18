f7293de3559acaf88858a4d499f4bc6d51811b5e
Stefan Arentz <stefan@arentz.ca>
2016-03-17 22:15:31 -0400

    Bug 1257574 - Added the SnapshotTestHandler

--- Client/Application/AppDelegate.swift
+++ Client/Application/AppDelegate.swift
@@ -340,6 +340,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         AboutHomeHandler.register(server)
         AboutLicenseHandler.register(server)
         SessionRestoreHandler.register(server)
+        if AppConstants.IsRunningFastlaneSnapshot {
+            SnapshotTestHandler.register(server)
+        }
         // Bug 1223009 was an issue whereby CGDWebserver crashed when moving to a background task
         // catching and handling the error seemed to fix things, but we're not sure why.
         // Either way, not implicitly unwrapping a try is not a great way of doing things
