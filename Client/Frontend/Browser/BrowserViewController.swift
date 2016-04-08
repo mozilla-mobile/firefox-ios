14a352d5e21753add601866592dfe2534e1bbc07
Emily Toop <etoop@mozilla.com>
2016-04-08 10:25:59 +0100

    Bug 1254586 - Implement Find In Page menu action

--- Client/Frontend/Browser/BrowserViewController.swift
+++ Client/Frontend/Browser/BrowserViewController.swift
@@ -1176,7 +1176,10 @@ extension BrowserViewController: MenuActionDelegate {
                     self.openURLInNewTab(nil, isPrivate: true)
                 }
             }
-
+        case .FindInPage:
+            dispatch_async(dispatch_get_main_queue()) {
+                self.updateFindInPageVisibility(visible: true)
+            }
         default: break
         }
     }
