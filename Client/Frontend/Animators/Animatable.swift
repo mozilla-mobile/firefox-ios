e2eadad821418a92283d03468f40175dc11885ce
Emily Toop <etoop@mozilla.com>
2016-04-12 15:57:49 +0100

    MPL

--- Client/Frontend/Animators/Animatable.swift
+++ Client/Frontend/Animators/Animatable.swift
@@ -1,10 +1,6 @@
-//
-//  Animatable.swift
-//  Client
-//
-//  Created by Emily Toop on 08/04/2016.
-//  Copyright © 2016 Mozilla. All rights reserved.
-//
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import UIKit
 
