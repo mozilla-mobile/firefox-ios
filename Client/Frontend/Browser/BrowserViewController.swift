e85b23a88abbe532136456d090711d4501cea091
jhugman <jhugman@users.noreply.github.com>
2016-04-22 12:04:05 +0100

    Bug 1265688 – Cancel editing of URL bar when entering app from external URL. (#1729)
    This affects opening links from external apps and today widget. It also affects opening bookmarks from 3DT.
    
    It does not affect opening new tabs.

--- Client/Frontend/Browser/BrowserViewController.swift
+++ Client/Frontend/Browser/BrowserViewController.swift
@@ -1008,6 +1008,8 @@ class BrowserViewController: UIViewController {
         if currentViewController != self,
             let _ = forTab {
             self.navigationController?.popViewControllerAnimated(true)
+        } else if urlBar.inOverlayMode {
+            urlBar.SELdidClickCancel()
         }
     }
 
