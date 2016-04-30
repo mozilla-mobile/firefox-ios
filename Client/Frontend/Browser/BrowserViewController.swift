320eaf1c4783457cf28ad3380c9c9d4b1f482f20
James Hugman <james@hugman.tv>
2016-04-30 21:24:21 +0100

    Bug 1269315 – Add OpenHomePage behavior to selected tab.

--- Client/Frontend/Browser/BrowserViewController.swift
+++ Client/Frontend/Browser/BrowserViewController.swift
@@ -1182,6 +1182,9 @@ extension BrowserViewController: MenuActionDelegate {
                 openHomePanel(.History, forAppState: appState)
             case .OpenReadingList:
                 openHomePanel(.ReadingList, forAppState: appState)
+            case .OpenHomePage:
+                guard let tab = tabManager.selectedTab else { break }
+                HomePageHelper(prefs: profile.prefs).openHomePage(tab)
             default: break
             }
         }
