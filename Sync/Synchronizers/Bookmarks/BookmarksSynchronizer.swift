14a6872612d82b8d5eb2c12febdf48944e98082e
Stephan Leroux <sleroux@mozilla.com>
2016-05-05 15:42:27 -0400

    Bug 1260899 - Detect buffer validation errors and pass error through Deferred to caller

--- Sync/Synchronizers/Bookmarks/BookmarksSynchronizer.swift
+++ Sync/Synchronizers/Bookmarks/BookmarksSynchronizer.swift
@@ -178,9 +178,13 @@ class MergeApplier {
             return deferMaybe(SyncStatus.Completed)
         }
 
-        return self.merger.merge()
-          >>== self.applyResult
-           >>> always(SyncStatus.Completed)
+        return self.merger.merge().bind { result in
+            // Pass on any failures we get when trying to merge
+            if let fail = result.failureValue {
+                return deferMaybe(fail)
+            }
+            return self.applyResult(result.successValue!) >>> always(SyncStatus.Completed)
+        }
     }
 }
 
@@ -361,10 +365,14 @@ class ThreeWayBookmarksStorageMerger: BookmarksStorageMerger {
                     return self.applyLocalDirectlyToMirror()
                 case (false, true):
                     // No outgoing changes. Unilaterally apply remote changes if they're consistent.
-                    return self.buffer.validate() >>> self.applyIncomingDirectlyToMirror
+                    return self.buffer.validate().bind { result in
+                        result.isSuccess ? self.applyIncomingDirectlyToMirror() : deferMaybe(BookmarksMergeConsistencyError())
+                    }
                 default:
                     // Changes on both sides. Merge.
-                    return self.buffer.validate() >>> self.threeWayMerge
+                    return self.buffer.validate().bind { result in
+                        result.isSuccess ? self.threeWayMerge() : deferMaybe(BookmarksMergeConsistencyError())
+                    }
                 }
             }
         }
