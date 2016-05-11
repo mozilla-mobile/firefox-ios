55d85ebea3e0e7f9961ded1835fff85d54dd5350
Stefan Arentz <stefan@arentz.ca>
2016-05-11 13:31:04 -0400

    Bug 1270693 - FindInPageBar Keyboard Improvements (#1782)
    * Bug 1270693 - Do not show the shortcuts bar under the Find In Page bar
    
    * Bug 1270693 - Auto enable the return key if the field has text in it
    
    * Bug 1270693 - Change the 'enter' key to 'Search'

--- Client/Frontend/Browser/FindInPageBar.swift
+++ Client/Frontend/Browser/FindInPageBar.swift
@@ -63,6 +63,12 @@ class FindInPageBar: UIView {
         searchText.font = FindInPageUX.SearchTextFont
         searchText.autocapitalizationType = UITextAutocapitalizationType.None
         searchText.autocorrectionType = UITextAutocorrectionType.No
+        if #available(iOS 9.0, *) {
+            searchText.inputAssistantItem.leadingBarButtonGroups = []
+            searchText.inputAssistantItem.trailingBarButtonGroups = []
+        }
+        searchText.enablesReturnKeyAutomatically = true
+        searchText.returnKeyType = .Search
         addSubview(searchText)
 
         matchCountView.textColor = FindInPageUX.MatchCountColor
