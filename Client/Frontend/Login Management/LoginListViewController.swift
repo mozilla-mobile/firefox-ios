ed9b575f4152b76d43f63c2eae755b1382528144
Stephan Leroux <sleroux@mozilla.com>
2016-06-08 10:13:17 -0400

    Bug 1278224 - Wrapped loadLogins selector with parameter-less version to avoid Swift default param selector bug (#1888) r=fluffyemily

--- Client/Frontend/Login Management/LoginListViewController.swift
+++ Client/Frontend/Login Management/LoginListViewController.swift
@@ -85,7 +85,7 @@ class LoginListViewController: SensitiveViewController {
         super.viewDidLoad()
 
         let notificationCenter = NSNotificationCenter.defaultCenter()
-        notificationCenter.addObserver(self, selector: #selector(LoginListViewController.loadLogins(_:)), name: NotificationDataRemoteLoginChangesWereApplied, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(LoginListViewController.remoteLoginsDidChange), name: NotificationDataRemoteLoginChangesWereApplied, object: nil)
 
         automaticallyAdjustsScrollViewInsets = false
         self.view.backgroundColor = UIColor.whiteColor()
@@ -186,7 +186,11 @@ class LoginListViewController: SensitiveViewController {
 
 // MARK: - Selectors
 private extension LoginListViewController {
-    @objc func loadLogins(query: String? = nil) {
+    @objc func remoteLoginsDidChange() {
+        loadLogins()
+    }
+
+    func loadLogins(query: String? = nil) {
         loadingStateView.hidden = false
 
         // Fill in an in-flight query and re-query
