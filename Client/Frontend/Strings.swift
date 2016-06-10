eecb67562525b6117de478af19a5aa69532a47ec
jhugman <jhugman@users.noreply.github.com>
2016-06-10 16:50:27 +0100

    Bug 1279500 – Land strings for Night mode and No image mode. (#1914)

--- Client/Frontend/Strings.swift
+++ Client/Frontend/Strings.swift
@@ -188,6 +188,20 @@ extension Strings {
     public static let UnableToAddPassErrorDismiss = NSLocalizedString("AddPass.Error.Dismiss", value: "OK", comment: "Button to dismiss the 'Add Pass Failed' alert.  See https://support.apple.com/HT204003 for context on Wallet.")
 }
 
+// Night mode
+extension Strings {
+    public static let MenuNightModeTitleString = NSLocalizedString("Menu.NightModeAction.Title", value: "Night Mode",  comment: "Label for the button, displayed in the menu, used to toggle night mode on or off")
+    public static let MenuNightModeTurnOnTitleString = NSLocalizedString("Menu.NightModeTurnOnAction.Title", value: "Night Mode",  comment: "Label for the button, displayed in the menu, used to turn night mode on")
+    public static let MenuNightModeTurnOffTitleString = NSLocalizedString("Menu.NightModeTurnOffAction.Title", value: "Night Mode Off", comment: "Label for the button, displayed in the menu, used to turn night mode off")
+}
+
+// No image mode
+extension Strings {
+    public static let MenuNoImageModeTitleString = NSLocalizedString("Menu.NoImageModeAction.Title", value: "No Image Mode",  comment: "Label for the button, displayed in the menu, used to toggle no image mode on or off")
+    public static let MenuNoImageModeTurnOnTitleString = NSLocalizedString("Menu.NoImageModeTurnOnAction.Title", value: "Hide Images",  comment: "Label for the button, displayed in the menu, used to turn no image mode on")
+    public static let MenuNoImageModeTurnOffTitleString = NSLocalizedString("Menu.NoImageModeTurnOffAction.Title", value: "Show Images", comment: "Label for the button, displayed in the menu, used to turn no image mode off")
+}
+
 // open in
 
 extension Strings {
