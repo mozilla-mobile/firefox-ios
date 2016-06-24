59bc5ba3b0579f03d2325fb9516ade6bb85fca49
Stefan Arentz <stefan@arentz.ca>
2016-06-24 10:23:40 -0400

    No Bug - Adding accessibilityIdentifier for TabTrayController.maskButton

--- Client/Frontend/Browser/TabTrayController.swift
+++ Client/Frontend/Browser/TabTrayController.swift
@@ -1141,6 +1141,7 @@ class TrayToolbar: UIView {
         let button = ToggleButton()
         button.accessibilityLabel = PrivateModeStrings.toggleAccessibilityLabel
         button.accessibilityHint = PrivateModeStrings.toggleAccessibilityHint
+        button.accessibilityIdentifier = "TabTrayController.maskButton"
         return button
     }()
 
