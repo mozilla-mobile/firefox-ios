d8e33778a08946f9677d3ae0a4eeef20995ac94d
varkor <github@varkor.com>
2016-06-24 14:30:30 -0700

    Bug 1274290 - Using google search operations that have the format 'operation:' do nothing when searching
    By only accepting URLs that have valid schemes (i.e. on the approved list of URI schemes), we can now make searching in the form “command:value” (used by Google, for instance). It would hypothetically need to be updated if this list changes, but as the list changes rarely (not considering the provisional schemes), this would not appear to be an issue.

--- Utils/Extensions/NSURLExtensions.swift
+++ Utils/Extensions/NSURLExtensions.swift
@@ -90,6 +90,9 @@ extension NSURL {
     }
 }
 
+// The list of permanent URI schemes has been taken from http://www.iana.org/assignments/uri-schemes/uri-schemes.xhtml 
+private let permanentURISchemes = ["aaa", "aaas", "about", "acap", "acct", "cap", "cid", "coap", "coaps", "crid", "data", "dav", "dict", "dns", "example", "file", "ftp", "geo", "go", "gopher", "h323", "http", "https", "iax", "icap", "im", "imap", "info", "ipp", "ipps", "iris", "iris.beep", "iris.lwz", "iris.xpc", "iris.xpcs", "jabber", "ldap", "mailto", "mid", "msrp", "msrps", "mtqp", "mupdate", "news", "nfs", "ni", "nih", "nntp", "opaquelocktoken", "pkcs11", "pop", "pres", "reload", "rtsp", "rtsps", "rtspu", "service", "session", "shttp", "sieve", "sip", "sips", "sms", "snmp", "soap.beep", "soap.beeps", "stun", "stuns", "tag", "tel", "telnet", "tftp", "thismessage", "tip", "tn3270", "turn", "turns", "tv", "urn", "vemmi", "vnc", "ws", "wss", "xcon", "xcon-userid", "xmlrpc.beep", "xmlrpc.beeps", "xmpp", "z39.50r", "z39.50s"]
+
 extension NSURL {
 
     public func withQueryParams(params: [NSURLQueryItem]) -> NSURL {
@@ -235,6 +238,14 @@ extension NSURL {
 
     public var isLocal: Bool {
         return host?.lowercaseString == "localhost" || host == "127.0.0.1" || host == "::1"
+    }
+    
+    /**
+     Returns whether the URL's scheme is one of those listed on the official list of URI schemes.
+     This only accepts permanent schemes: historical and provisional schemes are not accepted.
+     */
+    public var schemeIsValid: Bool {
+        return permanentURISchemes.contains(scheme)
     }
 }
 
