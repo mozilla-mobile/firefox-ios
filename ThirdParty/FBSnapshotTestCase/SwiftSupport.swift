ea20afbabc4ec6acfca0bf7ee6c3e45acd6de6c9
Stephan Leroux <sleroux@mozilla.com>
2016-06-27 15:52:09 -0400

    Converts Facebook's SnapshotTestCase helper to Swift 3.0

--- ThirdParty/FBSnapshotTestCase/SwiftSupport.swift
+++ ThirdParty/FBSnapshotTestCase/SwiftSupport.swift
@@ -9,33 +9,33 @@
 */
 
 public extension FBSnapshotTestCase {
-  public func FBSnapshotVerifyView(view: UIView, identifier: String = "", suffixes: NSOrderedSet = FBSnapshotTestCaseDefaultSuffixes(), file: String = #file, line: UInt = #line) {
+  public func FBSnapshotVerifyView(_ view: UIView, identifier: String = "", suffixes: OrderedSet = FBSnapshotTestCaseDefaultSuffixes(), file: String = #file, line: UInt = #line) {
     FBSnapshotVerifyViewOrLayer(view, identifier: identifier, suffixes: suffixes)
   }
 
-  public func FBSnapshotVerifyLayer(layer: CALayer, identifier: String = "", suffixes: NSOrderedSet = FBSnapshotTestCaseDefaultSuffixes(), file: String = #file, line: UInt = #line) {
+  public func FBSnapshotVerifyLayer(_ layer: CALayer, identifier: String = "", suffixes: OrderedSet = FBSnapshotTestCaseDefaultSuffixes(), file: String = #file, line: UInt = #line) {
     FBSnapshotVerifyViewOrLayer(layer, identifier: identifier, suffixes: suffixes)
   }
 
-  private func FBSnapshotVerifyViewOrLayer(viewOrLayer: AnyObject, identifier: String = "", suffixes: NSOrderedSet = FBSnapshotTestCaseDefaultSuffixes(), file: StaticString = #file, line: UInt = #line) {
-    let envReferenceImageDirectory = self.getReferenceImageDirectoryWithDefault(FB_REFERENCE_IMAGE_DIR)
+  private func FBSnapshotVerifyViewOrLayer(_ viewOrLayer: AnyObject, identifier: String = "", suffixes: OrderedSet = FBSnapshotTestCaseDefaultSuffixes(), file: StaticString = #file, line: UInt = #line) {
+    let envReferenceImageDirectory = self.getReferenceImageDirectory(withDefault: FB_REFERENCE_IMAGE_DIR)
     var error: NSError?
     var comparisonSuccess = false
 
     if let envReferenceImageDirectory = envReferenceImageDirectory {
       for suffix in suffixes {
         let referenceImagesDirectory = "\(envReferenceImageDirectory)\(suffix)"
-        if viewOrLayer.isKindOfClass(UIView) {
+        if viewOrLayer.isKind(UIView) {
           do {
-            try compareSnapshotOfView(viewOrLayer as! UIView, referenceImagesDirectory: referenceImagesDirectory, identifier: identifier, tolerance: 0)
+            try compareSnapshot(of: viewOrLayer as! UIView, referenceImagesDirectory: referenceImagesDirectory, identifier: identifier, tolerance: 0)
             comparisonSuccess = true
           } catch let error1 as NSError {
             error = error1
             comparisonSuccess = false
           }
-        } else if viewOrLayer.isKindOfClass(CALayer) {
+        } else if viewOrLayer.isKind(CALayer) {
           do {
-            try compareSnapshotOfLayer(viewOrLayer as! CALayer, referenceImagesDirectory: referenceImagesDirectory, identifier: identifier, tolerance: 0)
+            try compareSnapshot(of: viewOrLayer as! CALayer, referenceImagesDirectory: referenceImagesDirectory, identifier: identifier, tolerance: 0)
             comparisonSuccess = true
           } catch let error1 as NSError {
             error = error1
@@ -58,7 +58,7 @@ public extension FBSnapshotTestCase {
     }
   }
 
-  func assert(assertion: Bool, message: String, file: StaticString, line: UInt) {
+  func assert(_ assertion: Bool, message: String, file: StaticString, line: UInt) {
     if !assertion {
       XCTFail(message, file: file, line: line)
     }
