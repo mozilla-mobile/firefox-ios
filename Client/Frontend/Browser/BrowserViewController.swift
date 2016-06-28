e3940ee453a41c805be636319e562414402dc19d
varkor <github@varkor.com>
2016-06-28 10:38:29 -0700

    Bug 1171522 – Closing cross-origin windows
    Cross-origin window.close() calls are now supported. This case occurred
    with Persona login windows, which were not closing. Firefox now has the
    same behaviour as Safari in this case.

--- Client/Frontend/Browser/BrowserViewController.swift
+++ Client/Frontend/Browser/BrowserViewController.swift
@@ -1745,9 +1745,11 @@ extension BrowserViewController: TabDelegate {
         let errorHelper = ErrorPageHelper()
         tab.addHelper(errorHelper, name: ErrorPageHelper.name())
 
-        let windowCloseHelper = WindowCloseHelper(tab: tab)
-        windowCloseHelper.delegate = self
-        tab.addHelper(windowCloseHelper, name: WindowCloseHelper.name())
+        if #available(iOS 9, *) {} else {
+            let windowCloseHelper = WindowCloseHelper(tab: tab)
+            windowCloseHelper.delegate = self
+            tab.addHelper(windowCloseHelper, name: WindowCloseHelper.name())
+        }
 
         let findInPageHelper = FindInPageHelper(tab: tab)
         findInPageHelper.delegate = self
@@ -2479,6 +2481,13 @@ extension BrowserViewController: WKUIDelegate {
 
         openInHelper.open()
         decisionHandler(WKNavigationResponsePolicy.Cancel)
+    }
+    
+    @available(iOS 9, *)
+    func webViewDidClose(webView: WKWebView) {
+        if let tab = tabManager[webView] {
+            self.tabManager.removeTab(tab)
+        }
     }
 }
 
