a2cfdef657671307347cfe1454b74139094995db
varkor <github@varkor.com>
2016-06-28 15:06:08 -0700

    Bug 1282951 – "Add search engine button visible" even in invalid cases
    This replaces the script to check whether an input field is valid to be
    used for searching with the code that actually attempts to build the
    query. The two scripts for this are unnecessary.

--- Client/Assets/CustomSearchHelper.js
+++ Client/Assets/CustomSearchHelper.js
@@ -8,13 +8,6 @@ if (!window.__firefox__) {
 }
 
 (function() {
-  window.__firefox__.isActiveElementSearchField = function() {
-    var input = document.activeElement;
-    if (input.tagName.toLowerCase() !== "input") return false;
-    var form = input.form;
-    return form && form.method && form.method.toLowerCase() == 'get';
-  };
-
   window.__firefox__.searchQueryForField = function() {
     var input = document.activeElement;
     if (input.tagName.toLowerCase() !== "input") return null;
