3a037216e7234a01126ea250bd313bd2aedf903a
Emily Toop <etoop@mozilla.com>
2016-07-04 14:45:09 +0100

    Remove reading list tests that rely on attributed string accessibility labels as they cannot be found for interaction with KIF Just commenting them out though as we might find a way of doing this later and reinstate them

--- UITests/ReaderViewUITests.swift
+++ UITests/ReaderViewUITests.swift
@@ -20,10 +20,10 @@ class ReaderViewUITests: KIFTestCase, UITextFieldDelegate {
         addToReadingList()
         markAsReadFromReaderView()
         markAsUnreadFromReaderView()
-        markAsReadFromReadingList()
-        markAsUnreadFromReadingList()
-        removeFromReadingList()
-        addToReadingList()
+//        markAsReadFromReadingList()
+//        markAsUnreadFromReadingList()
+//        removeFromReadingList()
+//        addToReadingList()
         removeFromReadingListInView()
     }
 
