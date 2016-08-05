b0ae040ca40fcc6d9a854a7e7a89b98b2d379c81
varkor <github@varkor.com>
2016-08-05 11:27:55 -0700

    Bug 1292236 – Tab tray button reverts to normal theme when pressed on a new private tab
    This is the biggest problem with property observers. It’s so hard to
    track what’s actually going on, because it’s unclear when a variable
    assignment is getting modified behind the scenes without looking
    through _all_ the declarations.

--- Client/Frontend/Widgets/TabsButton.swift
+++ Client/Frontend/Widgets/TabsButton.swift
@@ -134,6 +134,8 @@ class TabsButton: UIControl {
         button.accessibilityLabel = accessibilityLabel
         button.titleLabel.text = titleLabel.text
 
+        button.theme = theme
+
         // Copy all of the styable properties over to the new TabsButton
         button.titleLabel.font = titleLabel.font
         button.titleLabel.textColor = titleLabel.textColor
