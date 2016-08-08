6630e6bf1751e85294ae21574dbf746d1310f5dd
varkor <github@varkor.com>
2016-08-08 11:24:19 -0700

    Fixed the BaseCaseText license header

--- XCUITests/BaseTestCase.swift
+++ XCUITests/BaseTestCase.swift
@@ -1,10 +1,6 @@
-//
-//  BaseTestCase.swift
-//  Client
-//
-//  Created by Farhan Patel on 7/14/16.
-//  Copyright © 2016 Mozilla. All rights reserved.
-//
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import XCTest
 
