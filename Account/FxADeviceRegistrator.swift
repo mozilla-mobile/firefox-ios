f0591bc0c194d8236fb09ab706b31c8250a74b5b
Brian Nicholson <bnicholson@mozilla.com>
2016-08-19 11:46:15 -0700

    Review comment: Clean up error handling

--- Account/FxADeviceRegistrator.swift
+++ Account/FxADeviceRegistrator.swift
@@ -13,113 +13,104 @@ private let log = Logger.syncLogger
 private let DeviceRegistrationVersion = 1
 
 public enum FxADeviceRegistrationResult {
-    case RegisteredOrUpdated
+    case Registered
+    case Updated
     case AlreadyRegistered
 }
 
-public class FxADeviceRegistrator {
-    public enum DeviceRegistratorError: MaybeErrorType {
-        case DeviceRegistrationFailed
+public enum FxADeviceRegistratorError: MaybeErrorType {
+    case AccountDeleted
+    case CurrentDeviceNotFound
+    case InvalidSession
 
-        public var description: String {
-            switch self {
-            case DeviceRegistrationFailed: return "Device registration failed."
-            }
+    public var description: String {
+        switch self {
+        case AccountDeleted: return "Account no longer exists."
+        case CurrentDeviceNotFound: return "Current device not found."
+        case InvalidSession: return "Session token was invalid."
         }
     }
+}
 
+public class FxADeviceRegistrator {
     public static func registerOrUpdateDevice(account: FirefoxAccount, state: MarriedState) -> Deferred<Maybe<FxADeviceRegistrationResult>> {
         if let _ = account.fxaDeviceId where account.deviceRegistrationVersion == DeviceRegistrationVersion {
-            return Deferred(value: Maybe(success: FxADeviceRegistrationResult.AlreadyRegistered))
+            return deferMaybe(FxADeviceRegistrationResult.AlreadyRegistered)
         }
 
         let client = FxAClient10(endpoint: account.configuration.authEndpointURL)
         let sessionToken = state.sessionToken
         let name = DeviceInfo.defaultClientName()
         let device: FxADevice
+        let registrationResult: FxADeviceRegistrationResult
         if let deviceId = account.fxaDeviceId {
             device = FxADevice.forUpdate(name, id: deviceId)
+            registrationResult = FxADeviceRegistrationResult.Updated
         } else {
             device = FxADevice.forRegister(name, type: "mobile")
+            registrationResult = FxADeviceRegistrationResult.Registered
         }
 
-        return client.registerOrUpdateDevice(sessionToken, device: device).bind { result in
-            let deferred = Deferred<Maybe<FxADeviceRegistrationResult>>()
-
-            if let device = result.successValue,
-                let deviceId = device.id {
-                account.fxaDeviceId = deviceId
-                account.deviceRegistrationVersion = DeviceRegistrationVersion
-                deferred.fill(Maybe(success: FxADeviceRegistrationResult.RegisteredOrUpdated))
-                return deferred
+        let registeredDevice = client.registerOrUpdateDevice(sessionToken, device: device)
+        let deviceId: Deferred<Maybe<String>> = registeredDevice.bind { result in
+            if let device = result.successValue {
+                return deferMaybe(device.id!)
             }
 
-            let error = result.failureValue as? FxAClientError
-            switch (error) {
-            case let .Remote(remoteError)?:
+            // Recover from the error -- if we can.
+            if let error = result.failureValue as? FxAClientError,
+               case .Remote(let remoteError) = error {
                 switch (remoteError.code) {
                 case FxAccountRemoteError.DeviceSessionConflict:
-                    recoverFromDeviceSessionConflict(account, client: client, deferred: deferred, sessionToken: sessionToken)
-                case FxAccountRemoteError.UnknownDevice:
-                    // TODO: Shouldn't this also clear registration version? Android doesn't.
-                    handleUnknownDevice(account)
+                    return recoverFromDeviceSessionConflict(account, client: client, sessionToken: sessionToken)
                 case FxAccountRemoteError.InvalidAuthenticationToken:
-                    logErrorAndResetDeviceRegistrationVersion(account, description: String(remoteError))
-                    handleTokenError(account, client: client)
-                default:
-                    logErrorAndResetDeviceRegistrationVersion(account, description: String(remoteError))
-                }
-            case let .Local(localError)?:
-                self.logErrorAndResetDeviceRegistrationVersion(account, description: localError.description)
-            default:
-                // This shouldn't happen.
-                self.logErrorAndResetDeviceRegistrationVersion(account, description: "invalid error type")
-                assertionFailure()
-            }
-
-            deferred.fillIfUnfilled(Maybe(failure: DeviceRegistratorError.DeviceRegistrationFailed))
-
-            return deferred
-        }
-    }
-
-    private static func handleUnknownDevice(account: FirefoxAccount) {
-        log.info("unknown device id, clearing the cached device id")
-        account.fxaDeviceId = nil
-    }
-
-    private static func recoverFromDeviceSessionConflict(account: FirefoxAccount, client: FxAClient10, deferred: Deferred<Maybe<FxADeviceRegistrationResult>>, sessionToken: NSData) {
-        log.warning("device session conflict, attempting to ascertain the correct device id")
-        client.devices(sessionToken).upon { response in
-            if let success = response.successValue,
-               let currentDevice = success.devices.find({ $0.isCurrentDevice! }) {
-                account.fxaDeviceId = currentDevice.id
-                deferred.fill(Maybe(success: FxADeviceRegistrationResult.RegisteredOrUpdated))
-            } else {
-                self.logErrorAndResetDeviceRegistrationVersion(account, description: "conflict recovery failed: \(response.failureValue?.description ?? "")")
-                deferred.fill(Maybe(failure: DeviceRegistratorError.DeviceRegistrationFailed))
-            }
-        }
-    }
-
-    private static func handleTokenError(account: FirefoxAccount, client: FxAClient10) {
-        client.status(account.uid).upon() { result in
-            if let status = result.successValue {
-                if !status.exists {
-                    log.info("token was invalidated because the account no longer exists")
-                    // TODO: Should be in a "I have an Android account, but the FxA is gone." State.
-                    // This will do for now..
-                    account.makeDoghouse()
-                } else {
-                    log.error("the session token was invalid")
-                    account.makeDoghouse()
+                    return recoverFromTokenError(account, client: client)
+                default: break
                 }
             }
+
+            // Not an error we can recover from. Rethrow it and fall back to the failure handler.
+            return deferMaybe(result.failureValue!)
+        }
+
+        // Post-recovery. We either got the device ID or we didn't, but update the account either way.
+        return deviceId.bind { result in
+            switch result {
+            case .Success(let deviceId):
+                account.fxaDeviceId = deviceId.value
+                account.deviceRegistrationVersion = DeviceRegistrationVersion
+                return deferMaybe(registrationResult)
+            case .Failure(let error):
+                log.error("Device registration failed: \(error.description)")
+                account.fxaDeviceId = nil
+                account.deviceRegistrationVersion = 0
+                return deferMaybe(error)
+            }
         }
     }
 
-    private static func logErrorAndResetDeviceRegistrationVersion(account: FirefoxAccount, description: String) {
-        log.error("device registration failed: \(description)")
-        account.deviceRegistrationVersion = 0
+    private static func recoverFromDeviceSessionConflict(account: FirefoxAccount, client: FxAClient10, sessionToken: NSData) -> Deferred<Maybe<String>> {
+        log.warning("Device session conflict. Attempting to find the current device ID…")
+        return client.devices(sessionToken) >>== { response in
+            guard let currentDevice = response.devices.find({ $0.isCurrentDevice! }) else {
+                return deferMaybe(FxADeviceRegistratorError.CurrentDeviceNotFound)
+            }
+
+            return deferMaybe(currentDevice.id!)
+        }
+    }
+
+    private static func recoverFromTokenError(account: FirefoxAccount, client: FxAClient10) -> Deferred<Maybe<String>> {
+        return client.status(account.uid) >>== { status in
+            if !status.exists {
+                // TODO: Should be in an "I have an iOS account, but the FxA is gone." state.
+                // This will do for now...
+                account.makeDoghouse()
+                return deferMaybe(FxADeviceRegistratorError.AccountDeleted)
+            }
+
+            account.makeDoghouse()
+            return deferMaybe(FxADeviceRegistratorError.InvalidSession)
+        }
     }
 }
