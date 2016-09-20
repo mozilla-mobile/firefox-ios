8146275b24de5b4cfdbf4953243071ae189f41f4
farhanpatel <farhanpatel.17@gmail.com>
2016-09-19 17:11:29 -0700

    Bug 1302904 - Add a new util function that generates a default color to use for backgrounds in AS.

--- Utils/FaviconFetcher.swift
+++ Utils/FaviconFetcher.swift
@@ -210,6 +210,9 @@ public class FaviconFetcher: NSObject, NSXMLParserDelegate {
         return deferred
     }
 
+
+
+
     // Returns the default favicon for a site based on the first letter of the site's domain
     class func getDefaultFavicon(url: NSURL) -> UIImage {
         guard let character = url.baseDomain()?.characters.first else {
@@ -235,6 +238,18 @@ public class FaviconFetcher: NSObject, NSXMLParserDelegate {
 
         characterToFaviconCache[faviconLetter] = faviconImage
         return faviconImage
+    }
+
+    static let DefaultColors = ["2e761a", "399320", "40a624", "57bd35", "70cf5b", "90e07f", "b1eea5", "881606", "aa1b08", "c21f09", "d92215", "ee4b36", "f67964", "ffa792", "025295", "0568ba", "0675d3", "0996f8", "2ea3ff", "61b4ff", "95cdff", "00736f", "01908b", "01a39d", "01bdad", "27d9d2", "58e7e6", "89f4f5", "c84510", "e35b0f", "f77100", "ff9216", "ffad2e", "ffc446", "ffdf81", "911a2e", "b7223b", "cf2743", "ea385e", "fa526e", "ff7a8d", "ffa7b3" ]
+
+    // Returns a color based on the url's hash
+    class func getDefaultColor(url: NSURL) -> UIColor {
+        guard let hash = url.baseDomain()?.hashValue else {
+            return UIColor.grayColor()
+        }
+        let index = abs(hash) % (DefaultColors.count - 1)
+        let colorHex = DefaultColors[index]
+        return UIColor(colorString: colorHex)
     }
 }
 
