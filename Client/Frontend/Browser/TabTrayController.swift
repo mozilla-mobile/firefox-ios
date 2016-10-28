0705b8681e6bbfdfc2dfb0b523bee8d5d408e81b
Farhan Patel <farhanpatel.17@gmail.com>
2016-10-29 00:19:51 +0900

    Bug 1300499 - Prevent tab resize animation when returning to tabs tray from settings. (#2190) r=thebnich

--- Client/Frontend/Browser/TabTrayController.swift
+++ Client/Frontend/Browser/TabTrayController.swift
@@ -411,8 +411,9 @@ class TabTrayController: UIViewController {
 
         // Update the trait collection we reference in our layout delegate
         tabLayoutDelegate.traitCollection = traitCollection
+        self.collectionView.collectionViewLayout.invalidateLayout()
     }
-    
+
     private func cancelExistingGestures() {
         if let visibleCells = self.collectionView.visibleCells() as? [TabCell] {
             for cell in visibleCells {
