3ec3b70d4ada41c79d376c83098d52270cd60696
Brian Nicholson <bnicholson@mozilla.com>
2016-11-09 16:10:55 -0800

    [focus-ios] Dim InsetButton on touch

--- focus-ios/Blockzilla/InsetButton.swift
+++ focus-ios/Blockzilla/InsetButton.swift
@@ -8,9 +8,32 @@ import Foundation
  * Button whose insets are included in its intrinsic size.
  */
 class InsetButton: UIButton {
+    init() {
+        super.init(frame: CGRect.zero)
+
+        addTarget(self, action: #selector(didTouchDismiss), for: [.touchDown, .touchDragEnter])
+        addTarget(self, action: #selector(didTouchUpDismiss), for: [.touchDragExit, .touchUpInside, .touchUpOutside])
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     override var intrinsicContentSize: CGSize {
         let size = super.intrinsicContentSize
         return CGSize(width: size.width + titleEdgeInsets.left + titleEdgeInsets.right,
                       height: size.height + titleEdgeInsets.top + titleEdgeInsets.bottom)
+    }
+
+    @objc private func didTouchDismiss() {
+        UIView.animate(withDuration: 0.1) {
+            self.alpha = 0.5
+        }
+    }
+
+    @objc private func didTouchUpDismiss() {
+        UIView.animate(withDuration: 0.1) {
+            self.alpha = 1
+        }
     }
 }
