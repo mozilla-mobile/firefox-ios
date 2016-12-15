8e2037b9d7e62b8abd6b135985d8006cac1c2dfd
Farhan Patel <farhanpatel.17@gmail.com>
2016-12-14 20:38:49 -1000

    Bug 1322121 - Make sure home panels are displayed after session restore. (#2277) r=sleroux

--- Utils/AboutUtils.swift
+++ Utils/AboutUtils.swift
@@ -8,20 +8,37 @@ struct AboutUtils {
     private static let AboutPath = "/about/"
 
     static func isAboutHomeURL(url: NSURL?) -> Bool {
+        guard var url = url else {
+            return false
+        }
+        if let urlString = url.getQuery()["url"]?.unescape() where isErrorPageURL(url) {
+            url = NSURL(string: urlString) ?? url
+        }
         return getAboutComponent(url) == "home"
     }
 
     static func isAboutURL(url: NSURL?) -> Bool {
+        guard let url = url else {
+            return false
+        }
         return getAboutComponent(url) != nil
+    }
+
+    static func isErrorPageURL(url: NSURL) -> Bool {
+        if let host = url.host, path = url.path {
+            return url.scheme == "http" && host == "localhost" && path == "/errors/error.html"
+        }
+        return false
     }
 
     /// If the URI is an about: URI, return the path after "about/" in the URI.
     /// For example, return "home" for "http://localhost:1234/about/home/#panel=0".
     static func getAboutComponent(url: NSURL?) -> String? {
-        if let scheme = url?.scheme, host = url?.host, path = url?.path {
-            if scheme == "http" && host == "localhost" && path.startsWith(AboutPath) {
-                return path.substringFromIndex(AboutPath.endIndex)
-            }
+        guard let scheme = url?.scheme, host = url?.host, path = url?.path else {
+            return nil
+        }
+        if scheme == "http" && host == "localhost" && path.startsWith(AboutPath) {
+            return path.substringFromIndex(AboutPath.endIndex)
         }
         return nil
     }
