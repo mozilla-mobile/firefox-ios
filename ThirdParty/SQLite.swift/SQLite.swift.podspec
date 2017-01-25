d0264d0cb1cbe36a7fa86040466a512b736d8231
Emily Toop <etoop@mozilla.com>
2017-01-25 13:04:55 +0000

    Upgrade SQLite.swift to Swift 3.0 compatible version

--- ThirdParty/SQLite.swift/SQLite.swift.podspec
+++ ThirdParty/SQLite.swift/SQLite.swift.podspec
@@ -1,16 +1,70 @@
-require_relative 'Supporting Files/podspec.rb'
+Pod::Spec.new do |s|
+  s.name             = "SQLite.swift"
+  s.version          = "0.11.2"
+  s.summary          = "A type-safe, Swift-language layer over SQLite3 for iOS and OS X."
 
-Pod::Spec.new do |spec|
-  spec.name = 'SQLite.swift'
-  spec.summary = 'A type-safe, Swift-language layer over SQLite3.'
-
-  spec.description = <<-DESC
+  s.description      = <<-DESC
     SQLite.swift provides compile-time confidence in SQL statement syntax and
     intent.
-  DESC
+                       DESC
 
-  apply_shared_config spec, 'SQLite'
+  s.homepage         = "https://github.com/stephencelis/SQLite.swift"
+  s.license          = 'MIT'
+  s.author           = { "Stephen Celis" => "stephen@stephencelis.com" }
+  s.source           = { :git => "https://github.com/stephencelis/SQLite.swift.git", :tag => s.version.to_s }
+  s.social_media_url = 'https://twitter.com/stephencelis'
 
-  spec.exclude_files = 'Source/Cipher/Cipher.swift'
-  spec.library = 'sqlite3'
+  s.module_name      = 'SQLite'
+  s.ios.deployment_target = "8.0"
+  s.tvos.deployment_target = "9.0"
+  s.osx.deployment_target = "10.9"
+  s.watchos.deployment_target = "2.0"
+  s.default_subspec  = 'standard'
+  s.pod_target_xcconfig = {
+    'SWIFT_VERSION' => '3.0',
+  }
+
+  s.subspec 'standard' do |ss|
+    ss.source_files = 'Sources/{SQLite,SQLiteObjc}/**/*.{c,h,m,swift}'
+    ss.exclude_files = 'Sources/**/Cipher.swift'
+    ss.private_header_files = 'Sources/SQLiteObjc/*.h'
+
+    ss.library = 'sqlite3'
+    ss.preserve_paths = 'CocoaPods/**/*'
+    ss.pod_target_xcconfig = {
+      'SWIFT_INCLUDE_PATHS[sdk=macosx*]'             => '$(SRCROOT)/SQLite.swift/CocoaPods/macosx',
+      'SWIFT_INCLUDE_PATHS[sdk=macosx10.11]'         => '$(SRCROOT)/SQLite.swift/CocoaPods/macosx-10.11',
+      'SWIFT_INCLUDE_PATHS[sdk=macosx10.12]'         => '$(SRCROOT)/SQLite.swift/CocoaPods/macosx-10.12',
+      'SWIFT_INCLUDE_PATHS[sdk=iphoneos*]'           => '$(SRCROOT)/SQLite.swift/CocoaPods/iphoneos',
+      'SWIFT_INCLUDE_PATHS[sdk=iphoneos10.0]'        => '$(SRCROOT)/SQLite.swift/CocoaPods/iphoneos-10.0',
+      'SWIFT_INCLUDE_PATHS[sdk=iphonesimulator*]'    => '$(SRCROOT)/SQLite.swift/CocoaPods/iphonesimulator',
+      'SWIFT_INCLUDE_PATHS[sdk=iphonesimulator10.0]' => '$(SRCROOT)/SQLite.swift/CocoaPods/iphonesimulator-10.0',
+      'SWIFT_INCLUDE_PATHS[sdk=appletvos*]'          => '$(SRCROOT)/SQLite.swift/CocoaPods/appletvos',
+      'SWIFT_INCLUDE_PATHS[sdk=appletvsimulator*]'   => '$(SRCROOT)/SQLite.swift/CocoaPods/appletvsimulator',
+      'SWIFT_INCLUDE_PATHS[sdk=watchos*]'            => '$(SRCROOT)/SQLite.swift/CocoaPods/watchos',
+      'SWIFT_INCLUDE_PATHS[sdk=watchsimulator*]'     => '$(SRCROOT)/SQLite.swift/CocoaPods/watchsimulator'
+    }
+  end
+
+  s.subspec 'standalone' do |ss|
+    ss.source_files = 'Sources/{SQLite,SQLiteObjc}/**/*.{c,h,m,swift}'
+    ss.exclude_files = 'Sources/**/Cipher.swift'
+    ss.private_header_files = 'Sources/SQLiteObjc/*.h'
+    ss.xcconfig = {
+      'OTHER_SWIFT_FLAGS' => '$(inherited) -DSQLITE_SWIFT_STANDALONE'
+    }
+
+    ss.dependency 'sqlite3', '>= 3.14.0'
+  end
+
+  s.subspec 'SQLCipher' do |ss|
+    ss.source_files = 'Sources/{SQLite,SQLiteObjc}/**/*.{c,h,m,swift}'
+    ss.private_header_files = 'Sources/SQLiteObjc/*.h'
+    ss.xcconfig = {
+      'OTHER_SWIFT_FLAGS' => '$(inherited) -DSQLITE_SWIFT_SQLCIPHER',
+      'GCC_PREPROCESSOR_DEFINITIONS' => '$(inherited) SQLITE_HAS_CODEC=1'
+    }
+
+    ss.dependency 'SQLCipher', '>= 3.4.0'
+  end
 end
---
+++ ThirdParty/SQLite.swift/Sources/SQLite/Core/Value.swift
@@ -0,0 +1,132 @@
+//
+// SQLite.swift
+// https://github.com/stephencelis/SQLite.swift
+// Copyright © 2014-2015 Stephen Celis.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+/// - Warning: `Binding` is a protocol that SQLite.swift uses internally to
+///   directly map SQLite types to Swift types.
+///
+///   Do not conform custom types to the Binding protocol. See the `Value`
+///   protocol, instead.
+public protocol Binding {}
+
+public protocol Number : Binding {}
+
+public protocol Value : Expressible { // extensions cannot have inheritance clauses
+
+    associatedtype ValueType = Self
+
+    associatedtype Datatype : Binding
+
+    static var declaredDatatype: String { get }
+
+    static func fromDatatypeValue(_ datatypeValue: Datatype) -> ValueType
+
+    var datatypeValue: Datatype { get }
+
+}
+
+extension Double : Number, Value {
+
+    public static let declaredDatatype = "REAL"
+
+    public static func fromDatatypeValue(_ datatypeValue: Double) -> Double {
+        return datatypeValue
+    }
+
+    public var datatypeValue: Double {
+        return self
+    }
+
+}
+
+extension Int64 : Number, Value {
+
+    public static let declaredDatatype = "INTEGER"
+
+    public static func fromDatatypeValue(_ datatypeValue: Int64) -> Int64 {
+        return datatypeValue
+    }
+
+    public var datatypeValue: Int64 {
+        return self
+    }
+
+}
+
+extension String : Binding, Value {
+
+    public static let declaredDatatype = "TEXT"
+
+    public static func fromDatatypeValue(_ datatypeValue: String) -> String {
+        return datatypeValue
+    }
+
+    public var datatypeValue: String {
+        return self
+    }
+
+}
+
+extension Blob : Binding, Value {
+
+    public static let declaredDatatype = "BLOB"
+
+    public static func fromDatatypeValue(_ datatypeValue: Blob) -> Blob {
+        return datatypeValue
+    }
+
+    public var datatypeValue: Blob {
+        return self
+    }
+
+}
+
+// MARK: -
+
+extension Bool : Binding, Value {
+
+    public static var declaredDatatype = Int64.declaredDatatype
+
+    public static func fromDatatypeValue(_ datatypeValue: Int64) -> Bool {
+        return datatypeValue != 0
+    }
+
+    public var datatypeValue: Int64 {
+        return self ? 1 : 0
+    }
+
+}
+
+extension Int : Number, Value {
+
+    public static var declaredDatatype = Int64.declaredDatatype
+
+    public static func fromDatatypeValue(_ datatypeValue: Int64) -> Int {
+        return Int(datatypeValue)
+    }
+
+    public var datatypeValue: Int64 {
+        return Int64(self)
+    }
+
+}
