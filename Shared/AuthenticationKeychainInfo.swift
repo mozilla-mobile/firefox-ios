1b35ca143af76c452c3f5372e419a98d568aff21
farhanpatel <farhanpatel.17@gmail.com>
2017-01-25 15:19:14 -0800

    Fix linting errors

--- Shared/AuthenticationKeychainInfo.swift
+++ Shared/AuthenticationKeychainInfo.swift
@@ -6,7 +6,7 @@ import Foundation
 import SwiftKeychainWrapper
 // FIXME: comparison operators with optionals were removed from the Swift Standard Libary.
 // Consider refactoring the code to use the non-optional operators.
-fileprivate func < <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
+fileprivate func < <T: Comparable>(lhs: T?, rhs: T?) -> Bool {
   switch (lhs, rhs) {
   case let (l?, r?):
     return l < r
