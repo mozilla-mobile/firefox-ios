7e4795e8c52f6b6239aa81b72f6a1ac5a66d6bb8
James Hugman <james@hugman.tv>
2017-01-27 17:23:37 +0000

    Bug 1333763 / 1333766 – Centralize the post-login, pre-sync conversation into one place, the FxALoginHelper.

--- Client/Helpers/FxALoginHelper.swift
+++ Client/Helpers/FxALoginHelper.swift
@@ -11,15 +11,24 @@ import Shared
 /// asking the user for notification permissions, registering for 
 /// remote push notifications (APNS), registering for WebPush notifcations
 /// then creating an account and storing it in the profile.
-public class FxALoginHelper {
+class FxALoginHelper {
     private let deferred = Success()
 
     private let pushClient: PushClient
 
     private weak var application: UIApplication?
-    private weak var profile: BrowserProfile?
+    private weak var profile: Profile?
 
-    public init(application: UIApplication, profile: BrowserProfile) {
+    private var data: JSON!
+
+    static private(set) var sharedInstance: FxALoginHelper?
+
+    class func createSharedInstance(application: UIApplication?, profile: Profile?) -> FxALoginHelper {
+        sharedInstance = FxALoginHelper(application: application, profile: profile)
+        return sharedInstance!
+    }
+
+    init(application: UIApplication?, profile: Profile?) {
         let configuration = StagePushConfiguration()
         let client = PushClient(endpointURL: configuration.endpointURL)
 
@@ -28,19 +37,61 @@ public class FxALoginHelper {
         self.profile = profile
     }
 
-    public func userDidLogin() -> Success {
+    func userDidLogin(data: JSON) -> Success {
+        if data["keyFetchToken"].asString == nil || data["unwrapBKey"].asString == nil {
+            // The /settings endpoint sends a partial "login"; ignore it entirely.
+            NSLog("Ignoring didSignIn with keyFetchToken or unwrapBKey missing.")
+            self.loginDidFail()
+            return deferred
+        }
+
+        self.data = data
+
+        requestUserNotifications()
         return deferred
     }
 
-    public func userDidAcceptUserNotifications() {
-        
+    private func requestUserNotifications() {
+        let viewAction = UIMutableUserNotificationAction()
+        viewAction.identifier = SentTabAction.View.rawValue
+        viewAction.title = NSLocalizedString("View", comment: "View a URL - https://bugzilla.mozilla.org/attachment.cgi?id=8624438, https://bug1157303.bugzilla.mozilla.org/attachment.cgi?id=8624440")
+        viewAction.activationMode = UIUserNotificationActivationMode.Foreground
+        viewAction.destructive = false
+        viewAction.authenticationRequired = false
+
+        let bookmarkAction = UIMutableUserNotificationAction()
+        bookmarkAction.identifier = SentTabAction.Bookmark.rawValue
+        bookmarkAction.title = NSLocalizedString("Bookmark", comment: "Bookmark a URL - https://bugzilla.mozilla.org/attachment.cgi?id=8624438, https://bug1157303.bugzilla.mozilla.org/attachment.cgi?id=8624440")
+        bookmarkAction.activationMode = UIUserNotificationActivationMode.Foreground
+        bookmarkAction.destructive = false
+        bookmarkAction.authenticationRequired = false
+
+        let readingListAction = UIMutableUserNotificationAction()
+        readingListAction.identifier = SentTabAction.ReadingList.rawValue
+        readingListAction.title = NSLocalizedString("Add to Reading List", comment: "Add URL to the reading list - https://bugzilla.mozilla.org/attachment.cgi?id=8624438, https://bug1157303.bugzilla.mozilla.org/attachment.cgi?id=8624440")
+        readingListAction.activationMode = UIUserNotificationActivationMode.Foreground
+        readingListAction.destructive = false
+        readingListAction.authenticationRequired = false
+
+        let sentTabsCategory = UIMutableUserNotificationCategory()
+        sentTabsCategory.identifier = TabSendCategory
+        sentTabsCategory.setActions([readingListAction, bookmarkAction, viewAction], forContext: UIUserNotificationActionContext.Default)
+
+        sentTabsCategory.setActions([bookmarkAction, viewAction], forContext: UIUserNotificationActionContext.Minimal)
+
+        application?.registerUserNotificationSettings(UIUserNotificationSettings(forTypes: UIUserNotificationType.Alert, categories: [sentTabsCategory]))
     }
 
-    public func userDidRejectUserNotifications() {
+    func userDidRegister(notificationSettings notificationSettings: UIUserNotificationSettings) {
+        guard notificationSettings.types != .None else {
+            return readyForSyncing()
+        }
 
+        application?.registerForRemoteNotifications()
     }
 
-    public func apnsRegisterDidSucceed(apnsToken apnsToken: String) {
+
+    func apnsRegisterDidSucceed(apnsToken apnsToken: String) {
         pushClient.register(apnsToken).upon { res in
             if let pushRegistration = res.successValue {
                 return self.pushRegistrationDidSucceed(apnsToken: apnsToken, pushRegistration: pushRegistration)
@@ -49,17 +100,35 @@ public class FxALoginHelper {
         }
     }
 
-    public func apnsRegisterDidFail() {
+    func apnsRegisterDidFail() {
+        self.readyForSyncing()
+    }
+
+    func pushRegistrationDidSucceed(apnsToken apnsToken: String, pushRegistration: PushRegistration) {
 
     }
 
-    public func pushRegistrationDidSucceed(apnsToken apnsToken: String, pushRegistration: PushRegistration) {
+    func pushRegistrationDidFail() {
 
     }
 
-    public func pushRegistrationDidFail() {
-
+    func readyForSyncing() {
+        if let profile = self.profile, let data = data {
+            // TODO: Error handling.
+            let account = FirefoxAccount.fromConfigurationAndJSON(profile.accountConfiguration, data: data)!
+            
+            profile.setAccount(account)
+            // account.advance is idempotent.
+            if let account = profile.getAccount() {
+                account.advance()
+            }
+        }
+        FxALoginHelper.sharedInstance = nil
+        deferred.fill(Maybe(success: ()))
     }
 
-
+    func loginDidFail() {
+        FxALoginHelper.sharedInstance = nil
+        deferred.fill(Maybe(failure: FxADeviceRegistratorError.InvalidSession))
+    }
 }
