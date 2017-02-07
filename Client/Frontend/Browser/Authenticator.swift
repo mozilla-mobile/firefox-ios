cf277426f429e80c1454bde43133d67df96e7869
Emily Toop <etoop@mozilla.com>
2017-02-07 14:41:10 +0000

    Remove unnecessary call to `.value`

--- Client/Frontend/Browser/Authenticator.swift
+++ Client/Frontend/Browser/Authenticator.swift
@@ -84,7 +84,7 @@ class Authenticator {
                 let login = logins[0]
                 credentials = login.credentials
                 let new = Login(credential: login.credentials, protectionSpace: challenge.protectionSpace)
-                loginsProvider.updateLoginByGUID(login.guid, new: new, significant: true).value
+                loginsProvider.updateLoginByGUID(login.guid, new: new, significant: true)
             }
 
             // Found a single entry that matches the scheme and host - good to go.
