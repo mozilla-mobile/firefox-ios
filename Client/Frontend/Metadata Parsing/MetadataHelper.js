12f65f769200e2202d5a06cf4f33b89d8e35ac24
Emily Toop <etoop@mozilla.com>
2017-02-14 16:56:26 +0000

    Part 2 - fetch DataURI's for images and add to metadata

--- Client/Frontend/Metadata Parsing/MetadataHelper.js
+++ Client/Frontend/Metadata Parsing/MetadataHelper.js
@@ -16,9 +16,74 @@ var MetadataWrapper = function () {
             return;
         }
 
+        var metadataCallback = function(metadata) {
+            window.__firefox__.pageMetadata = metadata;
+            webkit.messageHandlers.metadataMessageHandler.postMessage(metadata);
+        }
+
         var metadata = metadataparser.getMetadata(window.document, window.location.href, metadataparser.metadataRules);
-        window.__firefox__.pageMetadata = metadata;
-        webkit.messageHandlers.metadataMessageHandler.postMessage(metadata);
+
+        var iconURL = metadata["icon_url"];
+        var imageURL = metadata["image_url"];
+        var iconLoaded = false, imageLoaded = false
+        if (iconURL) {
+            getDataUri(iconURL, function(dataURI) {
+                if (dataURI) {
+                    metadata["icon_data_url"] = dataURI;
+                }
+                if (imageLoaded) {
+                    metadataCallback(metadata);
+                } else {
+                    iconLoaded = true
+                }
+            });
+        } 
+        else { 
+            iconLoaded = true; 
+        }
+
+        if (imageURL) {
+            getDataUri(imageURL, function(dataURI) {
+                if (dataURI) {
+                    metadata["image_data_url"] = dataURI;
+                }
+                if (iconLoaded) {
+                    metadataCallback(metadata);
+                } else {
+                    imageLoaded = true
+                }
+            });
+        } 
+        else if (iconLoaded) { 
+            imageLoaded = true; 
+            metadataCallback(metadata);
+        }
+
+    }
+
+    return {
+        extractMetadata: extractMetadata
+    };
+
+    function getDataUri(url, callback) {
+        var image = new Image();
+
+        image.onload = function () {
+            try {
+                var canvas = document.createElement('canvas');
+                canvas.width = this.naturalWidth; // or 'width' if you want a special/scaled size
+                canvas.height = this.naturalHeight; // or 'height' if you want a special/scaled size
+
+                canvas.getContext('2d').drawImage(this, 0, 0);
+
+                var dataURI = canvas.toDataURL();
+                callback(dataURI);
+            } catch (exception) {
+                callback(false)
+            }
+        };
+
+        image.src = url;
     }
 
     return {
