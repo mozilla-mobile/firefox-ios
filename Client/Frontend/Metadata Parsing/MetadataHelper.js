7e45079f42f6a6d08899b9dd4c1cba5395af958f
Emily Toop <etoop@mozilla.com>
2017-02-16 11:10:31 +0000

    Bug 1311399 - download and store image metadata (#2426)
    * Part 1 - property reference metadata using keys and Any rather than AnyObject
    * Part 2 - fetch DataURI's for images and add to metadata
    * Part 3 - fetch data URI for images and store them as UIImages
    * Part 4 - Save the images to the image cache
    * Part 5 - Download and cache metadata images either from Data URI or direct if not possible to grab URI
    * Part 6 - detect if URL is a data URI before trying to fetch data URI, to ensure we don't grab stuff twice
    * Part 7 - Removing saving of icons from metadata

--- Client/Frontend/Metadata Parsing/MetadataHelper.js
+++ Client/Frontend/Metadata Parsing/MetadataHelper.js
@@ -9,6 +9,11 @@ if (!window.__firefox__) {
 }
 
 var MetadataWrapper = function () {
+    var dataURIRegex = /^\s*data:([a-z]+\/[a-z]+(;[a-z\-]+\=[a-z\-]+)?)?(;base64)?,[a-z0-9\!\$\&\'\,\(\)\*\+\,\;\=\-\.\_\~\:\@\/\?\%\s]*\s*$/i;
+    
+    function isDataURI(s) {
+        return !!s.match(dataURIRegex);
+    }
 
     function extractMetadata() {
         if (window.__firefox__.pageMetadata) {
@@ -16,9 +21,52 @@ var MetadataWrapper = function () {
             return;
         }
 
+        var metadataCallback = function(metadata) {
+            window.__firefox__.pageMetadata = metadata;
+            webkit.messageHandlers.metadataMessageHandler.postMessage(metadata);
+        }
+
         var metadata = metadataparser.getMetadata(window.document, window.location.href, metadataparser.metadataRules);
-        window.__firefox__.pageMetadata = metadata;
-        webkit.messageHandlers.metadataMessageHandler.postMessage(metadata);
+        var imageURL = metadata["image_url"];
+
+        if (imageURL) {
+            if (isDataURI(imageURL)) {
+                metadata["image_data_uri"] = imageURL;
+                metadataCallback(metadata);
+            } else {
+                getDataUri(imageURL, function(dataURI) {
+                    if (dataURI) {
+                        metadata["image_data_uri"] = dataURI;
+                    }
+                    metadataCallback(metadata);
+                });
+            }
+        } 
+        else { 
+            metadataCallback(metadata);
+        }
+
+    }
+
+    function getDataUri(url, callback) {
+        var image = new Image();
+
+        image.onload = function () {
+            try {
+                var canvas = document.createElement('canvas');
+                canvas.width = this.naturalWidth; // or 'width' if you want a special/scaled size
+                canvas.height = this.naturalHeight; // or 'height' if you want a special/scaled size
+
+                canvas.getContext('2d').drawImage(this, 0, 0);
+
+                var dataURI = canvas.toDataURL();
+                callback(dataURI);
+            } catch (exception) {
+                callback(false)
+            }
+        };
+
+        image.src = url;
     }
 
     return {
