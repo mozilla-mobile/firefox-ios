9ea2b25ccf28fc50dbcee58f87dba1ce5a86c0c6
Stephan Leroux <sleroux@mozilla.com>
2017-02-22 10:17:00 -0500

    Added a simple postclone script for buddybuild

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -1,2 +1,22 @@
 #!/usr/bin/env bash
+
 brew upgrade swiftlint
+
+# Install tooling for Badging
+brew update && brew install imagemagick
+echo password | sudo -S gem install badge
+
+# Install Python tooling for localizations scripts
+echo password | sudo -S pip install --upgrade pip
+echo password | sudo -S pip install virtualenv
+
+# Add badge to app icon
+badge --no_badge --shield_no_resize --shield "7.0-Build%2024-blue"
+
+# Localize the app
+./scripts/import-locales.sh
+
+
+
+
+
