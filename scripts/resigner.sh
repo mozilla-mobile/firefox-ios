a65c3bb7c9d9eb7913adb38d26c49fbb92c62cfa
Stephan Leroux <sleroux@mozilla.com>
2017-03-01 16:08:46 -0500

    No Bug - Fixup resigning script to fix subtarget entitlements

--- scripts/resigner.sh
+++ scripts/resigner.sh
@@ -33,6 +33,14 @@ function export_entitlements {
 function update_extension_entitlements {
   /usr/libexec/PlistBuddy -c "Set application-identifier $TEAM_ID.$BUNDLE_ID.$2" "$1"
   /usr/libexec/PlistBuddy -c "Set com.apple.developer.team-identifier $TEAM_ID" "$1"
+
+  # Delete other groups and only be left with one
+  /usr/libexec/PlistBuddy -c "Delete com.apple.security.application-groups:1" "$1"
+  /usr/libexec/PlistBuddy -c "Delete com.apple.security.application-groups:1" "$1"
+
+  /usr/libexec/PlistBuddy -c "Delete keychain-access-groups:1" "$1"
+  /usr/libexec/PlistBuddy -c "Delete keychain-access-groups:1" "$1"
+
   /usr/libexec/PlistBuddy -c "Set com.apple.security.application-groups:0 group.$BUNDLE_ID" "$1"
   /usr/libexec/PlistBuddy -c "Set keychain-access-groups:0 $TEAM_ID.$BUNDLE_ID" "$1"
   /usr/libexec/PlistBuddy -c "Add beta-reports-active bool true" "$1"
