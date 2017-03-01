d66101cc897ff90d8b2d3b85432ba989a21f5740
Farhan Patel <farhanpatel.17@gmail.com>
2017-02-28 19:41:44 -0800

    Bug 1335689 - Fix Top Tabs crash where reloadData is called when view is not yet created. (#2464) r=sleroux
    * Bug 1335689 - Fix Top Tabs crash where reloadData is called when view is not yet created.
    
    * Bug 1338164 - Fix issue with separator layout in Top Tabs.

--- Client/Frontend/Browser/TopTabsLayout.swift
+++ Client/Frontend/Browser/TopTabsLayout.swift
@@ -7,7 +7,7 @@ class TopTabsLayoutDelegate: NSObject, UICollectionViewDelegateFlowLayout {
     weak var tabSelectionDelegate: TabSelectionDelegate?
     
     @objc func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return 1
+        return TopTabsUX.SeparatorWidth
     }
     
     @objc func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
@@ -19,7 +19,7 @@ class TopTabsLayoutDelegate: NSObject, UICollectionViewDelegateFlowLayout {
     }
     
     @objc func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 1
+        return TopTabsUX.SeparatorWidth
     }
     
     @objc func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
@@ -32,30 +32,39 @@ class TopTabsViewLayout: UICollectionViewFlowLayout {
     var decorationAttributeArr: [Int : UICollectionViewLayoutAttributes?] = [:]
     
     override var collectionViewContentSize: CGSize {
-        return CGSize(width: CGFloat(collectionView!.numberOfItems(inSection: 0)) * (TopTabsUX.TabWidth+1)+TopTabsUX.TopTabsBackgroundShadowWidth*2,
-                      height: collectionView!.bounds.height)
+        let tabsWidth = ((CGFloat(collectionView!.numberOfItems(inSection: 0))) * (TopTabsUX.TabWidth + TopTabsUX.SeparatorWidth)) - TopTabsUX.SeparatorWidth
+        return CGSize(width: tabsWidth + (TopTabsUX.TopTabsBackgroundShadowWidth*2), height: collectionView!.bounds.height)
     }
     
     override func prepare() {
         super.prepare()
-        self.minimumLineSpacing = 2
+        self.minimumLineSpacing = TopTabsUX.SeparatorWidth
         scrollDirection = UICollectionViewScrollDirection.horizontal
         register(TopTabsBackgroundDecorationView.self, forDecorationViewOfKind: TopTabsBackgroundDecorationView.Identifier)
         register(TopTabsSeparator.self, forDecorationViewOfKind: TopTabsSeparatorUX.Identifier)
     }
     
     override func shouldInvalidateLayout(forBoundsChange newBounds: CGRect) -> Bool {
+        decorationAttributeArr = [:]
         return true
     }
 
     // MARK: layoutAttributesForElementsInRect
     override func layoutAttributesForDecorationView(ofKind elementKind: String, at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
-        if let attr = self.decorationAttributeArr[indexPath.row] {
-            return attr
-        } else {
-            // Sometimes decoration views will be requested for rows that might not exist. Just show an empty separator.
+        guard indexPath.row < self.collectionView!.numberOfItems(inSection: 0) else {
             let separatorAttr = UICollectionViewLayoutAttributes(forDecorationViewOfKind: TopTabsSeparatorUX.Identifier, with: indexPath)
             separatorAttr.frame = CGRect.zero
+            separatorAttr.zIndex = -1
+            return separatorAttr
+        }
+
+        if let attr = self.decorationAttributeArr[indexPath.item] {
+            return attr
+        } else {
+            // Compute the separator if it does not exist in the cache
+            let separatorAttr = UICollectionViewLayoutAttributes(forDecorationViewOfKind: TopTabsSeparatorUX.Identifier, with: indexPath)
+            let x = TopTabsUX.TopTabsBackgroundShadowWidth + ((CGFloat(indexPath.row) * (TopTabsUX.TabWidth + TopTabsUX.SeparatorWidth)) - TopTabsUX.SeparatorWidth)
+            separatorAttr.frame = CGRect(x: x, y: collectionView!.frame.height / 4, width: TopTabsUX.SeparatorWidth, height: collectionView!.frame.height / 2)
             separatorAttr.zIndex = -1
             return separatorAttr
         }
@@ -69,22 +78,21 @@ class TopTabsViewLayout: UICollectionViewFlowLayout {
         let size = collectionViewContentSize
         let offset = TopTabsUX.TopTabsBackgroundPadding-TopTabsUX.TopTabsBackgroundShadowWidth * 2
         decorationAttributes.frame = CGRect(x: -(offset)/2, y: 0, width: size.width + offset, height: size.height)
-        decorationAttributes.zIndex = -1
+        decorationAttributes.zIndex = -2
         decorationAttributes.themeColor = self.themeColor
 
         // Create attributes for the Tab Separator.
-        var separatorArr: [Int: UICollectionViewLayoutAttributes] = [:]
-        for i in attributes {
-            if i.indexPath.item > 0 {
-                let sep = UICollectionViewLayoutAttributes(forDecorationViewOfKind: TopTabsSeparatorUX.Identifier, with: i.indexPath)
-                sep.frame = CGRect(x: i.frame.origin.x - 2, y: i.frame.size.height / 4, width: TopTabsSeparatorUX.Width, height: i.frame.size.height / 2)
-                sep.zIndex = -1
-                separatorArr[i.indexPath.row] = sep
+        for i in attributes  {
+            let sep = UICollectionViewLayoutAttributes(forDecorationViewOfKind: TopTabsSeparatorUX.Identifier, with: i.indexPath)
+            sep.frame = CGRect(x: i.frame.origin.x - TopTabsUX.SeparatorWidth, y: i.frame.size.height / 4, width: TopTabsUX.SeparatorWidth, height: i.frame.size.height / 2)
+            sep.zIndex = -1
+            // Only add the seperator if it will be shown.
+            if i.indexPath.row != 0 &&  i.indexPath.row < self.collectionView!.numberOfItems(inSection: 0) {
                 attributes.append(sep)
+                decorationAttributeArr[i.indexPath.item] = sep
             }
         }
 
-        self.decorationAttributeArr = separatorArr
         attributes.append(decorationAttributes)
         return attributes
     }
