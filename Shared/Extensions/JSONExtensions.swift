9c2aed14f1124b3b60ee8651a2bee0dd4c2b1024
Richard Newman <rnewman@twinql.com>
2017-04-09 15:58:26 -0700

    Bug 1354935 - Part 1: correct SwiftyJSON type checks. r?sleroux
    SwiftyJSON doesn't tightly couple its type enum with values, so it's technically
    possible for a .string to have a value that returns nil when failably cast to a
    string.
    
    Additionally, some SwiftyJSON error flows set type to .unknown.
    
    We were mishandling both of these steps: assuming that if .type == .string then
    .string would always return a string, and that .unknown meant a null field.
    
    This commit fixes the behavior to match the pre-SwiftyJSON state: .isNull() is
    true when the value is an explicit null, and .isString() is true when .string is
    non-nil and the type is .string.

--- Shared/Extensions/JSONExtensions.swift
+++ Shared/Extensions/JSONExtensions.swift
@@ -7,7 +7,8 @@ import SwiftyJSON
 
 public extension JSON {
     func isStringOrNull() -> Bool {
-        return isNull() || isString()
+        return self.isString() ||
+               self.isNull()
     }
 
     func isError() -> Bool {
@@ -15,7 +16,10 @@ public extension JSON {
     }
 
     func isString() -> Bool {
-        return self.type == .string
+        // SwiftyJSON doesn't link values to types; it's possible for `self.type == .string` but
+        // `self.string` to return `nil`. Validate both.
+        return self.type == .string &&
+               self.string != nil
     }
 
     func isBool() -> Bool {
@@ -31,7 +35,7 @@ public extension JSON {
     }
 
     func isNull() -> Bool {
-        return self.type == .null || self.type == .unknown
+        return self.type == .null
     }
 
     func isInt() -> Bool {
