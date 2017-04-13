4e4d07aa1c60793313a263614fbf26715dc7ce12
Stephan Leroux <sleroux@mozilla.com>
2017-04-13 09:31:09 -0400

    Bug 1356228 - Removed extra String string in Plistbuddy command (#2627) r=st3fan

--- buddybuild_prebuild.sh
+++ buddybuild_prebuild.sh
@@ -7,8 +7,8 @@ if [ "$BUDDYBUILD_SCHEME" == FirefoxBeta ]; then
   /usr/libexec/PlistBuddy -c "Set AdjustEnvironment sandbox" "Client/Info.plist"
 elif [ "$BUDDYBUILD_SCHEME" == Firefox ]; then
   echo "Setting Adjust environment to PRODUCTION for $BUDDYBUILD_SCHEME"
-  /usr/libexec/PlistBuddy -c "Set AdjustAppToken String $ADJUST_KEY_PRODUCTION" "Client/Info.plist"
-  /usr/libexec/PlistBuddy -c "Set AdjustEnvironment String production" "Client/Info.plist"
+  /usr/libexec/PlistBuddy -c "Set AdjustAppToken $ADJUST_KEY_PRODUCTION" "Client/Info.plist"
+  /usr/libexec/PlistBuddy -c "Set AdjustEnvironment production" "Client/Info.plist"
 fi
 
 # Set the build number to match the Buddybuild number
