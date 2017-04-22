8b4242bc92e1f0aaadf0d4e720eab0a77d3e26cf
Stefan Arentz <stefan@arentz.ca>
2017-04-21 22:25:33 -0400

    No Bug - getModifiedHistoryToUpload logging

--- Storage/SQL/SQLiteHistory.swift
+++ Storage/SQL/SQLiteHistory.swift
@@ -9,6 +9,29 @@ import Deferred
 
 private let log = Logger.syncLogger
 
+func mach_task_self() -> task_t {
+    return mach_task_self_
+}
+
+func getMegabytesUsed() -> Float? {
+    var info = mach_task_basic_info()
+    var count = mach_msg_type_number_t(MemoryLayout.size(ofValue: info) / MemoryLayout<integer_t>.size)
+    let kerr = withUnsafeMutablePointer(to: &info) { infoPtr in
+        return infoPtr.withMemoryRebound(to: integer_t.self, capacity: Int(count)) { (machPtr: UnsafeMutablePointer<integer_t>) in
+            return task_info(
+                mach_task_self(),
+                task_flavor_t(MACH_TASK_BASIC_INFO),
+                machPtr,
+                &count
+            )
+        }
+    }
+    guard kerr == KERN_SUCCESS else {
+        return nil
+    }
+    return Float(info.resident_size) / (1024 * 1024)
+}
+
 class NoSuchRecordError: MaybeErrorType {
     let guid: GUID
     init(guid: GUID) {
@@ -969,7 +992,7 @@ extension SQLiteHistory: SyncableHistory {
     }
 
     public func getModifiedHistoryToUpload() -> Deferred<Maybe<[(Place, [Visit])]>> {
-        log.info("MOOMOO SQLiteHistory.getModifiedHistoryToUpload")
+        log.info("MOOMOO SQLiteHistory.getModifiedHistoryToUpload residentSize=\(getMegabytesUsed())")
 
         // What we want to do: find all items flagged for update, selecting some number of their
         // visits alongside.
@@ -1032,6 +1055,8 @@ extension SQLiteHistory: SyncableHistory {
         return db.runQuery(sql, args: args, factory: factory)
             >>== { c in
 
+                log.debug("MOOMOO Ran query c.count=\(c.count) residentSize=\(getMegabytesUsed())")
+
                 // Consume every row, with the side effect of populating the places
                 // and visit accumulators.
                 var n = 0
@@ -1044,19 +1069,21 @@ extension SQLiteHistory: SyncableHistory {
 
                     n += 1
                     if n == 50 {
-                        log.debug("MOOMOO Processed \(n) records")
+                        log.debug("MOOMOO Processed \(n) records residentSize=\(getMegabytesUsed())")
                     }
                 }
 
-                log.debug("MOOMOO Processed \(n) records in total")
+                log.debug("MOOMOO Processed \(n) records in total residentSize=\(getMegabytesUsed())")
 
                 // Now we're done with the cursor. Close it.
                 c.close()
 
-                log.debug("MOOMOO Closed cursor")
+                log.debug("MOOMOO Closed cursor residentSize=\(getMegabytesUsed())")
 
                 // Now collect the return value.
-                return deferMaybe(ids.map { return (places[$0]!, visits[$0]!) })
+                let r = deferMaybe(ids.map { return (places[$0]!, visits[$0]!) })
+                log.debug("MOOMOO Returning result residentSize=\(getMegabytesUsed())")
+                return r
         }
     }
 
