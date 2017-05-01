40e932c3bfcec0500e56cbd4c4ce8af3deab6335
Stephan Leroux <sleroux@mozilla.com>
2017-05-01 13:15:51 -0400

    Fixup: Fixed broken UserAgent tests when using Buddybuild build number in UA

--- ClientTests/ClientTests.swift
+++ ClientTests/ClientTests.swift
@@ -17,7 +17,7 @@ class ClientTests: XCTestCase {
         let ua = UserAgent.syncUserAgent
         let device = DeviceInfo.deviceModel()
         let systemVersion = UIDevice.current.systemVersion
-        let expectedRegex = "^Firefox-iOS-Sync/[0-9]\\.[0-9]b*[0-9] \\(\(device); iPhone OS \(systemVersion)\\) \\([-_A-Za-z0-9= \\(\\)]+\\)$"
+        let expectedRegex = "^Firefox-iOS-Sync/[0-9]\\.[0-9]b[0-9]* \\(\(device); iPhone OS \(systemVersion)\\) \\([-_A-Za-z0-9= \\(\\)]+\\)$"
         let loc = ua.range(of: expectedRegex, options: NSString.CompareOptions.regularExpression)
         XCTAssertTrue(loc != nil, "Sync UA is as expected. Was \(ua)")
     }
@@ -27,7 +27,7 @@ class ClientTests: XCTestCase {
         let appVersion = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String
 
         let compare: (String) -> Bool = { ua in
-            let range = ua.range(of: "^Mozilla/5\\.0 \\(.+\\) AppleWebKit/[0-9\\.]+ \\(KHTML, like Gecko\\) FxiOS/\(appVersion)b*[0-9] Mobile/[A-Za-z0-9]+ Safari/[0-9\\.]+$", options: NSString.CompareOptions.regularExpression)
+            let range = ua.range(of: "^Mozilla/5\\.0 \\(.+\\) AppleWebKit/[0-9\\.]+ \\(KHTML, like Gecko\\) FxiOS/\(appVersion)b[0-9]* Mobile/[A-Za-z0-9]+ Safari/[0-9\\.]+$", options: NSString.CompareOptions.regularExpression)
             return range != nil
         }
 
