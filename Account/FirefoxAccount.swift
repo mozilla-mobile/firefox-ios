ccf34a26b18a292aaa51e4a302dabdb1c7c251f9
James Hugman <james@hugman.tv>
2017-05-02 14:36:11 +0100

    Bug 1357762 – Increment local account schema json.
    This is so already logged in accounts can register push details.

--- Account/FirefoxAccount.swift
+++ Account/FirefoxAccount.swift
@@ -11,7 +11,7 @@ import SwiftyJSON
 private let log = Logger.syncLogger
 
 // The version of the account schema we persist.
-let AccountSchemaVersion = 1
+let AccountSchemaVersion = 2
 
 /// A FirefoxAccount mediates access to identity attached services.
 ///
