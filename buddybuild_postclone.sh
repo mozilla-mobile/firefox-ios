f384a572e397bd00593fddeb90d0d155b40ce407
Stephan Leroux <sleroux@mozilla.com>
2017-05-02 11:05:59 -0400

    Fixed scheme from escaping incorrectly

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -14,14 +14,14 @@ brew update && brew install imagemagick
 echo password | sudo -S gem install badge
 
 # Import the localize for our distribution builds.
-if [ $BUDDYBUILD_SCHEME = FirefoxBeta ]; then
+if [ "$BUDDYBUILD_SCHEME" = FirefoxBeta ]; then
   # Add badge to app icon.
-  CF_BUNDLE_SHORT_VERSION_STRING=`/usr/libexec/PlistBuddy -c "Print :CFBundleShortVersionString" Client/Info.plist`
+  CF_BUNDLE_SHORT_VERSION_STRING=$(/usr/libexec/PlistBuddy -c "Print :CFBundleShortVersionString" Client/Info.plist)
   badge --no_badge --shield_no_resize --shield "$CF_BUNDLE_SHORT_VERSION_STRING-Build%20$BUDDYBUILD_BUILD_NUMBER-blue"
 
   setup_virtualenv
   ./scripts/import-locales.sh
-elif [ $BUDDYBUILD_SCHEME = Firefox ]; then
+elif [ "$BUDDYBUILD_SCHEME" = Firefox ]; then
   setup_virtualenv
   ./scripts/import-locales.sh --release
 fi
