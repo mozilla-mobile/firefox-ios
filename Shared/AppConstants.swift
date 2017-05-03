a4d2f1c40ccb73d0687e9dd5dfba608291d8357c
jhugman <jhugman@users.noreply.github.com>
2017-05-03 13:34:09 +0100

    Bug 1357762 – Configure FxA on for all non-release users, to use production autopush. (#2641) r=sleroux
    * Bug 1344708 – Switch everyone to use Production push server.
    
    * Bug 1357762 – Pref on everyone except release to use FxA/Push.
    
    * Bug 1357762 – Increment local account schema json.
    
    This is so already logged in accounts can register push details.

--- Shared/AppConstants.swift
+++ Shared/AppConstants.swift
@@ -239,9 +239,9 @@ public struct AppConstants {
         #if MOZ_CHANNEL_RELEASE
             return false
         #elseif MOZ_CHANNEL_BETA
-            return false
+            return true
         #elseif MOZ_CHANNEL_NIGHTLY
-            return false
+            return true
         #elseif MOZ_CHANNEL_FENNEC
             return true
         #elseif MOZ_CHANNEL_AURORA
