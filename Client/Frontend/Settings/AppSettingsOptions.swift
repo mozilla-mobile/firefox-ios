4933b41b373aa04061957e93f4d0539c62fab5e8
Susheel Daswani <Sdaswani@cs.stanford.edu>
2017-05-03 12:21:00 -0700

    Bug 1361771 - Revert MMA Focus Promo (#2690)
    * Revert "Bug 1352569 - Telemetry for Focus Promo Results (#2681)"
    
    This reverts commit 5549dc9e97c17337e96508ffa9d802bd11bbcc89.
    
    * Revert "Bug 1353820 - Allow User to stop their LeanPlum Tracking (#2680)"
    
    This reverts commit cb5be603ea27518313656ac0cfd4fee3c24ecc00.
    
    * Revert "Bug 1353793 - Only call LeanPlum.start for Certain Locales"
    
    This reverts commit 7f9d3c91b73301e76acc26f28ae545f86125a248.
    
    * Revert "Nit - remove Leanplum.forceContentUpdate() (#2669)"
    
    This reverts commit d9c3ccb6894ad0ba6fd7b62b7521a778cf8288ab.
    
    * Revert "Nit - replace profile with getProfile() (#2666)"
    
    This reverts commit 32f24fbcea175ac1aaa41fb9b7968ea03564ce14.
    
    * Revert "Bug 1352568 - Drop LeanPlum events to serve as triggers for Focus Promotion (#2606)"
    
    This reverts commit 87cf5612a5db25ee607acefb6ea28593a8d9d91f.

--- Client/Frontend/Settings/AppSettingsOptions.swift
+++ Client/Frontend/Settings/AppSettingsOptions.swift
@@ -522,10 +522,7 @@ class SendAnonymousUsageDataSetting: BoolSetting {
             prefs: prefs, prefKey: "settings.sendUsageData", defaultValue: true,
             attributedTitleText: NSAttributedString(string: NSLocalizedString("Send Anonymous Usage Data", tableName: "SendAnonymousUsageData", comment: "See http://bit.ly/1SmEXU1")),
             attributedStatusText: NSAttributedString(string: NSLocalizedString("More Info…", tableName: "SendAnonymousUsageData", comment: "See http://bit.ly/1SmEXU1"), attributes: [NSForegroundColorAttributeName: UIConstants.HighlightBlue]),
-            settingDidChange: {
-                AdjustIntegration.setEnabled($0)
-                LeanplumIntegration.sharedInstance.setEnabled($0)
-            }
+            settingDidChange: { AdjustIntegration.setEnabled($0) }
         )
     }
 
@@ -609,7 +606,6 @@ class LoginsSetting: Setting {
     override func onClick(_: UINavigationController?) {
         guard let authInfo = KeychainWrapper.sharedAppContainerKeychain.authenticationInfo() else {
             settings?.navigateToLoginsList()
-            LeanplumIntegration.sharedInstance.track(eventName: .openedLogins)
             return
         }
 
@@ -618,7 +614,6 @@ class LoginsSetting: Setting {
             touchIDReason: AuthenticationStrings.loginsTouchReason,
             success: {
                 self.settings?.navigateToLoginsList()
-                LeanplumIntegration.sharedInstance.track(eventName: .openedLogins)
             },
             cancel: {
                 self.deselectRow()
@@ -629,7 +624,6 @@ class LoginsSetting: Setting {
             })
         } else {
             settings?.navigateToLoginsList()
-            LeanplumIntegration.sharedInstance.track(eventName: .openedLogins)
         }
     }
 }
