b2844901b01c4331cc9166412d61c6028a020b8f
Susheel Daswani <Sdaswani@cs.stanford.edu>
2017-05-03 09:38:58 -0700

    Revert "Bug 1353820 - Allow User to stop their LeanPlum Tracking (#2680)"
    This reverts commit cb5be603ea27518313656ac0cfd4fee3c24ecc00.

--- Client/Frontend/Settings/AppSettingsOptions.swift
+++ Client/Frontend/Settings/AppSettingsOptions.swift
@@ -520,10 +520,7 @@ class SendAnonymousUsageDataSetting: BoolSetting {
             prefs: prefs, prefKey: "settings.sendUsageData", defaultValue: true,
             attributedTitleText: NSAttributedString(string: NSLocalizedString("Send Anonymous Usage Data", tableName: "SendAnonymousUsageData", comment: "See http://bit.ly/1SmEXU1")),
             attributedStatusText: NSAttributedString(string: NSLocalizedString("More Info…", tableName: "SendAnonymousUsageData", comment: "See http://bit.ly/1SmEXU1"), attributes: [NSForegroundColorAttributeName: UIConstants.HighlightBlue]),
-            settingDidChange: {
-                AdjustIntegration.setEnabled($0)
-                LeanplumIntegration.sharedInstance.setEnabled($0)
-            }
+            settingDidChange: { AdjustIntegration.setEnabled($0) }
         )
     }
 
