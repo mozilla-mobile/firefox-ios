e7b3cb140b4ef03ad0cf3a24971bf0255dfb8a9b
James Hugman <james@hugman.tv>
2017-05-04 13:39:02 +0100

    Bug 1362015 – Update push configuration for production servers.

--- Client/Helpers/FxALoginHelper.swift
+++ Client/Helpers/FxALoginHelper.swift
@@ -191,7 +191,8 @@ class FxALoginHelper {
     }
 
     func getPushConfiguration() -> PushConfiguration? {
-        return ProductionPushConfiguration()
+        let label = PushConfigurationLabel(rawValue: AppConstants.scheme)
+        return label?.toConfiguration()
     }
 
     func apnsRegisterDidSucceed(_ deviceToken: Data) {
