4451ace6c4e55a1b064bb518e11e44ae51ca2d6d
James Hugman <james@hugman.tv>
2017-06-07 23:29:51 +0100

    Bug 1365518 – Only allow clients to be synced.
    We’ll syncEverything on next launch.

--- Extensions/NotificationService/ExtensionProfile.swift
+++ Extensions/NotificationService/ExtensionProfile.swift
@@ -3,6 +3,8 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import Foundation
+import Shared
+import Storage
 import Sync
 
 // This is a cut down version of the Profile. 
@@ -10,6 +12,14 @@ import Sync
 // It allows us to customize the SyncDelegate, and later the SyncManager.
 class ExtensionProfile: BrowserProfile {
     var syncDelegate: SyncDelegate!
+
+    override var logins: BrowserLogins & SyncableLogins & ResettableSyncStorage {
+        get {
+            fatalError("Cannot use logins.db in extension")
+        }
+        set {}
+    }
+
 
     init(localName: String) {
         super.init(localName: localName, app: nil, clear: false)
@@ -19,8 +29,9 @@ class ExtensionProfile: BrowserProfile {
     override func getSyncDelegate() -> SyncDelegate {
         return syncDelegate
     }
-
 }
+
+fileprivate let extensionSafeNames = Set(["clients"])
 
 class ExtensionSyncManager: BrowserProfile.BrowserSyncManager {
 
@@ -28,7 +39,14 @@ class ExtensionSyncManager: BrowserProfile.BrowserSyncManager {
         super.init(profile: profile)
     }
 
+    // We don't want to send ping data at all while we're in the extension.
     override func canSendUsageData() -> Bool {
         return false
+    }
+
+    // We should probably only want to sync client commands while we're in the extension.
+    override func syncNamedCollections(why: SyncReason, names: [String]) -> Success {
+        let names = names.filter { extensionSafeNames.contains($0) }
+        return super.syncNamedCollections(why: why, names: names)
     }
 }
