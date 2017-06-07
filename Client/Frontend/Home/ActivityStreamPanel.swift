23fcf82b30647417aea11c74b290af8a6fce43c0
Farhan Patel <farhanpatel.17@gmail.com>
2017-06-06 19:17:23 -0700

    Bug 1367253 - Update Activity Stream context menu UI. (#2753) r=sleroux
    * Bug 1367253 - Update Activity Stream context menu UI.
    
    * Review comments
    
    * remove spaces
    
    * remove unneeded
    
    * new icons
    
    * new trash icon
    
    * make things a little bigger

--- Client/Frontend/Home/ActivityStreamPanel.swift
+++ Client/Frontend/Home/ActivityStreamPanel.swift
@@ -102,6 +102,7 @@ class ActivityStreamPanel: UICollectionViewController, HomePanel {
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
         super.viewWillTransition(to: size, with: coordinator)
         coordinator.animate(alongsideTransition: {context in
+            self.presentedViewController?.dismiss(animated: true, completion: nil)
             self.collectionViewLayout.invalidateLayout()
             self.collectionView?.reloadData()
         }, completion: nil)
@@ -559,12 +560,12 @@ extension ActivityStreamPanel: DataObserverDelegate {
             pingSource = .HighlightsIntro
         }
 
-        let openInNewTabAction = ActionOverlayTableViewAction(title: Strings.OpenInNewTabContextMenuTitle, iconString: "action_new_tab") { action in
+        let openInNewTabAction = ActionOverlayTableViewAction(title: Strings.OpenInNewTabContextMenuTitle, iconString: "") { action in
             self.homePanelDelegate?.homePanelDidRequestToOpenInNewTab(siteURL, isPrivate: false)
             self.telemetry.reportEvent(.NewTab, source: pingSource, position: index)
         }
 
-        let openInNewPrivateTabAction = ActionOverlayTableViewAction(title: Strings.OpenInNewPrivateTabContextMenuTitle, iconString: "action_new_private_tab") { action in
+        let openInNewPrivateTabAction = ActionOverlayTableViewAction(title: Strings.OpenInNewPrivateTabContextMenuTitle, iconString: "") { action in
             self.homePanelDelegate?.homePanelDidRequestToOpenInNewTab(siteURL, isPrivate: true)
         }
 
