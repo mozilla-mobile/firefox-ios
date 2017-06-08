3df4fe83288063147c5ee4b92c7fc448cfa53cbf
Bryan Munar <brymunar@gmail.com>
2017-06-08 12:31:47 -0700

    Bug 1368237 - Disable LeanPlumIntegration while in private browsing (#2792)

--- Client/Application/AppDelegate.swift
+++ Client/Application/AppDelegate.swift
@@ -787,3 +787,10 @@ struct LaunchParams {
     let url: URL?
     let isPrivate: Bool?
 }
+
+extension UIApplication {
+    static var isInPrivateMode: Bool {
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        return appDelegate?.browserViewController.tabManager.selectedTab?.isPrivate ?? false
+    }
+}
