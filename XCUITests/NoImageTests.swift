d073b42006e759104f4dc0e3824981655327b65b
ugarlapati <ugarlapati@mozzila.com>
2017-06-16 07:20:08 -0700

    Bug 1373753 - XCUITestCase NoImageTests for Show and hide images

---
+++ XCUITests/NoImageTests.swift
@@ -0,0 +1,68 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ License, v. 2.0. If a copy of the MPL was not distributed with this
+ file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import XCTest
+
+class NoImageTests: BaseTestCase {
+    
+    var navigator: Navigator!
+    var app: XCUIApplication!
+
+    override func setUp() {
+        super.setUp()
+        app = XCUIApplication()
+        navigator = createScreenGraph(app).navigator(self)
+    }
+    
+    override func tearDown() {
+        navigator = nil
+        app = nil
+        super.tearDown()
+    }
+    
+    private func showImages() {
+        app.buttons["TabToolbar.menuButton"].tap()
+        app.collectionViews.containing(.cell, identifier:"FindInPageMenuItem").element.swipeLeft()
+        let collectionViewsQuery = app.collectionViews
+        collectionViewsQuery.cells["ShowImageModeMenuItem"].tap()
+    }
+    
+    private func hideImages() {
+        app.buttons["TabToolbar.menuButton"].tap()
+        app.collectionViews.containing(.cell, identifier:"FindInPageMenuItem").element.swipeLeft()
+        let collectionViewsQuery = app.collectionViews
+        collectionViewsQuery.cells["HideImageModeMenuItem"].tap()
+    }
+    
+    private func checkShowImages() {
+        navigator.goto(BrowserTabMenu)
+        app.collectionViews.containing(.cell, identifier:"FindInPageMenuItem").element.swipeLeft()
+        waitforExistence(app.collectionViews.cells["ShowImageModeMenuItem"])
+        navigator.goto(BrowserTab)
+    }
+    
+    private func checkHideImages() {
+        navigator.goto(BrowserTabMenu)
+        app.collectionViews.containing(.cell, identifier:"FindInPageMenuItem").element.swipeLeft()
+        waitforExistence(app.collectionViews.cells["HideImageModeMenuItem"])
+        navigator.goto(BrowserTab)
+    }
+    
+    func testBrowseUI() {
+        let url1 = "www.google.com"
+        
+        // Go to a webpage, and select no images or hide images, check it's hidden or not
+        navigator.openNewURL(urlString: url1)
+        hideImages()
+        checkShowImages()
+        
+        // Load a same page on a new tab, check images are hidden
+        //navigator.goto(NewTabScreen)
+        navigator.openNewURL(urlString: url1)
+        
+        // Open it, then select show images it, and check it's showing the images
+        showImages()
+        checkHideImages()
+    }
+}
