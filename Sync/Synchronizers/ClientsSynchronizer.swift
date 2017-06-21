31be34fbc045f64f4d8d3a8100c62114c4d6a25e
Justin D'Arcangelo <justindarc@gmail.com>
2017-06-21 11:25:22 -0400

    Bug 1373276 - Call POST /account/devices/notify in the send tab extension

--- Sync/Synchronizers/ClientsSynchronizer.swift
+++ Sync/Synchronizers/ClientsSynchronizer.swift
@@ -409,21 +409,19 @@ open class ClientsSynchronizer: TimestampedSingleCollectionSynchronizer, Synchro
 
         let clientsClient = storageClient.clientForCollection(self.collection, encrypter: encrypter!)
 
-        if !self.remoteHasChanges(info) {
-            log.debug("No remote changes for clients. (Last fetched \(self.lastFetched).)")
-            statsSession.start()
-            return self.maybeUploadOurRecord(false, ifUnmodifiedSince: nil, toServer: clientsClient)
-                >>> { self.uploadClientCommands(toLocalClients: localClients, withServer: clientsClient) }
-                >>> { deferMaybe(self.completedWithStats) }
-        }
-
         // TODO: some of the commands we process might involve wiping collections or the
         // entire profile. We should model this as an explicit status, and return it here
         // instead of .completed.
         statsSession.start()
-        return clientsClient.getSince(self.lastFetched)
+        // XXX: This is terrible. We always force a re-sync of the clients to work around
+        // the fact that `fxaDeviceId` may not have been populated if the list of clients
+        // hadn't changed since before the update to v8.0. To force a re-sync, we get all
+        // clients since the beginning of time instead of looking at `self.lastFetched`.
+        // We also call `localClients.wipeClients()` directly instead of relying on
+        // `self.wipeIfNecessary(localClients)` to force the list to always be current.
+        return clientsClient.getSince(0)
             >>== { response in
-                return self.wipeIfNecessary(localClients)
+                return localClients.wipeClients()
                     >>> { self.applyStorageResponse(response, toLocalClients: localClients, withServer: clientsClient) }
             }
             >>> { deferMaybe(self.completedWithStats) }
