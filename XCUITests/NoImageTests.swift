f0148231ed12e9fe29a691fa3435fc94ac1203e6
No-Jun Park <npark@mozilla.com>
2017-06-21 17:22:23 -0400

    Bug 1375247 - check actual image presense on browser view in NoImageTests

--- XCUITests/NoImageTests.swift
+++ XCUITests/NoImageTests.swift
@@ -1,6 +1,6 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
- License, v. 2.0. If a copy of the MPL was not distributed with this
- file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import XCTest
 
@@ -49,20 +49,25 @@ class NoImageTests: BaseTestCase {
         navigator.goto(BrowserTab)
     }
     
-    func testBrowseUI() {
-        let url1 = "www.google.com"
-        
-        // Go to a webpage, and select no images or hide images, check it's hidden or not
-        navigator.openNewURL(urlString: url1)
-        hideImages()
-        checkShowImages()
-        
-        // Load a same page on a new tab, check images are hidden
-        //navigator.goto(NewTabScreen)
-        navigator.openNewURL(urlString: url1)
-        
-        // Open it, then select show images it, and check it's showing the images
-        showImages()
-        checkHideImages()
+    func testImageOnOff() {
+    let url1 = "www.google.com"
+    
+    // Go to a webpage, and select no images or hide images, check it's hidden or not
+    navigator.openNewURL(urlString: url1)
+    XCTAssertTrue(app.images.count == 2)
+    hideImages()
+    
+    //After image is hidden, only image detected is the lock icon in the UI
+    XCTAssertTrue(app.images.count == 1)
+    checkShowImages()
+    
+    // Load a same page on a new tab, check images are hidden
+    //navigator.goto(NewTabScreen)
+    navigator.openNewURL(urlString: url1)
+    
+    // Open it, then select show images it, and check it's showing the images
+    showImages()
+    XCTAssertTrue(app.images.count == 2)
+    checkHideImages()
     }
 }
