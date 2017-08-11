b32d4a82c7319203989af567f120cc5a75e15cad
Stefan Arentz <stefan@arentz.ca>
2017-08-10 22:52:06 -0400

    Bug 1389348 - Data races in DiskImageStore (#3026)

--- Storage/DiskImageStore.swift
+++ Storage/DiskImageStore.swift
@@ -23,7 +23,7 @@ private class DiskImageStoreErrorType: MaybeErrorType {
 open class DiskImageStore {
     fileprivate let files: FileAccessor
     fileprivate let filesDir: String
-    fileprivate let queue = DispatchQueue(label: "DiskImageStore", attributes: DispatchQueue.Attributes.concurrent)
+    fileprivate let queue = DispatchQueue(label: "DiskImageStore")
     fileprivate let quality: CGFloat
     fileprivate var keys: Set<String>
 
@@ -44,11 +44,11 @@ open class DiskImageStore {
 
     /// Gets an image for the given key if it is in the store.
     open func get(_ key: String) -> Deferred<Maybe<UIImage>> {
-        if !keys.contains(key) {
-            return deferMaybe(DiskImageStoreErrorType(description: "Image key not found"))
-        }
-
         return deferDispatchAsync(queue) { () -> Deferred<Maybe<UIImage>> in
+            if !self.keys.contains(key) {
+                return deferMaybe(DiskImageStoreErrorType(description: "Image key not found"))
+            }
+
             let imagePath = URL(fileURLWithPath: self.filesDir).appendingPathComponent(key)
             if let data = try? Data(contentsOf: imagePath),
                    let image = UIImage.imageFromDataThreadSafe(data) {
@@ -63,11 +63,11 @@ open class DiskImageStore {
     /// This put is asynchronous; the image is not recorded in the cache until the write completes.
     /// Does nothing if this key already exists in the store.
     @discardableResult open func put(_ key: String, image: UIImage) -> Success {
-        if keys.contains(key) {
-            return deferMaybe(DiskImageStoreErrorType(description: "Key already in store"))
-        }
-
         return deferDispatchAsync(queue) { () -> Success in
+            if self.keys.contains(key) {
+                return deferMaybe(DiskImageStoreErrorType(description: "Key already in store"))
+            }
+
             let imageURL = URL(fileURLWithPath: self.filesDir).appendingPathComponent(key)
             if let data = UIImageJPEGRepresentation(image, self.quality) {
                 do {
@@ -84,18 +84,22 @@ open class DiskImageStore {
     }
 
     /// Clears all images from the cache, excluding the given set of keys.
-    open func clearExcluding(_ keys: Set<String>) {
-        let keysToDelete = self.keys.subtracting(keys)
+    open func clearExcluding(_ keys: Set<String>) -> Success {
+        return deferDispatchAsync(queue) { () -> Success in
+            let keysToDelete = self.keys.subtracting(keys)
 
-        for key in keysToDelete {
-            let url = URL(fileURLWithPath: filesDir).appendingPathComponent(key)
-            do {
-                try FileManager.default.removeItem(at: url)
-            } catch {
-                log.warning("Failed to remove DiskImageStore item at \(url.absoluteString): \(error)")
+            for key in keysToDelete {
+                let url = URL(fileURLWithPath: self.filesDir).appendingPathComponent(key)
+                do {
+                    try FileManager.default.removeItem(at: url)
+                } catch {
+                    log.warning("Failed to remove DiskImageStore item at \(url.absoluteString): \(error)")
+                }
             }
-        }
 
-        self.keys = self.keys.intersection(keys)
+            self.keys = self.keys.intersection(keys)
+
+            return succeed()
+        }
     }
 }
