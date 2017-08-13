bd1d3f178b7d145f49ff1f983218680c72106431
Bryan Munar <brymunar@gmail.com>
2017-08-13 12:25:33 -0700

    Drop an event when User has Reader View on/available (for Leanplum) (#3033)

--- Client/Frontend/Browser/BrowserViewController.swift
+++ Client/Frontend/Browser/BrowserViewController.swift
@@ -1535,6 +1535,8 @@ extension BrowserViewController: URLBarDelegate {
                 switch readerMode.state {
                 case .available:
                     enableReaderMode()
+
+                    LeanplumIntegration.sharedInstance.track(eventName: .useReaderView)
                 case .active:
                     disableReaderMode()
                 case .unavailable:
