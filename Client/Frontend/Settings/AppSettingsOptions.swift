af2b5d6965da255e8dff81af5c489a96477b1334
farhanpatel <farhanpatel.17@gmail.com>
2017-08-17 14:29:58 -0700

    Clean up some more nits

--- Client/Frontend/Settings/AppSettingsOptions.swift
+++ Client/Frontend/Settings/AppSettingsOptions.swift
@@ -71,7 +71,7 @@ class DisconnectSetting: WithAccountSetting {
                 self.settings.settings = self.settings.generateSettings()
                 self.settings.SELfirefoxAccountDidChange()
 
-                LeanplumIntegration.sharedInstance.setUserAttributes(attributes: [UserAttributeKeyName.signedInSync.rawValue : self.profile.hasAccount()])
+                LeanplumIntegration.sharedInstance.setUserAttributes(attributes: [UserAttributeKeyName.signedInSync.rawValue: self.profile.hasAccount()])
             })
         navigationController?.present(alertController, animated: true, completion: nil)
     }
@@ -562,7 +562,7 @@ class SendAnonymousUsageDataSetting: BoolSetting {
             attributedStatusText: NSAttributedString(string: NSLocalizedString("More Info…", tableName: "SendAnonymousUsageData", comment: "See http://bit.ly/1SmEXU1"), attributes: [NSForegroundColorAttributeName: UIConstants.HighlightBlue]),
             settingDidChange: {
                 AdjustIntegration.setEnabled($0)
-                LeanplumIntegration.sharedInstance.setUserAttributes(attributes: [UserAttributeKeyName.telemetryOptIn.rawValue : $0])
+                LeanplumIntegration.sharedInstance.setUserAttributes(attributes: [UserAttributeKeyName.telemetryOptIn.rawValue: $0])
                 LeanplumIntegration.sharedInstance.setEnabled($0)
             }
         )
