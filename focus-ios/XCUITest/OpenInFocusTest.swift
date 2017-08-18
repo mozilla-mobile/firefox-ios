143e8b336c139dfe913efa9eeb21d99efdcff857
No-Jun Park <npark@mozilla.com>
2017-08-18 00:48:21 -0400

    [focus-ios] Timing required to get the app.label properly (https://github.com/mozilla-mobile/focus-ios/pull/325)

--- focus-ios/XCUITest/OpenInFocusTest.swift
+++ focus-ios/XCUITest/OpenInFocusTest.swift
@@ -18,8 +18,9 @@ class OpenInFocusTest : BaseTestCase {
 
     func testOpenViaSafari() {
         let app = XCUIApplication()
+        waitforExistence(element: app.buttons["URLBar.activateButton"])
         let sharedExtName = app.label.contains("Klar") ? "Firefox Klar" : "Firefox Focus" as String
-
+        
         let safariApp = XCUIApplication(privateWithPath: nil, bundleID: "com.apple.mobilesafari")!
         safariApp.launchArguments = ["-u", "https://www.mozilla.org/en-US/"]
         safariApp.launch()
