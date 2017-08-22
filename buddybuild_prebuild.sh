3265ad93f4f742f9c51d1d496063bcc384639c19
Farhan Patel <farhanpatel.17@gmail.com>
2017-08-22 12:47:49 -0700

    No Bug - Add the Pocket API key via Buddybuild prebuild script. (#3068)

--- buddybuild_prebuild.sh
+++ buddybuild_prebuild.sh
@@ -33,6 +33,10 @@ echo "Setting Leanplum environment to PRODUCTION for $BUDDYBUILD_SCHEME"
 /usr/libexec/PlistBuddy -c "Set LeanplumEnvironment production" "Client/Info.plist"
 /usr/libexec/PlistBuddy -c "Set LeanplumKey $LEANPLUM_KEY_PRODUCTION" "Client/Info.plist"
 
+echo "Setting up Pocket Stories API Key"
+/usr/libexec/PlistBuddy -c "Set PocketEnvironmentAPIKey $POCKET_API_KEY" "Client/Info.plist"
+
+
 #
 # Setup Sentry. We have different DSNs for Beta and Production.
 #
