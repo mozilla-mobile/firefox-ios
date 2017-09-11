81b1d28991d4d947eb59196b4df0d54ee5086ebd
Garvan Keeley <garvankeeley@gmail.com>
2017-09-11 12:42:34 -0400

    Bug 1398151: Changes the 2nd footer to a More Info link We have no dedicated More Info string for this view, however there is an existing More Info label we can steal until we add strings for 9.1.

--- Client/Frontend/ContentBlocker/ContentBlockerSettingViewController.swift
+++ Client/Frontend/ContentBlocker/ContentBlockerSettingViewController.swift
@@ -6,7 +6,38 @@ import Foundation
 import Shared
 
 @available(iOS 11.0, *)
-class ContentBlockerSettingViewController: SettingsTableViewController {
+class ContentBlockerSettingsTableView : SettingsTableViewController {
+    // The first section header gets a More Info link
+    override func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        if section == 0 {
+            return super.tableView(tableView, viewForFooterInSection: section)
+        }
+
+        // TODO: Get a dedicated string for this.
+        let title = NSLocalizedString("More Info…", tableName: "SendAnonymousUsageData", comment: "Re-using more info label from 'anonymous usage data' item for showing a 'More Info' link on the Tracking Protection settings screen.")
+
+        var attributes = [String: AnyObject]()
+        attributes[NSFontAttributeName] = UIFont.systemFont(ofSize: 12, weight: UIFontWeightRegular)
+        attributes[NSForegroundColorAttributeName] = UIConstants.HighlightBlue
+
+        let button = UIButton()
+        button.setAttributedTitle(NSAttributedString(string: title, attributes: attributes), for: .normal)
+        button.contentHorizontalAlignment = .left
+        // Top and left insets are needed to match the table row style.
+        button.contentEdgeInsets = UIEdgeInsetsMake(8, 16, 0, 0)
+        button.addTarget(self, action: #selector(ContentBlockerSettingsTableView.moreInfoTapped), for: .touchUpInside)
+        return button
+    }
+
+    func moreInfoTapped() {
+        let viewController = SettingsContentViewController()
+        viewController.url = SupportUtils.URLForTopic("tracking-protection-ios")
+        navigationController?.pushViewController(viewController, animated: true)
+    }
+}
+
+@available(iOS 11.0, *)
+class ContentBlockerSettingViewController: ContentBlockerSettingsTableView {
     let prefs: Prefs
     let EnabledStates = ContentBlockerHelper.EnabledState.allOptions
     let BlockingStrengths = ContentBlockerHelper.BlockingStrength.allOptions
@@ -57,8 +88,10 @@ class ContentBlockerSettingViewController: SettingsTableViewController {
 
         let firstSection = SettingSection(title: NSAttributedString(string: Strings.TrackingProtectionOptionOnOffHeader), footerTitle: NSAttributedString(string: Strings.TrackingProtectionOptionOnOffFooter), children: enabledSetting)
 
+        // The bottom of the block lists section has a More Info button, implemented as a custom footer view,
+        // SettingSection needs footerTitle set to create a footer, which we then override the view for.
         let blockListsTitle = Strings.TrackingProtectionOptionBlockListsTitle
-        let secondSection = SettingSection(title: NSAttributedString(string: blockListsTitle), footerTitle: NSAttributedString(string: Strings.TrackingProtectionOptionFooter), children: strengthSetting)
+        let secondSection = SettingSection(title: NSAttributedString(string: blockListsTitle), footerTitle: NSAttributedString(string: "placeholder replaced with UIButton"), children: strengthSetting)
         return [firstSection, secondSection]
     }
 }
