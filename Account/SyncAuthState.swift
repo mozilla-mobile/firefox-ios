95dd2e5b37b6a19b288affc3921a3ade3ed4d7b8
jhugman <jhugman@users.noreply.github.com>
2017-09-22 15:11:28 +0100

    Bug 1395547 — Clear remote-only tabs when disconnecting from FxA account. (#3199) r=rnewman, justindarc
    * Bug 1395547 — Add FxADeviceId in SyncStateMachine.
    
    Persist it in the scratchpad prefs.
    
    This replaces the previous hack of poking deviceRegistration into a constructed prefs object.
    
    * Bug 1395547 — Detect a change in our client record and reset the record if changed.
    
    * Bug 1395547 — Only fill deferred once our tabs upload.
    
    This ultimately did not fix the bug we were seeking to fix, though should now return correct completed stats.
    
    * Bug 1395547 — Only remove the tabs from remote clients.
    
    Currently we do not close tabs when disconnecting an account. This `clear` method now reflects that, and leaves rows in the tabs table that are open on this device.
    
    * Bug 1395547 — Add a migration between storing the whole deviceRegistration to just storing the deviceID.
    
    * Bug 1395547 — Addressed nits.

--- Account/SyncAuthState.swift
+++ Account/SyncAuthState.swift
@@ -21,6 +21,7 @@ public struct SyncAuthStateCache {
 public protocol SyncAuthState {
     func invalidate()
     func token(_ now: Timestamp, canBeExpired: Bool) -> Deferred<Maybe<(token: TokenServerToken, forKey: Data)>>
+    var deviceID: String? { get }
 }
 
 public func syncAuthStateCachefromJSON(_ json: JSON) -> SyncAuthStateCache? {
@@ -53,6 +54,9 @@ extension SyncAuthStateCache: JSONLiteralConvertible {
 open class FirefoxAccountSyncAuthState: SyncAuthState {
     fileprivate let account: FirefoxAccount
     fileprivate let cache: KeychainCache<SyncAuthStateCache>
+    public var deviceID: String? {
+        return account.deviceRegistration?.id
+    }
 
     init(account: FirefoxAccount, cache: KeychainCache<SyncAuthStateCache>) {
         self.account = account
