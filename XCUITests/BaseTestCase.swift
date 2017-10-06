dfbe9fe73dde222c8524b91581a90355dc7a12da
James Hugman <james@hugman.tv>
2017-10-06 15:49:23 +0100

    Bug 1406125 — Fixup `loadWebPage()` for better reporting.

--- XCUITests/BaseTestCase.swift
+++ XCUITests/BaseTestCase.swift
@@ -61,7 +61,7 @@ class BaseTestCase: XCTestCase {
         }
     }
 
-    func loadWebPage(_ url: String, waitForLoadToFinish: Bool = true) {
+    func loadWebPage(_ url: String, waitForLoadToFinish: Bool = true, file: String = #file, line: UInt = #line) {
         let app = XCUIApplication()
         UIPasteboard.general.string = url
         app.textFields["url"].press(forDuration: 2.0)
@@ -70,10 +70,11 @@ class BaseTestCase: XCTestCase {
         if waitForLoadToFinish {
             let finishLoadingTimeout: TimeInterval = 30
             let progressIndicator = app.progressIndicators.element(boundBy: 0)
-            expectation(for: NSPredicate(format: "exists = true"), evaluatedWith: progressIndicator, handler: nil)
-            expectation(for: NSPredicate(format: "value BEGINSWITH '0'"), evaluatedWith: progressIndicator, handler: nil)
-            
-            waitForExpectations(timeout: finishLoadingTimeout, handler: nil)
+            waitFor(progressIndicator,
+                    with: "exists != true",
+                    description: "Problem loading \(url)",
+                    timeout: finishLoadingTimeout,
+                    file: file, line: line)
         }
     }
     
