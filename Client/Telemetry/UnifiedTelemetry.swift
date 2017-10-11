2b5985dda508a3e003cafa8ea5ab704ea65e1c42
Garvan Keeley <garvankeeley+github@gmail.com>
2017-10-11 10:02:24 -0400

    [nobug, beta-only] Re-add sentry logging for unified telemetry (#3275)
    We were getting spammed with upload error messages, this should ensure
    these are aggregated in Sentry. I hope.

--- Client/Telemetry/UnifiedTelemetry.swift
+++ Client/Telemetry/UnifiedTelemetry.swift
@@ -40,7 +40,7 @@ class UnifiedTelemetry {
 
     @objc func uploadError(notification: NSNotification) {
         guard let error = notification.userInfo?["error"] as? NSError else { return }
-        SentryIntegration.shared.send(message: error.localizedDescription, tag: "UnifiedTelemetry", severity: .info)
+        SentryIntegration.shared.send(message: "Upload Error", tag: "UnifiedTelemetry", severity: .info, extra: ["Error": error.localizedDescription])
     }
 }
 
