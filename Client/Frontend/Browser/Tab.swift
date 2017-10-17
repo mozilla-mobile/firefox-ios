9bdd9cb219b32ec38a0b780fe7af6b247247b33e
Garvan Keeley <garvankeeley+github@gmail.com>
2017-10-17 13:55:48 -0400

    bug 1385845: Detect PDF mime type, and don't animate-block to show toolbar (#3302)
    Avoiding using animation-block syntax to show the toolbar works around this bug,
    and showing is instead performed like hiding, that is in-sync with touch-drag.

--- Client/Frontend/Browser/Tab.swift
+++ Client/Frontend/Browser/Tab.swift
@@ -72,6 +72,7 @@ class Tab: NSObject {
     var restoring: Bool = false
     var pendingScreenshot = false
     var url: URL?
+    var mimeType: String?
 
     fileprivate var _noImageMode = false
 
