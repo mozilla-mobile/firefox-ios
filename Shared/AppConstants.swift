71882fc80f7e5ecf9c99ce2c3007f85fca2d9ead
Richard Newman <rnewman@twinql.com>
2017-10-19 09:12:54 -0700

    Part 2: add URL, title, and description maximum lengths to match desktop.

--- Shared/AppConstants.swift
+++ Shared/AppConstants.swift
@@ -195,4 +195,13 @@ public struct AppConstants {
             return true
         #endif
     }()
+
+    /// The maximum length of a URL stored by Firefox. Shared with Places on desktop.
+    public static let DB_URL_LENGTH_MAX = 65536
+
+    /// The maximum length of a page title stored by Firefox. Shared with Places on desktop.
+    public static let DB_TITLE_LENGTH_MAX = 4096
+
+    /// The maximum length of a bookmark description stored by Firefox. Shared with Places on desktop.
+    public static let DB_DESCRIPTION_LENGTH_MAX = 1024
 }
