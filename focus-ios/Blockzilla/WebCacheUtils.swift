dabd1a6e01c1e02e2c1b3feced9a40c7c07b0d55
Jeff Boek <jeff@jeffboek.com>
2017-10-24 12:46:44 -0700

    [focus-ios] Fixes https://github.com/mozilla-mobile/focus-ios/issues/620 - Clears everything WKWebView creates (https://github.com/mozilla-mobile/focus-ios/issues/626)

--- focus-ios/Blockzilla/WebCacheUtils.swift
+++ focus-ios/Blockzilla/WebCacheUtils.swift
@@ -3,13 +3,20 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import Foundation
+import WebKit
 
 class WebCacheUtils {
     static let FolderWhiteList = ["KSCrash", "io.sentry", "Snapshots"]
 
     static func reset() {
+        clearCaches()
+        clearCookies()
+        clearWebKitDataStore()
+        removeInMemoryHistory()
+    }
+
+    private static func clearCaches() {
         URLCache.shared.removeAllCachedResponses()
-        HTTPCookieStorage.shared.removeCookies(since: Date.distantPast)
 
         // Delete other remnants in the cache directory, such as HSTS.plist.
         if let cachesPath = NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true).first {
@@ -20,13 +27,34 @@ class WebCacheUtils {
                 }
             }
         }
+    }
+
+    private static func clearCookies() {
+        HTTPCookieStorage.shared.removeCookies(since: Date.distantPast)
 
         // Delete other cookies, such as .binarycookies files.
         if let libraryPath = NSSearchPathForDirectoriesInDomains(.libraryDirectory, .userDomainMask, true).first {
             let cookiesPath = (libraryPath as NSString).appendingPathComponent("Cookies")
             FileManager.default.removeItemAndContents(path: cookiesPath)
         }
+    }
 
+    private static func clearWebKitDataStore() {
+        // Clear everything that WKWebView creates
+        let dateFrom = Date(timeIntervalSince1970: 0)
+        let dataTypes: Set<String> = [WKWebsiteDataTypeDiskCache,
+                                      WKWebsiteDataTypeOfflineWebApplicationCache,
+                                      WKWebsiteDataTypeMemoryCache,
+                                      WKWebsiteDataTypeLocalStorage,
+                                      WKWebsiteDataTypeCookies,
+                                      WKWebsiteDataTypeSessionStorage,
+                                      WKWebsiteDataTypeIndexedDBDatabases,
+                                      WKWebsiteDataTypeWebSQLDatabases]
+
+        WKWebsiteDataStore.default().removeData(ofTypes: dataTypes, modifiedSince: dateFrom, completionHandler: {})
+    }
+
+    private static func removeInMemoryHistory() {
         // Remove the in-memory history that WebKit maintains.
         // With Swift 4 we have to cast it to AnyObject first
         // https://stackoverflow.com/questions/45957626/swift-4-objective-c-runtime-and-casting-to-nsobjectprotocol
