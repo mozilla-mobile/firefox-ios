82b095a92927a5e5cb0240c5bccdc61a5ef85a8e
Jeff Boek <jeff@jeffboek.com>
2017-11-10 14:39:04 -0800

    [focus-ios] Fixes https://github.com/mozilla-mobile/focus-ios/pull/674 - TP Drawer constraint when content protection is on (https://github.com/mozilla-mobile/focus-ios/pull/683)

--- focus-ios/Blockzilla/Lib/TrackingProtection/TrackingProtectionSummaryViewController.swift
+++ focus-ios/Blockzilla/Lib/TrackingProtection/TrackingProtectionSummaryViewController.swift
@@ -117,7 +117,7 @@ class TrackingProtectionBreakdownVisualizer: UIView {
         contentSection.snp.remakeConstraints { make in
             make.top.equalToSuperview()
             make.height.equalToSuperview()
-            make.width.equalTo(0)
+            make.width.equalTo(0).priority(500)
             make.width.equalToSuperview().multipliedBy(calculateMultiplier(info.contentCount))
             make.trailing.equalToSuperview().priority(500)
         }
