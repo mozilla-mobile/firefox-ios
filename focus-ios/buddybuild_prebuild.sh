8f63dd45856bd64bda14175eabdf83676682a0ed
Jeff Boek <jeff@jeffboek.com>
2017-11-17 10:28:00 -0800

    [focus-ios] Changes if statement to use strings (https://github.com/mozilla-mobile/focus-ios/pull/708)

--- focus-ios/buddybuild_prebuild.sh
+++ focus-ios/buddybuild_prebuild.sh
@@ -5,17 +5,13 @@
 # that we have some insight in beta usage.
 #
 
-FocusEnterprise="Focus (Enterprise)"
-FocusRelease="Focus"
-KlarRelease="Klar"
-
-if [ "$BUDDYBUILD_SCHEME" == FocusEnterprise ]; then
+if [ "$BUDDYBUILD_SCHEME" == "Focus (Enterprise)" ]; then
   echo "Setting Adjust token to SANDBOX for $BUDDYBUILD_SCHEME"
   /usr/libexec/PlistBuddy -c "Set AppToken $ADJUST_TOKEN_RELEASE" "$BUDDYBUILD_WORKSPACE/Blockzilla/Adjust-Focus.plist"
-elif [ "$BUDDYBUILD_SCHEME" == FocusRelease ]; then
+elif [ "$BUDDYBUILD_SCHEME" == "Focus" ]; then
   echo "Setting Adjust token to RELEASE for $BUDDYBUILD_SCHEME"
   /usr/libexec/PlistBuddy -c "Set AppToken $ADJUST_TOKEN_RELEASE" "$BUDDYBUILD_WORKSPACE/Blockzilla/Adjust-Focus.plist"
-elif [ "$BUDDYBUILD_SCHEME" == KlarRelease ]; then
+elif [ "$BUDDYBUILD_SCHEME" == "Klar" ]; then
   echo "Setting Adjust token to RELEASE for $BUDDYBUILD_SCHEME"
   /usr/libexec/PlistBuddy -c "Set AppToken $ADJUST_TOKEN_RELEASE" "$BUDDYBUILD_WORKSPACE/Blockzilla/Adjust-Focus.plist"
 fi
@@ -25,16 +21,20 @@ fi
 # that we have some insight in beta usage.
 #
 
-if [ "$BUDDYBUILD_SCHEME" == FocusEnterprise ]; then
+if [ "$BUDDYBUILD_SCHEME" == "Focus (Enterprise)" ]; then
   echo "Setting Sentry DSN to BETA for $BUDDYBUILD_SCHEME"
   /usr/libexec/PlistBuddy -c "Set SentryDSN $SENTRY_DSN_BETA" "Blockzilla/Info.plist"
-elif [ "$BUDDYBUILD_SCHEME" == FocusRelease ]; then
+elif [ "$BUDDYBUILD_SCHEME" == "Focus" ]; then
   echo "Setting Sentry DSN to PRODUCTION for $BUDDYBUILD_SCHEME"
   /usr/libexec/PlistBuddy -c "Set SentryDSN $SENTRY_DSN_RELEASE" "Blockzilla/Info.plist"
-elif [ "$BUDDYBUILD_SCHEME" == KlarRelease ]; then
+elif [ "$BUDDYBUILD_SCHEME" == "Klar" ]; then
   echo "Setting Sentry DSN to PRODUCTION for $BUDDYBUILD_SCHEME"
   /usr/libexec/PlistBuddy -c "Set SentryDSN $SENTRY_DSN_RELEASE" "Blockzilla/Info.plist"
 fi
+
+
+# Set the build number to match the Buddybuild number
+agvtool new-version -all "$BUDDYBUILD_BUILD_NUMBER"
 
 
 # Set the build number to match the Buddybuild number
