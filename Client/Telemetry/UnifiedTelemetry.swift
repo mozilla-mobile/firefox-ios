36fbbf40d55eef5c86cd69d479a30b4b62ab3ffc
Justin D'Arcangelo <justindarc@gmail.com>
2017-12-01 15:58:40 -0500

    Bug 1422396 - Add UI event telemetry probes

--- Client/Telemetry/UnifiedTelemetry.swift
+++ Client/Telemetry/UnifiedTelemetry.swift
@@ -34,6 +34,7 @@ class UnifiedTelemetry {
         }
 
         Telemetry.default.add(pingBuilderType: CorePingBuilder.self)
+        Telemetry.default.add(pingBuilderType: MobileEventPingBuilder.self)
     }
 
     @objc func uploadError(notification: NSNotification) {
@@ -42,3 +43,50 @@ class UnifiedTelemetry {
     }
 }
 
+// Enums for Event telemetry.
+extension UnifiedTelemetry {
+    public enum EventCategory: String {
+        case action = "action"
+    }
+
+    public enum EventMethod: String {
+        case add = "add"
+        case background = "background"
+        case delete = "delete"
+        case foreground = "foreground"
+        case open = "open"
+        case tap = "tap"
+        case view = "view"
+    }
+
+    public enum EventObject: String {
+        case app = "app"
+        case bookmark = "bookmark"
+        case bookmarksPanel = "bookmarks-panel"
+        case readerModeCloseButton = "reader-mode-close-button"
+        case readerModeOpenButton = "reader-mode-open-button"
+        case readingListItem = "reading-list-item"
+        case setting = "setting"
+    }
+
+    public enum EventValue: String {
+        case appMenu = "app-menu"
+        case awesomebarResults = "awesomebar-results"
+        case bookmarksPanel = "bookmarks-panel"
+        case homePanelHighlights = "home-panel-highlights"
+        case homePanelPocketStories = "home-panel-pocket-stories"
+        case homePanelTabButton = "home-panel-tab-button"
+        case homePanelTopSites = "home-panel-top-sites"
+        case longPress = "long-press"
+        case markAsRead = "mark-as-read"
+        case pageActionMenu = "page-action-menu"
+        case readerModeToolbar = "reader-mode-toolbar"
+        case shareExtension = "share-extension"
+        case shareMenu = "share-menu"
+        case swipe = "swipe"
+    }
+
+    public static func recordEvent(category: EventCategory, method: EventMethod, object: EventObject, value: EventValue? = nil, extras: [String : Any?]? = nil) {
+        Telemetry.default.recordEvent(category: category.rawValue, method: method.rawValue, object: object.rawValue, value: value?.rawValue, extras: extras)
+    }
+}
