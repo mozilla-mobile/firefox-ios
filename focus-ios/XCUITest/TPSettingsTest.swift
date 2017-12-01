b3b55afa4a920645f036dddc6035d35bbe5c3b5e
Aaron Train <aaron.train@gmail.com>
2017-12-01 13:31:03 -0500

    [focus-ios] Add XCUITest for TP Settings (https://github.com/mozilla-mobile/focus-ios/issues/753)
    * Add XCUITest for TP Settings
    
    Initial testing of in-app settings for tracking protection. This test can be expanded in the future for testing all settings but for now check against mozilla.org
    
    * Remove a waitforExistence check

---
+++ focus-ios/XCUITest/TPSettingsTest.swift
@@ -0,0 +1,71 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import XCTest
+
+class TrackingProtectionSettings: BaseTestCase {
+
+    override func setUp() {
+        super.setUp()
+        dismissFirstRunUI()
+    }
+
+    override func tearDown() {
+        app.terminate()
+        super.tearDown()
+    }
+
+    func testInactiveSettings() {
+
+        // Go to in-app settings
+        app.buttons["Settings"].tap()
+        waitforExistence(element: app.tables.switches["BlockerToggle.BlockAnalytics"])
+
+        // Disable 'block analytic trackers'
+        app.tables.switches["BlockerToggle.BlockAnalytics"].tap()
+        XCTAssertEqual(app.tables.switches["BlockerToggle.BlockAnalytics"].value as! String, "0")
+
+        // Exit in-app settings
+        app.navigationBars["Settings"].children(matching: .button).matching(identifier: "Back").element(boundBy: 0).tap()
+
+        // Visit https://www.mozilla.org
+        loadWebPage("mozilla")
+
+        // Check the correct site is reached
+        waitForWebPageLoad()
+
+        // Check for the presence of the shield
+        // Currently, Mozilla has one (1) analytical tracker that is un-blocked
+        XCTAssertEqual(app.staticTexts["TrackingProtectionBadge.counterLabel"].label, "0")
+
+        // Open the tracking protection sidebar
+        app.otherElements["URLBar.trackingProtectionIcon"].tap()
+
+        // Wait for the sidebar to open
+        waitforExistence(element: app.staticTexts["Tracking Protection"])
+
+        // Check for the existence of one (1) analytical tracker on Mozilla
+        let counters = app.staticTexts.matching(identifier: "TrackingProtectionBreakdownItem.counterLabel")
+
+        XCTAssertEqual(counters.element(boundBy: 0).label, "0") // Ad Trackers
+        XCTAssertEqual(counters.element(boundBy: 1).label, "0") // Analytical trackers
+        XCTAssertEqual(counters.element(boundBy: 2).label, "0") // Social trackers
+        XCTAssertEqual(counters.element(boundBy: 3).label, "0") // Content trackers
+
+        // Close the sidebar
+        app.buttons["TrackingProtectionView.closeButton"].tap()
+
+        // Erase the history
+        app.buttons["ERASE"].tap()
+
+        // Reset in-app settings (work-around until issue: #731)
+        app.buttons["Settings"].tap()
+        waitforExistence(element: app.tables.switches["BlockerToggle.BlockAnalytics"])
+
+        // Re-enable 'block analytic trackers'
+        app.tables.switches["BlockerToggle.BlockAnalytics"].tap()
+        XCTAssertEqual(app.tables.switches["BlockerToggle.BlockAnalytics"].value as! String, "1")
+    }
+
+}
