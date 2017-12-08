1771c832690177dbf2a373d27a704f6774d07ab5
Justin D'Arcangelo <justindarc@gmail.com>
2017-12-08 14:51:42 -0500

    Bug 1424345 - SyncTelemetry ping may incorrectly format dates to the wrong calendar

--- SyncTelemetry/SyncTelemetry.swift
+++ SyncTelemetry/SyncTelemetry.swift
@@ -86,6 +86,7 @@ open class SyncTelemetry {
     private static func commonPingFormat(forType type: TelemetryDocType) -> [String: Any] {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd"
+        formatter.locale = Locale(identifier: "en_US_POSIX")
         let date = formatter.string(from: NSDate() as Date)
         let displayVersion =  [
             AppInfo.appVersion,
