c9ff9a3ce822a29472d49f26b7036e0e6ee9e790
Adam Nemecek <adamnemecek@gmail.com>
2018-01-04 18:55:44 -0800

    * readded mocklistened

--- StorageTests/TestBrowserDB.swift
+++ StorageTests/TestBrowserDB.swift
@@ -90,7 +90,7 @@ class TestBrowserDB: XCTestCase {
 
         let center = NotificationCenter.default
         let listener = MockListener()
-        center.addObserver(listener, selector: #selector(onDatabaseWasRecreated), name: .DatabaseWasRecreated, object: nil)
+        center.addObserver(listener, selector: #selector(MockListener.onDatabaseWasRecreated), name: .DatabaseWasRecreated, object: nil)
         defer { center.removeObserver(listener) }
 
         // It'll still fail, but it moved our old DB.
