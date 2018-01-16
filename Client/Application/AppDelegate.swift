ac232ebcf49e75e3ac98cb5ae4a934fb31300e3b
Garvan Keeley <garvankeeley@gmail.com>
2018-01-16 11:35:14 -0500

    Add nimbledroid cold start measurement

--- Client/Application/AppDelegate.swift
+++ Client/Application/AppDelegate.swift
@@ -199,6 +199,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UIViewControllerRestorati
         // Override point for customization after application launch.
         var shouldPerformAdditionalDelegateHandling = true
 
+        NDScenario.setup() // NimbleDroid lib setup
+
         adjustIntegration?.triggerApplicationDidFinishLaunchingWithOptions(launchOptions)
 
         UNUserNotificationCenter.current().delegate = self
@@ -294,6 +296,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UIViewControllerRestorati
         }
 
         UnifiedTelemetry.recordEvent(category: .action, method: .foreground, object: .app)
+
+        NDScenario.coldStartupEnd()
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
