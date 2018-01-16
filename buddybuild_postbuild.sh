4b0e81aaf7e9421c026fd13d3e59f97b195e0d58
Garvan Keeley <garvankeeley@gmail.com>
2018-01-16 14:27:34 -0500

    Try uploading Fennec to nimbledroid

--- buddybuild_postbuild.sh
+++ buddybuild_postbuild.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
 bash <(curl -s https://codecov.io/bash)
+
+if [ "$BUDDYBUILD_SCHEME" = "Fennec" ]; then
+  curl -F ipa=@$BUDDYBUILD_IPA_PATH -u $NIMBLEDROID_API_KEY: https://nimbledroid.com/api/v2/ipas
+fi
+
