4e3bc15a680a1886c52d287fde56efd63fb5ed2c
Justin D'Arcangelo <justindarc@gmail.com>
2018-01-23 11:20:19 -0500

    Bug 1432442 - [Regression] Not possible to pin a pdf from PAM

--- webpack.config.js
+++ webpack.config.js
@@ -7,10 +7,18 @@ const AllFramesAtDocumentEnd = glob.sync("./Client/Frontend/UserContent/UserScri
 const MainFrameAtDocumentStart = glob.sync("./Client/Frontend/UserContent/UserScripts/MainFrame/AtDocumentStart/*.js");
 const MainFrameAtDocumentEnd = glob.sync("./Client/Frontend/UserContent/UserScripts/MainFrame/AtDocumentEnd/*.js");
 
-// Ensure the first script loaded is __firefox__.js since it
-// defines the `window.__firefox__` global for all scripts.
+// Ensure the first script loaded at document start is __firefox__.js
+// since it defines the `window.__firefox__` global.
 if (path.basename(AllFramesAtDocumentStart[0]) !== "__firefox__.js") {
   throw "ERROR: __firefox__.js is expected to be the first script in AllFramesAtDocumentStart.js";
+}
+
+// Ensure the first script loaded at document end is __firefox__.js
+// since it also defines the `window.__firefox__` global because PDF
+// content does not execute user scripts designated to run at document
+// start for some reason. ¯\_(ツ)_/¯
+if (path.basename(AllFramesAtDocumentEnd[0]) !== "__firefox__.js") {
+  throw "ERROR: __firefox__.js is expected to be the first script in AllFramesAtDocumentEnd.js";
 }
 
 module.exports = {
