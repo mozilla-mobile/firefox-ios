dac6be69d6e954360bd921ae066e68b78057fc5d
No-Jun Park <npark@mozilla.com>
2018-01-24 18:52:45 -0500

    try BUDDYBUILD_TEST_DIR

--- buddybuild_postbuild.sh
+++ buddybuild_postbuild.sh
@@ -9,9 +9,9 @@ bash <(curl -s https://codecov.io/bash)
 test_runner="XCUITests-Runner"
 
 if [ "$BUDDYBUILD_SCHEME" = "Fennec" ]; then
-  (cd $BUDDYBUILD_PRODUCT_DIR && zip -r $test_runner.zip $test_runner.app)
+  (cd $BUDDYBUILD_TEST_DIR && zip -r $test_runner.zip $test_runner.app)
   curl -F ipa=@$BUDDYBUILD_IPA_PATH \
-       -F test_runner=@$BUDDYBUILD_PRODUCT_DIR/$test_runner.zip -F test_identifiers="DomainAutocompleteTest.testAutocomplete" \
+       -F test_runner=@$BUDDYBUILD_TEST_DIR/$test_runner.zip -F test_identifiers="DomainAutocompleteTest.testAutocomplete" \
        -u $NIMBLEDROID_API_KEY: https://nimbledroid.com/api/v2/ipas
 fi
 
