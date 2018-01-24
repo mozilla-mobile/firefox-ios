51e7739f60c49c88a07a5b20f0c54c62c9019957
Garvan Keeley <garvankeeley@gmail.com>
2018-01-24 15:04:44 -0500

    BuddyBuild post build uploads test runner to NimbleDroid

--- buddybuild_postbuild.sh
+++ buddybuild_postbuild.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 bash <(curl -s https://codecov.io/bash)
 
+test_runner="XCUITests-Runner"
+
 if [ "$BUDDYBUILD_SCHEME" = "Fennec" ]; then
-  curl -F ipa=@$BUDDYBUILD_IPA_PATH -u $NIMBLEDROID_API_KEY: https://nimbledroid.com/api/v2/ipas
+  (cd $BUDDYBUILD_PRODUCT_DIR && zip -r $test_runner.zip $test_runner.app)
+  curl -F ipa=@$BUDDYBUILD_IPA_PATH \
+       -F test_runner=@$BUDDYBUILD_PRODUCT_DIR/$test_runner.zip -F test_identifiers="DomainAutocompleteTest.testAutocomplete" \
+       -u $NIMBLEDROID_API_KEY: https://nimbledroid.com/api/v2/ipas
 fi
 
