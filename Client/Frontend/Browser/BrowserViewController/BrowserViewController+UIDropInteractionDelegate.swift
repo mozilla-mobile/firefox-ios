d1e9ea2235af5e2206d1438e545ed499eece312c
Justin D'Arcangelo <justindarc@gmail.com>
2018-01-25 14:28:10 -0500

    Bug 1433205 - [iOS 11] Allow hyperlinks to be dragged and dropped into Firefox for iOS

---
+++ Client/Frontend/Browser/BrowserViewController/BrowserViewController+UIDropInteractionDelegate.swift
@@ -0,0 +1,27 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import Foundation
+import Storage
+
+@available(iOS 11.0, *)
+extension BrowserViewController: UIDropInteractionDelegate {
+    func dropInteraction(_ interaction: UIDropInteraction, canHandle session: UIDropSession) -> Bool {
+        return session.canLoadObjects(ofClass: URL.self)
+    }
+
+    func dropInteraction(_ interaction: UIDropInteraction, sessionDidUpdate session: UIDropSession) -> UIDropProposal {
+        return UIDropProposal(operation: .copy)
+    }
+
+    func dropInteraction(_ interaction: UIDropInteraction, performDrop session: UIDropSession) {
+        _ = session.loadObjects(ofClass: URL.self) { urls in
+            guard let url = urls.first else {
+                return
+            }
+
+            self.finishEditingAndSubmit(url, visitType: VisitType.typed)
+        }
+    }
+}
