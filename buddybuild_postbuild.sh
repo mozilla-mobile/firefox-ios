1bdd3148aa47e08c92c8d4405920b4c4f8ade27a
No-Jun Park <npark@mozilla.com>
2018-01-24 19:29:32 -0500

    Update path

--- buddybuild_postbuild.sh
+++ buddybuild_postbuild.sh
@@ -9,9 +9,11 @@ bash <(curl -s https://codecov.io/bash)
 test_runner="XCUITests-Runner"
 
 if [ "$BUDDYBUILD_SCHEME" = "Fennec" ]; then
-  (cd $BUDDYBUILD_TEST_DIR && zip -r $test_runner.zip $test_runner.app)
+  (cd $BUDDYBUILD_TEST_DIR/Build/Products/Fennec-iphonesimulator \
+	&& zip -r $test_runner.zip $test_runner.app)
   curl -F ipa=@$BUDDYBUILD_IPA_PATH \
-       -F test_runner=@$BUDDYBUILD_TEST_DIR/$test_runner.zip -F test_identifiers="DomainAutocompleteTest.testAutocomplete" \
+       -F test_runner=@$BUDDYBUILD_TEST_DIR/Build/Products/Fennec-iphonesimulator/$test_runner.zip -F \
+	test_identifiers="DomainAutocompleteTest.testAutocomplete" \
        -u $NIMBLEDROID_API_KEY: https://nimbledroid.com/api/v2/ipas
 fi
 
