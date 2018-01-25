86f455160c191c19d5cfea2b3e84d4531756043e
No-Jun Park <npark@mozilla.com>
2018-01-25 11:37:28 -0500

    comment out uploading of testrunner, since it is for simulator

--- buddybuild_postbuild.sh
+++ buddybuild_postbuild.sh
@@ -8,12 +8,13 @@ bash <(curl -s https://codecov.io/bash)
 
 test_runner="XCUITests-Runner"
 
+# comment out the section where it upload the testrunner
 if [ "$BUDDYBUILD_SCHEME" = "Fennec" ]; then
-  (cd $BUDDYBUILD_TEST_DIR/Build/Products/Fennec-iphonesimulator \
-	&& zip -r $test_runner.zip $test_runner.app)
+#  (cd $BUDDYBUILD_TEST_DIR/Build/Products/Fennec-iphonesimulator \
+#	&& zip -r $test_runner.zip $test_runner.app)
   curl -F ipa=@$BUDDYBUILD_IPA_PATH \
-       -F test_runner=@$BUDDYBUILD_TEST_DIR/Build/Products/Fennec-iphonesimulator/$test_runner.zip -F \
-	test_identifiers="DomainAutocompleteTest.testAutocomplete" \
+#      -F test_runner=@$BUDDYBUILD_TEST_DIR/Build/Products/Fennec-iphonesimulator/$test_runner.zip -F \
+#	test_identifiers="DomainAutocompleteTest.testAutocomplete" \
        -u $NIMBLEDROID_API_KEY: https://nimbledroid.com/api/v2/ipas
 fi
 
