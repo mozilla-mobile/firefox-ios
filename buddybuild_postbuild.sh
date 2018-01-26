ca4bf463339585f6f969dab882768b8dbd0f5810
Garvan Keeley <garvankeeley@gmail.com>
2018-01-26 11:44:54 -0500

    fix nimbledroid upload

--- buddybuild_postbuild.sh
+++ buddybuild_postbuild.sh
@@ -10,11 +10,8 @@ test_runner="XCUITests-Runner"
 
 # comment out the section where it upload the testrunner
 if [ "$BUDDYBUILD_SCHEME" = "Fennec" ]; then
-#  (cd $BUDDYBUILD_TEST_DIR/Build/Products/Fennec-iphonesimulator \
-#	&& zip -r $test_runner.zip $test_runner.app)
-  curl -F ipa=@$BUDDYBUILD_IPA_PATH \
-#      -F test_runner=@$BUDDYBUILD_TEST_DIR/Build/Products/Fennec-iphonesimulator/$test_runner.zip -F \
-#	test_identifiers="DomainAutocompleteTest.testAutocomplete" \
-       -u $NIMBLEDROID_API_KEY: https://nimbledroid.com/api/v2/ipas
+#  (cd $BUDDYBUILD_TEST_DIR/Build/Products/Fennec-iphonesimulator && zip -r $test_runner.zip $test_runner.app)
+  curl -F ipa=@$BUDDYBUILD_IPA_PATH  -u $NIMBLEDROID_API_KEY: https://nimbledroid.com/api/v2/ipas
+#      -F test_runner=@$BUDDYBUILD_TEST_DIR/Build/Products/Fennec-iphonesimulator/$test_runner.zip -F test_identifiers="DomainAutocompleteTest.testAutocomplete"
 fi
 
