e107d0c51867b6bd2d84425977a81f91742e1390
Justin D'Arcangelo <justindarc@gmail.com>
2018-01-30 14:35:56 -0500

    Bug 1434334 - +[UAUserActivity(Internal) checkWebpageURL:actionType:throwIfFailed:] - EXC_CRASH (SIGABRT) (#3610)

--- Client/Frontend/Browser/Tab.swift
+++ Client/Frontend/Browser/Tab.swift
@@ -469,6 +469,7 @@ class Tab: NSObject {
         }
 
         self.urlDidChangeDelegate?.tab(self, urlDidChangeTo: url)
+        TabEvent.post(.didChangeURL(url), for: self)
     }
 
     func isDescendentOf(_ ancestor: Tab) -> Bool {
