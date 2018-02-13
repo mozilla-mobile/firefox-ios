a4775eff225c5089292e801614fa43606dd2df5a
Garvan Keeley <garvankeeley+github@gmail.com>
2018-02-13 11:34:10 -0500

    Bug 1431490 - Scheme step to copy a test db into the Client bundle (#3648)
    Include a test case to show how it is used. The test case is a db with 1
    bookmark.

---
+++ XCUITests/DatabaseFixtureTest.swift
@@ -0,0 +1,20 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import XCTest
+
+class DatabaseFixtureTest: BaseTestCase {
+    func testDatabaseFixture() {
+        let file = "testDatabaseFixture-browser.db"
+        let arg = LaunchArguments.LoadDatabasePrefix + file
+        app.terminate()
+        restart(app, args: [LaunchArguments.SkipIntro, LaunchArguments.SkipWhatsNew, arg])
+
+        // app will now start with prepopulated database
+
+        navigator.browserPerformAction(.openBookMarksOption)
+        let list = app.tables["Bookmarks List"].cells.count
+        XCTAssertEqual(list, 1, "There should be an entry in the bookmarks list")
+    }
+}
