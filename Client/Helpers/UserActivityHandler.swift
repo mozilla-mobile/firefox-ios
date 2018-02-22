c60a1a6b76ec6f57ceabef00fbe7d382c78bdbc1
Justin D'Arcangelo <justindarc@gmail.com>
2018-02-22 14:42:21 -0500

    Updated to use URL from metadata parser when available.

--- Client/Helpers/UserActivityHandler.swift
+++ Client/Helpers/UserActivityHandler.swift
@@ -21,7 +21,7 @@ class UserActivityHandler {
                                 .didLoseFocus,
                                 .didGainFocus,
                                 .didChangeURL,
-                                // .didLoadMetadata // TODO: Bug 1390294
+                                .didLoadPageMetadata,
                                 // .didLoadFavicon, // TODO: Bug 1390294
                                 .didClose,
                                 queue: .main)
@@ -33,6 +33,22 @@ class UserActivityHandler {
 
     class func clearSearchIndex(completionHandler: ((Error?) -> Void)? = nil) {
         searchableIndex.deleteAllSearchableItems(completionHandler: completionHandler)
+    }
+
+    fileprivate func setUserActivityForTab(_ tab: Tab, url: URL) {
+        guard !tab.isPrivate, url.isWebPage(includeDataURIs: false), !url.isLocal else {
+            tab.userActivity?.resignCurrent()
+            tab.userActivity = nil
+            return
+        }
+
+        tab.userActivity?.invalidate()
+
+        let userActivity = NSUserActivity(activityType: browsingActivityType)
+        userActivity.webpageURL = url
+        userActivity.becomeCurrent()
+
+        tab.userActivity = userActivity
     }
 }
 
@@ -46,19 +62,15 @@ extension UserActivityHandler: TabEventHandler {
     }
 
     func tab(_ tab: Tab, didChangeURL url: URL) {
-        guard url.isWebPage(includeDataURIs: false), !url.isLocal, !tab.isPrivate else {
-            tab.userActivity?.resignCurrent()
-            tab.userActivity = nil
+        setUserActivityForTab(tab, url: url)
+    }
+
+    func tab(_ tab: Tab, didLoadPageMetadata metadata: PageMetadata) {
+        guard let url = URL(string: metadata.siteURL) else {
             return
         }
 
-        tab.userActivity?.invalidate()
-
-        let userActivity = NSUserActivity(activityType: browsingActivityType)
-        userActivity.webpageURL = url
-        userActivity.becomeCurrent()
-
-        tab.userActivity = userActivity
+        setUserActivityForTab(tab, url: url)
     }
 
     func tabDidClose(_ tab: Tab) {
