bdb5a169e822b25f853dc8e07263d88d77bdbab9
Justin D'Arcangelo <justindarc@gmail.com>
2018-02-28 14:18:06 -0500

    Bug 1441958 - mobile.twitter.com hangs on iOS 10.3

--- Client/Frontend/UserContent/UserScripts/MainFrame/AtDocumentStart/TrackingProtectionStats.js
+++ Client/Frontend/UserContent/UserScripts/MainFrame/AtDocumentStart/TrackingProtectionStats.js
@@ -3,134 +3,133 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 (function() {
-  "use strict";
+"use strict";
 
-  Object.defineProperty(window.__firefox__, "TrackingProtectionStats", {
-    enumerable: false,
-    configurable: false,
-    writable: false,
-    value: { enabled: false }
-  });
+if (!webkit.messageHandlers.trackingProtectionStats) {
+  return;
+}
 
-  Object.defineProperty(window.__firefox__.TrackingProtectionStats, "setEnabled", {
-    enumerable: false,
-    configurable: false,
-    writable: false,
-    value: function(enabled, securityToken) {
-      if (securityToken !== SECURITY_TOKEN) {
-        return;
-      }
-    
-      if (enabled === window.__firefox__.TrackingProtectionStats.enabled) {
-        return;
-      }
+Object.defineProperty(window.__firefox__, "TrackingProtectionStats", {
+  enumerable: false,
+  configurable: false,
+  writable: false,
+  value: { enabled: false }
+});
 
-      window.__firefox__.TrackingProtectionStats.enabled = enabled;
-
-      injectStatsTracking(enabled);
-    }
-  })
-
-  const messageHandler = window.webkit.messageHandlers.trackingProtectionStats;
-
-  function onLoadNativeCallback() {
-    var sendMessage = function(url) { messageHandler.postMessage({ url: url }) };
-
-    // Send back the sources of every script and image in the dom back to the host applicaiton
-    Array.prototype.map.call(document.scripts, function(t) { return t.src }).forEach(sendMessage);
-    Array.prototype.map.call(document.images, function(t) { return t.src }).forEach(sendMessage);
-  }
-
-  let originalOpen = null;
-  let originalSend = null;
-  let originalImageSrc = null;  
-  let mutationObserver = null;
-
-  function injectStatsTracking(enabled) {
-    // This enable/disable section is a change from the original Focus iOS  version
-    if (enabled) {
-      if (originalOpen != null) {
-        return;
-      }
-      window.addEventListener("load", onLoadNativeCallback, false);
-    } else {
-      window.removeEventListener("load", onLoadNativeCallback, false);
-
-      if (originalOpen) { // if one is set, then all the enable code has run
-        XMLHttpRequest.prototype.open = originalOpen;
-        XMLHttpRequest.prototype.send = originalSend;
-        Image.prototype.src = originalImageSrc;
-        mutationObserver.disconnect();
-
-        originalOpen = originalSend = originalImageSrc = mutationObserver = null;
-      }
+Object.defineProperty(window.__firefox__.TrackingProtectionStats, "setEnabled", {
+  enumerable: false,
+  configurable: false,
+  writable: false,
+  value: function(enabled, securityToken) {
+    if (securityToken !== SECURITY_TOKEN) {
       return;
     }
 
-    // -------------------------------------------------
-    // Send ajax requests URLs to the host application
-    // -------------------------------------------------
-    var xhrProto = XMLHttpRequest.prototype;
-    if (!originalOpen) {
-      originalOpen = xhrProto.open;
-      originalSend = xhrProto.send;
+    if (enabled === window.__firefox__.TrackingProtectionStats.enabled) {
+      return;
     }
 
-    xhrProto.open = function(method, url) {
-      this._url = url;
-      return originalOpen.apply(this, arguments);
-    };
+    window.__firefox__.TrackingProtectionStats.enabled = enabled;
 
-    xhrProto.send = function(body) {
-      messageHandler.postMessage({
-        url: this._url
-      })
-      return originalSend.apply(this, arguments)
-    };
+    injectStatsTracking(enabled);
+  }
+})
 
-    // -------------------------------------------------
-    // Detect when new sources get set on Image and send them to the host application
-    // -------------------------------------------------
-    if (!originalImageSrc) {
-      originalImageSrc = Object.getOwnPropertyDescriptor(Image.prototype, 'src');
+function sendMessage(url) {
+  webkit.messageHandlers.trackingProtectionStats.postMessage({ url: url });
+}
+
+function onLoadNativeCallback() {
+  // Send back the sources of every script and image in the DOM back to the host application.
+  [].slice.apply(document.scripts).forEach(function(el) { sendMessage(el.src); });
+  [].slice.apply(document.images).forEach(function(el) { sendMessage(el.src); });
+}
+
+let originalOpen = null;
+let originalSend = null;
+let originalImageSrc = null;
+let mutationObserver = null;
+
+function injectStatsTracking(enabled) {
+  // This enable/disable section is a change from the original Focus iOS version.
+  if (enabled) {
+    if (originalOpen) {
+      return;
     }
-    delete Image.prototype.src;
-    Object.defineProperty(Image.prototype, 'src', {
-      get: function() {
-        return originalImageSrc.get.call(this);
-      },
-      set: function(value) {
-        messageHandler.postMessage({
-          url: value
-        })
-        originalImageSrc.set.call(this, value);
-      }
-    });
+    window.addEventListener("load", onLoadNativeCallback, false);
+  } else {
+    window.removeEventListener("load", onLoadNativeCallback, false);
 
-    // -------------------------------------------------
-    // Listen to when new <script> elements get added to the dom
-    // and send the source to the host application
-    // -------------------------------------------------
-    mutationObserver = new MutationObserver(function(mutations) {
-      mutations.forEach(function(mutation) {
-        mutation.addedNodes.forEach(function(node) {
-          if (node.tagName === 'SCRIPT') {
-            messageHandler.postMessage({
-              url: node.src
-            })
-          }
-        });
-      });
-    });
+    if (originalOpen) { // if one is set, then all the enable code has run
+      XMLHttpRequest.prototype.open = originalOpen;
+      XMLHttpRequest.prototype.send = originalSend;
+      Image.prototype.src = originalImageSrc;
+      mutationObserver.disconnect();
 
-    mutationObserver.observe(document.documentElement, {
-      childList: true,
-      subtree: true
-    });
+      originalOpen = originalSend = originalImageSrc = mutationObserver = null;
+    }
+    return;
   }
 
-  // Default to on because there is a delay in being able to enable/disable
-  // from native, and we don't want to miss events
-  window.__firefox__.TrackingProtectionStats.enabled = true;
-  injectStatsTracking(true);
+  // -------------------------------------------------
+  // Send ajax requests URLs to the host application
+  // -------------------------------------------------
+  var xhrProto = XMLHttpRequest.prototype;
+  if (!originalOpen) {
+    originalOpen = xhrProto.open;
+    originalSend = xhrProto.send;
+  }
+
+  xhrProto.open = function(method, url) {
+    this._url = url;
+    return originalOpen.apply(this, arguments);
+  };
+
+  xhrProto.send = function(body) {
+    sendMessage(this._url);
+    return originalSend.apply(this, arguments);
+  };
+
+  // -------------------------------------------------
+  // Detect when new sources get set on Image and send them to the host application
+  // -------------------------------------------------
+  if (!originalImageSrc) {
+    originalImageSrc = Object.getOwnPropertyDescriptor(Image.prototype, "src");
+  }
+  delete Image.prototype.src;
+  Object.defineProperty(Image.prototype, "src", {
+    get: function() {
+      return originalImageSrc.get.call(this);
+    },
+    set: function(value) {
+      sendMessage(value);
+      originalImageSrc.set.call(this, value);
+    }
+  });
+
+  // -------------------------------------------------
+  // Listen to when new <script> elements get added to the DOM
+  // and send the source to the host application
+  // -------------------------------------------------
+  mutationObserver = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+      mutation.addedNodes.forEach(function(node) {
+        if (node.tagName === "SCRIPT") {
+          sendMessage(node.src);
+        }
+      });
+    });
+  });
+
+  mutationObserver.observe(document.documentElement, {
+    childList: true,
+    subtree: true
+  });
+}
+
+// Default to on because there is a delay in being able to enable/disable
+// from native, and we don't want to miss events
+window.__firefox__.TrackingProtectionStats.enabled = true;
+injectStatsTracking(true);
+
 })();
