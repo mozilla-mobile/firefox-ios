66947b6fe8f6a57a90724696f05de14795b5a8db
isabelrios <isabelrios@gmail.com>
2018-03-16 19:06:46 +0100

    Bug 1442374 - XCUITest that loads around 500 sites to show on History List (#3754)
    updating to use a bigger DB

--- XCUITests/DatabaseFixtureTest.swift
+++ XCUITests/DatabaseFixtureTest.swift
@@ -12,9 +12,21 @@ class DatabaseFixtureTest: BaseTestCase {
         restart(app, args: [LaunchArguments.SkipIntro, LaunchArguments.SkipWhatsNew, arg])
 
         // app will now start with prepopulated database
-
         navigator.browserPerformAction(.openBookMarksOption)
         let list = app.tables["Bookmarks List"].cells.count
         XCTAssertEqual(list, 1, "There should be an entry in the bookmarks list")
+    }
+
+    func testHistoryDatabaseFixture() {
+        let file = "testHistoryDatabase1500-browser.db"
+        let arg = LaunchArguments.LoadDatabasePrefix + file
+        app.terminate()
+        restart(app, args: [LaunchArguments.SkipIntro, LaunchArguments.SkipWhatsNew, arg])
+
+        // app will now start with prepopulated database
+        navigator.goto(HomePanel_History)
+        //History list has two cells that are for recently closed and synced devices that should not count as history items
+        let historyList = app.tables["History List"].cells.count-2
+        XCTAssertEqual(historyList, 100, "There should be an entry in the bookmarks list")
     }
 }
