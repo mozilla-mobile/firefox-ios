1774e76fd37ff504931e78ba0dbfa2945bec56a4
Michael Kaply <consulting@kaply.com>
2018-03-27 15:26:32 -0500

    Bug 1442738 - Update search config (#3789)

--- ClientTests/DefaultSearchPrefsTests.swift
+++ ClientTests/DefaultSearchPrefsTests.swift
@@ -15,16 +15,16 @@ class DefaultSearchPrefsTests: XCTestCase {
         let searchPrefs = DefaultSearchPrefs(with: Bundle.main.resourceURL!.appendingPathComponent("SearchPlugins").appendingPathComponent("list.json"))!
 
         // setup the most popular locales
-        let us = (lang: ["en-US", "en"], region: "US", resultList: ["google-2018", "yahoo", "bing", "amazondotcom", "ddg", "twitter", "wikipedia"], resultDefault: "Google")
-        let england = (lang: ["en-GB", "en"], region: "GB", resultList: ["google", "yahoo-en-GB", "bing", "amazon-co-uk", "ddg", "qwant", "twitter", "wikipedia"], resultDefault: "Google")
-        let france = (lang: ["fr-FR", "fr"], region: "FR", resultList: ["google", "yahoo-france", "bing", "ddg", "qwant", "twitter", "wikipedia-fr"], resultDefault: "Google")
+        let us = (lang: ["en-US", "en"], region: "US", resultList: ["google-2018", "bing", "amazondotcom", "ddg", "twitter", "wikipedia"], resultDefault: "Google")
+        let england = (lang: ["en-GB", "en"], region: "GB", resultList: ["google", "bing", "amazon-co-uk", "ddg", "qwant", "twitter", "wikipedia"], resultDefault: "Google")
+        let france = (lang: ["fr-FR", "fr"], region: "FR", resultList: ["google", "bing", "ddg", "qwant", "twitter", "wikipedia-fr"], resultDefault: "Google")
         let japan = (lang: ["ja-JP", "ja"], region: "JP", resultList: ["google", "amazon-jp", "bing", "twitter-ja", "wikipedia-ja", "yahoo-jp"], resultDefault: "Google")
-        let canada = (lang: ["en-CA", "en"], region: "CA", resultList: ["google", "yahoo", "bing", "amazondotcom", "ddg", "twitter", "wikipedia"], resultDefault: "Google")
+        let canada = (lang: ["en-CA", "en"], region: "CA", resultList: ["google", "bing", "amazondotcom", "ddg", "twitter", "wikipedia"], resultDefault: "Google")
         let russia = (lang: ["ru-RU", "ru"], region: "RU", resultList: ["google-nocodes", "yandex-ru", "twitter", "wikipedia-ru"], resultDefault: "Яндекс")
         let taiwan = (lang: ["zh-TW", "zh"], region: "TW", resultList: ["google", "bing", "ddg", "wikipedia-zh-TW"], resultDefault: "Google")
         let china = (lang: ["zh-hans-CN", "zh-CN", "zh"], region: "CN", resultList: ["google-nocodes", "baidu", "bing", "taobao", "wikipedia-zh-CN"], resultDefault: "百度")
-        let germany = (lang: ["de-DE", "de"], region: "DE", resultList: ["google", "yahoo-de", "bing", "amazon-de", "ddg", "qwant", "twitter", "wikipedia-de"], resultDefault: "Google")
-        let southAfrica = (lang: ["en-SA", "en"], region: "SA", resultList: ["google", "yahoo", "bing", "amazondotcom", "ddg", "twitter", "wikipedia"], resultDefault: "Google")
+        let germany = (lang: ["de-DE", "de"], region: "DE", resultList: ["google", "bing", "amazon-de", "ddg", "qwant", "twitter", "wikipedia-de"], resultDefault: "Google")
+        let southAfrica = (lang: ["en-SA", "en"], region: "SA", resultList: ["google", "bing", "amazondotcom", "ddg", "twitter", "wikipedia"], resultDefault: "Google")
         let testLocales = [us, england, france, japan, canada, russia, taiwan, china, germany, southAfrica]
 
         // run tests
