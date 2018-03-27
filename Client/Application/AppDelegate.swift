2c2f8785dc40c3f83dc025258c7fccb00c132779
Garvan Keeley <garvankeeley@gmail.com>
2018-03-27 13:56:27 -0400

    Bug 1449263 - Find in page test for large page + new XCUITests/WebPagesForTesting

--- Client/Application/AppDelegate.swift
+++ Client/Application/AppDelegate.swift
@@ -371,6 +371,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UIViewControllerRestorati
         AboutLicenseHandler.register(server)
         SessionRestoreHandler.register(server)
 
+        if AppConstants.IsRunningTest {
+            registerHandlersForTestMethods(server: server.server)
+        }
+
         // Bug 1223009 was an issue whereby CGDWebserver crashed when moving to a background task
         // catching and handling the error seemed to fix things, but we're not sure why.
         // Either way, not implicitly unwrapping a try is not a great way of doing things
