58b0e253bfa6d04872d8b577989470f01ca70d3f
isabelrios <isabelrios@gmail.com>
2018-04-02 17:31:46 +0200

    Bug 1449154 - XCUITests Fix timing issues (#3800)

--- XCUITests/DomainAutocompleteTest.swift
+++ XCUITests/DomainAutocompleteTest.swift
@@ -160,7 +160,7 @@ class DomainAutocompleteTest: BaseTestCase {
     }
 
     func testDeletingCharsUpdateTheResults() {
-        let url1 = ["url" : "git.com", "label" : "GIT.com"]
+        let url1 = ["url" : "git.es", "label" : "git.es - Dominio premium en venta"]
         let url2 = ["url" : "github.com", "label" : "The world's leading software development platform · GitHub"]
 
         navigator.openURL(url1["url"]!)
