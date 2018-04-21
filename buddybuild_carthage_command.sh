ccb2781c2c46dcd154323b918cd79efe49846167
Garvan Keeley <garvankeeley@gmail.com>
2018-04-20 23:47:03 -0400

    add brew install rome step

--- buddybuild_carthage_command.sh
+++ buddybuild_carthage_command.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-# Extract rome binary
-tar xzf rome.tgz -C ~
+brew install blender/homebrew-tap/rome
 
 mkdir -p ~/.aws
 echo "[default]
 region = us-west-2" > ~/.aws/config
 
 # download missing frameworks
-~/rome download --platform iOS
+rome download --platform iOS
 
 # list what is missing and update/build if needed
-~/rome list --missing --platform ios | awk '{print $1}' | xargs carthage update --platform ios
+rome list --missing --platform ios | awk '{print $1}' | xargs carthage update --platform ios
 
 # upload what is missing
-~/rome list --missing --platform ios | awk '{print $1}' | xargs ~/rome upload --platform ios
+rome list --missing --platform ios | awk '{print $1}' | xargs rome upload --platform ios
 
