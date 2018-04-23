806e30e7a185ea821699caf4cfc54eda95cc3069
Garvan Keeley <garvankeeley@gmail.com>
2018-04-23 11:05:28 -0400

    [nobug,build] add logging to Rome step during build

--- buddybuild_carthage_command.sh
+++ buddybuild_carthage_command.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
+echo "[Rome script] installing rome "
 brew install blender/homebrew-tap/rome
 
 mkdir -p ~/.aws
 echo "[default]
 region = us-west-2" > ~/.aws/config
 
-# download missing frameworks
+echo "[Rome script] download missing frameworks"
 rome download --platform iOS
 
-# list what is missing and update/build if needed
+echo "[Rome script] list what is missing and update/build if needed"
 rome list --missing --platform ios | awk '{print $1}' | xargs carthage update --platform ios
 
-# upload what is missing
+echo "[Rome script] upload what is missing"
 rome list --missing --platform ios | awk '{print $1}' | xargs rome upload --platform ios
 
-# Fuzi doesn't work with Rome, so it is ignored and built
-carthage update --platform ios Fuzi 
+echo "[Rome script] build Fuzi, it is exempt from Rome"
+carthage update --platform ios Fuzi
