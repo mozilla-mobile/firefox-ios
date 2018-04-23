7a93c6f98039220f086733f69933515c8534d8b9
Garvan Keeley <garvankeeley+github@gmail.com>
2018-04-23 10:31:23 -0400

    Bug 1454371: [build] add Rome (Carthage) to BuddyBuild (#3841)
    * Bug 1454371: [build] add Rome (Carthage) to BuddyBuild
    * add brew install rome step
    * Fuzi doesn't work with Rome, ignore it

--- buddybuild_carthage_command.sh
+++ buddybuild_carthage_command.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-# This is needed for Leanplum. Our bootstrap.sh is not run on
-# BuddyBuild and the carthage command that BuddyBuild runs is
-# not compatible with Leanplum for some reason. (It seems to
-# have a problem with the --toolchain argument).
+brew install blender/homebrew-tap/rome
 
-carthage bootstrap --platform ios
+mkdir -p ~/.aws
+echo "[default]
+region = us-west-2" > ~/.aws/config
 
+# download missing frameworks
+rome download --platform iOS
+
+# list what is missing and update/build if needed
+rome list --missing --platform ios | awk '{print $1}' | xargs carthage update --platform ios
+
+# upload what is missing
+rome list --missing --platform ios | awk '{print $1}' | xargs rome upload --platform ios
+
+# Fuzi doesn't work with Rome, so it is ignored and built
+carthage update --platform ios Fuzi 
