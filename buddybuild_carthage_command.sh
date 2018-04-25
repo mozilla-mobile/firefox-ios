e49df5001b26cf2d8375392cddee8d663406ee37
Garvan Keeley <garvankeeley@gmail.com>
2018-04-25 09:51:31 -0400

    Bug 1456801 - BuddyBuild env variable to control using Rome Default to off

--- buddybuild_carthage_command.sh
+++ buddybuild_carthage_command.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-echo "[Rome script] installing rome "
-brew install blender/homebrew-tap/rome
+if [ "$USE_ROME_CARTHAGE" = "YES" ]; then
+    echo "[Rome script] installing rome "
+    brew install blender/homebrew-tap/rome
 
-mkdir -p ~/.aws
-echo "[default]
-region = us-west-2" > ~/.aws/config
+    mkdir -p ~/.aws
+    echo "[default]
+    region = us-west-2" > ~/.aws/config
 
-echo "[Rome script] download missing frameworks"
-rome download --platform iOS
+    echo "[Rome script] download missing frameworks"
+    rome download --platform iOS
 
-echo "[Rome script] list what is missing and update/build if needed"
-rome list --missing --platform ios | awk '{print $1}' | xargs carthage update --platform ios
+    echo "[Rome script] list what is missing and update/build if needed"
+    rome list --missing --platform ios | awk '{print $1}' | xargs carthage update --platform ios
 
-echo "[Rome script] upload what is missing"
-rome list --missing --platform ios | awk '{print $1}' | xargs rome upload --platform ios
-
-echo "[Rome script] build Fuzi, it is exempt from Rome"
-carthage update --platform ios Fuzi
+    echo "[Rome script] upload what is missing"
+    rome list --missing --platform ios | awk '{print $1}' | xargs rome upload --platform ios
+else
+ carthage bootstrap --platform ios
+fi
