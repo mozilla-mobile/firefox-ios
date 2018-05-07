ec1f7522c47188204cbaf4172e3b42ec0a7c7340
isabelrios <isabelrios@gmail.com>
2018-05-07 15:58:45 +0200

    Bug 1459196 - UITests FollowUp bug to fix the update to earlgrey and kif (#3897)

--- UITests/BrowserTests.swift
+++ UITests/BrowserTests.swift
@@ -26,29 +26,29 @@ class BrowserTests: KIFTestCase {
     // Disabled with https://bugzilla.mozilla.org/show_bug.cgi?id=1409851
     func testDisplaySharesheetWhileJSPromptOccurs() {
         let url = "\(webRoot!)/JSPrompt.html"
-        EarlGrey.select(elementWithMatcher: grey_accessibilityID("url")).perform(grey_tap())
-        EarlGrey.select(elementWithMatcher: grey_accessibilityID("address")).perform(grey_replaceText(url))
-        EarlGrey.select(elementWithMatcher: grey_accessibilityID("address")).perform(grey_typeText("\n"))
+        EarlGrey.selectElement(with: grey_accessibilityID("url")).perform(grey_tap())
+        EarlGrey.selectElement(with: grey_accessibilityID("address")).perform(grey_replaceText(url))
+        EarlGrey.selectElement(with: grey_accessibilityID("address")).perform(grey_typeText("\n"))
         tester().waitForWebViewElementWithAccessibilityLabel("JS Prompt")
         //EarlGrey.select(elementWithMatcher: grey_kindOfClass(NSClassFromString("_UIAlertControllerInterfaceActionGroupView")!))
         //    .assert(grey_sufficientlyVisible())
         
         // Show share sheet and wait for the JS prompt to fire
-        EarlGrey.select(elementWithMatcher: grey_accessibilityLabel("Share")).perform(grey_tap())
+        EarlGrey.selectElement(with: grey_accessibilityLabel("Share")).perform(grey_tap())
         
         if BrowserUtils.iPad() {
             // iPad does not have cancel btn to close dialog
-            EarlGrey.select(elementWithMatcher: grey_accessibilityLabel("Share")).perform(grey_tap())
+            EarlGrey.selectElement(with: grey_accessibilityLabel("Share")).perform(grey_tap())
         } else {
             let matcher = grey_allOf([grey_accessibilityLabel("Cancel"),
                                       grey_accessibilityTrait(UIAccessibilityTraitButton),
                                       grey_sufficientlyVisible()])
-            EarlGrey.select(elementWithMatcher: matcher).perform(grey_tap())
+            EarlGrey.selectElement(with: matcher).perform(grey_tap())
         }
         
         // Check to see if the JS Prompt is dequeued and showing
         tester().waitForView(withAccessibilityLabel: "OK")
-        EarlGrey.select(elementWithMatcher: grey_accessibilityLabel("OK"))
+        EarlGrey.selectElement(with: grey_accessibilityLabel("OK"))
             .inRoot(grey_kindOfClass(NSClassFromString("_UIAlertControllerActionView")!))
             .assert(grey_enabled())
             .perform((grey_tap()))
