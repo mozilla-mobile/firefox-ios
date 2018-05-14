f634d4f574c0ee39c47ce68dc06317aa391872e8
Garvan Keeley <garvankeeley@gmail.com>
2018-05-14 10:26:09 -0400

    Bug 1461316 - Subclass UIAlertController to enable accessibility ids to be set

---
+++ Shared/AlertController.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+public class AlertController: UIAlertController {
+    private var accessibilityIdentifiers = [UIAlertAction: String]()
+
+    public func addAction(_ action: UIAlertAction, accessibilityIdentifier: String) {
+        super.addAction(action)
+        accessibilityIdentifiers[action] = accessibilityIdentifier
+    }
+
+    override public func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        // From https://stackoverflow.com/questions/38117410/how-can-i-set-accessibilityidentifier-to-uialertcontroller
+        for action in actions {
+            let item = action.value(forKey: "__representer") as? UIView
+            item?.accessibilityIdentifier = accessibilityIdentifiers[action]
+        }
+    }
+}
