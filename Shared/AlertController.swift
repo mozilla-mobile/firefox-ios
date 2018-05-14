f7e5cc04f7615ccbbf84acc8561c2dede777ef93
Garvan Keeley <garvankeeley@gmail.com>
2018-05-14 11:57:27 -0400

    Missed license comment block

--- Shared/AlertController.swift
+++ Shared/AlertController.swift
@@ -1,5 +1,10 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 import UIKit
 
+// Subclassed to support accessibility identifiers
 public class AlertController: UIAlertController {
     private var accessibilityIdentifiers = [UIAlertAction: String]()
 
