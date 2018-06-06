ac4d40e8dd31765bb6215b7a9a474ea5e0ce72e3
Justin D'Arcangelo <justindarc@gmail.com>
2018-06-06 16:45:44 -0400

    Bug 1467226 - Add 'Save logs to Files app on next launch' item to Settings app (#3973)
    * Bug 1467226 - Add 'Save logs to Files app on next launch' item to Settings app
    
    * Addressed review comments.

--- Client/Application/DebugSettingsBundleOptions.swift
+++ Client/Application/DebugSettingsBundleOptions.swift
@@ -3,33 +3,16 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import Foundation
-import MessageUI
 
 struct DebugSettingsBundleOptions {
+
+    /// Save logs to `~/Documents` folder
+    static var saveLogsToDocuments: Bool {
+        return UserDefaults.standard.bool(forKey: "SettingsBundleSaveLogsToDocuments")
+    }
 
     /// Don't restore tabs on app launch
     static var skipSessionRestore: Bool {
         return UserDefaults.standard.bool(forKey: "SettingsBundleSkipSessionRestore")
-    }
-
-    /// Disable the local web server we use for restoration, error pages, etc
-    static var disableLocalWebServer: Bool {
-        return UserDefaults.standard.bool(forKey: "SettingsBundleDisableLocalWebServer")
-    }
-
-    /// When enabled, the app launch will be replaced with the mail compose view appearing with the device
-    /// logs pre-attached. When the mail is sent, the app continues launching normally.
-    static var launchIntoEmailComposer: Bool {
-        return ((attachTabStateToDebugEmail || attachLogsToDebugEmail) &&  MFMailComposeViewController.canSendMail())
-    }
-
-    /// When enabled, the email composer will have the tab state attached.
-    static var attachTabStateToDebugEmail: Bool {
-        return UserDefaults.standard.bool(forKey: "SettingsBundleEmailTabState")
-    }
-
-    /// When enabled, the email composer will have the application logs attached.
-    static var attachLogsToDebugEmail: Bool {
-        return UserDefaults.standard.bool(forKey: "SettingsBundleEmailLogsOnLaunch")
     }
 }
--- Shared/Logger.swift
+++ Shared/Logger.swift
@@ -12,38 +12,62 @@ public extension Logger {
     static let logPII = false
 
     /// Logger used for recording happenings with Sync, Accounts, Providers, Storage, and Profiles
-    static let syncLogger = RollingFileLogger(filenameRoot: "sync", logDirectoryPath: Logger.logFileDirectoryPath())
+    static let syncLogger = RollingFileLogger(filenameRoot: "sync", logDirectoryPath: Logger.logFileDirectoryPath(inDocuments: saveLogsToDocuments))
 
     /// Logger used for recording frontend/browser happenings
-    static let browserLogger = RollingFileLogger(filenameRoot: "browser", logDirectoryPath: Logger.logFileDirectoryPath())
+    static let browserLogger = RollingFileLogger(filenameRoot: "browser", logDirectoryPath: Logger.logFileDirectoryPath(inDocuments: saveLogsToDocuments))
 
     /// Logger used for recording interactions with the keychain
     static let keychainLogger: XCGLogger = Logger.fileLoggerWithName("keychain")
 
     /// Logger used for logging database errors such as corruption
     static let corruptLogger: RollingFileLogger = {
-        let logger = RollingFileLogger(filenameRoot: "corruptLogger", logDirectoryPath: Logger.logFileDirectoryPath())
+        let logger = RollingFileLogger(filenameRoot: "corruptLogger", logDirectoryPath: Logger.logFileDirectoryPath(inDocuments: saveLogsToDocuments))
         logger.newLogWithDate(Date())
         return logger
     }()
+
+    /// Save logs to `~/Documents` folder. If this is `true`, the flag is reset in `UserDefaults` so it does not persist to the next launch.
+    static let saveLogsToDocuments: Bool = {
+        let value = UserDefaults.standard.bool(forKey: "SettingsBundleSaveLogsToDocuments")
+        if value {
+            UserDefaults.standard.set(false, forKey: "SettingsBundleSaveLogsToDocuments")
+        }
+        return value
+    }()
+
+    static func copyPreviousLogsToDocuments() {
+        if let defaultLogDirectoryPath = logFileDirectoryPath(inDocuments: false),
+            let documentsLogDirectoryPath = logFileDirectoryPath(inDocuments: true),
+            let previousLogFiles = try? FileManager.default.contentsOfDirectory(atPath: defaultLogDirectoryPath) {
+            let defaultLogDirectoryURL = URL(fileURLWithPath: defaultLogDirectoryPath, isDirectory: true)
+            let documentsLogDirectoryURL = URL(fileURLWithPath: documentsLogDirectoryPath, isDirectory: true)
+            for previousLogFile in previousLogFiles {
+                let previousLogFileURL = defaultLogDirectoryURL.appendingPathComponent(previousLogFile)
+                let targetLogFileURL = documentsLogDirectoryURL.appendingPathComponent(previousLogFile)
+                try? FileManager.default.copyItem(at: previousLogFileURL, to: targetLogFileURL)
+            }
+        }
+    }
 
     /**
     Return the log file directory path. If the directory doesn't exist, make sure it exist first before returning the path.
 
     :returns: Directory path where log files are stored
     */
-    static func logFileDirectoryPath() -> String? {
-        if let cacheDir = NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true).first {
-            let logDir = "\(cacheDir)/Logs"
-            if !FileManager.default.fileExists(atPath: logDir) {
+    static func logFileDirectoryPath(inDocuments: Bool) -> String? {
+        let searchPathDirectory: FileManager.SearchPathDirectory = inDocuments ? .documentDirectory : .cachesDirectory
+        if let targetDirectory = NSSearchPathForDirectoriesInDomains(searchPathDirectory, .userDomainMask, true).first {
+            let logsDirectory = "\(targetDirectory)/Logs"
+            if !FileManager.default.fileExists(atPath: logsDirectory) {
                 do {
-                    try FileManager.default.createDirectory(atPath: logDir, withIntermediateDirectories: false, attributes: nil)
-                    return logDir
+                    try FileManager.default.createDirectory(atPath: logsDirectory, withIntermediateDirectories: true, attributes: nil)
+                    return logsDirectory
                 } catch _ as NSError {
                     return nil
                 }
             } else {
-                return logDir
+                return logsDirectory
             }
         }
 
@@ -64,33 +88,11 @@ public extension Logger {
     }
 
     static private func urlForLogNamed(_ name: String) -> URL? {
-        guard let logDir = Logger.logFileDirectoryPath() else {
+        guard let logDir = Logger.logFileDirectoryPath(inDocuments: saveLogsToDocuments) else {
             return nil
         }
 
         return URL(string: "\(logDir)/\(name).log")
-    }
-
-    /**
-     Grabs all of the configured logs that write to disk and returns them in NSData format along with their
-     associated filename.
-
-     - returns: Tuples of filenames to each file's contexts in a NSData object
-     */
-    static func diskLogFilenamesAndData() throws -> [(String, Data?)] {
-        var filenamesAndURLs = [(String, URL)]()
-        filenamesAndURLs.append(("browser", urlForLogNamed("browser")!))
-        filenamesAndURLs.append(("keychain", urlForLogNamed("keychain")!))
-
-        // Grab all sync log files
-        do {
-            filenamesAndURLs += try syncLogger.logFilenamesAndURLs()
-            filenamesAndURLs += try corruptLogger.logFilenamesAndURLs()
-            filenamesAndURLs += try browserLogger.logFilenamesAndURLs()
-        } catch _ {
-        }
-
-        return filenamesAndURLs.map { ($0, try? Data(contentsOf: URL(fileURLWithPath: $1.absoluteString))) }
     }
 }
 
