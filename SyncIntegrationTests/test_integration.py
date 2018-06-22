c769e4bd71ff15b27dc4cf6dbb90cf0e960d3989
isabelrios <isabelrios@gmail.com>
2018-06-22 10:54:10 +0200

    Bug 1463105 - New Integration test History entry (#3967)
    History integration test using db

--- SyncIntegrationTests/test_integration.py
+++ SyncIntegrationTests/test_integration.py
@@ -1,3 +1,7 @@
 def test_sync_bookmark_from_device(tps, xcodebuild):
     xcodebuild.test('XCUITests/IntegrationTests/testFxASyncBookmark')
     tps.run('test_bookmark.js')
+
+def test_sync_history_from_device(tps, xcodebuild):
+    xcodebuild.test('XCUITests/IntegrationTests/testFxASyncHistory')
+    tps.run('test_history.js')
