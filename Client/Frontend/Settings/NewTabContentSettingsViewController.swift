64bf6fa16253521d4a20b037f3b012e74316f5fe
Meera Rachamallu <mrachamallu@mrachamallu-30htdf.local>
2018-06-25 10:09:57 -0700

    improveNewTabSettings - homepage only enables if one is set

--- Client/Frontend/Settings/NewTabContentSettingsViewController.swift
+++ Client/Frontend/Settings/NewTabContentSettingsViewController.swift
@@ -49,36 +49,29 @@ class NewTabContentSettingsViewController: SettingsTableViewController {
         let showTopSites = CheckmarkSetting(title: NSAttributedString(string: Strings.SettingsNewTabTopSites), subtitle: nil, accessibilityIdentifier: "hi", isEnabled: {return self.currentChoice == NewTabPage.topSites}, onChanged: {
             self.currentChoice = NewTabPage.topSites
             self.prefs.setString(self.currentChoice.rawValue, forKey: NewTabAccessors.PrefKey)
-            self.hasHomePage = HomePageAccessors.getHomePage(self.prefs) != nil
-            print(NewTabAccessors.getNewTabPage(self.prefs))
             self.tableView.reloadData()
         })
         let showBlankPage = CheckmarkSetting(title: NSAttributedString(string: Strings.SettingsNewTabBlankPage), subtitle: nil, accessibilityIdentifier: "hi", isEnabled: {return self.currentChoice == NewTabPage.blankPage}, onChanged: {
             self.currentChoice = NewTabPage.blankPage
             self.prefs.setString(self.currentChoice.rawValue, forKey: NewTabAccessors.PrefKey)
-            self.hasHomePage = HomePageAccessors.getHomePage(self.prefs) != nil
-            print(NewTabAccessors.getNewTabPage(self.prefs))
             self.tableView.reloadData()
         })
         let showBookmarks = CheckmarkSetting(title: NSAttributedString(string: Strings.SettingsNewTabBookmarks), subtitle: nil, accessibilityIdentifier: "hi", isEnabled: {return self.currentChoice == NewTabPage.bookmarks}, onChanged: {
             self.currentChoice = NewTabPage.bookmarks
             self.prefs.setString(self.currentChoice.rawValue, forKey: NewTabAccessors.PrefKey)
-            self.hasHomePage = HomePageAccessors.getHomePage(self.prefs) != nil
-            print(NewTabAccessors.getNewTabPage(self.prefs))
             self.tableView.reloadData()
         })
         let showHistory = CheckmarkSetting(title: NSAttributedString(string: Strings.SettingsNewTabHistory), subtitle: nil, accessibilityIdentifier: "hi", isEnabled: {return self.currentChoice == NewTabPage.history}, onChanged: {
             self.currentChoice = NewTabPage.history
             self.prefs.setString(self.currentChoice.rawValue, forKey: NewTabAccessors.PrefKey)
-            self.hasHomePage = HomePageAccessors.getHomePage(self.prefs) != nil
-            print(NewTabAccessors.getNewTabPage(self.prefs))
             self.tableView.reloadData()
         })
-        let showHomepage = CheckmarkSetting(title: NSAttributedString(string: Strings.SettingsNewTabHomePage), subtitle: nil, accessibilityIdentifier: "hi", isEnabled: {return self.currentChoice == NewTabPage.homePage}, onChanged: {
-            self.currentChoice = NewTabPage.homePage
-            self.prefs.setString(self.currentChoice.rawValue, forKey: NewTabAccessors.PrefKey)
+        let showHomepage = CheckmarkSetting(title: NSAttributedString(string: Strings.SettingsNewTabHomePage), subtitle: nil, accessibilityIdentifier: "hi", isEnabled: {return (self.currentChoice == NewTabPage.homePage) && self.hasHomePage}, onChanged: {
             self.hasHomePage = HomePageAccessors.getHomePage(self.prefs) != nil
-            print(NewTabAccessors.getNewTabPage(self.prefs))
+            if (self.hasHomePage){
+                self.currentChoice = NewTabPage.homePage
+                self.prefs.setString(self.currentChoice.rawValue, forKey: NewTabAccessors.PrefKey)
+            }
             self.tableView.reloadData()
         })
         let firstSection = SettingSection(title: NSAttributedString(string: Strings.SettingsNewTabSectionName), footerTitle: nil, children: [showTopSites, showBlankPage, showBookmarks, showHistory, showHomepage])
