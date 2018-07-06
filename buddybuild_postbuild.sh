68a538ccb205110b19927815ad182cbdf8af4335
No Jun Park <npark@mozilla.com>
2018-07-06 13:50:17 -0400

    reorder script

--- buddybuild_postbuild.sh
+++ buddybuild_postbuild.sh
@@ -8,11 +8,11 @@ bash <(curl -s https://codecov.io/bash)
 
 test_runner="XCUITests-Runner"
 
-# comment out the section where it upload the testrunner
 if [ "$BUDDYBUILD_SCHEME" = "Fennec" ]; then
-#  (cd $BUDDYBUILD_TEST_DIR/Build/Products/Fennec-iphonesimulator && zip -r $test_runner.zip $test_runner.app)
   curl -F ipa=@$BUDDYBUILD_IPA_PATH  -u $NIMBLEDROID_API_KEY: https://nimbledroid.com/api/v2/ipas \
        -F test_identifiers='ActivityStreamTest/testDefaultSites,DomainAutocompleteTest/testAutocomplete,DatabaseFixtureTest/testHistoryDatabaseFixture,BrowsingPDFTests/testBookmarkPDF,ActivityStreamTest/testTopSitesBookmarkNewTopSite,FindInPageTests/testFindInLargeDoc,FirstRunTourTests/testFirstRunTour'
+# comment out the section where it upload the testrunner
+#  (cd $BUDDYBUILD_TEST_DIR/Build/Products/Fennec-iphonesimulator && zip -r $test_runner.zip $test_runner.app)
 #      -F test_runner=@$BUDDYBUILD_TEST_DIR/Build/Products/Fennec-iphonesimulator/$test_runner.zip
 fi
 
