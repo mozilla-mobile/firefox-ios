13af8871a474d3473342362ff369391b0e763ec0
Meera Rachamallu <mrachamallu@gmail.com>
2018-07-10 10:00:51 -0700

    Bug 1473695 - Improve new tab settings (#3992)

--- Client/Frontend/Settings/NewTabContentSettingsViewController.swift
+++ Client/Frontend/Settings/NewTabContentSettingsViewController.swift
@@ -27,7 +27,13 @@ class CurrentTabSetting: Setting {
 
 class NewTabContentSettingsViewController: SettingsTableViewController {
 
-    init() {
+    /* variables for checkmark settings */
+    let prefs: Prefs
+    var currentChoice: NewTabPage!
+    var hasHomePage: Bool!
+
+    init(prefs: Prefs) {
+        self.prefs = prefs
         super.init(style: .grouped)
 
         self.title = Strings.SettingsNewTabTitle
@@ -39,9 +45,42 @@ class NewTabContentSettingsViewController: SettingsTableViewController {
     }
 
     override func generateSettings() -> [SettingSection] {
-        let tabSetting = CurrentTabSetting(profile: profile)
-        let firstSection = SettingSection(title: NSAttributedString(string: Strings.SettingsNewTabSectionName), footerTitle: nil, children: [tabSetting])
+        self.currentChoice = NewTabAccessors.getNewTabPage(self.prefs)
+        self.hasHomePage = HomePageAccessors.getHomePage(self.prefs) != nil
 
+        let showTopSites = CheckmarkSetting(title: NSAttributedString(string: Strings.SettingsNewTabTopSites), subtitle: nil, accessibilityIdentifier: nil, isEnabled: {return self.currentChoice == NewTabPage.topSites}, onChanged: {
+            self.currentChoice = NewTabPage.topSites
+            self.prefs.setString(self.currentChoice.rawValue, forKey: NewTabAccessors.PrefKey)
+            self.tableView.reloadData()
+        })
+        let showBlankPage = CheckmarkSetting(title: NSAttributedString(string: Strings.SettingsNewTabBlankPage), subtitle: nil, accessibilityIdentifier: nil, isEnabled: {return self.currentChoice == NewTabPage.blankPage}, onChanged: {
+            self.currentChoice = NewTabPage.blankPage
+            self.prefs.setString(self.currentChoice.rawValue, forKey: NewTabAccessors.PrefKey)
+            self.tableView.reloadData()
+        })
+        let showBookmarks = CheckmarkSetting(title: NSAttributedString(string: Strings.SettingsNewTabBookmarks), subtitle: nil, accessibilityIdentifier: nil, isEnabled: {return self.currentChoice == NewTabPage.bookmarks}, onChanged: {
+            self.currentChoice = NewTabPage.bookmarks
+            self.prefs.setString(self.currentChoice.rawValue, forKey: NewTabAccessors.PrefKey)
+            self.tableView.reloadData()
+        })
+        let showHistory = CheckmarkSetting(title: NSAttributedString(string: Strings.SettingsNewTabHistory), subtitle: nil, accessibilityIdentifier: nil, isEnabled: {return self.currentChoice == NewTabPage.history}, onChanged: {
+            self.currentChoice = NewTabPage.history
+            self.prefs.setString(self.currentChoice.rawValue, forKey: NewTabAccessors.PrefKey)
+            self.tableView.reloadData()
+        })
+        let showHomepage = CheckmarkSetting(title: NSAttributedString(string: Strings.SettingsNewTabHomePage), subtitle: nil, accessibilityIdentifier: nil, isEnabled: {return (self.currentChoice == NewTabPage.homePage) && self.hasHomePage}, onChanged: {
+            self.currentChoice = NewTabPage.homePage
+            self.prefs.setString(self.currentChoice.rawValue, forKey: NewTabAccessors.PrefKey)
+            self.tableView.reloadData()
+        })
+
+        var firstSection = SettingSection(title: NSAttributedString(string: Strings.NewTabSectionName), footerTitle: NSAttributedString(string: Strings.NewTabSectionNameFooter), children: [showTopSites, showBlankPage, showBookmarks, showHistory])
+
+        self.hasHomePage = HomePageAccessors.getHomePage(self.prefs) != nil
+        if self.hasHomePage {
+            firstSection = SettingSection(title: NSAttributedString(string: Strings.NewTabSectionName), footerTitle: NSAttributedString(string: Strings.NewTabSectionNameFooter), children: [showTopSites, showBlankPage, showBookmarks, showHistory, showHomepage])
+        }
+      
         let isPocketEnabledDefault = Pocket.IslocaleSupported(Locale.current.identifier)
         let pocketSetting = BoolSetting(prefs: profile.prefs, prefKey: PrefsKeys.ASPocketStoriesVisible, defaultValue: isPocketEnabledDefault, attributedTitleText: NSAttributedString(string: Strings.SettingsNewTabPocket))
         let bookmarks = BoolSetting(prefs: profile.prefs, prefKey: PrefsKeys.ASBookmarkHighlightsVisible, defaultValue: true, attributedTitleText: NSAttributedString(string: Strings.SettingsNewTabHighlightsBookmarks))
@@ -49,6 +88,7 @@ class NewTabContentSettingsViewController: SettingsTableViewController {
         let secondSection = SettingSection(title: NSAttributedString(string: Strings.SettingsNewTabASTitle), footerTitle: nil, children: [pocketSetting, bookmarks, history])
 
         return [firstSection, secondSection]
+
     }
 
     override func viewDidDisappear(_ animated: Bool) {
