3cf7831ff0d62af9a8d3a019d85ba11318313d9d
Stefan Arentz <stefan@arentz.ca>
2018-07-11 16:37:46 -0400

    [focus-ios] Fixes https://github.com/mozilla-mobile/focus-ios/issues/1106 - Import locale updates as part of CI Builds (https://github.com/mozilla-mobile/focus-ios/pull/1108)

--- focus-ios/buddybuild_postclone.sh
+++ focus-ios/buddybuild_postclone.sh
@@ -2,3 +2,10 @@
 
 ./build-disconnect.py
 
+# Update existing locales. This will only update locales that are already
+# imported to the application. It will not include new locales, that
+# remains a manual action.
+
+git clone --depth 1 https://github.com/mozilla-l10n/focusios-l10n.git \
+    && ./import-locales -allowIncomplete focusios-l10n/{??,???,??-??}/focus-ios.xliff
+
