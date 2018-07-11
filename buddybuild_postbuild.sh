0bdd5ce43399d20b02158bcbdc44cded41d29f03
Aaron Train <aaron.train@gmail.com>
2018-07-11 15:09:35 -0400

    Upload dSYMs in buddybuild_postbuild.sh (#4026)

--- buddybuild_postbuild.sh
+++ buddybuild_postbuild.sh
@@ -9,7 +9,10 @@ bash <(curl -s https://codecov.io/bash)
 test_runner="XCUITests-Runner"
 
 if [ "$BUDDYBUILD_SCHEME" = "Fennec" ]; then
+  cd  $BUDDYBUILD_PRODUCT_DIR
+  find . -name "*.dSYM" -print | zip /tmp/dsyms.zip 
   curl -F ipa=@$BUDDYBUILD_IPA_PATH  -u $NIMBLEDROID_API_KEY: https://nimbledroid.com/api/v2/ipas \
+       -F "dsym=@/tmp/dsyms.zip" \
        -F test_identifiers='ActivityStreamTest/testDefaultSites,DomainAutocompleteTest/testAutocomplete,DatabaseFixtureTest/testHistoryDatabaseFixture,BrowsingPDFTests/testBookmarkPDF,ActivityStreamTest/testTopSitesBookmarkNewTopSite,FindInPageTests/testFindInLargeDoc,FirstRunTourTests/testFirstRunTour'
 # comment out the section where it upload the testrunner
 #  (cd $BUDDYBUILD_TEST_DIR/Build/Products/Fennec-iphonesimulator && zip -r $test_runner.zip $test_runner.app)
