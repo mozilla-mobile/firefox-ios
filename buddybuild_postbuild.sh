a09a5d1e7c1425d0db1c32b8b11cc6cc8a4bd52d
Aaron Train <aaron.train@gmail.com>
2018-07-23 18:25:27 -0400

    Fix broken curl parameter
    re: https://dashboard.buddybuild.com/apps/57bf25c0f096bc01001e21e0/build/5b56422dd7342000016e5490#logs
    ` {"code":400,"message":["`test_runner` was injected, please provide `test_identifiers`"]}`

--- buddybuild_postbuild.sh
+++ buddybuild_postbuild.sh
@@ -15,7 +15,7 @@ if [ "$BUDDYBUILD_SCHEME" = "Fennec" ]; then
   curl -F ipa=@$BUDDYBUILD_IPA_PATH  -u $NIMBLEDROID_API_KEY: https://nimbledroid.com/api/v2/ipas \
        -F "dsyms=@/tmp/dsyms.zip" \
        -F device_config='ios10,ios11' \
-       -F launch_args='nimbledroid'
+       -F launch_args='nimbledroid' \
        -F test_identifiers='ActivityStreamTest/testDefaultSites,DomainAutocompleteTest/testAutocomplete,DatabaseFixtureTest/testHistoryDatabaseFixture,BrowsingPDFTests/testBookmarkPDF,ActivityStreamTest/testTopSitesBookmarkNewTopSite,FindInPageTests/testFindInLargeDoc,FirstRunTourTests/testFirstRunTour'
 # comment out the section where it upload the testrunner
 #  (cd $BUDDYBUILD_TEST_DIR/Build/Products/Fennec-iphonesimulator && zip -r $test_runner.zip $test_runner.app)
