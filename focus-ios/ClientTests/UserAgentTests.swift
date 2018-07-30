644aeea0deead8f54fc1cfab56fcebe358988170
Joe Gasiorek <joeyg@users.noreply.github.com>
2018-07-30 14:29:23 -0700

    [focus-ios] Closes https://github.com/mozilla-mobile/focus-ios/pull/1161: Fix UserAgentTests in Klar (https://github.com/mozilla-mobile/focus-ios/pull/1162)

--- focus-ios/ClientTests/UserAgentTests.swift
+++ focus-ios/ClientTests/UserAgentTests.swift
@@ -4,7 +4,12 @@
 
 
 import XCTest
+
+#if FOCUS
 @testable import Firefox_Focus
+#else
+@testable import Firefox_Klar
+#endif
 
 class UserAgentTests: XCTestCase {
     private let fakeUserAgent = "a-fake-browser"
