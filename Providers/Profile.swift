7ef1f54d24e2b62b82a277627285ed6fd0ddb81d
Justin D'Arcangelo <justindarc@gmail.com>
2018-07-31 14:25:23 -0400

    TEMP: Use stage FxA servers for beta testing.

--- Providers/Profile.swift
+++ Providers/Profile.swift
@@ -523,7 +523,7 @@ open class BrowserProfile: Profile {
         if prefs.boolForKey("useStageSyncService") ?? false {
             return StageFirefoxAccountConfiguration()
         }
-        return ProductionFirefoxAccountConfiguration()
+        return StageFirefoxAccountConfiguration()
     }
 
     fileprivate lazy var account: FirefoxAccount? = {
