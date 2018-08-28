20262d9510c7aefa597b16f2587d8a592b064f30
Meera Rachamallu <mrachamallu@mrachamallu-30htdf.local>
2018-08-28 11:04:25 -0700

    started looking at credentialviewcontroller

--- firefoxAutoFillExtension/CredentialProviderViewController.swift
+++ firefoxAutoFillExtension/CredentialProviderViewController.swift
@@ -25,7 +25,7 @@ class CredentialProviderViewController: ASCredentialProviderViewController {
      Provide the password by completing the extension request with the associated ASPasswordCredential.
      If using the credential would require showing custom UI for authenticating the user, cancel
      the request with error code ASExtensionError.userInteractionRequired.
-
+*/
     override func provideCredentialWithoutUserInteraction(for credentialIdentity: ASPasswordCredentialIdentity) {
         let databaseIsUnlocked = true
         if (databaseIsUnlocked) {
@@ -35,17 +35,17 @@ class CredentialProviderViewController: ASCredentialProviderViewController {
             self.extensionContext.cancelRequest(withError: NSError(domain: ASExtensionErrorDomain, code:ASExtensionError.userInteractionRequired.rawValue))
         }
     }
-    */
+
 
     /*
      Implement this method if provideCredentialWithoutUserInteraction(for:) can fail with
      ASExtensionError.userInteractionRequired. In this case, the system may present your extension's
      UI and call this method. Show appropriate UI for authenticating the user then provide the password
      by completing the extension request with the associated ASPasswordCredential.
-
+*/
     override func prepareInterfaceToProvideCredential(for credentialIdentity: ASPasswordCredentialIdentity) {
     }
-    */
+
 
     @IBAction func cancel(_ sender: AnyObject?) {
         self.extensionContext.cancelRequest(withError: NSError(domain: ASExtensionErrorDomain, code: ASExtensionError.userCanceled.rawValue))
