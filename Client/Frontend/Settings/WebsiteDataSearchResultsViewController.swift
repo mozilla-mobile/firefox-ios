fbc789c775c41dde91ec07615b3315655e03b5e7
Justin D'Arcangelo <justindarc@gmail.com>
2018-09-27 15:40:39 -0400

    Bug 1493989 - Data Management: Entries appearing multiple times (#4274)

---
+++ Client/Frontend/Settings/WebsiteDataSearchResultsViewController.swift
@@ -0,0 +1,95 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import UIKit
+import SnapKit
+import Shared
+import WebKit
+
+private let SectionHeaderFooterIdentifier = "SectionHeaderFooterIdentifier"
+
+class WebsiteDataSearchResultsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    var tableView: UITableView!
+    
+    private var filteredSiteRecords = [WKWebsiteDataRecord]()
+    var siteRecords = [WKWebsiteDataRecord]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView = UITableView()
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.separatorColor = UIColor.theme.tableView.separator
+        tableView.backgroundColor = UIColor.theme.tableView.headerBackground
+        tableView.isEditing = true
+        tableView.register(ThemedTableViewCell.self, forCellReuseIdentifier: "Cell")
+        tableView.register(ThemedTableSectionHeaderFooterView.self, forHeaderFooterViewReuseIdentifier: SectionHeaderFooterIdentifier)
+        view.addSubview(tableView)
+
+        let footer = ThemedTableSectionHeaderFooterView(frame: CGRect(width: tableView.bounds.width, height: SettingsUX.TableViewHeaderFooterHeight))
+        footer.showBottomBorder = false
+        tableView.tableFooterView = footer
+
+        updateConstraints()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filteredSiteRecords.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        if let record = filteredSiteRecords[safe: indexPath.row] {
+            cell.textLabel?.text = record.displayName
+        }
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, shouldHighlightRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        guard editingStyle == UITableViewCellEditingStyle.delete, let record = filteredSiteRecords[safe: indexPath.row] else {
+            return
+        }
+
+        let types = WKWebsiteDataStore.allWebsiteDataTypes()
+        WKWebsiteDataStore.default().removeData(ofTypes: types, for: [record]) {
+            self.filteredSiteRecords.remove(at: indexPath.row)
+            self.tableView.reloadData()
+        }
+    }
+
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = tableView.dequeueReusableHeaderFooterView(withIdentifier: SectionHeaderFooterIdentifier) as? ThemedTableSectionHeaderFooterView
+        headerView?.titleLabel.text = Strings.SettingsWebsiteDataTitle
+        return headerView
+    }
+
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return SettingsUX.TableViewHeaderFooterHeight
+    }
+
+    func filterContentForSearchText(_ searchText: String) {
+        filteredSiteRecords = siteRecords.filter({ siteRecord in
+            return siteRecord.displayName.lowercased().contains(searchText.lowercased())
+        })
+
+        tableView.reloadData()
+    }
+
+    private func updateConstraints() {
+        tableView.snp.makeConstraints { make in
+            make.edges.equalTo(view)
+        }
+    }
+}
+
+extension WebsiteDataSearchResultsViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        filterContentForSearchText(searchController.searchBar.text!)
+    }
+}
