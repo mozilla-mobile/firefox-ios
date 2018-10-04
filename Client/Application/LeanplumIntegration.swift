aea71f0bf6a9ad3d8789fc1e72df3e0972eab2da
Garvan Keeley <garvankeeley+github@gmail.com>
2018-10-04 12:05:37 -0400

    Bug 1485064 - disable tab bar drag reorder (#4294)

--- Client/Application/LeanplumIntegration.swift
+++ Client/Application/LeanplumIntegration.swift
@@ -102,7 +102,8 @@ class LeanPlumClient {
     private var useFxAPrePush: LPVar = LPVar.define("useFxAPrePush", with: false)
     var enablePocketVideo: LPVar = LPVar.define("pocketVideo", with: false)
     var enableDragDrop: LPVar = LPVar.define("tabTrayDrag", with: false)
-    
+    var enableTabBarReorder: LPVar = LPVar.define("tabBarDragReorder", with: false)
+
     var introScreenVars = LPVar.define("IntroScreen", with: IntroCard.defaultCards().compactMap({ $0.asDictonary() }))
 
     private func isPrivateMode() -> Bool {
