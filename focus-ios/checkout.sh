2a889ca0fad9a85e2ce2bf7d280240b5459be94f
Botary <narshodan@hotmail.com>
2018-10-25 04:45:16 +0900

    [focus-ios] Fixes https://github.com/mozilla-mobile/focus-ios/issues/1443: Support both Python 2.7 and >2.7 (https://github.com/mozilla-mobile/focus-ios/pull/1482)
    * Support both python 2.7 and >2.7
    
    * Print Python version

--- focus-ios/checkout.sh
+++ focus-ios/checkout.sh
@@ -4,6 +4,21 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-./build-disconnect.py
+if ! hash python; then
+    echo "python is not installed"
+    exit 1
+fi
+
+ver=$(python -V 2>&1 | sed 's/.* \([0-9]\).\([0-9]\).*/\1\2/')
+if [ "$ver" -lt "27" ]; then
+    echo "This script requires python 2.7 or greater"
+    exit 1
+elif [ "$ver" -eq "27" ]; then
+    echo "Python 27 detected. Running build-disconnect2.py"
+    ./build-disconnect2.py
+elif [ "$ver" -gt "27" ]; then
+    echo "Python ${ver} detected. Running build-disconnect3.py"
+    ./build-disconnect3.py
+fi
 
 carthage bootstrap --platform iOS
