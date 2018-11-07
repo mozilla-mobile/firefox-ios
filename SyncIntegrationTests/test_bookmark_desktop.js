a8bce553bd5ea0ec80ce96918524e67e6ae63f1e
Isabel Rios <isabelrios@MackBookirios.home>
2018-11-07 16:33:47 +0100

    Bug 1472709 - XCUITest SyncIntegration test starting on Desktop

---
+++ SyncIntegrationTests/test_bookmark_desktop.js
@@ -0,0 +1,26 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+/*
+ * The list of phases mapped to their corresponding profiles.  The object
+ * here must be in strict JSON format, as it will get parsed by the Python
+ * testrunner (no single quotes, extra comma's, etc).
+ */
+EnableEngines(["bookmarks"]);
+
+var phases = { "phase1": "profile1" };
+
+
+// expected bookmark state
+var bookmarksCreated = {
+"mobile": [{
+  uri: "http://www.example.com/",
+  title: "Example Domain"}]
+};
+
+// sync and verify bookmarks
+Phase("phase1", [
+  [Sync],
+  [Bookmarks.add, bookmarksCreated],
+  [Sync]
+]);
