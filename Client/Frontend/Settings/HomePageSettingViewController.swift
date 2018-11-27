c1e630db29509bf2e7e9fe7b89a7368c37539e75
farhanpatel <farhanpatel.17@gmail.com>
2018-11-27 12:40:22 -0800

    Bug 1510234 - Make sure 2 options are not selected in the NewTab/HomePage settings panel.

--- Client/Frontend/Settings/HomePageSettingViewController.swift
+++ Client/Frontend/Settings/HomePageSettingViewController.swift
@@ -32,21 +32,25 @@ class HomePageSettingViewController: SettingsTableViewController {
         let showTopSites = CheckmarkSetting(title: NSAttributedString(string: Strings.SettingsNewTabTopSites), subtitle: nil, accessibilityIdentifier: nil, isEnabled: {return self.currentChoice == NewTabPage.topSites}, onChanged: {
             self.currentChoice = NewTabPage.topSites
             self.prefs.setString(self.currentChoice.rawValue, forKey: NewTabAccessors.HomePrefKey)
+            self.prefs.removeObjectForKey(PrefsKeys.HomeButtonHomePageURL)
             self.tableView.reloadData()
         })
         let showBlankPage = CheckmarkSetting(title: NSAttributedString(string: Strings.SettingsNewTabBlankPage), subtitle: nil, accessibilityIdentifier: nil, isEnabled: {return self.currentChoice == NewTabPage.blankPage}, onChanged: {
             self.currentChoice = NewTabPage.blankPage
             self.prefs.setString(self.currentChoice.rawValue, forKey: NewTabAccessors.HomePrefKey)
+            self.prefs.removeObjectForKey(PrefsKeys.HomeButtonHomePageURL)
             self.tableView.reloadData()
         })
         let showBookmarks = CheckmarkSetting(title: NSAttributedString(string: Strings.SettingsNewTabBookmarks), subtitle: nil, accessibilityIdentifier: nil, isEnabled: {return self.currentChoice == NewTabPage.bookmarks}, onChanged: {
             self.currentChoice = NewTabPage.bookmarks
             self.prefs.setString(self.currentChoice.rawValue, forKey: NewTabAccessors.HomePrefKey)
+            self.prefs.removeObjectForKey(PrefsKeys.HomeButtonHomePageURL)
             self.tableView.reloadData()
         })
         let showHistory = CheckmarkSetting(title: NSAttributedString(string: Strings.SettingsNewTabHistory), subtitle: nil, accessibilityIdentifier: nil, isEnabled: {return self.currentChoice == NewTabPage.history}, onChanged: {
             self.currentChoice = NewTabPage.history
             self.prefs.setString(self.currentChoice.rawValue, forKey: NewTabAccessors.HomePrefKey)
+            self.prefs.removeObjectForKey(PrefsKeys.HomeButtonHomePageURL)
             self.tableView.reloadData()
         })
 
--- Client/Frontend/Settings/NewTabContentSettingsViewController.swift
+++ Client/Frontend/Settings/NewTabContentSettingsViewController.swift
@@ -30,21 +30,25 @@ class NewTabContentSettingsViewController: SettingsTableViewController {
         let showTopSites = CheckmarkSetting(title: NSAttributedString(string: Strings.SettingsNewTabTopSites), subtitle: nil, accessibilityIdentifier: nil, isEnabled: {return self.currentChoice == NewTabPage.topSites}, onChanged: {
             self.currentChoice = NewTabPage.topSites
             self.prefs.setString(self.currentChoice.rawValue, forKey: NewTabAccessors.NewTabPrefKey)
+            self.prefs.removeObjectForKey(HomePageConstants.HomePageURLPrefKey)
             self.tableView.reloadData()
         })
         let showBlankPage = CheckmarkSetting(title: NSAttributedString(string: Strings.SettingsNewTabBlankPage), subtitle: nil, accessibilityIdentifier: nil, isEnabled: {return self.currentChoice == NewTabPage.blankPage}, onChanged: {
             self.currentChoice = NewTabPage.blankPage
             self.prefs.setString(self.currentChoice.rawValue, forKey: NewTabAccessors.NewTabPrefKey)
+            self.prefs.removeObjectForKey(HomePageConstants.HomePageURLPrefKey)
             self.tableView.reloadData()
         })
         let showBookmarks = CheckmarkSetting(title: NSAttributedString(string: Strings.SettingsNewTabBookmarks), subtitle: nil, accessibilityIdentifier: nil, isEnabled: {return self.currentChoice == NewTabPage.bookmarks}, onChanged: {
             self.currentChoice = NewTabPage.bookmarks
             self.prefs.setString(self.currentChoice.rawValue, forKey: NewTabAccessors.NewTabPrefKey)
+            self.prefs.removeObjectForKey(HomePageConstants.HomePageURLPrefKey)
             self.tableView.reloadData()
         })
         let showHistory = CheckmarkSetting(title: NSAttributedString(string: Strings.SettingsNewTabHistory), subtitle: nil, accessibilityIdentifier: nil, isEnabled: {return self.currentChoice == NewTabPage.history}, onChanged: {
             self.currentChoice = NewTabPage.history
             self.prefs.setString(self.currentChoice.rawValue, forKey: NewTabAccessors.NewTabPrefKey)
+            self.prefs.removeObjectForKey(HomePageConstants.HomePageURLPrefKey)
             self.tableView.reloadData()
         })
 
