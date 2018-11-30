2acfb3b97b46b937bba3bc2a53313de4bfe8a06e
farhanpatel <farhanpatel.17@gmail.com>
2018-11-30 12:00:33 -0800

    Review nits.

--- Client/Frontend/Home/TabbedHomePanelController.swift
+++ Client/Frontend/Home/TabbedHomePanelController.swift
@@ -1,13 +1,8 @@
-//
-//  TabbedHomePanelController.swift
-//  Client
-//
-//  Created by Farhan Patel on 11/29/18.
-//  Copyright © 2018 Mozilla. All rights reserved.
-//
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import Foundation
-
 
 class TabbedHomePanelController: ThemedNavigationController {
     var currentPanelType: NewTabPage?
