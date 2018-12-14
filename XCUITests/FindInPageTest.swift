db857c54d5de152f9e8c2d1b91f70082f4c27612
Garvan Keeley <garvankeeley+github@gmail.com>
2018-12-14 18:34:11 -0500

    Bug 1514171 - tab restore followup: about/home was changed (#4430)

--- XCUITests/FindInPageTest.swift
+++ XCUITests/FindInPageTest.swift
@@ -17,7 +17,7 @@ class FindInPageTests: BaseTestCase {
     }
 
     func testFindInLargeDoc() {
-        navigator.openURL("http://localhost:6571/find-in-page-test.html")
+        navigator.openURL("http://localhost:6571/test-fixture/find-in-page-test.html")
         // Workaround until FxSGraph is fixed to allow the previos way with goto
         navigator.nowAt(BrowserTab)
 
@@ -80,7 +80,7 @@ class FindInPageTests: BaseTestCase {
     }
 
     func testFindInPageTwoWordsSearchLargeDoc() {
-        navigator.openURL("http://localhost:6571/find-in-page-test.html")
+        navigator.openURL("http://localhost:6571/test-fixture/find-in-page-test.html")
         // Workaround until FxSGraph is fixed to allow the previos way with goto
         navigator.nowAt(BrowserTab)
         waitForExistence(app/*@START_MENU_TOKEN@*/.buttons["TabLocationView.pageOptionsButton"]/*[[".buttons[\"Page Options Menu\"]",".buttons[\"TabLocationView.pageOptionsButton\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/, timeout: 15)
--- XCUITests/WebPagesForTesting.swift
+++ XCUITests/WebPagesForTesting.swift
@@ -8,7 +8,7 @@ import GCDWebServers
 
 func registerHandlersForTestMethods(server: GCDWebServer) {
     // Add tracking protection check page
-    server.addHandler(forMethod: "GET", path: "/find-in-page-test.html", request: GCDWebServerRequest.self) { (request: GCDWebServerRequest?) in
+    server.addHandler(forMethod: "GET", path: "/test-fixture/find-in-page-test.html", request: GCDWebServerRequest.self) { (request: GCDWebServerRequest?) in
 
         let node = "<span>  And the beast shall come forth surrounded by a roiling cloud of vengeance. The house of the unbelievers shall be razed and they shall be scorched to the earth. Their tags shall blink until the end of days. from The Book of Mozilla, 12:10 And the beast shall be made legion. Its numbers shall be increased a thousand thousand fold. The din of a million keyboards like unto a great storm shall cover the earth, and the followers of Mammon shall tremble. from The Book of Mozilla, 3:31 (Red Letter Edition) </span>"
 
