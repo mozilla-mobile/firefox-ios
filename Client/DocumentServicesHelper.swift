573c8cd78f38ffd2d5180df27637a8486ab7d998
farhanpatel <farhanpatel.17@gmail.com>
2018-12-18 12:07:14 -0800

    Make it more generic

--- Client/DocumentServicesHelper.swift
+++ Client/DocumentServicesHelper.swift
@@ -2,32 +2,38 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import JavaScriptCore
 import Shared
 import Storage
 import NaturalLanguage
 
 protocol DocumentAnalyser {
     var name: String { get }
-    func analyse(metadata: PageMetadata) -> DerivedMetadata?
-}
-
-struct DerivedMetadata {
-    let language: String?
+    associatedtype NewMetadata
+    func analyse(metadata: PageMetadata) -> NewMetadata?
 }
 
 struct LanguageDetector: DocumentAnalyser {
-    let name = "NLTranslator"
+    let name = "language" //This key matches the DerivedMetadata property
+    typealias NewMetadata = String //This matches the value for the DerivedMetadata key above
 
-    func analyse(metadata: PageMetadata) -> DerivedMetadata? {
-        let text = [metadata.description, metadata.title].compactMap({$0}).joined(separator: " ")
+    func analyse(metadata: PageMetadata) -> LanguageDetector.NewMetadata?  {
+        let text = [metadata.description].compactMap({$0}).joined(separator: " ")
         let language: String?
         if #available(iOS 12.0, *) {
             language = NLLanguageRecognizer.dominantLanguage(for: text)?.rawValue
         } else {
             language = NSLinguisticTagger.dominantLanguage(for: text)
         }
-        return DerivedMetadata(language: language)
+        return language
+    }
+}
+
+struct DerivedMetadata: Codable {
+    let language: String?
+
+    // New keys need to be mapped in this constructor
+    static func from(dict: [String: Any?]) -> DerivedMetadata? {
+        return DerivedMetadata(language: dict["language"] as? String)
     }
 }
 
@@ -43,9 +49,7 @@ class DocumentServicesHelper: TabEventHandler {
     }()
 
     init() {
-        self.tabObservers = registerFor(
-            .didLoadPageMetadata,
-            queue: singleThreadedQueue)
+        self.tabObservers = registerFor(.didLoadPageMetadata, queue: singleThreadedQueue)
     }
 
     deinit {
@@ -53,9 +57,13 @@ class DocumentServicesHelper: TabEventHandler {
     }
 
     func tab(_ tab: Tab, didLoadPageMetadata metadata: PageMetadata) {
-        guard let langaugeMetadata = LanguageDetector().analyse(metadata: metadata) else { return }
+        // New analyzers go here. We map through each one and reduce into one dictionary
+        let analyzers = [LanguageDetector()]
+        let dict = analyzers.map({ [$0.name: $0.analyse(metadata: metadata)] }).compactMap({$0}).reduce([:]) { $0.merging($1) { (current, _) in current } }
+
+        guard let derivedMetadata = DerivedMetadata.from(dict: dict) else { return }
         DispatchQueue.global().async {
-            TabEvent.post(.didDeriveMetadata(langaugeMetadata), for: tab)
+            TabEvent.post(.didDeriveMetadata(derivedMetadata), for: tab)
         }
     }
 }
