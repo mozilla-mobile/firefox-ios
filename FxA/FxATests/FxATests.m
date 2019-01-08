4bcab24011e44a9120f25f1b73f50e890f925325
Isabel Rios <isabelrios@MackBookirios.home>
2019-01-08 21:58:59 +0100

    fixing FxA signing

---
+++ FxA/FxATests/FxATests.m
@@ -0,0 +1,37 @@
+//
+//  FxATests.m
+//  FxATests
+//
+//  Created by Richard Newman on 2014-09-22.
+//  Copyright (c) 2014 Mozilla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <FxA/FxA.h>
+
+@interface FxATests : XCTestCase
+
+@end
+
+@implementation FxATests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
