2939044d9395cb1a93d2473da5b5ce507bb53853
farhanpatel <farhanpatel.17@gmail.com>
2019-01-11 11:45:08 -0800

    fix comment

--- Client/DocumentServicesHelper.swift
+++ Client/DocumentServicesHelper.swift
@@ -20,7 +20,7 @@ struct LanguageDetector: DocumentAnalyser {
         if let metadataLanguage = metadata.language {
             return metadataLanguage
         }
-        // Lets not use any language detection until we can pass more text then the webpage description.
+        // Lets not use any language detection until we can pass more text to the language detector
         return nil // https://bugzilla.mozilla.org/show_bug.cgi?id=1519503
         guard let text = metadata.description else { return nil }
         let language: String?
