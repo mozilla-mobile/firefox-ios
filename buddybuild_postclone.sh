9053cbb64dc3bf1bf2bc0b37e1eb95f60aaf2456
Garvan Keeley <garvankeeley+github@gmail.com>
2019-02-15 10:20:56 -0500

    [nobug, build] buddybuild_postclone remove unneeded rust arch (#4521)

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -21,7 +21,7 @@ npm run build
 
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 source $HOME/.cargo/env
-rustup target add aarch64-apple-ios armv7-apple-ios armv7s-apple-ios x86_64-apple-ios i386-apple-ios
+rustup target add aarch64-apple-ios x86_64-apple-ios
 cargo install cargo-lipo
 
 #
