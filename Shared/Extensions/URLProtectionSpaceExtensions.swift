4478290a7afaa5d4155c5c9deb136507fba20ea5
Justin D'Arcangelo <justindarc@gmail.com>
2019-02-21 18:36:32 -0500

    Bug 1413451 - A login's formSubmitURL should only contain the origin, no path components

--- Shared/Extensions/URLProtectionSpaceExtensions.swift
+++ Shared/Extensions/URLProtectionSpaceExtensions.swift
@@ -5,6 +5,20 @@
 import Foundation
 
 extension URLProtectionSpace {
+    public static func fromOrigin(_ origin: String) -> URLProtectionSpace {
+        // Break down the full url hostname into its scheme/protocol and host components
+        let hostnameURL = origin.asURL
+        let host = hostnameURL?.host ?? origin
+        let scheme = hostnameURL?.scheme ?? ""
+
+        // We should ignore any SSL or normal web ports in the URL.
+        var port = hostnameURL?.port ?? 0
+        if port == 443 || port == 80 {
+            port = 0
+        }
+
+        return URLProtectionSpace(host: host, port: port, protocol: scheme, realm: nil, authenticationMethod: nil)
+    }
 
     public func urlString() -> String {
         // If our host is empty, return nothing since it doesn't make sense to add the scheme or port.
