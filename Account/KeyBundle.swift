4362e8975e9ab9fca078357aab9c87507faed90d
Justin D'Arcangelo <justindarc@gmail.com>
2019-03-13 17:24:57 -0400

    Use CCAlgorithm instead.

--- Account/KeyBundle.swift
+++ Account/KeyBundle.swift
@@ -113,7 +113,7 @@ open class KeyBundle: Hashable {
 
         let success: CCCryptorStatus =
             CCCrypt(op,
-                    CCHmacAlgorithm(kCCAlgorithmAES128), // Block size, *NOT* key size
+                    CCAlgorithm(kCCAlgorithmAES128), // Block size, *NOT* key size
                     CCOptions(kCCOptionPKCS7Padding),
                     encKey.getBytes(),
                     kCCKeySizeAES256,
