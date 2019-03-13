74657d21167ab5b60138f990494f3678233d67b3
Justin D'Arcangelo <justindarc@gmail.com>
2019-03-13 15:57:39 -0400

    Bug 1535083 - Pinned Top Sites query is sloooowwww (#4609)
    * Bug 1535083 - Pinned Top Sites query is sloooowwww
    
    * Remove all other instances of view_history_id_favicon and remove queries no longer being used.

--- Shared/AppConstants.swift
+++ Shared/AppConstants.swift
@@ -115,19 +115,6 @@ public struct AppConstants {
         #endif
     }()
 
-    ///  Toggle full-text search for Awesomebar
-    public static let MOZ_ENABLE_HISTORY_FTS: Bool = {
-        #if MOZ_CHANNEL_RELEASE
-            return true
-        #elseif MOZ_CHANNEL_BETA
-            return true
-        #elseif MOZ_CHANNEL_FENNEC
-            return true
-        #else
-            return true
-        #endif
-    }()
-
     ///  Toggle use of FxA Messages (Pushbox) for "Send Tab"
     public static let MOZ_FXA_MESSAGES: Bool = {
         #if MOZ_CHANNEL_RELEASE
