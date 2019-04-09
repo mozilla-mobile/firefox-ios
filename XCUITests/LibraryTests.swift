5d2d47f9fceab5665e124293ed92260e1d14e453
kglazko <katglazko@gmail.com>
2019-04-09 07:12:00 -0700

    Bug 1534206 - XCUITests: new tests library shortcuts homepage (#4683)

--- XCUITests/LibraryTests.swift
+++ XCUITests/LibraryTests.swift
@@ -4,7 +4,7 @@
 
 import XCTest
 
-class LibraryTest: IpadOnlyTestCase {
+class LibraryTestsIpad: IpadOnlyTestCase {
 
     func testLibraryShortcut() {
         if skipPlatform {return}
@@ -21,5 +21,41 @@ class LibraryTest: IpadOnlyTestCase {
         waitForExistence(app.buttons["TabToolbar.libraryButton"])
         libraryShorcutButton.tap()
         waitForExistence(app.tables["History List"])
+    }
+}
+
+class LibraryTestsIphone: IphoneOnlyTestCase {
+    
+    func testLibraryShortcutHomePage () {
+        if skipPlatform {return}
+        waitForExistence(app.staticTexts["libraryTitle"])
+        waitForExistence(app.buttons["menu Bookmark"])
+        waitForExistence(app.buttons["menu panel History"])
+        waitForExistence(app.buttons["menu panel ReadingList"])
+        waitForExistence(app.buttons["menu panel Downloads"])
+        
+        // Check if clicking on Bookmark option shows bookmarks
+        app.buttons["menu Bookmark"].tap()
+        navigator.nowAt(HomePanel_Bookmarks)
+        waitForExistence(app.tables["Bookmarks List"])
+        navigator.goto(HomePanelsScreen)
+        
+        // Check if clicking on History option shows history
+        app.buttons["menu panel History"].tap()
+        navigator.nowAt(HomePanel_History)
+        waitForExistence(app.tables["History List"])
+        navigator.goto(HomePanelsScreen)
+        
+        // Check if clicking on Reading List option shows history
+        app.buttons["menu panel ReadingList"].tap()
+        navigator.nowAt(HomePanel_ReadingList)
+        waitForExistence(app.tables["ReadingTable"])
+        navigator.goto(HomePanelsScreen)
+        
+        // Check if clicking on Downloads option shows history
+        app.buttons["menu panel Downloads"].tap()
+        navigator.nowAt(HomePanel_Downloads)
+        waitForExistence(app.tables["DownloadsTable"])
+        navigator.goto(HomePanelsScreen)
     }
 }
