71607206c76bd3d9e8ef0bbc1de25fd17bae39e3
Alexey Karataev <35783149+akarataev@users.noreply.github.com>
2019-04-11 01:06:04 +0300

    Bug 1534856 - Add copy version feature (#4751) (#4767)

---
+++ ClientTests/VersionSettingTests.swift
@@ -0,0 +1,26 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import XCTest
+@testable import Client
+
+class VersionSettingTests: XCTestCase {
+
+    func testCopyAppVersion() {
+        // MARK: - given
+        let settingsTable = SettingsTableViewController(style: .grouped)
+        let navigationController = UINavigationController(rootViewController: settingsTable)
+        let versionSetting = VersionSetting(settings: settingsTable)
+        
+        // MARK: - when
+        versionSetting.onClick(navigationController)
+        
+        // MARK: - then
+        let appVersionString = UIPasteboard.general.string
+        let appVersionPredicate = (appVersionString?.contains("Version") ?? false) == true
+        XCTAssertNotNil(appVersionString, "App version doesn't be copying")
+        XCTAssert(appVersionPredicate, "Pasteboard doesn't contains app version")
+    }
+
+}
