7af9bff61f54890ef4d7aa1426d4596012a731af
Justin D'Arcangelo <justindarc@gmail.com>
2019-04-15 13:11:54 -0400

    Fix #4781 - Rename HomePanel to LibraryPanel, ActivityStreamPanel to FirefoxHomeViewController (#4783)

--- Extensions/ShareTo/ShareViewController.swift
+++ Extensions/ShareTo/ShareViewController.swift
@@ -328,7 +328,7 @@ extension ShareViewController {
 
         if let shareItem = shareItem, case .shareItem(let item) = shareItem {
             let profile = BrowserProfile(localName: "profile")
-            _ = profile.places.createBookmark(parentGUID: "mobile______", url: item.url, title: item.title).value // Intentionally block thread with database call.
+            _ = profile.places.createBookmark(parentGUID: BookmarkRoots.MobileFolderGUID, url: item.url, title: item.title).value // Intentionally block thread with database call.
             profile._shutdown()
 
             addAppExtensionTelemetryEvent(forMethod: "bookmark-this-page")
