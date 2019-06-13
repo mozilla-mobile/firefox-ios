ca54404ed961a63e9429f002628e9bb17907f02d
Justin D'Arcangelo <jdarcangelo@mozilla.com>
2019-06-13 16:14:30 -0400

    Fix #5067 - Reading List does not live-refresh if open when adding to it on iPad (#5141)

--- Client/Frontend/Browser/BrowserViewController/BrowserViewController+ReaderMode.swift
+++ Client/Frontend/Browser/BrowserViewController/BrowserViewController+ReaderMode.swift
@@ -155,6 +155,8 @@ extension BrowserViewController {
 
 extension BrowserViewController: ReaderModeBarViewDelegate {
     func readerModeBar(_ readerModeBar: ReaderModeBarView, didSelectButton buttonType: ReaderModeBarButtonType) {
+        libraryDrawerViewController?.close()
+
         switch buttonType {
         case .settings:
             if let readerMode = tabManager.selectedTab?.getContentScript(name: "ReaderMode") as? ReaderMode, readerMode.state == ReaderModeState.active {
