c774294f5c31994d687b68c2a1af85198a6a7f99
Garvan Keeley <garvankeeley+github@gmail.com>
2019-08-30 10:06:24 -0400

    Revert "bug5375: Add a Tab Sent notification (#5387)"
    This reverts commit 8ab721c28594b162ae4601b7d2dd37eb10c93397.

--- Client/Frontend/Browser/BrowserViewController.swift
+++ Client/Frontend/Browser/BrowserViewController.swift
@@ -2453,9 +2453,6 @@ extension BrowserViewController: DevicePickerViewControllerDelegate, Instruction
         }
         profile.sendItem(shareItem, toDevices: devices).uponQueue(.main) { _ in
             self.popToBVC()
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
-                SimpleToast().showAlertWithText(Strings.AppMenuTabSentConfirmMessage, bottomContainer: self.webViewContainer)
-            }
         }
     }
 }
