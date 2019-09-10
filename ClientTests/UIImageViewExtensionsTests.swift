cbcc5f17fc06d610b8677b0003484a3cc1a1c872
Garvan Keeley <garvankeeley+github@gmail.com>
2019-09-10 09:51:32 -0400

    [nobug] Remove FxLogo icon, modift test that was using it (#5426)

--- ClientTests/UIImageViewExtensionsTests.swift
+++ ClientTests/UIImageViewExtensionsTests.swift
@@ -35,7 +35,7 @@ class UIImageViewExtensionsTests: XCTestCase {
     }
 
     func testAsyncSetIcon() {
-        let originalImage = UIImage(named: "fxLogo")!
+        let originalImage = UIImage(named: "bookmark")!
 
         WebServer.sharedInstance.registerHandlerForMethod("GET", module: "favicon", resource: "icon") { (request) -> GCDWebServerResponse in
             return GCDWebServerDataResponse(data: originalImage.pngData()!, contentType: "image/png")
@@ -45,9 +45,8 @@ class UIImageViewExtensionsTests: XCTestCase {
         favImageView.setIcon(Favicon(url: "http://localhost:\(AppInfo.webserverPort)/favicon/icon"), forURL: URL(string: "http://localhost:\(AppInfo.webserverPort)"))
 
         let expect = expectation(description: "UIImageView async load")
-        let time = Int64(2 * Double(NSEC_PER_SEC))
-        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + Double(time) / Double(NSEC_PER_SEC)) {
-            XCTAssert(originalImage.isStrictlyEqual(to: favImageView.image!), "The correct favicon should be applied to the UIImageView")
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1.0) {
+            XCTAssert(originalImage.size.width * originalImage.scale == favImageView.image!.size.width * favImageView.image!.scale, "The correct favicon should be applied to the UIImageView")
             expect.fulfill()
         }
         waitForExpectations(timeout: 5, handler: nil)
