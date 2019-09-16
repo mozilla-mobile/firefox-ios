938387ff4acf4d040da50dd727d46eea900e008c
isabelrios <isabelrios@gmail.com>
2019-09-16 12:19:55 +0200

    Fix #5267 - XCUITests new test for Whats new page option

---
+++ XCUITests/WhatsNewTest.swift
@@ -0,0 +1,14 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import XCTest
+
+class WhatsNewTest: BaseTestCase {
+    func testWhatsNew() {
+        navigator.goto(BrowserTabMenu)
+        navigator.performAction(Action.OpenWhatsNewPage)
+        waitUntilPageLoad()
+        waitForValueContains(app.textFields["url"], value: "support")
+    }
+}
