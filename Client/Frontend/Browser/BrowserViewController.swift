916dda46a620d6c5a6970fff7bd2467aef1a245b
Yannick Heinrich <yannick.heinrich@gmail.com>
2019-10-10 22:01:10 +0200

    Fix #4946 (#5529)
    * Fix #4946
    
    Using `NSDataDetector`, I tried to fix #4946 by detecting the content of
    the text in case the current URL parsing failed.
    
    Two cases has been detected for now: link and phone number.
    
    * Update missing file
    
    * Triggers telemetry for every case

--- Client/Frontend/Browser/BrowserViewController.swift
+++ Client/Frontend/Browser/BrowserViewController.swift
@@ -1129,9 +1129,18 @@ extension BrowserViewController: QRCodeViewControllerDelegate {
     }
 
     func didScanQRCodeWithText(_ text: String) {
-        guard let tab = tabManager.selectedTab else { return }
-        submitSearchText(text, forTab: tab)
         UnifiedTelemetry.recordEvent(category: .action, method: .scan, object: .qrCodeText)
+        let content = TextContentDetector.detectTextContent(text)
+        switch content {
+        case .some(.link(let url)):
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        case .some(.phoneNumber(let phoneNumber)):
+            let url = URL(string: "tel:\(phoneNumber)")!
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        default:
+            guard let tab = tabManager.selectedTab else { return }
+            submitSearchText(text, forTab: tab)
+        }
     }
 }
 
