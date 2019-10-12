22f9b42c34248d23ad0d989e6d87638b02e52982
Steven Rockarts <stevenrockarts@gmail.com>
2019-10-12 11:25:58 -0600

    Fix #5573 Change site title and description color using theme manager.

--- Client/Frontend/Widgets/PhotonActionSheet/PhotonActionSheetWidgets.swift
+++ Client/Frontend/Widgets/PhotonActionSheet/PhotonActionSheetWidgets.swift
@@ -237,6 +237,9 @@ class PhotonActionSheetSiteHeaderView: UITableViewHeaderFooterView {
         }
         self.titleLabel.text = site.title.isEmpty ? site.url : site.title
         self.descriptionLabel.text = site.tileURL.baseDomain
+        self.titleLabel.textColor = ThemeManager.instance.current.actionMenu.foreground
+        self.descriptionLabel.textColor = ThemeManager.instance.current.actionMenu.foreground
+        
     }
 }
 
