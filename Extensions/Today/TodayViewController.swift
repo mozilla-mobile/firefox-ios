be6c3d5125892ada5a1ff81b50d1f7b81f4a31c5
Daniela Arcese <darcese@mozilla.com>
2019-10-17 13:24:38 -0400

    Fix #5551 - Small refactor

--- Extensions/Today/TodayViewController.swift
+++ Extensions/Today/TodayViewController.swift
@@ -119,12 +119,12 @@ class TodayViewController: UIViewController, NCWidgetProviding {
 
         let widgetView: UIView!
         self.extensionContext?.widgetLargestAvailableDisplayMode = .compact
+        let effectView: UIVisualEffectView
         if #available(iOS 13, *) {
-            let vibrancyEffect = UIVibrancyEffect.widgetEffect(forVibrancyStyle: .label)
+            effectView = UIVisualEffectView(effect: UIVibrancyEffect.widgetEffect(forVibrancyStyle: .label))
         } else {
-            let vibrancyEffect = UIVibrancyEffect.widgetPrimary()
+            effectView = UIVisualEffectView(effect: UIVibrancyEffect.widgetPrimary())
         }
-        let effectView = UIVisualEffectView(effect: vibrancyEffect)
         self.view.addSubview(effectView)
         effectView.snp.makeConstraints { make in
             make.edges.equalTo(self.view)
