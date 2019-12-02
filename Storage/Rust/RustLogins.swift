4afcbd6c7e7f268dcd6422ccc269fe7c0d98bc50
Garvan Keeley <garvankeeley@gmail.com>
2019-12-02 11:53:46 -0500

    Fix #5597: if sync is running, don't schedule a bg task

--- Storage/Rust/RustLogins.swift
+++ Storage/Rust/RustLogins.swift
@@ -157,16 +157,18 @@ public class RustLogins {
         return error
     }
 
-    public func forceClose() -> NSError? {
-        var error: NSError?
-
+    public func interrupt() {
         do {
             try storage.interrupt()
         } catch let err as NSError {
-            error = err
-
             Sentry.shared.sendWithStacktrace(message: "Error interrupting Logins database", tag: SentryTag.rustLogins, severity: .error, description: err.localizedDescription)
         }
+    }
+
+    public func forceClose() -> NSError? {
+        var error: NSError?
+
+        interrupt()
 
         queue.sync {
             guard isOpen else { return }
