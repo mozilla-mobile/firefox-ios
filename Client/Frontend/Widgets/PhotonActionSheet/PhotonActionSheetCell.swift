fb19045d8159eee51296a61ca6be2f86ea68ba60
Andrea Perić <andreaperic007@gmail.com>
2020-01-22 11:51:44 +0100

    Photon removed

--- Client/Frontend/Widgets/PhotonActionSheet/PhotonActionSheetCell.swift
+++ Client/Frontend/Widgets/PhotonActionSheet/PhotonActionSheetCell.swift
@@ -91,7 +91,7 @@ class PhotonActionSheetCell: UITableViewCell {
         func setOn(_ on: Bool) {
             foreground.image = on ? UIImage(named: "menu-customswitch-on") : UIImage(named: "menu-customswitch-off")
             mainView.accessibilityIdentifier = on ? "enabled" : "disabled"
-            mainView.tintColor = on ? UIColor.theme.general.controlTint : UIColor.Photon.theme.general.switchToggle }
+            mainView.tintColor = on ? UIColor.theme.general.controlTint : UIColor.theme.general.switchToggle }
     }
 
     let toggleSwitch = ToggleSwitch()
