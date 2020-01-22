46d555ea6b46b9e4a1dae80603cabee1a6d10f6d
Nishant Bhasin <nikieme3@gmail.com>
2020-01-22 15:36:25 -0500

    Feature #5697 - Cover Sheet for Firefox Updates  (#6037)
    * Feature #5697 - Cover Sheet for Firefox Updates
    
    * Added UpdateViewController
    
    * Added more constraints
    
    * Cleanup
    
    * General cleanup
    
    * - Refactored code
    
    * Cleanup
    
    * Cleanup
    
    * - cleanup
    
    * Removed unwanted debug code
    
    * Minor refactoring

---
+++ Client/Frontend/Update/UpdateCoverSheetTableViewCell.swift
@@ -0,0 +1,68 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import Foundation
+import Shared
+
+struct UpdateCoverSheetTableViewCellUX {
+    struct ImageView {
+        static let paddingTop = 2
+        static let paddingLeft = 18
+        static let height = 30
+    }
+    
+    struct DescriptionLabel {
+        static let paddingTop = 2
+        static let paddingTrailing = 18
+        static let bottom = -10
+        static let leading = 10
+    }
+}
+    
+class UpdateCoverSheetTableViewCell: UITableViewCell {
+    // Tableview cell items
+    var updateCoverSheetCellImageView: UIImageView = {
+        let imgView = UIImageView(image: #imageLiteral(resourceName: "darkModeUpdate"))
+        imgView.contentMode = .scaleAspectFit
+        imgView.clipsToBounds = true
+        return imgView
+    }()
+    var coverSheetCellDescriptionLabel: UILabel = {
+        let label = UILabel()
+        label.text = Strings.CoverSheetV22DarkModeTitle
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 20, weight: .regular)
+        label.textAlignment = .left
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        initialViewSetup()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func initialViewSetup() {
+        self.contentView.backgroundColor = .white
+        self.selectionStyle = .none
+        addSubview(updateCoverSheetCellImageView)
+        addSubview(coverSheetCellDescriptionLabel)
+        updateCoverSheetCellImageView.snp.makeConstraints { make in
+            make.left.equalToSuperview().inset(UpdateCoverSheetTableViewCellUX.ImageView.paddingLeft)
+            make.height.width.equalTo(UpdateCoverSheetTableViewCellUX.ImageView.height)
+            make.top.equalToSuperview().offset(UpdateCoverSheetTableViewCellUX.ImageView.paddingTop)
+        }
+        
+        coverSheetCellDescriptionLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(UpdateCoverSheetTableViewCellUX.DescriptionLabel.paddingTop)
+            make.trailing.equalToSuperview().inset(UpdateCoverSheetTableViewCellUX.DescriptionLabel.paddingTrailing)
+            make.bottom.equalTo(snp.bottom).offset(UpdateCoverSheetTableViewCellUX.DescriptionLabel.bottom)
+            make.leading.equalTo(updateCoverSheetCellImageView.snp.trailing).offset(UpdateCoverSheetTableViewCellUX.DescriptionLabel.leading)
+        }
+    }
+}
---
+++ Client/Frontend/Update/UpdateViewController.swift
@@ -0,0 +1,236 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import Foundation
+import UIKit
+import SnapKit
+import Shared
+import Leanplum
+
+// Update view UX constants
+struct UpdateViewControllerUX {
+    struct DoneButton {
+        static let paddingTop = 20
+        static let paddingRight = 20
+        static let height = 20
+    }
+     
+    struct ImageView {
+        static let paddingTop = 50
+        static let paddingLeft = 18
+        static let height = 70
+    }
+    
+    struct TitleLabel {
+        static let paddingTop = 15
+        static let paddingLeft = 18
+        static let height = 40
+    }
+    
+    struct MidTableView {
+        static let cellIdentifier = "UpdatedCoverSheetTableViewCellIdentifier"
+        static let paddingTop = 20
+        static let paddingBottom = -10
+    }
+    
+    struct StartBrowsingButton {
+        static let colour = UIColor.Photon.Blue50
+        static let cornerRadius:CGFloat = 10
+        static let font = UIFont.systemFont(ofSize: 18, weight: .semibold)
+        static let height = 46
+        static let edgeInset = 18
+    }
+}
+
+/* The layout for update view controller.
+    
+ |----------------|
+ |            Done|
+ |Image           | (Top View)
+ |                |
+ |Title Multiline |
+ |----------------|
+ |(TableView)     |
+ |                |
+ | [img] Descp.   | (Mid View)
+ |                |
+ | [img] Descp.   |
+ |                |
+ |                |
+ |----------------|
+ |                |
+ |                |
+ |    [Button]    | (Bottom View)
+ |----------------|
+ 
+ */
+
+class UpdateViewController: UIViewController {
+    // Private vars
+    let viewModel:UpdateViewModel = UpdateViewModel()
+    private var updatesTableView: UITableView = {
+        let tableView = UITableView(frame: CGRect.zero, style: .grouped)
+        tableView.register(UpdateCoverSheetTableViewCell.self, forCellReuseIdentifier: UpdateViewControllerUX.MidTableView.cellIdentifier)
+        tableView.backgroundColor = UIColor.green
+        tableView.separatorStyle = .none
+        tableView.sectionHeaderHeight = 0
+        tableView.sectionFooterHeight = 0
+        tableView.rowHeight = UITableView.automaticDimension
+        return tableView
+    }()
+    private lazy var titleImageView: UIImageView = {
+        let imgView = UIImageView(image: viewModel.updateCoverSheetModel?.titleImage)
+        imgView.contentMode = .scaleAspectFit
+        imgView.clipsToBounds = true
+        return imgView
+    }()
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = viewModel.updateCoverSheetModel?.titleText
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 34)
+        label.textAlignment = .left
+        label.numberOfLines = 0
+        return label
+    }()
+    private var doneButton: UIButton = {
+        let button = UIButton()
+        button.setTitle(Strings.SettingsSearchDoneButton, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .regular)
+        button.setTitleColor(UIColor.systemBlue, for: .normal)
+        return button
+    }()
+    private var startBrowsingButton: UIButton = {
+        let button = UIButton()
+        button.setTitle(Strings.StartBrowsingButtonTitle, for: .normal)
+        button.titleLabel?.font = UpdateViewControllerUX.StartBrowsingButton.font
+        button.layer.cornerRadius = UpdateViewControllerUX.StartBrowsingButton.cornerRadius
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.backgroundColor = UpdateViewControllerUX.StartBrowsingButton.colour
+        return button
+    }()
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initialViewSetup()
+        setupTopView()
+        setupMidView()
+        setupBottomView()
+    }
+    
+    private func initialViewSetup() {
+        self.view.backgroundColor = UIColor.white
+        
+        // Initialize
+        self.view.addSubview(doneButton)
+        self.view.addSubview(titleImageView)
+        self.view.addSubview(titleLabel)
+        self.view.addSubview(startBrowsingButton)
+        self.view.addSubview(updatesTableView)
+    }
+    
+    private func setupTopView() {
+        // Done button target setup
+        doneButton.addTarget(self, action: #selector(dismissAnimated), for: .touchUpInside)
+        
+        // Done button constraints setup
+        // This button is located at top right hence top, right and height
+        doneButton.snp.makeConstraints { make in
+            make.top.equalTo(view.snp.topMargin).offset(UpdateViewControllerUX.DoneButton.paddingTop)
+            make.right.equalToSuperview().inset(UpdateViewControllerUX.DoneButton.paddingRight)
+            make.height.equalTo(UpdateViewControllerUX.DoneButton.height)
+        }
+        
+        // The top imageview constraints setup
+        // This imageview is located at the top left of the view hence top, left, height, width
+        titleImageView.snp.makeConstraints { make in
+            make.left.equalToSuperview().inset(UpdateViewControllerUX.ImageView.paddingLeft)
+            make.top.equalTo(view.snp.topMargin).inset(UpdateViewControllerUX.ImageView.paddingTop)
+            make.height.width.equalTo(UpdateViewControllerUX.ImageView.height)
+        }
+        
+        // Top title label constraints setup
+        // This is the bigger tittle that is located right below the top image hence left, right, height and top (relating to imageview)
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleImageView.snp.bottom).offset(UpdateViewControllerUX.TitleLabel.paddingTop)
+            make.left.equalToSuperview().inset(UpdateViewControllerUX.TitleLabel.paddingLeft)
+            make.right.equalToSuperview()
+            make.height.equalTo(UpdateViewControllerUX.TitleLabel.height)
+        }
+    }
+    
+    private func setupMidView() {
+        // Mid tableview setup
+        // Mid tableview hosts the items for updated cover sheet
+        self.updatesTableView.delegate = self
+        self.updatesTableView.dataSource = self
+        // Mid tableview constraints
+        // The tableview sits b/w top and bottom view hence top, bottom constraints with equal width of the superview
+        self.updatesTableView.snp.makeConstraints({ make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(UpdateViewControllerUX.MidTableView.paddingTop)
+            make.bottom.equalTo(startBrowsingButton.snp.top).offset(UpdateViewControllerUX.MidTableView.paddingBottom)
+            make.width.equalToSuperview()
+            make.centerX.equalToSuperview()
+        })
+    }
+    
+    private func setupBottomView() {
+        // Bottom start browsing target setup
+        startBrowsingButton.addTarget(self, action: #selector(startBrowsing), for: .touchUpInside)
+        
+        // Bottom start button constraints
+        // Bottom start button sits at the bottom of the screen with some padding on left and right hence left, right, bottom, height
+        startBrowsingButton.snp.makeConstraints { make in
+            make.left.right.equalToSuperview().inset(UpdateViewControllerUX.StartBrowsingButton.edgeInset)
+            let h = view.frame.height
+            // On large iPhone screens, bump this up from the bottom
+            let offset: CGFloat = UIDevice.current.userInterfaceIdiom == .pad ? 20 : (h > 800 ? 60 : 20)
+            make.bottom.equalToSuperview().inset(offset)
+            make.height.equalTo(UpdateViewControllerUX.StartBrowsingButton.height)
+        }
+    }
+    
+    // Button Actions
+    @objc private func dismissAnimated() {
+        self.dismiss(animated: true, completion: nil)
+        LeanPlumClient.shared.track(event: .dismissedUpdateCoverSheet)
+        UnifiedTelemetry.recordEvent(category: .action, method: .press, object: .dismissedUpdateCoverSheet)
+    }
+    
+    @objc private func startBrowsing() {
+        viewModel.shouldStartBrowsing?()
+        LeanPlumClient.shared.track(event: .dismissUpdateCoverSheetAndStartBrowsing)
+        UnifiedTelemetry.recordEvent(category: .action, method: .press, object: .dismissUpdateCoverSheetAndStartBrowsing)
+    }
+}
+
+extension UpdateViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return CGFloat.leastNormalMagnitude
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return CGFloat.leastNormalMagnitude
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.updates.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: UpdateViewControllerUX.MidTableView.cellIdentifier, for: indexPath) as? UpdateCoverSheetTableViewCell
+        let currentLastItem = viewModel.updates[indexPath.row]
+        cell?.coverSheetCellDescriptionLabel.text = currentLastItem.updateText
+        cell?.updateCoverSheetCellImageView.image = currentLastItem.updateImage
+        return cell!
+    }
+}
