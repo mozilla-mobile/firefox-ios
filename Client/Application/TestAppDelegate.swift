f63c6b371f5ae2444f931ad7ef4d2b8a0ee83cc1
isabelrios <isabelrios@gmail.com>
2020-01-23 17:15:03 +0100

    Fix #6044 - XCUITests Skip Whats New Page

--- Client/Application/TestAppDelegate.swift
+++ Client/Application/TestAppDelegate.swift
@@ -62,7 +62,7 @@ class TestAppDelegate: AppDelegate {
 
         // Don't show the What's New page.
         if launchArguments.contains(LaunchArguments.SkipWhatsNew) {
-            profile.prefs.setString(AppInfo.appVersion, forKey: LatestAppVersionProfileKey)
+            profile.prefs.setInt(1, forKey: LatestAppVersionProfileKey)
         }
 
         // Skip the intro when requested by for example tests or automation
