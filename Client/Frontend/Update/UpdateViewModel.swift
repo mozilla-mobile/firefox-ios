8b90e5a4f4c413ced789ea9c273a29d3b906a2cf
Nishant Bhasin <nikieme3@gmail.com>
2020-02-04 17:32:38 -0500

    Feature #5697 - Added tests for Update coversheet viewmodel

--- Client/Frontend/Update/UpdateViewModel.swift
+++ Client/Frontend/Update/UpdateViewModel.swift
@@ -6,7 +6,7 @@ import Foundation
 import Shared
 
 class UpdateViewModel {
-    //  Internal vars
+    // Internal vars
     var updateCoverSheetModel: UpdateCoverSheetModel?
     var shouldStartBrowsing: (() -> Void)?
     
