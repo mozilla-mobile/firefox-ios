d3cafd6536485103b132eb8bd9a9e679a0488866
Garvan Keeley <garvankeeley@gmail.com>
2020-05-08 15:11:50 -0400

    Fix #6575: debug setting to toggle china edition of app

--- Client/Frontend/Settings/AppSettingsTableViewController.swift
+++ Client/Frontend/Settings/AppSettingsTableViewController.swift
@@ -136,6 +136,7 @@ class AppSettingsTableViewController: SettingsTableViewController {
                 SlowTheDatabase(settings: self),
                 ForgetSyncAuthStateDebugSetting(settings: self),
                 SentryIDSetting(settings: self),
+                ChangeToChinaSetting(settings: self)
             ])]
 
         return settings
