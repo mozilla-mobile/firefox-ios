ae8535eb387407adc04cad1b68ea50d7d4617e50
noorhashem <nour.hashem95@gmail.com>
2020-06-04 10:06:31 +0200

    Revert "removed spaces"
    This reverts commit 7ca7e93e9f12e68dfdebb0c24755fc7d0b83daa9.

--- Extensions/Today/TodayViewController.swift
+++ Extensions/Today/TodayViewController.swift
@@ -8,6 +8,8 @@ import Shared
 import SnapKit
 import XCGLogger
 
+
+
 private let log = Logger.browserLogger
 
 struct TodayStrings {
@@ -28,6 +30,7 @@ private struct TodayUX {
     static let privateSearchButtonColorThree = UIColor(red: 117.0/255.0, green: 41.0/255.0, blue: 167.0/255.0, alpha: 1.0)
     static let privateSearchButtonColorTwo = UIColor(red: 73.0/255.0, green: 46.0/255.0, blue: 133.0/255.0, alpha: 1.0)
     static let privateSearchButtonColorOne = UIColor(red: 56.0/255.0, green: 51.0/255.0, blue: 114.0/255.0, alpha: 1.0)
+
 }
 
 @objc (TodayViewController)
@@ -230,6 +233,7 @@ extension UIButton {
         gradientLayer.cornerRadius = self.frame.size.width/2
         layer.masksToBounds = true
         layer.insertSublayer(gradientLayer, below: self.imageView?.layer)
+
     }
 }
 
@@ -248,6 +252,7 @@ class ImageButtonWithLabel: UIView {
     }
 
     func performLayout() {
+        
         addSubview(button)
         addSubview(label)
         
@@ -257,6 +262,7 @@ class ImageButtonWithLabel: UIView {
             make.right.greaterThanOrEqualTo(self.safeAreaLayoutGuide).offset(30)
             make.left.greaterThanOrEqualTo(self.safeAreaLayoutGuide).inset(30)
             make.height.width.equalTo(60)
+            
         }
 
         label.snp.makeConstraints { make in
