a3ec31825ff47974369c2b71d680d3fda7a0b140
isabel rios <isabelrios@MBP-de-isabel-2.home>
2020-06-23 21:08:14 +0200

    remove old changes

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -40,9 +40,6 @@ fi
 
 # workaround, earlgrey needs to have dependencies downloaded before setup
 # https://github.com/google/EarlGrey/issues/732
-
-brew install carthage@0.34.0
-
 carthage checkout
 ./Carthage/Checkouts/EarlGrey/Scripts/setup-earlgrey.sh
 
