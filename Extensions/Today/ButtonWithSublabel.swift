07cf392478b696ba1233b058ec82bcb515c654fe
noorhashem <nour.hashem95@gmail.com>
2020-06-24 20:03:40 +0200

    edits to the constraints of the Button with subtitle to new constraints

--- Extensions/Today/ButtonWithSublabel.swift
+++ Extensions/Today/ButtonWithSublabel.swift
@@ -6,52 +6,52 @@ import UIKit
 
 class ButtonWithSublabel: UIButton {
     lazy var subtitleLabel = UILabel()
-    lazy var label = UILabel()
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    convenience init() {
-        self.init(frame: .zero)
-    }
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        performLayout()
-    }
-    
-    fileprivate func performLayout() {
-        let titleLabel = self.label
-        
-        self.titleLabel?.removeFromSuperview()
-        addSubview(titleLabel)
-        
-        let imageView = self.imageView!
-        let subtitleLabel = self.subtitleLabel
-        subtitleLabel.textColor = UIColor.lightGray
-        self.addSubview(subtitleLabel)
-        
-        imageView.snp.makeConstraints { make in
-            make.centerY.left.equalTo(self)
-            make.width.equalTo(TodayUX.copyLinkImageWidth)
-        }
-        
-        titleLabel.snp.makeConstraints { make in
-            make.left.equalTo(imageView.snp.right).offset(TodayUX.margin)
-            make.trailing.top.equalTo(self)
-        }
-        
-        subtitleLabel.lineBreakMode = .byTruncatingTail
-        subtitleLabel.snp.makeConstraints { make in
-            make.bottom.equalTo(self)
-            make.top.equalTo(titleLabel.snp.bottom)
-            make.leading.trailing.equalTo(titleLabel)
-        }
-    }
-    
-    override func setTitle(_ text: String?, for state: UIControl.State) {
-        self.label.text = text
-        super.setTitle(text, for: state)
-    }
+     lazy var label = UILabel()
+
+     required init?(coder aDecoder: NSCoder) {
+         fatalError("init(coder:) has not been implemented")
+     }
+
+     convenience init() {
+         self.init(frame: .zero)
+     }
+
+     override init(frame: CGRect) {
+         super.init(frame: frame)
+         performLayout()
+     }
+
+     fileprivate func performLayout() {
+         let titleLabel = self.label
+         self.titleLabel?.removeFromSuperview()
+         addSubview(titleLabel)
+
+         let imageView = self.imageView!
+         let subtitleLabel = self.subtitleLabel
+         self.addSubview(subtitleLabel)
+
+         imageView.snp.makeConstraints { make in
+             make.centerY.left.equalTo(10)
+             make.width.equalTo(TodayUX.copyLinkImageWidth)
+         }
+
+         titleLabel.snp.makeConstraints { make in
+             make.left.equalTo(imageView.snp.right).offset(10)
+             make.trailing.top.equalTo(self)
+             make.height.greaterThanOrEqualTo(12)
+         }
+
+         subtitleLabel.lineBreakMode = .byTruncatingTail
+         subtitleLabel.snp.makeConstraints { make in
+             make.bottom.equalTo(self).inset(10)
+             make.top.equalTo(titleLabel.snp.bottom)
+             make.leading.trailing.equalTo(titleLabel)
+             make.height.greaterThanOrEqualTo(10)
+         }
+     }
+
+     override func setTitle(_ text: String?, for state: UIControl.State) {
+         self.label.text = text
+         super.setTitle(text, for: state)
+     }
 }
