3dd808c98edbd0ecf97ddc293d4e2abf8eca9e08
noorhashem <nour.hashem95@gmail.com>
2020-06-25 22:36:03 +0200

    removed gradient function for buttons (unneeded)

--- Extensions/Today/UIButtonExtensions.swift
+++ Extensions/Today/UIButtonExtensions.swift
@@ -5,20 +5,6 @@
 import UIKit
 
 extension UIButton {
-func performGradient(colorOne: UIColor, colorTwo: UIColor, colorThree: UIColor) {
-    let gradientLayer = CAGradientLayer()
-    gradientLayer.frame = self.frame
-    gradientLayer.colors = [colorOne.cgColor, colorTwo.cgColor, colorThree.cgColor]
-    gradientLayer.startPoint = CGPoint(x: 1.0, y: 0.0)
-    gradientLayer.endPoint = CGPoint(x: 0.0, y: 1.0)
-    gradientLayer.locations = [0.0, 0.5, 1.0]
-    gradientLayer.cornerRadius = self.frame.size.width/2
-    layer.masksToBounds = true
-    layer.insertSublayer(gradientLayer, below: self.imageView?.layer)
-    }
-}
-
-extension UIButton {
     func setBackgroundColor(_ color: UIColor, forState state: UIControl.State) {
         let colorView = UIView(frame: CGRect(width: 1, height: 1))
         colorView.backgroundColor = color
