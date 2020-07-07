0e3d1db779251c4099ffc59436559ebd12f65742
noorhashem <nour.hashem95@gmail.com>
2020-07-07 18:12:22 +0200

    fixed nits

--- Extensions/Today/ImageButtonWithLabel.swift
+++ Extensions/Today/ImageButtonWithLabel.swift
@@ -3,6 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import UIKit
+
 class ImageButtonWithLabel: UIView {
     lazy var button = UIButton()
     lazy var label = UILabel()
