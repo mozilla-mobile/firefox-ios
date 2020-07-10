adf6201f6e546b69751b8c8f8cdf5f6b9a16b0ba
Aaron Train <aaron.train@gmail.com>
2020-07-10 09:33:23 -0400

    No issue: Dummy test

---
+++ XCUITests/DummyTest.swift
@@ -0,0 +1,14 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import XCTest
+
+class DummyTest: BaseTestCase {
+
+    func testExample() throws {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
