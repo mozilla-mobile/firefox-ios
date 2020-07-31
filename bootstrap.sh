8ee604e9cdcd4e2cf96b3e89d83cb42f3bbe2f0f
Garvan Keeley <garvankeeley@gmail.com>
2020-07-31 10:46:21 -0400

    Xcode Beta Carthage+lipo bug workaround

--- bootstrap.sh
+++ bootstrap.sh
@@ -25,6 +25,13 @@ if [ ! -z "$XCS_BOT_ID"  ]; then
   CARTHAGE_VERBOSE="--verbose"
 fi
 
+echo 'EXCLUDED_ARCHS__EFFECTIVE_PLATFORM_SUFFIX_simulator__NATIVE_ARCH_64_BIT_x86_64=arm64 arm64e armv7 armv7s armv6 armv8' > /tmp/tmp.xcconfig
+echo 'EXCLUDED_ARCHS=$(inherited) $(EXCLUDED_ARCHS__EFFECTIVE_PLATFORM_SUFFIX_$(EFFECTIVE_PLATFORM_SUFFIX)__NATIVE_ARCH_64_BIT_$(NATIVE_ARCH_64_BIT))' >> /tmp/tmp.xcconfig
+echo 'IPHONEOS_DEPLOYMENT_TARGET=11.4' >> /tmp/tmp.xcconfig
+echo 'SWIFT_TREAT_WARNINGS_AS_ERRORS=NO' >> /tmp/tmp.xcconfig
+echo 'GCC_TREAT_WARNINGS_AS_ERRORS=NO' >> /tmp/tmp.xcconfig
+export XCODE_XCCONFIG_FILE=/tmp/tmp.xcconfig
+
 carthage bootstrap $CARTHAGE_VERBOSE --platform ios --color auto --cache-builds
 
 # Install Node.js dependencies and build user scripts
