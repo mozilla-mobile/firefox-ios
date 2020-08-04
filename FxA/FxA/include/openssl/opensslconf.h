9d5bfa78a81fe9bb04412091567ec690af91f124
Garvan Keeley <garvankeeley@gmail.com>
2020-08-04 10:16:59 -0400

    [ios14,nobug,build] Fix opensslconf.h target detection

--- FxA/FxA/include/openssl/opensslconf.h
+++ FxA/FxA/include/openssl/opensslconf.h
@@ -8,19 +8,9 @@
 
 #include <TargetConditionals.h>
 
-#if TARGET_OS_IOS && TARGET_OS_SIMULATOR && TARGET_CPU_X86_64
+#if TARGET_OS_IOS && TARGET_OS_SIMULATOR
 # include <openssl/opensslconf_ios_x86_64.h>
-#elif TARGET_OS_IOS && TARGET_OS_SIMULATOR && TARGET_CPU_X86
-# include <openssl/opensslconf_ios_i386.h>
-#elif TARGET_OS_IOS && TARGET_OS_EMBEDDED && TARGET_CPU_ARM64
-# include <openssl/opensslconf_ios_arm64.h>
-#elif TARGET_OS_IOS && TARGET_OS_EMBEDDED && TARGET_CPU_ARM && defined(__ARM_ARCH_7S__)
-# include <openssl/opensslconf_ios_armv7s.h>
-#elif TARGET_OS_IOS && TARGET_OS_EMBEDDED && TARGET_CPU_ARM && !defined(__ARM_ARCH_7S__)
-# include <openssl/opensslconf_ios_armv7.h>
-#elif TARGET_OS_TV && TARGET_OS_SIMULATOR && TARGET_CPU_X86_64
-# include <openssl/opensslconf_tvos_x86_64.h>
-#elif TARGET_OS_TV && TARGET_OS_EMBEDDED && TARGET_CPU_ARM64
+#elif
 # include <openssl/opensslconf_tvos_arm64.h>
 #else
 # error Unable to determine target or target not included in OpenSSL build
