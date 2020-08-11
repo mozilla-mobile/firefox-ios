5b1060c5259885b6f3c7be8bb94f48615ac055db
noorhashem <nour.hashem95@gmail.com>
2020-08-11 02:17:44 +0200

    adding license

--- WidgetKit/WidgetKit.swift
+++ WidgetKit/WidgetKit.swift
@@ -1,10 +1,6 @@
-//
-//  WidgetKit.swift
-//  WidgetKit
-//
-//  Created by McNoor's  on 8/11/20.
-//  Copyright © 2020 Mozilla. All rights reserved.
-//
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import WidgetKit
 import SwiftUI
