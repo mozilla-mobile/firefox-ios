38cb56888bd88f2d6b2c16bc80ec365018c6ca92
Garvan Keeley <garvankeeley@gmail.com>
2020-08-25 13:22:21 -0400

    Remove adjust framework everywhere.

--- buddybuild_prebuild.sh
+++ buddybuild_prebuild.sh
@@ -1,23 +1,6 @@
 #!/usr/bin/env bash
 
 bundle install
-bundle exec danger --fail-on-errors=false
-
-#
-# Add our Adjust keys to the build depending on the scheme. We use the sandbox for beta so
-# that we have some insight in beta usage.
-#
-
-if [ "$BUDDYBUILD_SCHEME" == FirefoxBeta ]; then
-  echo "Setting Adjust environment to SANDBOX for $BUDDYBUILD_SCHEME"
-  /usr/libexec/PlistBuddy -c "Set AdjustAppToken $ADJUST_KEY_BETA" "Client/Info.plist"
-  /usr/libexec/PlistBuddy -c "Set AdjustEnvironment production" "Client/Info.plist"
-elif [ "$BUDDYBUILD_SCHEME" == Firefox ]; then
-  echo "Setting Adjust environment to PRODUCTION for $BUDDYBUILD_SCHEME"
-  /usr/libexec/PlistBuddy -c "Set AdjustAppToken $ADJUST_KEY_PRODUCTION" "Client/Info.plist"
-  /usr/libexec/PlistBuddy -c "Set AdjustEnvironment production" "Client/Info.plist"
-fi
-
 #
 # Leanplum is included only for the Firefox and FirefoxBeta builds.
 #
