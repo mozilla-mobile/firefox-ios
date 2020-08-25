c525bf6c3d94ccdc0f35e3a83824565d80108db1
Nishant Bhasin <nikieme3@gmail.com>
2020-08-25 16:35:08 -0400

    FXIOS-805 ⁃ Default browser change, remove adjust, remove photopicker string (#7215)
    * Removed NSPhotoLibraryUsageDescription for iOS14 default browser support (#7173)
    * Remove universal links (#7171)
    * Add http and https CFBundleURLSchemes to client info.plist (#7142)
    
    Co-authored-by: Kayla Galway <kayla.galway@gmail.com>
    Co-authored-by: Garvan Keeley <garvankeeley+github@gmail.com>
    Co-authored-by: Garvan Keeley <garvankeeley@gmail.com>

--- Client/Application/AppDelegate.swift
+++ Client/Application/AppDelegate.swift
@@ -37,7 +37,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UIViewControllerRestorati
     var rootViewController: UIViewController!
     weak var profile: Profile?
     var tabManager: TabManager!
-    var adjustIntegration: AdjustIntegration?
     var applicationCleanlyBackgrounded = true
     var shutdownWebServer: DispatchSourceTimer?
 
@@ -134,8 +133,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UIViewControllerRestorati
             }
         }
 
-        adjustIntegration = AdjustIntegration(profile: profile)
-
         self.updateAuthenticationInfo()
         SystemUtils.onFirstRun()
 
@@ -196,8 +193,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UIViewControllerRestorati
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         var shouldPerformAdditionalDelegateHandling = true
-
-        adjustIntegration?.triggerApplicationDidFinishLaunchingWithOptions(launchOptions)
 
         UIScrollView.doBadSwizzleStuff()
 
@@ -499,7 +494,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UIViewControllerRestorati
 
             // Check for fxa sign-in code and launch the login screen directly
             if query["signin"] != nil {
-                bvc.launchFxAFromDeeplinkURL(url)
+                // bvc.launchFxAFromDeeplinkURL(url) // Was using Adjust. Consider hooking up again when replacement system in-place.
                 return true
             }
 
