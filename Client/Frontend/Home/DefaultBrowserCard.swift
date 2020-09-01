df67bf714a71987611793cdd2152e1f30e8a3e9f
Daniela Arcese <darcese@mozilla.com>
2020-09-01 12:39:35 -0400

    Leanplum tracking

--- Client/Frontend/Home/DefaultBrowserCard.swift
+++ Client/Frontend/Home/DefaultBrowserCard.swift
@@ -128,10 +128,14 @@ class DefaultBrowserCard: UIView {
     @objc private func dismissCard() {
         self.dismissClosure?()
         UserDefaults.standard.set(true, forKey: "DidDismissDefaultBrowserCard")
+        TelemetryWrapper.gleanRecordEvent(category: .action, method: .tap, object: .dismissDefaultBrowserCard)
+        LeanPlumClient.shared.track(event: .dismissDefaultBrowserCard)
     }
     
     @objc private func showSettings() {
         UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!, options: [:])
+        TelemetryWrapper.gleanRecordEvent(category: .action, method: .tap, object: .goToSettingsDefaultBrowserCard)
+        LeanPlumClient.shared.track(event: .goToSettingsDefaultBrowserCard)
     }
     
     func applyTheme() {
