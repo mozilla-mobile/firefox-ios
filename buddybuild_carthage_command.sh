3b8a422bcb44dfece086d672c908d592b7248c0e
Garvan Keeley <garvankeeley+github@gmail.com>
2020-09-08 18:30:18 -0400

    Update buddybuild_carthage_command.sh

--- buddybuild_carthage_command.sh
+++ buddybuild_carthage_command.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-carthage bootstrap $CARTHAGE_VERBOSE --platform ios --color auto --cache-builds
-
-# Workaround to Carthage issue with latest version 0.35.0
-# https://github.com/Carthage/Carthage/issues/3003
-# Issue still open but error on BB carthage formula file from an arbitrary URL is disabled!
-# brew uninstall --force carthage
-# brew install https://github.com/Homebrew/homebrew-core/raw/09ceff6c1de7ebbfedb42c0941a48bfdca932c0f/Formula/carthage.rb
-
 carthage version
-
 ./carthage_command.sh
