de1c6b8d26c42a487b76fc082aa26f98670193e9
Garvan Keeley <garvankeeley+github@gmail.com>
2020-09-08 10:39:43 -0400

    [nobug, build] Fix carthage_command.sh detection of xcode 12

--- carthage_command.sh
+++ carthage_command.sh
@@ -1,6 +1,6 @@
-xcodevers=`xcodebuild -scheme Firefox  -showBuildSettings  | grep -i 'SDK_VERSION =' | sed 's/[ ]*SDK_VERSION = //'`
+xcodevers=`xcodebuild -scheme Firefox  -showBuildSettings  | grep -i 'SDK_VERSION =' | sed 's/[ ]*SDK_VERSION = //' | awk NF=1 FPAT=..`
 echo Xcode SDK: "$xcodevers"
-if [[ "$xcodevers" != "13.6" ]]; then
+if [[ "$xcodevers" != "13" ]]; then
   echo XCode 12 version detected! ••• Please ensure this is correct. ••• 
 echo 'EXCLUDED_ARCHS__EFFECTIVE_PLATFORM_SUFFIX_simulator__NATIVE_ARCH_64_BIT_x86_64=arm64 arm64e armv7 armv7s armv6 armv8' > /tmp/tmp.xcconfig
 echo 'EXCLUDED_ARCHS=$(inherited) $(EXCLUDED_ARCHS__EFFECTIVE_PLATFORM_SUFFIX_$(EFFECTIVE_PLATFORM_SUFFIX)__NATIVE_ARCH_64_BIT_$(NATIVE_ARCH_64_BIT))' >> /tmp/tmp.xcconfig
