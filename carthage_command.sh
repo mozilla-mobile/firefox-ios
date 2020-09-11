3510c6ff95ff2308d14cff4e32449732d709d3b7
Garvan Keeley <garvankeeley+github@gmail.com>
2020-09-11 17:19:05 -0400

    FXIOS-879 ⁃ [nobug, build] carthage: use 0.34.0 because of cross-volume bug on BB (#7294)
    * [nobug, build] carthage: use 0.34.0 because of cross-volume bug on BB with 0.35.0
    
    * Update carthage_command.sh

--- carthage_command.sh
+++ carthage_command.sh
@@ -1,3 +1,10 @@
+# use 0.34.0 because of cross-volume bug with 0.35.0 on BuddyBuild
+brew uninstall carthage
+wget https://github.com/Carthage/Carthage/releases/download/0.34.0/Carthage.pkg
+installer -pkg Carthage.pkg -target CurrentUserHomeDirectory
+cd /usr/local/bin && ln -s ~/usr/local/bin/carthage .
+cd -
+
 xcodesdk=`xcodebuild -scheme Firefox  -showBuildSettings  | grep -i 'SDK_VERSION =' | sed 's/[ ]*SDK_VERSION = //' | colrm 3`
 echo Xcode SDK: "$xcodesdk"
 if [[ "$xcodesdk" != "13" ]]; then
