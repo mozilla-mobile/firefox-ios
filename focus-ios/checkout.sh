3cc2a9600630c03bee72aee317dc8888918d35e2
Jeff Boek <jeff@jeffboek.com>
2020-09-28 11:47:24 -0700

    [focus-ios] Update Cartfile(s)

--- focus-ios/checkout.sh
+++ focus-ios/checkout.sh
@@ -4,9 +4,11 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-command -v swiftlint > /dev/null 2>&1 || { echo >&2 "swiftlint is not installed"; exit 1; }
-cp swiftlint.sh .git/hooks/pre-commit
+if [ "$1" == "--force" ]; then
+    rm -rf Carthage/*
+    rm -rf ~/Library/Caches/org.carthage.CarthageKit
+fi
 
-carthage bootstrap --platform iOS
+./carthage_command.sh
 
 (cd content-blocker-lib-ios/ContentBlockerGen && swift run)
