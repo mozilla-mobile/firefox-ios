c41be622e27d75fb49d59cb26bbaae3ea21987ad
Nishant Bhasin <nikieme3@gmail.com>
2020-10-01 10:23:41 -0400

    FXIOS-974 ⁃ Added l10n localization support (#7410)
    - we ignore .strings files as we don't want to bulk our repo with l10n strings
    - updated bootstrap.sh to support only locale fetching using --locale option and not fetch locales from repo when l10n directories already exist locally
    
    Co-authored-by: Garvan Keeley <garvankeeley+github@gmail.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,13 +10,37 @@
 # CI services where the Carthage directory can be cached.
 #
 # Use the --force option to force a rebuild of the dependencies.
+# Use the --locale option to fetch and update locales only
 #
 
+getLocale()
+{
+  echo "Getting locale..."
+  git clone https://github.com/mozilla-mobile/ios-l10n-scripts.git
+  ./ios-l10n-scripts/import-locales-firefox.sh
+}
+
+if [ "$1" == "--locale" ]; then
+  getLocale
+  exit 0
+fi
+
 if [ "$1" == "--force" ]; then
+    rm -rf firefoxios-l10n
+    rm -rf ios-l10n-scripts
     rm -rf Carthage/*
     rm -rf ~/Library/Caches/org.carthage.CarthageKit
 fi
 
+# Import locales
+if [ -d "/firefoxios-l10n" ] && [ -d "/ios-l10n-scripts" ]; then
+    echo "l10n directories found. Not downloading scripts."
+else
+    echo "l10n directory not found. Downloading repo and scripts."
+    getLocale
+fi
+
+# Run carthage
 ./carthage_command.sh
 
 # Install Node.js dependencies and build user scripts
