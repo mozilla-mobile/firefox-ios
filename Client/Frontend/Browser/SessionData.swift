3d7733eb9ca0b91d0052265b3af9de292db0baa4
Nishant Bhasin <nikieme3@gmail.com>
2020-10-08 14:22:14 -0400

    FXIOS-1037 ⁃ Fix #7449 - Restoring last tab is broken, shows white screen (#7477)
    - Have added back an important step to migrate internal urls without which we would have incorrect urls

--- Client/Frontend/Browser/SessionData.swift
+++ Client/Frontend/Browser/SessionData.swift
@@ -6,6 +6,37 @@ import Foundation
 
 import Shared
 
+/// PR: https://github.com/mozilla-mobile/firefox-ios/pull/4387
+/// Commit: https://github.com/mozilla-mobile/firefox-ios/commit/8b1450fbeb87f1f559a2f8e42971c715dc96bcaf
+/// InternalURL helps  encapsulate all internal scheme logic for urls rather than using URL extension. Extensions to built-in classes should be more minimal that what was being done previously.
+/// This migration was required mainly for above PR which is related to a PI request that reduces security risk. Also, this particular method helps in cleaning up / migrating old localhost:6571 URLs to internal: SessionData urls 
+private func migrate(urls: [URL]) -> [URL] {
+    return urls.compactMap { url in
+        var url = url
+        let port = AppInfo.webserverPort
+        [("http://localhost:\(port)/errors/error.html?url=", "\(InternalURL.baseUrl)/\(SessionRestoreHandler.path)?url=")
+            // TODO: handle reader pages ("http://localhost:6571/reader-mode/page?url=", "\(InternalScheme.url)/\(ReaderModeHandler.path)?url=")
+            ].forEach {
+            oldItem, newItem in
+            if url.absoluteString.hasPrefix(oldItem) {
+                var urlStr = url.absoluteString.replacingOccurrences(of: oldItem, with: newItem)
+                let comp = urlStr.components(separatedBy: newItem)
+                if comp.count > 2 {
+                    // get the last instance of incorrectly nested urls
+                    urlStr = newItem + (comp.last ?? "")
+                    assertionFailure("SessionData urls have nested internal links, investigate: [\(url.absoluteString)]")
+                }
+                url = URL(string: urlStr) ?? url
+            }
+        }
+
+        if let internalUrl = InternalURL(url), internalUrl.isAuthorized, let stripped = URL(string: internalUrl.stripAuthorization) {
+            return stripped
+        }
+
+        return url
+    }
+}
 
 class SessionData: NSObject, NSCoding {
     let currentPage: Int
@@ -30,7 +61,7 @@ class SessionData: NSObject, NSCoding {
     **/
     init(currentPage: Int, urls: [URL], lastUsedTime: Timestamp) {
         self.currentPage = currentPage
-        self.urls = urls
+        self.urls = migrate(urls: urls)
         self.lastUsedTime = lastUsedTime
 
         assert(urls.count > 0, "Session has at least one entry")
@@ -39,13 +70,13 @@ class SessionData: NSObject, NSCoding {
 
     required init?(coder: NSCoder) {
         self.currentPage = coder.decodeAsInt(forKey: "currentPage")
-        self.urls = (coder.decodeObject(forKey: "urls") as? [URL]) ?? [URL]()
+        self.urls = migrate(urls: coder.decodeObject(forKey: "urls") as? [URL] ?? [URL]())
         self.lastUsedTime = coder.decodeAsUInt64(forKey: "lastUsedTime")
     }
 
     func encode(with coder: NSCoder) {
         coder.encode(currentPage, forKey: "currentPage")
-        coder.encode(urls, forKey: "urls")
+        coder.encode(migrate(urls: urls), forKey: "urls")
         coder.encode(Int64(lastUsedTime), forKey: "lastUsedTime")
     }
 }
