e5822cf3ccb0cc029c7f1c10fac98ccb200893bb
isabelrios <isabelrios@gmail.com>
2020-10-20 10:57:42 +0200

    FXIOS-1069 ⁃ Issue7500-XCUIPerformanceTabsTestFollowUp (#7506)
    * Issue7500-XCUIPerformanceTabsTestFollowUp
    
    * fix reviewer comment

--- XCUITests/CronTabsPerfTests.swift
+++ XCUITests/CronTabsPerfTests.swift
@@ -2,7 +2,7 @@ import XCTest
 
 class CronTabsPerformanceTest: BaseTestCase {
 
-    let fixtures:[String:String] = ["testPerfTabs20": "tabsState20.archive", "testPerfTabs1280": "tabsState1280.archive", "testPerfTabs20tabTray":"tabsState20.archive", "testPerfTabs1280tabTray":"tabsState1280.archive"]
+    let fixtures:[String:String] = ["testPerfTabs20startup": "tabsState20.archive", "testPerfTabs20tabTray":"tabsState20.archive", "testPerfTabs1280startup": "tabsState1280.archive", "testPerfTabs1280tabTray":"tabsState1280.archive"]
 
     override func setUp() {
         // Test name looks like: "[Class testFunc]", parse out function name
@@ -24,13 +24,17 @@ class CronTabsPerformanceTest: BaseTestCase {
         super.tearDown()
     }
 
-    // TODO: 1 perf test per tabsStateArchive of size:
-    // 1, 10, 20, 40, 80, 160, 320, etc.(tab count)
-    
+    // This test run first to install the app in the device
+    func testAppStart() {
+        app.launch()
+    }
+
+    // 1 perf test per tabsStateArchive of size: 20, 1280
+    // Taking the edges, low and high load. For more values in the middle
+    // check the available archives
 
     func testPerfTabs20startup() {
         if #available(iOS 13.0, *) {
-
             measure(metrics: [
                 XCTMemoryMetric(),
                 XCTClockMetric(), // to measure timeClock Mon
@@ -45,7 +49,6 @@ class CronTabsPerformanceTest: BaseTestCase {
 
     func testPerfTabs1280startup() {
         if #available(iOS 13.0, *) {
-
             measure(metrics: [
                 XCTMemoryMetric(),
                 XCTClockMetric(), // to measure timeClock Mon
@@ -58,8 +61,7 @@ class CronTabsPerformanceTest: BaseTestCase {
         }
     }
 
-
-    func testPerfTabs1280() {
+    func testPerfTabs20tabTray() {
         if #available(iOS 13.0, *) {
             app.launch()
             measure(metrics: [
@@ -67,11 +69,26 @@ class CronTabsPerformanceTest: BaseTestCase {
                 XCTCPUMetric(), // to measure cpu cycles
                 XCTStorageMetric(), // to measure storage consuming
                 XCTMemoryMetric()]) {
-                // activity measurement here
+                // go to tab tray
+                navigator.goto(TabTray)
             }
         }
     }
     
+    func testPerfTabs1280tabTray() {
+        if #available(iOS 13.0, *) {
+            app.launch()
+            measure(metrics: [
+                XCTClockMetric(), // to measure timeClock Mon
+                XCTCPUMetric(), // to measure cpu cycles
+                XCTStorageMetric(), // to measure storage consuming
+                XCTMemoryMetric()]) {
+                // go to tab tray
+                navigator.goto(TabTray)
+            }
+        }
+    }
+
     // Additional Scenarios
     // 1. Start app
     // 2. Open tabs Tray
@@ -82,74 +99,38 @@ class CronTabsPerformanceTest: BaseTestCase {
     // 7. Switch back from private browsing
     // 8. Create new tab in private browsing
 
-    func testPerfTabs20tabTray() {
-
-        if #available(iOS 13.0, *) {
-            app.launch()
-            measure(metrics: [
-                XCTClockMetric(), // to measure timeClock Mon
-                XCTCPUMetric(), // to measure cpu cycles
-                XCTStorageMetric(), // to measure storage consuming
-                XCTMemoryMetric()]) {
-                // go to tab tray
-                //navigator.performAction(Action.TogglePrivateMode)
-                navigator.goto(TabTray)
-
-            }
-        }
-    }
-    
-    func testPerfTabs1280tabTray() {
-
-        if #available(iOS 13.0, *) {
-            app.launch()
-            measure(metrics: [
-                XCTClockMetric(), // to measure timeClock Mon
-                XCTCPUMetric(), // to measure cpu cycles
-                XCTStorageMetric(), // to measure storage consuming
-                XCTMemoryMetric()]) {
-                // go to tab tray
-                //navigator.performAction(Action.TogglePrivateMode)
-                navigator.goto(TabTray)
-
-            }
-        }
-    }
 
     // This can be used for generating new tabs
-//    func testTabs5Setup() {
-//        let archiveSize = 1080
-//        let fileName = "/Users/rpappalax/git/firefox-ios-TABS-PERF-WIP/Client/Assets/topdomains.txt"
-//        var contents = ""
-//        var urls = [String]()
-//
-//        do {
-//            contents = try String(contentsOfFile: fileName)
-//            urls = contents.components(separatedBy: "\n")
-//        } catch {
-//            print("COULDNT LOAD")
-//        }
-//        //navigator.performAction(Action.TogglePrivateMode)
-//        navigator.goto(NewTabScreen)
-//
-//        var counter = 0
-//        let topDomainsCount = urls.count
-//
-//        //for url in urls {
-//        for n in 0...archiveSize {
-//
-////            navigator.openNewURL(urlString: urls[n])
-////            waitForTabsButton()
-//            print(urls[counter])
-//
-//            if (counter >= archiveSize) {
-//                break
-//            }
-//            // if we don't have enough urls, start from the beginning of list
-//            if (counter >= (topDomainsCount - 1)) {
-//                counter = 0
-//            }
-//            counter += 1
-//        }
-//    }
+    /*
+    func testTabs5Setup() {
+        let archiveSize = 1080
+        let fileName = "/Users/rpappalax/git/firefox-ios-TABS-PERF-WIP/Client/Assets/topdomains.txt"
+        var contents = ""
+        var urls = [String]()
+
+        do {
+            contents = try String(contentsOfFile: fileName)
+            urls = contents.components(separatedBy: "\n")
+        } catch {
+            print("COULDNT LOAD")
+        }
+        navigator.goto(NewTabScreen)
+
+        var counter = 0
+        let topDomainsCount = urls.count
+
+        //for url in urls {
+        for n in 0...archiveSize {
+            print(urls[counter])
+
+            if (counter >= archiveSize) {
+                break
+            }
+            // if we don't have enough urls, start from the beginning of list
+            if (counter >= (topDomainsCount - 1)) {
+                counter = 0
+            }
+            counter += 1
+        }
+    }*/
 }
