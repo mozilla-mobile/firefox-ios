208eecd52a0c314da5b644efbb734cd85dc77963
Nishant Bhasin <nikieme3@gmail.com>
2020-11-26 16:36:22 -0500

    FXIOS-1228 ⁃ [chron tabs] Enhanced bottom sheet (#7666)
    - Supports top, mid and bottom state along with rotation and dynamic height for full screen 
    - Bottom sheet currently starts with full in landscape mode and also when we rotate due to small screen size
    - Tableview of more sheet is now scrollable in landscape mode 
    - Close more menu on share
    
    Co-authored-by: Daniela Arcese <darcese@mozilla.com>

--- Client/BottomSheet/BottomSheetViewController.swift
+++ Client/BottomSheet/BottomSheetViewController.swift
@@ -21,35 +21,50 @@ protocol BottomSheetDelegate {
 class BottomSheetViewController: UIViewController, Themeable {
     // Delegate
     var delegate: BottomSheetDelegate?
-    
-    // Orientation independent screen size
-    private let screenSize = DeviceInfo.screenSizeOrientationIndependent()
-    
-    // Bottom sheet location var
-    
-    // Shows how much bottom sheet should be visible
-    // 1 = full, 0.5 = half, 0 = hidden
-    private var heightSpecifier: CGFloat {
-        let height = screenSize.height
-        let heightForTallScreen: CGFloat = height > 850 ? 0.65 : 0.74
-        return height > 668 ? heightForTallScreen : 0.84
+    private var currentState: BottomSheetState = .none
+    private var isLandscape: Bool {
+        return UIApplication.shared.statusBarOrientation.isLandscape
     }
-    private lazy var maxY = view.frame.height - frameHeight
-    private lazy var minY = view.frame.height
+    private var orientationBasedHeight: CGFloat {
+        return isLandscape ? DeviceInfo.screenSizeOrientationIndependent().width : DeviceInfo.screenSizeOrientationIndependent().height
+    }
+    // shows how much bottom sheet should be visible
+    // 1 = full, 0.5 = half, 0 = hidden
+    // and for landscape we show 0.5 specifier just because of very small height
+    private var heightSpecifier: CGFloat {
+        let height = orientationBasedHeight
+        let heightForTallScreen: CGFloat = height > 850 ? 0.65 : 0.74
+        var specifier = height > 668 ? heightForTallScreen : 0.84
+        if isLandscape {
+            specifier = 0.5
+        }
+        return specifier
+    }
+    private var navHeight: CGFloat {
+        return navigationController?.navigationBar.frame.height ?? 0
+    }
+    private var fullHeight: CGFloat {
+        return orientationBasedHeight - navHeight
+    }
+    private var partialHeight: CGFloat {
+        return fullHeight * heightSpecifier
+    }
+    private var maxY: CGFloat {
+        return fullHeight - partialHeight
+    }
+    private var minY: CGFloat {
+        return orientationBasedHeight
+    }
     private var endedYVal: CGFloat = 0
     private var endedTranslationYVal: CGFloat = 0
-    private var isFullyHidden = false
-    private var frameHeight: CGFloat {
-        return view.frame.height * heightSpecifier
-    }
-    
+
     // Container child view controller
     var containerViewController: UIViewController?
     
     // Views
     private var overlay: UIView = {
         let view = UIView()
-        view.backgroundColor = UIColor.black.withAlphaComponent(0.2)
+        view.backgroundColor = UIColor.black.withAlphaComponent(0.50)
         return view
     }()
     private var panView: UIView = {
@@ -87,7 +102,7 @@ class BottomSheetViewController: UIViewController, Themeable {
             make.bottom.equalTo(self.view.safeArea.bottom)
             make.centerX.equalToSuperview()
             make.left.right.equalToSuperview()
-            make.height.equalTo(frameHeight)
+            make.height.equalTo(fullHeight)
         }
         
         let gesture = UIPanGestureRecognizer.init(target: self, action: #selector(panGesture))
@@ -108,59 +123,67 @@ class BottomSheetViewController: UIViewController, Themeable {
 
     // MARK: Bottomsheet swipe methods
     private func moveView(state: BottomSheetState) {
-        let yPosition = state == .none ? minY : maxY
-        panView.frame = CGRect(x: 0, y: yPosition, width: view.frame.width, height: frameHeight)
+        self.currentState = state
+        let yVal = state == .full ? navHeight : state == .partial ? maxY : minY
+        panView.frame = CGRect(x: 0, y: yVal, width: view.frame.width, height: fullHeight)
     }
-
+    
     private func moveView(panGestureRecognizer recognizer: UIPanGestureRecognizer) {
         let translation = recognizer.translation(in: view)
-        let yVal:CGFloat = translation.y
+        let yVal: CGFloat = translation.y
         let startedYVal = endedTranslationYVal + maxY
-        let newYVal = startedYVal + yVal
-        
-        // Top
-        guard newYVal >= maxY else {
+        let newYVal = currentState == .full ? navHeight + yVal : startedYVal + yVal
+        let downYShiftSpecifier: CGFloat = isLandscape ? 0.3 : 0.2
+
+        // top
+        guard newYVal >= navHeight else {
             endedTranslationYVal = 0
             return
         }
         
-        panView.frame = CGRect(x: 0, y: newYVal, width: view.frame.width, height: frameHeight)
-
+        // move the frame according to pan gesture
+        panView.frame = CGRect(x: 0, y: newYVal, width: view.frame.width, height: fullHeight)
+        
         if recognizer.state == .ended {
-            // past middle
-            if newYVal > (maxY - 80)*2 {
-                endedTranslationYVal = 0
-                hideView(shouldAnimate: true)
-                return
+            self.endedTranslationYVal = 0
+            // moving down
+            if newYVal > self.maxY {
+                // past middle
+                if newYVal > self.maxY + (self.partialHeight * downYShiftSpecifier) {
+                    hideView(shouldAnimate: true)
+                } else {
+                    self.moveView(state: .partial)
+                }
+            // moving up
+            } else if newYVal < self.maxY {
+                self.showFullView(shouldAnimate: true)
             }
-            
-            endedYVal = maxY + yVal
-            endedTranslationYVal = 0
-            
-            UIView.animate(withDuration: 0.1, delay: 0.0, options: [.allowUserInteraction], animations: {
-                let state: BottomSheetState = recognizer.velocity(in: self.view).y >= 0 ? .partial : .full
-                self.moveView(state: state)
-            }, completion: nil)
         }
     }
     
     @objc func hideView(shouldAnimate: Bool) {
-        delegate?.showBottomToolbar()
         let closure = {
             self.moveView(state: .none)
-            self.isFullyHidden = true
             self.view.isUserInteractionEnabled = true
-            self.overlay.alpha = 0
-            self.view.isHidden = true
         }
         guard shouldAnimate else {
             closure()
+            self.overlay.alpha = 0
+            self.view.isHidden = true
+            delegate?.showBottomToolbar()
             return
         }
         self.view.isUserInteractionEnabled = false
-        UIView.animate(withDuration: 0.4, animations: {
+        UIView.animate(withDuration: 0.25) {
             closure()
-        })
+            self.overlay.alpha = 0
+        } completion: { value in
+            if value {
+                self.view.isHidden = true
+                self.delegate?.showBottomToolbar()
+                self.containerViewController?.view.removeFromSuperview()
+            }
+        }
     }
 
     @objc func showView() {
@@ -168,10 +191,24 @@ class BottomSheetViewController: UIViewController, Themeable {
             panView.addSubview(container.view)
         }
         UIView.animate(withDuration: 0.26, animations: {
-            self.moveView(state: .full)
-            self.isFullyHidden = false
+            self.moveView(state: self.isLandscape ? .full : .partial)
             self.overlay.alpha = 1
             self.view.isHidden = false
+        })
+    }
+    
+    func showFullView(shouldAnimate: Bool) {
+        let closure = {
+            self.moveView(state: .full)
+            self.overlay.alpha = 1
+            self.view.isHidden = false
+        }
+        guard shouldAnimate else {
+            closure()
+            return
+        }
+        UIView.animate(withDuration: 0.26, animations: {
+            closure()
         })
     }
 
@@ -189,5 +226,24 @@ class BottomSheetViewController: UIViewController, Themeable {
         } else {
             panView.backgroundColor = UIColor(rgb: 0x1C1C1E)
         }
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        coordinator.animate(alongsideTransition: { (UIViewControllerTransitionCoordinatorContext) -> Void in
+            let orient = UIApplication.shared.statusBarOrientation
+            switch orient {
+            case .portrait:
+                self.moveView(state: .partial)
+            case .landscapeLeft, .landscapeRight:
+                self.moveView(state: .full)
+            default:
+                print("orientation not supported")
+            }
+        }, completion: { (UIViewControllerTransitionCoordinatorContext) -> Void in
+            self.view.setNeedsLayout()
+            self.view.layoutIfNeeded()
+            self.containerViewController?.view.setNeedsLayout()
+        })
     }
 }
