1d315b37040285153f16690156582c90111108fd
Cristian Gomez <cjgm23@gmail.com>
2021-01-04 16:37:08 -0500

    Fixes #6995 If URL schema is valid but empty don't ask to… (#7798)
    open in external app.

--- Shared/Extensions/URLExtensions.swift
+++ Shared/Extensions/URLExtensions.swift
@@ -286,7 +286,7 @@ extension URL {
      */
     public var schemeIsValid: Bool {
         guard let scheme = scheme else { return false }
-        return permanentURISchemes.contains(scheme.lowercased())
+        return permanentURISchemes.contains(scheme.lowercased()) && self.absoluteURL.absoluteString.lowercased() != scheme + ":"
     }
 
     public func havingRemovedAuthorisationComponents() -> URL {
