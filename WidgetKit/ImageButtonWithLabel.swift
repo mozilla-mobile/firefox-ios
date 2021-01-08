ef36d7c3a20db7b42a493ef6d23ab3df8c874e64
Cristian Gomez <c.gomez@gbh.com.do>
2021-01-08 00:27:56 -0500

    FXIOS-1281: closes #7722 widgets font size too large on medium search widget.
    Changed font size from .headline (17), to 13 as instructed.

--- WidgetKit/ImageButtonWithLabel.swift
+++ WidgetKit/ImageButtonWithLabel.swift
@@ -71,7 +71,7 @@ struct ImageButtonWithLabel: View {
                     HStack(alignment: .top) {
                         VStack(alignment: .leading){
                                 Text(link.label)
-                                    .font(.headline)
+                                    .font(.system(size: 13, weight: .semibold, design: .default))
                                     .minimumScaleFactor(0.75)
                                     .layoutPriority(1000)
                         }
