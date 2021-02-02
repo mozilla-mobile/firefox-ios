a78b86efa230dc5f1cda44fdf0cd5422316b99b8
Jeff Boek <jeff@jeffboek.com>
2021-02-02 11:39:53 -0800

    Updates scripts

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,8 +16,13 @@
 getLocale()
 {
   echo "Getting locale..."
-  git clone https://github.com/mozilla-mobile/ios-l10n-scripts.git
-  ./ios-l10n-scripts/import-locales-firefox.sh
+  git clone https://github.com/boek/ios-l10n-scripts.git -b new_tool || exit 1
+
+  echo "Creating firefoxios-l10n Git repo"
+  rm -rf firefoxios-l10n
+  git clone --depth 1 https://github.com/mozilla-l10n/firefoxios-l10n firefoxios-l10n || exit 1
+
+  ./ios-l10n-scripts/ios-l10n-tools --project-path Client.xcodeproj --l10n-project-path ./firefoxios-l10n --import
 }
 
 if [ "$1" == "--locale" ]; then
