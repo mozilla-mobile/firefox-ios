3d8952a30c5299de07e0b5f48f75af91efb99f7f
Jeff Boek <jeff@jeffboek.com>
2021-02-16 10:40:20 -0800

    Embed strings (#7948)
    * Adds strings to repository
    
    * Adds --importLocales flag to bootstrap
    
    * Imports strings
    
    * Updates documentation, uses of --locale

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,7 +10,7 @@
 # CI services where the Carthage directory can be cached.
 #
 # Use the --force option to force a rebuild of the dependencies.
-# Use the --locale option to fetch and update locales only
+# Use the --importLocales option to fetch and update locales only
 #
 
 getLocale()
@@ -21,14 +21,7 @@ getLocale()
   echo "Creating firefoxios-l10n Git repo"
   rm -rf firefoxios-l10n
   git clone --depth 1 https://github.com/mozilla-l10n/firefoxios-l10n firefoxios-l10n || exit 1
-
-  ./ios-l10n-scripts/ios-l10n-tools --project-path Client.xcodeproj --l10n-project-path ./firefoxios-l10n --import
 }
-
-if [ "$1" == "--locale" ]; then
-  getLocale
-  exit 0
-fi
 
 if [ "$1" == "--force" ]; then
     rm -rf firefoxios-l10n
@@ -37,12 +30,17 @@ if [ "$1" == "--force" ]; then
     rm -rf ~/Library/Caches/org.carthage.CarthageKit
 fi
 
-# Import locales
-if [ -d "/firefoxios-l10n" ] && [ -d "/ios-l10n-scripts" ]; then
-    echo "l10n directories found. Not downloading scripts."
-else
-    echo "l10n directory not found. Downloading repo and scripts."
-    getLocale
+if [ "$1" == "--importLocales" ]; then
+  # Import locales
+  if [ -d "/firefoxios-l10n" ] && [ -d "/ios-l10n-scripts" ]; then
+      echo "l10n directories found. Not downloading scripts."
+  else
+      echo "l10n directory not found. Downloading repo and scripts."
+      getLocale
+  fi
+
+  ./ios-l10n-scripts/ios-l10n-tools --project-path Client.xcodeproj --l10n-project-path ./firefoxios-l10n --import
+  exit 0
 fi
 
 # Run carthage
