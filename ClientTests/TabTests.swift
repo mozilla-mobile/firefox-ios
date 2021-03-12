a29827f19a055d6f92416a0de9877b986c2aa685
Jeff Boek <jeff@jeffboek.com>
2021-03-12 11:15:29 -0800

    For #7586 - Removes mobile prefixes when toggling desktop mode

---
+++ ClientTests/TabTests.swift
@@ -0,0 +1,21 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import XCTest
+@testable import Client
+
+class TabTests: XCTestCase {
+
+    func testWithoutMobilePrefixRemovesMobilePrefixes() {
+        let url = URL(string: "https://m.wikipedia.org/wiki/Firefox")!
+        let newUrl = url.withoutMobilePrefix()
+        XCTAssertEqual(newUrl.host, "wikipedia.org")
+    }
+
+    func testWithoutMobilePrefixRemovesMobile() {
+        let url = URL(string: "https://en.mobile.wikipedia.org/wiki/Firefox")!
+        let newUrl = url.withoutMobilePrefix()
+        XCTAssertEqual(newUrl.host, "en.wikipedia.org")
+    }
+}
