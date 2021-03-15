3bc65dd1e64081f74120856ed201fe63fff8c03a
Jeff Boek <jeff@jeffboek.com>
2021-03-15 13:04:34 -0700

    For #8058 - Fixes crash on versions of iOS with a WKWebView bug (#8146)

--- Shared/Extensions/WKWebViewExtensions.swift
+++ Shared/Extensions/WKWebViewExtensions.swift
@@ -18,7 +18,7 @@ extension WKWebView {
     ///     - javascript: String representing javascript to be evaluated
     public func evaluateJavascriptInDefaultContentWorld(_ javascript: String) {
         #if compiler(>=5.3)
-            if #available(iOS 14.0, *), USE_NEW_SANDBOX_APIS {
+            if #available(iOS 14.3, *), USE_NEW_SANDBOX_APIS {
                 self.evaluateJavaScript(javascript, in: nil, in: .defaultClient, completionHandler: { _ in })
             } else {
                 self.evaluateJavaScript(javascript)
@@ -36,7 +36,7 @@ extension WKWebView {
     ///     - completion: Tuple containing optional data and an optional error
     public func evaluateJavascriptInDefaultContentWorld(_ javascript: String,_ completion: @escaping ((Any?, Error?) -> Void)) {
         #if compiler(>=5.3)
-            if #available(iOS 14.0, *), USE_NEW_SANDBOX_APIS {
+            if #available(iOS 14.3, *), USE_NEW_SANDBOX_APIS {
                 self.evaluateJavaScript(javascript, in: nil, in: .defaultClient) { result in
                     switch result {
                     case .success(let value):
@@ -60,7 +60,7 @@ extension WKWebView {
 
 extension WKUserContentController {
     public func addInDefaultContentWorld(scriptMessageHandler: WKScriptMessageHandler, name: String) {
-        if #available(iOS 14.0, *), USE_NEW_SANDBOX_APIS {
+        if #available(iOS 14.3, *), USE_NEW_SANDBOX_APIS {
             add(scriptMessageHandler, contentWorld: .defaultClient, name: name)
         } else {
             add(scriptMessageHandler, name: name)
@@ -70,7 +70,7 @@ extension WKUserContentController {
 
 extension WKUserScript {
     public class func createInDefaultContentWorld(source: String, injectionTime: WKUserScriptInjectionTime, forMainFrameOnly: Bool) -> WKUserScript {
-        if #available(iOS 14.0, *), USE_NEW_SANDBOX_APIS {
+        if #available(iOS 14.3, *), USE_NEW_SANDBOX_APIS {
             return WKUserScript(source: source, injectionTime: injectionTime, forMainFrameOnly: forMainFrameOnly, in: .defaultClient)
         } else {
             return WKUserScript(source: source, injectionTime: injectionTime, forMainFrameOnly: forMainFrameOnly)
