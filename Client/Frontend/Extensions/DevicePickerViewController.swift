1ed201be5724280a1bb7b98506decfac601d0de1
Jeff Boek <jeff@jeffboek.com>
2021-03-25 13:02:21 -0700

    Upgrade application services: v74.0.1 and Glean v36.0.0 (#8219)
    * Update to application-services v73.0.0.
    
    This release includes new auto-generated Swift bindings for the FxA Client
    component, which come with a few breaking API changes. There should be
    no changes in functionality though.
    
    * Upgrade application services
    
    Co-authored-by: Ryan Kelly <ryan@rfk.id.au>

--- Client/Frontend/Extensions/DevicePickerViewController.swift
+++ Client/Frontend/Extensions/DevicePickerViewController.swift
@@ -101,7 +101,8 @@ class DevicePickerViewController: UITableViewController {
 
             self.devices = state.remoteDevices.map { d in
                 let t = "\(d.deviceType)"
-                return RemoteDevice(id: d.id, name: d.displayName, type: t, isCurrentDevice: d.isCurrentDevice, lastAccessTime: d.lastAccessTime, availableCommands: nil)
+                let lastAccessTime = d.lastAccessTime == nil ? nil : UInt64(clamping: d.lastAccessTime!)
+                return RemoteDevice(id: d.id, name: d.displayName, type: t, isCurrentDevice: d.isCurrentDevice, lastAccessTime: lastAccessTime, availableCommands: nil)
             }
 
             if self.devices.isEmpty {
