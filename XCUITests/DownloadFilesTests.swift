93e59188382995d2209be240dfbfb82416bd7d95
isabelrios <isabelrios@gmail.com>
2021-03-30 11:44:54 +0200

    wait for the page to be loaded

--- XCUITests/DownloadFilesTests.swift
+++ XCUITests/DownloadFilesTests.swift
@@ -137,6 +137,7 @@ class DownloadFilesTests: BaseTestCase {
 
     private func downloadBLOBFile() {
         navigator.openURL(testBLOBURL)
+        sleep(3)
         waitForExistence(app.webViews.links["Download Text"], timeout: 10)
         app.webViews.links["Download Text"].press(forDuration: 1)
         app.buttons["Download Link"].tap()
