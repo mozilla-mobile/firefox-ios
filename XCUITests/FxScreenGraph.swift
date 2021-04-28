4970e81033ef492ac5354ed6a6498c7cc9a6b2c8
Daniela Arcese <darcese@mozilla.com>
2021-04-28 16:00:26 -0400

    Fix #8404: Add Home back to menu

--- XCUITests/FxScreenGraph.swift
+++ XCUITests/FxScreenGraph.swift
@@ -1096,6 +1096,10 @@ func createScreenGraph(for test: XCTestCase, with app: XCUIApplication) -> MMScr
         screenState.tap(app.tables.cells["menu-NightMode"], forAction: Action.ToggleNightMode, transitionTo: BrowserTabMenu) { userState in
             userState.nightMode = !userState.nightMode
         }
+
+        screenState.tap(app.tables.cells["menu-Home"], forAction: Action.GoToHomePage) { userState in
+        }
+
         screenState.tap(app.tables.cells["whatsnew"], forAction: Action.OpenWhatsNewPage) { userState in
         }
 
