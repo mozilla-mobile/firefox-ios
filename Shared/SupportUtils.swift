d0d87b61b310856f72d1a11ae91c26bbf9336b60
Karl Dubost <karl+github@la-grange.net>
2021-06-01 12:32:02 +0900

    Issue #1905 - Adds identification of the device type
    The reporting URL will now send a device-type extra label.
    When the device is an iPad, it will become easier to classify
    for webcompat folks as the iPad usually sends a desktop UA.

--- Shared/SupportUtils.swift
+++ Shared/SupportUtils.swift
@@ -3,6 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import Foundation
+import UIKit
 
 /// Utility functions related to SUMO and Webcompat
 public struct SupportUtils {
@@ -26,10 +27,17 @@ public struct SupportUtils {
         /// It specifies the source as mobile-reporter. This helps the webcompat server to classify the issue.
         /// It also adds browser-firefox-ios to the labels in the URL to make it clear
         /// that this about Firefox on iOS. It makes it easier for webcompat people doing triage and diagnostics.
+        /// It adds a device-type label to help discriminating in between tablet and mobile devices.
+        let deviceType: String
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            deviceType = "device-tablet"
+        } else {
+            deviceType = "device-mobile"
+        }
         guard let escapedUrl = siteUrl?.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed)
         else {
             return nil
         }
-        return URL(string: "https://webcompat.com/issues/new?src=mobile-reporter&label=browser-firefox-ios&url=\(escapedUrl)")
+        return URL(string: "https://webcompat.com/issues/new?src=mobile-reporter&label=browser-firefox-ios&label=\(deviceType)&url=\(escapedUrl)")
     }
 }
