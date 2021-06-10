7f356d0b4544adeb484fc388f1867c619bcfe56a
Jeff Boek <jeff@jeffboek.com>
2021-06-10 10:55:31 -0700

    Fix #7608 - Leaves overlay mode when homepanel is scrolled (#8618)

--- Client/Frontend/Home/FirefoxHomeViewController.swift
+++ Client/Frontend/Home/FirefoxHomeViewController.swift
@@ -58,6 +58,7 @@ protocol HomePanelDelegate: AnyObject {
     func homePanelDidRequestToOpenInNewTab(_ url: URL, isPrivate: Bool)
     func homePanel(didSelectURL url: URL, visitType: VisitType, isGoogleTopSite: Bool)
     func homePanelDidRequestToOpenLibrary(panel: LibraryPanelType)
+    func homePanelDidScroll()
 }
 
 protocol HomePanel: Themeable {
@@ -379,6 +380,9 @@ extension FirefoxHomeViewController {
 
 // MARK: -  Tableview Delegate
 extension FirefoxHomeViewController: UICollectionViewDelegateFlowLayout {
+    override func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        homePanelDelegate?.homePanelDidScroll()
+    }
 
     override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
         switch kind {
