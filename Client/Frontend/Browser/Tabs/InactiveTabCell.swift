c9c286376479f3cabab1b93e6f5634bd5e424ca8
Nishant <nikieme3@gmail.com>
2021-06-28 22:12:25 -0400

    Mostly working view

--- Client/Frontend/Browser/Tabs/InactiveTabCell.swift
+++ Client/Frontend/Browser/Tabs/InactiveTabCell.swift
@@ -5,70 +5,19 @@
 import Foundation
 import SnapKit
 import UIKit
+import Shared
 
-//class InactiveTabCell: UICollectionViewCell, UITableViewDataSource, UITableViewDelegate {
-//    var inactiveTabsViewModel: InactiveViewModel?
-//    static let Identifier = "InactiveTabCellIdentifier"
-//    let InactiveTabsTableIdentifier = "InactiveTabsTableIdentifier"
-//
-//    var tableView = UITableView()
-//    let cellIdentifier: String = "tableCell"
-//    override init(frame: CGRect) {
-////        super.init(frame: frame)
-////        super.init(frame: .zero)
-//        super.init(frame: CGRect(width: 150, height: 150))
-//        tableView.frame = CGRect(width: 150, height: 150)
-//        addSubviews(tableView)
-//
-//
-//    }
-//
-//    init() {
-//        super.init(frame: CGRect(width: 150, height: 150))
-//        tableView.frame = CGRect(width: 150, height: 150)
-//        addSubviews(tableView)
-//    }
-//
-//    required init?(coder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-//
-//    override func layoutSubviews() {
-//        super.layoutSubviews()
-//        backgroundColor = .white
-//        tableView.delegate = self
-//        tableView.dataSource = self
-//        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
-//    }
-//
-//    func numberOfSections(in tableView: UITableView) -> Int {
-//        return 1
-//    }
-//
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return 4
-//    }
-//
-//    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-//        return UIView()
-//    }
-//
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//
-//        let cell: UITableViewCell = UITableViewCell(style: UITableViewCell.CellStyle.value1, reuseIdentifier: cellIdentifier)
-//
-//        cell.textLabel?.text = "1 CUP"
-//        cell.detailTextLabel?.text = "Whole"
-//
-//        return cell
-//    }
-//
-//    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
-//        return 40
-//    }
-//}
+enum InactiveTabSection: Int, CaseIterable {
+    case inactive
+    case recentlyClosed
 
-
+//    var footerText: String? {
+//        switch self {
+//        case .inactive: return nil
+//        case .recentlyClosed: return String.TabsTrayRecentlyClosedTabsDescritpion
+//        }
+//    }
+}
 
 class InactiveTabCell: UICollectionViewCell, Themeable, UITableViewDataSource, UITableViewDelegate {
     var inactiveTabsViewModel: InactiveViewModel?
@@ -77,19 +26,22 @@ class InactiveTabCell: UICollectionViewCell, Themeable, UITableViewDataSource, U
 
     // Views
     lazy var tableView: UITableView = {
-        let tableView = UITableView()
-        tableView.tableFooterView = UIView()
+        let tableView = UITableView(frame: .zero, style: .grouped)
         tableView.register(OneLineTableViewCell.self, forCellReuseIdentifier: InactiveTabsTableIdentifier)
-        tableView.allowsMultipleSelectionDuringEditing = true
+        tableView.allowsMultipleSelectionDuringEditing = false
+        tableView.sectionHeaderHeight = 0
+        tableView.sectionFooterHeight = 0
+        tableView.tableHeaderView = UIView(frame: CGRect(origin: .zero, size: CGSize(width: 0, height: CGFloat.leastNormalMagnitude)))
+        tableView.separatorInset = UIEdgeInsets(top: 0, left: 5, bottom: 0, right: 0)
+        tableView.isScrollEnabled = false
+//        tableView.tableFooterView = UIView(frame: CGRect(origin: .zero, size: CGSize(width: 0, height: CGFloat.leastNormalMagnitude)))
+//        tableView.style = .grouped
         return tableView
     }()
 
     convenience init(viewModel: InactiveViewModel) {
         self.init()
         inactiveTabsViewModel = viewModel
-//        addSubviews(tableView)
-//        setupConstraints()
-//        applyTheme()
     }
 
     override func layoutSubviews() {
@@ -125,23 +77,23 @@ class InactiveTabCell: UICollectionViewCell, Themeable, UITableViewDataSource, U
         tableView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
         }
-//        NSLayoutConstraint.activate([
-//            tableView.topAnchor.constraint(equalTo: self.topAnchor),
-//            tableView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
-//            tableView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
-//            tableView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
-////            tableView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
-//        ])
-//
         self.bringSubviewToFront(tableView)
     }
 
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return InactiveTabSection.allCases.count
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        inactiveTabsViewModel?.inactiveTabs.count ?? 0
+//        guard let count = inactiveTabsViewModel?.inactiveTabs.count else { return 0 }
+        switch InactiveTabSection(rawValue: section) {
+        case .inactive:
+            return inactiveTabsViewModel?.inactiveTabs.count ?? 0
+        case .recentlyClosed:
+            return 1
+        case .none:
+            return 0
+        }
     }
 
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -150,20 +102,65 @@ class InactiveTabCell: UICollectionViewCell, Themeable, UITableViewDataSource, U
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: InactiveTabsTableIdentifier, for: indexPath) as! OneLineTableViewCell
-
-        guard let tab = inactiveTabsViewModel?.inactiveTabs[indexPath.item] else {
+        switch InactiveTabSection(rawValue: indexPath.section) {
+        case .inactive:
+            guard let tab = inactiveTabsViewModel?.inactiveTabs[indexPath.item] else { return cell }
+            cell.backgroundColor = .clear
+            cell.accessoryView = nil
+            cell.titleLabel.text = tab.displayTitle
+            cell.leftImageView.setImageAndBackground(forIcon: tab.displayFavicon, website: getTabDomainUrl(tab: tab)) {}
+            cell.shouldLeftAlignTitle = false
+            cell.updateMidConstraint()
+            cell.accessoryType = .none
+            return cell
+        case .recentlyClosed:
+            cell.backgroundColor = .clear
+            cell.accessoryView = nil
+            cell.titleLabel.text = String.TabsTrayRecentlyCloseTabsSectionTitle
+            cell.leftImageView.image = nil
+            cell.shouldLeftAlignTitle = true
+            cell.updateMidConstraint()
+            cell.accessoryType = .disclosureIndicator
+            return cell
+        case .none:
             return cell
         }
-        cell.backgroundColor = .clear
-
-        cell.accessoryView = nil
-        cell.titleLabel.text = tab.displayTitle
-        cell.leftImageView.setImageAndBackground(forIcon: tab.displayFavicon, website: getTabDomainUrl(tab: tab)) {}
-        cell.accessoryType = .none
-        cell.editingAccessoryType = .disclosureIndicator
-        return cell
     }
 
+    func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+        switch InactiveTabSection(rawValue: section) {
+        case .inactive, .none:
+            return nil
+        case .recentlyClosed:
+            return String.TabsTrayRecentlyClosedTabsDescritpion
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        switch InactiveTabSection(rawValue: section) {
+        case .inactive, .none:
+            return CGFloat.leastNormalMagnitude
+        case .recentlyClosed:
+            return 45
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("\(indexPath)")
+    }
+//    
+//    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+//        return nil
+//    }
+//    
+//    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+//        return CGFloat.leastNormalMagnitude
+//    }
+//
+//    func tableView(_ tableView: UITableView, estimatedHeightForHeaderInSection section: Int) -> CGFloat {
+//        return CGFloat.leastNormalMagnitude
+//    }
+    
     func getTabDomainUrl(tab: Tab) -> URL? {
         guard tab.url != nil else {
             return tab.sessionData?.urls.last?.domainURL
@@ -173,5 +170,7 @@ class InactiveTabCell: UICollectionViewCell, Themeable, UITableViewDataSource, U
 
     func applyTheme() {
         self.backgroundColor = .clear
+        self.tableView.backgroundColor = .clear
+        tableView.reloadData()
     }
 }
