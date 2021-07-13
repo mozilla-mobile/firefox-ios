43f2b31cf6d1c6672e815e4019e15a2f2e900be7
Nishant Bhasin <nikieme3@gmail.com>
2021-07-12 23:24:30 -0400

    Fixing tests x2

--- ClientTests/TabManagerTests.swift
+++ ClientTests/TabManagerTests.swift
@@ -89,7 +89,7 @@ open class MockTabManagerDelegate: TabManagerDelegate {
 class TabManagerTests: XCTestCase {
 
     let didRemove = MethodSpy(functionName: "tabManager(_:didRemoveTab:isRestoring:)")
-    let didAdd = MethodSpy(functionName: "tabManager(_:didAddTab:isRestoring:)")
+    let didAdd = MethodSpy(functionName: "tabManager(_:didAddTab:placeNextToParentTab:isRestoring:)")
 
     var profile: TabManagerMockProfile!
     var manager: TabManager!
