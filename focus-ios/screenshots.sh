fb7ccf8ad673744168bcb01af884c7a4bf2fa57a
isabelrios <isabelrios@gmail.com>
2021-07-16 13:31:40 +0200

    [focus-ios] l10nScreenshotsTests-add-workflow-Part1-bis (https://github.com/mozilla-mobile/focus-ios/pull/1981)
    * l10nScreenshotsTests-add-workflow-Part1-bis
    
    * changes to get the test artifact
    
    * resolving conflict

--- focus-ios/screenshots.sh
+++ focus-ios/screenshots.sh
@@ -11,33 +11,40 @@ function get_abs_path {
 CURRENT_DIR="$(get_abs_path $0)"
 PROJECT_DIR="$(get_abs_path $CURRENT_DIR/../../../..)"
 
-mkdir -p screenshots
+if [ -d l10n-screenshots ]; then
+    echo "The l10n-screenshots directory already exists. You decide."
+    exit 1
+fi
+
+
+mkdir -p l10n-screenshots
 
 if [ "$1" = '--test-without-building' ]; then
     EXTRA_FAST_LANE_ARGS='--test_without_building'
     shift
 fi
 
-# Note that fil is tl in the l10n repo
-LANGUAGES="af,an,ar,ast,az,bn,br,bs,ca,cs,cy,da,de,dsb,el,en,eo,es-AR,es-CL,es-ES,es-MX,eu,fa,fi,fil,fr,ga,gd,he,hi-IN,hsb,hu,hy-AM,ia,id,is,it,ja,ka,kab,kk,kn,ko,lo,ms,my,nb,ne-NP,nl,nn,pl,pt-BR,pt-PT,ro,ru,ses,sk,sl,sq,sv,ta,te,th,tr,uk,ur,uz,vi,zh-CN,zh-TW"
-
-if [ $# -eq 1 ]; then
-  LANGUAGES=$1
+LOCALES=$*
+if [ $# -eq 0 ]; then
+    echo "Please provide locales to test. Available locales live in 'l10n-screenshots-config.yml'. E.g.: $0 af an anp ar"
+    exit 1
 fi
 
 DEVICE="iPhone 11"
 
-for PRODUCT in Focus Klar; do
-    echo "Snapshotting $PRODUCT on $DEVICE"
-        DEVICEDIR="${DEVICE// /}"
-        mkdir -p "screenshots/$PRODUCT/$DEVICEDIR"
-        fastlane snapshot --project Blockzilla.xcodeproj --scheme "${PRODUCT}SnapshotTests" \
-          --derived_data_path screenshots-derived-data \
-          --skip_open_summary \
-          --erase_simulator --localize_simulator \
-          --devices "$DEVICE" \
-          --languages "$LANGUAGES" \
-          --output_directory "screenshots/$PRODUCT/$DEVICEDIR" \
-           $EXTRA_FAST_LANE_ARGS
-    echo "Fastlane exited with code: $?"
+for lang in $LOCALES; do
+    for PRODUCT in Focus Klar; do
+        echo "Snapshotting $PRODUCT on $DEVICE"
+            DEVICEDIR="${DEVICE// /}"
+            mkdir -p "l10n-screenshots/$PRODUCT/$lang"
+            fastlane snapshot --project Blockzilla.xcodeproj --scheme "${PRODUCT}SnapshotTests" \
+              --derived_data_path l10n-screenshots-dd \
+              --skip_open_summary \
+              --erase_simulator --localize_simulator \
+              --devices "$DEVICE" \
+              --languages "$lang" \
+              --output_directory "l10n-screenshots/$PRODUCT/$lang" \
+              $EXTRA_FAST_LANE_ARGS
+        echo "Fastlane exited with code: $?"
+    done
 done
