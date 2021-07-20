d4ab767f46166ae79b24696e7ccc2c728745032a
roux g. buciu <11182210+electricRGB@users.noreply.github.com>
2021-07-20 11:28:42 -0400

    FxiOS2908 - Library Section A/B test setup (#8865)

---
+++ Client/Experiments/UserResearch.swift
@@ -0,0 +1,17 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import MozillaAppServices
+
+/// This procol will give access to the `Experiments` singleton and require
+/// the conforming class to set up an experiment.
+protocol UserResearch {
+    func setupExperiment()
+}
+
+extension UserResearch {
+    var experiments: NimbusApi {
+        return Experiments.shared
+    }
+}
