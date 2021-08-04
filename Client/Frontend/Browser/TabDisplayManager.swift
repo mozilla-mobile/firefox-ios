1780c232429c062ab12f121b367ba276e346a8db
Nishant Bhasin <nikieme3@gmail.com>
2021-08-04 13:16:20 -0400

    Cleanup

--- Client/Frontend/Browser/TabDisplayManager.swift
+++ Client/Frontend/Browser/TabDisplayManager.swift
@@ -92,7 +92,7 @@ class TabDisplayManager: NSObject, FeatureFlagsProtocol {
     }
     
     private var tabsToDisplay: [Tab] {
-        if shouldEnableInactiveTabs { return retriveTabsAndupdateInactiveState() }
+        if shouldEnableInactiveTabs { return getTabsAndupdateInactiveState() }
         let allTabs = self.isPrivate ? tabManager.privateTabs : tabManager.normalTabs
         return allTabs
     }
@@ -161,7 +161,7 @@ class TabDisplayManager: NSObject, FeatureFlagsProtocol {
     /// This is a helper method to update inactive tab state and should not be called directly
     /// Even when we have inactive tabs enabled try to call `tabsToDisplay`
     /// `tabsToDisplay` will make sure to get the correct set ot tabs and also check if feature is enabled
-    private func retriveTabsAndupdateInactiveState() -> [Tab] {
+    private func getTabsAndupdateInactiveState() -> [Tab] {
         let allTabs = self.isPrivate ? tabManager.privateTabs : tabManager.normalTabs
         guard allTabs.count > 0, let inactiveViewModel = inactiveViewModel else { return [Tab]() }
         guard allTabs.count > 1 else { return allTabs }
