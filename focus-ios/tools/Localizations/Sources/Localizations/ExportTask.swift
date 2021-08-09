e40524f58d2a93b38dfcbc10ef9a027a4c1381f7
Stefan Arentz <stefan@arentz.ca>
2021-08-09 13:23:27 -0400

    [focus-ios] Fixes https://github.com/mozilla-mobile/focus-ios/issues/2122 - Import Latvian (lv) and Lithuanian (lt) strings (https://github.com/mozilla-mobile/focus-ios/issues/2123)
    * Fixes https://github.com/mozilla-mobile/focus-ios/issues/2122 - Import Latvian (lv) and Lithuanian (lt) strings
    
    * Fixes https://github.com/mozilla-mobile/focus-ios/issues/2122 - Import Latvian (lv) and Lithuanian (lt) strings
    
    * Fixes https://github.com/mozilla-mobile/focus-ios/issues/2122 - Import Latvian (lv) and Lithuanian (lt) strings

--- focus-ios/tools/Localizations/Sources/Localizations/ExportTask.swift
+++ focus-ios/tools/Localizations/Sources/Localizations/ExportTask.swift
@@ -16,7 +16,7 @@ struct ExportTask {
         "af", "an", "ar", "ast", "az", "bg", "bn", "br", "bs", "ca", "cs", "cy", "da", "de", "dsb",
         "el", "en", "eo", "es-AR", "es-CL", "es-ES", "es-MX", "eu", "fa", "fi", "fil", "fr", "ga", "gd", "gu-IN",
         "he", "hi-IN", "hsb", "hu", "hy-AM", "ia", "id", "is", "it", "ja", "ka", "kab", "kk", "kn", "ko",
-        "lo", "mr", "ms", "my", "nb", "ne-NP", "nl", "nn", "pl", "pt-BR", "pt-PT", "ro", "ru", "ses", "sk",
+        "lo", "lt", "lv", "mr", "ms", "my", "nb", "ne-NP", "nl", "nn", "pl", "pt-BR", "pt-PT", "ro", "ru", "ses", "sk",
         "sl", "sq", "sv", "ta", "te", "th", "tr", "uk", "ur", "uz", "vi", "zh-CN", "zh-TW",
     ]
     
