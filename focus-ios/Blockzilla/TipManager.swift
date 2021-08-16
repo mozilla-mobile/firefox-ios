c4d8cab821b4c366f7b36a4ea790fa63a2cca61e
Stefan Arentz <stefan@arentz.ca>
2021-08-16 11:51:55 -0400

    [focus-ios] Fixes https://github.com/mozilla-mobile/focus-ios/pull/2161 - Show the home screen tips for all English locales (https://github.com/mozilla-mobile/focus-ios/pull/2163)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- focus-ios/Blockzilla/TipManager.swift
+++ focus-ios/Blockzilla/TipManager.swift
@@ -107,7 +107,7 @@ class TipManager {
     }
 
     func shouldShowTips() -> Bool {
-        return NSLocale.current.identifier == "en_US" && !AppInfo.isKlar
+        return NSLocale.current.languageCode == "en" && !AppInfo.isKlar
     }
     
     func getNextTip() -> Tip? {
