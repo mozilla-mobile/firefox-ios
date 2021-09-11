2c477cda489aeea627e506efa26553749fa9c030
Harry Li <harryl.byread@gmail.com>
2021-09-11 21:47:53 +1000

    Check webView before add print activity

--- Client/Frontend/Browser/TabPrintPageRenderer.swift
+++ Client/Frontend/Browser/TabPrintPageRenderer.swift
@@ -27,8 +27,8 @@ class TabPrintPageRenderer: UIPrintPageRenderer {
         self.footerHeight = PrintedPageUX.PageMarginScale * PrintedPageUX.PageInsets
         self.headerHeight = PrintedPageUX.PageMarginScale * PrintedPageUX.PageInsets
 
-        if let tab = self.tab {
-            let formatter = tab.webView!.viewPrintFormatter()
+        if let tab = self.tab, let webview = tab.webView {
+            let formatter = webview.viewPrintFormatter()
             formatter.perPageContentInsets = UIEdgeInsets(top: PrintedPageUX.PageInsets, left: PrintedPageUX.PageInsets, bottom: PrintedPageUX.PageInsets, right: PrintedPageUX.PageInsets)
             addPrintFormatter(formatter, startingAtPageAt: 0)
         }
