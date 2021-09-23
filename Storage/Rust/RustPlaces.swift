a925510159e05466f9a3862a7e4793121d02194a
Grisha Kruglov <gkruglov@mozilla.com>
2021-09-23 15:24:28 -0700

    Fix breaking changes in A-S 85.0.0

--- Storage/Rust/RustPlaces.swift
+++ Storage/Rust/RustPlaces.swift
@@ -354,13 +354,21 @@ public class RustPlaces {
      */
     public func noteHistoryMetadataObservation(key: HistoryMetadataKey, observation: HistoryMetadataObservation) -> Deferred<Maybe<Void>> {
         return withWriter { connection in
-            return try connection.noteHistoryMetadataObservation(key: key, observation: observation)
+            if let title = observation.title {
+                return try connection.noteHistoryMetadataObservationTitle(key: key, title: title)
+            }
+            if let documentType = observation.documentType {
+                return try connection.noteHistoryMetadataObservationDocumentType(key: key, documentType: documentType)
+            }
+            if let viewTime = observation.viewTime {
+                return try connection.noteHistoryMetadataObservationViewTime(key: key, viewTime: viewTime)
+            }
         }
     }
     
     public func deleteHistoryMetadataOlderThan(olderThan: Int64) -> Deferred<Maybe<Void>> {
         return withWriter { connection in
-            return try connection.deleteHistoryMetadaOlderThan(olderThan: olderThan)
+            return try connection.deleteHistoryMetadataOlderThan(olderThan: olderThan)
         }
     }
 }
