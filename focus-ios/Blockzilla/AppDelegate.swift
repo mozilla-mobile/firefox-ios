44110e9fc91b01e54c42bee9c0685caa5b7d2909
Sammy Khamis <sbkhamis@gmail.com>
2021-10-01 12:23:26 -1000

    [focus-ios] Fixes https://github.com/mozilla-mobile/focus-ios/pull/1866 - Integrate Nimbus into Focus
    * add application services as swift packages
    
    * updated rust-component-swift
    
    * wrapped nimbus creation around a do/try
    
    * removed hard crash from error reporter
    
    * updated version num
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- focus-ios/Blockzilla/AppDelegate.swift
+++ focus-ios/Blockzilla/AppDelegate.swift
@@ -5,6 +5,9 @@
 import UIKit
 import Telemetry
 import Glean
+import Viaduct
+import RustLog
+import Nimbus
 
 protocol AppSplashController {
     var splashView: UIView { get }
@@ -427,6 +430,55 @@ extension AppDelegate {
         GleanMetrics.TrackingProtection.hasContentBlocked.set(Settings.getToggle(.blockOther))
         GleanMetrics.TrackingProtection.hasSocialBlocked.set(Settings.getToggle(.blockSocial))
         GleanMetrics.MozillaProducts.hasFirefoxInstalled.set(UIApplication.shared.canOpenURL(URL(string: "firefox://")!))
+
+        // Proof-of-Concept application-services guff here.
+        //
+        // We're just seeing if we can successfully import and use various application-services
+        // components, they don't really do anything in the app yet.
+        //
+        
+        // Hook up basic logging.
+        if !RustLog.shared.tryEnable({ (level, tag, message) -> Bool in
+            let logString = "[RUST][\(tag ?? "no-tag")] \(message)"
+            print(logString)
+            return true
+        }) {
+            print("ERROR: Unable to enable logging from Rust")
+        }
+
+        // Enable networking.
+        Viaduct.shared.useReqwestBackend()
+
+        var nimbusDbPath: String {
+            let profilePath = FileManager.default.containerURL(
+                forSecurityApplicationGroupIdentifier: AppInfo.sharedContainerIdentifier
+            )?
+            .appendingPathComponent("profile.profile")
+            .path
+            let dbPath = profilePath.flatMap {
+                URL(fileURLWithPath: $0).appendingPathComponent("nimbus.db").path
+            } ?? ""
+            
+            return dbPath
+        }
+
+        let errorReporter: NimbusErrorReporter = { err in
+            print(err)
+        }
+        do {
+            let nimbus = try Nimbus.create(
+                NimbusServerSettings(url: URL(string: "https://firefox.settings.services.mozilla.com")!),
+                appSettings: NimbusAppSettings(appName: "focus_ios", channel: "release"),
+                dbPath: nimbusDbPath,
+                resourceBundles: [],
+                errorReporter: errorReporter
+            )
+                nimbus.initialize()
+                nimbus.fetchExperiments()
+            }
+         catch {
+            print("ERROR: Unable to create Nimbus")
+        }
     }
 
     func presentModal(viewController: UIViewController, animated: Bool) {
