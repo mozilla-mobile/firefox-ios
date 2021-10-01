aaa4dedd2e8df1fbdd796d87cdf7c98536a51995
Stefan Arentz <stefan@arentz.ca>
2021-10-01 16:36:52 -0400

    [focus-ios] Fixes https://github.com/mozilla-mobile/focus-ios/pull/2508 - Update the shavar lists to 93.0 (https://github.com/mozilla-mobile/focus-ios/pull/2509)

--- focus-ios/checkout.sh
+++ focus-ios/checkout.sh
@@ -4,9 +4,11 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+set -x
+
 git clone https://github.com/mozilla-services/shavar-prod-lists.git || exit 1
 
-# This revision is taken from the original Cartfile.resolved
-(cd shavar-prod-lists && git checkout -q 3910527004252af3aa9dd701566a2cb3b78e5c3a)
+# Grab the las known (pinned) commit on the 93.0 branch
+(cd shavar-prod-lists && git checkout -q 352f772269f13e70893d0d112d26aed1f079ce6e)
 
 (cd content-blocker-lib-ios/ContentBlockerGen && swift run)
