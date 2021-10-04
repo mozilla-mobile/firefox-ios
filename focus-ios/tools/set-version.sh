68e9a3e49b001986179af66a08ab3f0d077dd997
Stefan Arentz <stefan@arentz.ca>
2021-10-04 08:54:09 -0400

    [focus-ios] Fixes https://github.com/mozilla-mobile/focus-ios/pull/2513 - Create a tool to more easily change the app version (https://github.com/mozilla-mobile/focus-ios/issues/2514)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

---
+++ focus-ios/tools/set-version.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [ $# -eq 0 ]; then
+  echo "usage: set-version.sh <version-number>"
+  exit 1
+fi
+
+
+for plist in Blockzilla/Info.plist ContentBlocker/Info.plist FocusIntentExtension/Info.plist OpenInFocus/Info.plist; do
+  current=`/usr/libexec/PlistBuddy -c "Print :CFBundleShortVersionString" "$plist"`
+  echo "Changing CFBundleShortVersionString in $plist from $current to $1"
+  /usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $1" "$plist"
+done
+
+
