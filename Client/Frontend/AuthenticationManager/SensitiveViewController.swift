8eb01c332332af5e4a75a8d4525a0244a27c3cb5
Stefan Arentz <stefan@arentz.ca>
2021-10-06 17:25:00 -0400

    Fixes #8934 - Replace Firefox-specific passcode with native device authentication

--- Client/Frontend/AuthenticationManager/SensitiveViewController.swift
+++ Client/Frontend/AuthenticationManager/SensitiveViewController.swift
@@ -2,85 +2,62 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import Foundation
+import UIKit
 import SnapKit
-import SwiftKeychainWrapper
-
-enum AuthenticationState {
-    case notAuthenticating
-    case presenting
-}
+import Shared
 
 class SensitiveViewController: UIViewController {
-    var promptingForTouchID: Bool = false
-    var backgroundedBlur: UIImageView?
-    var authState: AuthenticationState = .notAuthenticating
+    private var backgroundedBlur: UIVisualEffectView?
+    private var isAuthenticated: Bool = false
 
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        let notificationCenter = NotificationCenter.default
-        notificationCenter.addObserver(self, selector: #selector(checkIfUserRequiresValidation), name: UIApplication.willEnterForegroundNotification, object: nil)
-        notificationCenter.addObserver(self, selector: #selector(checkIfUserRequiresValidation), name: UIApplication.didBecomeActiveNotification, object: nil)
-        notificationCenter.addObserver(self, selector: #selector(blurContents), name: UIApplication.willResignActiveNotification, object: nil)
-        notificationCenter.addObserver(self, selector: #selector(blurContents), name: UIApplication.didEnterBackgroundNotification, object: nil)
-    }
-
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        NotificationCenter.default.removeObserver(self)
-    }
-
-    @objc func checkIfUserRequiresValidation() {
-        guard authState != .presenting else {
-            return
-        }
-
-        presentedViewController?.dismiss(animated: false, completion: nil)
-        
-        promptingForTouchID = true
-
-        AppAuthenticator.authenticateWithDeviceOwnerAuthentication { result in
-            switch result {
-                case .success():
-                    self.promptingForTouchID = false
-                    self.authState = .notAuthenticating
-                    self.removeBackgroundedBlur()
-                case .failure(let error):
-                    self.promptingForTouchID = false
-                    self.authState = .notAuthenticating
-                    print("MOO Error \(error)")
-                    self.navigationController?.dismiss(animated: true, completion: nil)
-                    self.dismiss(animated: true)
+    private var willEnterForegroundNotificationObserver: NSObjectProtocol?
+    private var didEnterBackgroundNotificationObserver: NSObjectProtocol?
+    
+    override func viewDidLoad() {
+        willEnterForegroundNotificationObserver = NotificationCenter.default.addObserver(forName: UIApplication.willEnterForegroundNotification, object: nil, queue: .main) { [unowned self] notification in
+            if !self.isAuthenticated {
+                AppAuthenticator.authenticateWithDeviceOwnerAuthentication { result in
+                    switch result {
+                        case .success():
+                            self.isAuthenticated = true
+                            self.removeBackgroundedBlur()
+                        case .failure(_):
+                            self.isAuthenticated = false
+                            self.navigationController?.dismiss(animated: true, completion: nil)
+                            self.dismiss(animated: true)
+                    }
+                }
             }
         }
-        authState = .presenting
-    }
 
-    @objc func blurContents() {
-        if backgroundedBlur == nil {
-            backgroundedBlur = addBlurredContent()
+        didEnterBackgroundNotificationObserver = NotificationCenter.default.addObserver(forName: UIApplication.didEnterBackgroundNotification, object: nil, queue: .main) { [unowned self] notification in
+            self.isAuthenticated = false
+            self.blurContents()
         }
     }
-
-    func removeBackgroundedBlur() {
-        if !promptingForTouchID {
-            backgroundedBlur?.removeFromSuperview()
-            backgroundedBlur = nil
+    
+    deinit {
+        if let observer = willEnterForegroundNotificationObserver {
+            NotificationCenter.default.removeObserver(observer)
         }
-    }
-
-    fileprivate func addBlurredContent() -> UIImageView? {
-        guard let snapshot = view.screenshot() else {
-            return nil
+        if let observer = didEnterBackgroundNotificationObserver {
+            NotificationCenter.default.removeObserver(observer)
         }
-
-        let blurredSnapshot = snapshot.applyBlur(withRadius: 10, blurType: BOXFILTER, tintColor: UIColor(white: 1, alpha: 0.3), saturationDeltaFactor: 1.8, maskImage: nil)
-        let blurView = UIImageView(image: blurredSnapshot)
-        view.addSubview(blurView)
-        blurView.snp.makeConstraints { $0.edges.equalTo(self.view) }
-        view.layoutIfNeeded()
-
-        return blurView
     }
 }
 
+extension SensitiveViewController {
+    private func blurContents() {
+        if backgroundedBlur == nil {
+            backgroundedBlur = UIVisualEffectView(effect: UIBlurEffect(style: .extraLight))
+            view.addSubview(backgroundedBlur!)
+            backgroundedBlur!.snp.makeConstraints { $0.edges.equalTo(self.view) }
+            view.layoutIfNeeded()
+        }
+    }
+
+    private func removeBackgroundedBlur() {
+        backgroundedBlur?.removeFromSuperview()
+        backgroundedBlur = nil
+    }
+}
