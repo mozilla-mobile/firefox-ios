4fbe248eedd80d9d237f8cb7d526dff213ea73a1
roux g. buciu <11182210+electricRGB@users.noreply.github.com>
2021-10-08 12:57:37 -0400

    Homepage settings update (#9272)
    (cherry picked from commit 7b89e6dfa146a4402e2b3b7fbe85d951577b17b2)

--- Client/FeatureFlags/FeatureFlagsManager.swift
+++ Client/FeatureFlags/FeatureFlagsManager.swift
@@ -4,6 +4,7 @@
 
 import Shared
 
+// MARK: - Protocol
 protocol FeatureFlagsProtocol { }
 
 extension FeatureFlagsProtocol {
@@ -12,6 +13,7 @@ extension FeatureFlagsProtocol {
     }
 }
 
+// MARK: - FeatureFlagName
 /// An enum describing the featureID of all features, historical, avalable, or in development.
 /// Please add new features alphabetically.
 enum FeatureFlagName: String, CaseIterable {
@@ -20,6 +22,7 @@ enum FeatureFlagName: String, CaseIterable {
     case groupedTabs
     case jumpBackIn
     case nimbus
+    case pocket
     case pullToRefresh
     case recentlySaved
     case shakeToRestore
@@ -31,13 +34,13 @@ enum FeatureFlagName: String, CaseIterable {
 /// To add a new feature flag, you must do four things:
 ///
 /// 1. Add a name in the `FeatureFlagName` enum
-/// 2. Add a new `FlaggableFeature` in the `FeatureFlagManager.setupFeatures` and add it
+/// 2. Add a new `FlaggableFeature` in the ``FeatureFlagManager.initializeFeatures`` and add it
 /// to the `features` dictionary using its key.
 /// 3. Optional: If the feature is meant to be togglable, add a key for the feature
 /// in the `PrefsKeys` struct, and then also add it to the `FlaggableFeature.featureKey`
 /// function to allow the flag status to be changed.
 /// 4. Add the `FeatureFlagsProtocol` protocol to the class you wish to use the feature
-/// flag in, and access the required flag using `featureFlags.isFeatureActive`.
+/// flag in, and access the required flag using `featureFlags.isFeatureActiveForBuild`.
 class FeatureFlagsManager {
 
     /// This Singleton should only be accessed directly in places where the
@@ -46,52 +49,120 @@ class FeatureFlagsManager {
     /// `featureFlags` variable.
     static let shared = FeatureFlagsManager()
 
+    // MARK: - Variables
+
     private var profile: Profile!
     private var features: [FeatureFlagName: FlaggableFeature] = [:]
 
-    public func isFeatureActive(_ featureID: FeatureFlagName) -> Bool {
+    // MARK: - Public methods
+
+    /// Used as the main way to find out whether a feature is active or not,
+    /// specifically for the build.
+    public func isFeatureActiveForBuild(_ featureID: FeatureFlagName) -> Bool {
         guard let feature = features[featureID] else { return false }
-        return feature.isActive
+        return feature.isActiveForBuild()
     }
 
-    /// Toggles the feature on/off, depending on its current status AND whether or not it is
-    /// a feature that can be saved to disk. For more information, see `FlaggableFeature`
-    public func toggle(_ featureID: FeatureFlagName) {
-        features[featureID]?.toggle()
+    public func toggleBuildFeature(_ featureID: FeatureFlagName) {
+        features[featureID]?.toggleBuildFeature()
+    }
+
+    /// Retrieves a feature key for any specific feature, if it has one.
+    public func featureKey(for featureID: FeatureFlagName) -> String? {
+        return features[featureID]?.featureOptionsKey
+    }
+
+    /// Main interface for accessing feature options.
+    ///
+    /// Function must have context when called: `let foo: Type = featureOption(.example)`
+    /// Any feature with an option attached must be listed, and further converted into
+    /// it's appropriate type in the switch statement.
+    public func getUserPreferenceFor<T>(_ featureID: FeatureFlagName) -> T? {
+        guard let feature = features[featureID],
+              let userSetting = feature.getUserPreference()
+        else { return nil }
+
+        switch featureID {
+        case .startAtHome: return StartAtHomeSetting(rawValue: userSetting) as? T
+        default: return UserFeaturePreference(rawValue: userSetting) as? T
+        }
+    }
+
+    /// Main interface for setting a feature's state and options. Options are enums of
+    /// `FlaggableFeatureOptions` type and also conform to Int.
+    public func setUserPreferenceFor<T:FlaggableFeatureOptions>(_ featureID: FeatureFlagName, to option: T) {
+
+        switch featureID {
+        case .startAtHome:
+            if let option = option as? StartAtHomeSetting {
+                features[featureID]?.setUserPreferenceFor(option.rawValue)
+            }
+        default:
+            if let option = option as? UserFeaturePreference {
+                features[featureID]?.setUserPreferenceFor(option.rawValue)
+            }
+        }
     }
 
     /// Sets up features with default channel availablility. For ease of use, please add
     /// new features alphabetically.
-    public func setupFeatures(with profile: Profile) {
+    ///
+    /// This should ONLY be called when instatiating the feature flag system,
+    /// and never again.
+    public func initializeFeatures(with profile: Profile) {
         features.removeAll()
 
-        let chronTabs = FlaggableFeature(withID: .chronologicalTabs, and: profile, enabledFor: [.developer])
+        let chronTabs = FlaggableFeature(withID: .chronologicalTabs,
+                                         and: profile,
+                                         enabledFor: [.developer])
         features[.chronologicalTabs] = chronTabs
 
-        let inactiveTabs = FlaggableFeature(withID: .inactiveTabs, and: profile, enabledFor: [.developer])
+        let inactiveTabs = FlaggableFeature(withID: .inactiveTabs,
+                                            and: profile,
+                                            enabledFor: [.developer])
         features[.inactiveTabs] = inactiveTabs
-        
-        let groupedTabs = FlaggableFeature(withID: .groupedTabs, and: profile, enabledFor: [.beta, .developer])
+
+        let groupedTabs = FlaggableFeature(withID: .groupedTabs,
+                                           and: profile,
+                                           enabledFor: [.beta, .developer])
         features[.groupedTabs] = groupedTabs
 
-        let jumpBackIn = FlaggableFeature(withID: .jumpBackIn, and: profile, enabledFor: [.beta, .developer])
+        let jumpBackIn = FlaggableFeature(withID: .jumpBackIn,
+                                          and: profile,
+                                          enabledFor: [.beta, .developer])
         features[.jumpBackIn] = jumpBackIn
 
         /// Use the Nimbus experimentation platform. If this is `true` then
         /// `Experiments.shared` provides access to Nimbus. If false, it is a dummy object.
-        let nimbus = FlaggableFeature(withID: .nimbus, and: profile, enabledFor: [.release, .beta, .developer])
+        let nimbus = FlaggableFeature(withID: .nimbus,
+                                      and: profile,
+                                      enabledFor: [.release, .beta, .developer])
         features[.nimbus] = nimbus
-        
-        let pullToRefresh = FlaggableFeature(withID: .pullToRefresh, and: profile, enabledFor: [.release ,.beta, .developer])
+
+        let pocket = FlaggableFeature(withID: .pocket,
+                                      and: profile,
+                                      enabledFor: [.release, .beta, .developer])
+        features[.pocket] = pocket
+
+
+        let pullToRefresh = FlaggableFeature(withID: .pullToRefresh,
+                                             and: profile,
+                                             enabledFor: [.release ,.beta, .developer])
         features[.pullToRefresh] = pullToRefresh
 
-        let recentlySaved = FlaggableFeature(withID: .recentlySaved, and: profile, enabledFor: [.beta, .developer])
+        let recentlySaved = FlaggableFeature(withID: .recentlySaved,
+                                             and: profile,
+                                             enabledFor: [.beta, .developer])
         features[.recentlySaved] = recentlySaved
 
-        let shakeToRestore = FlaggableFeature(withID: .shakeToRestore, and: profile, enabledFor: [.beta, .developer, .other])
+        let shakeToRestore = FlaggableFeature(withID: .shakeToRestore,
+                                              and: profile,
+                                              enabledFor: [.beta, .developer, .other])
         features[.shakeToRestore] = shakeToRestore
-        
-        let startAtHome = FlaggableFeature(withID: .startAtHome, and: profile, enabledFor: [])
+
+        let startAtHome = FlaggableFeature(withID: .startAtHome,
+                                           and: profile,
+                                           enabledFor: [])
         features[.startAtHome] = startAtHome
     }
 }
