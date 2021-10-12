23719146269ff6d47c48005ce89df55e1a085ced
nishant2718 <nishantpatel2718@gmail.com>
2021-10-12 12:40:15 -0400

    FXIOS-3087: jumpBackIn UI updates

--- Client/UIWindowExtensions.swift
+++ Client/UIWindowExtensions.swift
@@ -5,15 +5,13 @@
 import Foundation
 
 extension UIWindow {
+    
     static var isLandscape: Bool {
-        if #available(iOS 13.0, *) {
-            return UIApplication.shared.windows
-                .first?
-                .windowScene?
-                .interfaceOrientation
-                .isLandscape ?? false
-        } else {
-            return UIApplication.shared.statusBarOrientation.isLandscape
-        }
+        return UIApplication.shared.windows
+            .first?
+            .windowScene?
+            .interfaceOrientation
+            .isLandscape ?? false
     }
+    
 }
