24d59e23688fe3eb598c326aa0ac913031ca87d8
Stefan Arentz <stefan@arentz.ca>
2021-10-20 22:53:03 -0400

    Fixes #9378 - Remove SnapKit usage from CredentialProvider and Login Management

--- Client/Utils/Layout.swift
+++ Client/Utils/Layout.swift
@@ -5,11 +5,18 @@
 import UIKit
 
 extension NSLayoutConstraint {
+    
+    /// Builder function that return a new NSLayoutConstraints with the priority set. This is useful  to inline constraint creation in a call to `NSLayoutConstraint.active()`.
+    /// - Parameter priority: the priority to set
+    /// - Returns: the same `NSLayoutConstraint` with the priority set
     func priority(_ priority: UILayoutPriority) -> NSLayoutConstraint {
         self.priority = priority
         return self
     }
     
+    /// Builder function that return a new NSLayoutConstraints with the multiplier set. This is useful  to inline constraint creation in a call to `NSLayoutConstraint.active()`.
+    /// - Parameter multiplier: the multiplier to set
+    /// - Returns: a new `NSLayoutContraint` instance
     func multiplier(_ multiplier: CGFloat) -> NSLayoutConstraint {
         return NSLayoutConstraint(
             item: self.firstItem!,
