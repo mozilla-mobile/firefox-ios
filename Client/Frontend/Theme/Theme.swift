26b55dda5f9b32608213a6680a3e6de807db29f3
roux g. buciu <gbuciu@mozilla.com>
2021-10-27 11:07:23 -0400

    Add Theme, FxDefaultTheme, new ThemeManager

--- Client/Frontend/Theme/Theme.swift
+++ Client/Frontend/Theme/Theme.swift
@@ -2,13 +2,44 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+/// The `Theme` protocol, which contains the implementation of themes,
+/// which comprise of a set of standardized colours (including light and
+/// dark mode) and fonts for the application.
 protocol Theme {
     var colours: ThemeColourPalette { get }
     var fonts: ThemeFontPalette { get }
 }
 
+/// The colour palette for the theme.
 protocol ThemeColourPalette {
+    var actionPrimary: UIColor { get }
+    var actionSecondary: UIColor { get }
+    var borderDivider: UIColor { get }
+    var borderSelectedDefault: UIColor { get }
+    var borderSelectedPrivate: UIColor { get }
+    var controlActive: UIColor { get }
+    var controlBase: UIColor { get }
+    var iconAccentBlue: UIColor { get }
+    var iconAccentGreen: UIColor { get }
+    var iconAccentPink: UIColor { get }
+    var iconAccentViolet: UIColor { get }
+    var iconAccentYellow: UIColor { get }
+    var iconDisabled: UIColor { get }
+    var iconInverted: UIColor { get }
+    var iconPrimary: UIColor { get }
+    var iconSecondary: UIColor { get }
     var layer1: UIColor { get }
+    var layer2: UIColor { get }
+    var layer2Blur: UIColor { get }
+    var layer3: UIColor { get }
+    var layerEmphasis: UIColor { get }
+    var scrim: UIColor { get }
+    var textDisabled: UIColor { get }
+    var textInverted: UIColor { get }
+    var textLink: UIColor { get }
+    var textPrimary: UIColor { get }
+    var textSecondary: UIColor { get }
+    var textWarning: UIColor { get }
 }
 
 protocol ThemeFontPalette {
--- Client/Frontend/Theme/ThemeManager.swift
+++ Client/Frontend/Theme/ThemeManager.swift
@@ -3,24 +3,91 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import UIKit
+import Shared
 
-// MARK: - Protocol
+/// The `Themable` protocol gives access to the `ThemeManager` singleton
+/// through the `themeManager` variable, while using dependency injection.
 protocol Themeable { }
 
 extension Themeable {
     var themeManager: ThemeManager { return ThemeManager.shared }
 }
 
+/// The `ThemeManager` will be responsible for providing the theme throughout the app
+///
+/// IMPORTANT NOTE:
+/// The current functionality of `ThemeManager` is restricted to providing the default
+/// theme. The rest of the functionality, is a sketing out of how implementing user
+/// defined themes might work. It's a work in progress and needs not be touched until
+/// such a feature is actually added.
 class ThemeManager {
 
     static let shared = ThemeManager()
 
-    // If needed, we can easily check the system theme here.
-    var systemTheme: UIUserInterfaceStyle {
-        return UIScreen.main.traitCollection.userInterfaceStyle
+    fileprivate var profile: Profile!
+
+    /// The theme key of the current set theme. If no custom theme is set, then
+    /// this will return the `defaultThemeKey`
+    fileprivate var themeKey: String {
+        if let key = profile.prefs.stringForKey(PrefsKeys.ThemeManagerCustomizationKey) { return key }
+        return defaultThemeKey
     }
 
+    fileprivate let defaultThemeKey = "FxDefaultThemeThemeManagerKey"
+
+    /// The current theme set in the application.
+    ///
+    /// Currently, the `currentTheme` simply returns the default Firefox theme.
+    /// However, in the future, this is expandable to allow custom themes to
+    /// be set by the user.
     var currentTheme: Theme {
-        return FxDefaultTheme()
+        guard let customThemeKey = profile.prefs.stringForKey(themeKey),
+              let customTheme = getTheme(from: customThemeKey)
+        else { return FxDefaultTheme() }
+
+        return customTheme
+    }
+
+    /// This is `ThemeManager`s initializer, esentially, as we require the profile
+    /// to save custom themes to disk.
+    ///
+    /// This should only be called in AppDelegate.
+    public func updateProfile(with profile: Profile) {
+        self.profile = profile
+    }
+
+    public func updateTheme(named name: String, with customTheme: CustomTheme) {
+        let keyName = "UserCustomThemeNamed\(name)"
+        do {
+            let jsonData = try JSONEncoder().encode(customTheme)
+            let jsonString = String(data: jsonData, encoding: .utf8)!
+            profile.prefs.setString(jsonString, forKey: keyName)
+        } catch {
+            print("Something's gone wrong saving a custom theme.")
+        }
+    }
+
+    private func getTheme(from theme: String) -> Theme? {
+        if themeKey != defaultThemeKey, let customThemeData = profile.prefs.stringForKey(themeKey) {
+            let jsonData = Data(customThemeData.utf8)
+
+            do {
+                let decodedTheme = try JSONDecoder().decode(CustomTheme.self, from: jsonData)
+                return verify(customTheme: decodedTheme)
+            } catch {
+                print("Something's gone wrong decoding a custom theme.")
+            }
+        }
+
+        return nil
+    }
+
+    /// The verify function will return a true `Theme` object, as the `CustomTheme`
+    /// might not have all values specified. Because the initial implementation of
+    /// `ThemeManager` is a rough outline of how things might work for custom themes
+    /// in the future, this empty function is merely serving as a placeholder.
+    private func verify(customTheme: CustomTheme) -> Theme? {
+
+        return nil
     }
 }
