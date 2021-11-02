5876ee3272d2b4ff1f2f0bd75b87ae6c360eb670
Mustafa Hastürk <mustafa.hasturk@yandex.com>
2021-11-02 15:57:42 +0300

    make optional locales and regionOverrides

--- Client/Frontend/Browser/DefaultSearchPrefs.swift
+++ Client/Frontend/Browser/DefaultSearchPrefs.swift
@@ -8,10 +8,10 @@ import Shared
 /*
  This only makes sense if you look at the structure of List.json
 */
-class DefaultSearchPrefs {
+final class DefaultSearchPrefs {
     fileprivate let defaultSearchList: [String]
-    fileprivate let locales: [String: Any]
-    fileprivate let regionOverrides: [String: Any]
+    fileprivate let locales: [String: Any]?
+    fileprivate let regionOverrides: [String: Any]?
     fileprivate let globalDefaultEngine: String
 
     public init?(with filePath: URL) {
@@ -26,8 +26,8 @@ class DefaultSearchPrefs {
         }
 
         // Split up the JSON into useful parts
-        locales = json["locales"] as! [String : Any]
-        regionOverrides = json["regionOverrides"] as! [String : Any]
+        locales = json["locales"] as? [String : Any]
+        regionOverrides = json["regionOverrides"] as? [String : Any]
         // These are the fallback defaults
         guard let defaultDict = json["default"] as? [String: Any],
               let searchList = defaultDict["visibleDefaultEngines"] as? [String],
@@ -43,10 +43,10 @@ class DefaultSearchPrefs {
      Returns an array of the visibile engines. It overrides any of the returned engines from the regionOverrides list
      Each language in the locales list has a default list of engines and then a region override list.
      */
-    open func visibleDefaultEngines(for possibleLocales: [String], and region: String) -> [String] {
+    public func visibleDefaultEngines(for possibleLocales: [String], and region: String) -> [String] {
         let engineList = possibleLocales
             .compactMap {
-                locales[$0] as? [String: Any]
+                locales?[$0] as? [String: Any]
             }
             .compactMap { localDict -> [String]? in
                 let visibleDefaultEngines = "visibleDefaultEngines"
@@ -66,7 +66,7 @@ class DefaultSearchPrefs {
         var usersEngineList = engineList ?? defaultSearchList
 
         // Overrides for specific regions.
-        if let overrides = regionOverrides[region] as? [String: Any] {
+        if let overrides = regionOverrides?[region] as? [String: Any] {
             usersEngineList = usersEngineList.map({ overrides[$0] as? String ?? $0 })
         }
         return usersEngineList
@@ -77,10 +77,10 @@ class DefaultSearchPrefs {
      The list.json locales list contains searchDefaults for a few locales.
      Create a list of these and return the last one. The globalDefault acts as the fallback in case the list is empty.
      */
-    open func searchDefault(for possibleLocales: [String], and region: String) -> String {
+    public func searchDefault(for possibleLocales: [String], and region: String) -> String {
         return possibleLocales
             .compactMap {
-                locales[$0] as? [String: Any]
+                locales?[$0] as? [String: Any]
             }
             .reduce(globalDefaultEngine) {
                 (defaultEngine, localeJSON) -> String in
