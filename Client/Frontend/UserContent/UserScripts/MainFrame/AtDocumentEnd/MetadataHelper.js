70757a3a690e0e07063dbc6d27b5f3d7bfd3163e
lmarceau <lmarceau@mozilla.com>
2021-11-10 21:33:54 -0500

    Add default header that will be used for any file created (#9474)
    * Add default header that will be used for any file created
    
    * Replace all headers with single line format comment instead of multiline

--- Client/Frontend/UserContent/UserScripts/MainFrame/AtDocumentEnd/MetadataHelper.js
+++ Client/Frontend/UserContent/UserScripts/MainFrame/AtDocumentEnd/MetadataHelper.js
@@ -1,7 +1,7 @@
 /* vim: set ts=2 sts=2 sw=2 et tw=80: */
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0
 
 "use strict";
 
--- Client/Frontend/photon-colors.swift
+++ Client/Frontend/photon-colors.swift
@@ -1,6 +1,6 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0
 
 /* Photon Colors iOS Variables v3.3.1
  From https://github.com/FirefoxUX/photon-colors/#readme */
