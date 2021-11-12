ecc80405a8cb63207a9696877ba52c32d41449c9
IORDAN RALUCA <ralucaiordan@icloud.com>
2021-11-12 11:27:36 +0200

    [focus-ios] Fixes https://github.com/mozilla-mobile/focus-ios/issues/2784 - [iPad] URL bar loses focus after changing orientation (https://github.com/mozilla-mobile/focus-ios/pull/2807)
    * Fixes https://github.com/mozilla-mobile/focus-ios/issues/2784 - [iPad] URL bar loses focus after changing orientation
    
    * Fixes https://github.com/mozilla-mobile/focus-ios/issues/2784 - [iPad] URL bar loses focus after changing orientation
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- focus-ios/Blockzilla/BrowserViewController.swift
+++ focus-ios/Blockzilla/BrowserViewController.swift
@@ -82,6 +82,7 @@ class BrowserViewController: UIViewController {
         }
     }
     private var initialUrl: URL?
+    private var orientationWillChange = false
     var tipManager: TipManager
     var shortcutManager: ShortcutsManager
 
@@ -755,6 +756,7 @@ class BrowserViewController: UIViewController {
         // Fixes the issue of a user fresh-opening Focus via Split View
         guard isViewLoaded else { return }
 
+        orientationWillChange = true
         // UIDevice.current.orientation isn't reliable. See https://bugzilla.mozilla.org/show_bug.cgi?id=1315370#c5
         // As a workaround, consider the phone to be in landscape if the new width is greater than the height.
         showsToolsetInURLBar = (UIDevice.current.userInterfaceIdiom == .pad && (UIScreen.main.bounds.width == size.width || size.width > size.height)) || (UIDevice.current.userInterfaceIdiom == .phone && size.width > size.height)
@@ -1753,9 +1755,11 @@ extension BrowserViewController: KeyboardHelperDelegate {
     }
 
     func keyboardHelper(_ keyboardHelper: KeyboardHelper, keyboardDidHideWithState state: KeyboardState) {
-        if UIDevice.current.userInterfaceIdiom == .pad {
+        if UIDevice.current.userInterfaceIdiom == .pad && !orientationWillChange {
             urlBar.dismiss()
         }
+        orientationWillChange = false
+                                                                                                           
     }
     func keyboardHelper(_ keyboardHelper: KeyboardHelper, keyboardDidShowWithState state: KeyboardState) { }
 }
