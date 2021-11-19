a5ed6ccd2f80039aba724a5116d4f22a77e42c79
Laurie Marceau <lmarceau@mozilla.com>
2021-11-19 10:10:25 -0500

    Revert looking for inactive state for keyWindow

--- Client/UIWindowExtensions.swift
+++ Client/UIWindowExtensions.swift
@@ -7,7 +7,7 @@ import Foundation
 extension UIWindow {
     static var keyWindow: UIWindow? {
         return UIApplication.shared.connectedScenes
-            .filter { $0.activationState == .foregroundActive || $0.activationState == .foregroundInactive }
+            .filter { $0.activationState == .foregroundActive }
             .first(where: { $0 is UIWindowScene })
             .flatMap({ $0 as? UIWindowScene })?.windows
             .first(where: \.isKeyWindow)
