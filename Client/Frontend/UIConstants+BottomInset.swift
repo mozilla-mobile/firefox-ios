f5fbaebf02025a588a50a5a6acc1ec0cb6db2871
lmarceau <lauriemarceau6@gmail.com>
2021-11-19 11:44:28 -0500

    #9288 Bottom toolbar shrinked (#9535)
    * Bottom toolbar height is set while in background. Take attachedKeyWindow for that one case - avoiding to change other UIWindow.keyWindow
    
    * Revert looking for inactive state for keyWindow
    
    * Update Client/UIWindowExtensions.swift comment
    Co-authored-by: roux g. buciu <11182210+electricRGB@users.noreply.github.com>

--- Client/Frontend/UIConstants+BottomInset.swift
+++ Client/Frontend/UIConstants+BottomInset.swift
@@ -8,7 +8,7 @@ extension UIConstants {
     static var BottomToolbarHeight: CGFloat {
         get {
             var bottomInset: CGFloat = 0.0
-            if let window = UIWindow.keyWindow {
+            if let window = UIWindow.attachedKeyWindow {
                 bottomInset = window.safeAreaInsets.bottom
             }
             return ToolbarHeight + bottomInset
--- Client/UIWindowExtensions.swift
+++ Client/UIWindowExtensions.swift
@@ -7,7 +7,16 @@ import Foundation
 extension UIWindow {
     static var keyWindow: UIWindow? {
         return UIApplication.shared.connectedScenes
-            .filter { $0.activationState == .foregroundActive || $0.activationState == .foregroundInactive }
+            .filter { $0.activationState == .foregroundActive }
+            .first(where: { $0 is UIWindowScene })
+            .flatMap({ $0 as? UIWindowScene })?.windows
+            .first(where: \.isKeyWindow)
+    }
+
+    /// Filter for any scenes that are attached, regardless of state (i.e. active, inactive and background)
+    static var attachedKeyWindow: UIWindow? {
+        return UIApplication.shared.connectedScenes
+            .filter { $0.activationState != .unattached }
             .first(where: { $0 is UIWindowScene })
             .flatMap({ $0 as? UIWindowScene })?.windows
             .first(where: \.isKeyWindow)
