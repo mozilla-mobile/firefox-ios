dc2909b8dd1fcad715020f264840f10e2e5e5e8e
Callie Dunn <q2r5@live.com>
2021-11-23 10:59:25 -0500

    [Cleanup] Remove unused FxA-related code (#9531)

--- Account/Account-Bridging-Header.h
+++ Account/Account-Bridging-Header.h
@@ -1,18 +1,7 @@
 #ifndef Client_Account_Bridging_Header_h
 #define Client_Account_Bridging_Header_h
 
-#include "RSAKeyPair.h"
-#include "JSONWebTokenUtils.h"
 #include "NSData+Base16.h"
-#include "NSData+Base32.h"
-#include "NSData+SHA.h"
-#include "NSData+Utils.h"
-#include "NSData+KeyDerivation.h"
-
-// These are all the ones the compiler complains are missing.
-// Some are commented out because they rely on openssl/bn.h, which we can't find
-// when we try the import. *shrug*
-#include "ASNUtils.h"
 #include "ThirdParty/ecec/include/ece.h"
 
 #import <Foundation/Foundation.h>
