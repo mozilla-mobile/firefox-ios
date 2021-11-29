b5d94f8bba6fd8db6e4809523970896c59fa43eb
isabelrios <isabelrios@gmail.com>
2021-11-29 10:28:23 +0100

    XCUITest - fix a few tests failing on full functional suite (#9560)

--- XCUITests/TabCounterTests.swift
+++ XCUITests/TabCounterTests.swift
@@ -46,13 +46,14 @@ class TabCounterTests: BaseTestCase {
         XCTAssertEqual("2", tabsOpen as? String)
 
         navigator.goto(TabTray)
-        tabsOpen = app.buttons["2"].label
-        XCTAssertTrue(app.buttons["2"].isSelected)
-        XCTAssertEqual("2", tabsOpen as? String)
 
         if isTablet {
             app.otherElements["Tabs Tray"].collectionViews.cells.element(boundBy: 0).buttons["tab close"].tap()
         } else {
+            tabsOpen = app.segmentedControls.buttons["2"].label
+            XCTAssertTrue(app.buttons["2"].isSelected)
+            XCTAssertEqual("2", tabsOpen as? String)
+
             app.otherElements["Tabs Tray"].cells.element(boundBy: 0).buttons["tab close"].tap()
         }
 
@@ -65,8 +66,11 @@ class TabCounterTests: BaseTestCase {
         XCTAssertEqual("1", tabsOpen as? String)
 
         navigator.goto(TabTray)
-        tabsOpen = app.segmentedControls["navBarTabTray"].buttons.element(boundBy:0).label
+        tabsOpen = app.segmentedControls.buttons.element(boundBy:0).label
         XCTAssertTrue(app.segmentedControls.buttons.element(boundBy: 0).isSelected)
-        XCTAssertEqual("1", tabsOpen as? String)
+        if !isTablet {
+            waitForExistence(app.segmentedControls.firstMatch, timeout: 5)
+            XCTAssertEqual("1", tabsOpen as? String)
+        }
     }
 }
