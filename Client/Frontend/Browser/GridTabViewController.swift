ec69f36bce45c13e98e8f91b4947651146a8330b
Laurie Marceau <lmarceau@mozilla.com>
2021-11-30 16:31:04 -0500

    Fix double ternary operators and #9548

--- Client/Frontend/Browser/GridTabViewController.swift
+++ Client/Frontend/Browser/GridTabViewController.swift
@@ -782,14 +782,20 @@ extension TabTrayCell {
     /// Use the display title unless it's an empty string, then use the base domain from the url
     func getTabTrayTitle(tab: Tab) -> String? {
         let baseDomain = tab.sessionData?.urls.last?.baseDomain ?? tab.url?.baseDomain
-        let urlLabel = baseDomain != nil ? baseDomain!.contains("local") ? "" : baseDomain : ""
-        return tab.displayTitle.isEmpty ? urlLabel : tab.displayTitle
+        let backUpName: String // In case display title is empty
+        if let baseDomain = baseDomain {
+            backUpName = baseDomain.contains("local") ? .AppMenuOpenHomePageTitleString : baseDomain
+        } else {
+            backUpName = ""
+        }
+        return tab.displayTitle.isEmpty ? backUpName : tab.displayTitle
     }
 
     func getA11yTitleLabel(tab: Tab) -> String? {
         var aboutComponent = ""
         if let url = tab.url, let about = InternalURL(url)?.aboutComponent { aboutComponent = about }
-        let baseName = tab.displayTitle.isEmpty ? aboutComponent.isEmpty ? getTabTrayTitle(tab: tab) : aboutComponent : tab.displayTitle
+        let backUpName = aboutComponent.isEmpty ? getTabTrayTitle(tab: tab) : aboutComponent
+        let baseName = tab.displayTitle.isEmpty ? backUpName : tab.displayTitle
 
         if isSelectedTab, let baseName = baseName, !baseName.isEmpty {
             return baseName + ". " + String.TabTrayCurrentlySelectedTabAccessibilityLabel
