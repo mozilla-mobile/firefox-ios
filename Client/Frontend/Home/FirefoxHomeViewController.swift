3af27b7b044fe1b0b3b1ba3cb5958befcfbde1d8
lmarceau <lmarceau@mozilla.com>
2021-12-10 13:00:21 -0500

    Remove #9616 [v96] Extra collectionView.reload (#9619)

--- Client/Frontend/Home/FirefoxHomeViewController.swift
+++ Client/Frontend/Home/FirefoxHomeViewController.swift
@@ -953,8 +953,6 @@ extension FirefoxHomeViewController: DataObserverDelegate {
                 jumpBackInViewModel.updateData {}
             }
         }
-
-        collectionView?.reloadData()
     }
 
     // Reloads both highlights and top sites data from their respective caches. Does not invalidate the cache.
