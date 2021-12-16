cd22847807726406029569861af80743e3b892f3
Laurie Marceau <lmarceau@mozilla.com>
2021-12-16 14:32:08 -0500

    Refresh sections when tab is removed
    We should only reload the jump back in section but since it can't self-size atm we can't do that

--- Client/Frontend/Home/FirefoxHomeViewController.swift
+++ Client/Frontend/Home/FirefoxHomeViewController.swift
@@ -285,7 +285,7 @@ class FirefoxHomeViewController: UICollectionViewController, HomePanel, FeatureF
         collectionView?.addGestureRecognizer(longPressRecognizer)
         currentTab?.lastKnownUrl?.absoluteString.hasPrefix("internal://") ?? false ? collectionView?.addGestureRecognizer(tapGestureRecognizer) : nil
 
-        let refreshEvents: [Notification.Name] = [.DynamicFontChanged, .HomePanelPrefsChanged, .DisplayThemeChanged]
+        let refreshEvents: [Notification.Name] = [.DynamicFontChanged, .HomePanelPrefsChanged, .DisplayThemeChanged, .TabClosed]
         refreshEvents.forEach { NotificationCenter.default.addObserver(self, selector: #selector(reload), name: $0, object: nil) }
     }
 
