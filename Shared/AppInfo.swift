aa23f6798b4b87874cff743d66b18267644bf9b0
Nishant Bhasin <nikieme3@gmail.com>
2021-12-20 14:35:22 -0500

    [Refactor] - Ability to set dot version in what's new link (#9674)

--- Shared/AppInfo.swift
+++ Shared/AppInfo.swift
@@ -78,11 +78,13 @@ open class AppInfo {
 
     // Return the MozWhatsNewTopic key from the Info.plist
     public static var whatsNewTopic: String? {
+        // By default we don't want to add dot version to what's new section. Set this to true if you'd like to add dot version for whats new article.
+        let shouldAddDotVersion = false
         let appVersionSplit = AppInfo.appVersion.components(separatedBy: ".")
         let majorAppVersion = appVersionSplit[0]
         var dotVersion = ""
         if appVersionSplit.count > 1, appVersionSplit[0] != "0" { dotVersion = appVersionSplit[1] }
-        let topic = "whats-new-ios-\(majorAppVersion)\(dotVersion)"
+        let topic = "whats-new-ios-\(majorAppVersion)\(shouldAddDotVersion ? dotVersion : "")"
         return topic
     }
 
