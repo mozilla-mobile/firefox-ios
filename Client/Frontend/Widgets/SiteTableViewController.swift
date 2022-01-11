4b17987ca81e664b1a6e1d9a4bbfd4a3a53107fd
junbio <66826029+junbio@users.noreply.github.com>
2022-01-11 13:19:00 -0800

    Bugfix #9746 [v97] Incorrect layout after rotating device in Bookmarks view (#9751)
    * Update SiteTableViewController.swift - Fix bug when rotating
    
    Co-authored-by: lmarceau <lmarceau@mozilla.com>

--- Client/Frontend/Widgets/SiteTableViewController.swift
+++ Client/Frontend/Widgets/SiteTableViewController.swift
@@ -139,12 +139,10 @@ class SiteTableViewController: UIViewController, UITableViewDelegate, UITableVie
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
         super.viewWillTransition(to: size, with: coordinator)
         tableView.setEditing(false, animated: false)
-        coordinator.animate(alongsideTransition: { context in
-            //The AS context menu does not behave correctly. Dismiss it when rotating.
-            if let _ = self.presentedViewController as? PhotonActionSheet {
-                self.presentedViewController?.dismiss(animated: true, completion: nil)
-            }
-        }, completion: nil)
+        // The AS context menu does not behave correctly. Dismiss it when rotating.
+        if let _ = self.presentedViewController as? PhotonActionSheet {
+            self.presentedViewController?.dismiss(animated: true, completion: nil)
+        }
     }
 
     func reloadData() {
