2bf49ac8f4f96a68c3426fb6343cdec025c4248a
isabelrios <isabelrios@gmail.com>
2022-01-14 18:19:14 +0100

    set two threesholds

--- test-fixtures/generate-metrics.sh
+++ test-fixtures/generate-metrics.sh
@@ -4,6 +4,9 @@ set -e
 
 BUILD_LOG_FILE="$1"
 METRICS_FILE="${2}/metrics.txt"
+TYPE_LOG_FILE="$3"
+THREESHOLD_UNIT_TEST=310
+THREESHOLD_XCUITEST=850
 
 # Remove if found
 rm -rf "$METRICS_FILE"
@@ -13,8 +16,16 @@ echo Counting warnings
 WARNING_COUNT=`egrep '^(\/.+:[0-9+:[0-9]+:.|warning:|ld: warning:|<unknown>:0: warning:|fatal|===)' "$BUILD_LOG_FILE" | uniq | wc -l`
 echo "warnings" $WARNING_COUNT >> "$METRICS_FILE"
 
-if [ $WARNING_COUNT \> 850 ]; then
-	exit 1
+if  [ $3 == "unit-test" ]; then
+    if [ $WARNING_COUNT \> $THREESHOLD_UNIT_TEST ]; then
+    echo "Error due to the increase number of warnings in unit test build"
+    exit 1
+    fi
+else
+    if [ $WARNING_COUNT \> $THREESHOLD_XCUITEST ]; then
+    echo "Error due to the increase number of warnings in build"
+    exit 1
+    fi
 fi
 
 # Count errors
