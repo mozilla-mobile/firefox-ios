9d59291bcfff61cb266d4f96adfbdffb2e24c2f5
isabelrios <isabelrios@gmail.com>
2022-01-17 17:15:00 +0100

    fix error to fail on purpose

--- test-fixtures/generate-metrics.sh
+++ test-fixtures/generate-metrics.sh
@@ -3,20 +3,14 @@
 set -e
 
 BUILD_LOG_FILE="$1"
-METRICS_FILE="${2}/metrics.txt"
-TYPE_LOG_FILE="$3"
+TYPE_LOG_FILE="$2"
 THREESHOLD_UNIT_TEST=110
 THREESHOLD_XCUITEST=850
 
-# Remove if found
-rm -rf "$METRICS_FILE"
 
-# Count warnings
-#echo Counting warnings
 WARNING_COUNT=`egrep '^(\/.+:[0-9+:[0-9]+:.|warning:|ld: warning:|<unknown>:0: warning:|fatal|===)' "$BUILD_LOG_FILE" | uniq | wc -l`
-#echo "warnings" $WARNING_COUNT >> "$METRICS_FILE"
 
-if  [ $3 == "unit-test" ]; then
+if  [ $2 == "unit-test" ]; then
     if [ $WARNING_COUNT -ge $THREESHOLD_UNIT_TEST ]; then
         echo "Number of warnings is $WARNING_COUNT. This is greater than $THREESHOLD_UNIT_TEST"
     else
