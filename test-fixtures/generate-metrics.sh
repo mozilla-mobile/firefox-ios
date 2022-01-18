bad5b7079e5226aa9594b1d6c982ea5d24de505e
isabelrios <isabelrios@gmail.com>
2022-01-18 19:19:26 +0100

    remove throw warning and reduce the threshold

--- test-fixtures/generate-metrics.sh
+++ test-fixtures/generate-metrics.sh
@@ -4,8 +4,8 @@ set -e
 
 BUILD_LOG_FILE="$1"
 TYPE_LOG_FILE="$2"
-THREESHOLD_UNIT_TEST=310
-THREESHOLD_XCUITEST=850
+THREESHOLD_UNIT_TEST=250
+THREESHOLD_XCUITEST=750
 
 
 WARNING_COUNT=`egrep '^(\/.+:[0-9+:[0-9]+:.|warning:|ld: warning:|<unknown>:0: warning:|fatal|===)' "$BUILD_LOG_FILE" | uniq | wc -l`
