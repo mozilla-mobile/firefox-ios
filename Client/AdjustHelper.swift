478a403a6c814adeb4f2294fd29022ad2105e159
lmarceau <lmarceau@mozilla.com>
2022-01-24 16:31:31 -0500

    Add FXIOS-3456 [v97] Adjust with SKAdNetwork (#9768)
    * Add Adjust sdk to product + basic setup
    
    * Add AdjustHelper + Take app token from plist
    
    * Make helper class final
    
    * Add license
    
    * React to existing settings to disable Adjust
    
    * Remove Adjust from Carthage and add it through SPM instead
    
    * Add GDPR forget me option
    
    * Disable third party sharing
    
    * Let SDK ping once for attribution
    
    * Apply suggestions from code review
    
    Co-authored-by: Nishant Bhasin <nikieme3@gmail.com>
    
    * Revert package-lock changes
    
    Co-authored-by: Nishant Bhasin <nikieme3@gmail.com>

---
+++ Client/AdjustHelper.swift
@@ -0,0 +1,94 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import Foundation
+import Adjust
+import Shared
+
+private let log = Logger.browserLogger
+
+final class AdjustHelper {
+
+    private static let adjustAppTokenKey = "AdjustAppToken"
+    private let profile: Profile
+
+    init(profile: Profile) {
+        self.profile = profile
+    }
+
+    func setupAdjust() {
+        guard let config = getConfig() else { return }
+
+        // Always initialize Adjust if we have a config - otherwise we cannot enable/disable it later. Their SDK must be
+        // initialized through appDidFinishLaunching otherwise it will be in a bad state.
+        Adjust.appDidLaunch(config)
+
+        AdjustHelper.setEnabled(shouldEnable)
+    }
+
+    /// Used to enable or disable Adjust SDK and it's features. We disable third party sharing by default.
+    /// If user has disabled Send Anonymous Usage Data then we ask Adjust to erase the user's data as well.
+    static func setEnabled(_ enabled: Bool) {
+        Adjust.disableThirdPartySharing()
+        Adjust.setEnabled(enabled)
+
+        if !enabled {
+            Adjust.gdprForgetMe()
+        }
+    }
+
+    // MARK: - Private
+
+    private func getConfig() -> ADJConfig? {
+        let bundle = AppInfo.applicationBundle
+        guard let appToken = bundle.object(forInfoDictionaryKey: AdjustHelper.adjustAppTokenKey) as? String, !appToken.isEmpty else {
+            log.debug("Adjust - Not enabling Adjust; Not configured in Info.plist")
+            return nil
+        }
+
+        let isProd = FeatureFlagsManager.shared.isFeatureActiveForBuild(.adjustEnvironmentProd)
+        let environment = isProd ? ADJEnvironmentProduction : ADJEnvironmentSandbox
+        let config = ADJConfig(appToken: appToken, environment: environment)
+        config?.logLevel = isProd ? ADJLogLevelSuppress : ADJLogLevelDebug
+        return config
+    }
+
+    /// Return true if Adjust should be enabled. If the user has disabled the Send Anonymous Usage Data then we only do one ping
+    /// to get the attribution and turn it off (i.e. we only enable it if we have not seen the attribution data yet).
+    private var shouldEnable: Bool {
+        return shouldTrackRetention || !hasAttribution
+    }
+
+    /// Return true if retention (session) tracking should be enabled. This follows the Send Anonymous Usage Data setting.
+    private var shouldTrackRetention: Bool {
+        return profile.prefs.boolForKey(AppConstants.PrefSendUsageData) ?? true
+    }
+
+    // MARK: - UserDefaults
+
+    private enum UserDefaultsKey: String {
+        case hasAttribution = "com.moz.adjust.hasAttribution.key"
+    }
+
+    private var hasAttribution: Bool {
+        get { UserDefaults.standard.object(forKey: UserDefaultsKey.hasAttribution.rawValue) as? Bool ?? false }
+        set { UserDefaults.standard.set(newValue, forKey: UserDefaultsKey.hasAttribution.rawValue) }
+    }
+}
+
+// MARK: - AdjustDelegate
+extension AdjustHelper: AdjustDelegate {
+
+    /// This is called when Adjust has figured out the attribution. It will call us with a summary
+    /// of all the things it knows. Like the campaign ID. We simply save a boolean that attribution
+    /// has changed so we know the single attribution ping to Adjust was done.
+    ///
+    /// We also disable Adjust based on the Send Anonymous Usage Data setting.
+    func adjustAttributionChanged(_ attribution: ADJAttribution?) {
+        hasAttribution = true
+        if !shouldEnable {
+            AdjustHelper.setEnabled(false)
+        }
+    }
+}
