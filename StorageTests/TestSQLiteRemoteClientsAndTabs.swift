78ad57b1a3a787e6578b9046ee107fa9d91d915a
lmarceau <lmarceau@mozilla.com>
2022-01-31 16:37:13 -0500

    Refactor #7867 [v98] Fix BR unit tests and fix warnings (#9885)
    * Add failure information on test case
    
    * Fix minor warnings
    
    * Try to fix testPrivateTabsAreArchived test on Bitrise
    
    * Changes to fix test - hopefully
    
    * Fix memory leak in test - change doesn't affect production code
    
    * Rename for clarity
    
    * reopen and shutdown profile + add check for numberOfTabsStored

--- StorageTests/TestSQLiteRemoteClientsAndTabs.swift
+++ StorageTests/TestSQLiteRemoteClientsAndTabs.swift
@@ -162,7 +162,7 @@ class SQLRemoteClientsAndTabsTests: XCTestCase {
             remoteDevicesToInsert.append(remoteDevice)
             clientsAndTabs.insertOrUpdateTabsForClientGUID(c.client.guid, tabs: c.tabs).succeeded()
         }
-        _ = clientsAndTabs.replaceRemoteDevices(remoteDevicesToInsert).succeeded()
+        clientsAndTabs.replaceRemoteDevices(remoteDevicesToInsert).succeeded()
 
         let f = self.expectation(description: "Get after insert.")
         clientsAndTabs.getClientsAndTabs().upon {
@@ -281,13 +281,13 @@ class SQLRemoteClientsAndTabsTests: XCTestCase {
         let device3 = RemoteDevice(id: nil, name: "Device 3 (fauly)", type: "desktop", isCurrentDevice: false, lastAccessTime: 12345678, availableCommands: [:])
         let device4 = RemoteDevice(id: "fx4", name: "Device 4 (fauly)", type: nil, isCurrentDevice: false, lastAccessTime: 12345678, availableCommands: [:])
 
-        _ = clientsAndTabs.replaceRemoteDevices([device1, device2, device3, device4]).succeeded()
+        clientsAndTabs.replaceRemoteDevices([device1, device2, device3, device4]).succeeded()
 
         let devices = clientsAndTabs.db.runQuery("SELECT * FROM remote_devices", args: nil, factory: SQLiteRemoteClientsAndTabs.remoteDeviceFactory).value.successValue!.asArray()
         XCTAssertEqual(devices.count, 1) // Fauly devices + local device were not inserted.
 
         let device5 = RemoteDevice(id: "fx5", name: "Device 5", type: "mobile", isCurrentDevice: false, lastAccessTime: 12345678, availableCommands: [:])
-        _ = clientsAndTabs.replaceRemoteDevices([device5]).succeeded()
+        clientsAndTabs.replaceRemoteDevices([device5]).succeeded()
 
         let newDevices = clientsAndTabs.db.runQuery("SELECT * FROM remote_devices", args: nil, factory: SQLiteRemoteClientsAndTabs.remoteDeviceFactory).value.successValue!.asArray()
         XCTAssertEqual(newDevices.count, 1) // replaceRemoteDevices wipes the whole list before inserting.
