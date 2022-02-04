f8f26acef79505e5a58a9c168b719f2e4f42c17d
Manuel Lorenzo <noloman@users.noreply.github.com>
2022-02-04 16:46:40 +0100

    Update Client/Frontend/Browser/BackForwardListViewController.swift
    Co-authored-by: lmarceau <lmarceau@mozilla.com>

--- Client/Frontend/Browser/BackForwardListViewController.swift
+++ Client/Frontend/Browser/BackForwardListViewController.swift
@@ -189,7 +189,7 @@ class BackForwardListViewController: UIViewController, UITableViewDataSource, UI
             } else {
                 var statusBarHeight: CGFloat = 0
                 if #available(iOS 13.0, *) {
-                    let window = UIApplication.shared.windows.filter {$0.isKeyWindow}.first
+                    let window = UIApplication.shared.windows.filter { $0.isKeyWindow }.first
                     statusBarHeight = window?.windowScene?.statusBarManager?.statusBarFrame.height ?? 0
                 } else {
                     statusBarHeight = UIApplication.shared.statusBarFrame.height
