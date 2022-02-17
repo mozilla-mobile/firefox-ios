d1b30f27d8b160bd58934ada79129016905806af
junbio <66826029+junbio@users.noreply.github.com>
2022-02-17 15:34:22 -0800

    Update TopTabsViewController.swift

--- Client/Frontend/Browser/TopTabsViewController.swift
+++ Client/Frontend/Browser/TopTabsViewController.swift
@@ -45,7 +45,6 @@ class TopTabsViewController: UIViewController {
         collectionView.bounces = false
         collectionView.clipsToBounds = true
         collectionView.accessibilityIdentifier = "Top Tabs View"
-        collectionView.semanticContentAttribute = .forceLeftToRight
         return collectionView
     }()
 
