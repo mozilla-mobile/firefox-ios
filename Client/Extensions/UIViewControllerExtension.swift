86c0706072cb505818b6a4bfe1f681979e70c507
Laurie Marceau <lmarceau@mozilla.com>
2022-02-24 10:19:30 -0500

    Customize home page show from menu fix

--- Client/Extensions/UIViewControllerExtension.swift
+++ Client/Extensions/UIViewControllerExtension.swift
@@ -70,7 +70,7 @@ extension UIViewController {
         } else {
             themedNavigationController.modalPresentationStyle = .fullScreen
         }
-        present(viewController: themedNavigationController, animated: true)
+        presentDismissIfNeeded(viewController: themedNavigationController, animated: true)
     }
     
     @objc func dismissVC() {
@@ -79,7 +79,7 @@ extension UIViewController {
 
     /// A convenience function to dismiss modal presentation views if they are
     /// currently presented.
-    func present(viewController: UIViewController, animated: Bool) {
+    func presentDismissIfNeeded(viewController: UIViewController, animated: Bool) {
         if let presentedViewController = presentedViewController {
             presentedViewController.dismiss(animated: false, completion: {
                 self.present(viewController, animated: animated, completion: nil)
