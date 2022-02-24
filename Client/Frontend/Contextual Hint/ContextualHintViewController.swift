54fa53cb9442e11ddce71401a32a36df15699318
roux g. buciu <roux@mozilla.com>
2022-02-24 12:47:13 -0500

    First pass

--- Client/Frontend/Contextual Hint/ContextualHintViewController.swift
+++ Client/Frontend/Contextual Hint/ContextualHintViewController.swift
@@ -7,23 +7,23 @@ import UIKit
 import Shared
 
 class ContextualHintViewController: UIViewController, OnViewDismissable {
-    
+
     struct UX {
         static let closeButtonSize = CGSize(width: 35, height: 35)
         static let closeButtonTrailing: CGFloat = 5
         static let closeButtonTop: CGFloat = 5
-        
+
         static let labelLeading: CGFloat = 16
         static let labelTop: CGFloat = 10
         static let labelBottom: CGFloat = 23
         static let labelTrailing: CGFloat = 3
     }
-    
+
     // MARK: - UI Elements
     private lazy var containerView: UIView = .build { [weak self] view in
         view.backgroundColor = .clear
     }
-    
+
     private lazy var closeButton: UIButton = .build { [weak self] button in
         button.setImage(UIImage(named: "find_close")?.withRenderingMode(.alwaysTemplate),
                         for: .normal)
@@ -45,7 +45,7 @@ class ContextualHintViewController: UIViewController, OnViewDismissable {
         label.textColor = .white
         label.numberOfLines = 0
     }
-    
+
     private lazy var actionButton: UIButton = .build { [weak self] button in
         button.titleLabel?.textAlignment = .left
         button.titleLabel?.numberOfLines = 0
@@ -53,14 +53,14 @@ class ContextualHintViewController: UIViewController, OnViewDismissable {
                          action: #selector(self?.performAction),
                          for: .touchUpInside)
     }
-    
+
     private lazy var stackView: UIStackView = .build { [weak self] stack in
         stack.backgroundColor = .clear
         stack.distribution = .fillProportionally
         stack.alignment = .leading
         stack.axis = .vertical
     }
-    
+
     private lazy var gradient: CAGradientLayer = {
         let gradient = CAGradientLayer()
         gradient.type = .axial
@@ -73,7 +73,7 @@ class ContextualHintViewController: UIViewController, OnViewDismissable {
         gradient.locations = [0, 0.63]
         return gradient
     }()
-    
+
     // MARK: - Properties
     private var viewModel: ContextualHintViewModel
     private var onViewSummoned: (() -> Void)? = nil
@@ -81,26 +81,26 @@ class ContextualHintViewController: UIViewController, OnViewDismissable {
     private var onActionTapped: (() -> Void)? = nil
     private var topContainerConstraint: NSLayoutConstraint?
     private var bottomContainerConstraint: NSLayoutConstraint?
-    
+
     var isPresenting: Bool = false
-    
+
     private var popupContentHeight: CGFloat {
         let spacingWidth = UX.labelLeading + UX.closeButtonSize.width + UX.closeButtonTrailing
         let labelHeight = descriptionLabel.heightForLabel(
             descriptionLabel,
             width: containerView.frame.width - spacingWidth,
-            text: viewModel.hintType.descriptionText())
-       
-        switch viewModel.hintType.isActionType() {
+            text: viewModel.descriptionText())
+
+        switch viewModel.isActionType() {
         case true:
             guard let titleLabel = actionButton.titleLabel else { fallthrough }
             let buttonHeight = titleLabel.heightForLabel(
                 titleLabel,
                 width: containerView.frame.width - spacingWidth,
-                text: viewModel.hintType.buttonActionText())
-        
+                text: viewModel.buttonActionText())
+
             return buttonHeight + labelHeight + UX.labelTop + UX.labelBottom
-            
+
         case false:
             return labelHeight + UX.labelTop + UX.labelBottom
         }
@@ -111,40 +111,40 @@ class ContextualHintViewController: UIViewController, OnViewDismissable {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     // MARK: - View lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         commonInit()
         isPresenting = true
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         onViewSummoned?()
         onViewSummoned = nil
         view.setNeedsLayout()
         view.layoutIfNeeded()
-        
+
         // Portrait orientation: lock enable
         AppDelegate.AppUtility.lockOrientation(UIInterfaceOrientationMask.portrait,
                                                andRotateTo: UIInterfaceOrientation.portrait)
     }
-    
+
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         preferredContentSize = CGSize(width: 350, height: popupContentHeight)
     }
-    
+
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         viewModel.markContextualHintPresented()
     }
-    
+
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         // Portrait orientation: lock disable
@@ -158,26 +158,27 @@ class ContextualHintViewController: UIViewController, OnViewDismissable {
         onViewDismissed?()
         onViewDismissed = nil
     }
-    
+
     private func commonInit() {
         setupView()
         setupContent()
     }
-    
+
     private func setupView() {
         gradient.frame = view.bounds
         view.layer.addSublayer(gradient)
-        
+
         stackView.addArrangedSubview(descriptionLabel)
-        if viewModel.hintType.isActionType() { stackView.addArrangedSubview(actionButton) }
-        
+        if viewModel.isActionType() { stackView.addArrangedSubview(actionButton) }
+
         containerView.addSubview(closeButton)
         containerView.addSubview(stackView)
         view.addSubview(containerView)
-        
+
         setupConstraints()
+        toggleArrowBasedConstraints()
     }
-    
+
     private func setupConstraints() {
         NSLayoutConstraint.activate([
             closeButton.topAnchor.constraint(equalTo: containerView.topAnchor),
@@ -192,8 +193,8 @@ class ContextualHintViewController: UIViewController, OnViewDismissable {
             stackView.trailingAnchor.constraint(equalTo: closeButton.leadingAnchor,
                                                 constant: -UX.labelTrailing),
             stackView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor),
-            
-           
+
+
             containerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             containerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
         ])
@@ -202,45 +203,45 @@ class ContextualHintViewController: UIViewController, OnViewDismissable {
         bottomContainerConstraint = containerView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         bottomContainerConstraint?.isActive = true
     }
-    
-    private func toggleArrowBasedConstraints(arrowDirection: UIPopoverArrowDirection = .down) {
-        let topPadding = arrowDirection == .up ? UX.labelBottom : UX.labelTop
-        let bottomPadding = arrowDirection == .up ? UX.labelTop : UX.labelBottom
-        
+
+    private func toggleArrowBasedConstraints() {
+        let topPadding = viewModel.arrowDirection == .up ? UX.labelBottom : UX.labelTop
+        let bottomPadding = viewModel.arrowDirection == .up ? UX.labelTop : UX.labelBottom
+
         topContainerConstraint?.constant = topPadding
         bottomContainerConstraint?.constant = -bottomPadding
-        
+
         view.setNeedsLayout()
         view.layoutIfNeeded()
     }
-    
+
     private func setupContent() {
-        descriptionLabel.text = viewModel.hintType.descriptionText()
-        
-        if viewModel.hintType.isActionType() {
-            
+        descriptionLabel.text = viewModel.descriptionText()
+
+        if viewModel.isActionType() {
+
             let textAttributes: [NSAttributedString.Key: Any] = [
                 .font: DynamicFontHelper.defaultHelper.preferredFont(withTextStyle: .body,
                                                                      maxSize: 28),
                 .foregroundColor: UIColor.white,
                 .underlineStyle: NSUnderlineStyle.single.rawValue
             ]
-            
+
             let attributeString = NSMutableAttributedString(
-                string: viewModel.hintType.buttonActionText(),
+                string: viewModel.buttonActionText(),
                 attributes: textAttributes
             )
-            
+
             actionButton.setAttributedTitle(attributeString, for: .normal)
         }
     }
-    
+
     // MARK: - Button Actions
     @objc private func dismissAnimated() {
         viewModel.sendTelemetryEvent(for: .closeButton)
         self.dismiss(animated: true, completion: nil)
     }
-    
+
     @objc private func performAction() {
         self.viewModel.sendTelemetryEvent(for: .performAction)
         self.dismiss(animated: true) {
@@ -248,12 +249,12 @@ class ContextualHintViewController: UIViewController, OnViewDismissable {
             self.onActionTapped = nil
         }
     }
-    
+
     // MARK: - Interface
     public func shouldPresentHint() -> Bool {
         return viewModel.shouldPresentContextualHint()
     }
-    
+
     public func configure(
         anchor: UIView,
         withArrowDirection arrowDirection: UIPopoverArrowDirection,
@@ -272,11 +273,12 @@ class ContextualHintViewController: UIViewController, OnViewDismissable {
         self.onViewDismissed = postAction
         self.onActionTapped = buttonAction
         viewModel.presentFromTimer = presentation
-        
-        toggleArrowBasedConstraints(arrowDirection: arrowDirection)
+        viewModel.arrowDirection = arrowDirection
+
+        toggleArrowBasedConstraints()
         if !viewModel.shouldPresentContextualHint() { viewModel.startTimer() }
     }
-    
+
     public func stopTimer() {
         viewModel.stopTimer()
     }
--- Client/Frontend/Contextual Hint/ContextualHintViewModel.swift
+++ Client/Frontend/Contextual Hint/ContextualHintViewModel.swift
@@ -13,42 +13,9 @@ enum CFRTelemetryEvent {
 }
 
 enum ContextualHintViewType: String {
-    typealias CFRStrings = String.ContextualHints
-    
     case jumpBackIn = "JumpBackIn"
     case inactiveTabs = "InactiveTabs"
     case toolbarLocation = "ToolbarLocation"
-    
-    func descriptionText() -> String {
-        switch self {
-        case .inactiveTabs: return CFRStrings.TabsTray.InactiveTabs.Body
-        case .jumpBackIn: return CFRStrings.FirefoxHomepage.JumpBackIn.PersonalizedHome
-            
-        case .toolbarLocation:
-            switch BrowserViewController.foregroundBVC().isBottomSearchBar {
-            case true: return CFRStrings.Toolbar.SearchBarPlacementForNewUsers
-            case false: return CFRStrings.Toolbar.SearchBarPlacementForExistingUsers
-            }
-        }
-    }
-    
-    func buttonActionText() -> String {
-        switch self {
-        case .inactiveTabs: return CFRStrings.TabsTray.InactiveTabs.Action
-        case .toolbarLocation: return CFRStrings.Toolbar.SearchBarPlacementButtonText
-        default: return ""
-        }
-    }
-    
-    func isActionType() -> Bool {
-        switch self {
-        case .inactiveTabs,
-                .toolbarLocation:
-            return true
-
-        default: return false
-        }
-    }
 }
 
 class ContextualHintViewModel {
@@ -59,16 +26,18 @@ class ContextualHintViewModel {
     var timer: Timer?
     var presentFromTimer: (() -> Void)? = nil
     private var profile: Profile
-    private var hasSentDismissEvent = false
-    
+    private var hasSentTelemetryEvent = false
+
+    var arrowDirection: UIPopoverArrowDirection?
+
     private var hasAlreadyBeenPresented: Bool {
         guard let contextualHintData = profile.prefs.boolForKey(prefsKey) else {
             return false
         }
-        
+
         return contextualHintData
     }
-    
+
     // Prevent JumpBackIn CFR from being presented if the onboarding
     // CFR has not yet been presented.
     private var canJumpBackInBePresented: Bool {
@@ -76,15 +45,15 @@ class ContextualHintViewModel {
            hasShownOboardingCFR {
             return true
         }
-        
+
         return false
     }
-    
+
     // Do not present contextual hint in landscape on iPhone
     private var isDeviceHintReady: Bool {
         !UIWindow.isLandscape || UIDevice.current.userInterfaceIdiom == .pad
     }
-    
+
     private var prefsKey: String {
         switch hintType {
         case .inactiveTabs: return CFRPrefsKeys.InactiveTabsKey.rawValue
@@ -92,13 +61,13 @@ class ContextualHintViewModel {
         case .toolbarLocation: return CFRPrefsKeys.ToolbarOnboardingKey.rawValue
         }
     }
-    
+
     // MARK: - Initializers
     init(forHintType hintType: ContextualHintViewType, with profile: Profile) {
         self.hintType = hintType
         self.profile = profile
     }
-    
+
     // MARK: - Interface
     func shouldPresentContextualHint() -> Bool {
         guard isDeviceHintReady else { return false }
@@ -114,36 +83,36 @@ class ContextualHintViewModel {
             return !hasAlreadyBeenPresented
         }
     }
-    
+
     func markContextualHintPresented() {
         profile.prefs.setBool(true, forKey: prefsKey)
     }
-    
+
     func startTimer() {
         var timeInterval: TimeInterval = 0
-        
+
         switch hintType {
         case .toolbarLocation: timeInterval = 0.5
         default: timeInterval = 1.25
         }
-        
+
         timer?.invalidate()
-        
+
         timer = Timer.scheduledTimer(timeInterval: timeInterval,
                                      target: self,
                                      selector: #selector(presentHint),
                                      userInfo: nil,
                                      repeats: false)
     }
-    
+
     func stopTimer() {
         timer?.invalidate()
     }
-    
+
     // MARK: - Telemetry
     func sendTelemetryEvent(for eventType: CFRTelemetryEvent) {
         let extra = [TelemetryWrapper.EventExtraKey.cfrType.rawValue: hintType.rawValue]
-        
+
         switch eventType {
         case .closeButton:
             TelemetryWrapper.recordEvent(category: .action,
@@ -151,10 +120,10 @@ class ContextualHintViewModel {
                                          object: .contextualHint,
                                          value: .dismissCFRFromButton,
                                          extras: extra)
-            hasSentDismissEvent = true
-            
+            hasSentTelemetryEvent = true
+
         case .tapToDismiss:
-            if hasSentDismissEvent { return }
+            if hasSentTelemetryEvent { return }
             TelemetryWrapper.recordEvent(category: .action,
                                          method: .tap,
                                          object: .contextualHint,
@@ -167,10 +136,10 @@ class ContextualHintViewModel {
                                          object: .contextualHint,
                                          value: .pressCFRActionButton,
                                          extras: extra)
-            hasSentDismissEvent = true
+            hasSentTelemetryEvent = true
         }
     }
-    
+
     // MARK: - Present
     @objc private func presentHint() {
         timer?.invalidate()
@@ -178,5 +147,39 @@ class ContextualHintViewModel {
         presentFromTimer?()
         presentFromTimer = nil
     }
-}
 
+
+    // MARK: - Type Text
+    typealias CFRStrings = String.ContextualHints
+
+    func descriptionText() -> String {
+        switch hintType {
+        case .inactiveTabs: return CFRStrings.TabsTray.InactiveTabs.Body
+        case .jumpBackIn: return CFRStrings.FirefoxHomepage.JumpBackIn.PersonalizedHome
+
+        case .toolbarLocation:
+            switch BrowserViewController.foregroundBVC().isBottomSearchBar {
+            case true: return CFRStrings.Toolbar.SearchBarPlacementForNewUsers
+            case false: return CFRStrings.Toolbar.SearchBarPlacementForExistingUsers
+            }
+        }
+    }
+
+    func buttonActionText() -> String {
+        switch hintType {
+        case .inactiveTabs: return CFRStrings.TabsTray.InactiveTabs.Action
+        case .toolbarLocation: return CFRStrings.Toolbar.SearchBarPlacementButtonText
+        default: return ""
+        }
+    }
+
+    func isActionType() -> Bool {
+        switch hintType {
+        case .inactiveTabs,
+                .toolbarLocation:
+            return true
+
+        default: return false
+        }
+    }
+}
