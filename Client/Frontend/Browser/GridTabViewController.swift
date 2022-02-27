91bb7ba3f51f0e1a3dd0c573d2735238948b5e1e
Callie Dunn <q2r5@live.com>
2022-02-27 16:38:22 -0500

    #10053 - The distance between tabs tray buttons and the first tab is too large

--- Client/Frontend/Browser/GridTabViewController.swift
+++ Client/Frontend/Browser/GridTabViewController.swift
@@ -704,10 +704,13 @@ fileprivate class TabLayoutDelegate: NSObject, UICollectionViewDelegateFlowLayou
         }
     }
 
-    @objc func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         switch TabDisplaySection(rawValue: section) {
         case .regularTabs, .none:
             return UIEdgeInsets(top: GridTabTrayControllerUX.Margin, left: GridTabTrayControllerUX.Margin + collectionView.safeAreaInsets.left, bottom: GridTabTrayControllerUX.Margin, right: GridTabTrayControllerUX.Margin + collectionView.safeAreaInsets.right)
+        case .inactiveTabs:
+            guard !tabDisplayManager.isPrivate, tabDisplayManager.inactiveViewModel?.inactiveTabs.count ?? 0 > 0 else { return .zero }
+            return UIEdgeInsets(equalInset: GridTabTrayControllerUX.Margin)
         default:
             return UIEdgeInsets(equalInset: GridTabTrayControllerUX.Margin)
         }
