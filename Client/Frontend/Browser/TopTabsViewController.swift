a817c61efbbd16b76cfc878cb8159cc7dab86c92
lmarceau <lmarceau@mozilla.com>
2022-03-04 15:28:57 -0500

    Bugfix #10142 [v99] iPad JumpBackIn crash fix with closed tab (#10150)
    * Send notification that top tabs has closed a tab, so firefox home view controller can react
    
    * Inverse order

--- Client/Frontend/Browser/TopTabsViewController.swift
+++ Client/Frontend/Browser/TopTabsViewController.swift
@@ -267,6 +267,7 @@ extension TopTabsViewController: TabDisplayer {
 extension TopTabsViewController: TopTabCellDelegate {
     func tabCellDidClose(_ cell: UICollectionViewCell) {
         topTabDisplayManager.closeActionPerformed(forCell: cell)
+        NotificationCenter.default.post(name: .TabClosed, object: nil)
     }
 }
 
