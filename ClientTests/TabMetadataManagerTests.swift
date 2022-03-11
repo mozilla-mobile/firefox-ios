8c0683f40fc5cc842a358bb4552276e5310e51e4
yoanarios <yriosdiaz@mozilla.com>
2022-03-11 10:12:27 -0500

    Add unit test for shouldUpdateSearch term and update observing for openURL case

--- ClientTests/TabMetadataManagerTests.swift
+++ ClientTests/TabMetadataManagerTests.swift
@@ -12,6 +12,7 @@ class TabMetadataManagerTests: XCTestCase {
     
     private var profile: MockProfile!
     private var tabManager: TabManager!
+    private var manager: TabMetadataManager!
 
     override func setUpWithError() throws {
         super.setUp()
@@ -19,6 +20,7 @@ class TabMetadataManagerTests: XCTestCase {
         profile = MockProfile(databasePrefix: "metadata_recording_tests")
         profile._reopen()
         tabManager = TabManager(profile: profile, imageStore: nil)
+        manager = TabMetadataManager(profile: profile)
     }
 
     override func tearDownWithError() throws {
@@ -27,21 +29,74 @@ class TabMetadataManagerTests: XCTestCase {
         profile._shutdown()
         profile = nil
         tabManager = nil
+        manager = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testShouldUpdateSearchTermData() throws {
+        let stringUrl = "www.mozilla.org"
+        
+        manager.tabGroupData.tabAssociatedSearchTerm = "test search"
+        manager.tabGroupData.tabAssociatedSearchUrl = "internal://home"
+        manager.tabGroupData.tabAssociatedNextUrl = ""
+        
+        let shouldUpdate = manager.shouldUpdateSearchTermData(webViewUrl: stringUrl)
+        XCTAssertTrue(shouldUpdate)
     }
+    
+    func testNotShouldUpdateSearchTermData_NilNextUrl() throws {
+        let shouldUpdate = manager.shouldUpdateSearchTermData(webViewUrl: nil)
+        XCTAssertFalse(shouldUpdate)
+    }
+    
+    func testNotShouldUpdateSearchTermData_SameSearchURL() throws {
+        let stringUrl = "www.mozilla.org"
+        
+        manager.tabGroupData.tabAssociatedSearchTerm = "test search"
+        manager.tabGroupData.tabAssociatedSearchUrl = stringUrl
+        manager.tabGroupData.tabAssociatedNextUrl = "www.apple.com"
+        
+        let shouldUpdate = manager.shouldUpdateSearchTermData(webViewUrl: stringUrl)
+        XCTAssertFalse(shouldUpdate)
+    }
+    
+    func testNotShouldUpdateSearchTermData_SameNextURL() throws {
+        let stringUrl = "www.mozilla.org"
+        
+        manager.tabGroupData.tabAssociatedSearchTerm = "test search"
+        manager.tabGroupData.tabAssociatedSearchUrl = "www.apple.com"
+        manager.tabGroupData.tabAssociatedNextUrl = stringUrl
+        
+        let shouldUpdate = manager.shouldUpdateSearchTermData(webViewUrl: stringUrl)
+        XCTAssertFalse(shouldUpdate)
+    }
+    
+    func testUpdateTimerAndObserving_ForOpenURLOnly() throws {
+        let stringUrl = "www.mozilla.org"
+        let title = "Mozilla title"
+        
+        emptyDB()
+        manager.updateTimerAndObserving(state: .openURLOnly, searchTerm: title, searchProviderUrl: stringUrl, nextUrl: "")
+        
+        let hhWeigths = HistoryHighlightWeights(viewTime: 10.0,
+                                                frequency: 4.0)
+        let singleItemRead = profile.places.getHistoryMetadataSince(since: 0).value
+        profile.places.getHighlights(weights:hhWeigths, limit: 1000).uponQueue(.main) { result in
+            
+            guard let ASHighlights = result.successValue, !ASHighlights.isEmpty else { return completion(nil) }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+            XCTAssertTrue(singleItemRead.isSuccess)
+            XCTAssertNotNil(singleItemRead.successValue)
+            XCTAssertEqual(singleItemRead.successValue!.count, 1)
+            XCTAssertEqual(singleItemRead.successValue![0].url, stringUrl)
+            XCTAssertEqual(singleItemRead.successValue![0].title?.lowercased(), title)
+            completion(ASHighlights)
         }
     }
-
+    
+    // MARK: - Helper functions
+    private func emptyDB() {
+        XCTAssertTrue(profile.places.deleteHistoryMetadataOlderThan(olderThan: 0).value.isSuccess)
+        XCTAssertTrue(profile.places.deleteHistoryMetadataOlderThan(olderThan: INT64_MAX).value.isSuccess)
+        XCTAssertTrue(profile.places.deleteHistoryMetadataOlderThan(olderThan: -1).value.isSuccess)
+    }
 }
