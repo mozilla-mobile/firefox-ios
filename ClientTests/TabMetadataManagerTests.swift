fe7f184a1a4f4d3b6e7e233f596d385d43861c0f
yoanarios <yriosdiaz@mozilla.com>
2022-03-14 14:28:59 -0400

    Remove updateTimer temporarily unit test because fail randomly

--- ClientTests/TabMetadataManagerTests.swift
+++ ClientTests/TabMetadataManagerTests.swift
@@ -11,25 +11,18 @@ import Shared
 class TabMetadataManagerTests: XCTestCase {
     
     private var profile: MockProfile!
-    private var tabManager: TabManager!
     private var manager: TabMetadataManager!
-    private let weigths = HistoryHighlightWeights(viewTime: 10.0, frequency: 4.0)
 
     override func setUp() {
         super.setUp()
-
-        profile = MockProfile(databasePrefix: "metadata_recording_tests")
-        profile._reopen()
-        tabManager = TabManager(profile: profile, imageStore: nil)
+        
+        profile = MockProfile(databasePrefix: "historyHighlights_tests")
         manager = TabMetadataManager(profile: profile)
     }
 
     override func tearDown() {
         super.tearDown()
-        
-        profile._shutdown()
-        profile = nil
-        tabManager = nil
+
         manager = nil
     }
 
@@ -69,50 +62,5 @@ class TabMetadataManagerTests: XCTestCase {
         
         let shouldUpdate = manager.shouldUpdateSearchTermData(webViewUrl: stringUrl)
         XCTAssertFalse(shouldUpdate)
-    }
-    
-    func testUpdateTimerAndObserving_ForOpenURLOnly() throws {
-        let stringUrl = "https://www.mozilla.org/"
-        let title = "mozilla title"
-//        let expectation = expectation(description: "wait for database recording")
-        manager.updateTimerAndObserving(state: .openURLOnly, tabTitle: title)
-        
-        // Waiting 5 seconds for results to be available
-        DispatchQueue.main.asyncAfter(deadline: .now() + 8) {
-            let hhWeigths = HistoryHighlightWeights(viewTime: 10.0, frequency: 4.0)
-            let result = self.profile.places.getHighlights(weights: hhWeigths, limit: 1000).value
-            
-            XCTAssertTrue(result.isSuccess)
-            XCTAssertNotNil(result.successValue)
-            XCTAssertEqual(result.successValue!.count, 1)
-            XCTAssertEqual(result.successValue![0].url, stringUrl)
-            XCTAssertEqual(result.successValue![0].title?.lowercased(), title)
-//            expectation.fulfill()
-        }
-//        waitForExpectations(timeout: 10, handler: nil)
-    }
-    
-    func testUpdateObservationTitle_ForOpenURLOnly() throws {
-        let stringUrl = "https://www.developer.org/"
-        let title = "Updated Title"
-        
-//        let expectation = expectation(description: "wait for database recording")
-        manager.tabGroupData.tabHistoryCurrentState = TabGroupTimerState.openURLOnly.rawValue
-        manager.tabGroupData.tabAssociatedSearchUrl = stringUrl
-        manager.updateObservationTitle(title)
-        
-        // Waiting 5 seconds for results to be available
-        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
-            
-            let result = self.profile.places.getHighlights(weights: self.weigths, limit: 1000).value
-            
-            XCTAssertTrue(result.isSuccess)
-            XCTAssertNotNil(result.successValue)
-            XCTAssertEqual(result.successValue!.count, 1)
-            XCTAssertEqual(result.successValue![0].url, stringUrl)
-            XCTAssertEqual(result.successValue![0].title?.lowercased(), title)
-//            expectation.fulfill()
-        }
-//        waitForExpectations(timeout: 5, handler: nil)
     }
 }
