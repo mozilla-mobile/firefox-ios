c05fc68cc159820c783779f05b6b9c5191f02050
Daniela Arcese <darcese@mozilla.com>
2022-03-16 17:03:45 -0400

    Update version update script

--- update_version.sh
+++ update_version.sh
@@ -1,4 +1,4 @@
-if [[ "${#}" -ne 1 ]]
+if [[ "${#}" -ne 2 ]]
 then
   echo 'arg missing for updated version number'
   exit 1
@@ -6,4 +6,5 @@ fi
 
 # If needed, modify the starting version number locally
 # This script is mainly a reference as to what needs to be updated
-ls Client/Info.plist Extensions/*/*Info.plist | xargs perl -pi -e "s/0\.0\.1/$1/g" 
+ls Client/Info.plist Extensions/*/*Info.plist | xargs perl -pi -e "s/0\.0\.1/$2/g"
+perl -pi -e "s/$1/$2/g" bitrise.yml
