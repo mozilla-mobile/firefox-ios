884bc57106c211c2a5f768cea69356e11c93f88e
isabelrios <isabelrios@gmail.com>
2022-03-29 10:45:10 +0200

    Refactor #10293 [v99] - Fix and automate the import strings process

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,7 +10,8 @@
 
 getLocale() {
   echo "Getting locale..."
-  git clone https://github.com/boek/ios-l10n-scripts.git -b new_tool || exit 1
+  rm -rf LocalizationTools
+  git clone https://github.com/mozilla-mobile/LocalizationTools.git || exit 1
 
   echo "Creating firefoxios-l10n Git repo"
   rm -rf firefoxios-l10n
@@ -19,19 +20,19 @@ getLocale() {
 
 if [ "$1" == "--force" ]; then
     rm -rf firefoxios-l10n
-    rm -rf ios-l10n-scripts
+    rm -rf LocalizationTools
 fi
 
 if [ "$1" == "--importLocales" ]; then
   # Import locales
-  if [ -d "/firefoxios-l10n" ] && [ -d "/ios-l10n-scripts" ]; then
+  if [ -d "/firefoxios-l10n" ] && [ -d "/LocalizationTools" ]; then
       echo "l10n directories found. Not downloading scripts."
   else
       echo "l10n directory not found. Downloading repo and scripts."
       getLocale
   fi
 
-  ./ios-l10n-scripts/ios-l10n-tools --project-path Client.xcodeproj --l10n-project-path ./firefoxios-l10n --import
+  ./import-strings.sh
   exit 0
 fi
 
