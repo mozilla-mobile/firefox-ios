ba539d7bbd77e28850cb78922e7510019c7f388d
lmarceau <lmarceau@mozilla.com>
2022-04-12 18:56:14 -0400

    Revert "Bugfix fx3992 [v100] Favicons background color (#10458)"
    This reverts commit bdc1e009354679b7b811748a0d55383452b44ef4.

--- ClientTests/XCTestCaseExtensions.swift
+++ ClientTests/XCTestCaseExtensions.swift
@@ -27,10 +27,4 @@ extension XCTestCase {
             wait(0.1)
         }
     }
-
-    func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line) {
-        addTeardownBlock { [weak instance] in
-            XCTAssertNil(instance, "Instance should have been deallocated, potential memory leak.", file: file, line: line)
-        }
-    }
 }
