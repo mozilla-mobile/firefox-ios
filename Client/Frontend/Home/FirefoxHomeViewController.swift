2339335ff87c83b5bd957441eaec1cbb7b641cc0
Laurie Marceau <lmarceau@mozilla.com>
2022-04-12 18:42:14 -0400

    Add tests for Firefox home view controller leaks + fix it

--- Client/Frontend/Home/FirefoxHomeViewController.swift
+++ Client/Frontend/Home/FirefoxHomeViewController.swift
@@ -56,7 +56,10 @@ class FirefoxHomeViewController: UICollectionViewController, HomePanel {
         super.init(collectionViewLayout: flowLayout)
 
         contextMenuHelper.delegate = self
-        contextMenuHelper.getPopoverSourceRect = getPopoverSourceRect
+        contextMenuHelper.getPopoverSourceRect = { [weak self] popoverView in
+            guard let self = self else { return CGRect() }
+            return self.getPopoverSourceRect(sourceView: popoverView)
+        }
 
         viewModel.delegate = self
         collectionView?.delegate = self
