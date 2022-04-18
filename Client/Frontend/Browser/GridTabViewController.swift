a31da7b0e4aef671cda0b0eeb71fb6366b4c6aa6
roux g. buciu <roux@mozilla.com>
2022-04-18 16:55:43 -0400

    Fix bug

--- Client/Frontend/Browser/GridTabViewController.swift
+++ Client/Frontend/Browser/GridTabViewController.swift
@@ -427,10 +427,9 @@ extension GridTabViewController {
     }
 
     func dismissTabTray() {
-        _ = self.navigationController?.dismiss(animated: true, completion: nil)
+        self.navigationController?.dismiss(animated: true, completion: nil)
         TelemetryWrapper.recordEvent(category: .action, method: .close, object: .tabTray)
     }
-
 }
 
 // MARK: - App Notifications
@@ -460,6 +459,9 @@ extension GridTabViewController {
 extension GridTabViewController: TabSelectionDelegate {
     func didSelectTabAtIndex(_ index: Int) {
         if let tab = tabDisplayManager.dataStore.at(index) {
+            if tab.isFxHomeTab {
+                notificationCenter.post(name: .TabsTrayDidSelectHomeTab, object: nil)
+            }
             tabManager.selectTab(tab)
             dismissTabTray()
         }
