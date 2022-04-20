3afe94ab6c14e0e3617f54fae799eb118eb87d13
Laurie Marceau <lmarceau@mozilla.com>
2022-04-20 17:33:36 -0400

    Write down test cases to cover

---
+++ ClientTests/ContileProviderTests.swift
@@ -0,0 +1,19 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import Foundation
+
+import XCTest
+
+@testable import Client
+
+class ContileProviderTests: XCTestCase {
+    // TODO: Test getting error
+    // TODO: Test getting a 403
+    // TODO: Test getting no data
+    // TODO: Test getting wrong JSON
+    // TODO: Test ordering position
+    // TODO: Test ordering position with partly nil
+    // TODO: Test success
+}
