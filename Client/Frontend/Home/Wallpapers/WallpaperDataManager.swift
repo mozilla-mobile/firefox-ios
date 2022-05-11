d7521d30d3ea59556d71b25fbc012c324ba983de
roux g. buciu <11182210+electricRGB@users.noreply.github.com>
2022-05-11 15:36:41 -0400

    Refactor fx3791 [v101] Collate wallpaper collections (#10724)

--- Client/Frontend/Home/Wallpapers/WallpaperDataManager.swift
+++ Client/Frontend/Home/Wallpapers/WallpaperDataManager.swift
@@ -138,27 +138,8 @@ struct WallpaperDataManager {
     private func allSpecialCollections() -> [WallpaperCollection]? {
         var specialCollections = [WallpaperCollection]()
 
-        let houseExpiryDate = Calendar.current.date(
-            from: DateComponents(year: 2022, month: 5, day: 1))
-        let projectHouse = WallpaperCollection(
-            wallpaperFileNames: [WallpaperID(name: "trRed",
-                                             accessibilityLabel: "Turning Red wallpaper, giant red panda"),
-                                 WallpaperID(name: "trGroup",
-                                             accessibilityLabel: "Turning Red wallpaper, Mei and friends")],
-            ofType: .themed(type: .projectHouse),
-            expiringOn: houseExpiryDate,
-            limitedToLocales: ["en_US", "es_US"])
-
-        specialCollections.append(projectHouse)
-
-        let v100Celebration = WallpaperCollection(
-            wallpaperFileNames: [WallpaperID(name: "beachVibes",
-                                             accessibilityLabel: accessibilityIDs.FxBeachHillsWallpaper),
-                                 WallpaperID(name: "twilightHills",
-                                             accessibilityLabel: accessibilityIDs.FxTwilightHillsWallpaper)],
-            ofType: .themed(type: .v100Celebration))
-
-        specialCollections.append(v100Celebration)
+        specialCollections.append(projectHouseCollection())
+        specialCollections.append(v100CelebrationCollection())
 
         return specialCollections.isEmpty ? nil : specialCollections
     }
@@ -170,5 +151,34 @@ struct WallpaperDataManager {
         else { return }
 
         resourceManager.verifyResources(for: specialCollections)
+    }
+}
+
+// MARK: - Wallpaper Collections
+// These collections should remain in code for as long as possible.
+// They are required for not just downloading wallpapers, but also
+// deleting them from the disk once they expire.
+extension WallpaperDataManager {
+    private func projectHouseCollection() -> WallpaperCollection {
+        let houseExpiryDate = Calendar.current.date(
+            from: DateComponents(year: 2022, month: 5, day: 1))
+
+        return WallpaperCollection(
+            wallpaperFileNames: [WallpaperID(name: "trRed",
+                                             accessibilityLabel: "Turning Red wallpaper, giant red panda"),
+                                 WallpaperID(name: "trGroup",
+                                             accessibilityLabel: "Turning Red wallpaper, Mei and friends")],
+            ofType: .themed(type: .projectHouse),
+            expiringOn: houseExpiryDate,
+            limitedToLocales: ["en_US", "es_US"])
+    }
+
+    private func v100CelebrationCollection() -> WallpaperCollection {
+        return WallpaperCollection(
+            wallpaperFileNames: [WallpaperID(name: "beachVibes",
+                                             accessibilityLabel: accessibilityIDs.FxBeachHillsWallpaper),
+                                 WallpaperID(name: "twilightHills",
+                                             accessibilityLabel: accessibilityIDs.FxTwilightHillsWallpaper)],
+            ofType: .themed(type: .v100Celebration))
     }
 }
