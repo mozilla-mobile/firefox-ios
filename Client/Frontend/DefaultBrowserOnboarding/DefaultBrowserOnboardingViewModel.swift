6254b049b5e90de6369952aa5f27530a49dda420
Litianu Razvan <litianu.razvan@gmail.com>
2022-05-12 17:35:08 +0300

    Bugfix #10134 [v101] VoiceOver should announce "Close button" instead of "Button" (#10713)
    * VoiceOver should announce "Close button" instead of "Button"
    
    * Update string with separate key and version
    
    * Change variable strings to HomeTabBanner
    
    * Update comment to mention home tab banner

--- Client/Frontend/DefaultBrowserOnboarding/DefaultBrowserOnboardingViewModel.swift
+++ Client/Frontend/DefaultBrowserOnboarding/DefaultBrowserOnboardingViewModel.swift
@@ -54,14 +54,14 @@ class DefaultBrowserOnboardingViewModel {
 
     private func setupModel() {
         model = DefaultBrowserOnboardingModel(
-            titleText: String.FirefoxHomepage.HomeTabBanner.EvergreenMessage.DefaultBrowserTitle,
+            titleText: String.FirefoxHomepage.HomeTabBanner.EvergreenMessage.HomeTabBannerTitle,
             descriptionText: descriptionText,
             imageText: String.DefaultBrowserOnboardingScreenshot
         )
     }
 
     private var descriptionText: [String] {
-        [String.FirefoxHomepage.HomeTabBanner.EvergreenMessage.DefaultBrowserDescription,
+        [String.FirefoxHomepage.HomeTabBanner.EvergreenMessage.HomeTabBannerDescription,
          String.DefaultBrowserOnboardingDescriptionStep1,
          String.DefaultBrowserOnboardingDescriptionStep2,
          String.DefaultBrowserOnboardingDescriptionStep3]
