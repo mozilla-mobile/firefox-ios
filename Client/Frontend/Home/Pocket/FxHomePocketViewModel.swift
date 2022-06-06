123beafb691a8dd78d2a7d193d9ae12cbd2a1ef3
Razvan Litianu <litianu.razvan@gmail.com>
2022-06-06 13:08:37 +0300

    Fix update with sponsored content

--- Client/Frontend/Home/Pocket/FxHomePocketViewModel.swift
+++ Client/Frontend/Home/Pocket/FxHomePocketViewModel.swift
@@ -105,31 +105,40 @@ class FxHomePocketViewModel {
 
     // MARK: - Private
 
+    func updatePocketStoryViewModels(with stories: [PocketStory]) {
+        self.pocketStoriesViewModels = []
+        for story in stories {
+            self.bind(pocketStoryViewModel: .init(story: story))
+        }
+    }
+
     private func getPocketSites(completion: @escaping () -> Void) {
         pocketAPI
             .globalFeed(items: UX.numberOfItemsInSection)
             .uponQueue(.main) { [weak self] (pocketStory: [PocketFeedStory]) -> Void in
                 var globalTemp = pocketStory.map(PocketStory.init)
+
+                // Check if sponsored stories are enabled, otherwise drop api call
+                guard self?.featureFlags.isFeatureEnabled(.sponsoredPocket, checking: .userOnly)  == true else {
+                    self?.updatePocketStoryViewModels(with: globalTemp)
+                    completion()
+                    return
+                }
+
                 self?.pocketAPI.sponsoredFeed().uponQueue(.main) { sponsored in
-                    if self?.featureFlags.isFeatureEnabled(.sponsoredPocket, checking: .userOnly)  == true {
-                        // Convert sponsored feed to PocketStory, take the desired number of sponsored stories
-                        var sponsoredTemp = sponsored.map(PocketStory.init).prefix(UX.numberOfSponsoredItemsInSection)
+                    // Convert sponsored feed to PocketStory, take the desired number of sponsored stories
+                    var sponsoredTemp = sponsored.map(PocketStory.init).prefix(UX.numberOfSponsoredItemsInSection)
 
-                        // Making sure we insert a sponsored story at a valid index
-                        let firstIndex = min(UX.indexOfFirstSponsoredItem, globalTemp.endIndex)
-                        sponsoredTemp.first.map { globalTemp.insert($0, at: firstIndex) }
-                        sponsoredTemp.removeFirst()
+                    // Making sure we insert a sponsored story at a valid index
+                    let firstIndex = min(UX.indexOfFirstSponsoredItem, globalTemp.endIndex)
+                    sponsoredTemp.first.map { globalTemp.insert($0, at: firstIndex) }
+                    sponsoredTemp.removeFirst()
 
-                        let secondIndex = min(UX.indexOfSecondSponsoredItem, globalTemp.endIndex)
-                        sponsoredTemp.first.map { globalTemp.insert($0, at: secondIndex) }
-                        sponsoredTemp.removeFirst()
-                    }
+                    let secondIndex = min(UX.indexOfSecondSponsoredItem, globalTemp.endIndex)
+                    sponsoredTemp.first.map { globalTemp.insert($0, at: secondIndex) }
+                    sponsoredTemp.removeFirst()
 
-                    self?.pocketStoriesViewModels = []
-                    // Add the story in the view models list
-                    for story in globalTemp {
-                        self?.bind(pocketStoryViewModel: .init(story: story))
-                    }
+                    self?.updatePocketStoryViewModels(with: globalTemp)
                     completion()
                 }
             }
