4d90b4bcf520b9800c8d1cd7545b90e3941c3c6a
yoanarios <yriosdiaz@mozilla.com>
2022-06-07 16:18:07 -0400

    Add FXIOS-3981 [v102] - Add default cases to ViewModel: Wallpapers, Home page settings, Sync (#10901)

---
+++ Client/Frontend/Components/ResizableButton.swift
@@ -0,0 +1,46 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import UIKit
+
+class ResizableButton: UIButton {
+
+    struct UX {
+        static let buttonEdgeSpacing: CGFloat = 8
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func commonInit() {
+        titleLabel?.numberOfLines = 0
+        titleLabel?.lineBreakMode = .byWordWrapping
+        contentEdgeInsets = UIEdgeInsets(top: 0,
+                                         left: UX.buttonEdgeSpacing,
+                                         bottom: 0,
+                                         right: UX.buttonEdgeSpacing)
+    }
+
+    override var intrinsicContentSize: CGSize {
+        guard let title = titleLabel else {
+            return super.intrinsicContentSize
+        }
+        let size = title.intrinsicContentSize
+        return CGSize(width: size.width + contentEdgeInsets.left + contentEdgeInsets.right, height: size.height + contentEdgeInsets.top + contentEdgeInsets.bottom)
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        guard let title = titleLabel else { return }
+
+        titleLabel?.preferredMaxLayoutWidth = title.frame.size.width
+    }
+}
