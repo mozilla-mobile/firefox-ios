b61ebdd88d657da0a3b4c6d6d1a463fc421d274e
roux g. buciu <11182210+electricRGB@users.noreply.github.com>
2022-06-15 10:46:34 -0400

    Add FXIOS-3147 [v102] Context menu for history highlights (#11003)

--- Client/Frontend/Home/FirefoxHomeViewController.swift
+++ Client/Frontend/Home/FirefoxHomeViewController.swift
@@ -467,7 +467,15 @@ private extension FirefoxHomeViewController {
                 return
             }
 
-            self?.homePanelDelegate?.homePanel(didSelectURL: url, visitType: .link, isGoogleTopSite: false)
+            self?.homePanelDelegate?.homePanel(didSelectURL: url,
+                                               visitType: .link,
+                                               isGoogleTopSite: false)
+        }
+
+        viewModel.historyHighlightsViewModel.historyHighlightLongPressHandler = { [weak self] (highlightItem, sourceView) in
+            self?.contextMenuHelper.presentContextMenu(for: highlightItem,
+                                                       with: sourceView,
+                                                       sectionType: .historyHighlights)
         }
 
         viewModel.historyHighlightsViewModel.headerButtonAction = { [weak self] button in
@@ -630,6 +638,7 @@ extension FirefoxHomeViewController: FirefoxHomeViewModelDelegate {
     func reloadSection(section: FXHomeViewModelProtocol) {
         ensureMainThread { [weak self] in
             guard let self = self else { return }
+            self.viewModel.updateEnabledSections()
             self.viewModel.reloadSection(section, with: self.collectionView)
         }
     }
--- Client/Frontend/Home/HomePanelContextMenu.swift
+++ Client/Frontend/Home/HomePanelContextMenu.swift
@@ -10,9 +10,15 @@ protocol HomePanelContextMenu {
     func getContextMenuActions(for site: Site, with sourceView: UIView?, sectionType: FirefoxHomeSectionType) -> [PhotonRowActions]?
     func presentContextMenu(for site: Site, with sourceView: UIView?, sectionType: FirefoxHomeSectionType)
     func presentContextMenu(for site: Site, with sourceView: UIView?, sectionType: FirefoxHomeSectionType, completionHandler: @escaping () -> PhotonActionSheet?)
+
+    func getContextMenuActions(for highlightItem: HighlightItem, with sourceView: UIView?, sectionType: FirefoxHomeSectionType) -> [PhotonRowActions]?
+    func presentContextMenu(for highlightItem: HighlightItem, with sourceView: UIView?, sectionType: FirefoxHomeSectionType)
+    func presentContextMenu(for highlightItem: HighlightItem, with sourceView: UIView?, sectionType: FirefoxHomeSectionType, completionHandler: @escaping () -> PhotonActionSheet?)
+
 }
 
 extension HomePanelContextMenu {
+    // MARK: Site
     func presentContextMenu(for site: Site, with sourceView: UIView?, sectionType: FirefoxHomeSectionType) {
         presentContextMenu(for: site, with: sourceView, sectionType: sectionType, completionHandler: {
             return self.contextMenu(for: site, with: sourceView, sectionType: sectionType)
@@ -20,9 +26,51 @@ extension HomePanelContextMenu {
     }
 
     func contextMenu(for site: Site, with sourceView: UIView?, sectionType: FirefoxHomeSectionType) -> PhotonActionSheet? {
-        guard let actions = getContextMenuActions(for: site, with: sourceView, sectionType: sectionType) else { return nil }
+        guard let actions = getContextMenuActions(for: site,
+                                                  with: sourceView,
+                                                  sectionType: sectionType)
+        else { return nil }
 
-        let viewModel = PhotonActionSheetViewModel(actions: [actions], site: site, modalStyle: .overFullScreen)
+        let viewModel = PhotonActionSheetViewModel(actions: [actions],
+                                                   site: site,
+                                                   modalStyle: .overFullScreen)
+        let contextMenu = PhotonActionSheet(viewModel: viewModel)
+        contextMenu.modalTransitionStyle = .crossDissolve
+
+        let generator = UIImpactFeedbackGenerator(style: .heavy)
+        generator.impactOccurred()
+
+        return contextMenu
+    }
+
+    // MARK: - Highlight Item
+    func presentContextMenu(for highlightItem: HighlightItem, with sourceView: UIView?, sectionType: FirefoxHomeSectionType) {
+        presentContextMenu(for: highlightItem, with: sourceView, sectionType: sectionType, completionHandler: {
+            return self.contextMenu(for: highlightItem, with: sourceView, sectionType: sectionType)
+        })
+    }
+    func contextMenu(for highlightItem: HighlightItem, with sourceView: UIView?, sectionType: FirefoxHomeSectionType) -> PhotonActionSheet? {
+        guard let actions = getContextMenuActions(for: highlightItem,
+                                                  with: sourceView,
+                                                  sectionType: sectionType)
+        else { return nil }
+
+        var viewModel: PhotonActionSheetViewModel
+
+        switch highlightItem.type {
+        case .item:
+            guard let url = highlightItem.siteUrl?.absoluteString else { return nil }
+            let site = Site(url: url, title: highlightItem.displayTitle)
+
+            viewModel = PhotonActionSheetViewModel(actions: [actions],
+                                                   site: site,
+                                                   modalStyle: .overFullScreen)
+        case .group:
+            viewModel = PhotonActionSheetViewModel(actions: [actions],
+                                                   title: highlightItem.displayTitle,
+                                                   modalStyle: .overFullScreen)
+        }
+
         let contextMenu = PhotonActionSheet(viewModel: viewModel)
         contextMenu.modalTransitionStyle = .crossDissolve
 
