1f732ca5005c30bc36144bf38d3ed4d3cd19d286
Winnie Teichmann <4530+thatswinnie@users.noreply.github.com>
2022-06-15 15:39:55 -0400

    Update InactiveTabHeader

--- Client/Application/AccessibilityIdentifiers.swift
+++ Client/Application/AccessibilityIdentifiers.swift
@@ -75,6 +75,7 @@ public struct AccessibilityIdentifiers {
         static let deleteCloseAllButton = "TabTrayController.deleteButton.closeAll"
         static let deleteCancelButton = "TabTrayController.deleteButton.cancel"
         static let syncedTabs = "Synced Tabs"
+        static let inactiveTabHeader = "InactiveTabs.header"
     }
 
     struct LibraryPanels {
