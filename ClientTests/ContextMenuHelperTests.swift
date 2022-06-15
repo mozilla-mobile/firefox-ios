78888ab6814759b3db2faf4254c8f6a7dda33ef3
roux g. buciu <11182210+electricRGB@users.noreply.github.com>
2022-06-15 10:46:34 -0400

    Add FXIOS-3147 [v102] Context menu for history highlights (#11003)

---
+++ ClientTests/ContextMenuHelperTests.swift
@@ -0,0 +1,35 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import Glean
+import XCTest
+
+@testable import Client
+
+class ContextMenuHelperTests: XCTestCase {
+
+    var profile: Profile!
+
+    override func setUp() {
+        super.setUp()
+        profile = MockProfile()
+
+        Glean.shared.resetGlean(clearStores: true)
+    }
+
+    override func tearDown() {
+        super.tearDown()
+        profile = nil
+    }
+
+    func testHistoryHighlightsTelemetry() {
+        let viewModel = FirefoxHomeViewModel(profile: profile,
+                                             isPrivate: false)
+        let helper = FirefoxHomeContextMenuHelper(viewModel: viewModel)
+
+        helper.sendHistoryHighlightContextualTelemetry(type: .remove)
+
+        testEventMetricRecordingSuccess(metric: GleanMetrics.FirefoxHomePage.historyHighlightsContext)
+    }
+}
