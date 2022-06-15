d04866c9e3ac84a202de67b6476f77c873d11243
yoanarios <yriosdiaz@mozilla.com>
2022-06-15 16:37:24 -0400

    Bitrise bump for v102 (#11030)
    * bitrise bump for v102
    * Add credentialProvider and WidgetKit info plist path to script
    * Update version to 102 on info.plist

--- update_version.sh
+++ update_version.sh
@@ -7,5 +7,5 @@ fi
 
 # If needed, modify the starting version number locally
 # This script is mainly a reference as to what needs to be updated
-ls Client/Info.plist Extensions/*/*Info.plist | xargs perl -pi -e "s/0\.0\.1/$2/g"
+ls Client/Info.plist Extensions/*/*Info.plist CredentialProvider/Info.plist WidgetKit/Info.plist | xargs perl -pi -e "s/0\.0\.1/$2/g"
 perl -pi -e "s/$1/$2/g" bitrise.yml
