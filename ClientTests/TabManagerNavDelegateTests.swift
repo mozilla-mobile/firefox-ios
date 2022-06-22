9d7a194dcb5934eca1aa7fc0ef2793485411cb9f
mauriciomaniglia <manigliamauricio@gmail.com>
2022-06-21 23:41:19 -0300

    Test `webViewDidCommit` sends correct message.

---
+++ ClientTests/TabManagerNavDelegateTests.swift
@@ -0,0 +1,89 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import XCTest
+import WebKit
+
+@testable import Client
+
+class TabManagerNavDelegateTests: XCTestCase {
+    let navigation = WKNavigation()
+
+    func test_webViewDidCommit_sendsCorrectMessage() {
+        let sut = TabManagerNavDelegate()
+        let delegate1 = WKNavigationDelegateSpy()
+        let delegate2 = WKNavigationDelegateSpy()
+
+        sut.insert(delegate1)
+        sut.insert(delegate2)
+        sut.webView(anyWebView(), didCommit: navigation)
+
+        XCTAssertEqual(delegate1.receivedMessages, [.webViewDidCommit])
+        XCTAssertEqual(delegate2.receivedMessages, [.webViewDidCommit])
+    }
+}
+
+// MARK: - Helpers
+
+private func anyWebView() -> WKWebView {
+    return WKWebView(frame: CGRect(width: 100, height: 100))
+}
+
+private class WKNavigationDelegateSpy: WKNavigationDelegate {
+    enum Message {
+        case webViewDidCommit
+    }
+
+    var receivedMessages = [Message]()
+
+    func isEqual(_ object: Any?) -> Bool {
+        return true
+    }
+
+    var hash: Int = 1
+
+    var superclass: AnyClass?
+
+    func `self`() -> Self {
+        return self
+    }
+
+    func perform(_ aSelector: Selector!) -> Unmanaged<AnyObject>! {
+        return .none
+    }
+
+    func perform(_ aSelector: Selector!, with object: Any!) -> Unmanaged<AnyObject>! {
+        return .none
+    }
+
+    func perform(_ aSelector: Selector!, with object1: Any!, with object2: Any!) -> Unmanaged<AnyObject>! {
+        return .none
+    }
+
+    func isProxy() -> Bool {
+        return true
+    }
+
+    func isKind(of aClass: AnyClass) -> Bool {
+        return true
+    }
+
+    func isMember(of aClass: AnyClass) -> Bool {
+        return true
+    }
+
+    func conforms(to aProtocol: Protocol) -> Bool {
+        return true
+    }
+
+    func responds(to aSelector: Selector!) -> Bool {
+        return true
+    }
+
+    var description: String = ""
+
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        receivedMessages.append(.webViewDidCommit)
+    }
+}
