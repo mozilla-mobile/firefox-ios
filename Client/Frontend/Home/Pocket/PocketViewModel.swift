7fe232651d2e19346d3f6a3b424d620593cba58e
Razvan Litianu <litianu.razvan@gmail.com>
2022-07-12 11:09:01 +0300

    Move StoryProvider to separate file

--- Client/Frontend/Home/Pocket/PocketViewModel.swift
+++ Client/Frontend/Home/Pocket/PocketViewModel.swift
@@ -6,62 +6,6 @@ import Foundation
 import Storage
 import Shared
 
-class StoryProvider {
-    private let numberOfPocketStories: Int
-    private let sponsoredIndices: [Int]
-
-    init(
-        pocketAPI: PocketStoriesProviding,
-        pocketSponsoredAPI: PocketSponsoredStoriesProviding,
-        numberOfPocketStories: Int = 11,
-        sponsoredIndices: [Int] = [1, 9],
-        showSponsoredStories: @escaping () -> Bool
-    ) {
-        self.pocketAPI = pocketAPI
-        self.pocketSponsoredAPI = pocketSponsoredAPI
-        self.numberOfPocketStories = numberOfPocketStories
-        self.sponsoredIndices = sponsoredIndices
-        self.showSponsoredStories = showSponsoredStories
-    }
-
-    private let pocketAPI: PocketStoriesProviding
-    private let pocketSponsoredAPI: PocketSponsoredStoriesProviding
-    private var showSponsoredStories: () -> Bool
-
-    private func insert(sponsoredStories: [PocketStory], into globalFeed: [PocketStory], indices: [Int]) -> [PocketStory] {
-        var global = globalFeed
-        var sponsored = sponsoredStories
-        for index in indices {
-            // Making sure we insert a sponsored story at a valid index
-            let normalisedIndex = min(index, global.endIndex)
-            if let first = sponsored.first {
-                global.insert(first, at: normalisedIndex)
-                sponsored.removeAll(where: { $0 == first })
-            }
-        }
-        return global
-    }
-
-    func fetchPocketStories() async -> [PocketStory] {
-        let global = (try? await pocketAPI.fetchStories(items: numberOfPocketStories)) ?? []
-        // Convert global feed to PocketStory
-        var globalTemp = global.map(PocketStory.init)
-
-        if showSponsoredStories(), let sponsored = try? await pocketSponsoredAPI.fetchSponsoredStories() {
-            // Convert sponsored feed to PocketStory, take the desired number of sponsored stories
-            let sponsoredTemp = Array(sponsored.map(PocketStory.init).prefix(sponsoredIndices.count))
-            globalTemp = Array(globalTemp.prefix(numberOfPocketStories - sponsoredIndices.count))
-            globalTemp = insert(
-                sponsoredStories: sponsoredTemp,
-                into: globalTemp,
-                indices: sponsoredIndices
-            )
-        }
-
-        return globalTemp
-    }
-}
-
 class PocketViewModel {
 
     struct UX {
