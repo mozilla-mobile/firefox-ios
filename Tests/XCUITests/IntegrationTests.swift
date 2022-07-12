eaad90778745764928c3958c091497543e77380f
isabelrios <isabelrios@gmail.com>
2022-07-12 11:49:36 +0200

    Refactor #11240 [v104] XCUITests sync int update desktop tabs test (#11241)

--- Tests/XCUITests/IntegrationTests.swift
+++ Tests/XCUITests/IntegrationTests.swift
@@ -218,7 +218,8 @@ class IntegrationTests: BaseTestCase {
         waitForInitialSyncComplete()
 
         // Check synced Tabs
-        navigator.goto(NewTabScreen)
+        app.buttons["Done"].tap()
+        navigator.nowAt(HomePanelsScreen)
         navigator.goto(TabTray)
         navigator.performAction(Action.ToggleSyncMode)
 
