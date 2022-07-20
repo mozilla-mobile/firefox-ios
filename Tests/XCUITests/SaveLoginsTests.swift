112f59a6bc1e379e9dcdceb2fe943fb0e8145f5e
isabelrios <isabelrios@gmail.com>
2022-07-20 13:27:35 +0200

    Refactor #11299 [v104] XCUITests fix iPad save logins test (#11321)

--- Tests/XCUITests/SaveLoginsTests.swift
+++ Tests/XCUITests/SaveLoginsTests.swift
@@ -97,8 +97,12 @@ class SaveLoginTest: BaseTestCase {
         waitForExistence(app.tables["Login List"])
         XCTAssertTrue(app.staticTexts[domain].exists)
         // XCTAssertTrue(app.staticTexts[domainSecondLogin].exists)
-        // Chage to 3 entries since the hostname is not saved and only one entry appears
-        XCTAssertEqual(app.tables["Login List"].cells.count, defaultNumRowsLoginsList + 1)
+        // Chage to 3 entries for iPhone since the hostname is not saved and only one entry appears
+        if iPad() {
+            XCTAssertEqual(app.tables["Login List"].cells.count, defaultNumRowsLoginsList + 2)
+        } else {
+            XCTAssertEqual(app.tables["Login List"].cells.count, defaultNumRowsLoginsList + 1)
+        }
     }
 
     func testDoNotSaveLogin() {
