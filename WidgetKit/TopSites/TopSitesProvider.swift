ebde475ba36d22af7880ac5cb3d064c91fda1507
roux g. buciu <11182210+electricRGB@users.noreply.github.com>
2022-08-04 16:24:31 -0400

    Add FXIOS-4706 [v105] Swiftlint: empty collection literal and large tuples (#11520)

--- WidgetKit/TopSites/TopSitesProvider.swift
+++ WidgetKit/TopSites/TopSitesProvider.swift
@@ -70,14 +70,3 @@ fileprivate extension UIImage {
     return UIGraphicsGetImageFromCurrentImageContext() ?? self
   }
 }
-
-fileprivate extension UIColor {
-    var components: (red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) {
-        var r: CGFloat = 0
-        var g: CGFloat = 0
-        var b: CGFloat = 0
-        var a: CGFloat = 0
-        getRed(&r, green: &g, blue: &b, alpha: &a)
-        return (r, g, b, a)
-    }
-}
