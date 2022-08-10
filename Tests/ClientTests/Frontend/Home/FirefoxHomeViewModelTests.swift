afe6cee6e21bb123fbc3f930bcdc267589863da8
Laurie Marceau <lmarceau@mozilla.com>
2022-08-10 15:04:13 -0400

    Fix test mistake

--- Tests/ClientTests/Frontend/Home/FirefoxHomeViewModelTests.swift
+++ Tests/ClientTests/Frontend/Home/FirefoxHomeViewModelTests.swift
@@ -35,9 +35,10 @@ class FirefoxHomeViewModelTests: XCTestCase {
                                           isPrivate: false,
                                           tabManager: MockTabManager(),
                                           urlBar: URLBarView(profile: profile))
-        XCTAssertEqual(viewModel.shownSections.count, 2)
+        XCTAssertEqual(viewModel.shownSections.count, 3)
         XCTAssertEqual(viewModel.shownSections[0], HomepageSectionType.logoHeader)
-        XCTAssertEqual(viewModel.shownSections[1], HomepageSectionType.jumpBackIn)
+        XCTAssertEqual(viewModel.shownSections[1], HomepageSectionType.messageCard)
+        XCTAssertEqual(viewModel.shownSections[2], HomepageSectionType.customizeHome)
     }
 
     func testNumberOfSection_updatingData_adds2Sections() throws {
