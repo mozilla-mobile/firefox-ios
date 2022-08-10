71dd2238f9fe98bc89a7d0cac925dd87305653c5
Nishant Bhasin <nikieme3@gmail.com>
2022-08-10 16:08:02 -0400

    Update Client/Frontend/Browser/TopTabsViews.swift
    Co-authored-by: lmarceau <lmarceau@mozilla.com>

--- Client/Frontend/Browser/TopTabsViews.swift
+++ Client/Frontend/Browser/TopTabsViews.swift
@@ -124,9 +124,7 @@ class TopTabCell: UICollectionViewCell, NotificationThemeable, TabTrayCell, Reus
         if let favIcon = tab.displayFavicon, let url = URL(string: favIcon.url) {
             ImageLoadingHandler.shared.getImageFromCacheOrDownload(with: url,
                                                                    limit: ImageLoadingConstants.NoLimitImageSize) { image, error in
-                guard error == nil, let image = image else {
-                    return
-                }
+                guard error == nil, let image = image else { return }
                 self.favicon.image = image
             }
         }
