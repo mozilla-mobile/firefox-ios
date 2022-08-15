dcae330f288742d52a2609a4aae8708c0f1cfbf3
yoanarios <yriosdiaz@mozilla.com>
2022-08-15 09:44:45 -0400

    Fix unit test for IntroViewModel

--- Tests/ClientTests/IntroViewModelTests.swift
+++ Tests/ClientTests/IntroViewModelTests.swift
@@ -20,18 +20,18 @@ class IntroViewModelTests: XCTestCase {
     }
 
     func testGetWelcomeViewModel() {
-        let cardType = viewModel.getCardViewModel(index: 0)?.cardType
-        XCTAssertTrue(cardType == IntroViewModel.InformationCards.welcome)
+        let cardViewModel = viewModel.getCardViewModel(cardType: .welcome)
+        XCTAssertEqual(cardViewModel?.cardType, IntroViewModel.InformationCards.welcome)
     }
 
     func testGetWallpaperViewModel() {
-        let cardType = viewModel.getCardViewModel(index: 1)?.cardType
-        XCTAssertTrue(cardType == IntroViewModel.InformationCards.wallpapers)
+        let cardViewModel = viewModel.getCardViewModel(cardType: .wallpapers)
+        XCTAssertEqual(cardViewModel?.cardType, IntroViewModel.InformationCards.wallpapers)
     }
 
     func testGetSyncViewModel() {
-        let cardType = viewModel.getCardViewModel(index: 2)?.cardType
-        XCTAssertTrue(cardType == IntroViewModel.InformationCards.signSync)
+        let cardViewModel = viewModel.getCardViewModel(cardType: .signSync)
+        XCTAssertEqual(cardViewModel?.cardType, IntroViewModel.InformationCards.signSync)
     }
 
     func testNextIndexAfterLastCard() {
