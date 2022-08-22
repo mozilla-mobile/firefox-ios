d1ff72fe99832fefaf7c092a9f78b9569249e72b
lmarceau <lmarceau@mozilla.com>
2022-08-22 15:50:53 -0400

    Bugfix FXIOS-4761 [v106] Remove QuickLinkSelectionIntent (#11670)
    * Rename WidgetIntents to QuickActionIntent and remove QuickLinkSelectionIntent
    
    * Fix bug where top sites widget wasnt setup due to object being released

--- WidgetKit/QuickLink.swift
+++ WidgetKit/QuickLink.swift
@@ -3,11 +3,9 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0
 
 import SwiftUI
-// Enum file that holds the different cases for the Quick Actions small widget with
-// their configurations (string, backgrounds, images) as selected by the user in edit
-// mode. It maps the values of IntentQuickLink enum in the QuickLinkSelectionIntent to
-// the designated values of each case.
 
+/// Enum file that holds the different cases for the Quick Actions small widget with their
+/// configurations (string, backgrounds, images) as selected by the user in edit mode.
 enum QuickLink: Int {
     case search = 1
     case copiedLink
@@ -76,21 +74,6 @@ enum QuickLink: Int {
             return [Color("goToCopiedLinkSolid")]
         case .closePrivateTabs:
             return [Color("privateGradientThree"), Color("privateGradientTwo"), Color("privateGradientOne")]
-        }
-    }
-
-    static func from(_ configuration: QuickLinkSelectionIntent) -> Self {
-        switch configuration.quickLink {
-        case .search:
-            return .search
-        case .privateSearch:
-            return .privateSearch
-        case .closePrivateTabs:
-            return .closePrivateTabs
-        case .copiedLink:
-            return .copiedLink
-        default:
-            return .search
         }
     }
 }
