aea392070b61194a0de80ea8c9a94bff6e28df2f
OrlaM <omitchell@mozilla.com>
2022-09-01 12:47:38 -0400

    Fix weird time issue with sync tabs

--- Client/Frontend/Browser/Tab.swift
+++ Client/Frontend/Browser/Tab.swift
@@ -385,7 +385,7 @@ class Tab: NSObject {
                 URL: displayURL,
                 title: tab.displayTitle,
                 history: history,
-                lastUsed: Date.now(),
+                lastUsed: sessionData.lastUsedTime,
                 icon: nil)
         } else if let sessionData = tab.sessionData, !sessionData.urls.isEmpty {
             let history = Array(sessionData.urls.filter(RemoteTab.shouldIncludeURL).reversed())
