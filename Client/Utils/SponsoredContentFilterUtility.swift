8d0130220846c0b72aada22c44ba9906e02bd6be
lmarceau <lmarceau@mozilla.com>
2022-09-09 16:53:14 -0400

    Bugfix FXIOS-4821 [v106] Filter sponsored content in necessary places + move test folder under utils folder when needed (#11871)
    * Make sure we filter sponsored content in necessary places (top sites, jump back in and history highlights). This means any sponsored click won't appear in those particular sections. The url parameter is used to filter. A utility was create to provide a central place to gather this information. Tests were also added for it.
    * Move test folder under utils folder when needed

---
+++ Client/Utils/SponsoredContentFilterUtility.swift
@@ -0,0 +1,28 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import Foundation
+import Storage
+
+// Utility to filter sponsored content out of certain data type
+struct SponsoredContentFilterUtility {
+
+    /// Hide with search param is defined by adMarketplace, indicates this URL was registered through sponsored clicks
+    /// and should not show in top sites, jump back in or recently visited sections on the homepage
+    private var hideWithSearchParam: String {
+        return "mfadid=adm"
+    }
+
+    func filterSponsoredSites(from sites: [Site]) -> [Site] {
+        return sites.filter { !$0.url.contains(hideWithSearchParam) }
+    }
+
+    func filterSponsoredTabs(from tabs: [Tab]) -> [Tab] {
+        return tabs.filter { !($0.lastKnownUrl?.absoluteString.contains(hideWithSearchParam) ?? false) }
+    }
+
+    func filterSponsoredHighlights(from items: [HistoryHighlight]) -> [HistoryHighlight] {
+        return items.filter { !$0.url.contains(hideWithSearchParam) }
+    }
+}
