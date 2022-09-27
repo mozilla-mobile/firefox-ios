ca156b8473671908610fd813a75611ed5a12f6a9
Winnie Teichmann <4530+thatswinnie@users.noreply.github.com>
2022-09-27 13:07:51 -0400

    Add InstructionsView (SwiftUI)

---
+++ Client/Frontend/InstructionsView.swift
@@ -0,0 +1,59 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import SwiftUI
+import UIKit
+import Shared
+
+struct InstructionsView: View {
+
+    private struct UX {
+        static let padding: CGFloat = 20
+        static let textFont = Font.body
+    }
+
+    var backgroundColor: UIColor
+    var textColor: UIColor
+    var imageColor: UIColor
+    var dismissAction: (() -> Void)?
+
+    var body: some View {
+        ZStack {
+            Color(backgroundColor).edgesIgnoringSafeArea(.all)
+            ScrollView {
+                VStack(alignment: .center, spacing: UX.padding) {
+                    Image(ImageIdentifiers.emptySyncImageName)
+                        .renderingMode(.template)
+                        .foregroundColor(Color(imageColor))
+                        .padding(.top, UX.padding)
+                    Text(String.SendToNotSignedInText).font(UX.textFont).multilineTextAlignment(.center)
+                    Text(String.SendToNotSignedInMessage).font(UX.textFont).multilineTextAlignment(.center)
+
+                    Spacer()
+                }
+                .padding(UX.padding)
+            }
+        }
+        .foregroundColor(Color(textColor))
+        .navigationBarItems(leading:
+            Button(action: {
+                dismissAction?()
+            }) {
+                Text(String.CloseButtonTitle)
+            }
+            .accessibility(identifier: "InstructionsViewController.navigationItem.leftBarButtonItem")
+        )
+        .navigationBarBackButtonHidden(true)
+    }
+}
+
+struct InstructionsView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            InstructionsView(backgroundColor: .white,
+                             textColor: .darkGray,
+                             imageColor: .darkGray)
+        }
+    }
+}
