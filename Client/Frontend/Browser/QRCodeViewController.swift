b87eb1fb0a9defa54c9062e4daa48b7fd0216e03
Winnie Teichmann <4530+thatswinnie@users.noreply.github.com>
2022-09-28 10:34:30 -0400

    Remove SnapKit

--- Client/Frontend/Browser/QRCodeViewController.swift
+++ Client/Frontend/Browser/QRCodeViewController.swift
@@ -4,16 +4,7 @@
 
 import Foundation
 import AVFoundation
-import SnapKit
 import Shared
-
-private struct QRCodeViewControllerUX {
-    static let navigationBarBackgroundColor = UIColor.black
-    static let navigationBarTitleColor = UIColor.Photon.White100
-    static let maskViewBackgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.5)
-    static let isLightingNavigationItemColor = UIColor(red: 0.45, green: 0.67, blue: 0.84, alpha: 1)
-    static let viewBackgroundDeniedColor = UIColor.black
-}
 
 protocol QRCodeViewControllerDelegate: AnyObject {
     func didScanQRCodeWithURL(_ url: URL)
@@ -21,6 +12,15 @@ protocol QRCodeViewControllerDelegate: AnyObject {
 }
 
 class QRCodeViewController: UIViewController {
+
+    private struct UX {
+        static let navigationBarBackgroundColor = UIColor.black
+        static let navigationBarTitleColor = UIColor.Photon.White100
+        static let maskViewBackgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.5)
+        static let isLightingNavigationItemColor = UIColor(red: 0.45, green: 0.67, blue: 0.84, alpha: 1)
+        static let viewBackgroundDeniedColor = UIColor.black
+    }
+
     var qrCodeDelegate: QRCodeViewControllerDelegate?
 
     fileprivate lazy var captureSession: AVCaptureSession = {
@@ -34,20 +34,31 @@ class QRCodeViewController: UIViewController {
     }()
 
     private var videoPreviewLayer: AVCaptureVideoPreviewLayer?
-    private let scanLine = UIImageView(image: UIImage(named: "qrcode-scanLine"))
-    private let scanBorder = UIImageView(image: UIImage(named: "qrcode-scanBorder"))
-    private lazy var instructionsLabel: UILabel = {
-        let label = UILabel()
+
+    private let scanLine: UIImageView = .build { imageView in
+        imageView.image = UIImage(named: "qrcode-scanLine") // use image identifiers
+    }
+
+    private let scanBorder: UIImageView = .build { imageView in
+        imageView.image = UIImage(named: "qrcode-scanBorder") // use image identifiers
+    }
+
+    private lazy var instructionsLabel: UILabel = .build { label in
         label.text = .ScanQRCodeInstructionsLabel
         label.textColor = UIColor.Photon.White100
         label.textAlignment = .center
         label.numberOfLines = 0
-        return label
-    }()
-    private var maskView = UIView()
+    }
+
+    private var maskView: UIView = .build { view in
+        view.backgroundColor = UX.maskViewBackgroundColor
+    }
     private var isAnimationing: Bool = false
     private var isLightOn: Bool = false
     private var shapeLayer = CAShapeLayer()
+
+    private var scanLineTopConstraint: NSLayoutConstraint!
+    private var scanBorderWidthConstraint: NSLayoutConstraint!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -60,8 +71,8 @@ class QRCodeViewController: UIViewController {
         self.navigationItem.title = .ScanQRCodeViewTitle
 
         // Setup the NavigationBar
-        self.navigationController?.navigationBar.barTintColor = QRCodeViewControllerUX.navigationBarBackgroundColor
-        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: QRCodeViewControllerUX.navigationBarTitleColor]
+        self.navigationController?.navigationBar.barTintColor = UX.navigationBarBackgroundColor
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UX.navigationBarTitleColor]
 
         // Setup the NavigationItem
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(
@@ -87,7 +98,7 @@ class QRCodeViewController: UIViewController {
         if getAuthorizationStatus != .denied {
             setupCamera()
         } else {
-            view.backgroundColor = QRCodeViewControllerUX.viewBackgroundDeniedColor
+            view.backgroundColor = UX.viewBackgroundDeniedColor
             self.navigationItem.rightBarButtonItem?.isEnabled = false
 
             let alert = UIAlertController(title: "", message: .ScanQRCodePermissionErrorMessage, preferredStyle: .alert)
@@ -97,20 +108,16 @@ class QRCodeViewController: UIViewController {
             self.present(alert, animated: true, completion: nil)
         }
 
-        maskView.backgroundColor = QRCodeViewControllerUX.maskViewBackgroundColor
-        self.view.addSubview(maskView)
-        self.view.addSubview(scanBorder)
-        self.view.addSubview(scanLine)
-        self.view.addSubview(instructionsLabel)
-
-        setupConstraints()
         setupVideoPreviewLayer()
-        isAnimationing = true
-        startScanLineAnimation()
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+
+        setupConstraints()
+        isAnimationing = true
+        startScanLineAnimation()
+
         applyShapeLayer()
     }
 
@@ -129,39 +136,58 @@ class QRCodeViewController: UIViewController {
         view.layoutIfNeeded()
         shapeLayer.removeFromSuperlayer()
         let rectPath = UIBezierPath(rect: view.bounds)
+        print("WT: \(scanBorder.frame)")
         rectPath.append(UIBezierPath(rect: scanBorder.frame).reversing())
         shapeLayer.path = rectPath.cgPath
         maskView.layer.mask = shapeLayer
     }
 
     private func setupConstraints() {
-        maskView.snp.remakeConstraints { (make) in
-            make.edges.equalTo(view)
-        }
+        view.addSubview(maskView)
+        view.addSubview(scanBorder)
+        view.addSubview(scanLine)
+        view.addSubview(instructionsLabel)
 
         let minSize = min(view.frame.width, view.frame.height)
+        var scanBorderSize = minSize / 3 * 2
+
         if UIDevice.current.userInterfaceIdiom == .pad || UIDevice.current.orientation.isLandscape {
-            scanBorder.snp.remakeConstraints { make in
-                make.center.equalTo(self.view)
-                make.width.height.equalTo(minSize / 2)
-            }
-        } else {
-            scanBorder.snp.remakeConstraints { make in
-                make.center.equalTo(self.view)
-                make.width.height.equalTo(minSize / 3 * 2)
-            }
-        }
-        scanLine.snp.remakeConstraints { make in
-            make.left.equalTo(scanBorder.snp.left)
-            make.top.equalTo(scanBorder.snp.top).offset(6)
-            make.width.equalTo(scanBorder.snp.width)
-            make.height.equalTo(6)
+            scanBorderSize = minSize / 2
         }
 
-        instructionsLabel.snp.remakeConstraints { make in
-            make.left.right.equalTo(self.view.layoutMarginsGuide)
-            make.top.equalTo(scanBorder.snp.bottom).offset(30)
+        scanLineTopConstraint = scanLine.topAnchor.constraint(equalTo: scanBorder.topAnchor, constant: 6)
+        scanBorderWidthConstraint = scanBorder.widthAnchor.constraint(equalToConstant: scanBorderSize)
+
+        NSLayoutConstraint.activate([
+            maskView.topAnchor.constraint(equalTo: view.topAnchor),
+            maskView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            maskView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            maskView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+
+            scanBorder.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            scanBorder.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            scanBorderWidthConstraint,
+            scanBorder.heightAnchor.constraint(equalTo: scanBorder.widthAnchor),
+
+            scanLineTopConstraint,
+            scanLine.leadingAnchor.constraint(equalTo: scanBorder.leadingAnchor),
+            scanLine.widthAnchor.constraint(equalTo: scanBorder.widthAnchor),
+            scanLine.heightAnchor.constraint(equalToConstant: 6),
+
+            instructionsLabel.topAnchor.constraint(equalTo: scanBorder.bottomAnchor, constant: 30),
+            instructionsLabel.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+            instructionsLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+        ])
+    }
+
+    private func updateContraintsAfterTransition() {
+        let minSize = min(view.frame.width, view.frame.height)
+        var scanBorderSize = minSize / 3 * 2
+
+        if UIDevice.current.userInterfaceIdiom == .pad || UIDevice.current.orientation.isLandscape {
+            scanBorderSize = minSize / 2
         }
+        scanBorderWidthConstraint.constant = scanBorderSize
     }
 
     private func setupVideoPreviewLayer() {
@@ -191,14 +217,10 @@ class QRCodeViewController: UIViewController {
                        delay: 0,
                        options: [.repeat],
                        animations: {
-            self.scanLine.snp.updateConstraints({ (make) in
-                make.top.equalTo(self.scanBorder.snp.top).offset(self.scanBorder.frame.size.height - 6)
-            })
+            self.scanLineTopConstraint.constant = self.scanBorder.frame.size.height - 6
             self.view.layoutIfNeeded()
         }) { (value: Bool) in
-            self.scanLine.snp.updateConstraints({ (make) in
-                make.top.equalTo(self.scanBorder.snp.top).offset(6)
-            })
+            self.scanLineTopConstraint.constant = 6
             self.perform(#selector(self.startScanLineAnimation), with: nil, afterDelay: 0)
         }
     }
@@ -219,7 +241,7 @@ class QRCodeViewController: UIViewController {
                 try captureDevice.lockForConfiguration()
                 captureDevice.torchMode = AVCaptureDevice.TorchMode.off
                 captureDevice.unlockForConfiguration()
-                navigationItem.rightBarButtonItem?.image = UIImage(named: "qrcode-light")
+                navigationItem.rightBarButtonItem?.image = UIImage(named: "qrcode-light") // use image identifier
                 navigationItem.rightBarButtonItem?.tintColor = UIColor.Photon.White100
             } catch {
                 print(error)
@@ -230,7 +252,7 @@ class QRCodeViewController: UIViewController {
                 captureDevice.torchMode = AVCaptureDevice.TorchMode.on
                 captureDevice.unlockForConfiguration()
                 navigationItem.rightBarButtonItem?.image = UIImage(named: "qrcode-isLighting")
-                navigationItem.rightBarButtonItem?.tintColor = QRCodeViewControllerUX.isLightingNavigationItemColor
+                navigationItem.rightBarButtonItem?.tintColor = UX.isLightingNavigationItemColor
             } catch {
                 print(error)
             }
@@ -268,7 +290,7 @@ class QRCodeViewController: UIViewController {
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
         super.viewWillTransition(to: size, with: coordinator)
         coordinator.animate(alongsideTransition: { _ in
-            self.setupConstraints()
+            self.updateContraintsAfterTransition()
             self.setupVideoPreviewLayer()
         }, completion: nil)
     }
