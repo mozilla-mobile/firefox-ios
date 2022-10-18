ca75ac7d5013cde9b845c0aecf84d3f4fc5ab162
Litianu Razvan <litianu.razvan@gmail.com>
2022-10-18 17:18:10 +0300

    [focus-ios] Enable old onboarding for beta testing (https://github.com/mozilla-mobile/focus-ios/issues/3515)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- focus-ios/Blockzilla/AppDelegate.swift
+++ focus-ios/Blockzilla/AppDelegate.swift
@@ -60,16 +60,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     private lazy var onboardingEventsHandler: OnboardingEventsHandling = {
         var shouldShowNewOnboarding: () -> Bool = { [unowned self] in
-            #if DEBUG
             if UserDefaults.standard.bool(forKey: OnboardingConstants.ignoreOnboardingExperiment) {
                 return !UserDefaults.standard.bool(forKey: OnboardingConstants.showOldOnboarding)
             } else {
                 return nimbus.shouldShowNewOnboarding
             }
-            #else
-            return nimbus.shouldShowNewOnboarding
-            #endif
-    }
+        }
         guard !AppInfo.isTesting() else { return TestOnboarding() }
         return OnboardingFactory.makeOnboardingEventsHandler(shouldShowNewOnboarding)
     }()
