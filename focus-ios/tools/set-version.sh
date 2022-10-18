d698a3ff67c5e8f3cb5aed02c5f3c2644736d657
Litianu Razvan <litianu.razvan@gmail.com>
2022-10-18 12:22:06 +0300

    [focus-ios] Update script to bump version number in Widgets (https://github.com/mozilla-mobile/focus-ios/issues/3519)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- focus-ios/tools/set-version.sh
+++ focus-ios/tools/set-version.sh
@@ -6,7 +6,7 @@ if [ $# -eq 0 ]; then
 fi
 
 
-for plist in Blockzilla/Info.plist ContentBlocker/Info.plist FocusIntentExtension/Info.plist OpenInFocus/Info.plist; do
+for plist in Blockzilla/Info.plist ContentBlocker/Info.plist FocusIntentExtension/Info.plist OpenInFocus/Info.plist Widgets/Info.plist; do
   current=`/usr/libexec/PlistBuddy -c "Print :CFBundleShortVersionString" "$plist"`
   echo "Changing CFBundleShortVersionString in $plist from $current to $1"
   /usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $1" "$plist"
