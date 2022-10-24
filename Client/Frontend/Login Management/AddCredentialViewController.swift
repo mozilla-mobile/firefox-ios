315f56c6d450229c1a71a7dfffe25926bc46f1d7
Laurie Marceau <lmarceau@mozilla.com>
2022-10-24 17:42:20 -0400

    Fix button colors applied in two places

--- Client/Frontend/Login Management/AddCredentialViewController.swift
+++ Client/Frontend/Login Management/AddCredentialViewController.swift
@@ -40,14 +40,12 @@ class AddCredentialViewController: UIViewController, Themeable {
 
     fileprivate lazy var cancelButton: UIBarButtonItem = {
         let button = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancel))
-        button.tintColor = themeManager.currentTheme.colors.actionPrimary
         return button
     }()
 
     fileprivate lazy var saveButton: UIBarButtonItem = {
         let button = UIBarButtonItem(title: .SettingsAddCustomEngineSaveButtonText, style: .done, target: self, action: #selector(addCredential))
         button.isEnabled = false
-        button.tintColor = themeManager.currentTheme.colors.actionPrimary
         return button
     }()
 
