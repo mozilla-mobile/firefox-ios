6f9aa8889f12fbe1f77323a6a2df6ac50684238c
Litianu Razvan <litianu.razvan@gmail.com>
2022-10-28 15:57:54 +0300

    [focus-ios]  Fixes https://github.com/mozilla-mobile/focus-ios/pull/3531 Widget selection copy incorrect (https://github.com/mozilla-mobile/focus-ios/issues/3540)

--- focus-ios/Widgets/Widgets.swift
+++ focus-ios/Widgets/Widgets.swift
@@ -44,7 +44,7 @@ struct Widgets: Widget {
             FocusWidgetsEntryView(entry: entry)
         }
         .configurationDisplayName(String.quickActionsGalleryTitle)
-        .description(String(format: .quickActionGalleryDescription, String.appNameForBundle))
+        .description(String(format: .quickActionGalleryDescriptionV2, String.appNameForBundle))
         .supportedFamilies([.systemSmall])
     }
 }
