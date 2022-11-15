1a9c1cae8473deeb44b17b82c506f20a62264174
Winnie Teichmann <4530+thatswinnie@users.noreply.github.com>
2022-11-15 13:36:56 -0500

    Refactor FXIOS-5248 [v109] Remove SnapKit from BadgeWithBackdrop (#12382)

--- Client/Frontend/Widgets/ToolbarBadge.swift
+++ Client/Frontend/Widgets/ToolbarBadge.swift
@@ -9,38 +9,46 @@ class ToolbarBadge: UIView {
     // MARK: - Variables
     private let badgeSize: CGFloat
     private let badgeOffset = CGFloat(10)
-    private let background: UIImageView
-    private let badge: UIImageView
+    private lazy var background: UIImageView = .build { _ in }
+    private lazy var badge: UIImageView = .build { _ in }
 
     // MARK: - Initializers
     init(imageName: String, imageMask: String, size: CGFloat) {
         badgeSize = size
-        background = UIImageView(image: UIImage(imageLiteralResourceName: imageMask))
-        badge = UIImageView(image: UIImage(imageLiteralResourceName: imageName))
         super.init(frame: CGRect(width: badgeSize, height: badgeSize))
-        addSubview(background)
-        addSubview(badge)
-        isUserInteractionEnabled = false
 
-        [background, badge].forEach {
-            $0.snp.makeConstraints { make in
-                make.edges.equalToSuperview()
-            }
-        }
+        background.image = UIImage(imageLiteralResourceName: imageMask)
+        badge.image = UIImage(imageLiteralResourceName: imageName)
+
+        setupLayout()
     }
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
-    // MARK: - View setup
-    func layout(onButton button: UIView) {
-        snp.remakeConstraints { make in
-            make.size.equalTo(badgeSize)
-            make.centerX.equalTo(button).offset(badgeOffset)
-            make.centerY.equalTo(button).offset(-badgeOffset)
-        }
+    private func setupLayout() {
+        addSubview(background)
+        addSubview(badge)
+        isUserInteractionEnabled = false
+
+        NSLayoutConstraint.activate([
+            background.topAnchor.constraint(equalTo: topAnchor),
+            background.leadingAnchor.constraint(equalTo: leadingAnchor),
+            background.bottomAnchor.constraint(equalTo: bottomAnchor),
+            background.trailingAnchor.constraint(equalTo: trailingAnchor),
+
+            badge.topAnchor.constraint(equalTo: topAnchor),
+            badge.leadingAnchor.constraint(equalTo: leadingAnchor),
+            badge.bottomAnchor.constraint(equalTo: bottomAnchor),
+            badge.trailingAnchor.constraint(equalTo: trailingAnchor),
+
+            widthAnchor.constraint(equalToConstant: badgeSize),
+            heightAnchor.constraint(equalToConstant: badgeSize)
+        ])
     }
+
+    // MARK: - View setup
 
     func tintBackground(color: UIColor) {
         background.tintColor = color
