40ea1b5f54f209c216d8c16dbe5bce69dccca4bf
Clare So <cso@mozilla.com>
2022-11-17 00:44:29 -0500

    Pin the website to the top site using the context menu

--- Tests/XCUITests/ActivityStreamTest.swift
+++ Tests/XCUITests/ActivityStreamTest.swift
@@ -127,6 +127,11 @@ class ActivityStreamTest: BaseTestCase {
         waitForExistence(app.collectionViews.cells.staticTexts[newTopSite["topSiteLabel"]!], timeout: TIMEOUT)
         XCTAssertTrue(app.collectionViews.cells.staticTexts[newTopSite["topSiteLabel"]!].exists)
         checkNumberOfExpectedTopSites(numberOfExpectedTopSites: 6)
+        
+        app.collectionViews.cells.staticTexts[newTopSite["topSiteLabel"]!].press(forDuration: 1)
+        selectOptionFromContextMenu(option: "Pin")
+        waitForExistence(app.collectionViews.cells.staticTexts[newTopSite["bookmarkLabel"]!], timeout: TIMEOUT)
+        XCTAssertTrue(app.collectionViews.cells.staticTexts[newTopSite["bookmarkLabel"]!].exists)
 
         navigator.performAction(Action.CloseURLBarOpen)
         navigator.nowAt(NewTabScreen)
