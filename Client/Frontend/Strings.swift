12ae7d11979c4ef1d2fa160ce6bf39528bb97892
Daniela Arcese <dnarcese@gmail.com>
2022-11-21 16:57:57 -0500

    Localize FXIOS-5106 [v108]: Land share sheet strings

--- Client/Frontend/Strings.swift
+++ Client/Frontend/Strings.swift
@@ -1313,7 +1313,20 @@ extension String {
 // MARK: - Share Sheet
 extension String {
     public struct ShareSheet {
-
+        public static let CopyButtonTitle = MZLocalizedString(
+            "ShareSheet.Copy.Title",
+            tableName: nil,
+            value: "Copy",
+            comment: "Button in share sheet to copy the url of the current tab.",
+            lastUpdated: .v108)
+        }
+        public static let SendToDeviceButtonTitle = MZLocalizedString(
+            "ShareSheet.SendToDevice.Title",
+            tableName: nil,
+            value: "Send Link to Device",
+            comment: "Button in the share sheet to send the current link to another device.",
+            lastUpdated: .v108)
+        }
     }
 }
 
