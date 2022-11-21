6a573be54d45f9d6ad849c4c05933bc92cb10bea
Tarik Eshaq <teshaq@mozilla.com>
2022-11-21 12:27:27 -0500

    bring back adding sites to keep favicon and pinned site dependency

--- Storage/SQL/SQLiteHistory.swift
+++ Storage/SQL/SQLiteHistory.swift
@@ -7,7 +7,13 @@ import Shared
 import Glean
 
 private let log = Logger.syncLogger
-public let TopSiteCacheSize: Int32 = 16
+
+open class IgnoredSiteError: MaybeErrorType {
+    open var description: String {
+        return "Ignored site."
+    }
+}
+
 
 private var ignoredSchemes = ["about"]
 
@@ -49,9 +55,8 @@ open class SQLiteHistory {
     let database: BrowserDB
     let favicons: SQLiteFavicons
     let prefs: Prefs
-    let clearTopSitesQuery: (String, Args?) = ("DELETE FROM cached_top_sites", nil)
     let notificationCenter: NotificationCenter
-
+    
     required public init(database: BrowserDB,
                          prefs: Prefs,
                          notificationCenter: NotificationCenter = NotificationCenter.default) {
@@ -59,19 +64,8 @@ open class SQLiteHistory {
         self.favicons = SQLiteFavicons(db: self.database)
         self.prefs = prefs
         self.notificationCenter = notificationCenter
-
-        // We report the number of visits a user has
-        // this is helpful in determining what the size of users' history visits
-        // is like, to help guide testing the migration to the
-        // application-services implementation and testing the
-        // performance of the awesomebar.
-        self.countVisits { numVisits in
-            if let numVisits = numVisits {
-                GleanMetrics.History.numVisits.set(Int64(numVisits))
-            }
-        }
     }
-
+    
     public func getSites(forURLs urls: [String]) -> Deferred<Maybe<Cursor<Site?>>> {
         let inExpression = urls.joined(separator: "\",\"")
         let sql = """
@@ -79,37 +73,96 @@ open class SQLiteHistory {
         FROM history
         WHERE history.url IN (\"\(inExpression)\")
         """
-
+        
         let args: Args = []
         return database.runQueryConcurrently(sql, args: args, factory: SQLiteHistory.iconHistoryColumnFactory)
     }
+    
+    func recordVisitedSite(_ site: Site) -> Success {
+         // Don't store visits to sites with about: protocols
+         if isIgnoredURL(site.url as String) {
+             return deferMaybe(IgnoredSiteError())
+         }
 
-    public func countVisits(callback: @escaping (Int?) -> Void) {
-        let sql = "SELECT COUNT(*) FROM visits"
-        database.runQueryConcurrently(sql, args: nil, factory: SQLiteHistory.countAllVisitsFactory).uponQueue(.main) { result in
-            guard result.isSuccess else {
-                callback(nil)
-                return
-            }
-            // The result of a count query is only one row
-            if let res = result.successValue?.asArray().first {
-                if let res = res {
-                    callback(res)
-                    return
-                }
-            }
-            callback(nil)
-        }
-    }
+
+         return database.withConnection { conn -> Void in
+             let now = Date.now()
+
+             if self.updateSite(site, atTime: now, withConnection: conn) > 0 {
+                 return
+             }
+
+             // Insert instead.
+             if self.insertSite(site, atTime: now, withConnection: conn) > 0 {
+                 return
+             }
+
+             let err = DatabaseError(description: "Unable to update or insert site; Invalid key returned")
+             log.error("recordVisitedSite encountered an error: \(err.localizedDescription)")
+             throw err
+         }
+     }
+
+     func updateSite(_ site: Site, atTime time: Timestamp, withConnection conn: SQLiteDBConnection) -> Int {
+         // We know we're adding a new visit, so we'll need to upload this record.
+         // If we ever switch to per-visit change flags, this should turn into a CASE statement like
+         //   CASE WHEN title IS ? THEN max(should_upload, 1) ELSE should_upload END
+         // so that we don't flag this as changed unless the title changed.
+         //
+         // Note that we will never match against a deleted item, because deleted items have no URL,
+         // so we don't need to unset is_deleted here.
+         guard let host = (site.url as String).asURL?.normalizedHost else {
+             return 0
+         }
+
+         let update = "UPDATE history SET title = ?, local_modified = ?, should_upload = 1, domain_id = (SELECT id FROM domains where domain = ?) WHERE url = ?"
+         let updateArgs: Args? = [site.title, time, host, site.url]
+         if Logger.logPII {
+             log.debug("Setting title to \(site.title) for URL \(site.url)")
+         }
+         do {
+             try conn.executeChange(update, withArgs: updateArgs)
+             return conn.numberOfRowsModified
+         } catch let error as NSError {
+             log.warning("Update failed with error: \(error.localizedDescription)")
+             return 0
+         }
+     }
+
+     fileprivate func insertSite(_ site: Site, atTime time: Timestamp, withConnection conn: SQLiteDBConnection) -> Int {
+         if let host = (site.url as String).asURL?.normalizedHost {
+             do {
+                 try conn.executeChange("INSERT OR IGNORE INTO domains (domain) VALUES (?)", withArgs: [host])
+             } catch let error as NSError {
+                 log.warning("Domain insertion failed with \(error.localizedDescription)")
+                 return 0
+             }
+
+             let insert = """
+                 INSERT INTO history (
+                     guid, url, title, local_modified, is_deleted, should_upload, domain_id
+                 )
+                 SELECT ?, ?, ?, ?, 0, 1, id FROM domains WHERE domain = ?
+                 """
+
+             let insertArgs: Args? = [site.guid ?? Bytes.generateGUID(), site.url, site.title, time, host]
+             do {
+                 try conn.executeChange(insert, withArgs: insertArgs)
+             } catch let error as NSError {
+                 log.warning("Site insertion failed with \(error.localizedDescription)")
+                 return 0
+             }
+
+             return 1
+         }
+
+         if Logger.logPII {
+             log.warning("Invalid URL \(site.url). Not stored in history.")
+         }
+         return 0
+     }
 }
-
-private let topSitesQuery = """
-        SELECT cached_top_sites.*, page_metadata.provider_name \
-        FROM cached_top_sites \
-        LEFT OUTER JOIN page_metadata ON cached_top_sites.url = page_metadata.site_url \
-        ORDER BY frecencies DESC LIMIT (?)
-        """
-
+    
 extension SQLiteHistory: PinnedSites {
     public func removeFromPinnedTopSites(_ site: Site) -> Success {
         guard let host = (site.url as String).asURL?.normalizedHost else {
@@ -143,18 +196,20 @@ extension SQLiteHistory: PinnedSites {
     }
 
     public func addPinnedTopSite(_ site: Site) -> Success { // needs test
-        let now = Date.now()
-        guard let host = (site.url as String).asURL?.normalizedHost else {
-            return deferMaybe(DatabaseError(description: "Invalid site \(site.url)"))
-        }
+        return self.recordVisitedSite(site) >>== {
+            let now = Date.now()
+            guard let host = (site.url as String).asURL?.normalizedHost else {
+                return deferMaybe(DatabaseError(description: "Invalid site \(site.url)"))
+            }
 
-        let args: Args = [site.url, now, site.title, site.id, host]
-        let arglist = BrowserDB.varlist(args.count)
+            let args: Args = [site.url, now, site.title, site.id, host]
+            let arglist = BrowserDB.varlist(args.count)
 
-        return self.database.run([("INSERT OR REPLACE INTO pinned_top_sites (url, pinDate, title, historyID, domain) VALUES \(arglist)", args)])
-        >>== {
-            self.notificationCenter.post(name: .TopSitesUpdated, object: self)
-            return Success()
+            return self.database.run([("INSERT OR REPLACE INTO pinned_top_sites (url, pinDate, title, historyID, domain) VALUES \(arglist)", args)])
+            >>== {
+                self.notificationCenter.post(name: .TopSitesUpdated, object: self)
+                return Success()
+            }
         }
     }
 
