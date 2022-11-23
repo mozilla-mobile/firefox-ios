931eb91e122256677ab7cf97bd5cb0c7ad149bb1
lmarceau <lmarceau@mozilla.com>
2022-11-23 11:06:50 -0500

    Add FXIOS-5262 [v109] Add letter image generator (#12465)
    * Add LetterImageGenerator
    
    * Clean up
    
    * Change comment place

---
+++ BrowserKit/Sources/SiteImageView/ImageProcessing/LetterImageGenerator.swift
@@ -0,0 +1,132 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0
+
+import UIKit
+
+/// Generate a default letter image from the domain name
+protocol LetterImageGenerator {
+
+    /// Generates a letter image based on the first character in the
+    /// domain name or gets an already-generated icon from the cache.
+    /// - Parameter domain: The string domain name
+    /// - Returns: The generated letter image
+    func generateLetterImage(domain: String) -> UIImage
+}
+
+class DefaultLetterImageGenerator: LetterImageGenerator {
+
+    // An in-memory cache of "default" images keyed by the
+    // first character of a site's domain name.
+    private var defaultImagesCache = [String: UIImage]()
+
+    private let defaultLetter: Character = "#"
+
+    func generateLetterImage(domain: String) -> UIImage {
+        let letter: Character = domain.first ?? defaultLetter
+        let capitalizedLetter = letter.uppercased()
+        // In-memory cache
+        if let cachedImage = defaultImagesCache[capitalizedLetter] {
+            return cachedImage
+        }
+
+        let color = generateBackgroundColor(forDomain: domain)
+        let image = generateImage(fromLetter: capitalizedLetter,
+                                  color: color)
+        defaultImagesCache[capitalizedLetter] = image
+        return image
+    }
+
+    func generateImage(fromLetter letter: String, color: UIColor) -> UIImage {
+        var image = UIImage()
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 60, height: 60))
+        label.text = letter
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 40, weight: UIFont.Weight.medium)
+        label.textColor = .white
+        UIGraphicsBeginImageContextWithOptions(label.bounds.size, false, 0.0)
+        let rect = CGRect(origin: .zero, size: label.bounds.size)
+        let context = UIGraphicsGetCurrentContext()!
+        context.setFillColor(color.cgColor)
+        context.fill(rect)
+        label.layer.render(in: context)
+        image = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        return image
+    }
+
+    private func generateBackgroundColor(forDomain domain: String) -> UIColor {
+        let index = abs(stableHash(domain)) % (defaultBackgroundColors.count - 1)
+        let colorHex = defaultBackgroundColors[index]
+        return UIColor(colorString: colorHex)
+    }
+
+    // A stable hash (unlike hashValue), from https://useyourloaf.com/blog/swift-hashable/
+    private func stableHash(_ str: String) -> Int {
+        let unicodeScalars = str.unicodeScalars.map { $0.value }
+        return unicodeScalars.reduce(5381) {
+            ($0 << 5) &+ $0 &+ Int($1)
+        }
+    }
+
+    // Used as background color for generated letters
+    private let defaultBackgroundColors = ["2e761a",
+                                           "399320",
+                                           "40a624",
+                                           "57bd35",
+                                           "70cf5b",
+                                           "90e07f",
+                                           "b1eea5",
+                                           "881606",
+                                           "aa1b08",
+                                           "c21f09",
+                                           "d92215",
+                                           "ee4b36",
+                                           "f67964",
+                                           "ffa792",
+                                           "025295",
+                                           "0568ba",
+                                           "0675d3",
+                                           "0996f8",
+                                           "2ea3ff",
+                                           "61b4ff",
+                                           "95cdff",
+                                           "00736f",
+                                           "01908b",
+                                           "01a39d",
+                                           "01bdad",
+                                           "27d9d2",
+                                           "58e7e6",
+                                           "89f4f5",
+                                           "c84510",
+                                           "e35b0f",
+                                           "f77100",
+                                           "ff9216",
+                                           "ffad2e",
+                                           "ffc446",
+                                           "ffdf81",
+                                           "911a2e",
+                                           "b7223b",
+                                           "cf2743",
+                                           "ea385e",
+                                           "fa526e",
+                                           "ff7a8d",
+                                           "ffa7b3" ]
+}
+
+// MARK: - UIColor extension
+extension UIColor {
+    convenience init(rgb: Int) {
+        self.init(
+            red: CGFloat((rgb & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgb & 0x00FF00) >> 8)  / 255.0,
+            blue: CGFloat((rgb & 0x0000FF) >> 0)  / 255.0,
+            alpha: 1)
+    }
+
+    convenience init(colorString: String) {
+        var colorInt: UInt64 = 0
+        Scanner(string: colorString).scanHexInt64(&colorInt)
+        self.init(rgb: (Int) (colorInt))
+    }
+}
