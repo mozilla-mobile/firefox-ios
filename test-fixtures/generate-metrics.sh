0baf185bed7520b3d2ad5ba42a3e34f0325aafe1
lmarceau <lmarceau@mozilla.com>
2022-11-25 12:13:42 -0500

    Bugfix FXIOS-5328 [v109] Fix run script warnings (#12503)
    * There's no outputs on those script so unchecked "Based on dependency analysis"
    
    * Temporarely increase to 101 to be able to run builds and figure out what decreases warnings count

--- test-fixtures/generate-metrics.sh
+++ test-fixtures/generate-metrics.sh
@@ -4,10 +4,10 @@ set -e
 
 BUILD_LOG_FILE="$1"
 TYPE_LOG_FILE="$2"
-THREESHOLD_UNIT_TEST=100
-THREESHOLD_XCUITEST=100
+THREESHOLD_UNIT_TEST=101
+THREESHOLD_XCUITEST=101
 
-
+ 
 WARNING_COUNT=`egrep '^(\/.+:[0-9+:[0-9]+:.|warning:|⚠️|ld: warning:|<unknown>:0: warning:|fatal|===)' "$BUILD_LOG_FILE" | uniq | wc -l`
 
 if  [ $2 == "unit-test" ]; then
