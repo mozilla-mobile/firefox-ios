05d5eaa0839ed8653dda3c111204c97949541716
Clare So <cso@mozilla.com>
2022-11-28 12:51:13 -0500

    Update the portion of test that could run on M1. :( Here are the menu items I see on my end.

--- Tests/XCUITests/NavigationTest.swift
+++ Tests/XCUITests/NavigationTest.swift
@@ -250,12 +250,16 @@ class NavigationTest: BaseTestCase {
             if iPad() {
                 XCTAssertTrue(app.menuItems["Copy"].exists)
                 XCTAssertTrue(app.menuItems["Cut"].exists)
-                XCTAssertTrue(app.menuItems["Look Up"].exists)
+                XCTAssertTrue(app.menuItems["Paste"].exists)
+                XCTAssertTrue(app.menuItems["Open Link"].exists)
+                XCTAssertTrue(app.menuItems["Add to Reading List"].exists)
                 XCTAssertTrue(app.menuItems["Share…"].exists)
+                XCTAssertTrue(app.menuItems["Paste & Go"].exists)
             } else {
                 XCTAssertTrue(app.menuItems["Copy"].exists)
                 XCTAssertTrue(app.menuItems["Cut"].exists)
-                XCTAssertTrue(app.menuItems["Look Up"].exists)
+                XCTAssertTrue(app.menuItems["Paste"].exists)
+                XCTAssertTrue(app.menuItems["Open Link"].exists)
             }
 
             app.textFields["address"].typeText("\n")
