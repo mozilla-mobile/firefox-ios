c2bf245e58321682b4b11b8d7dac19ad183ced41
lmarceau <lmarceau@mozilla.com>
2022-12-01 15:18:19 -0500

    Add FXIOS-5260 [v109] Adding SiteImageCache in BrowserKit (#12543)
    * Adding SiteImageCache
    
    * Adjust async await to throw
    
    * Fix mock name
    
    * Fix swiftlint

--- BrowserKit/Sources/SiteImageView/ImageProcessing/ImageError.swift
+++ BrowserKit/Sources/SiteImageView/ImageProcessing/ImageError.swift
@@ -6,11 +6,17 @@ import Foundation
 
 enum ImageError: Error, CustomStringConvertible {
     case unableToDownloadImage(String)
+    case unableToCacheImage(String)
+    case unableToRetrieveFromCache(String)
 
     var description: String {
         switch self {
         case .unableToDownloadImage(let error):
             return "Unable to download image with reason: \(error)"
+        case .unableToCacheImage(let error):
+            return "Unable to cache image with reason: \(error)"
+        case .unableToRetrieveFromCache(let error):
+            return "Unable to retrieve image from cache with reason: \(error)"
         }
     }
 }
---
+++ BrowserKit/Sources/SiteImageView/ImageProcessing/SiteImageCache/DefaultImageCache.swift
@@ -0,0 +1,46 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0
+
+import Kingfisher
+import UIKit
+
+// MARK: - Kingfisher wrapper
+
+/// Image cache wrapper around Kingfisher image cache
+/// Used in SiteImageCache
+protocol DefaultImageCache {
+    func retrieveImage(forKey key: String) async throws -> UIImage?
+
+    func store(image: UIImage, forKey key: String) async throws
+}
+
+extension ImageCache: DefaultImageCache {
+
+    func retrieveImage(forKey key: String) async throws -> UIImage? {
+        return try await withCheckedThrowingContinuation { continuation in
+            retrieveImage(forKey: key) { result in
+                switch result {
+                case .success(let imageResult):
+                    continuation.resume(returning: imageResult.image)
+                case .failure(let error):
+                    continuation.resume(throwing: error)
+                }
+            }
+        }
+    }
+
+    func store(image: UIImage, forKey key: String) async throws {
+        return try await withCheckedThrowingContinuation { continuation in
+            store(image, forKey: key) { result in
+                // memoryCacheResult never fails
+                switch result.diskCacheResult {
+                case .success:
+                    continuation.resume()
+                case .failure(let error):
+                    continuation.resume(throwing: error)
+                }
+            }
+        }
+    }
+}
