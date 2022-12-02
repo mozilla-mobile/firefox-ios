1ccaa05392029edc7d4240a9e618a2e5554a7667
Winnie Teichmann <4530+thatswinnie@users.noreply.github.com>
2022-12-02 13:36:07 -0500

    Rename file and move to extensions

--- Client/UIModalPresentationStyle+Photon.swift
+++ Client/UIModalPresentationStyle+Photon.swift
@@ -2,14 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0
 
-import Foundation
-import Storage
-import SnapKit
-import Shared
 import UIKit
-import MapKit
-
-// Misc table components used for the PhotonActionSheet table view.
 
 extension UIModalPresentationStyle {
     func getPhotonPresentationStyle() -> PresentationStyle {
