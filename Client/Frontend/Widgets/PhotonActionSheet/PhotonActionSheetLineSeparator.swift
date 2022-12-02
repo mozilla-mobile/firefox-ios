c15a1c865b03b053f53d24c76787a6838881d64f
Winnie Teichmann <4530+thatswinnie@users.noreply.github.com>
2022-12-02 13:32:37 -0500

    Move PhotonActionSheetTitleHeaderView and PhotonActionSheetLineSeparator to their own files

---
+++ Client/Frontend/Widgets/PhotonActionSheet/PhotonActionSheetLineSeparator.swift
@@ -0,0 +1,28 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import UIKit
+
+// MARK: - PhotonActionSheetSeparator
+class PhotonActionSheetLineSeparator: UITableViewHeaderFooterView, ReusableCell {
+
+    let separatorLineView = UIView()
+
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        self.backgroundView = UIView()
+        self.backgroundView?.backgroundColor = .clear
+        separatorLineView.backgroundColor = UIColor.Photon.Grey40
+        self.contentView.addSubview(separatorLineView)
+        separatorLineView.snp.makeConstraints { make in
+            make.leading.trailing.equalTo(self)
+            make.centerY.equalTo(self)
+            make.height.equalTo(0.5)
+        }
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
