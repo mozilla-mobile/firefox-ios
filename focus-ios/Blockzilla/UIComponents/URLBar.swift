0e391024ec156dd522713d5f168c335acb0ae6bb
PARAIPAN SORIN <51127880+PARAIPAN9@users.noreply.github.com>
2022-12-02 21:00:26 +0200

    [focus-ios] Fixes https://github.com/mozilla-mobile/focus-ios/issues/3561 - 'Drag URL text' feature interferes with basic text-editing cursor

--- focus-ios/Blockzilla/UIComponents/URLBar.swift
+++ focus-ios/Blockzilla/UIComponents/URLBar.swift
@@ -157,7 +157,7 @@ class URLBar: UIView {
         textAndLockContainer.addGestureRecognizer(longPress)
 
         let dragInteraction = UIDragInteraction(delegate: self)
-        textAndLockContainer.addInteraction(dragInteraction)
+        urlBarBackgroundView.addInteraction(dragInteraction)
 
         addSubview(toolset.backButton)
         addSubview(toolset.forwardButton)
@@ -944,24 +944,6 @@ extension URLBar: UIDragInteractionDelegate {
         GleanMetrics.UrlInteraction.dragStarted.record()
         return [dragItem]
     }
-
-    func dragInteraction(_ interaction: UIDragInteraction, previewForLifting item: UIDragItem, session: UIDragSession) -> UITargetedDragPreview? {
-        let params = UIDragPreviewParameters()
-        params.backgroundColor = UIColor.clear
-        return UITargetedDragPreview(view: draggableUrlTextView, parameters: params)
-    }
-
-    func dragInteraction(_ interaction: UIDragInteraction, sessionDidMove session: UIDragSession) {
-        for item in session.items {
-            item.previewProvider = {
-                guard let url = self.url else {
-                    return UIDragPreview(view: UIView())
-                }
-                return UIDragPreview(for: url)
-            }
-        }
-    }
-
 }
 
 private class URLTextField: AutocompleteTextField {
