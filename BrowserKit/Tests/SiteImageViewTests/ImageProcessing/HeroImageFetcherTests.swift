652d09e497a45eb4902dc55e78e1dd582c4629c9
lmarceau <lmarceau@mozilla.com>
2022-12-06 13:44:57 -0500

    Add FXIOS-5366 [v109] Add hero image fetcher in BrowserKit (#12594)
    * Add hero image fetcher
    
    * Add unit tests

---
+++ BrowserKit/Tests/SiteImageViewTests/ImageProcessing/HeroImageFetcherTests.swift
@@ -0,0 +1,111 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0
+
+import LinkPresentation
+import XCTest
+@testable import SiteImageView
+
+final class HeroImageFetcherTests: XCTestCase {
+
+    private var metadataProvider: MetadataProviderFake!
+
+    override func setUp() {
+        super.setUp()
+        self.metadataProvider = MetadataProviderFake()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+        self.metadataProvider = nil
+    }
+
+    func testHeroImageLoading_whenError_throwsError() async {
+        metadataProvider.errorResult = TestError.invalidResult
+        let subject = DefaultHeroImageFetcher(metadataProvider: metadataProvider)
+        do {
+            _ = try await subject.fetchHeroImage(from: URL(string: "www.example.com")!)
+            XCTFail("Should have failed")
+
+        } catch let error as TestError {
+            XCTAssertEqual("A test error",
+                           error.description)
+        } catch {
+            XCTFail("Should have failed with TestError type")
+        }
+    }
+
+    func testHeroImageLoads_whenEmptyMetadata_throwsError() async {
+        let subject = DefaultHeroImageFetcher(metadataProvider: metadataProvider)
+        do {
+            _ = try await subject.fetchHeroImage(from: URL(string: "www.example.com")!)
+            XCTFail("Should have failed")
+
+        } catch let error as ImageError {
+            XCTAssertEqual("Unable to download image with reason: Metadata image provider could not be retrieved.",
+                           error.description)
+        } catch {
+            XCTFail("Should have failed with ImageError type")
+        }
+    }
+
+    func testHeroImageLoads_whenProviderImageError_throwsError() async {
+        let providerFake = ItemProviderFake()
+        providerFake.errorResult = TestError.invalidResult
+        providerFake.imageResult = nil
+        metadataProvider.metadataResult.imageProvider = providerFake
+        let subject = DefaultHeroImageFetcher(metadataProvider: metadataProvider)
+        do {
+            _ = try await subject.fetchHeroImage(from: URL(string: "www.example.com")!)
+            XCTFail("Should have failed")
+
+        } catch let error as ImageError {
+            XCTAssertEqual("Unable to download image with reason: Optional(A test error)",
+                           error.description)
+        } catch {
+            XCTFail("Should have failed with ImageError type")
+        }
+    }
+
+    func testHeroImageLoads_whenImage_returnsImage() async {
+        metadataProvider.metadataResult.imageProvider = ItemProviderFake()
+        let subject = DefaultHeroImageFetcher(metadataProvider: metadataProvider)
+        do {
+            let image = try await subject.fetchHeroImage(from: URL(string: "www.example.com")!)
+            XCTAssertNotNil(image)
+
+        } catch {
+            XCTFail("Should have succeed with image")
+        }
+    }
+}
+
+// MARK: - Helper
+private extension HeroImageFetcherTests {
+    enum TestError: Error, CustomStringConvertible {
+        var description: String { "A test error" }
+
+        case invalidResult
+    }
+}
+
+// MARK: - MetadataProviderFake
+private class MetadataProviderFake: LPMetadataProvider {
+
+    var metadataResult = LPLinkMetadata()
+    var errorResult: Error?
+    override func startFetchingMetadata(for URL: URL, completionHandler: @escaping (LPLinkMetadata?, Error?) -> Void) {
+        completionHandler(metadataResult, errorResult)
+    }
+}
+
+// MARK: - ItemProviderFake
+private  class ItemProviderFake: NSItemProvider {
+
+    var imageResult: UIImage? = UIImage()
+    var errorResult: Error?
+    override func loadObject(ofClass aClass: NSItemProviderReading.Type, completionHandler: @escaping (NSItemProviderReading?, Error?) -> Void) -> Progress {
+        completionHandler(imageResult, errorResult)
+        return Progress()
+    }
+}
