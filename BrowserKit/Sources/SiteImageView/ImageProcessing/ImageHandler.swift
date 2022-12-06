8773f4ef060267578ea578cf7824d07b869c9dc7
Laurie Marceau <lmarceau@mozilla.com>
2022-12-06 16:37:16 -0500

    Add tests and document usage of image handler

--- BrowserKit/Sources/SiteImageView/ImageProcessing/ImageHandler.swift
+++ BrowserKit/Sources/SiteImageView/ImageProcessing/ImageHandler.swift
@@ -5,8 +5,34 @@
 import UIKit
 
 protocol ImageHandler {
-    func fetchFavicon(imageURL: URL?, domain: String) async throws -> UIImage
-    func fetchHeroImage(siteURL: URL, domain: String) async throws -> UIImage
+
+    /// The ImageHandler will fetch the favicon with the following precedence:
+    ///     1. Tries to fetch from the bundle.
+    ///     2. Tries to fetch from the cache.
+    ///     3. Tries to fetch from the image fetcher (from the web) if there's a URL. If there's no URL it fallbacks to the letter favicon.
+    ///     4. When all fails it returns the letter favicon.
+    ///
+    /// Any time the favicon is fetched, it will be cache for future usage.
+    ///
+    /// - Parameters:
+    ///   - imageURL: The image URL, can be nil if it could not be retrieved from the site
+    ///   - domain: The domain this favicon will be associated with
+    /// - Returns: The favicon image
+    func fetchFavicon(imageURL: URL?,
+                      domain: String) async throws -> UIImage
+
+    /// The ImageHandler will fetch the hero image with the following precedence
+    ///     1. Tries to fetch from the cache.
+    ///     2. Tries to fetch from the hero image fetcher (from the web).
+    ///     3. If all fails it throws an error
+    ///
+    /// Any time the hero image  is fetched, it will be cache for future usage.
+    /// - Parameters:
+    ///   - siteURL: The site URL to fetch the hero image from
+    ///   - domain: The domain this hero image will be associated with
+    /// - Returns: The hero image
+    func fetchHeroImage(siteURL: URL,
+                        domain: String) async throws -> UIImage
 }
 
 class DefaultImageHandler: ImageHandler {
@@ -29,16 +55,40 @@ class DefaultImageHandler: ImageHandler {
         self.heroImageFetcher = heroImageFetcher
     }
 
-    func fetchFavicon(imageURL: URL?, domain: String) async throws -> UIImage {
+    func fetchFavicon(imageURL: URL?,
+                      domain: String) async throws -> UIImage {
         do {
             return try bundleImageFetcher.getImageFromBundle(domain: domain)
+        } catch {
+            return try await fetchFaviconFromCache(imageURL: imageURL, domain: domain)
+        }
+    }
 
-        } catch is BundleError {
+    func fetchHeroImage(siteURL: URL,
+                        domain: String) async throws -> UIImage {
+        do {
+            return try await imageCache.getImageFromCache(domain: domain, type: .heroImage)
+        } catch {
+            return try await fetchHeroImageFromFetcher(siteURL: siteURL, domain: domain)
+        }
+    }
+
+    // MARK: Private
+
+    private func fetchFaviconFromCache(imageURL: URL?,
+                                       domain: String) async throws -> UIImage {
+        do {
             return try await imageCache.getImageFromCache(domain: domain, type: .favicon)
+        } catch {
+            return try await fetchFaviconFromFetcher(imageURL: imageURL, domain: domain)
+        }
+    }
 
-        } catch SiteImageError.unableToRetrieveFromCache {
+    private func fetchFaviconFromFetcher(imageURL: URL?,
+                                         domain: String) async throws -> UIImage {
+        do {
             guard let url = imageURL else {
-                return await fallbackLetterFavicon(domain: domain)
+                return await fallbackToLetterFavicon(domain: domain)
             }
 
             let image = try await imageFetcher.fetchImage(from: url)
@@ -46,22 +96,13 @@ class DefaultImageHandler: ImageHandler {
             return image
 
         } catch {
-            return await fallbackLetterFavicon(domain: domain)
+            return await fallbackToLetterFavicon(domain: domain)
         }
     }
 
-    // If we have no image URL or all other method fails, then use the letter favicon
-    private func fallbackLetterFavicon(domain: String) async -> UIImage {
-        let image = letterImageGenerator.generateLetterImage(domain: domain)
-        await imageCache.cacheImage(image: image, domain: domain, type: .favicon)
-        return image
-    }
-
-    func fetchHeroImage(siteURL: URL, domain: String) async throws -> UIImage {
+    private func fetchHeroImageFromFetcher(siteURL: URL,
+                                           domain: String) async throws -> UIImage {
         do {
-            return try await imageCache.getImageFromCache(domain: domain, type: .heroImage)
-
-        } catch SiteImageError.unableToRetrieveFromCache {
             let image = try await heroImageFetcher.fetchHeroImage(from: siteURL)
             await imageCache.cacheImage(image: image, domain: domain, type: .heroImage)
             return image
@@ -69,5 +110,11 @@ class DefaultImageHandler: ImageHandler {
         } catch {
             throw SiteImageError.noHeroImage
         }
+    }
+
+    private func fallbackToLetterFavicon(domain: String) async -> UIImage {
+        let image = letterImageGenerator.generateLetterImage(domain: domain)
+        await imageCache.cacheImage(image: image, domain: domain, type: .favicon)
+        return image
     }
 }
