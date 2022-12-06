41b9bdc1f550bc19f15204a77b4fd2e8496fda13
lmarceau <lmarceau@mozilla.com>
2022-12-06 13:44:57 -0500

    Add FXIOS-5366 [v109] Add hero image fetcher in BrowserKit (#12594)
    * Add hero image fetcher
    
    * Add unit tests

---
+++ BrowserKit/Sources/SiteImageView/ImageProcessing/HeroImageFetcher/HeroImageFetcher.swift
@@ -0,0 +1,33 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0
+
+import LinkPresentation
+import UIKit
+
+protocol HeroImageFetcher {
+    func fetchHeroImage(from imageURL: URL) async throws -> UIImage
+}
+
+class DefaultHeroImageFetcher: HeroImageFetcher {
+
+    private let metadataProvider: LPMetadataProvider
+
+    init(metadataProvider: LPMetadataProvider = LPMetadataProvider()) {
+        self.metadataProvider = metadataProvider
+    }
+
+    func fetchHeroImage(from imageURL: URL) async throws -> UIImage {
+        do {
+            let metadata = try await metadataProvider.startFetchingMetadata(for: imageURL)
+            guard let imageProvider = metadata.imageProvider else {
+                throw ImageError.unableToDownloadImage("Metadata image provider could not be retrieved.")
+            }
+
+            return try await imageProvider.loadObject(ofClass: UIImage.self)
+
+        } catch {
+            throw error
+        }
+    }
+}
