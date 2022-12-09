3668759f68afcccfd6aefdfb4ebdb3445f7667b9
nishant2718 <nishantpatel2718@gmail.com>
2022-12-09 08:29:29 -0500

    PR updates

--- Client/Frontend/Library/RecentlyClosedTabsPanel.swift
+++ Client/Frontend/Library/RecentlyClosedTabsPanel.swift
@@ -58,6 +58,11 @@ class RecentlyClosedTabsPanel: UIViewController, LibraryPanel {
     }
 
     override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        /// BVC is assigned as `RecentlyClosedTabsPanel` delegate, to support opening tabs from within it.
+        /// Previously, BVC was assigned it on panel creation via a foregroundBVC call. But it can be done this way, to
+        /// avoid that call. `sceneForVC` will use the focused, active and foregrounded scene's BVC.
         guard recentlyClosedTabsDelegate != nil else {
             recentlyClosedTabsDelegate = sceneForVC?.browserViewController
 
