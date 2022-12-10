5df9219575569ba91349ebfad7e7a805bd204c53
Orla <omitchell@mozilla.com>
2022-12-09 21:39:05 -0500

    Fix performance issue

--- BrowserKit/Sources/Common/Extensions/URLExtension.swift
+++ BrowserKit/Sources/Common/Extensions/URLExtension.swift
@@ -142,9 +142,8 @@ extension URL {
         for offset in 0..<tokenCount {
             // Store the offset for use outside of this scope so we can add additional parts if needed
             let nextDot: String? = offset + 1 < tokenCount ? tokens[offset + 1..<tokenCount].joined(separator: ".") : nil
-            let etldEntries = loadEntries()
 
-            if let entry = etldEntries[currentDomain] {
+            if let entry = etldEntries?[currentDomain] {
                 if entry.isWild && (previousDomain != nil) {
                     suffix = previousDomain
                     break
@@ -187,25 +186,6 @@ extension URL {
 
         return baseDomain
     }
-
-    private func loadEntries() -> [String: ETLDEntry] {
-        var entries = [String: ETLDEntry]()
-        for line in ETLD_NAMES_LIST where !line.isEmpty && !line.hasPrefix("//") {
-            let entry = ETLDEntry(entry: line)
-            let key: String
-            if entry.isWild {
-                // Trim off the '*.' part of the line
-                key = String(line[line.index(line.startIndex, offsetBy: 2)...])
-            } else if entry.isException {
-                // Trim off the '!' part of the line
-                key = String(line[line.index(line.startIndex, offsetBy: 1)...])
-            } else {
-                key = line
-            }
-            entries[key] = entry
-        }
-        return entries
-    }
 }
 
 private struct ETLDEntry: CustomStringConvertible {
@@ -225,3 +205,28 @@ private struct ETLDEntry: CustomStringConvertible {
         return "{ Entry: \(entry), isWildcard: \(isWild), isException: \(isException) }"
     }
 }
+
+private typealias TLDEntryMap = [String: ETLDEntry]
+
+private func loadEntries() -> TLDEntryMap? {
+    var entries = TLDEntryMap()
+    for line in ETLD_NAMES_LIST where !line.isEmpty && !line.hasPrefix("//") {
+        let entry = ETLDEntry(entry: line)
+        let key: String
+        if entry.isWild {
+            // Trim off the '*.' part of the line
+            key = String(line[line.index(line.startIndex, offsetBy: 2)...])
+        } else if entry.isException {
+            // Trim off the '!' part of the line
+            key = String(line[line.index(line.startIndex, offsetBy: 1)...])
+        } else {
+            key = line
+        }
+        entries[key] = entry
+    }
+    return entries
+}
+
+private var etldEntries: TLDEntryMap? = {
+    return loadEntries()
+}()
