5299a9eb23aa08eaf9dd8197aa650dfccce30a6e
Laurie Marceau <lmarceau@mozilla.com>
2022-12-12 17:55:39 -0500

    Fix header

--- BrowserKit/Sources/Common/Extensions/EffectiveTLDNames.swift
+++ BrowserKit/Sources/Common/Extensions/EffectiveTLDNames.swift
@@ -1,6 +1,6 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
-* License, v. 2.0. If a copy of the MPL was not distributed with this
-* file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0
 
 let ETLD_NAMES_LIST: [String] = {
     // The Swift compiler (as of Xcode 12.5) will crash during optimised builds if ETLD_NAMES_LIST is a single array.
