42fd5fca412cea9bd64062aec4e0de26385ff086
lmarceau <lmarceau@mozilla.com>
2022-12-20 17:13:24 -0500

    Refactor FXIOS-5162 [v110] Adjust content blocker (#12632)
    * Recreate content blocker package to remove sub library. Clean up logic and use @main executable
    
    * Adjust content blocking part 1 
    
    Ingests new lists format
    Add comment in parser to understand previous method better
    Note down question.
    
    * Generate unless domain as needed for white listing using the entity list
    
    * Delete, not used
    
    * Rename functions, document, create ContentBlockerFileManager
    
    * Add generator tests
    
    * Update read me and script
    
    * Adjust file path + name
    
    * Add missing test case + adjustment
    
    * Add back content file since project expects it (and anyway Focus uses it at the moment)
    
    * Add missing [ at beginning of file
    
    * revert unwanted change on shared data
    
    * Adjust to search if resource is contained to find entity in list
    
    * Fix issue of creating directory
    
    * Fix swiftlint
    
    * Fix indentation

--- content_blocker_update.sh
+++ content_blocker_update.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-#
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-# Update commit hash when you need to update shavar prod list
-# Note: we can update this to use a tag / branch in future
-SHAVAR_COMMIT_HASH="3910527004252af3aa9dd701566a2cb3b78e5c3a"
+
+# Version 107.0 hash
+SHAVAR_COMMIT_HASH="1d71be25893b05ba85850078b1ccedc0824ca958"
 
 # Install Node.js dependencies and build user scripts
 npm install
@@ -16,4 +15,4 @@ npm run build
 # Clone shavar prod list
 rm -rf shavar-prod-lists && git clone https://github.com/mozilla-services/shavar-prod-lists.git && git -C shavar-prod-lists checkout $SHAVAR_COMMIT_HASH
 
-(cd content-blocker-lib-ios/ContentBlockerGen && swift run)
+(cd content-blocker-lib-ios/ContentBlockerGenerator && swift run)
