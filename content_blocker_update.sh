f30faed8e5c2514841db8580831b896745359434
isabelrios <isabelrios@gmail.com>
2022-12-21 14:23:35 +0100

    Add MTE-401 [v110] Content Blocker Updated and Shavar Github Actions

--- content_blocker_update.sh
+++ content_blocker_update.sh
@@ -4,15 +4,26 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+# Current shavar version
+CURRENT_SHAVAR_LIST_VERSION="107"
 
-# Version 107.0 hash
-SHAVAR_COMMIT_HASH="1d71be25893b05ba85850078b1ccedc0824ca958"
+# Download shavar prod list
+echo "shavar-lists-v${CURRENT_SHAVAR_LIST_VERSION}"
+rm -rf shavar-prod-lists && mkdir shavar-prod-lists && mkdir shavar-prod-lists/normalized-lists
 
 # Install Node.js dependencies and build user scripts
 npm install
 npm run build
 
-# Clone shavar prod list
-rm -rf shavar-prod-lists && git clone https://github.com/mozilla-services/shavar-prod-lists.git && git -C shavar-prod-lists checkout $SHAVAR_COMMIT_HASH
+# Download shavar prod list
+files="ads-track-digest256.json analytics-track-digest256.json base-cryptomining-track-digest256.json base-fingerprinting-track-digest256.json content-track-digest256.json social-track-digest256.json disconnect-entitylist.json"
+
+for file in $files
+do
+	echo "$file"
+	curl "https://storage.googleapis.com/shavar-lists-ios-public/Public/shavar-lists-v${CURRENT_SHAVAR_LIST_VERSION}/${file}" -o "shavar-prod-lists/normalized-lists/${file}"
+done
+
+cp shavar-prod-lists/normalized-lists/disconnect-entitylist.json shavar-prod-lists/
 
 (cd content-blocker-lib-ios/ContentBlockerGenerator && swift run)
