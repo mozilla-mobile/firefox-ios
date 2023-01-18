ce42e7ed9b76441a5340dac3b3eb05f036b2428e
Nishant Bhasin <nish.bhasin@gmail.com>
2023-01-17 21:31:55 -0500

    credit card initial views and setup

---
+++ Client/Frontend/Autofill/CreditCard/CreditCardModifierView.swift
@@ -0,0 +1,46 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import Foundation
+import SwiftUI
+
+struct CreditCardModifierView: View {
+    var body: some View {
+        ZStack {
+            Color(UIColor.clear)
+                .edgesIgnoringSafeArea(.all)
+            VStack() {
+                Spacer()
+                Text(String.CreditCard.Settings.EmptyListTitle)
+                    .font(.system(size: 22))
+                    .padding(.leading, 10)
+                    .padding(.trailing, 10)
+                Text(String.CreditCard.Settings.EmptyListDescription)
+                    .font(.system(size: 16))
+                    .multilineTextAlignment(.center)
+                    .lineLimit(2)
+                    .padding(.leading, 10)
+                    .padding(.trailing, 10)
+                    .padding([.top], -5)
+                Spacer()
+                    .frame(height: 25)
+                Image("largePrivateMask")
+                    .resizable()
+                    .frame(width: 200, height: 200)
+                    .aspectRatio(contentMode: .fit)
+                    .fixedSize()
+                    .padding([.top], 10)
+                Spacer()
+                Spacer()
+                Spacer()
+            }
+        }
+    }
+}
+
+struct CreditCardModifierView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreditCardModifierView()
+    }
+}
