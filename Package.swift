d876883dbf2be10d93e888130c11a0c9948bc3a5
isabelrios <isabelrios@gmail.com>
2023-01-24 11:42:12 +0100

    add package.swift and step in bitrise

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "Danger",
+    platforms: [.iOS("15"), .macOS("11")],
+    products: [
+        .library(name: "DangerDeps", type: .dynamic, targets: ["DangerDependencies"]), // dev
+    ],
+    dependencies: [
+        .package(url: "https://github.com/danger/swift.git", exact: "3.15.0"), // dev
+        .package(url: "https://github.com/f-meloni/danger-swift-coverage", exact: "1.2.1") // dev
+    ],
+    targets: [
+        .target(
+            name: "DangerDependencies",
+            dependencies: [
+                .product(name: "Danger", package: "swift"),
+                .product(name: "DangerSwiftCoverage", package: "danger-swift-coverage")
+            ]
+        ) // dev
+    ]
+)
