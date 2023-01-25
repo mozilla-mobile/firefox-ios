623739d75fe9053184dff71a7e6d02d43a5495eb
isabelrios <isabelrios@gmail.com>
2023-01-25 15:05:15 +0100

    remove wrong folder

--- Source/DangerDependencies/Fake.swift
+++
@@ -1,7 +0,0 @@
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v. 2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at http://mozilla.org/MPL/2.0/
-/// Danger documentation recommends having a file that only imports `Foundation`, so
-/// libDanger is created and there's at least one build target in a Swift package.
-/// https://danger.systems/swift/guides/getting_started.html#creating-a-dangerfile
-import Foundation
