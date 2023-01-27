7b8c330a7c7bb787c07248827351024f4b949f45
Nishant Bhasin <nish.bhasin@gmail.com>
2023-01-27 14:58:29 -0500

    Added settings view in form of SwiftUI

---
+++ Client/Frontend/Autofill/CreditCard/ViewComponents+Style/Style.swift
@@ -0,0 +1,21 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import Foundation
+import SwiftUI
+
+struct ErrorTextStyle : ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.system(size: 15))
+            .padding(.leading, 10)
+            .foregroundColor(.red)
+    }
+}
+
+extension View {
+    func errorTextStyle() -> some View {
+        self.modifier(ErrorTextStyle())
+    }
+}
