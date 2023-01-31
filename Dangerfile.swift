3760117a87b64ab36353e72f50ec298b6d04dd67
isabelrios <isabelrios@gmail.com>
2023-01-31 12:42:48 +0100

    Add MTE-528 [v111] Include dangerfile to CI (#13018)
    * Add MTE-528 [v111] Include dangerfile to CI
    
    * use pre built test without building
    
    * deploy pipeline_intermediate_files
    
    * use fennec scheme
    
    * add unit test to Fennec
    
    * Browserkit unit tests
    
    * fix spelling error

---
+++ Dangerfile.swift
@@ -0,0 +1,35 @@
+import Danger
+import DangerSwiftCoverage
+import Foundation
+
+let danger = Danger()
+
+coverage()
+changedFiles()
+
+func changedFiles() {
+    message("Edited \(danger.git.modifiedFiles.count) files")
+    message("Created \(danger.git.createdFiles.count) files")
+}
+
+func coverage() {
+    guard let xcresult = ProcessInfo.processInfo.environment["BITRISE_XCRESULT_PATH"]?.escapeString() else {
+        fail("Could not get the BITRISE_XCRESULT_PATH to generate code coverage")
+        return
+    }
+
+    Coverage.xcodeBuildCoverage(
+        .xcresultBundle(xcresult),
+        minimumCoverage: 50
+    )
+}
+
+extension String {
+    // Helper function to escape (iOS) in our file name for xcov.
+    func escapeString() -> String {
+        var newString = self.replacingOccurrences(of: "(", with: "\\(")
+        newString = newString.replacingOccurrences(of: ")", with: "\\)")
+        newString = newString.replacingOccurrences(of: " ", with: "\\ ")
+        return newString
+    }
+}
