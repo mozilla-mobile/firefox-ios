fbd7d317ebf9e4d7d828d30a8854f88c117f42ea
OrlaM <omitchell@mozilla.com>
2023-02-01 15:56:37 -0500

    Refactor FXIOS-5685 [v111] Clean up some dead code in BrowserDB (#13106)

--- Storage/SQL/BrowserDB.swift
+++ Storage/SQL/BrowserDB.swift
@@ -32,61 +32,14 @@ open class BrowserDB {
         self.db = SwiftData(filename: self.databasePath, schema: schema, files: files)
     }
 
-    // Returns the SQLite version for debug purposes.
-    public func sqliteVersion() -> Deferred<Maybe<String>> {
-        return withConnection { connection -> String in
-            let result = connection.executeQueryUnsafe(
-                "SELECT sqlite_version()",
-                factory: { row -> String in
-                    return row[0] as? String ?? ""
-                },
-                withArgs: nil)
-            return result.asArray().first ?? ""
-        }
-    }
-
-    // Returns the SQLite compile_options for debug purposes.
-    public func sqliteCompileOptions() -> Deferred<Maybe<[String]>> {
-        return withConnection { connection -> [String] in
-            let result = connection.executeQueryUnsafe(
-                "PRAGMA compile_options",
-                factory: { row -> String in
-                    return row[0] as? String ?? ""
-                },
-                withArgs: nil)
-            return result.asArray().filter({ !$0.isEmpty })
-        }
-    }
-
-    // Returns the SQLite secure_delete setting for debug purposes.
-    public func sqliteSecureDelete() -> Deferred<Maybe<Int>> {
-        return withConnection { connection -> Int in
-            let result = connection.executeQueryUnsafe(
-                "PRAGMA secure_delete",
-                factory: { row -> Int in
-                    return row[0] as? Int ?? 0
-                },
-                withArgs: nil)
-            return result.asArray().first ?? 0
-        }
-    }
-
-    // For testing purposes or other cases where we want to ensure that this `BrowserDB`
-    // instance has been initialized (schema is created/updated).
-    public func touch() -> Success {
-        return withConnection { connection -> Void in
-            guard connection as? ConcreteSQLiteDBConnection != nil else {
-                throw DatabaseError(description: "Could not establish a database connection")
-            }
-        }
-    }
-
     /*
      * Opening a WAL-using database with a hot journal cannot complete in read-only mode.
      * The supported mechanism for a read-only query against a WAL-using SQLite database is to use PRAGMA query_only,
      * but this isn't all that useful for us, because we have a mixed read/write workload.
      */
-    @discardableResult func withConnection<T>(flags: SwiftData.Flags = .readWriteCreate, _ callback: @escaping (_ connection: SQLiteDBConnection) throws -> T) -> Deferred<Maybe<T>> {
+    @discardableResult func withConnection<T>(flags: SwiftData.Flags = .readWriteCreate, _
+                                              callback: @escaping (_ connection: SQLiteDBConnection) throws -> T)
+    -> Deferred<Maybe<T>> {
         return db.withConnection(flags, callback)
     }
 
@@ -94,97 +47,8 @@ open class BrowserDB {
         return db.transaction(callback)
     }
 
-    @discardableResult func vacuum() -> Success {
-        return withConnection({ connection -> Void in
-            try connection.vacuum()
-        })
-    }
-
-    @discardableResult func checkpoint() -> Success {
-        return transaction { connection in
-            connection.checkpoint()
-        }
-    }
-
     public class func varlist(_ count: Int) -> String {
         return "(" + Array(repeating: "?", count: count).joined(separator: ", ") + ")"
-    }
-
-    enum InsertOperation: String {
-        case Insert = "INSERT"
-        case Replace = "REPLACE"
-        case InsertOrIgnore = "INSERT OR IGNORE"
-        case InsertOrReplace = "INSERT OR REPLACE"
-        case InsertOrRollback = "INSERT OR ROLLBACK"
-        case InsertOrAbort = "INSERT OR ABORT"
-        case InsertOrFail = "INSERT OR FAIL"
-    }
-
-    /**
-     * Insert multiple sets of values into the given table.
-     *
-     * Assumptions:
-     * 1. The table exists and contains the provided columns.
-     * 2. Every item in `values` is the same length.
-     * 3. That length is the same as the length of `columns`.
-     * 4. Every value in each element of `values` is non-nil.
-     *
-     * If there are too many items to insert, multiple individual queries will run
-     * in sequence.
-     *
-     * A failure anywhere in the sequence will cause immediate return of failure, but
-     * will not roll back — use a transaction if you need one.
-     */
-    func bulkInsert(_ table: String, op: InsertOperation, columns: [String], values: [Args]) -> Success {
-        // Note that there's a limit to how many ?s can be in a single query!
-        // So here we execute 999 / (columns * rows) insertions per query.
-        // Note that we can't use variables for the column names, so those don't affect the count.
-        if values.isEmpty {
-            return succeed()
-        }
-
-        let variablesPerRow = columns.count
-
-        // Sanity check.
-        assert(values[0].count == variablesPerRow)
-
-        let cols = columns.joined(separator: ", ")
-        let queryStart = "\(op.rawValue) INTO \(table) (\(cols)) VALUES "
-
-        let varString = BrowserDB.varlist(variablesPerRow)
-
-        let insertChunk: ([Args]) -> Success = { vals -> Success in
-            let valuesString = Array(repeating: varString, count: vals.count).joined(separator: ", ")
-            let args: Args = vals.flatMap { $0 }
-            return self.run(queryStart + valuesString, withArgs: args)
-        }
-
-        let rowCount = values.count
-        if (variablesPerRow * rowCount) < BrowserDB.MaxVariableNumber {
-            return insertChunk(values)
-        }
-
-        let rowsPerInsert = (999 / variablesPerRow)
-        let chunks = chunk(values, by: rowsPerInsert)
-        logger.log("Inserting in \(chunks.count) chunks in BrowserDB.",
-                   level: .debug,
-                   category: .storage)
-
-        // There's no real reason why we can't pass the ArraySlice here, except that I don't
-        // want to keep fighting Swift.
-        return walk(chunks, f: { insertChunk(Array($0)) })
-    }
-
-    func write(_ sql: String, withArgs args: Args? = nil) -> Deferred<Maybe<Int>> {
-        return withConnection { connection -> Int in
-            try connection.executeChange(sql, withArgs: args)
-
-            let modified = connection.numberOfRowsModified
-            self.logger.log("Write modified rows: \(modified) in BrowserDB.",
-                            level: .debug,
-                            category: .storage)
-            return modified
-        }
     }
 
     public func forceClose() {
@@ -197,10 +61,6 @@ open class BrowserDB {
 
     public func run(_ sql: String, withArgs args: Args? = nil) -> Success {
         return run([(sql, args)])
-    }
-
-    func run(_ commands: [String]) -> Success {
-        return run(commands.map { (sql: $0, args: nil) })
     }
 
     /**
@@ -232,22 +92,9 @@ open class BrowserDB {
         }
     }
 
-    func runQueryUnsafe<T, U>(_ sql: String, args: Args?, factory: @escaping (SDRow) -> T, block: @escaping (Cursor<T>) throws -> U) -> Deferred<Maybe<U>> {
-        return withConnection { connection -> U in
-            let cursor = connection.executeQueryUnsafe(sql, factory: factory, withArgs: args)
-            defer { cursor.close() }
-            return try block(cursor)
-        }
-    }
-
     func queryReturnsResults(_ sql: String, args: Args? = nil) -> Deferred<Maybe<Bool>> {
         return runQuery(sql, args: args, factory: { _ in true })
          >>== { deferMaybe($0[0] ?? false) }
-    }
-
-    func queryReturnsNoResults(_ sql: String, args: Args? = nil) -> Deferred<Maybe<Bool>> {
-        return runQuery(sql, args: nil, factory: { _ in false })
-          >>== { deferMaybe($0[0] ?? true) }
     }
 }
 
