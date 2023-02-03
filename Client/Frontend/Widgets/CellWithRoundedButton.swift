85e18592f8db8bbe816c54162fbd2f98c0bd3d5b
Nishant Bhasin <nish.bhasin@gmail.com>
2023-02-03 17:36:29 -0500

    Readded 4 preferred font warnings as that could break UI

--- Client/Frontend/Widgets/CellWithRoundedButton.swift
+++ Client/Frontend/Widgets/CellWithRoundedButton.swift
@@ -26,11 +26,10 @@ class CellWithRoundedButton: UITableViewCell, NotificationThemeable, ReusableCel
 
     private lazy var roundedButton: UIButton = {
         let button = UIButton()
-
         button.titleLabel?.font = DynamicFontHelper.defaultHelper.preferredFont(
             withTextStyle: .body,
-            size: 16,
-            weight: .semibold)
+            weight: .semibold,
+            maxSize: 16)
         button.setImage(UIImage(named: ImageIdentifiers.trashIconMonochrome), for: .normal)
         button.tintColor = .black
         button.backgroundColor = .Photon.LightGrey30
