b7302b45b801ea24edeab2a6ec21e16d6c18d1f5
Laurie Marceau <lmarceau@mozilla.com>
2023-02-09 13:56:26 -0500

    Fix conflicts on 110 branch

--- BrowserKit/Sources/SiteImageView/FaviconImageView.swift
+++ BrowserKit/Sources/SiteImageView/FaviconImageView.swift
@@ -50,7 +50,7 @@ public class FaviconImageView: UIImageView, SiteImageView {
     // MARK: - SiteImageView
 
     func setURL(_ viewModel: FaviconImageViewModel) {
-        guard canMakeRequest(with: viewModel.siteURLString ?? viewModel.faviconURL?.absoluteString) else { return }
+        guard canMakeRequest(with: viewModel.urlStringRequest) else { return }
 
         let id = UUID()
         uniqueID = id
