cff4f72d4513f79bf38de315e30d49c7b41e4e51
Nishant Bhasin <nish.bhasin@gmail.com>
2023-02-10 13:28:17 -0500

    Add [v112] FXIOS-5537 credit card settings and empty page (#13120)
    Added basic structure for the following credit card views
    
    FXIOS-5044 - Storage layer for credit card
    FXIOS-5537 - Added empty state
    FXIOS-5526 - Validation Check
    FXIOS-5538 - Credit card states (Empty, List, Edit and Add)

---
+++ Client/Frontend/Autofill/CreditCard/CreditCardSettingsView/CreditCardEditView.swift
@@ -0,0 +1,60 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import Foundation
+import SwiftUI
+
+struct CreditCardEditView: View {
+    @ObservedObject var viewModel: CreditCardEditViewModel
+    let removeButtonColor: Color
+    let borderColor: Color
+
+    var body: some View {
+        VStack(spacing: 11) {
+            let colors = FloatingTextField.Colors(
+                errorColor: .red,
+                titleColor: .gray,
+                textFieldColor: .gray)
+
+            FloatingTextField(label: String.CreditCard.EditCard.NameOnCardTitle,
+                              textVal: $viewModel.nameOnCard,
+                              errorString: String.CreditCard.ErrorState.NameOnCardSublabel,
+                              showError: !viewModel.nameIsValid,
+                              colors: colors)
+            Divider()
+                .frame(height: 0.7)
+
+            FloatingTextField(label: String.CreditCard.EditCard.CardNumberTitle,
+                              textVal: $viewModel.cardNumber,
+                              errorString: String.CreditCard.ErrorState.CardNumberTitle,
+                              showError: !viewModel.numberIsValid,
+                              colors: colors)
+            Divider()
+                .frame(height: 0.7)
+
+            FloatingTextField(label: String.CreditCard.EditCard.CardExpirationDateTitle,
+                              textVal: $viewModel.expirationDate,
+                              errorString: String.CreditCard.ErrorState.CardExpirationDateTitle,
+                              showError: !viewModel.expirationIsValid,
+                              colors: colors)
+            Divider()
+                .frame(height: 0.7)
+
+            Spacer()
+
+            RemoveCardButton(removeButtonColor: removeButtonColor,
+                             borderColor: borderColor)
+        }
+        .padding(.top, 20)
+    }
+}
+
+struct CreditCardEditView_Previews: PreviewProvider {
+    static var previews: some View {
+        let viewModel = CreditCardEditViewModel(firstName: "Mike", lastName: "Simmons", errorState: "Temp", enteredValue: "")
+        CreditCardEditView(viewModel: viewModel,
+                           removeButtonColor: .gray,
+                           borderColor: .gray)
+    }
+}
