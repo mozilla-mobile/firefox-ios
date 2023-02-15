207de5e38dac80ba1237ae8ab27c6a1b8413eca0
PARAIPAN SORIN <paraipan.sorin1993@gmail.com>
2023-02-15 15:58:51 +0100

    refact: Change version to v112

--- Client/Frontend/Strings.swift
+++ Client/Frontend/Strings.swift
@@ -180,7 +180,7 @@ extension String {
     public struct FirefoxHomepage {
         public struct Common {
             public static let PagesCount = MZLocalizedString(
-                "FirefoxHomepage.Common.PagesCount.v101",
+                "FirefoxHomepage.Common.PagesCount.v112",
                 tableName: nil,
                 value: "Pages: %d",
                 comment: "Label showing how many pages there is in a search group. %d represents a number")
