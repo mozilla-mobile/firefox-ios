34a5648e303c3386e9a7e7838c54ed64d0ddded6
Winnie Teichmann <4530+thatswinnie@users.noreply.github.com>
2023-02-16 15:05:45 -0500

    Add FXIOS-5692 [v112] Credit card settings UX - Add accessibility for credit card list (#13246)
    ---------
    
    Co-authored-by: Nishant Bhasin <nish.bhasin@gmail.com>
    Co-authored-by: yoanarios <yriosdiaz@mozilla.com>

--- Client/Frontend/Autofill/CreditCard/CreditCardSettingsView/CreditCardSettingsEmptyView.swift
+++ Client/Frontend/Autofill/CreditCard/CreditCardSettingsView/CreditCardSettingsEmptyView.swift
@@ -6,32 +6,51 @@ import Foundation
 import SwiftUI
 
 struct CreditCardSettingsEmptyView: View {
+    struct Colors {
+        let titleTextColor: Color
+        let subTextColor: Color
+        let toggleTextColor: Color
+    }
+
+    let colors: Colors
+    @ObservedObject var toggleModel: ToggleModel
+
     var body: some View {
         ZStack {
-            Color(UIColor.clear)
+            UIColor.clear.color
                 .edgesIgnoringSafeArea(.all)
-            VStack {
-                Spacer()
-                Spacer()
-                    .frame(height: 25)
-                Image(ImageIdentifiers.creditCardPlaceholder)
-                    .resizable()
-                    .frame(width: 200, height: 200)
-                    .aspectRatio(contentMode: .fit)
-                    .fixedSize()
-                    .padding([.top], 10)
-                Text(String.CreditCard.Settings.EmptyListTitle)
-                    .font(.system(size: 22))
-                    .padding(.leading, 10)
-                    .padding(.trailing, 10)
-                Text(String.CreditCard.Settings.EmptyListDescription)
-                    .font(.system(size: 16))
-                    .multilineTextAlignment(.center)
-                    .lineLimit(2)
-                    .padding(.leading, 10)
-                    .padding(.trailing, 10)
-                    .padding([.top], -5)
-                Spacer()
+            GeometryReader { proxy in
+                ScrollView {
+                    VStack {
+                        CreditCardAutofillToggle(
+                            textColor: colors.toggleTextColor,
+                            model: toggleModel)
+                        Spacer()
+                        Image(ImageIdentifiers.creditCardPlaceholder)
+                            .resizable()
+                            .frame(width: 200, height: 200)
+                            .aspectRatio(contentMode: .fit)
+                            .fixedSize()
+                            .padding([.top], 10)
+                            .accessibility(hidden: true)
+                        Text(String.CreditCard.Settings.EmptyListTitle)
+                            .preferredBodyFont(size: 22)
+                            .foregroundColor(colors.titleTextColor)
+                            .multilineTextAlignment(.center)
+                            .padding(.leading, 10)
+                            .padding(.trailing, 10)
+                        Text(String.CreditCard.Settings.EmptyListDescription)
+                            .preferredBodyFont(size: 16)
+                            .foregroundColor(colors.subTextColor)
+                            .multilineTextAlignment(.center)
+                            .padding(.leading, 10)
+                            .padding(.trailing, 10)
+                            .padding([.top], -5)
+                        Spacer()
+                    }
+                    .frame(minHeight: proxy.size.height)
+                }
+                .frame(maxWidth: .infinity)
             }
         }
     }
@@ -39,6 +58,10 @@ struct CreditCardSettingsEmptyView: View {
 
 struct CreditCardSettingsEmptyView_Previews: PreviewProvider {
     static var previews: some View {
-        CreditCardSettingsEmptyView()
+        let toggleModel = ToggleModel(isEnabled: true)
+        let colors = CreditCardSettingsEmptyView.Colors(titleTextColor: .gray,
+                                                        subTextColor: .gray,
+                                                        toggleTextColor: .gray)
+        CreditCardSettingsEmptyView(colors: colors, toggleModel: toggleModel)
     }
 }
