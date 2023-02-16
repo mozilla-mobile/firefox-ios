c47e2613b69565f5d8aeb0d76d81e1abf355fcd8
Clare So <cso@mozilla.com>
2023-02-16 04:09:14 -0500

    Bugfix MTE-638 [v111] SaveLoginTest for M1 (#13258)
    * M1's iOS simulator displays the login list the same in both devices
    
    * Empty commit as a workaround for mergify

--- Tests/XCUITests/SaveLoginsTests.swift
+++ Tests/XCUITests/SaveLoginsTests.swift
@@ -90,11 +90,15 @@ class SaveLoginTest: BaseTestCase {
         waitForExistence(app.tables["Login List"])
         XCTAssertTrue(app.staticTexts[domain].exists)
         // XCTAssertTrue(app.staticTexts[domainSecondLogin].exists)
-        // Change to 3 entries for iPhone since the hostname is not saved and only one entry appears
-        if iPad() {
+        if processIsTranslatedStr() == m1Rosetta {
             XCTAssertEqual(app.tables["Login List"].cells.count, defaultNumRowsLoginsList + 2)
         } else {
-            XCTAssertEqual(app.tables["Login List"].cells.count, defaultNumRowsLoginsList + 1)
+            // Change to 3 entries for iPhone since the hostname is not saved and only one entry appears
+            if iPad() {
+                XCTAssertEqual(app.tables["Login List"].cells.count, defaultNumRowsLoginsList + 2)
+            } else {
+                XCTAssertEqual(app.tables["Login List"].cells.count, defaultNumRowsLoginsList + 1)
+            }
         }
     }
 
