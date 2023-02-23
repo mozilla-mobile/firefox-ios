0d53ca26126e034fd35045c982c5e77a23a2936e
Nishant Patel <nishantpatel2718@gmail.com>
2023-02-23 15:28:42 -0500

    Add FXIOS-5544 [v115] CC deletion support in edit view (#13332)

--- Client/Frontend/Autofill/CreditCard/CreditCardSettingsView/CreditCardEditViewModel.swift
+++ Client/Frontend/Autofill/CreditCard/CreditCardSettingsView/CreditCardEditViewModel.swift
@@ -5,10 +5,14 @@
 import Foundation
 import SwiftUI
 import Common
+import Storage
 
 class CreditCardEditViewModel: ObservableObject {
     typealias CreditCardText = String.CreditCard.Alert
+
     let profile: Profile
+    let autofill: RustAutofill
+    let creditCard: CreditCard?
 
     @Published var firstName: String = ""
     @Published var lastName: String = ""
@@ -42,7 +46,11 @@ class CreditCardEditViewModel: ObservableObject {
             return RemoveCardButton.AlertDetails(
                 alertTitle: Text(CreditCardText.RemoveCardTitle),
                 alertBody: Text(CreditCardText.RemoveCardSublabel),
-                primaryButtonStyleAndText: .destructive(Text(CreditCardText.RemovedCardLabel)),
+                primaryButtonStyleAndText: .destructive(Text(CreditCardText.RemovedCardLabel)) { [self] in
+                    guard let creditCard = creditCard else { return }
+
+                    removeSelectedCreditCard(creditCard: creditCard)
+                },
                 secondaryButtonStyleAndText: .cancel(),
                 primaryButtonAction: {},
                 secondaryButtonAction: {})
@@ -51,26 +59,46 @@ class CreditCardEditViewModel: ObservableObject {
         return RemoveCardButton.AlertDetails(
             alertTitle: Text(CreditCardText.RemoveCardTitle),
             alertBody: nil,
-            primaryButtonStyleAndText: .destructive(Text(CreditCardText.RemovedCardLabel)),
+            primaryButtonStyleAndText: .destructive(Text(CreditCardText.RemovedCardLabel)) { [self] in
+                guard let creditCard = creditCard else { return }
+
+                removeSelectedCreditCard(creditCard: creditCard)
+            },
             secondaryButtonStyleAndText: .cancel(),
             primaryButtonAction: {},
             secondaryButtonAction: {}
         )
     }
 
-    init(profile: Profile) {
+    init(profile: Profile,
+         creditCard: CreditCard? = nil
+    ) {
         self.profile = profile
+        self.autofill = profile.autofill
+        self.creditCard = creditCard
     }
 
     init(profile: Profile = AppContainer.shared.resolve(),
          firstName: String,
          lastName: String,
          errorState: String,
-         enteredValue: String) {
+         enteredValue: String,
+         creditCard: CreditCard? = nil
+    ) {
         self.profile = profile
         self.firstName = firstName
         self.lastName = lastName
         self.errorState = errorState
         self.enteredValue = enteredValue
+        self.autofill = profile.autofill
+        self.creditCard = creditCard
+    }
+
+    // MARK: - Private helpers
+
+    private func removeSelectedCreditCard(creditCard: CreditCard) {
+        autofill.deleteCreditCard(id: creditCard.guid) { _, error in
+            // no-op
+        }
     }
 }
