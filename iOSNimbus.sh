9f35f8d5074f2ce0369f0010040c7dba83fe15fc
roux g. buciu <roux@mozilla.com>
2023-02-23 15:50:10 -0500

    Document the nimbus utility

--- iOSNimbus.sh
+++ iOSNimbus.sh
@@ -4,15 +4,23 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-# Use the --add option to add a new feature
-# Use the --update option to update all features in the FML
+# General:
 
+# This utility simplifies managing the `include` block of the
+# nimbus.fml.yaml file. There are two main options:
+
+# --add fileName : option to add a new feature. Argument should be camelCase
+# --update : updates the `include` block of the FML file.
+
+# Adds the files in the 'nimbus-features' directory in the
+# `include` block of the FML
 addFeatureFilesToNimbus() {
     for filename in nimbus-features/*.yaml; do
         echo "  - $filename" >> $1
     done
 }
 
+# Removes the files listed in the FML's `include` block
 cleanupNimbusFile() {
     grep -v "nimbus-features" $1 > temp
     rm $1
@@ -26,10 +34,12 @@ updateNimbusFML() {
     addFeatureFilesToNimbus $NIMBUSFML
 }
 
+# Takes a given feature name in camelCase and coverts it to kebab-case
 configureFeatureName() {
     echo $1 | sed -r 's/([a-z0-9])([A-Z])/\1-\2/g' | tr '[:upper:]' '[:lower:]'
 }
 
+# Prefills a newly created feature file with
 addNewFeatureContent() {
     KEBAB_FEATURE_NAME=$(configureFeatureName $2)
     echo """# The configuration for the $2 feature
@@ -71,8 +81,4 @@ fi
 
 if [ "$1" == "--update" ]; then
     updateNimbusFML
-fi
-
-if [ "$1" == "--test" ]; then
-    configureFeatureName $2
 fi
