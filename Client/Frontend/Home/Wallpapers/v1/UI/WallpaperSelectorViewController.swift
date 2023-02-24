6be9b08801e167e55c2f87a48b2a6002e7429e15
Catalin <catalin.neculaide.git@gmail.com>
2023-02-24 19:34:38 +0200

    Remove FXIOS-5788 [v112] Remove Explore more from wallpaper onboarding (#13333)
    * remove settings button from Wallpaper onboarding
    
    * delete settings button from WallpaperSelectorView
    
    * delete strings and accessibility identifiers for wallpaper selector view
    
    ---------
    
    Co-authored-by: Catalin Andrei Neculaide <catalin.neculaide@softvision.com>

--- Client/Frontend/Home/Wallpapers/v1/UI/WallpaperSelectorViewController.swift
+++ Client/Frontend/Home/Wallpapers/v1/UI/WallpaperSelectorViewController.swift
@@ -58,14 +58,6 @@ class WallpaperSelectorViewController: WallpaperBaseViewController, Themeable {
         return collectionView
     }()
 
-    private lazy var settingsButton: ResizableButton = .build { button in
-        button.titleLabel?.font = DynamicFontHelper.defaultHelper.preferredFont(withTextStyle: .body,
-                                                                                size: 16)
-        button.titleLabel?.textAlignment = .center
-        button.setTitle(.Onboarding.WallpaperSelectorAction, for: .normal)
-        button.accessibilityIdentifier = AccessibilityIdentifiers.Onboarding.Wallpaper.settingsButton
-    }
-
     // MARK: - Initializers
     init(viewModel: WallpaperSelectorViewModel,
          notificationCenter: NotificationProtocol = NotificationCenter.default,
@@ -85,8 +77,6 @@ class WallpaperSelectorViewController: WallpaperBaseViewController, Themeable {
         super.viewDidLoad()
         listenForThemeChange()
         setupView()
-
-        settingsButton.addTarget(self, action: #selector(self.settingsButtonTapped), for: .touchUpInside)
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -113,7 +103,6 @@ class WallpaperSelectorViewController: WallpaperBaseViewController, Themeable {
         contentView.backgroundColor = theme.colors.layer1
         headerLabel.textColor = theme.colors.textPrimary
         instructionLabel.textColor = theme.colors.textPrimary
-        settingsButton.setTitleColor(theme.colors.actionPrimary, for: .normal)
     }
 }
 
@@ -144,7 +133,7 @@ private extension WallpaperSelectorViewController {
     func setupView() {
         configureCollectionView()
 
-        contentView.addSubviews(headerLabel, instructionLabel, collectionView, settingsButton)
+        contentView.addSubviews(headerLabel, instructionLabel, collectionView)
         view.addSubview(contentView)
 
         collectionViewHeightConstraint = collectionView.heightAnchor.constraint(equalToConstant: 300)
@@ -166,13 +155,9 @@ private extension WallpaperSelectorViewController {
             instructionLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -34),
 
             collectionView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
-            collectionView.bottomAnchor.constraint(equalTo: settingsButton.topAnchor, constant: -14),
+            collectionView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -43),
             collectionView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
-            collectionViewHeightConstraint,
-
-            settingsButton.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 34),
-            settingsButton.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -43),
-            settingsButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -34),
+            collectionViewHeightConstraint
         ])
     }
 
@@ -234,11 +219,6 @@ private extension WallpaperSelectorViewController {
                 }
             }
         }
-    }
-
-    /// Settings button tapped
-    @objc func settingsButtonTapped(_ sender: UIButton) {
-        viewModel.openSettingsAction()
     }
 }
 
