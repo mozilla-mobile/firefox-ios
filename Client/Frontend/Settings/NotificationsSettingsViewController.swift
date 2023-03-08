ea5630a12f8f52643ee73d7b10d79cf041a347da
Razvan Litianu <litianu.razvan@gmail.com>
2023-03-08 17:11:08 +0200

    Fix swiftlint

--- Client/Frontend/Settings/NotificationsSettingsViewController.swift
+++ Client/Frontend/Settings/NotificationsSettingsViewController.swift
@@ -114,7 +114,7 @@ class NotificationsSettingsViewController: SettingsTableViewController, FeatureF
         case .denied:
             sendNotifications = false
             await MainActor.run {
-                let accessDenied = UIAlertController(title: .Settings.Notifications.TurnOnNotificationsTitle , message: .Settings.Notifications.TurnOnNotificationsMessage, preferredStyle: .alert)
+                let accessDenied = UIAlertController(title: .Settings.Notifications.TurnOnNotificationsTitle, message: .Settings.Notifications.TurnOnNotificationsMessage, preferredStyle: .alert)
                 let dismissAction = UIAlertAction(title: .CancelString, style: .default, handler: nil)
                 accessDenied.addAction(dismissAction)
                 let settingsAction = UIAlertAction(title: .OpenSettingsString, style: .default ) { _ in
