e46e014e5eb8bb6235e35ecb15249b3a042ac2dc
OrlaM <omitchell@mozilla.com>
2023-03-08 14:24:07 -0500

    Remove FXIOS-5664 [v112] Remove today extension (#13498)
    * Remove today extension
    
    * Remove unused strings

--- Client/Extensions/UIButton+Extensions.swift
+++ Client/Extensions/UIButton+Extensions.swift
@@ -5,20 +5,6 @@
 import UIKit
 
 extension UIButton {
-    func setBackgroundColor(_ color: UIColor, forState state: UIControl.State) {
-        let colorView = UIView(frame: CGRect(width: 1, height: 1))
-        colorView.backgroundColor = color
-
-        UIGraphicsBeginImageContext(colorView.bounds.size)
-        if let context = UIGraphicsGetCurrentContext() {
-            colorView.layer.render(in: context)
-        }
-        let colorImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-
-        self.setBackgroundImage(colorImage, for: state)
-    }
-
     func setInsets(forContentPadding contentPadding: UIEdgeInsets,
                    imageTitlePadding: CGFloat) {
         let isLTR = effectiveUserInterfaceLayoutDirection == .leftToRight
