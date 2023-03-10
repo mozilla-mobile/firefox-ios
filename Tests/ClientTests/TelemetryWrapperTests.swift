c8d71443ead75ffe719432064d481c5e56901194
lmarceau <lmarceau@mozilla.com>
2023-03-10 16:33:28 -0500

    Bump FXIOS-5855 [v112] Glean to 52.4.0 (#13525)
    * Bump Glean to 52.4.0
    
    * Adjust test in the meantime we get a Glean update that fixes the problem
    
    - Needed to account for store not being empty in some test cases.
    - Removing test_tabsNormalQuantityWithoutExtras_GleanIsNotCalled and test_tabsPrivateQuantityWithoutExtras_GleanIsNotCalled since they are covered in TabsQuantityTelemetryTests

--- Tests/ClientTests/TelemetryWrapperTests.swift
+++ Tests/ClientTests/TelemetryWrapperTests.swift
@@ -10,12 +10,12 @@ import XCTest
 class TelemetryWrapperTests: XCTestCase {
     override func setUp() {
         super.setUp()
-        Glean.shared.resetGlean(clearStores: true)
+        Glean.shared.resetGlean(clearStores: false)
     }
 
     override func tearDown() {
         super.tearDown()
-        Glean.shared.resetGlean(clearStores: true)
+        Glean.shared.resetGlean(clearStores: false)
     }
 
     // MARK: - Top Site
@@ -152,16 +152,6 @@ class TelemetryWrapperTests: XCTestCase {
         testQuantityMetricSuccess(metric: GleanMetrics.Tabs.privateTabsQuantity,
                                   expectedValue: expectTabCount,
                                   failureMessage: "Should have \(expectTabCount) tabs for private tabs")
-    }
-
-    func test_tabsNormalQuantityWithoutExtras_GleanIsNotCalled() {
-        TelemetryWrapper.recordEvent(category: .information, method: .background, object: .tabNormalQuantity, value: nil, extras: nil)
-        XCTAssertNil(GleanMetrics.Tabs.normalTabsQuantity.testGetValue())
-    }
-
-    func test_tabsPrivateQuantityWithoutExtras_GleanIsNotCalled() {
-        TelemetryWrapper.recordEvent(category: .information, method: .background, object: .tabPrivateQuantity, value: nil, extras: nil)
-        XCTAssertNil(GleanMetrics.Tabs.privateTabsQuantity.testGetValue())
     }
 
     // MARK: - Onboarding
