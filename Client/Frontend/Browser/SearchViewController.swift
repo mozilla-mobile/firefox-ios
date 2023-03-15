739d56e72a07c4aba7908e819d7d36da7825fef2
Catalin Andrei Neculaide <catalin.neculaide@softvision.com>
2023-03-15 17:05:22 +0200

    check end of section

--- Client/Frontend/Browser/SearchViewController.swift
+++ Client/Frontend/Browser/SearchViewController.swift
@@ -843,8 +843,12 @@ extension SearchViewController {
                     return
                 } else {
                     // We can go to the next section.
-                    nextSection = current.section + 1
-                    nextItem = 0
+                    if current.section + 1 == initialSection {
+                        nextSection = current.section + 1
+                        nextItem = 0
+                    } else {
+                        return
+                    }
                 }
             } else {
                 nextSection = current.section
