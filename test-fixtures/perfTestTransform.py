ede32ea6bb8243f55b3550bb6b95a5973e1f7bba
Jackie Johnson <107960801+jjSDET@users.noreply.github.com>
2023-03-21 11:31:37 -0500

    add testdata to perfherder data transform

---
+++ test-fixtures/perfTestTransform.py
@@ -0,0 +1,23 @@
+import json
+
+PERFHERDER_DATA = { "framework": 'mozperftest',
+                   "application": { "name": 'firefox-ios' },
+                     "suites": []
+                    }
+# get json data from ./test.json
+with open('test.json') as f:
+    data = json.load(f)
+    for i in data:
+        suite = {}
+        suite['name'] = i['testName']
+        subtests = []
+        for key in i:
+            if key != 'testName':
+                subtest = {}
+                subtest['name'] = key
+                subtest['replicates'] = [i[key]]
+                subtests.append(subtest)
+                suite['subtests'] = subtests
+                PERFHERDER_DATA['suites'].append(suite)
+
+print(json.dumps(PERFHERDER_DATA))
