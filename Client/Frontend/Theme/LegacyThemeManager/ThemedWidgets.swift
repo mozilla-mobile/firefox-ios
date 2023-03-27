6320fa31013e6b2d1868a39f3457743d16cb5667
Aalaa <aalaa@ryets.com>
2023-03-27 18:56:00 +0200

    swift lint warnings fix

--- Client/Frontend/Theme/LegacyThemeManager/ThemedWidgets.swift
+++ Client/Frontend/Theme/LegacyThemeManager/ThemedWidgets.swift
@@ -50,33 +50,31 @@ class ThemedTableViewCell: UITableViewCell, ThemeApplicable {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-
     // When UITableViewAutomaticDimension is enabled the system calls
     // -systemLayoutSizeFittingSize:withHorizontalFittingPriority:verticalFittingPriority: to calculate the cell height.
     // Unfortunately, it ignores the height of the detailTextLabel in its computation (bug !?).
     // As a result, for UITableViewCellStyleSubtitle the cell height is always going to be too short.
     // So we override to include detailTextLabel height.
-    override func systemLayoutSizeFitting(_ targetSize: CGSize, withHorizontalFittingPriority horizontalFittingPriority: UILayoutPriority, verticalFittingPriority: UILayoutPriority) -> CGSize {
-      self.layoutIfNeeded()
-      var size = super.systemLayoutSizeFitting(targetSize, withHorizontalFittingPriority: horizontalFittingPriority, verticalFittingPriority: verticalFittingPriority)
-      if let textLabel = self.textLabel, let detailTextLabel = self.detailTextLabel {
-        let detailHeight = detailTextLabel.frame.size.height
-
-        if detailTextLabel.frame.origin.x > textLabel.frame.origin.x { // style = Value1 or Value2
-          let textHeight = textLabel.frame.size.height
-          if (detailHeight > textHeight) {
-            size.height += detailHeight - textHeight
-          }
-        } else { // style = Subtitle, so always add subtitle height
-          size.height += detailHeight
+    override func systemLayoutSizeFitting(_ targetSize: CGSize,
+                                          withHorizontalFittingPriority horizontalFittingPriority: UILayoutPriority,
+                                          verticalFittingPriority: UILayoutPriority) -> CGSize {
+        self.layoutIfNeeded()
+        var size = super.systemLayoutSizeFitting(targetSize,
+                                                 withHorizontalFittingPriority: horizontalFittingPriority,
+                                                 verticalFittingPriority: verticalFittingPriority)
+        if let textLabel = self.textLabel, let detailTextLabel = self.detailTextLabel {
+            let detailHeight = detailTextLabel.frame.size.height
+            if detailTextLabel.frame.origin.x > textLabel.frame.origin.x { // style = Value1 or Value2
+                let textHeight = textLabel.frame.size.height
+                if detailHeight > textHeight {
+                    size.height += detailHeight - textHeight
+                }
+            } else { // style = Subtitle, so always add subtitle height
+                size.height += detailHeight
+            }
         }
-      }
-      return size
-        
+        return size
     }
-    
-
     func applyTheme(theme: Theme) {
         self.viewModel?.setColors(theme: theme)
         // Take view model color if it exists, otherwise fallback to default colors
