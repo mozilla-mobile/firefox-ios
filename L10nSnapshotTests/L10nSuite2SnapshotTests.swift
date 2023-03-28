bc19a7d424d0bb2133b8de5cdd05c3f7e4b09c39
Clare So <cso@mozilla.com>
2023-03-28 00:45:52 -0400

    Stablize the tests through config and sleep()

--- L10nSnapshotTests/L10nSuite2SnapshotTests.swift
+++ L10nSnapshotTests/L10nSuite2SnapshotTests.swift
@@ -122,6 +122,7 @@ class L10nSuite2SnapshotTests: L10nBaseSnapshotTests {
         let passcodeInput = springboard.secureTextFields.firstMatch
         waitForExistence(passcodeInput, timeout: 20)
         passcodeInput.tap()
+        sleep(1)
         passcodeInput.typeText("foo\n")
 
         waitForExistence(app.tables["Login List"], timeout: 10)
@@ -129,10 +130,13 @@ class L10nSuite2SnapshotTests: L10nBaseSnapshotTests {
         waitForExistence(app.tables["Add Credential"], timeout: 10)
         snapshot("CreateLogin")
         app.tables["Add Credential"].cells.element(boundBy: 0).tap()
+        sleep(1)
         app.keyboards.keys.element(boundBy: 3).tap()
         app.tables["Add Credential"].cells.element(boundBy: 1).tap()
+        sleep(1)
         app.keyboards.keys.element(boundBy: 3).tap()
         app.tables["Add Credential"].cells.element(boundBy: 2).tap()
+        sleep(1)
         app.keyboards.keys.element(boundBy: 3).tap()
         app.navigationBars["Client.AddCredentialView"].buttons.element(boundBy: 1).tap()
 
