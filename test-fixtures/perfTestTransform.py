8634078e7d762f2ebce99949705671373aff038c
Jackie Johnson <107960801+jjSDET@users.noreply.github.com>
2023-03-29 13:44:34 -0500

    fix transform output

--- test-fixtures/perfTestTransform.py
+++ test-fixtures/perfTestTransform.py
@@ -1,23 +1,22 @@
 import json
 
-PERFHERDER_DATA = { "framework": 'mozperftest',
-                   "application": { "name": 'firefox-ios' },
-                     "suites": []
-                    }
-# get json data from ./test.json
-with open('test.json') as f:
-    data = json.load(f)
-    for i in data:
+PERFHERDER_DATA = {"framework": 'mozperftest',
+                   "application": {"name": 'firefox-ios'},
+                   "suites": []
+                   }
+
+with open('test.json') as json_file:
+    data = json.load(json_file)
+    for p in data:
         suite = {}
-        suite['name'] = i['testName']
-        subtests = []
-        for key in i:
-            if key != 'testName':
+        suite["name"] = p["testName"]
+        suite["subtests"] = []
+        for key, value in p.items():
+            if key != "testName":
                 subtest = {}
-                subtest['name'] = key
-                subtest['replicates'] = [i[key]]
-                subtests.append(subtest)
-                suite['subtests'] = subtests
-                PERFHERDER_DATA['suites'].append(suite)
+                subtest["name"] = key
+                subtest["replicates"] = [value]
+                suite["subtests"].append(subtest)
+        PERFHERDER_DATA["suites"].append(suite)
 
 print("PERFHERDER_DATA:", json.dumps(PERFHERDER_DATA))
