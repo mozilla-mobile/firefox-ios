bd835c44d609b664e90bfa82429e0a814cb01ce1
lmarceau <lmarceau@mozilla.com>
2023-03-29 13:22:15 -0400

    Add FXIOS-5980 [v113] Add base coordinator and router protocol and classes (#13774)
    * Add base coordinator and router protocol/classes w/ tests
    
    * Suggested changes: rename methods, MockRouter in it's own file
    
    * Apply suggestions from code review
    
    Co-authored-by: roux g. buciu <11182210+adudenamedruby@users.noreply.github.com>
    
    * Adjust variables to be in the right place
    
    * Remove guard since this is a programmer error
    
    * Fix swiftlint
    
    ---------
    
    Co-authored-by: roux g. buciu <11182210+adudenamedruby@users.noreply.github.com>

---
+++ Client/Coordinators/Coordinator.swift
@@ -0,0 +1,36 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import Foundation
+
+protocol Coordinator {
+    var id: UUID { get }
+    var childCoordinators: [Coordinator] { get }
+    var router: Router { get }
+
+    func add(child coordinator: Coordinator)
+    func remove(child coordinator: Coordinator?)
+}
+
+open class BaseCoordinator: Coordinator {
+    var id: UUID = UUID()
+    var childCoordinators: [Coordinator] = []
+    var router: Router
+
+    init(router: Router) {
+        self.router = router
+    }
+
+    func add(child coordinator: Coordinator) {
+        childCoordinators.append(coordinator)
+    }
+
+    func remove(child coordinator: Coordinator?) {
+        guard let coordinator = coordinator,
+              let index = childCoordinators.firstIndex(where: { $0.id == coordinator.id })
+        else { return }
+
+        childCoordinators.remove(at: index)
+    }
+}
