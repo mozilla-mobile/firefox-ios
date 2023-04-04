04f7ca9c83d2506f7ff6da344f9565f90f463578
Stefan Vladut <stefan.vladut@softvision.com>
2023-04-04 17:49:11 +0300

    Refactor FXIOS-5877 [v113] added logging, unit tests and save/load/clear data

--- BrowserKit/Sources/TabDataStore/TabDataStore.swift
+++ BrowserKit/Sources/TabDataStore/TabDataStore.swift
@@ -3,61 +3,208 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
 import Foundation
+import UIKit
+import Common
 
+//MARK: Protocol
 protocol TabDataStore {
-    func fetchTabData() async -> WindowData
-    func saveTabData(window: WindowData) async
-    func clearAllTabData() async
+    func fetchWindowData() async -> WindowData
+    func saveWindowData(window: WindowData) async
+    func clearAllWindowsData() async
 }
 
 actor DefaultTabDataStore: TabDataStore {
-    private var lastSaveTime: Date = .distantPast
-    private let minimumSaveInterval: TimeInterval = 5
+    //MARK: Variables
+    let browserKitInfo = BrowserKitInformation.shared
+    static let storePath = "codableWindowsState.archive"
+    private let saveQueue = DispatchQueue(label: "com.example.tabdatastore.save")
+    private var saveWorkItem: DispatchWorkItem?
+    private let throttleInterval: TimeInterval = 5
+    private var logger: Logger = DefaultLogger.shared
 
-    private let filePath: URL = {
-        let fileManager = FileManager.default
-        let documentsDirectory = try? fileManager.url(for: .documentDirectory,
-                                                      in: .userDomainMask,
-                                                      appropriateFor: nil,
-                                                      create: false)
-        return documentsDirectory?.appendingPathComponent("WindowData.json") ?? URL(fileURLWithPath: "")
-    }()
-
-    func fetchTabData() async -> WindowData {
-        do {
-            let data = try Data(contentsOf: filePath)
-            let windowData = try JSONDecoder().decode(WindowData.self, from: data)
-            return windowData
-        } catch {
-            return WindowData(id: UUID(), isPrimary: true, activeTabId: UUID(), tabData: [])
-        }
+    //MARK: URL Utils
+    private var windowDataDirectoryURL: URL? {
+        FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: browserKitInfo.sharedContainerIdentifier)?
+            .appendingPathComponent("profile.profile")
     }
 
-    func saveTabData(window: WindowData) async {
-        let now = Date()
-        guard now.timeIntervalSince(lastSaveTime) >= minimumSaveInterval else { return }
-        lastSaveTime = now
+    private func windowURLPath(for windowId: UUID) -> URL? {
+        if let profileURL = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: browserKitInfo.sharedContainerIdentifier)?
+            .appendingPathComponent("profile.profile") {
 
-        do {
-            let data = try JSONEncoder().encode(window)
-            let url = try FileManager.default.url(for: .documentDirectory,
-                                                  in: .userDomainMask,
-                                                  appropriateFor: nil,
-                                                  create: false)
-                .appendingPathComponent("WindowData.json")
-            try data.write(to: url)
-        } catch {
-            print("Error saving tab data: \(error)")
+            do {
+                try FileManager.default.createDirectory(at: profileURL, withIntermediateDirectories: true, attributes: nil)
+            } catch {
+                logger.log("Error creating profile.profile directory: \(error)",
+                           level: .debug,
+                           category: .tabs)
+                return nil
+            }
+
+            let filePath = DefaultTabDataStore.storePath + "_\(windowId.uuidString)"
+            return profileURL.appendingPathComponent(filePath)
         }
+        return nil
     }
 
-    func clearAllTabData() async {
+    //MARK: Fetching Window Data
+    func fetchWindowData() async -> WindowData {
+        return WindowData(id: UUID(), isPrimary: true, activeTabId: UUID(), tabData: [])
+    }
+
+    func fetchWindowData(withId id: UUID) async -> WindowData? {
+        guard let profileURL = windowDataDirectoryURL else {
+            return nil
+        }
+
         do {
-            if let url = try? FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false).appendingPathComponent("WindowData.json") {
-                try FileManager.default.removeItem(at: url)
+            let fileURLs = try FileManager.default.contentsOfDirectory(at: profileURL, includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
+
+            let windowDataFiles = fileURLs.filter { $0.path.contains(DefaultTabDataStore.storePath) }
+
+            for fileURL in windowDataFiles {
+                do {
+                    let data = try Data(contentsOf: fileURL)
+                    let windowData = try JSONDecoder().decode(WindowData.self, from: data)
+                    if windowData.id == id {
+                        return windowData
+                    }
+                } catch {
+                    logger.log("Error decoding window data: \(error)",
+                               level: .debug,
+                               category: .tabs)
+                }
             }
         } catch {
-            print("Error while clearing tab data: \(error)")
+            logger.log("Error fetching window data with ID: \(error)",
+                       level: .debug,
+                       category: .tabs)
+        }
+
+        return nil
+    }
+
+    func fetchAllWindowsData() async -> [WindowData] {
+        guard let profileURL = windowDataDirectoryURL else {
+            return []
+        }
+
+        do {
+            let fileURLs = try FileManager.default.contentsOfDirectory(at: profileURL, includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
+
+            let windowDataFiles = fileURLs.filter { $0.path.contains(DefaultTabDataStore.storePath) }
+
+            var windowsData: [WindowData] = []
+            for fileURL in windowDataFiles {
+                do {
+                    let data = try Data(contentsOf: fileURL)
+                    let windowData = try JSONDecoder().decode(WindowData.self, from: data)
+                    windowsData.append(windowData)
+                } catch {
+                    logger.log("Error decoding window data: \(error)",
+                               level: .debug,
+                               category: .tabs)
+                }
+            }
+            return windowsData
+        } catch {
+            logger.log("Error fetching all window data: \(error)",
+                       level: .debug,
+                       category: .tabs)
+            return []
+        }
+    }
+
+    //MARK: Saving Data
+    func saveWindowData(window: WindowData) async {
+        saveWorkItem?.cancel()
+
+        let workItem = DispatchWorkItem { [weak self] in
+            guard let self = self else { return }
+            Task.init(priority: .userInitiated) {
+                if let windowSavingPath = await self.windowURLPath(for: window.id) {
+                    do {
+                        try await self.writeWindowData(windowData: window, to: windowSavingPath)
+                    } catch {
+                        await self.logger.log("Failed to save window data: \(error)",
+                                   level: .debug,
+                                   category: .tabs)
+                    }
+                }
+            }
+        }
+
+        saveWorkItem = workItem
+        saveQueue.asyncAfter(deadline: .now() + throttleInterval, execute: workItem)
+    }
+
+    private func writeWindowData(windowData: WindowData, to url: URL) async throws {
+        try await withUnsafeThrowingContinuation { continuation in
+            do {
+                let data = try JSONEncoder().encode(windowData)
+                try data.write(to: url, options: .atomicWrite)
+                continuation.resume(returning: ())
+            } catch {
+                continuation.resume(throwing: error)
+            }
+        }
+    }
+
+    //MARK: Deleting Window Data
+    func clearWindowData(for id: UUID) async {
+        guard let profileURL = windowDataDirectoryURL else {
+            return
+        }
+
+        do {
+            let fileURLs = try FileManager.default.contentsOfDirectory(at: profileURL, includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
+
+            let windowDataFiles = fileURLs.filter { $0.path.contains(DefaultTabDataStore.storePath) }
+
+            for fileURL in windowDataFiles {
+                do {
+                    let data = try Data(contentsOf: fileURL)
+                    let windowData = try JSONDecoder().decode(WindowData.self, from: data)
+                    if windowData.id == id {
+                        try FileManager.default.removeItem(at: fileURL)
+                        return
+                    }
+                } catch {
+                    logger.log("Error while clearing window data: \(error)",
+                               level: .debug,
+                               category: .tabs)
+                }
+            }
+        } catch {
+            logger.log("Error clearing window data for ID: \(error)",
+                       level: .debug,
+                       category: .tabs)
+        }
+    }
+
+    func clearAllWindowsData() async {
+        guard let profileURL = windowDataDirectoryURL else {
+            return
+        }
+
+        do {
+            let fileURLs = try FileManager.default.contentsOfDirectory(at: profileURL, includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
+
+            let windowDataFiles = fileURLs.filter { $0.path.contains(DefaultTabDataStore.storePath) }
+
+            for fileURL in windowDataFiles {
+                do {
+                    try FileManager.default.removeItem(at: fileURL)
+                } catch {
+                    logger.log("Error while clearing all window data: \(error)",
+                               level: .debug,
+                               category: .tabs)
+                }
+            }
+        } catch {
+            logger.log("Error fetching all window data for clearing: \(error)",
+                       level: .debug,
+                       category: .tabs)
         }
     }
 }
--- BrowserKit/Tests/TabDataStoreTests/TempTests.swift
+++ BrowserKit/Tests/TabDataStoreTests/TempTests.swift
@@ -4,8 +4,168 @@
 
 import XCTest
 @testable import TabDataStore
+import Common
 
 final class TempTests: XCTestCase {
+
+    private var tabDataStore: DefaultTabDataStore!
+
+    override func setUp() {
+        super.setUp()
+        tabDataStore = DefaultTabDataStore()
+        BrowserKitInformation.shared.configure(buildChannel: .other,
+                                               nightlyAppVersion: "",
+                                               sharedContainerIdentifier: "group.org.mozilla.ios.Fennec")
+    }
+
+    override func tearDown() {
+        super.tearDown()
+        tabDataStore = nil
+    }
+
+    func testSaveTabData() async throws {
+        let tab = TabData(id: UUID(),
+                          title: "Test",
+                          siteUrl: "https://test.com",
+                          faviconURL: "https://test.com/favicon.ico",
+                          isPrivate: false,
+                          lastUsedTime: Date(),
+                          createdAtTime: Date())
+
+        let windowData = WindowData(id: UUID(),
+                                    isPrimary: true,
+                                    activeTabId: tab.id,
+                                    tabData: [tab])
+
+        let expectation = XCTestExpectation(description: "Save window data")
+
+        await tabDataStore.saveWindowData(window: windowData)
+        DispatchQueue.global().asyncAfter(deadline: .now() + 6) { // Slightly longer than throttleInterval
+            expectation.fulfill()
+        }
+
+        wait(for: [expectation], timeout: 7)
+
+        let fetchedWindowData = await tabDataStore.fetchWindowData()
+
+        XCTAssertEqual(fetchedWindowData.id, windowData.id)
+        XCTAssertEqual(fetchedWindowData.isPrimary, windowData.isPrimary)
+        XCTAssertEqual(fetchedWindowData.activeTabId, windowData.activeTabId)
+        XCTAssertEqual(fetchedWindowData.tabData.count, windowData.tabData.count)
+    }
+
+    func testFetchAllWindowsData() async throws {
+        // Create some sample TabData and WindowData objects
+        let tab1 = TabData(id: UUID(),
+                           title: "Test1",
+                           siteUrl: "https://test1.com",
+                           faviconURL: "https://test1.com/favicon.ico",
+                           isPrivate: true,
+                           lastUsedTime: Date(),
+                           createdAtTime: Date())
+
+        let tab2 = TabData(id: UUID(),
+                           title: "Test2",
+                           siteUrl: "https://test2.com",
+                           faviconURL: "https://test2.com/favicon.ico",
+                           isPrivate: false,
+                           lastUsedTime: Date(),
+                           createdAtTime: Date())
+
+        let windowData1 = WindowData(id: UUID(),
+                                     isPrimary: true,
+                                     activeTabId: tab1.id,
+                                     tabData: [tab1])
+
+        let windowData2 = WindowData(id: UUID(),
+                                     isPrimary: false,
+                                     activeTabId: tab2.id,
+                                     tabData: [tab2])
+
+        // Save the WindowData objects
+        await tabDataStore.saveWindowData(window: windowData1)
+
+        let expectation = XCTestExpectation(description: "Wait for save operation")
+        DispatchQueue.global().asyncAfter(deadline: .now() + 6) {
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 7)
+
+        await tabDataStore.saveWindowData(window: windowData2)
+
+        let expectation2 = XCTestExpectation(description: "Wait for save operation")
+        DispatchQueue.global().asyncAfter(deadline: .now() + 6) {
+            expectation2.fulfill()
+        }
+        wait(for: [expectation2], timeout: 7)
+
+        // Fetch all WindowData objects
+        let fetchedWindowsData = await tabDataStore.fetchAllWindowsData()
+
+        // Verify the fetched data
+        XCTAssertEqual(fetchedWindowsData.count, 2)
+        XCTAssertTrue(fetchedWindowsData.contains(where: { $0.id == windowData1.id }))
+        XCTAssertTrue(fetchedWindowsData.contains(where: { $0.id == windowData2.id }))
+    }
+
+    func testFetchWindowDataWithId() async throws {
+        // Create a sample TabData and WindowData object
+        let tab = TabData(id: UUID(),
+                          title: "Test",
+                          siteUrl: "https://test.com",
+                          faviconURL: "https://test.com/favicon.ico",
+                          isPrivate: false,
+                          lastUsedTime: Date(),
+                          createdAtTime: Date())
+
+        let windowData = WindowData(id: UUID(),
+                                    isPrimary: true,
+                                    activeTabId: tab.id,
+                                    tabData: [tab])
+
+        // Save the WindowData object
+        await tabDataStore.saveWindowData(window: windowData)
+
+        let expectation = XCTestExpectation(description: "Wait for save operation")
+        DispatchQueue.global().asyncAfter(deadline: .now() + 6) {
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 7)
+        
+        // Fetch the WindowData object using its ID
+        let fetchedWindowData = await tabDataStore.fetchWindowData(withId: windowData.id)
+
+        // Verify the fetched data
+        XCTAssertNotNil(fetchedWindowData)
+        XCTAssertEqual(fetchedWindowData?.id, windowData.id)
+        XCTAssertEqual(fetchedWindowData?.isPrimary, windowData.isPrimary)
+        XCTAssertEqual(fetchedWindowData?.activeTabId, windowData.activeTabId)
+        XCTAssertEqual(fetchedWindowData?.tabData.count, windowData.tabData.count)
+    }
+
+    func testClearAllTabData() async throws {
+        let tab = TabData(id: UUID(),
+                          title: "Test",
+                          siteUrl: "https://test.com",
+                          faviconURL: "https://test.com/favicon.ico",
+                          isPrivate: false,
+                          lastUsedTime: Date(),
+                          createdAtTime: Date())
+
+        let windowData = WindowData(id: UUID(),
+                                    isPrimary: true,
+                                    activeTabId: tab.id,
+                                    tabData: [tab])
+
+        await tabDataStore.saveWindowData(window: windowData)
+        await tabDataStore.clearAllWindowsData()
+
+        let fetchedWindowData = await tabDataStore.fetchWindowData()
+
+        // Assuming the default fetchTabData() returns an empty WindowData object
+        XCTAssertTrue(fetchedWindowData.tabData.isEmpty)
+    }
+    
     func testTemp() {
         XCTAssertTrue(true)
     }
