4987a7313999a61ef6518581b1aad0a9a3d88e5c
Razvan Litianu <litianu.razvan@gmail.com>
2023-04-06 16:28:04 +0300

    Create URLScanner

--- Client/Coordinators/Router/Route.swift
+++ Client/Coordinators/Router/Route.swift
@@ -95,6 +95,111 @@ enum Route: Equatable {
         case tutorial = "tutorial"
         case systemSettings = "system-settings"
     }
+
+    init?(url: URL) {
+        guard let urlScanner = URLScanner(url: url) else { return nil }
+
+        if urlScanner.isOurScheme, let host = DeeplinkInput.Host(rawValue: urlScanner.host.lowercased()) {
+            let urlQuery = urlScanner.value(query: "url")?.asURL
+            // Unless the `open-url` URL specifies a `private` parameter,
+            // use the last browsing mode the user was in.
+            let isPrivate = Bool(urlScanner.value(query: "private") ?? "") ?? UserDefaults.standard.bool(forKey: PrefsKeys.LastSessionWasPrivate)
+
+            switch host {
+            case .deepLink:
+                let deepLinkURL = urlScanner.value(query: "url")?.lowercased()
+                let paths = deepLinkURL?.split(separator: "/") ?? []
+                guard let pathRaw = paths[safe: 0].flatMap(String.init),
+                        let path = DeeplinkInput.Path(rawValue: pathRaw),
+                        let subPath = paths[safe: 1].flatMap(String.init)
+                else { return nil }
+                if path == .settings, let subPath = SettingsSection(rawValue: subPath) {
+                    self = .settings(section: subPath)
+                } else if path == .homepanel, let subPath = HomepanelSection(rawValue: subPath) {
+                    self = .homepanel(section: subPath)
+                } else if path == .defaultBrowser, let subPath = DefaultBrowserSection(rawValue: subPath) {
+                    self = .defaultBrowser(section: subPath)
+                } else {
+                    return nil
+                }
+            case .fxaSignIn where urlScanner.value(query: "signin") != nil:
+                self = .fxaSignIn(FxALaunchParams(entrypoint: .fxaDeepLinkNavigation, query: url.getQuery()))
+
+            case .openUrl:
+                self = .search(url: urlQuery, isPrivate: isPrivate)
+
+            case .openText:
+                self = .search(query: urlScanner.value(query: "text") ?? "")
+            case .glean:
+                self = .glean(url: url)
+
+            case .widgetMediumTopSitesOpenUrl:
+                // Widget Top sites - open url
+                TelemetryWrapper.recordEvent(category: .action, method: .open, object: .mediumTopSitesWidget)
+                self = .search(url: urlQuery, isPrivate: isPrivate)
+
+            case .widgetSmallQuickLinkOpenUrl:
+                // Widget Quick links - small - open url private or regular
+                TelemetryWrapper.recordEvent(category: .action, method: .open, object: .smallQuickActionSearch)
+                self = .search(url: urlQuery, isPrivate: isPrivate)
+
+            case .widgetMediumQuickLinkOpenUrl:
+                // Widget Quick Actions - medium - open url private or regular
+                TelemetryWrapper.recordEvent(category: .action, method: .open, object: isPrivate ?.mediumQuickActionPrivateSearch:.mediumQuickActionSearch)
+                self = .search(url: urlQuery, isPrivate: isPrivate)
+
+            case .widgetSmallQuickLinkOpenCopied, .widgetMediumQuickLinkOpenCopied:
+                // Widget Quick links - medium - open copied url
+                TelemetryWrapper.recordEvent(category: .action, method: .open, object: .mediumQuickActionCopiedLink)
+                if !UIPasteboard.general.hasURLs {
+                    let searchText = UIPasteboard.general.string ?? ""
+                    self = .search(query: searchText)
+                } else {
+                    let url = UIPasteboard.general.url
+                    let isPrivate = UserDefaults.standard.bool(forKey: PrefsKeys.LastSessionWasPrivate)
+                    self = .search(url: url, isPrivate: isPrivate)
+                }
+
+            case .widgetSmallQuickLinkClosePrivateTabs, .widgetMediumQuickLinkClosePrivateTabs:
+                // Widget Quick links - medium - close private tabs
+                TelemetryWrapper.recordEvent(category: .action, method: .open, object: .mediumQuickActionClosePrivate)
+                self = .action(action: .closePrivateTabs)
+
+            case .widgetTabsMediumOpenUrl:
+                // Widget Tabs Quick View - medium
+                TelemetryWrapper.recordEvent(category: .action, method: .open, object: .mediumTabsOpenUrl)
+                let tabs = SimpleTab.getSimpleTabs()
+                if let uuid = urlScanner.value(query: "uuid"), !tabs.isEmpty {
+                    let tab = tabs[uuid]
+                    self = .search(url: tab?.url, tabId: uuid)
+                } else {
+                    self = .search(url: nil, isPrivate: false)
+                }
+
+            case .widgetTabsLargeOpenUrl:
+                // Widget Tabs Quick View - large
+                TelemetryWrapper.recordEvent(category: .action, method: .open, object: .largeTabsOpenUrl)
+                let tabs = SimpleTab.getSimpleTabs()
+                if let uuid = urlScanner.value(query: "uuid"), !tabs.isEmpty {
+                    let tab = tabs[uuid]
+                    self = .search(url: tab?.url, tabId: uuid)
+                } else {
+                    self = .search(url: nil, isPrivate: false)
+                }
+                    
+            case .fxaSignIn:
+                return nil
+            }
+        } else if urlScanner.isHTTPScheme {
+            TelemetryWrapper.gleanRecordEvent(category: .action, method: .open, object: .asDefaultBrowser)
+            RatingPromptManager.isBrowserDefault = true
+            // Use the last browsing mode the user was in
+            let isPrivate = UserDefaults.standard.bool(forKey: PrefsKeys.LastSessionWasPrivate)
+            self = .search(url: url, isPrivate: isPrivate)
+        } else {
+            return nil
+        }
+    }
 }
 
 public extension URL {
@@ -137,10 +242,15 @@ enum DeeplinkInput {
     }
 }
 
-extension Route {
+struct URLScanner {
+    var components: [String]
+    var queries: [URLQueryItem]
+    let scheme: String
+    let host: String
+
     init?(url: URL) {
         let url = url.sanitized
-        guard let components = URLComponents(url: url, resolvingAgainstBaseURL: false),
+        guard let urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false),
               let urlTypes = Bundle.main.object(forInfoDictionaryKey: "CFBundleURLTypes") as? [AnyObject],
               let urlSchemes = urlTypes.first?["CFBundleURLSchemes"] as? [String]
         else {
@@ -148,110 +258,22 @@ extension Route {
             return nil
         }
 
-        guard let scheme = components.scheme, urlSchemes.contains(scheme) else { return nil }
+        guard let scheme = urlComponents.scheme, urlSchemes.contains(scheme) else { return nil }
+        self.scheme = scheme
+        self.host = urlComponents.host ?? ""
+        self.components = URL(string: urlComponents.path)?.pathComponents ?? []
+        self.queries = urlComponents.queryItems ?? []
+    }
 
-        let isOurScheme = [URL.mozPublicScheme, URL.mozInternalScheme].contains(scheme)
-        if isOurScheme, let hostRaw = components.host?.lowercased(), !hostRaw.isEmpty, let host = DeeplinkInput.Host(rawValue: hostRaw) {
-            let urlQuery = components.valueForQuery("url")?.asURL
-            // Unless the `open-url` URL specifies a `private` parameter,
-            // use the last browsing mode the user was in.
-            let isPrivate = Bool(components.valueForQuery("private") ?? "") ?? UserDefaults.standard.bool(forKey: PrefsKeys.LastSessionWasPrivate)
+    var isOurScheme: Bool {
+        return [URL.mozPublicScheme, URL.mozInternalScheme].contains(self.scheme)
+    }
 
-            switch host {
-            case .deepLink:
-                let deepLinkURL = components.valueForQuery("url")?.lowercased()
-                let paths = deepLinkURL?.split(separator: "/") ?? []
-                guard let pathRaw = paths[safe: 0].flatMap(String.init),
-                        let path = DeeplinkInput.Path(rawValue: pathRaw),
-                        let subPath = paths[safe: 1].flatMap(String.init)
-                else { return nil }
-                if path == .settings, let link = SettingsSection(rawValue: subPath) {
-                    self = .settings(section: link)
-                } else if path == .homepanel, let link = HomepanelSection(rawValue: subPath) {
-                    self = .homepanel(section: link)
-                } else if path == .defaultBrowser, let link = DefaultBrowserSection(rawValue: subPath) {
-                    self = .defaultBrowser(section: link)
-                } else {
-                    return nil
-                }
+    func value(query: String) -> String? {
+        return self.queries.first { $0.name == query }?.value
+    }
 
-            case .fxaSignIn where components.valueForQuery("signin") != nil:
-                self = .fxaSignIn(FxALaunchParams(entrypoint: .fxaDeepLinkNavigation, query: url.getQuery()))
-
-            case .openUrl:
-                self = .search(url: urlQuery, isPrivate: isPrivate)
-
-            case .openText:
-                self = .search(query: components.valueForQuery("text") ?? "")
-
-            case .glean:
-                self = .glean(url: url)
-
-            case .widgetMediumTopSitesOpenUrl:
-                // Widget Top sites - open url
-                TelemetryWrapper.recordEvent(category: .action, method: .open, object: .mediumTopSitesWidget)
-                self = .search(url: urlQuery, isPrivate: isPrivate)
-
-            case .widgetSmallQuickLinkOpenUrl:
-                // Widget Quick links - small - open url private or regular
-                TelemetryWrapper.recordEvent(category: .action, method: .open, object: .smallQuickActionSearch)
-                self = .search(url: urlQuery, isPrivate: isPrivate)
-
-            case .widgetMediumQuickLinkOpenUrl:
-                // Widget Quick Actions - medium - open url private or regular
-                TelemetryWrapper.recordEvent(category: .action, method: .open, object: isPrivate ?.mediumQuickActionPrivateSearch :.mediumQuickActionSearch)
-                self = .search(url: urlQuery, isPrivate: isPrivate)
-
-            case .widgetSmallQuickLinkOpenCopied, .widgetMediumQuickLinkOpenCopied:
-                // Widget Quick links - medium - open copied url
-                TelemetryWrapper.recordEvent(category: .action, method: .open, object: .mediumQuickActionCopiedLink)
-                if !UIPasteboard.general.hasURLs {
-                    let searchText = UIPasteboard.general.string ?? ""
-                    self = .search(query: searchText)
-                } else {
-                    let url = UIPasteboard.general.url
-                    let isPrivate = UserDefaults.standard.bool(forKey: PrefsKeys.LastSessionWasPrivate)
-                    self = .search(url: url, isPrivate: isPrivate)
-                }
-
-            case .widgetSmallQuickLinkClosePrivateTabs, .widgetMediumQuickLinkClosePrivateTabs:
-                // Widget Quick links - medium - close private tabs
-                TelemetryWrapper.recordEvent(category: .action, method: .open, object: .mediumQuickActionClosePrivate)
-                self = .action(action: .closePrivateTabs)
-
-            case .widgetTabsMediumOpenUrl:
-                // Widget Tabs Quick View - medium
-                TelemetryWrapper.recordEvent(category: .action, method: .open, object: .mediumTabsOpenUrl)
-                let tabs = SimpleTab.getSimpleTabs()
-                if let uuid = components.valueForQuery("uuid"), !tabs.isEmpty {
-                    let tab = tabs[uuid]
-                    self = .search(url: tab?.url, tabId: uuid)
-                } else {
-                    self = .search(url: nil, isPrivate: false)
-                }
-
-            case .widgetTabsLargeOpenUrl:
-                // Widget Tabs Quick View - large
-                TelemetryWrapper.recordEvent(category: .action, method: .open, object: .largeTabsOpenUrl)
-                let tabs = SimpleTab.getSimpleTabs()
-                if let uuid = components.valueForQuery("uuid"), !tabs.isEmpty {
-                    let tab = tabs[uuid]
-                    self = .search(url: tab?.url, tabId: uuid)
-                } else {
-                    self = .search(url: nil, isPrivate: false)
-                }
-                    
-            case .fxaSignIn:
-                return nil
-            }
-        } else if ["http", "https"].contains(scheme) {
-            TelemetryWrapper.gleanRecordEvent(category: .action, method: .open, object: .asDefaultBrowser)
-            RatingPromptManager.isBrowserDefault = true
-            // Use the last browsing mode the user was in
-            let isPrivate = UserDefaults.standard.bool(forKey: PrefsKeys.LastSessionWasPrivate)
-            self = .search(url: url, isPrivate: isPrivate)
-        } else {
-            return nil
-        }
+    var isHTTPScheme: Bool {
+        return ["http", "https"].contains(scheme)
     }
 }
