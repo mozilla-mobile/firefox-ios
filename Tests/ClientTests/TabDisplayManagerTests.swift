ddf9afc68378c0d596ee73275856945236d34929
yoanarios <yriosdiaz@mozilla.com>
2023-04-18 09:15:49 -0400

    Fix failing test on XCode 14.3 and possible crash related to batchUpdate

--- Tests/ClientTests/TabDisplayManagerTests.swift
+++ Tests/ClientTests/TabDisplayManagerTests.swift
@@ -319,6 +319,8 @@ class TabDisplayManagerTests: XCTestCase {
         tabDisplayManager.inactiveViewModel?.inactiveTabs = [inactiveTab1,
                                                              inactiveTab2]
 
+        // Force collectionView reload section to avoid crash
+        collectionView.reloadSections(IndexSet(integer: 0))
         cfrDelegate.confirmClose = true
         tabDisplayManager.didTapCloseInactiveTabs(tabsCount: 2)
 
@@ -347,6 +349,8 @@ class TabDisplayManagerTests: XCTestCase {
         tabDisplayManager.inactiveViewModel?.inactiveTabs = [inactiveTab1,
                                                              inactiveTab2]
 
+        // Force collectionView reload section to avoid crash
+        collectionView.reloadSections(IndexSet(integer: 0))
         cfrDelegate.confirmClose = false
         tabDisplayManager.didTapCloseInactiveTabs(tabsCount: 2)
 
