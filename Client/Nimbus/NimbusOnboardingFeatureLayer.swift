cc4c90ae36f6952f99a990f020626569f21a9897
roux g. buciu <roux@mozilla.com>
2023-04-19 13:46:21 -0400

    Comply!

--- Client/Nimbus/NimbusOnboardingFeatureLayer.swift
+++ Client/Nimbus/NimbusOnboardingFeatureLayer.swift
@@ -15,9 +15,32 @@ class NimbusOnboardingFeatureLayer {
     func getOnboardingModel() -> OnboardingViewModel {
         let framework = nimbus.features.onboardingFrameworkFeature.value()
 
-        return OnboardingViewModel(cards: nil,
-                                   cardOrder: nil,
+        return OnboardingViewModel(cards: framework.cards,
+                                   cardOrder: framework.cardOrdering,
                                    dismissable: framework.dismissable)
+    }
+
+    private func getOnboardingCards(from cardData: [OnboardingCardData]) -> [OnboardingCardInfo] {
+        var cards = [OnboardingCardInfo]()
+
+        card.forEach { card in
+            let buttons = getOnboardingCardButtons(from: card.buttons)
+            cards.append(OnboardingCardInfo(
+                name: card.name,
+                title: card.title,
+                body: card.body,
+                link: getOnboardingLink(from: card.link),
+                buttons: getOnboardingCardButtons(from: card.buttons),
+                type: card.type))
+        }
+    }
+
+    private func getOnboardingCardButtons(from cardButtons: [OnboardingButton]) -> [OnboardingButtonInfo] {
+
+    }
+
+    private func getOnboardingLink(from cardLink: OnboardingLink) -> OnboardingLinkInfo? {
+
     }
 }
 
