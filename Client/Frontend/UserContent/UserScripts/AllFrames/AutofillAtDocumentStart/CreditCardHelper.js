2badde04053a4398e00a10e8cf13200e7b530f69
Nishant Bhasin <nish.bhasin@gmail.com>
2023-05-03 11:11:51 -0400

    Refactor FXIOS-5808 [v115] js injection scripts for credit card

--- Client/Frontend/UserContent/UserScripts/AllFrames/AutofillAtDocumentStart/CreditCardHelper.js
+++ Client/Frontend/UserContent/UserScripts/AllFrames/AutofillAtDocumentStart/CreditCardHelper.js
@@ -3,7 +3,7 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
 "use strict";
-
+ 
 import { CreditCardAutofill } from "Assets/CC_Script/CreditCardAutofill.js";
 
 const messageTypes = {
