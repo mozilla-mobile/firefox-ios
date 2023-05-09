337207b8cbcfbfb5116b1bece92dcf1dc8fa7158
Laurie Marceau <lmarceau@mozilla.com>
2023-05-09 10:21:11 -0400

    Make sure we deinit launch coordinator
    Also start browser coordinator at the same time so we handle the survey action

--- Tests/ClientTests/Coordinators/Launch/LaunchCoordinatorTests.swift
+++ Tests/ClientTests/Coordinators/Launch/LaunchCoordinatorTests.swift
@@ -121,7 +121,7 @@ final class LaunchCoordinatorTests: XCTestCase {
     }
 
     // MARK: - Delegates
-    func testStart_surveySetsDelegate() {
+    func testStart_surveySetsDelegate() throws {
         let messageManager = MockGleanPlumbMessageManagerProtocol()
         let message = createMessage(isExpired: false)
         messageManager.message = message
@@ -131,7 +131,8 @@ final class LaunchCoordinatorTests: XCTestCase {
         let subject = createSubject(isIphone: false)
         subject.start(with: .survey(manager: manager))
 
-        XCTAssertNotNil(manager.dismissClosure)
+        let presentedVC = try XCTUnwrap(mockRouter.presentedViewController as? SurveySurfaceViewController)
+        XCTAssertNotNil(presentedVC.delegate)
     }
 
     // MARK: - Helpers
