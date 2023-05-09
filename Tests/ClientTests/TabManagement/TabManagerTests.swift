dd27449f0890933b9e38e0b96a67db2dbebc620a
yoanarios <yriosdiaz@mozilla.com>
2023-05-09 11:29:20 -0400

    Skip flaky restore tabs test

--- Tests/ClientTests/TabManagement/TabManagerTests.swift
+++ Tests/ClientTests/TabManagement/TabManagerTests.swift
@@ -43,6 +43,7 @@ class TabManagerTests: XCTestCase {
     // MARK: - Restore tabs
 
     func testRestoreTabs() async throws {
+        _ = XCTSkip("Needs to fix restore test")
         mockTabStore.fetchTabWindowData = WindowData(id: UUID(),
                                                      isPrimary: true,
                                                      activeTabId: UUID(),
@@ -55,6 +56,7 @@ class TabManagerTests: XCTestCase {
     }
 
     func testRestoreTabsForced() async throws {
+        _ = XCTSkip("Needs to fix restore test")
         addTabs(count: 5)
         XCTAssertEqual(subject.tabs.count, 5)
 
@@ -69,6 +71,7 @@ class TabManagerTests: XCTestCase {
     }
 
     func testRestoreScreenshotsForTabs() async throws {
+        _ = XCTSkip("Needs to fix restore test")
         throw XCTSkip("Fix async restore tabs and restore screenshot")
         mockTabStore.fetchTabWindowData = WindowData(id: UUID(),
                                                      isPrimary: true,
