9f4ae3b727d3c934858cc8db6ab4b5efacab4981
roux g. buciu <roux@mozilla.com>
2023-05-18 18:37:19 -0400

    Set up prerequistes, but it's not working

--- Client/Nimbus/OnboardingFeatureLayer/NimbusOnboardingFeatureLayer.swift
+++ Client/Nimbus/OnboardingFeatureLayer/NimbusOnboardingFeatureLayer.swift
@@ -2,8 +2,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import Foundation
 import Shared
+import MozillaAppServices
 
 class NimbusOnboardingFeatureLayer: NimbusOnboardingFeatureLayerProtocol {
     /// Fetches an ``OnboardingViewModel`` from ``FxNimbus`` configuration.
@@ -20,7 +20,8 @@ class NimbusOnboardingFeatureLayer: NimbusOnboardingFeatureLayerProtocol {
             cards: getOrderedOnboardingCards(
                 for: onboardingType,
                 from: framework.cards,
-                using: framework.cardOrdering),
+                using: framework.cardOrdering,
+                and: framework.prerequisites),
             isDismissable: framework.dismissable)
     }
 
@@ -35,9 +36,10 @@ class NimbusOnboardingFeatureLayer: NimbusOnboardingFeatureLayerProtocol {
     private func getOrderedOnboardingCards(
         for onboardingType: OnboardingType,
         from cardData: [NimbusOnboardingCardData],
-        using cardOrder: [String]
+        using cardOrder: [String],
+        and prerequisiteTable: [String: String]
     ) -> [OnboardingCardInfoModel] {
-        let cards = getOnboardingCards(from: cardData)
+        let cards = getOnboardingCards(from: cardData, and: prerequisiteTable)
 
         // Sorting the cards this way, instead of a simple sort, to account for human
         // error in the order naming. If a card name is misspelled, it will be ignored
@@ -62,33 +64,102 @@ class NimbusOnboardingFeatureLayer: NimbusOnboardingFeatureLayerProtocol {
     ///
     /// - Parameter cardData: Card data from ``FxNimbus/shared``
     /// - Returns: An array of viable ``OnboardingCardInfoModel``
-    private func getOnboardingCards(from cardData: [NimbusOnboardingCardData]) -> [OnboardingCardInfoModel] {
+    private func getOnboardingCards(
+        from cardData: [NimbusOnboardingCardData],
+        and prerequisiteTable: [String: String]
+    ) -> [OnboardingCardInfoModel] {
         let a11yOnboarding = AccessibilityIdentifiers.Onboarding.onboarding
         let a11yUpgrade = AccessibilityIdentifiers.Upgrade.upgrade
+        var jexlCache = [String: Bool]()
+
+        // If `GleanPlumbHelper` creation fails, we cannot continue with
+        // evaluating card triggers based on their JEXL prerequisites.
+        // Therefore, we return an empty array.
+        guard let helper = GleanPlumbMessageUtility().createGleanPlumbHelper() else { return [] }
 
         return cardData.compactMap { card in
-            return OnboardingCardInfoModel(
-                name: card.name,
-                title: String(format: card.title, AppName.shortName.rawValue),
-                body: String(format: card.body, AppName.shortName.rawValue),
-                link: getOnboardingLink(from: card.link),
-                buttons: getOnboardingCardButtons(from: card.buttons),
-                type: card.type,
-                a11yIdRoot: card.type == .freshInstall ? a11yOnboarding : a11yUpgrade,
-                imageID: getOnboardingImageID(from: card.image))
-        }
-            .enumerated()
-            .map { index, card in
+            if verifyPrerequisites(
+                from: card,
+                against: prerequisiteTable,
+                using: &jexlCache,
+                and: helper
+            ) {
                 return OnboardingCardInfoModel(
                     name: card.name,
-                    title: card.title,
-                    body: card.body,
-                    link: card.link,
-                    buttons: card.buttons,
+                    title: String(format: card.title, AppName.shortName.rawValue),
+                    body: String(format: card.body, AppName.shortName.rawValue),
+                    link: getOnboardingLink(from: card.link),
+                    buttons: getOnboardingCardButtons(from: card.buttons),
                     type: card.type,
-                    a11yIdRoot: "\(card.a11yIdRoot)\(index)",
-                    imageID: card.imageID)
+                    a11yIdRoot: card.type == .freshInstall ? a11yOnboarding : a11yUpgrade,
+                    imageID: getOnboardingImageID(from: card.image))
             }
+
+            return nil
+        }
+        .enumerated()
+        .map { index, card in
+            return OnboardingCardInfoModel(
+                name: card.name,
+                title: card.title,
+                body: card.body,
+                link: card.link,
+                buttons: card.buttons,
+                type: card.type,
+                a11yIdRoot: "\(card.a11yIdRoot)\(index)",
+                imageID: card.imageID)
+        }
+    }
+
+    private func verifyPrerequisites(
+        from card: NimbusOnboardingCardData,
+        against prerequisiteTable: [String: String],
+        using jexlCache: inout [String: Bool],
+        and helper: GleanPlumbMessageHelper
+    ) -> Bool {
+        // Make sure prerequisites exist and have a value, and that the number
+        // of valid prerequisites matches the number of prerequisites on the card.
+        // If these mismatch, that means a card contains a prerequisite that's
+        // not in the prerequisites lookup table. JEXLS can only be evaluated on
+        // supported prerequisites. Otherwise, consider the card invalid.
+        guard card.prerequisites.compactMap({ prerequisiteTable[$0] })
+            .count == card.prerequisites.count
+        else { return false }
+
+        do {
+            return try isCardValid(checking: card.prerequisites,
+                                   using: helper,
+                                   and: &jexlCache)
+        } catch {
+            return false
+        }
+    }
+
+    private func isCardValid(
+        checking prerequisites: [String],
+        using helper: GleanPlumbMessageHelper,
+        and jexlCache: inout [String: Bool]
+    ) throws -> Bool {
+        // Some unit test are failing in Bitrise during the jexlEvaluation
+        // process. We will bypass the check for unit test while we find a
+        // solution to mock properly `GleanPlumbMessageUtility` that right
+        // now is highly tied to `Experiments.shared`
+        guard !AppConstants.isRunningTest else { return true }
+
+        return try prerequisites.reduce(true) { accumulator, prerequisite in
+            guard accumulator else { return false }
+
+            // Check the jexlCache for the `Bool`, in the case we already
+            // evaluated it. Otherwise, perform an expensive Foreign Function
+            // Interface (FFI) operation once for the trigger.
+            guard let evaluation = jexlCache[prerequisite] else {
+                let evaluation = try helper.evalJexl(expression: prerequisite)
+                jexlCache[prerequisite] = evaluation
+                return evaluation
+            }
+
+            return evaluation
+        }
     }
 
     /// Returns an optional array of ``OnboardingButtonInfoModel`` given the data.
