5897a603cf5cdea7711b1900ceff5f8358857190
Catalin <catalin.neculaide.git@gmail.com>
2023-05-24 17:23:21 +0300

    Add FXIOS-5922 [v115] Create Onboarding DefaultSettings View (#14456)
    * made onboardingSettingsViewController
    
    * convert UX struct to enum
    
    * add boldable string
    
    * change filefolder
    
    * add labels
    
    * add number of labels with steps
    
    * change IntroVC to only show BottomSheetView
    
    * delete file
    
    * hook viewmodel to view
    
    * fix scrollview issue and tidy up things
    
    * fix some comments
    
    * addressing comments
    
    * typo
    
    ---------
    
    Co-authored-by: Catalin Andrei Neculaide <catalin.neculaide@softvision.com>

---
+++ Client/Frontend/Onboarding/Models/OnboardingDefaultBrowserInfoModel.swift
@@ -0,0 +1,29 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import Foundation
+import Shared
+
+struct OnboardingDefaultBrowserInfoModel: OnboardingDefaultBrowserModelProtocol {
+    var title: String
+    var instructionSteps: [String]
+    var buttonTitle: String
+    var a11yIdRoot: String
+
+    init(title: String = String.Onboarding.DefaultBrowserPopup.Title,
+         instructionSteps: [String] = [String.Onboarding.DefaultBrowserPopup.FirstInstruction,
+              String.Onboarding.DefaultBrowserPopup.SecondInstruction,
+              String(format: String.Onboarding.DefaultBrowserPopup.ThirdInstruction, AppName.shortName.rawValue)],
+         buttonTitle: String = String.Onboarding.DefaultBrowserPopup.ButtonTitle,
+         a11yIdRoot: String) {
+        self.title = title
+        self.instructionSteps = instructionSteps
+        self.buttonTitle = buttonTitle
+        self.a11yIdRoot = a11yIdRoot
+    }
+
+    func getAttributedStrings(with font: UIFont) -> [NSAttributedString] {
+        return instructionSteps.map { MarkupAttributeUtility(baseFont: font).addAttributesTo(text: $0) }
+    }
+}
