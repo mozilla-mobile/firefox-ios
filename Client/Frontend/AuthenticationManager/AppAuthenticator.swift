7a2375fd48df96edb874275db92c912a0e2d6005
petruSt24 <126153378+petruSt24@users.noreply.github.com>
2023-05-25 18:01:06 +0300

    Bugfix FXIOS-6006 [v115] wrong text displayed in Touch ID Biometrics (#14285)
    * Bugfix FXIOS-6006 [v114] wrong text displayed in Touch ID Biometrics #13636
    
    * Bugfix FXIOS-6006 [v115] added the text provided by Emily #13636
    
    * Bugfix FXIOS-6006 [v115] bumped string version #13636
    
    ---------
    
    Co-authored-by: Stefan Vladut <stefan.vladut@softvision.com>

--- Client/Frontend/AuthenticationManager/AppAuthenticator.swift
+++ Client/Frontend/AuthenticationManager/AppAuthenticator.swift
@@ -25,8 +25,9 @@ class AppAuthenticator: AppAuthenticationProtocol {
 
         // First check if we have the needed hardware support.
         var error: NSError?
+        let localizedErrorMessage = String.Biometry.Screen.UniversalAuthenticationReason
         if context.canEvaluatePolicy(.deviceOwnerAuthentication, error: &error) {
-            context.evaluatePolicy(.deviceOwnerAuthentication, localizedReason: .Biometry.Screen.UniversalAuthenticationReason) { success, error in
+            context.evaluatePolicy(.deviceOwnerAuthentication, localizedReason: localizedErrorMessage) { success, error in
                 if success {
                     DispatchQueue.main.async {
                         completion(.success(()))
