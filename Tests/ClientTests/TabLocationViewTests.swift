ff7cef196f6c03b98936f3e35ea895dd34e48648
Laurie Marceau <lmarceau@mozilla.com>
2023-05-25 11:22:15 -0400

    Try to fix error in test

--- Tests/ClientTests/TabLocationViewTests.swift
+++ Tests/ClientTests/TabLocationViewTests.swift
@@ -2,11 +2,22 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
+import Common
 import XCTest
 
 @testable import Client
 
 class TabLocationViewTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+        DependencyHelperMock().bootstrapDependencies()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+        AppContainer.shared.reset()
+    }
+
     func testDelegateMemoryLeak() {
         let tabLocationView = TabLocationView()
         let delegate = MockTabLocationViewDelegate()
