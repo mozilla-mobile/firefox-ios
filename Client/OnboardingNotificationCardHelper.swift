1f62a8ddd45a6532cf3bb9050cd7f70994bf3028
roux g. buciu <11182210+adudenamedruby@users.noreply.github.com>
2023-05-26 12:17:13 -0400

    Bugfix FXIOS-6467 [v114] Ask for permission after sync sign in bug (#14592)
    * Fix bug & add test
    
    * add more tests and FPify things
    
    * Minor improvements
    
    * Update Tests/ClientTests/NotificationSurface/OnboardingNotificationCardHelperTests.swift
    
    Co-authored-by: lmarceau <lmarceau@mozilla.com>
    
    * NO LEEKS!
    
    * LET ME MERGE, BR!!!
    
    ---------
    
    Co-authored-by: lmarceau <lmarceau@mozilla.com>
    (cherry picked from commit b9fe0bfb7d19a6f1233729194c63b616e2f54319)
    
    # Conflicts:
    #	Client/OnboardingNotificationCardHelper.swift

--- Client/OnboardingNotificationCardHelper.swift
+++ Client/OnboardingNotificationCardHelper.swift
@@ -4,6 +4,7 @@
 
 import Foundation
 
+<<<<<<< HEAD
 class OnboardingNotificationCardHelper: FeatureFlaggable {
     var cardPosition: OnboardingNotificationCardPosition {
         return featureFlags.getCustomState(for: .onboardingNotificationCard) ?? .noCard
@@ -16,5 +17,24 @@ class OnboardingNotificationCardHelper: FeatureFlaggable {
         case .beforeSync, .afterSync:
             return !isOnboarding // we ask for permission on notification card instead
         }
+=======
+struct OnboardingNotificationCardHelper {
+    private func notificationCardIsInOnboarding(
+        from featureLayer: NimbusOnboardingFeatureLayer = NimbusOnboardingFeatureLayer()
+    ) -> Bool {
+        return featureLayer
+            .getOnboardingModel(for: .freshInstall)
+            .cards
+            .contains {
+                return $0.buttons.primary.action == .requestNotifications
+                || $0.buttons.secondary?.action == .requestNotifications
+            }
+    }
+
+    func askForPermissionDuringSync(isOnboarding: Bool) -> Bool {
+        if notificationCardIsInOnboarding() { return false }
+
+        return isOnboarding
+>>>>>>> b9fe0bfb7 (Bugfix FXIOS-6467 [v114] Ask for permission after sync sign in bug (#14592))
     }
 }
