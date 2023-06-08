2db43cc59eb4d494b853be4b0e7b894efec876cc
roux g. buciu <roux@mozilla.com>
2023-06-08 13:43:06 -0400

    Fix it like a boss

--- Client/Nimbus/OnboardingFeatureLayer/NimbusOnboardingFeatureLayer.swift
+++ Client/Nimbus/OnboardingFeatureLayer/NimbusOnboardingFeatureLayer.swift
@@ -94,7 +94,7 @@ class NimbusOnboardingFeatureLayer: NimbusOnboardingFeatureLayerProtocol {
                 return OnboardingCardInfoModel(
                     name: card.name,
                     title: String(format: card.title, AppName.shortName.rawValue),
-                    body: String(format: card.body, AppName.shortName.rawValue),
+                    body: String(format: card.body, AppName.shortName.rawValue, AppName.shortName.rawValue),
                     link: getOnboardingLink(from: card.link),
                     buttons: getOnboardingCardButtons(from: card.buttons),
                     type: card.type,
