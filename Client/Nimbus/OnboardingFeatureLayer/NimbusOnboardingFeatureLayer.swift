ad2410172862a765f81d47f89e1c203c3cd836cc
roux g. buciu <roux@mozilla.com>
2023-06-08 14:33:12 -0400

    Fix conflicts

--- Client/Nimbus/OnboardingFeatureLayer/NimbusOnboardingFeatureLayer.swift
+++ Client/Nimbus/OnboardingFeatureLayer/NimbusOnboardingFeatureLayer.swift
@@ -70,7 +70,7 @@ class NimbusOnboardingFeatureLayer: NimbusOnboardingFeatureLayerProtocol {
             return OnboardingCardInfoModel(
                 name: card.name,
                 title: String(format: card.title, AppName.shortName.rawValue),
-                body: String(format: card.body, AppName.shortName.rawValue),
+                body: String(format: card.body, AppName.shortName.rawValue, AppName.shortName.rawValue),
                 link: getOnboardingLink(from: card.link),
                 buttons: getOnboardingCardButtons(from: card.buttons),
                 type: card.type,
@@ -91,92 +91,6 @@ class NimbusOnboardingFeatureLayer: NimbusOnboardingFeatureLayerProtocol {
             }
     }
 
-<<<<<<< HEAD
-=======
-    private func getOnboardingCards(
-        from cardData: [NimbusOnboardingCardData],
-        withConditions conditionTable: [String: String]
-    ) -> [OnboardingCardInfoModel] {
-        let a11yOnboarding = AccessibilityIdentifiers.Onboarding.onboarding
-        let a11yUpgrade = AccessibilityIdentifiers.Upgrade.upgrade
-
-        // AppServices' Foreign Function Interface JEXL evaluator is an expensive
-        // function. Therefore, we create a JEXL cache at the top level, to
-        // be reused for each card, because the same conditions may have
-        // already been evaluated, increasing performance.
-        // However, this is unsing an `inout` operator, and that's poor practice.
-        // It will be removed in:
-        // TODO: https://mozilla-hub.atlassian.net/browse/FXIOS-6572
-        var jexlCache = [String: Bool]()
-
-        // If `NimbusMessagingHelper` creation fails, we cannot continue with
-        // evaluating card triggers based on their JEXL prerequisites.
-        // Therefore, we return an empty array.
-        guard let helper = helperUtility.createNimbusMessagingHelper() else { return [] }
-
-        return cardData.compactMap { card in
-            if cardIsValid(with: card, using: conditionTable, jexlCache: &jexlCache, and: helper) {
-                return OnboardingCardInfoModel(
-                    name: card.name,
-                    title: String(format: card.title, AppName.shortName.rawValue),
-                    body: String(format: card.body, AppName.shortName.rawValue, AppName.shortName.rawValue),
-                    link: getOnboardingLink(from: card.link),
-                    buttons: getOnboardingCardButtons(from: card.buttons),
-                    type: card.type,
-                    a11yIdRoot: card.type == .freshInstall ? a11yOnboarding : a11yUpgrade,
-                    imageID: getOnboardingImageID(from: card.image))
-            }
-
-            return nil
-        }
-    }
-
-    private func cardIsValid(
-        with card: NimbusOnboardingCardData,
-        using conditionTable: [String: String],
-        jexlCache: inout [String: Bool],
-        and helper: NimbusMessagingHelperProtocol
-    ) -> Bool {
-        let prerequisitesAreMet = verifyConditionEligibility(
-            from: card.prerequisites,
-            checkingAgainst: conditionTable,
-            using: &jexlCache,
-            and: helper)
-        let noDisqualifiersAreMet = !verifyConditionEligibility(
-            from: card.disqualifiers,
-            checkingAgainst: conditionTable,
-            using: &jexlCache,
-            and: helper)
-
-        return prerequisitesAreMet && noDisqualifiersAreMet
-    }
-
-    private func verifyConditionEligibility(
-        from cardConditions: [String],
-        checkingAgainst conditionLookupTable: [String: String],
-        using jexlCache: inout [String: Bool],
-        and helper: NimbusMessagingHelperProtocol
-    ) -> Bool {
-        // Make sure conditions exist and have a value, and that the number
-        // of valid conditions matches the number of conditions on the card's
-        // respective prerequisite or disqualifier table. If these mismatch,
-        // that means a card contains a condition that's not in the feature
-        // conditions lookup table. JEXLS can only be evaluated on
-        // supported conditions. Otherwise, consider the card invalid.
-        let conditions = cardConditions.compactMap({ conditionLookupTable[$0] })
-        guard conditions.count == cardConditions.count else { return false }
-
-        do {
-            return try NimbusMessagingEvaluationUtility().doesObjectMeet(
-                verificationRequirements: conditions,
-                using: helper,
-                and: &jexlCache)
-        } catch {
-            return false
-        }
-    }
-
->>>>>>> fd0476f0b (Bugfix FXIOS-6627 [v114] Add second parameter for app name (#14820))
     /// Returns an optional array of ``OnboardingButtonInfoModel`` given the data.
     /// A card is not viable without buttons.
     private func getOnboardingCardButtons(from cardButtons: NimbusOnboardingButtons) -> OnboardingButtons {
