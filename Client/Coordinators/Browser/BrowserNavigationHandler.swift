589bd6adf27b26352a8005796067e5590e7a6d51
lmarceau <lmarceau@mozilla.com>
2023-06-09 13:48:24 -0400

    Refactor FXIOS-6535 [v116] Handle settings presentation from MainMenuActionHelper (#14831)
    - Remove show settings from BrowserDelegate, trying to keep things tidy

---
+++ Client/Coordinators/Browser/BrowserNavigationHandler.swift
@@ -0,0 +1,11 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import Foundation
+
+protocol BrowserNavigationHandler: AnyObject {
+    /// Asks to show a settings page, can be a general settings page or a child page
+    /// - Parameter settings: The settings route we're trying to get to
+    func show(settings: Route.SettingsSection)
+}
