eee69b38fdaa5e429f0077e353ee730a3af06589
yoanarios <yriosdiaz@mozilla.com>
2023-06-12 10:17:06 -0400

    Create Thunk to call middleware

--- BrowserKit/Sources/Redux/Store.swift
+++ BrowserKit/Sources/Redux/Store.swift
@@ -26,7 +26,21 @@ public class Store<State: StateType>: DefaultDispatchStore {
     private var reducer: Reducer<State>
     private var middlewares: [Middleware<State>]
     private var subscriptions: Set<SubscriptionType> = []
-    public var dispatchFunction: DispatchFunction!
+    public var dispatchFunction: DispatchFunction {
+        let dispatchFunc = middlewares
+            .reversed()
+            .reduce(
+                { [unowned self] action in
+                    self.dispatch(action)
+                },
+                { dispatchFunction, middleware in
+                    let dispatch: (Action) -> Void = { [weak self] in self?.dispatch($0)
+                    }
+                    let getState = { [weak self] in self?.state }
+                    return middleware(dispatch, getState)(dispatchFunction)
+            })
+        return dispatchFunc
+    }
 
     public init(state: State,
                 reducer: @escaping Reducer<State>,
@@ -54,18 +68,7 @@ public class Store<State: StateType>: DefaultDispatchStore {
     private func dispatch(_ currentState: State, _ action: Action) {
         let newState = reducer(currentState, action)
 
-        self.dispatchFunction = middlewares
-            .reversed()
-            .reduce(
-                { [unowned self] action in
-                    self.dispatch(action)
-                },
-                { dispatchFunction, middleware in
-                    let dispatch: (Action) -> Void = { [weak self] in self?.dispatch($0)
-                    }
-                    let getState = { [weak self] in self?.state }
-                    return middleware(dispatch, getState)(dispatchFunction)
-            })
+        dispatchFunction(action)
 
         state = newState
     }
