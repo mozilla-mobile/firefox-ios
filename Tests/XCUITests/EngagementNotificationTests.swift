b5afe6d21b2842defc54078e540c09d0e369f836
Winnie Teichmann <4530+thatswinnie@users.noreply.github.com>
2023-06-15 10:58:08 -0400

    Add FXIOS-6208 [v116] Create UI test for flow (#14989)
    * Add json with notification config
    
    * Extract method
    
    * Schedule notification directly when going to bakcground if running UITests
    
    * Add launch argument for experiments
    
    * Add engagement notification test
    
    * Remove notification from yaml
    
    * Load experiment when UITesting
    
    * Update onboarding flow
    
    * Update test
    
    * Remove code
    
    * Exclude test data from all but Fennec builds
    
    * Address PR comments
    
    * Disable test in certain testplans

---
+++ Tests/XCUITests/EngagementNotificationTests.swift
@@ -0,0 +1,73 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import XCTest
+
+class EngagementNotificationTests: BaseTestCase {
+    var currentScreen = 0
+    var rootA11yId: String {
+        return "\(AccessibilityIdentifiers.Onboarding.onboarding)\(currentScreen)"
+    }
+
+    override func setUp() {
+        launchArguments = [LaunchArguments.ClearProfile,
+                           "\(LaunchArguments.LoadExperiment)engagementNotificationWithoutConditions"]
+        super.setUp()
+    }
+
+    func testShowingNotification() {
+        goThroughOnboarding()
+
+        // As we cannot trigger the background refresh 
+        XCUIDevice.shared.press(XCUIDevice.Button.home)
+
+        let notification = springboard.otherElements["Notification"]
+        XCTAssertTrue(notification.waitForExistence(timeout: TIMEOUT_LONG)) // implicit wait
+        notification.tap()
+
+        waitForExistence(app.textFields["url"])
+        let url = app.textFields["url"].value as! String
+        XCTAssertEqual(url, "mozilla.com", "Wrong url loaded")
+    }
+
+    // MARK: Helper
+
+    private func goThroughOnboarding() {
+        // Complete the First run from first screen to the latest one
+        // Check that the first's tour screen is shown as well as all the elements in there
+        waitForExistence(app.images["\(rootA11yId)ImageView"], timeout: TIMEOUT)
+        XCTAssertTrue(app.images["\(rootA11yId)ImageView"].exists)
+        XCTAssertTrue(app.buttons["\(rootA11yId)PrimaryButton"].exists)
+        XCTAssertTrue(app.buttons["\(AccessibilityIdentifiers.Onboarding.closeButton)"].exists)
+
+        // Go to the second screen
+        app.buttons["\(rootA11yId)PrimaryButton"].tap()
+        currentScreen += 1
+        waitForExistence(app.images["\(rootA11yId)ImageView"], timeout: TIMEOUT)
+        XCTAssertTrue(app.images["\(rootA11yId)ImageView"].exists)
+        XCTAssertTrue(app.buttons["\(rootA11yId)PrimaryButton"].exists)
+        XCTAssertTrue(app.buttons["\(rootA11yId)SecondaryButton"].exists)
+
+        // Go to the third screen
+        app.buttons["\(rootA11yId)SecondaryButton"].tap()
+        currentScreen += 1
+        waitForExistence(app.images["\(rootA11yId)ImageView"], timeout: TIMEOUT)
+        XCTAssertTrue(app.images["\(rootA11yId)ImageView"].exists)
+        XCTAssertTrue(app.buttons["\(rootA11yId)PrimaryButton"].exists)
+        XCTAssertTrue(app.buttons["\(rootA11yId)SecondaryButton"].exists)
+
+        // Finish onboarding
+        app.buttons["\(rootA11yId)PrimaryButton"].tap()
+
+        sleep(1)
+
+        // Allow notifications
+        if springboard.alerts.buttons["Allow"].exists {
+            springboard.alerts.buttons["Allow"].tap()
+        }
+
+        let topSites = app.collectionViews.cells[AccessibilityIdentifiers.FirefoxHomepage.TopSites.itemCell]
+        waitForExistence(topSites)
+    }
+}
