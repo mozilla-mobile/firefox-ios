ca87b065f02b5f39504997c16d31b5c141015dd5
mauriciomaniglia <manigliamauricio@gmail.com>
2023-06-19 21:50:13 -0300

    Create helper function `makeSUT`.

--- Tests/ClientTests/Frontend/Browser/EnhancedTrackingProtection/EnhancedTrackingProtectionMenuVMTests.swift
+++ Tests/ClientTests/Frontend/Browser/EnhancedTrackingProtection/EnhancedTrackingProtectionMenuVMTests.swift
@@ -8,56 +8,42 @@ import XCTest
 
 class EnhancedTrackingProtectionMenuVMTests: XCTestCase {
     func test_websiteTitle_whenURLHasBaseDomainDeliversCorrectTitle() {
-        let sut = EnhancedTrackingProtectionMenuVM(
-            url: URL(string: "https://firefox.com")!,
-            displayTitle: "any",
-            connectionSecure: true,
-            globalETPIsEnabled: true,
-            contentBlockerStatus: anyContentBlockerStatus())
+        let sut = makeSUT(url: URL(string: "https://firefox.com")!)
 
         XCTAssertEqual(sut.websiteTitle, "firefox.com")
     }
 
     func test_websiteTitle_whenURLDoesNotHaveBaseDomainDeliversEmptyTitle() {
-        let sut = EnhancedTrackingProtectionMenuVM(
-            url: URL(string: "https://192.168.0.1:8080/path/to/resource")!,
-            displayTitle: "any",
-            connectionSecure: true,
-            globalETPIsEnabled: true,
-            contentBlockerStatus: anyContentBlockerStatus())
+        let sut = makeSUT(url: URL(string: "https://192.168.0.1:8080/path/to/resource")!)
 
         XCTAssertEqual(sut.websiteTitle, "")
     }
 
     func test_connectionStatusString_whenConnectionIsSecureDeliversCorrectStatus() {
-        let sut = EnhancedTrackingProtectionMenuVM(
-            url: anyURL(),
-            displayTitle: "any",
-            connectionSecure: true,
-            globalETPIsEnabled: true,
-            contentBlockerStatus: anyContentBlockerStatus())
+        let sut = makeSUT(connectionSecure: true)
 
         XCTAssertEqual(sut.connectionStatusString, .ProtectionStatusSecure)
     }
 
     func test_connectionStatusString_whenConnectionIsNotSecureDeliversCorrectStatus() {
-        let sut = EnhancedTrackingProtectionMenuVM(
-            url: anyURL(),
-            displayTitle: "any",
-            connectionSecure: false,
-            globalETPIsEnabled: true,
-            contentBlockerStatus: anyContentBlockerStatus())
+        let sut = makeSUT(connectionSecure: false)
 
         XCTAssertEqual(sut.connectionStatusString, .ProtectionStatusNotSecure)
     }
 
     // MARK: Helpers
 
-    private func anyURL() -> URL {
-        URL(string: "https://any-url.com")!
-    }
-
-    private func anyContentBlockerStatus() -> BlockerStatus {
-        .blocking
+    private func makeSUT(
+        url: URL = URL(string: "https://any-url.com")!,
+        displayTitle: String = "any",
+        connectionSecure: Bool = true,
+        globalETPIsEnabled: Bool = true,
+        contentBlockerStatus: BlockerStatus = .blocking) -> EnhancedTrackingProtectionMenuVM {
+        EnhancedTrackingProtectionMenuVM(
+            url: url,
+            displayTitle: displayTitle,
+            connectionSecure: connectionSecure,
+            globalETPIsEnabled: globalETPIsEnabled,
+            contentBlockerStatus: contentBlockerStatus)
     }
 }
