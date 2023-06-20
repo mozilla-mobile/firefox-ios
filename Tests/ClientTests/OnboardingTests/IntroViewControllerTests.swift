57345bfdef245dc5e2ece21deb1c6684f573caaf
roux g. buciu <11182210+adudenamedruby@users.noreply.github.com>
2023-06-20 12:21:24 -0400

    Bugfix FXIOS-6748 [v115] Change to next card from set-default-browser action (#15065)
    * Change to next card from set to browser default button
    
    * Test
    
    * Fix tests breaking due to ui action

---
+++ Tests/ClientTests/OnboardingTests/IntroViewControllerTests.swift
@@ -0,0 +1,95 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import Foundation
+import XCTest
+
+@testable import Client
+
+class IntroViewControllerTests: XCTestCase {
+    var mockNotificationCenter: MockNotificationCenter!
+
+    override func setUp() {
+        super.setUp()
+        DependencyHelperMock().bootstrapDependencies()
+        mockNotificationCenter = MockNotificationCenter()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+        mockNotificationCenter = nil
+    }
+
+    func testBasicSetupReturnsExpectedItems() {
+        let subject = createSubject()
+        XCTAssertEqual(subject.viewModel.availableCards.count, 3)
+        XCTAssertEqual(
+            subject.viewModel.availableCards[0].viewModel.buttons.primary.action,
+            .setDefaultBrowser
+        )
+        XCTAssertEqual(subject.viewModel.availableCards[0].viewModel.name, "Name 1")
+        XCTAssertEqual(subject.viewModel.availableCards[1].viewModel.name, "Name 2")
+        XCTAssertEqual(subject.viewModel.availableCards[2].viewModel.name, "Name 3")
+    }
+
+    func testSubjectRegistersForNotification() {
+        XCTAssertEqual(mockNotificationCenter.addObserverCallCount, 0)
+        let subject = createSubject()
+
+        XCTAssertEqual(mockNotificationCenter.addObserverCallCount, 1)
+        subject.registerForNotification()
+
+        XCTAssertEqual(mockNotificationCenter.addObserverCallCount, 2)
+    }
+
+    func testViewMovesToNextScreenWithFunction() {
+        let subject = createSubject()
+
+        XCTAssertEqual(subject.pageControl.currentPage, 0)
+        subject.appDidEnterBackgroundNotification()
+
+        XCTAssertEqual(subject.pageControl.currentPage, 1)
+    }
+
+    func testViewMovesToNextScreenAfterNotification() {
+        let subject = createSubject()
+        XCTAssertEqual(subject.pageControl.currentPage, 0)
+
+        // Pretending to tap the button that would register for the notification
+        subject.registerForNotification()
+        mockNotificationCenter.post(name: UIApplication.didEnterBackgroundNotification)
+
+        XCTAssertEqual(mockNotificationCenter.postCallCount, 1)
+        XCTAssertEqual(subject.pageControl.currentPage, 1)
+    }
+
+    // MARK: - Private Helpers
+    func createSubject(
+        file: StaticString = #file,
+        line: UInt = #line
+    ) -> IntroViewController {
+        NimbusOnboardingTestingConfigUtility().setupNimbusWith(
+            cards: 3,
+            image: .notifications,
+            type: .freshInstall,
+            dismissable: true,
+            shouldAddLink: false,
+            withSecondaryButton: true,
+            withPrimaryButtonAction: .setDefaultBrowser
+        )
+
+        let onboardingViewModel = NimbusOnboardingFeatureLayer().getOnboardingModel(for: .freshInstall)
+        let telemetryUtility = OnboardingTelemetryUtility(with: onboardingViewModel)
+        let viewModel = IntroViewModel(profile: MockProfile(),
+                                       model: onboardingViewModel,
+                                       telemetryUtility: telemetryUtility)
+        let subject = IntroViewController(viewModel: viewModel,
+                                          notificationCenter: mockNotificationCenter)
+
+        trackForMemoryLeaks(subject, file: file, line: line)
+        mockNotificationCenter.notifiableListener = subject
+
+        return subject
+    }
+}
