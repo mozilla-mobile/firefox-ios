f2c417236735b380a8cb5db3ecf82bd10cc1cad6
lmarceau <lmarceau@mozilla.com>
2023-06-21 09:55:57 -0400

    Refactor FXIOS-6484 [v116] support showing settings subpage when settings already exists (#15089)
    * Handle route when settings already exists
    
    - both cases with and without settings coordinator
    
    * Make sure we cover deeplinks needed for #14954
    
    * Add tests

--- Client/Coordinators/Router/Route.swift
+++ Client/Coordinators/Router/Route.swift
@@ -93,6 +93,36 @@ enum Route: Equatable {
         case toolbar
         case topSites
         case wallpaper
+
+        // Will be clean up with FXIOS-6529
+        func getSettingsRoute() -> AppSettingsDeeplinkOption? {
+            switch self {
+            case .contentBlocker:
+                return .contentBlocker
+            case .homePage:
+                return .customizeHomepage
+            case .tabs:
+                return .customizeTabs
+            case .toolbar:
+                return .customizeToolbar
+            case .topSites:
+                return .customizeTopSites
+            case .wallpaper:
+                return .wallpaper
+            case .creditCard:
+                return .creditCard
+            case .fxa:
+                return .fxa
+            case .mailto:
+                return .mailto
+            case .newTab:
+                return .newTab
+            case .search:
+                return .search
+            case .clearPrivateData, .general, .theme:
+                return nil // not handling those since not needed to temporary fix #14954
+            }
+        }
     }
 
     /// An enumeration representing different actions that can be performed within the application.
