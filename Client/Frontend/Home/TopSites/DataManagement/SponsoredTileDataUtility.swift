113fcc886e603c4548a60cb25cb835212ad7f655
Laurie Marceau <lmarceau@mozilla.com>
2023-06-21 15:46:52 -0400

    Fix filtering in top sites for search engine

---
+++ Client/Frontend/Home/TopSites/DataManagement/SponsoredTileDataUtility.swift
@@ -0,0 +1,20 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import Foundation
+import Storage
+
+protocol SponsoredTileDataUtilityInterface {
+    func shouldAdd(site: Site, with searchEngine: OpenSearchEngine?) -> Bool
+}
+
+struct SponsoredTileDataUtility: SponsoredTileDataUtilityInterface {
+    func shouldAdd(site: Site, with searchEngine: OpenSearchEngine?) -> Bool {
+        guard let defaultSearchEngine = searchEngine,
+              let secondLevelDomain = site.secondLevelDomain else { return true }
+
+        let hasMatchedEngine = defaultSearchEngine.shortName.caseInsensitiveCompare(secondLevelDomain) == .orderedSame
+        return !hasMatchedEngine
+    }
+}
