7ab13deebedc34cd8790a055c31c7ced234850ae
OrlaM <omitchell@mozilla.com>
2023-06-26 09:53:35 -0400

    Refactor FXIOS-6817 [v116] Fix crashing test (#15176)

---
+++ Tests/ClientTests/Mocks/MockThemeManager.swift
@@ -0,0 +1,31 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import Shared
+
+class MockThemeManager: ThemeManager {
+    var currentTheme: Theme = LightTheme()
+    var window: UIWindow?
+
+    func getInterfaceStyle() -> UIUserInterfaceStyle {
+        return .light
+    }
+
+    func changeCurrentTheme(_ newTheme: ThemeType) {
+        switch newTheme {
+        case .light:
+            currentTheme = LightTheme()
+        case .dark:
+            currentTheme = DarkTheme()
+        }
+    }
+
+    func systemThemeChanged() {}
+
+    func setSystemTheme(isOn: Bool) {}
+
+    func setAutomaticBrightness(isOn: Bool) {}
+
+    func setAutomaticBrightnessValue(_ value: Float) {}
+}
