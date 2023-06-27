4234c5f207d4b683e51d5c9c1337bff587d052e4
Stefan Vladut <stefan.vladut@softvision.com>
2023-06-27 15:35:18 +0300

    Bugfix FXIOS-6789 [v116] made the scroll dependent on the contentHeight instead of the number of cards #15212

--- Client/Frontend/Autofill/CreditCard/CreditCardBottomSheet/CreditCardBottomSheetViewController.swift
+++ Client/Frontend/Autofill/CreditCard/CreditCardBottomSheet/CreditCardBottomSheetViewController.swift
@@ -108,6 +108,7 @@ class CreditCardBottomSheetViewController: UIViewController, UITableViewDelegate
 
         self.viewModel.didUpdateCreditCard = { [weak self] in
             self?.cardTableView.reloadData()
+            self?.cardTableView.isScrollEnabled = self?.cardTableView.contentSize.height ?? 0 > self?.view.frame.height ?? 0
         }
 
         // Only allow selection when we are in selectSavedCard state
@@ -172,7 +173,6 @@ class CreditCardBottomSheetViewController: UIViewController, UITableViewDelegate
         let contentViewWidth = UX.contentViewWidth > view.frame.width ? view.frame.width - UX.containerPadding : UX.contentViewWidth
         contentWidthConstraint = contentView.widthAnchor.constraint(equalToConstant: contentViewWidth)
         contentWidthConstraint.priority = UILayoutPriority(999)
-        cardTableView.isScrollEnabled = viewModel.creditCards?.count ?? 0 > 6
 
         NSLayoutConstraint.activate([
             contentView.topAnchor.constraint(equalTo: view.topAnchor),
