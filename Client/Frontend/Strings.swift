b97db1fe0e74782dd91b1fe5f52620604eb050df
Nishant Patel <nishantpatel2718@gmail.com>
2023-06-27 18:22:51 -0400

    Add FXIOS-6547 [v116] Update strings based on l10n feedback (#15226)

--- Client/Frontend/Strings.swift
+++ Client/Frontend/Strings.swift
@@ -326,7 +326,7 @@ extension String {
                 value: "Remove Card",
                 comment: "Title label for button that allows user to remove their saved credit card.")
             public static let ToggleToAllowAutofillTitle = MZLocalizedString(
-                key: "CreditCard.EditCard.ToggleToAllowAutofillTitle.v116",
+                key: "CreditCard.EditCard.ToggleToAllowAutofillTitle.v112",
                 tableName: "EditCard",
                 value: "Save and Autofill Cards",
                 comment: "Title label for user to use the toggle settings to allow saving and autofilling of credit cards for webpages.")
@@ -436,17 +436,17 @@ extension String {
         // Snackbar / toast
         public struct SnackBar {
             public static let SavedCardLabel = MZLocalizedString(
-                key: "CreditCard.SnackBar.SavedCardLabel.v116",
+                key: "CreditCard.SnackBar.SavedCardLabel.v112",
                 tableName: "SnackBar",
                 value: "New Card Saved",
                 comment: "Label text that gets presented as a confirmation at the bottom of screen when credit card information gets saved successfully")
             public static let UpdatedCardLabel = MZLocalizedString(
-                key: "CreditCard.SnackBar.UpdatedCardLabel.v116",
+                key: "CreditCard.SnackBar.UpdatedCardLabel.v112",
                 tableName: "SnackBar",
                 value: "Card Information updated",
                 comment: "Label text that gets presented as a confirmation at the bottom of screen when credit card information gets updated successfully")
             public static let RemovedCardLabel = MZLocalizedString(
-                key: "CreditCard.SnackBar.RemovedCardLabel.v116",
+                key: "CreditCard.SnackBar.RemovedCardLabel.v112",
                 tableName: "SnackBar",
                 value: "Card Removed",
                 comment: "Label text that gets presented as a confirmation at the bottom of screen when the credit card is successfully removed.")
