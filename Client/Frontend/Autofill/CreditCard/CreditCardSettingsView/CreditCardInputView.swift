7807b5ed6f1852c90986770e72aa817b0a1e625f
Nishant Bhasin <nish.bhasin@gmail.com>
2023-07-10 16:35:52 -0400

    cleanup x2

--- Client/Frontend/Autofill/CreditCard/CreditCardSettingsView/CreditCardInputView.swift
+++ Client/Frontend/Autofill/CreditCard/CreditCardSettingsView/CreditCardInputView.swift
@@ -158,7 +158,7 @@ struct CreditCardInputView: View {
         }
         .foregroundColor(viewModel.isRightBarButtonEnabled ? barButtonColor : saveButtonDisabledColor)
         .onAppear {
-            viewModel.isRightBarButtonEnabled = viewModel.shouldEnableTopRightButton()
+            viewModel.isRightBarButtonEnabled = viewModel.initialStateToEnableTopRightButton()
         }
     }
 
