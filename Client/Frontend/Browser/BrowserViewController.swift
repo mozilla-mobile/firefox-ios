fc72669a0a5544e54c7439d3070e1eb7cc07f6ae
Alaa <aalaaeiid@gmail.com>
2023-07-13 19:15:47 +0300

    removing ETPMenuDelegate conformation in BrowserVC

--- Client/Frontend/Browser/BrowserViewController.swift
+++ Client/Frontend/Browser/BrowserViewController.swift
@@ -2095,12 +2095,6 @@ extension BrowserViewController: SettingsDelegate {
     }
 }
 
-extension BrowserViewController: EnhancedTrackingProtectionMenuDelegate {
-    func settingsOpenPage(settings: Route.SettingsSection) {
-        self.homePanelDidRequestToOpenSettings(at: .contentBlocker)
-    }
-}
-
 extension BrowserViewController: PresentingModalViewControllerDelegate {
     func dismissPresentedModalViewController(_ modalViewController: UIViewController, animated: Bool) {
         self.dismiss(animated: animated, completion: nil)
