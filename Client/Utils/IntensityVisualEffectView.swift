c638ff5774d235deb80e33cb5dab4243a2dab54b
FilippoZazzeroni <55580351+FilippoZazzeroni@users.noreply.github.com>
2023-07-14 23:29:13 +0200

    Bugfix FXIOS-6981 [v117] memory leak in PhotonActionSheet (#15555)
    * Remove Memory leak in PhotonActionSheet due to background image blur, Add new background blur with UIVisualEffect
    
    * Modified file name of IntensityVisualEffectView
    
    ---------
    
    Co-authored-by: Filippo Zazzeroni <f.zazzeroni@hidonix.com>

---
+++ Client/Utils/IntensityVisualEffectView.swift
@@ -0,0 +1,27 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import Foundation
+
+class IntensityVisualEffectView: UIVisualEffectView {
+    // MARK: - Initializers
+
+    /// Create visual effect view with given effect and its intensity
+    ///
+    /// - Parameters:
+    ///   - effect: visual effect, eg UIBlurEffect(style: .dark)
+    ///   - intensity: custom intensity from 0.0 (no effect) to 1.0 (full effect) using linear scale
+    init(effect: UIVisualEffect, intensity: CGFloat) {
+        super.init(effect: nil)
+        animator = UIViewPropertyAnimator(duration: 1, curve: .linear) { [unowned self] in self.effect = effect }
+        animator.fractionComplete = intensity
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError()
+    }
+
+    // MARK: - Properties
+    private var animator: UIViewPropertyAnimator!
+}
