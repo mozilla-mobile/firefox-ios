b8176983e97a0b28d7ce815f2c8676634c1342dd
Laurie Marceau <lmarceau@mozilla.com>
2023-07-18 16:13:09 -0400

    Fix tests

--- Tests/ClientTests/Frontend/Home/HomepageViewControllerTests.swift
+++ Tests/ClientTests/Frontend/Home/HomepageViewControllerTests.swift
@@ -23,20 +23,11 @@ class HomepageViewControllerTests: XCTestCase {
         profile = nil
     }
 
-    func testHomepageViewController_creationFromBVC_nilByDefault() {
-        let tabManager = LegacyTabManager(profile: profile, imageStore: nil)
-        let browserViewController = BrowserViewController(profile: profile, tabManager: tabManager)
-        browserViewController.addSubviews()
-        XCTAssertNil(browserViewController.homepageViewController, "Homepage is nil on creation")
-    }
-
     func testHomepageViewController_simpleCreation_hasNoLeaks() {
         let tabManager = LegacyTabManager(profile: profile, imageStore: nil)
         let urlBar = URLBarView(profile: profile)
         let overlayManager = MockOverlayModeManager()
         overlayManager.setURLBar(urlBarView: urlBar)
-
-        LegacyFeatureFlagsManager.shared.initializeDeveloperFeatures(with: profile)
 
         let firefoxHomeViewController = HomepageViewController(profile: profile,
                                                                toastContainer: UIView(),
@@ -44,28 +35,5 @@ class HomepageViewControllerTests: XCTestCase {
                                                                overlayManager: overlayManager)
 
         trackForMemoryLeaks(firefoxHomeViewController)
-    }
-
-    // MARK: - UpdateInContentHomePanel
-
-    func testUpdateInContentHomePanel_nilURL_doesntShowHomepage() {
-        let tabManager = LegacyTabManager(profile: profile, imageStore: nil)
-        let browserViewController = BrowserViewController(profile: profile, tabManager: tabManager)
-        browserViewController.addSubviews()
-
-        browserViewController.updateInContentHomePanel(nil)
-
-        XCTAssertNil(browserViewController.homepageViewController, "Homepage isn't shown")
-    }
-
-    func testUpdateInContentHomePanel_notHomeURL_doesntShowHomepage() {
-        let tabManager = LegacyTabManager(profile: profile, imageStore: nil)
-        let browserViewController = BrowserViewController(profile: profile, tabManager: tabManager)
-        browserViewController.addSubviews()
-
-        let notHomeURL = URL(string: "www.google.com")!
-        browserViewController.updateInContentHomePanel(notHomeURL)
-
-        XCTAssertNil(browserViewController.homepageViewController, "Homepage isn't shown")
     }
 }
