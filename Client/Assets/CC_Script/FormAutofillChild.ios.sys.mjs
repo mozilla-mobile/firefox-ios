bb25e2490cf4a0a31cae043b839cc8649829ea5d
Issam Mani <issamouu69@gmail.com>
2023-08-01 10:44:56 +0200

    fix: normalize cc record before capture

--- Client/Assets/CC_Script/FormAutofillChild.ios.sys.mjs
+++ Client/Assets/CC_Script/FormAutofillChild.ios.sys.mjs
@@ -55,7 +55,13 @@ export class FormAutofillChild {
     this.fieldDetailsManager.activeHandler.onFormSubmitted();
     const records = this.fieldDetailsManager.activeHandler.createRecords();
     if (records.creditCard) {
-      this.onSubmitCallback(records.creditCard.map(entry => entry.record));
+      // Normalize record format so we always get a consistent
+      // credit card record format: {cc-number, cc-name, cc-exp-month, cc-exp-year}
+      const creditCardRecords = records.creditCard.map(entry => {
+        CreditCardRecord.normalizeFields(entry.record);
+        return entry.record;
+      });
+      this.onSubmitCallback(creditCardRecords);
     }
   }
 
