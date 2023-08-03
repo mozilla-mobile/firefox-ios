9e4d64240167ec0531e9595edb458f7ffa31485d
yoanarios <yrrios13@gmail.com>
2023-08-03 15:34:15 -0400

    Bugfix FXIOS-4979 [v116] After update IOS to 16.0 strange cookie behavior. (#15889)
    (cherry picked from commit 24e6aba425dbfa15dbf5ee028198fe1b0c9d5dbc)

--- Client/Application/AccessibilityIdentifiers.swift
+++ Client/Application/AccessibilityIdentifiers.swift
@@ -269,6 +269,10 @@ public struct AccessibilityIdentifiers {
             static let title = "NoImageMode"
         }
 
+        struct BlockPopUp {
+            static let title = "BlockPopUp"
+        }
+
         struct OpenWithMail {
             static let title = "OpenWith.Setting"
         }
