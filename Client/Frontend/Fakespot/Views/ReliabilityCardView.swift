5c1757f53ba6bc73121aac6eddc059afe583e89b
Winnie Teichmann <4530+thatswinnie@users.noreply.github.com>
2023-08-03 14:06:30 +0200

    Update ReliabilityCardView

--- Client/Frontend/Fakespot/Views/ReliabilityCardView.swift
+++ Client/Frontend/Fakespot/Views/ReliabilityCardView.swift
@@ -6,12 +6,20 @@ import ComponentLibrary
 import Common
 import UIKit
 
+struct ReliabilityCardViewModel {
+    let cardA11yId: String
+    let title: String
+    let titleA11yId: String
+    let rating: ReliabilityRating
+    let ratingLetterA11yId: String
+    let ratingDescriptionA11yId: String
+}
+
 class ReliabilityCardView: UIView, ThemeApplicable {
     private struct UX {
         static let verticalPadding: CGFloat = 8
         static let horizontalPadding: CGFloat = 8
-        static let titleHorizontalPadding: CGFloat = 16
-        static let titleTopPadding: CGFloat = 16
+        static let cornerRadius: CGFloat = 4
     }
 
     private lazy var cardContainer: CardContainer = .build { view in
@@ -23,11 +31,28 @@ class ReliabilityCardView: UIView, ThemeApplicable {
     private lazy var titleLabel: UILabel = .build { view in
         view.adjustsFontForContentSizeCategory = true
         view.font = DefaultDynamicFontHelper.preferredBoldFont(withTextStyle: .body, size: 17)
-        view.text = .Shopping.ReliabilityCardTitleLabel
     }
 
     private lazy var reliabilityScoreView: UIView = .build { view in
+        view.layer.cornerRadius = UX.cornerRadius
+        view.layer.borderWidth = 1
+        view.clipsToBounds = true
     }
+
+    private lazy var reliabilityLetterView: UIView = .build { _ in }
+    private lazy var reliabilityDescriptionView: UIView = .build { _ in }
+
+    private lazy var reliabilityLetterLabel: UILabel = .build { view in
+        view.adjustsFontForContentSizeCategory = true
+        view.font = DefaultDynamicFontHelper.preferredBoldFont(withTextStyle: .body, size: 15)
+    }
+
+    private lazy var reliabilityDescriptionLabel: UILabel = .build { view in
+        view.adjustsFontForContentSizeCategory = true
+        view.font = DefaultDynamicFontHelper.preferredFont(withTextStyle: .caption1, size: 12)
+    }
+
+    private var viewModel: ReliabilityCardViewModel?
 
     // MARK: - Inits
     override init(frame: CGRect) {
@@ -43,12 +68,39 @@ class ReliabilityCardView: UIView, ThemeApplicable {
         fatalError("init(coder:) has not been implemented")
     }
 
+    func configure(_ viewModel: ReliabilityCardViewModel) {
+        self.viewModel = viewModel
+
+        titleLabel.text = viewModel.title
+        titleLabel.accessibilityIdentifier = viewModel.titleA11yId
+
+        reliabilityLetterLabel.text = viewModel.rating.letter
+        reliabilityLetterLabel.accessibilityIdentifier = viewModel.ratingLetterA11yId
+        reliabilityDescriptionLabel.text = viewModel.rating.description
+        reliabilityDescriptionLabel.accessibilityIdentifier = viewModel.ratingDescriptionA11yId
+        reliabilityLetterView.layer.backgroundColor = viewModel.rating.color.cgColor
+        reliabilityDescriptionView.layer.backgroundColor = viewModel.rating.color.withAlphaComponent(0.15).cgColor
+
+        let cardModel = CardContainerModel(view: contentView, a11yId: viewModel.cardA11yId)
+        cardContainer.configure(cardModel)
+    }
+
     func applyTheme(theme: Theme) {
         cardContainer.applyTheme(theme: theme)
+        reliabilityScoreView.layer.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1).cgColor
+        reliabilityScoreView.layer.borderColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.15).cgColor
+        reliabilityLetterLabel.textColor = theme.colors.textOnColor
+        reliabilityDescriptionLabel.textColor = theme.colors.textPrimary // FXColors.DarkGrey90 for light & dark themes
     }
 
     private func setupLayout() {
         addSubview(cardContainer)
+
+        reliabilityLetterView.addSubview(reliabilityLetterLabel)
+        reliabilityDescriptionView.addSubview(reliabilityDescriptionLabel)
+        reliabilityScoreView.addSubview(reliabilityLetterView)
+        reliabilityScoreView.addSubview(reliabilityDescriptionView)
+
         contentView.addSubview(titleLabel)
         contentView.addSubview(reliabilityScoreView)
 
@@ -59,20 +111,39 @@ class ReliabilityCardView: UIView, ThemeApplicable {
             cardContainer.bottomAnchor.constraint(equalTo: bottomAnchor),
 
             titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor,
-                                                constant: UX.titleHorizontalPadding),
+                                                constant: UX.horizontalPadding),
             titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor,
-                                            constant: UX.titleTopPadding),
+                                            constant: UX.verticalPadding),
             titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor,
-                                                 constant: -UX.titleHorizontalPadding),
+                                                 constant: -UX.horizontalPadding),
             titleLabel.bottomAnchor.constraint(equalTo: reliabilityScoreView.topAnchor,
                                                constant: -UX.verticalPadding),
 
             reliabilityScoreView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor,
                                                           constant: UX.horizontalPadding),
-            reliabilityScoreView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor,
+            reliabilityScoreView.trailingAnchor.constraint(lessThanOrEqualTo: contentView.trailingAnchor,
                                                            constant: -UX.horizontalPadding),
             reliabilityScoreView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor,
                                                          constant: -UX.verticalPadding),
+
+            reliabilityLetterView.leadingAnchor.constraint(equalTo: reliabilityScoreView.leadingAnchor),
+            reliabilityLetterView.topAnchor.constraint(equalTo: reliabilityScoreView.topAnchor),
+            reliabilityLetterView.trailingAnchor.constraint(equalTo: reliabilityDescriptionView.leadingAnchor),
+            reliabilityLetterView.bottomAnchor.constraint(equalTo: reliabilityScoreView.bottomAnchor),
+
+            reliabilityDescriptionView.topAnchor.constraint(equalTo: reliabilityScoreView.topAnchor),
+            reliabilityDescriptionView.trailingAnchor.constraint(equalTo: reliabilityScoreView.trailingAnchor),
+            reliabilityDescriptionView.bottomAnchor.constraint(equalTo: reliabilityScoreView.bottomAnchor),
+
+            reliabilityLetterLabel.leadingAnchor.constraint(equalTo: reliabilityLetterView.leadingAnchor, constant: 4),
+            reliabilityLetterLabel.topAnchor.constraint(equalTo: reliabilityLetterView.topAnchor, constant: 1),
+            reliabilityLetterLabel.trailingAnchor.constraint(equalTo: reliabilityLetterView.trailingAnchor, constant: -4),
+            reliabilityLetterLabel.bottomAnchor.constraint(equalTo: reliabilityLetterView.bottomAnchor, constant: -1),
+
+            reliabilityDescriptionLabel.leadingAnchor.constraint(equalTo: reliabilityDescriptionView.leadingAnchor, constant: 8),
+            reliabilityDescriptionLabel.topAnchor.constraint(equalTo: reliabilityDescriptionView.topAnchor, constant: 2),
+            reliabilityDescriptionLabel.trailingAnchor.constraint(equalTo: reliabilityDescriptionView.trailingAnchor, constant: -8),
+            reliabilityDescriptionLabel.bottomAnchor.constraint(equalTo: reliabilityDescriptionView.bottomAnchor, constant: -2),
         ])
     }
 }
