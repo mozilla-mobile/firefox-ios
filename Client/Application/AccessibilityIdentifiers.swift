a65a819562c85a2998e13aa2c81d0f800f4f3716
yoanarios <yrrios13@gmail.com>
2023-08-09 13:17:58 -0400

    Bugfix FXIOS-7135 [v117] Settings button replaced with back button in tracking protection screen (#15951)
    (cherry picked from commit 4f523deecaf4be72675c6badbc018c2987a84a5d)

--- Client/Application/AccessibilityIdentifiers.swift
+++ Client/Application/AccessibilityIdentifiers.swift
@@ -320,6 +320,11 @@ public struct AccessibilityIdentifiers {
         struct Version {
             static let title = "FxVersion"
         }
+
+        struct TrackingProtection {
+            static let basic = "Settings.TrackingProtectionOption.BlockListBasic"
+            static let strict = "Settings.TrackingProtectionOption.BlockListStrict"
+        }
     }
 
     struct ShareTo {
