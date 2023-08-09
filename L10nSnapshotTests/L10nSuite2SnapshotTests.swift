53a2c674cb625228b0e2483afd06ae8590eeec6b
Laurie Marceau <lmarceau@mozilla.com>
2023-08-09 16:26:30 -0400

    Put if statement, since onboarding isn't necessarely always shown

--- L10nSnapshotTests/L10nSuite2SnapshotTests.swift
+++ L10nSnapshotTests/L10nSuite2SnapshotTests.swift
@@ -121,8 +121,10 @@ class L10nSuite2SnapshotTests: L10nBaseSnapshotTests {
         app.cells["Logins"].tap()
 
         waitForExistence(app.navigationBars.element(boundBy: 0), timeout: 3)
-        waitForExistence(app.buttons[AccessibilityIdentifiers.Settings.Passwords.onboardingContinue])
-        app.buttons[AccessibilityIdentifiers.Settings.Passwords.onboardingContinue].tap()
+        // Press continue button on the password onboarding if it's shown
+        if app.buttons[AccessibilityIdentifiers.Settings.Passwords.onboardingContinue].exists {
+            app.buttons[AccessibilityIdentifiers.Settings.Passwords.onboardingContinue].tap()
+        }
 
         let passcodeInput = springboard.secureTextFields.firstMatch
         waitForExistence(passcodeInput, timeout: 30)
