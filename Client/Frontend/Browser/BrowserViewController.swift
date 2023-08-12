2f405d6a14e105d402659f2197003963c07af994
lmarceau <lmarceau@mozilla.com>
2023-08-11 21:58:45 -0400

    Add FXIOS-7205 [v116] Adding other logs related to tabs loss investigation (#16027)
    * Adding other logs related to tabs loss investigation
    
    * Oups
    
    * adjust comment
    
    (cherry picked from commit 60138aab9b58db0646843ad7b0c7f99d3f8c24d4)

--- Client/Frontend/Browser/BrowserViewController.swift
+++ Client/Frontend/Browser/BrowserViewController.swift
@@ -1700,6 +1700,10 @@ class BrowserViewController: UIViewController,
 
         // Credit card sync telemetry
         self.profile.hasSyncAccount { [unowned self] hasSync in
+            logger.log("User has sync account setup \(hasSync)",
+                       level: .debug,
+                       category: .setup)
+
             guard hasSync else { return }
             let syncStatus = self.profile.syncManager.checkCreditCardEngineEnablement()
             TelemetryWrapper.recordEvent(
