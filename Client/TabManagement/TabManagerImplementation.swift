09062c8b6941252c80983faea5f138f802491f1b
Orla <omitchell@mozilla.com>
2023-08-13 13:11:32 -0400

    Add a safety check to ensure a migration is run

--- Client/TabManagement/TabManagerImplementation.swift
+++ Client/TabManagement/TabManagerImplementation.swift
@@ -97,6 +97,12 @@ class TabManagerImplementation: LegacyTabManager, Notifiable {
             await buildTabRestore(window: await self.tabDataStore.fetchWindowData())
             logger.log("Tabs restore ended after fetching window data", level: .debug, category: .tabs)
             logger.log("Normal tabs count; \(normalTabs.count), Inactive tabs count; \(inactiveTabs.count), Private tabs count; \(privateTabs).count", level: .debug, category: .tabs)
+
+            // Safety check incase something went wrong during launch where a migration should have occured
+            if tabs.count <= 1 && store.tabs.count > 1 {
+                isRestoringTabs = true
+                migrateAndRestore()
+            }
         }
     }
 
@@ -209,10 +215,10 @@ class TabManagerImplementation: LegacyTabManager, Notifiable {
         // If there are no tabs then we do not need to preserve as we are initalizing the app
         // (there should always be at least a homepage)
         // Also if the a restore is in progress then presevering tabs could cause issues
-        if tabs.count == 0 || isRestoringTabs {
+        if tabs.isEmpty || isRestoringTabs {
             return
         }
-        
+
         // For now we want to continue writing to both data stores so that we can revert to the old system if needed
         super.preserveTabs()
         guard shouldUseNewTabStore() else { return }
