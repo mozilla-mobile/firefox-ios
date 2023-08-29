a7af7b017c9fe2dff32762262a41e9471834971c
dragosb01 <dbigu@mozilla.com>
2023-08-29 15:21:08 +0300

    Add MTE-1464 [V119] search engine logo test

--- Tests/XCUITests/UrlBarTests.swift
+++ Tests/XCUITests/UrlBarTests.swift
@@ -3,6 +3,8 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
 import Foundation
+let iPadSearchIcon = XCUIApplication().children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element(boundBy: 1).children(matching: .other).element(boundBy: 1).children(matching: .image).element
+let iPhoneSearchIcon = XCUIApplication().children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element(boundBy: 1).children(matching: .other).element.children(matching: .image).element
 
 class UrlBarTests: BaseTestCase {
     // https://testrail.stage.mozaws.net/index.php?/cases/view/2189026
@@ -23,5 +25,53 @@ class UrlBarTests: BaseTestCase {
         navigator.performAction(Action.OpenNewTabFromTabTray)
         // The URL bar is empty on the new tab
         XCTAssertEqual(app.textFields["url"].value as! String, "Search or enter address")
+    }
+
+    // https://testrail.stage.mozaws.net/index.php?/cases/view/1286320
+    func testSearchEngineLogo() {
+        tapUrlBarValidateKeyboardAndIcon()
+        // Type a search term and hit "go"
+        typeSearchTermAndHitGo(searchTerm: "Firefox")
+        // The search is conducted correctly trough the default search engine
+        waitForValueContains(app.textFields["url"], value: "google")
+        // Add a custom search engine and add it as default search engine
+        navigator.goto(SearchSettings)
+        let defaultSearchEngine = app.tables.cells.element(boundBy: 0)
+        waitForExistence(app.tables.cells.staticTexts[defaultSearchEngine1])
+        defaultSearchEngine.tap()
+        app.tables.staticTexts[defaultSearchEngine2].tap()
+        waitForExistence(app.tables.cells.staticTexts[defaultSearchEngine2])
+        navigator.goto(SettingsScreen)
+        app.navigationBars.buttons["Done"].tap()
+        app.buttons[AccessibilityIdentifiers.Toolbar.homeButton].tap()
+        tapUrlBarValidateKeyboardAndIcon()
+        typeSearchTermAndHitGo(searchTerm: "Firefox")
+        // The search is conducted correctly trough the default search engine
+        waitForValueContains(app.textFields["url"], value: "amazon")
+    }
+
+    private func tapUrlBarValidateKeyboardAndIcon() {
+        // Tap on the URL bar
+        waitForTabsButton()
+        app.buttons[AccessibilityIdentifiers.Toolbar.searchButton].tap()
+        // The keyboard pops up and the default search icon is correctly displayed in the URL bar
+        let addressBar = app.textFields["address"]
+        XCTAssertTrue(addressBar.value(forKey: "hasKeyboardFocus") as? Bool ?? false)
+        let keyboardCount = app.keyboards.count
+        XCTAssert(keyboardCount > 0, "The keyboard is not shown")
+        if iPad() {
+            XCTAssertTrue(iPadSearchIcon.exists)
+            XCTAssertTrue(iPadSearchIcon.isLeftOf(rightElement: addressBar))
+        } else {
+            XCTAssertTrue(iPhoneSearchIcon.exists)
+            XCTAssertTrue(iPhoneSearchIcon.isLeftOf(rightElement: addressBar))
+        }
+    }
+
+    private func typeSearchTermAndHitGo(searchTerm: String) {
+        app.textFields["address"].typeText(searchTerm)
+        waitUntilPageLoad()
+        waitForExistence(app.buttons["Go"])
+        app.buttons["Go"].tap()
     }
 }
