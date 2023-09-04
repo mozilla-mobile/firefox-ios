2b71971f660fb8ca5b25b35aa9f20f0592d4ba86
Litianu Razvan <litianu.razvan@gmail.com>
2023-09-04 16:40:49 +0300

    Add FXIOS-7242 [v119] #16064 Improve error card with accessibility sizes (#16215)
    * FXIOS-7242 #16064 Improve error card with accessibility sizes
    
    * Rename to iconImageHeightConstraint

--- Client/Frontend/Fakespot/FakespotViewController.swift
+++ Client/Frontend/Fakespot/FakespotViewController.swift
@@ -114,7 +114,7 @@ class FakespotViewController: UIViewController, Themeable, UIAdaptivePresentatio
         let errorCardViewModel = FakespotErrorCardViewModel(title: .Shopping.ErrorCardTitle,
                                                             description: .Shopping.ErrorCardDescription,
                                                             actionTitle: .Shopping.ErrorCardButtonText)
-        errorCardView.configure(viewModel: errorCardViewModel)
+        errorCardView.configure(errorCardViewModel)
 
         let highlightsCardViewModel = HighlightsCardViewModel(
             footerTitle: .Shopping.HighlightsCardFooterText,
