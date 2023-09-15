b5557f6a167fdbb7a980a651ead792f502e841f3
PARAIPAN SORIN <paraipan.sorin1993@gmail.com>
2023-09-15 15:10:41 +0300

    Move shoppingLarge for alphabetic order

--- BrowserKit/Sources/Common/Constants/StandardImageIndentifiers.swift
+++ BrowserKit/Sources/Common/Constants/StandardImageIndentifiers.swift
@@ -64,8 +64,8 @@ public struct StandardImageIdentifiers {
         public static let price = "priceLarge"
         public static let quality = "qualityLarge"
         public static let qrCode = "qrCodeLarge"
-        public static let shopping = "shoppingLarge"
         public static let shipping = "shippingLarge"
+        public static let shopping = "shoppingLarge"
         public static let tabTray = "tabTrayLarge"
         public static let warningFill = "warningFillLarge"
     }
