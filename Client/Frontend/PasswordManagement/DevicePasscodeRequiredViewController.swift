5b2af5a287738755bbc88090f4f5c6bdbceee23d
Akshat Chaturvedi <akshatchaturvedi422@gmail.com>
2023-09-17 15:56:38 +0530

    Refactor FXIOS-6340 [v119] Replaced DeviceFontExtraLarge with preferredFont #14260

--- Client/Frontend/PasswordManagement/DevicePasscodeRequiredViewController.swift
+++ Client/Frontend/PasswordManagement/DevicePasscodeRequiredViewController.swift
@@ -9,7 +9,7 @@ class DevicePasscodeRequiredViewController: SettingsViewController {
     private var warningLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.font = LegacyDynamicFontHelper().DeviceFontExtraLarge
+        label.font = DefaultDynamicFontHelper.preferredFont(withTextStyle: .body, size: 17)
         label.text = .LoginsDevicePasscodeRequiredMessage
         label.textAlignment = .center
         label.numberOfLines = 0
@@ -21,7 +21,7 @@ class DevicePasscodeRequiredViewController: SettingsViewController {
         button.translatesAutoresizingMaskIntoConstraints = false
         button.setTitle(.LoginsDevicePasscodeRequiredLearnMoreButtonTitle, for: .normal)
         button.addTarget(self, action: #selector(learnMoreButtonTapped), for: .touchUpInside)
-        button.titleLabel?.font = LegacyDynamicFontHelper().DeviceFontExtraLarge
+        button.titleLabel?.font = DefaultDynamicFontHelper.preferredFont(withTextStyle: .body, size: 17)
         return button
     }()
 
