f9fe36935ffa05f34e552ef41a10da9c4fa798b9
Akshat Chaturvedi <akshatchaturvedi422@gmail.com>
2023-09-17 15:22:27 +0530

    Refactor FXIOS-6342 [v119] Replaced DeviceFontSmall with preferredFont #14262

--- Client/Helpers/LegacyDynamicFontHelper.swift
+++ Client/Helpers/LegacyDynamicFontHelper.swift
@@ -45,9 +45,6 @@ class LegacyDynamicFontHelper: NSObject {
     var DeviceFont: UIFont {
         return UIFont.systemFont(ofSize: deviceFontSize, weight: UIFont.Weight.medium)
     }
-    var DeviceFontSmall: UIFont {
-        return UIFont.systemFont(ofSize: deviceFontSize - 1, weight: UIFont.Weight.medium)
-    }
     var DeviceFontSmallLight: UIFont {
         return UIFont.systemFont(ofSize: deviceFontSize - 1, weight: UIFont.Weight.light)
     }
