d33d0104b175812eabaa0ad5bc58f9411ea3dbfe
Akshat Chaturvedi <akshatchaturvedi422@gmail.com>
2023-09-18 19:44:30 +0530

    Refactor FXIOS-6345 [v119] Replaced DefaultSmallFont with preferredFont mozilla-mobile#14265

--- Client/Helpers/LegacyDynamicFontHelper.swift
+++ Client/Helpers/LegacyDynamicFontHelper.swift
@@ -88,9 +88,6 @@ class LegacyDynamicFontHelper: NSObject {
      * Small
      */
     fileprivate var defaultSmallFontSize: CGFloat
-    var DefaultSmallFont: UIFont {
-        return UIFont.systemFont(ofSize: defaultSmallFontSize, weight: UIFont.Weight.regular)
-    }
     var DefaultSmallFontBold: UIFont {
         return UIFont.boldSystemFont(ofSize: defaultSmallFontSize)
     }
