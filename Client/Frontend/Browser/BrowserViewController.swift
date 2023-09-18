2c8d39aa4820c519d135f21105a411e1d94b1e44
jnrahme <jnrahme@gmail.com>
2023-09-18 08:28:02 -0400

    Add FXIOS-7406 [v119] Fixed line length (#16419)

--- Client/Frontend/Browser/BrowserViewController.swift
+++ Client/Frontend/Browser/BrowserViewController.swift
@@ -1084,7 +1084,10 @@ class BrowserViewController: UIViewController,
 
         let isPrivate = tabManager.selectedTab?.isPrivate ?? false
         let searchViewModel = SearchViewModel(isPrivate: isPrivate, isBottomSearchBar: isBottomSearchBar)
-        let searchController = SearchViewController(profile: profile, viewModel: searchViewModel, model: profile.searchEngines, tabManager: tabManager)
+        let searchController = SearchViewController(profile: profile,
+                                                    viewModel: searchViewModel,
+                                                    model: profile.searchEngines,
+                                                    tabManager: tabManager)
         searchController.searchEngines = profile.searchEngines
         searchController.searchDelegate = self
 
