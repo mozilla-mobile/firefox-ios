c20e51e301d8f39db5833ba528a76347313753e7
isabelrios <isabelrios@gmail.com>
2023-09-19 09:39:08 +0200

    Refactor MTE-15510 [v119] Logins TestRail matching (#16435)
    * Refactor MTE-15510 [v119] Logins TestRail matching
    
    * error in smoketest4
    
    * comment assertion as investigation required

--- Tests/XCUITests/LoginsTests.swift
+++ Tests/XCUITests/LoginsTests.swift
@@ -2,6 +2,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
+import Common
+import Foundation
 import XCTest
 
 let domain = "http://localhost:\(serverPort)"
@@ -19,7 +21,7 @@ let loginsListPasswordLabel = "Password"
 let defaultNumRowsLoginsList = 2
 let defaultNumRowsEmptyFilterList = 0
 
-class SaveLoginTest: BaseTestCase {
+class LoginTest: BaseTestCase {
     private func saveLogin(givenUrl: String) {
         navigator.openURL(givenUrl)
         waitUntilPageLoad()
@@ -39,6 +41,17 @@ class SaveLoginTest: BaseTestCase {
         waitForExistence(app.tables["Login List"])
     }
 
+    private func openLoginsSettingsFromBrowserTab() {
+        waitForExistence(app.buttons["TabToolbar.menuButton"], timeout: TIMEOUT)
+        navigator.goto(BrowserTabMenu)
+        waitForExistence(app.tables.otherElements[StandardImageIdentifiers.Large.login], timeout: 5)
+        navigator.goto(LoginsSettings)
+
+        unlockLoginsView()
+        waitForExistence(app.tables["Login List"])
+        navigator.nowAt(LoginsSettings)
+    }
+
     private func unlockLoginsView() {
         // Press continue button on the password onboarding if it's shown
         if app.buttons[AccessibilityIdentifiers.Settings.Passwords.onboardingContinue].exists {
@@ -51,6 +64,7 @@ class SaveLoginTest: BaseTestCase {
         passcodeInput.typeText("foo\n")
     }
 
+    // https://testrail.stage.mozaws.net/index.php?/cases/view/2288345
     func testLoginsListFromBrowserTabMenu() {
         closeURLBar()
         // Make sure you can access empty Login List from Browser Tab Menu
@@ -72,22 +86,35 @@ class SaveLoginTest: BaseTestCase {
         XCTAssertEqual(app.tables["Login List"].cells.count, defaultNumRowsLoginsList + 1)
     }
 
+    // https://testrail.stage.mozaws.net/index.php?/cases/view/52768
     // Smoketest
     func testSaveLogin() {
         closeURLBar()
         // Initially the login list should be empty
-        openLoginsSettings()
+        openLoginsSettingsFromBrowserTab()
         XCTAssertEqual(app.tables["Login List"].cells.count, defaultNumRowsLoginsList)
-        // Save a login and check that it appears on the list
+        // Save a login and check that it appears on the list from BrowserTabMenu
+        app.buttons["Settings"].tap()
+        navigator.nowAt(SettingsScreen)
+        waitForExistence(app.buttons["Done"])
+        app.buttons["Done"].tap()
+        navigator.nowAt(HomePanelsScreen)
+
         saveLogin(givenUrl: testLoginPage)
-        openLoginsSettings()
+        openLoginsSettingsFromBrowserTab()
         waitForExistence(app.tables["Login List"])
         XCTAssertTrue(app.staticTexts[domain].exists)
         // XCTAssertTrue(app.staticTexts[domainLogin].exists)
         XCTAssertEqual(app.tables["Login List"].cells.count, defaultNumRowsLoginsList + 1)
         // Check to see how it works with multiple entries in the list- in this case, two for now
+        app.buttons["Settings"].tap()
+        navigator.nowAt(SettingsScreen)
+        waitForExistence(app.buttons["Done"])
+        app.buttons["Done"].tap()
+
+        navigator.nowAt(HomePanelsScreen)
         saveLogin(givenUrl: testSecondLoginPage)
-        openLoginsSettings()
+        openLoginsSettingsFromBrowserTab()
         waitForExistence(app.tables["Login List"])
         XCTAssertTrue(app.staticTexts[domain].exists)
         // XCTAssertTrue(app.staticTexts[domainSecondLogin].exists)
@@ -99,6 +126,7 @@ class SaveLoginTest: BaseTestCase {
         }
     }
 
+    // https://testrail.stage.mozaws.net/index.php?/cases/view/2288353
     func testDoNotSaveLogin() {
         navigator.openURL(testLoginPage)
         waitUntilPageLoad()
@@ -111,7 +139,7 @@ class SaveLoginTest: BaseTestCase {
         XCTAssertEqual(app.tables["Login List"].cells.count, defaultNumRowsLoginsList)
     }
 
-    // Smoketest
+    // https://testrail.stage.mozaws.net/index.php?/cases/view/2288348
     func testSavedLoginSelectUnselect() {
         saveLogin(givenUrl: testLoginPage)
         navigator.goto(SettingsScreen)
@@ -131,6 +159,7 @@ class SaveLoginTest: BaseTestCase {
         XCTAssertTrue(app.buttons["Delete"].exists)
     }
 
+    // https://testrail.stage.mozaws.net/index.php?/cases/view/2288349
     func testDeleteLogin() {
         saveLogin(givenUrl: testLoginPage)
         openLoginsSettings()
@@ -144,19 +173,24 @@ class SaveLoginTest: BaseTestCase {
         XCTAssertEqual(app.tables["Login List"].cells.count, defaultNumRowsLoginsList)
     }
 
-//    func testEditOneLoginEntry() {
-//        saveLogin(givenUrl: testLoginPage)
-//        openLoginsSettings()
-//        XCTAssertTrue(app.staticTexts[domain].exists)
-//        XCTAssertTrue(app.staticTexts[domainLogin].exists)
-//        app.staticTexts[domain].tap()
-//        waitForExistence(app.tables["Login Detail List"])
-//        XCTAssertTrue(app.tables.cells[loginsListURLLabel].exists)
-//        XCTAssertTrue(app.tables.cells[loginsListUsernameLabel].exists)
-//        XCTAssertTrue(app.tables.cells[loginsListPasswordLabel].exists)
-//        XCTAssertTrue(app.tables.cells.staticTexts["Delete"].exists)
-//    }
+    // https://testrail.stage.mozaws.net/index.php?/cases/view/2288354
+    func testEditOneLoginEntry() throws {
+        throw XCTSkip("This test has been disabled for some time now, investigation required")
+            /*
+            saveLogin(givenUrl: testLoginPage)
+            openLoginsSettings()
+            XCTAssertTrue(app.staticTexts[domain].exists)
+            XCTAssertTrue(app.staticTexts[domainLogin].exists)
+            app.staticTexts[domain].tap()
+            waitForExistence(app.tables["Login Detail List"])
+            XCTAssertTrue(app.tables.cells[loginsListURLLabel].exists)
+            XCTAssertTrue(app.tables.cells[loginsListUsernameLabel].exists)
+            XCTAssertTrue(app.tables.cells[loginsListPasswordLabel].exists)
+            XCTAssertTrue(app.tables.cells.staticTexts["Delete"].exists)
+            */
+    }
 
+    // https://testrail.stage.mozaws.net/index.php?/cases/view/2288351
     func testSearchLogin() {
         saveLogin(givenUrl: testLoginPage)
         openLoginsSettings()
@@ -176,6 +210,7 @@ class SaveLoginTest: BaseTestCase {
         XCTAssertEqual(app.tables["Login List"].cells.count, defaultNumRowsLoginsList + 1)
     }
 
+    // https://testrail.stage.mozaws.net/index.php?/cases/view/52769
     // Smoketest
     func testSavedLoginAutofilled() {
         navigator.openURL(urlLogin)
@@ -204,12 +239,14 @@ class SaveLoginTest: BaseTestCase {
         navigator.openURL(urlLogin)
         waitUntilPageLoad()
         waitForExistence(app.webViews.textFields.element(boundBy: 0), timeout: 3)
+        // Investigate why locally the data is shown but not on TR
         // let emailValue = app.webViews.textFields.element(boundBy: 0).value!
         // XCTAssertEqual(emailValue as! String, mailLogin)
         // let passwordValue = app.webViews.secureTextFields.element(boundBy: 0).value!
         // XCTAssertEqual(passwordValue as! String, "••••••••")
     }
 
+    // https://testrail.stage.mozaws.net/index.php?/cases/view/1468479
     // Smoketest
     func testCreateLoginManually() {
         closeURLBar()
