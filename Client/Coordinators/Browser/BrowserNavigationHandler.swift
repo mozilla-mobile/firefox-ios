699e971090685f68d54768f73505632c99d526ea
yoanarios <yriosdiaz@mozilla.com>
2023-09-20 16:26:05 -0400

    Add documentation for protocol func

--- Client/Coordinators/Browser/BrowserNavigationHandler.swift
+++ Client/Coordinators/Browser/BrowserNavigationHandler.swift
@@ -44,6 +44,7 @@ protocol BrowserNavigationHandler: AnyObject {
     /// Shows authentication view controller to authorize access to sensitive data.
     func showRequiredPassCode()
 
+    /// Shows the Tab Tray View Controller 
     func showTabTray()
 }
 
