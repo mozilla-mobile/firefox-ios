84556c00052530d5bd8ad4fac7a2d25ef205132c
Winnie Teichmann <4530+thatswinnie@users.noreply.github.com>
2023-09-25 10:26:01 +0200

    Add comment

--- Client/Frontend/Fakespot/FakespotCoordinator.swift
+++ Client/Frontend/Fakespot/FakespotCoordinator.swift
@@ -38,6 +38,7 @@ class FakespotCoordinator: BaseCoordinator, FakespotViewControllerDelegate, Feat
             if let sheet = fakespotViewController.sheetPresentationController {
                 sheet.detents = [.medium(), .large()]
 
+                // Show onboarding in full height
                 if !isOptedIn {
                     sheet.selectedDetentIdentifier = .large
                 }
