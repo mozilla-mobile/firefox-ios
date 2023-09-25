5f4a866d271ea0b27e336881994887b0b3325ceb
Benjamin Forehand Jr <bforehand@mozilla.com>
2023-09-25 15:57:03 -0500

    Add EXP-3823 - Add nimbus survey message test - Landscape mode

--- Tests/ExperimentIntegrationTests/test_integration.py
+++ Tests/ExperimentIntegrationTests/test_integration.py
@@ -20,3 +20,13 @@ def test_survey_no_thanks_navigates_correctly(xcodebuild, setup_experiment, star
     xcodebuild.test(
         "XCUITests/ExperimentIntegrationTests/testMessageNoThanksNavigatesCorrectly", erase=False
     )
+
+@pytest.mark.parametrize("load_branches", [("branch")], indirect=True)
+def test_survey_landscape_looks_correct(xcodebuild, setup_experiment, start_app, load_branches):
+    xcodebuild.install()
+    setup_experiment(load_branches)
+    xcodebuild.test("XCUITests/ExperimentIntegrationTests/testVerifyExperimentEnrolled", erase=False)
+    start_app()
+    xcodebuild.test(
+        "XCUITests/ExperimentIntegrationTests/testMessageLandscapeUILooksCorrect", erase=False
+    )
