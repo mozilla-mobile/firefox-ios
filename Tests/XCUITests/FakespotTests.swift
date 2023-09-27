d01f30a9edabbcff82707f0f1443d3c47a9f930c
Clare So <clare.m.so@gmail.com>
2023-09-27 07:54:23 -0400

    Update Tests/XCUITests/FakespotTests.swift
    Co-authored-by: Winnie Teichmann <4530+thatswinnie@users.noreply.github.com>

--- Tests/XCUITests/FakespotTests.swift
+++ Tests/XCUITests/FakespotTests.swift
@@ -28,7 +28,7 @@ class FakespotTests: BaseTestCase {
             mozWaitForElementToExist(app.buttons[AccessibilityIdentifiers.Toolbar.shoppingButton])
             app.buttons[AccessibilityIdentifiers.Toolbar.shoppingButton].tap()
             mozWaitForElementToExist(app.staticTexts[AccessibilityIdentifiers.Shopping.sheetHeaderTitle])
-            XCTAssertEqual(app.staticTexts[AccessibilityIdentifiers.Shopping.sheetHeaderTitle].label, "Review checker")
+            XCTAssertEqual(app.staticTexts[AccessibilityIdentifiers.Shopping.sheetHeaderTitle].label, String.Shopping.SheetHeaderTitle)
 
             // Close the popover
             app.otherElements.buttons[AccessibilityIdentifiers.Shopping.sheetCloseButton].tap()
