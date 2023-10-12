0e9196e8e20c0b7e67f95dc3bb2614287abecfe2
Laurie Marceau <lmarceau@mozilla.com>
2023-10-12 08:26:54 -0400

    Fix swiftlint

--- Tests/ClientTests/Mocks/MockCoordinator.swift
+++ Tests/ClientTests/Mocks/MockCoordinator.swift
@@ -12,7 +12,7 @@ class MockCoordinator: Coordinator {
     var router: Router
     var savedRoute: Route?
     var logger: Logger = MockLogger()
-    var isDismissable: Bool = true
+    var isDismissable = true
 
     var addChildCalled = 0
     var removedChildCalled = 0
