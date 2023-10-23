201256e9cb523b344565d5e5a1f5b0cfbec8f767
Litianu Razvan <litianu.razvan@gmail.com>
2023-10-23 13:37:40 +0300

    Add FXIOS-7611 [v120] Add proposed changes to “unprocessable” status (#16938)

--- Client/Frontend/Fakespot/Client/ProductAnalyzeResponse.swift
+++ Client/Frontend/Fakespot/Client/ProductAnalyzeResponse.swift
@@ -4,16 +4,34 @@
 
 import Foundation
 
-/// Enumeration representing different analysis statuses.
+/// Enumeration representing the various analysis statuses for a product.
 enum AnalysisStatus: String, Decodable {
+    /// The analysis is pending and has not yet started.
     case pending = "pending"
+
+    /// The analysis job is in progress, indicating it is currently being processed.
     case inProgress = "in_progress"
+
+    /// The analysis has been completed successfully.
     case completed = "completed"
+
+    /// The product cannot be analyzed due to eligibility constraints.
     case notAnalyzable = "not_analyzable"
+
+    /// The current analysis status with provided parameters is not found.
     case notFound = "not_found"
+
+    /// The product cannot be analyzed with the provided data, often due to issues like an incorrect website or product ID.
     case unprocessable = "unprocessable"
+
+    /// The analysis is stale, returned if the analysis is not finished within 3 minutes.
     case stale = "stale"
+
+    /// The product cannot be analyzed due to an insufficient number of reviews.
     case notEnoughReviews = "not_enough_reviews"
+
+    /// The system does not support analyses for the provided product.
+    case pageNotSupported = "page_not_supported"
 
     var isAnalyzing: Bool {
         switch self {
