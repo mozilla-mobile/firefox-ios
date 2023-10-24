f30b99733f13ae34189503a1c2bf44f95affb3e7
Litianu Razvan <litianu.razvan@gmail.com>
2023-10-24 11:01:50 +0300

    Refactor FXIOS-7612 [v121] Update-strings (#16979)

--- Client/Frontend/Strings.swift
+++ Client/Frontend/Strings.swift
@@ -3884,7 +3884,7 @@ extension String {
         public static let OptInCardFirstParagraph = MZLocalizedString(
             key: "Shopping.OptInCard.FirstParagraph.Description.v120",
             tableName: "Shopping",
-            value: "See how reliable product reviews are on %1$@ before you buy. Review checker, an experimental feature from %2$@, is built right into the browser. It works on %3$@ and  %4$@, too.",
+            value: "See how reliable product reviews are on %1$@ before you buy. Review checker, an experimental feature from %2$@, is built right into the browser. It works on %3$@ and %4$@, too.",
             comment: "Label for the first paragraph of the Shopping Experience Opt In onboarding Card (Fakespot). The first parameter will be the website the user is coming from when viewing this screen (default Amazon). The second parameter will be replaced by the app name. The third and fourth parameters will be the other two websites that are currently supported (Amazon, Best Buy or Walmart) besides the one used for the first parameter.")
         public static let OptInCardSecondParagraph = MZLocalizedString(
             key: "Shopping.OptInCard.SecondParagraph.Description.v120",
