15b19b5a7a65a5ac04122da180d6d48bc90c3a20
yoanarios <yriosdiaz@mozilla.com>
2023-10-25 11:37:42 -0400

    Add unit test + fix PR feedback

--- Client/Frontend/Browser/Tabs/Legacy/LegacyTabTrayViewModel.swift
+++ Client/Frontend/Browser/Tabs/Legacy/LegacyTabTrayViewModel.swift
@@ -6,45 +6,6 @@ import Common
 import Shared
 import Storage
 
-enum TabTrayPanelType: Int, CaseIterable {
-    case tabs
-    case privateTabs
-    case syncedTabs
-
-    var navTitle: String {
-        switch self {
-        case .tabs:
-            return .TabTrayV2Title
-        case .privateTabs:
-            return .TabTrayPrivateBrowsingTitle
-        case .syncedTabs:
-            return .AppMenu.AppMenuSyncedTabsTitleString
-        }
-    }
-
-    var label: String {
-        switch self {
-        case .tabs:
-            return String.TabTraySegmentedControlTitlesTabs
-        case .privateTabs:
-            return String.TabTraySegmentedControlTitlesPrivateTabs
-        case .syncedTabs:
-            return String.TabTraySegmentedControlTitlesSyncedTabs
-        }
-    }
-
-    var image: UIImage? {
-        switch self {
-        case .tabs:
-            return UIImage(named: ImageIdentifiers.navTabCounter)
-        case .privateTabs:
-            return UIImage(named: ImageIdentifiers.privateMaskSmall)
-        case .syncedTabs:
-            return UIImage(named: ImageIdentifiers.syncedDevicesIcon)
-        }
-    }
-}
-
 class LegacyTabTrayViewModel {
     enum Layout: Equatable {
         case regular // iPad
--- Tests/ClientTests/Coordinators/Mocks/MockBrowserCoordinator.swift
+++ Tests/ClientTests/Coordinators/Mocks/MockBrowserCoordinator.swift
@@ -48,7 +48,7 @@ class MockBrowserCoordinator: BrowserNavigationHandler, ParentCoordinatorDelegat
         showEnhancedTrackingProtectionCalled += 1
     }
 
-    func showTabTray(selectedPanel: Client.TabTrayPanelType) {
+    func showTabTray(selectedPanel: TabTrayPanelType) {
         showTabTrayCalled += 1
     }
 
