410af55a0a7b54d6f960aabcfefd287a1db750ed
dragosb01 <dbigu@mozilla.com>
2023-10-30 12:07:09 +0200

    Refactor MTE-1814 [v121] test suites with TestRail

--- Tests/XCUITests/SettingsTests.swift
+++ Tests/XCUITests/SettingsTests.swift
@@ -5,6 +5,16 @@
 import XCTest
 
 class SettingsTests: BaseTestCase {
+    private func checkShowImages(showImages: Bool = true) {
+        let noImageStatusMode = app.otherElements.tables.cells.switches["NoImageModeStatus"]
+        mozWaitForElementToExist(noImageStatusMode)
+        if showImages {
+            XCTAssertEqual(noImageStatusMode.value as! String, "0")
+        } else {
+            XCTAssertEqual(noImageStatusMode.value as! String, "1")
+        }
+    }
+
     // https://testrail.stage.mozaws.net/index.php?/cases/view/2334757
     func testHelpOpensSUMOInTab() {
         navigator.nowAt(NewTabScreen)
@@ -59,5 +69,60 @@ class SettingsTests: BaseTestCase {
         // Check Offer to open copied links, when opening firefox is on
         let value3 = app.tables.cells.switches["Offer to Open Copied Links, When Opening Firefox"].value
         XCTAssertEqual(value3 as? String, "1")
+    }
+
+    // https://testrail.stage.mozaws.net/index.php?/cases/view/2307041
+    func testOpenMailAppSettings() {
+        waitForTabsButton()
+        navigator.nowAt(NewTabScreen)
+        navigator.goto(OpenWithSettings)
+
+        // Check that the list is shown
+        mozWaitForElementToExist(app.tables["OpenWithPage.Setting.Options"])
+        XCTAssertTrue(app.tables["OpenWithPage.Setting.Options"].exists)
+
+        // Check that the list is shown with all elements disabled
+        XCTAssertTrue(app.tables.staticTexts["OPEN MAIL LINKS WITH"].exists)
+        XCTAssertFalse(app.tables.cells.staticTexts["Mail"].isSelected)
+        XCTAssertFalse(app.tables.cells.staticTexts["Outlook"].isSelected)
+        XCTAssertFalse(app.tables.cells.staticTexts["ProtonMail"].isSelected)
+        XCTAssertFalse(app.tables.cells.staticTexts["Airmail"].isSelected)
+        XCTAssertFalse(app.tables.cells.staticTexts["myMail"].isSelected)
+        XCTAssertFalse(app.tables.cells.staticTexts["Spark"].isSelected)
+        XCTAssertFalse(app.tables.cells.staticTexts["YMail!"].isSelected)
+        XCTAssertFalse(app.tables.cells.staticTexts["Gmail"].isSelected)
+        XCTAssertFalse(app.tables.cells.staticTexts["Fastmail"].isSelected)
+
+        // Check that tapping on an element does nothing
+        mozWaitForElementToExist(app.tables["OpenWithPage.Setting.Options"])
+        app.tables.cells.staticTexts["Airmail"].tap()
+        XCTAssertFalse(app.tables.cells.staticTexts["Airmail"].isSelected)
+
+        // Check that user can go back from that setting
+        navigator.nowAt(OpenWithSettings)
+        navigator.goto(SettingsScreen)
+    }
+
+    // https://testrail.stage.mozaws.net/index.php?/cases/view/2307058
+    // Functionality is tested by UITests/NoImageModeTests, here only the UI is updated properly
+    // SmokeTest
+    func testImageOnOff() {
+        // Select no images or hide images, check it's hidden or not
+        waitUntilPageLoad()
+
+        // Select hide images
+        let blockImagesSwitch = app.otherElements.tables.cells.switches[AccessibilityIdentifiers.Settings.BlockImages.title]
+        navigator.goto(SettingsScreen)
+        navigator.nowAt(SettingsScreen)
+        mozWaitForElementToExist(blockImagesSwitch)
+        navigator.performAction(Action.ToggleNoImageMode)
+        checkShowImages(showImages: false)
+
+        // Select show images
+        navigator.goto(SettingsScreen)
+        navigator.nowAt(SettingsScreen)
+        mozWaitForElementToExist(blockImagesSwitch)
+        navigator.performAction(Action.ToggleNoImageMode)
+        checkShowImages(showImages: true)
     }
 }
