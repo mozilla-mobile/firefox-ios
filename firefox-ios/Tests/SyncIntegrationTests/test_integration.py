8a328fa2f481984c1fba37865577e82cbb9e22b2
isabelrios <isabelrios@gmail.com>
2023-11-08 11:29:40 +0100

    correct path to js files

--- firefox-ios/Tests/SyncIntegrationTests/test_integration.py
+++ firefox-ios/Tests/SyncIntegrationTests/test_integration.py
@@ -1,38 +1,40 @@
 
 def test_sync_bookmark_from_device(tps, xcodebuild):
     xcodebuild.test('XCUITests/IntegrationTests/testFxASyncBookmark')
-    tps.run('firefox-ios/Tests/SyncIntegrationTests/test_bookmark.js')
+    tps.run('test_bookmark.js')
 
 def test_sync_bookmark_from_desktop(tps, xcodebuild):
-    tps.run('firefox-ios/Tests/SyncIntegrationTeststest_bookmark_desktop.js')
+    tps.run('test_bookmark_desktop.js')
     xcodebuild.test('XCUITests/IntegrationTests/testFxASyncBookmarkDesktop')
 
 def test_sync_history_from_device(tps, xcodebuild):
     xcodebuild.test('XCUITests/IntegrationTests/testFxASyncHistory')
-    tps.run('firefox-ios/Tests/SyncIntegrationTests/test_history.js')
+    tps.run('test_history.js')
 
 def test_sync_tabs_from_device(tps, xcodebuild):
     xcodebuild.test('XCUITests/IntegrationTests/testFxASyncTabs')
-    tps.run('firefox-ios/Tests/SyncIntegrationTests/test_tabs.js')
+    tps.run('test_tabs.js')
 
 def test_sync_history_from_desktop(tps, xcodebuild):
-    tps.run('firefox-ios/Tests/SyncIntegrationTests/test_history_desktop.js')
+    tps.run('test_history_desktop.js')
     xcodebuild.test('XCUITests/IntegrationTests/testFxASyncHistoryDesktop')
+
 '''
 def test_sync_logins_from_device(tps, xcodebuild):
     xcodebuild.test('XCUITests/IntegrationTests/testFxASyncLogins')
     tps.run('firefox-ios/Tests/SyncIntegrationTests/test_password.js')
 '''
+
 def test_sync_logins_from_desktop(tps, xcodebuild):
-    tps.run('firefox-ios/Tests/SyncIntegrationTests/test_password_desktop.js')
+    tps.run('test_password_desktop.js')
     xcodebuild.test('XCUITests/IntegrationTests/testFxASyncPasswordDesktop')
 
 def test_sync_tabs_from_desktop(tps, xcodebuild):
-    tps.run('firefox-ios/Tests/SyncIntegrationTests/test_tabs_desktop.js')
+    tps.run('test_tabs_desktop.js')
     xcodebuild.test('XCUITests/IntegrationTests/testFxASyncTabsDesktop')
 
 def test_sync_disconnect_connect_fxa(tps, xcodebuild):
-    tps.run('firefox-ios/Tests/SyncIntegrationTests/test_bookmark_login.js')
+    tps.run('test_bookmark_login.js')
     xcodebuild.test('XCUITests/IntegrationTests/testFxADisconnectConnect')
 
 def test_sync_china_fxa_server(xcodebuild):
