dfb6e881677490619bf73ce43f04fedd7de7a2a4
mattreaganmozilla <145381717+mattreaganmozilla@users.noreply.github.com>
2023-11-14 14:52:37 -0800

    Bugfix FXIOS-7741 [v121] Fix duplicate tabs when tapping Recently Closed item (#17282)
    * [7741] Fix issue with duplicate tabs opening when opening url from Recently Closed history panel.
    
    * [7741] Clean up per PR feedback
    
    * [7741] Minor safety change for EventQueue, allow calls to complete() to always update event status as expected, even in unexpected scenarios or in cases of API misuse.

--- Client/Frontend/Library/RecentlyClosedTabsPanel.swift
+++ Client/Frontend/Library/RecentlyClosedTabsPanel.swift
@@ -118,9 +118,21 @@ class RecentlyClosedTabsPanelSiteTableViewController: SiteTableViewController {
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        recentlyClosedTabsDelegate?.openRecentlyClosedSiteInNewTab(recentlyClosedTabs[indexPath.row].url, isPrivate: false)
-        let visitType = VisitType.typed    // Means History, too.
-        libraryPanelDelegate?.libraryPanel(didSelectURL: recentlyClosedTabs[indexPath.row].url, visitType: visitType)
+        let url = recentlyClosedTabs[indexPath.row].url
+        recentlyClosedTabsDelegate?.openRecentlyClosedSiteInNewTab(url, isPrivate: false)
+
+        // The code above creates new tab and selects it, but TabManagerImplementation.selectTab()
+        // currently performs the actual selection update asynchronously via Swift Async + Task/Await.
+        // This means that selectTab() returns before the tab is actually selected. As a result, the
+        // delegate callback below will incorrectly cause a duplicate tab (since it will treat the
+        // url as having been applied to the current tab, not our newly-added tab from above). This
+        // is avoided by making sure we wait for our expected tab above to be selected before
+        // notifying our library panel delegate. [FXIOS-7741]
+
+        AppEventQueue.wait(for: .selectTab(url)) {
+            let visitType = VisitType.typed    // Means History, too.
+            self.libraryPanelDelegate?.libraryPanel(didSelectURL: url, visitType: visitType)
+        }
     }
 
     override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
