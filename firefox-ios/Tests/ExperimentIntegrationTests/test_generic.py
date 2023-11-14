e830daa41963015cf9ad7192a495699c64ab565d
Benjamin Forehand Jr <bforehand@mozilla.com>
2023-11-14 02:46:01 -0600

    Add EXP-3965 [v121] Add unenrollment tests to experiment integration test suite. (#17251)
    * Add EXP-3965 [v121] Add unenrollment tests to experiment integration test suite.
    
    * Fix spacing.

--- firefox-ios/Tests/ExperimentIntegrationTests/test_generic.py
+++ firefox-ios/Tests/ExperimentIntegrationTests/test_generic.py
@@ -1,5 +1,17 @@
 import pytest
 
+
+@pytest.mark.parametrize("load_branches", [("branch")], indirect=True)
+def test_experiment_unenrolls_after_studies_toggle(xcodebuild, setup_experiment, start_app, load_branches):
+    xcodebuild.install()
+    setup_experiment(load_branches)
+    xcodebuild.test("XCUITests/ExperimentIntegrationTests/testVerifyExperimentEnrolled", erase=False)
+    start_app()
+    xcodebuild.test(
+        "XCUITests/ExperimentIntegrationTests/testStudiesToggleDisablesExperiment", erase=False
+    )
+
+
 @pytest.mark.parametrize("load_branches", [("branch")], indirect=True)
 def test_experiment_unenrolls_after_studies_toggle(xcodebuild, setup_experiment, start_app, load_branches):
     xcodebuild.install()
