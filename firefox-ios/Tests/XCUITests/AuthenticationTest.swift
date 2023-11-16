313ee8067efbb72e7be94e6ea58a32d73f3d3526
dragosb01 <134391433+dragosb01@users.noreply.github.com>
2023-11-16 11:16:47 +0200

    Refactor MTE-1922 [v121] Authentication test with TestRail (#17329)

--- firefox-ios/Tests/XCUITests/AuthenticationTest.swift
+++ firefox-ios/Tests/XCUITests/AuthenticationTest.swift
@@ -7,6 +7,7 @@ import XCTest
 let testBasicHTTPAuthURL = "https://jigsaw.w3.org/HTTP/Basic/"
 
 class AuthenticationTest: BaseTestCase {
+    // https://testrail.stage.mozaws.net/index.php?/cases/view/2360560
     func testBasicHTTPAuthenticationPromptVisible() {
         mozWaitForElementToExist(app.textFields["url"], timeout: 5)
         navigator.nowAt(NewTabScreen)
