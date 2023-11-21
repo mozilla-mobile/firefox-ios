2cf96a36bf04baf858010b93360fec5215305c9e
PARAIPAN SORIN <paraipan.sorin1993@gmail.com>
2023-11-21 15:03:27 +0200

    Remove condition that checks only for grid tabs to save them in order

--- Client/Frontend/Browser/TabDisplayManager.swift
+++ Client/Frontend/Browser/TabDisplayManager.swift
@@ -758,9 +758,7 @@ extension LegacyTabDisplayManager: UICollectionViewDropDelegate {
 
         filteredTabs.insert(tab, at: destinationIndexPath.item)
 
-        if tabDisplayType == .TabGrid {
-            saveRegularOrderedTabs(from: filteredTabs)
-        }
+        saveRegularOrderedTabs(from: filteredTabs)
 
         /// According to Apple's documentation the best place to make the changes to the collectionView and dataStore is
         /// during the completion call of performBatchUpdates
