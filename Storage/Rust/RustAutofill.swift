9cf2d543280ea0296bea9baa0ac195e331d84f36
jnrahme <jnrahme@gmail.com>
2023-11-21 14:48:03 -0500

    Implemented getAddress, addAddress, listAllAddresses  methods, along with corresponding test case for each

--- Storage/Rust/RustAutofill.swift
+++ Storage/Rust/RustAutofill.swift
@@ -7,28 +7,47 @@ import Shared
 @_exported import MozillaAppServices
 import Common
 
+/// Typealias for AutofillStore.
 typealias AutofillStore = Store
 
+/// Enum representing errors related to Autofill encryption keys.
 public enum AutofillEncryptionKeyError: Error {
+    /// Indicates an illegal state error.
     case illegalState
+    /// Indicates that no key was created.
     case noKeyCreated
 }
 
 public class RustAutofill {
+    /// The path to the Autofill database file.
     let databasePath: String
+    /// DispatchQueue for synchronization.
     let queue: DispatchQueue
+    /// AutofillStore instance.
     var storage: AutofillStore?
 
     private(set) var isOpen = false
     private var didAttemptToMoveToBackup = false
     private let logger: Logger
 
+    // MARK: - Initialization
+
+    /// Initializes a new RustAutofill instance.
+    ///
+    /// - Parameters:
+    ///   - databasePath: The path to the Autofill database file.
+    ///   - logger: An optional logger for recording informational and error messages. Default is shared DefaultLogger.
     public init(databasePath: String, logger: Logger = DefaultLogger.shared) {
         self.databasePath = databasePath
         queue = DispatchQueue(label: "RustAutofill queue: \(databasePath)")
         self.logger = logger
     }
 
+    // MARK: - Database Operations
+
+    /// Opens the Autofill database.
+    ///
+    /// - Returns: An optional NSError if an error occurs during the database opening process.
     internal func open() -> NSError? {
         do {
             try getStoredKey()
@@ -41,12 +60,18 @@ public class RustAutofill {
         }
     }
 
+    /// Closes the Autofill database.
+    ///
+    /// - Returns: An optional NSError if an error occurs during the database closing process.
     internal func close() -> NSError? {
         storage = nil
         isOpen = false
         return nil
     }
 
+    /// Reopens the database if it is closed.
+    ///
+    /// - Returns: An optional NSError if an error occurs during the reopening process.
     public func reopenIfClosed() -> NSError? {
         var error: NSError?
         queue.sync {
@@ -56,6 +81,9 @@ public class RustAutofill {
         return error
     }
 
+    /// Forces the database to close.
+    ///
+    /// - Returns: An optional NSError if an error occurs during the closing process.
     public func forceClose() -> NSError? {
         var error: NSError?
         queue.sync {
@@ -65,9 +93,15 @@ public class RustAutofill {
         return error
     }
 
+    /// Adds a credit card to the database.
+    ///
+    /// - Parameters:
+    ///   - creditCard: UnencryptedCreditCardFields representing the credit card to be added.
+    ///   - completion: A closure called upon completion with the added credit card or an error.
+    /// - Note: Uses a completion handler for asynchronous code execution.
     public func addCreditCard(creditCard: UnencryptedCreditCardFields, completion: @escaping (CreditCard?, Error?) -> Void) {
         performDatabaseOperation { error in
-            if let error = error {
+            guard error == nil else {
                 completion(nil, error)
                 return
             }
@@ -80,6 +114,11 @@ public class RustAutofill {
         }
     }
 
+    /// Decrypts a credit card number using RustAutofillEncryptionKeys.
+    ///
+    /// - Parameter encryptedCCNum: The encrypted credit card number.
+    /// - Returns: The decrypted credit card number or nil if the input is invalid.
+    /// - Note: Uses guard statements and optionals effectively, following Swift best practices.
     public func decryptCreditCardNumber(encryptedCCNum: String?) -> String? {
         guard let encryptedCCNum = encryptedCCNum, !encryptedCCNum.isEmpty else {
             return nil
@@ -88,13 +127,18 @@ public class RustAutofill {
         return keys.decryptCreditCardNum(encryptedCCNum: encryptedCCNum)
     }
 
+    /// Retrieves a credit card from the database by its identifier.
+    ///
+    /// - Parameters:
+    ///   - id: The identifier of the credit card.
+    ///   - completion: A closure called upon completion with the retrieved credit card or an error.
+    /// - Note: Follows the common pattern of using completion handlers for asynchronous tasks.
     public func getCreditCard(id: String, completion: @escaping (CreditCard?, Error?) -> Void) {
         performDatabaseOperation { error in
-            if let error = error {
+            guard error == nil else {
                 completion(nil, error)
                 return
             }
-
             do {
                 let record = try self.storage?.getCreditCard(guid: id)
                 completion(record, nil)
@@ -104,9 +148,13 @@ public class RustAutofill {
         }
     }
 
+    /// Retrieves all credit cards from the database.
+    ///
+    /// - Parameter completion: A closure called upon completion with the list of credit cards or an error.
+    /// - Note: Uses a completion handler for handling asynchronous code execution.
     public func listCreditCards(completion: @escaping ([CreditCard]?, Error?) -> Void) {
         performDatabaseOperation { error in
-            if let error = error {
+            guard error == nil else {
                 completion(nil, error)
                 return
             }
@@ -119,9 +167,15 @@ public class RustAutofill {
         }
     }
 
+    /// Checks for the existence of a credit card in the database.
+    ///
+    /// - Parameters:
+    ///   - cardNumber: The last four digits of the credit card.
+    ///   - completion: A closure called upon completion with the found credit card or an error.
+    /// - Note: Checks for the existence of a credit card and uses a completion handler for result reporting.
     public func checkForCreditCardExistance(cardNumber: String, completion: @escaping (CreditCard?, Error?) -> Void) {
         performDatabaseOperation { error in
-            if let error = error {
+            guard error == nil else {
                 completion(nil, error)
                 return
             }
@@ -139,9 +193,16 @@ public class RustAutofill {
         }
     }
 
+    /// Updates a credit card in the database.
+    ///
+    /// - Parameters:
+    ///   - id: The identifier of the credit card to be updated.
+    ///   - creditCard: UnencryptedCreditCardFields representing the updated credit card details.
+    ///   - completion: A closure called upon completion with a boolean indicating success and an error if any.
+    /// - Note: Updates a credit card and reports the result using a completion handler.
     public func updateCreditCard(id: String, creditCard: UnencryptedCreditCardFields, completion: @escaping (Bool, Error?) -> Void) {
         performDatabaseOperation { error in
-            if let error = error {
+            guard error == nil else {
                 completion(false, error)
                 return
             }
@@ -157,9 +218,15 @@ public class RustAutofill {
         }
     }
 
+    /// Deletes a credit card from the database.
+    ///
+    /// - Parameters:
+    ///   - id: The identifier of the credit card to be deleted.
+    ///   - completion: A closure called upon completion with a boolean indicating success and an error if any.
+    /// - Note: Deletes a credit card and reports the result using a completion handler.
     public func deleteCreditCard(id: String, completion: @escaping (Bool, Error?) -> Void) {
         performDatabaseOperation { error in
-            if let error = error {
+            guard error == nil else {
                 completion(false, error)
                 return
             }
@@ -172,9 +239,15 @@ public class RustAutofill {
         }
     }
 
+    /// Marks a credit card as used in the database.
+    ///
+    /// - Parameters:
+    ///   - creditCard: The credit card to be marked as used.
+    ///   - completion: A closure called upon completion with a boolean indicating success and an error if any.
+    /// - Note: Marks a credit card as used and reports the result using a completion handler.
     public func use(creditCard: CreditCard, completion: @escaping (Bool, Error?) -> Void) {
         performDatabaseOperation { error in
-            if let error = error {
+            guard error == nil else {
                 completion(false, error)
                 return
             }
@@ -187,9 +260,13 @@ public class RustAutofill {
         }
     }
 
+    /// Performs an operation to scrub encrypted credit card numbers from the database.
+    ///
+    /// - Parameter completion: A closure called upon completion with a boolean indicating success and an error if any.
+    /// - Note: Scrubs encrypted credit card numbers and reports the result using a completion handler.
     public func scrubCreditCardNums(completion: @escaping (Bool, Error?) -> Void) {
         performDatabaseOperation { error in
-            if let error = error {
+            guard error == nil else {
                 completion(false, error)
                 return
             }
@@ -202,12 +279,102 @@ public class RustAutofill {
         }
     }
 
+    /// Adds an address to the database.
+    ///
+    /// - Parameters:
+    ///   - address: UpdatableAddressFields representing the address to be added.
+    ///   - completion: A closure called upon completion with the added address or an error.
+    public func addAddress(address: UpdatableAddressFields, completion: @escaping (Address?, Error?) -> Void) {
+        performDatabaseOperation { error in
+            guard error == nil else {
+                completion(nil, error)
+                return
+            }
+            do {
+                // Use optional binding to safely unwrap the optional result of addAddress.
+                if let id = try self.storage?.addAddress(a: address) {
+                    // Successfully added the address, call the completion handler with the result.
+                    completion(id, nil)
+                } else {
+                    // Handle the case where addAddress returns nil, possibly due to an internal error.
+                    let internalError = NSError(domain: "YourDomain", code: 1, userInfo: [
+                        NSLocalizedDescriptionKey: "Internal error: Failed to add address."
+                    ])
+                    completion(nil, internalError)
+                }
+            } catch let err as NSError {
+                // Handle any other errors that might occur during the database operation.
+                completion(nil, err)
+            }
+        }
+    }
+
+    /// Retrieves an address from the database by its identifier.
+    ///
+    /// - Parameters:
+    ///   - id: The identifier of the address.
+    ///   - completion: A closure called upon completion with the retrieved address or an error.
+    public func getAddress(id: String, completion: @escaping (Address?, Error?) -> Void) {
+        performDatabaseOperation { error in
+            guard error == nil else {
+                completion(nil, error)
+                return
+            }
+            do {
+                // Use optional binding to safely unwrap the optional result of getAddress.
+                if let record = try self.storage?.getAddress(guid: id) {
+                    // Successfully retrieved the address, call the completion handler with the result.
+                    completion(record, nil)
+                } else {
+                    // Handle the case where getAddress returns nil, possibly due to a missing record.
+                    let missingRecordError = NSError(domain: "YourDomain", code: 2, userInfo: [
+                        NSLocalizedDescriptionKey: "Record with id \(id) not found."
+                    ])
+                    completion(nil, missingRecordError)
+                }
+            } catch let err as NSError {
+                // Handle any other errors that might occur during the database operation.
+                completion(nil, err)
+            }
+        }
+    }
+
+    /// Retrieves all addresses from the database.
+    ///
+    /// - Parameter completion: A closure called upon completion with the list of addresses or an error.
+    /// - Note: Uses a completion handler for handling asynchronous code execution.
+    public func listAllAddresses(completion: @escaping ([Address]?, Error?) -> Void) {
+        performDatabaseOperation { error in
+            guard error == nil else {
+                completion(nil, error)
+                return
+            }
+            do {
+                let records = try self.storage?.getAllAddresses()
+                completion(records, nil)
+            } catch let error {
+                completion(nil, error)
+            }
+        }
+    }
+
+    // MARK: - Sync Manager Interaction
+
+    /// Asynchronously registers the instance with a sync manager.
+    ///
+    /// - Note: Uses `async` to perform the operation asynchronously.
     public func registerWithSyncManager() {
         queue.async { [unowned self] in
             self.storage?.registerWithSyncManager()
         }
     }
 
+    // MARK: - Key Management
+
+    /// Retrieves the stored encryption key.
+    /// - Throws: An error if there is an issue retrieving the key.
+    /// - Returns: The retrieved encryption key.
+    /// - Note: Uses Swift's `throws` for error handling, providing a clear indication of potential failures.
     @discardableResult
     public func getStoredKey() throws -> String {
         let rustKeys = RustAutofillEncryptionKeys()
