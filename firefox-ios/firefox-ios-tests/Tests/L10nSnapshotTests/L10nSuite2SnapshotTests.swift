fb9d779a7f8e396ee45c7547dcb03b57abddf38c
Clare So <cso@mozilla.com>
2023-11-23 13:48:51 -0500

    Fix failures in testFakespotAvailable.

--- firefox-ios/firefox-ios-tests/Tests/L10nSnapshotTests/L10nSuite2SnapshotTests.swift
+++ firefox-ios/firefox-ios-tests/Tests/L10nSnapshotTests/L10nSuite2SnapshotTests.swift
@@ -166,7 +166,8 @@ class L10nSuite2SnapshotTests: L10nBaseSnapshotTests {
         mozWaitForElementToExist(website.textFields.firstMatch)
         website.textFields.firstMatch.tap()
         website.textFields.firstMatch.typeText("Shoe")
-        website.buttons.element(boundBy: 5).tap()
+        mozWaitForElementToExist(website.otherElements.buttons.firstMatch)
+        website.otherElements.buttons.element(boundBy: 1).tap()
         waitUntilPageLoad()
         website.images.firstMatch.tap()
 
@@ -184,11 +185,16 @@ class L10nSuite2SnapshotTests: L10nBaseSnapshotTests {
         // Tap on each option in that menu
         mozWaitForElementToExist(app.buttons["Shopping.ReviewQualityCard.ExpandButton"])
         app.buttons["Shopping.ReviewQualityCard.ExpandButton"].tap()
+        app.swipeUp(velocity: 15)
         snapshot("ReviewQualityCard")
         app.buttons["Shopping.ReviewQualityCard.ExpandButton"].tap()
 
         mozWaitForElementToExist(app.buttons["Shopping.SettingsCard.ExpandButton"])
         app.buttons["Shopping.SettingsCard.ExpandButton"].tap()
         snapshot("SettingsCard")
+
+        app.buttons["Shopping.Sheet.CloseButton"].tap()
+        app.buttons[AccessibilityIdentifiers.Toolbar.shoppingButton].tap()
+        snapshot("HalfReviewChecker")
     }
 }
