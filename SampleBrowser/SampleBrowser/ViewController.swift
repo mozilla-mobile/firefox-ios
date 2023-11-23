e4d8a80133397a456f8dfb15c3948657dca0c7b6
Laurie Marceau <lmarceau@mozilla.com>
2023-11-23 14:44:09 -0500

    Initial commit for sample browser app

---
+++ SampleBrowser/SampleBrowser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleBrowser
+//
+//  Created by Laurie Marceau on 2023-11-23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
