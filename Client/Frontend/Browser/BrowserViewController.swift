0097fb3091407e739e516bfc1603ca2f5ee50f81
OrlaM <omitchell@mozilla.com>
2023-11-27 16:02:05 -0500

    Bugfix FXIOS-7832 [v121] Enter overlay mode when opening a new tab (#17485)

--- Client/Frontend/Browser/BrowserViewController.swift
+++ Client/Frontend/Browser/BrowserViewController.swift
@@ -2470,7 +2470,7 @@ extension BrowserViewController: TopTabsDelegate {
     }
 
     func topTabsDidPressNewTab(_ isPrivate: Bool) {
-        openBlankNewTab(focusLocationField: false, isPrivate: isPrivate)
+        openBlankNewTab(focusLocationField: true, isPrivate: isPrivate)
         overlayManager.openNewTab(url: nil,
                                   newTabSettings: newTabSettings)
     }
