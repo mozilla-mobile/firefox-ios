cecd1ea7d8b2fdfd35890ea8631d6d8a09bd1cba
roux g. buciu <github@fringe.foundation>
2023-11-30 11:22:06 -0500

    Add helper functions

--- Client/Frontend/Browser/Tabs/Views/TabTrayViewController.swift
+++ Client/Frontend/Browser/Tabs/Views/TabTrayViewController.swift
@@ -199,7 +199,7 @@ class TabTrayViewController: UIViewController,
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
-        subscribeRedux()
+        subscribeToRedux()
         listenForThemeChange(view)
         updateToolbarItems()
     }
@@ -229,24 +229,7 @@ class TabTrayViewController: UIViewController,
         super.viewDidDisappear(animated)
         delegate?.didFinish()
 
-        store.dispatch(ActiveScreensStateAction.closeScreen(.tabsTray))
-        store.unsubscribe(self)
-    }
-
-    private func subscribeRedux() {
-        store.dispatch(ActiveScreensStateAction.showScreen(.tabsTray))
-        store.dispatch(TabTrayAction.tabTrayDidLoad(tabTrayState.selectedPanel))
-        store.subscribe(self, transform: {
-            return $0.select(TabTrayState.init)
-        })
-    }
-
-    func newState(state: TabTrayState) {
-        tabTrayState = state
-
-        if tabTrayState.shouldDismiss {
-            dismissVC()
-        }
+        unsubscribeFromRedux()
     }
 
     private func updateLayout() {
@@ -262,6 +245,29 @@ class TabTrayViewController: UIViewController,
             navigationItem.titleView = segmentedControl
         }
         updateToolbarItems()
+    }
+
+    // MARK: - Redux
+
+    func subscribeToRedux() {
+        store.dispatch(ActiveScreensStateAction.showScreen(.tabsTray))
+        store.dispatch(TabTrayAction.tabTrayDidLoad(tabTrayState.selectedPanel))
+        store.subscribe(self, transform: {
+            return $0.select(TabTrayState.init)
+        })
+    }
+
+    func unsubscribeFromRedux() {
+        store.dispatch(ActiveScreensStateAction.closeScreen(.tabsTray))
+        store.unsubscribe(self)
+    }
+
+    func newState(state: TabTrayState) {
+        tabTrayState = state
+
+        if tabTrayState.shouldDismiss {
+            dismissVC()
+        }
     }
 
     // MARK: Themeable
