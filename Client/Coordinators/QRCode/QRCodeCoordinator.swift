df93ad1a94ad654e999ebac5c75c0291a93e43fe
yoanarios <yriosdiaz@mozilla.com>
2023-12-11 17:21:25 -0500

    Revert change due to side effects, fix FxA signin flow using Coordinator

--- Client/Coordinators/QRCode/QRCodeCoordinator.swift
+++ Client/Coordinators/QRCode/QRCodeCoordinator.swift
@@ -35,5 +35,6 @@ class QRCodeCoordinator: BaseCoordinator, QRCodeDismissHandler {
 
     func dismiss(_ completion: (() -> Void)?) {
         router.dismiss(animated: true, completion: completion)
+        parentCoordinator?.didFinish(from: self)
     }
 }
