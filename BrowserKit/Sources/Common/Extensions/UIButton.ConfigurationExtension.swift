2ce44deedafa246679e7d56e7b2ef0f25db97662
tonell_m <marceau.tonelli@epitech.eu>
2023-12-11 16:16:17 +0100

    Added a comment to tell that extension is only a temporary fix

--- BrowserKit/Sources/Common/Extensions/UIButton.ConfigurationExtension.swift
+++ BrowserKit/Sources/Common/Extensions/UIButton.ConfigurationExtension.swift
@@ -8,6 +8,12 @@ extension UIButton.Configuration {
     /// Helper function to set a button's font through UIButton.Configuration using `titleTextAttributesTransformer`.
     ///
     /// - Parameter font: The new button title font.
+    ///
+    /// - Note: This extension is only temporary to avoid code duplication until all buttons are setup to use
+    ///         button components (`PrimaryRoundedButton`, `SecondaryRoundedButton` or `LinkButton`). Once
+    ///         this migration is done, this extension should be removed.
+    ///         See [this thread](https://github.com/mozilla-mobile/firefox-ios/pull/17616#discussion_r1415780465)
+    ///         for more details.
     public mutating func setFont(_ font: UIFont) {
         titleTextAttributesTransformer = UIConfigurationTextAttributesTransformer { incoming in
             var outgoing = incoming
