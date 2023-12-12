8618301d449cb4a8e3d01207da69d46b876f2fc6
Clare So <cso@mozilla.com>
2023-12-12 14:18:22 -0500

    Update index of context menu items

--- firefox-ios/firefox-ios-tests/Tests/XCUITests/PhotonActionSheetTests.swift
+++ firefox-ios/firefox-ios-tests/Tests/XCUITests/PhotonActionSheetTests.swift
@@ -71,9 +71,9 @@ class PhotonActionSheetTests: BaseTestCase {
         mozWaitForElementToExist(app.otherElements["ActivityListView"].otherElements["example.com"])
         mozWaitForElementToExist(app.collectionViews.cells["Copy"], timeout: TIMEOUT)
 
-        var  fennecElement = app.collectionViews.scrollViews.cells.element(boundBy: 3)
+        var  fennecElement = app.collectionViews.scrollViews.cells.element(boundBy: 2)
         if iPad() {
-            fennecElement = app.collectionViews.scrollViews.cells.element(boundBy: 2)
+            fennecElement = app.collectionViews.scrollViews.cells.element(boundBy: 1)
         }
         mozWaitForElementToExist(fennecElement, timeout: 5)
         fennecElement.tap()
