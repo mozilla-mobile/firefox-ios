d558f8cdf69a75f7370bdbbad4cd1413605ce9d3
Laurie Marceau <lmarceau@mozilla.com>
2023-12-12 17:34:02 -0500

    Fix comment

--- Shared/AppInfo.swift
+++ Shared/AppInfo.swift
@@ -15,7 +15,7 @@ extension AppInfo {
     }
 
     /// The port for the internal webserver, tests can change this
-    /// Please be aware that we needed to duplicate this webserverPort in the Common package due to Shared target issues in #17721
+    /// Please be aware that we needed to migrate this webserverPort in WebEngine.WKEngineInfo due to Shared target issues in #17721
     /// This webserverPort needs to be deleted with FXIOS-7960 once the WebEngine package is integrated in Firefox iOS
     public static var webserverPort = 6571
 
