3f69df9e5d62bee2c0ac059676abf9cfb63b0f27
Matt Reagan <mreagan@mozilla.com>
2023-12-12 10:07:26 -0800

    [7931] Add logging per PR feedback, new log category for iPad multiwindow messages, some minor refactors

--- Client/Application/WindowManager.swift
+++ Client/Application/WindowManager.swift
@@ -3,6 +3,7 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
 import Foundation
+import Common
 
 /// General window management class that provides some basic coordination and
 /// state management for multiple windows shared across a single running app.
@@ -30,7 +31,16 @@ final class WindowManagerImplementation: WindowManager {
         get { return uuidForActiveWindow() }
         set { _activeWindowUUID = newValue }
     }
+    private let logger: Logger
     private var _activeWindowUUID: WindowUUID?
+
+    // MARK: - Initializer
+
+    init(logger: Logger = DefaultLogger.shared) {
+        self.logger = logger
+    }
+
+    // MARK: - Public API
 
     func tabManagerDidConnectToBrowserWindow(_ tabManager: TabManager) {
         let uuid = tabManager.windowUUID
@@ -51,16 +61,25 @@ final class WindowManagerImplementation: WindowManager {
     // MARK: - Internal Utilities
 
     private func updateWindow(_ info: AppWindowInfo?, for uuid: WindowUUID) {
-        assert(info != nil || windows.count > 1,
-               "Cannot remove the only active window in the app. This is a client error.")
+        guard info != nil || windows.count > 1 else {
+            let message = "Cannot remove the only active window in the app. This is a client error."
+            logger.log(message, level: .fatal, category: .window)
+            assertionFailure(message)
+            return
+        }
 
         windows[uuid] = info
+        didUpdateWindow(uuid)
+    }
 
+    /// Called internally when a window is updated (or removed).
+    /// - Parameter uuid: the UUID of the window that changed.
+    private func didUpdateWindow(_ uuid: WindowUUID) {
+        // Convenience. If the client has successfully configured
+        // a window and it is the only window in the app, we can
+        // be sure we automatically set it as the active window.
         if windows.count == 1 {
-            // Convenience. If the client has successfully configured
-            // a window and it is the only window in the app, we can
-            // automatically set it as the active window.
-            activeWindow = uuid
+            activeWindow = windows.keys.first!
         }
     }
 
@@ -76,7 +95,9 @@ final class WindowManagerImplementation: WindowManager {
         }
 
         guard let uuid = _activeWindowUUID else {
-            assertionFailure("The app has multiple windows but there is no active window UUID. This is a client error.")
+            let message = "App has multiple windows but no active window UUID. This is a client error."
+            logger.log(message, level: .fatal, category: .window)
+            assertionFailure(message)
             return windows.keys.first!
         }
         return uuid
