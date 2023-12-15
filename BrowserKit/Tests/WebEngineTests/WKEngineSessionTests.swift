d20e7b9201065021b17bd23bc07004d060adde6b
Laurie Marceau <lmarceau@mozilla.com>
2023-12-15 16:21:26 -0500

    Create mocks for tests

--- BrowserKit/Tests/WebEngineTests/WKEngineSessionTests.swift
+++ BrowserKit/Tests/WebEngineTests/WKEngineSessionTests.swift
@@ -6,5 +6,56 @@ import XCTest
 @testable import WebEngine
 
 final class WKEngineSessionTests: XCTestCase {
+    private var configurationProvider: MockWKEngineConfigurationProvider!
+    private var webViewProvider: MockWKWebViewProvider!
 
+    override func setUp() {
+        super.setUp()
+        configurationProvider = MockWKEngineConfigurationProvider()
+        webViewProvider = MockWKWebViewProvider()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+        configurationProvider = nil
+        webViewProvider = nil
+    }
+
+    func testLoadURLGivenNotAURLThenDoesNothing() {
+        let subject = createSubject()
+        let url = "NotAURL"
+
+        subject?.load(url: url)
+
+        XCTAssertEqual(webViewProvider.webView.loadCalled, 0)
+    }
+
+    func testLoadURLGivenNormalURLThenLoadsRequest() {
+        let subject = createSubject()
+        let url = "https://example.com"
+
+        subject?.load(url: url)
+
+        XCTAssertEqual(webViewProvider.webView.loadCalled, 1)
+    }
+
+    func testLoadURLGivenReaderModeURLThenLoadsReques() {
+        let subject = createSubject()
+        let url = "file://location"
+
+        subject?.load(url: url)
+
+        XCTAssertEqual(webViewProvider.webView.loadCalled, 1)
+    }
+
+    // MARK: Helper
+
+    func createSubject() -> WKEngineSession? {
+        guard let subject = WKEngineSession(configurationProvider: configurationProvider,
+                                            webViewProvider: webViewProvider) else {
+            return nil
+        }
+        trackForMemoryLeaks(subject)
+        return subject
+    }
 }
