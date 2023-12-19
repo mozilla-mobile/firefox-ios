85ce67c956d1199b6cb4de1c915ef5877a1cd052
lmarceau <lmarceau@mozilla.com>
2023-12-19 12:05:49 -0500

    Refactor FXIOS-7980 [v123] Adjust load file URL in WKEngineSession (#17814)
    * Remove privileged from file URL in session, adjust tests
    
    * swiftlint

--- BrowserKit/Tests/WebEngineTests/Mock/MockWKEngineWebView.swift
+++ BrowserKit/Tests/WebEngineTests/Mock/MockWKEngineWebView.swift
@@ -22,15 +22,22 @@ class MockWKEngineWebView: WKEngineWebView {
     var removeAllUserScriptsCalled = 0
     var removeFromSuperviewCalled = 0
 
+    var loadRequest: URLRequest?
+    var loadFileURL: URL?
+    var loadFileReadAccessURL: URL?
+
     required init?(frame: CGRect,
                    configurationProvider: WKEngineConfigurationProvider) {}
 
     func load(_ request: URLRequest) -> WKNavigation? {
+        loadRequest = request
         loadCalled += 1
         return nil
     }
 
     func loadFileURL(_ URL: URL, allowingReadAccessTo readAccessURL: URL) -> WKNavigation? {
+        loadFileURL = URL
+        loadFileReadAccessURL = readAccessURL
         loadFileURLCalled += 1
         return nil
     }
