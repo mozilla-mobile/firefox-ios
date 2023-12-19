aea175e46d0bc0f6d4097ed78bd1b597853e1825
Orla <omitchell@mozilla.com>
2023-12-19 13:27:22 -0500

    Fix tests

--- Client/TabManagement/Legacy/LegacyTabManager.swift
+++ Client/TabManagement/Legacy/LegacyTabManager.swift
@@ -577,7 +577,9 @@ class LegacyTabManager: NSObject, FeatureFlaggable, TabManager, TabEventHandler 
 
         let tab = tabs[index]
         let viableTabsIndex = deletedIndexForViableTabs(tab)
-        backupCloseTab = BackupCloseTab(tab: tab, restorePosition: viableTabsIndex)
+        TabTrayFlagManager.isRefactorEnabled {
+            backupCloseTab = BackupCloseTab(tab: tab, restorePosition: viableTabsIndex)
+        }
         self.removeTab(tab, flushToDisk: true)
         self.updateIndexAfterRemovalOf(tab, deletedIndex: index, viableTabsIndex: viableTabsIndex)
 
