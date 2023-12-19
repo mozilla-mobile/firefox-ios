0303763805358f0c0608a4c57cf042c89c7c10de
Clare So <cso@mozilla.com>
2023-12-19 14:30:49 -0500

    Lint issue with var vs let

--- firefox-ios/firefox-ios-tests/Tests/XCUITests/PhotonActionSheetTests.swift
+++ firefox-ios/firefox-ios-tests/Tests/XCUITests/PhotonActionSheetTests.swift
@@ -71,7 +71,7 @@ class PhotonActionSheetTests: BaseTestCase {
         mozWaitForElementToExist(app.otherElements["ActivityListView"].otherElements["example.com"])
         mozWaitForElementToExist(app.collectionViews.cells["Copy"], timeout: TIMEOUT)
 
-        var  fennecElement = app.collectionViews.scrollViews.cells.elementContainingText("Fennec")
+        let fennecElement = app.collectionViews.scrollViews.cells.elementContainingText("Fennec")
         mozWaitForElementToExist(fennecElement, timeout: 5)
         fennecElement.tap()
         mozWaitForElementToExist(app.navigationBars["ShareTo.ShareView"])
