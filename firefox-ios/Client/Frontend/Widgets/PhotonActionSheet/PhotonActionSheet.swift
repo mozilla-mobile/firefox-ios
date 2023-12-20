0981fe83ba007d3e2a32c97c657186fda2a9153b
Sanchit Mittal <sanchitmittal18@gmail.com>
2023-12-20 23:58:44 +0530

    issue-10925 | fixed trailing constraint

--- firefox-ios/Client/Frontend/Widgets/PhotonActionSheet/PhotonActionSheet.swift
+++ firefox-ios/Client/Frontend/Widgets/PhotonActionSheet/PhotonActionSheet.swift
@@ -213,11 +213,9 @@ class PhotonActionSheet: UIViewController, Themeable {
         ]
 
         // Can't set this on iPad (not in multitasking) since it causes the menu to take all the width of the screen.
-        if PhotonActionSheetViewModel.isSmallSizeForTraitCollection(trait: view.traitCollection) {
-            tableViewConstraints.append(
-                tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
-            )
-        }
+        tableViewConstraints.append(
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        )
 
         constraints.append(contentsOf: tableViewConstraints)
     }
