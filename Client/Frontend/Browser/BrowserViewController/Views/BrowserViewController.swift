9c5ab66a50219c6387a0f54f4d42cce85934e69d
roux g. buciu <11182210+adudenamedruby@users.noreply.github.com>
2023-12-21 15:31:03 -0500

    Bugfix FXIOS-8006 [v122] Fix homepage loading issue (#17873)
    * Squishy da bug
    
    * Format things like a civilized human being
    
    ---------
    
    Co-authored-by: roux g. buciu <github@fringe.foundation>
    (cherry picked from commit ccbf3a90e1705f1d276cbc083bde23917f14834e)

--- Client/Frontend/Browser/BrowserViewController/Views/BrowserViewController.swift
+++ Client/Frontend/Browser/BrowserViewController/Views/BrowserViewController.swift
@@ -491,6 +491,8 @@ class BrowserViewController: UIViewController,
             } else if !state.fakespotState.isOpen {
                 dismissFakespotIfNeeded()
             }
+
+            updateInContentHomePanel(tabManager.selectedTab?.url)
         }
     }
 
