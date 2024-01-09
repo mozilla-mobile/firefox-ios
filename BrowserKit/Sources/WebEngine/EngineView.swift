b7c838deb3cd6043a19757ec412d96ce68e70dab
Laurie Marceau <lmarceau@mozilla.com>
2024-01-09 12:00:00 -0500

    Create WKEngineView

--- BrowserKit/Sources/WebEngine/EngineView.swift
+++ BrowserKit/Sources/WebEngine/EngineView.swift
@@ -2,10 +2,10 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import Foundation
+import UIKit
 
 /// View that renders web content.
-public protocol EngineView {
+public protocol EngineView: UIView {
     /// Render the content of the given session.
     func render(session: EngineSession)
 }
