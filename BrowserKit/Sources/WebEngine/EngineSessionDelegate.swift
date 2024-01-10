65667cefce2bb842331375367fbe9cecae50684b
Laurie Marceau <lmarceau@mozilla.com>
2024-01-10 15:42:30 -0500

    Document public interfaces of web engine

--- BrowserKit/Sources/WebEngine/EngineSessionDelegate.swift
+++ BrowserKit/Sources/WebEngine/EngineSessionDelegate.swift
@@ -4,11 +4,23 @@
 
 import Foundation
 
+/// Delegate used by the class that want to observe an engine session
 public protocol EngineSessionDelegate: AnyObject {
+    /// Event to indicate the scroll position of the content has changed.
     func onScrollChange(scrollX: Int, scrollY: Int)
+
+    /// Event to indicate that this session has had a long press.
     func onLongPress(touchPoint: CGPoint)
+
+    /// Event to indicate the title has changed.
     func onTitleChange(title: String)
+
+    /// Event to indicate the loading progress has been updated.
     func onProgress(progress: Int)
+
+    /// Event to indicate there has been a navigation change.
     func onNavigationStateChange(canGoBack: Bool?, canGoForward: Bool?)
+
+    /// Event to indicate that a url was loaded to this session.
     func onLoadUrl()
 }
