13f294ffcc6cfc8e54dfff5870476e93c1d2557d
Cyndi Chin <cgc95@cornell.edu>
2024-01-10 15:51:30 -0500

    Add FXIOS-8096 [v122] Telemetry for data clearance button (#18080)

---
+++ firefox-ios/firefox-ios-tests/Tests/ClientTests/PrivateBrowsingTelemetryTests.swift
@@ -0,0 +1,38 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import Glean
+import XCTest
+
+@testable import Client
+
+final class PrivateBrowsingTelemetryTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+        Glean.shared.resetGlean(clearStores: true)
+        Glean.shared.enableTestingMode()
+    }
+
+    func testDataClearanceConfirmed() throws {
+        let subject = PrivateBrowsingTelemetry()
+
+        subject.sendDataClearanceTappedTelemetry(didConfirm: true)
+
+        testEventMetricRecordingSuccess(metric: GleanMetrics.PrivateBrowsing.dataClearanceIconTapped)
+
+        let resultValue = try XCTUnwrap(GleanMetrics.PrivateBrowsing.dataClearanceIconTapped.testGetValue())
+        XCTAssertEqual(resultValue[0].extra?["did_confirm"], "true")
+    }
+
+    func testDataClearanceCancelled() throws {
+        let subject = PrivateBrowsingTelemetry()
+
+        subject.sendDataClearanceTappedTelemetry(didConfirm: false)
+
+        testEventMetricRecordingSuccess(metric: GleanMetrics.PrivateBrowsing.dataClearanceIconTapped)
+
+        let resultValue = try XCTUnwrap(GleanMetrics.PrivateBrowsing.dataClearanceIconTapped.testGetValue())
+        XCTAssertEqual(resultValue[0].extra?["did_confirm"], "false")
+    }
+}
