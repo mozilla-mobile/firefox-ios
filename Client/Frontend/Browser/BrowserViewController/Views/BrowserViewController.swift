05e9e9bb385bf0cc6b54f82215697a6e8e1e9b5e
Litianu Razvan <litianu.razvan@gmail.com>
2024-01-16 14:33:37 +0200

    Bugfix FXIOS-7199 [v123] Url remains static despite webpage change or window selection (#18140)
    * FXIOS-7199 #15971 Url remains static despite webpage change or window selection
    
    * Don't dismiss if url is nil
    
    (cherry picked from commit 963cac9b8e3fe5932342e5b3de02d5fe45c6c792)

--- Client/Frontend/Browser/BrowserViewController/Views/BrowserViewController.swift
+++ Client/Frontend/Browser/BrowserViewController/Views/BrowserViewController.swift
@@ -2251,6 +2251,7 @@ extension BrowserViewController: TabManagerDelegate {
 
         if let tab = selected, let webView = tab.webView {
             updateURLBarDisplayURL(tab)
+            if urlBar.inOverlayMode, tab.url?.displayURL != nil { urlBar.leaveOverlayMode(didCancel: false) }
 
             if previous == nil || tab.isPrivate != previous?.isPrivate {
                 applyTheme()
