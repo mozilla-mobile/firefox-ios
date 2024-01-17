e73bfc07fab65fff34a58933cdb1aecb5337f7eb
petruSt24 <126153378+petruSt24@users.noreply.github.com>
2024-01-17 22:33:08 +0200

    Bugfix FXIOS-6672 [v123] Awesomebar.location telemetry does not add up to 100 (#17937)
    * Bugfix FXIOS-6672 [v123] Awesomebar.location telemetry does not add up to 100 (#15348)
    
    * Update firefox-ios/Client/Frontend/Browser/BrowserViewController/Views/BrowserViewController.swift
    
    ---------
    
    Co-authored-by: Nishant Bhasin <nish.bhasin@gmail.com>

--- firefox-ios/Client/Frontend/Browser/BrowserViewController/Views/BrowserViewController.swift
+++ firefox-ios/Client/Frontend/Browser/BrowserViewController/Views/BrowserViewController.swift
@@ -396,6 +396,7 @@ class BrowserViewController: UIViewController,
         // individual TabManager instances for each BVC, so we perform these here instead.
         tabManager.preserveTabs()
         // TODO: [FXIOS-7856] Some additional updates for telemetry forthcoming, once iPad multi-window is enabled.
+        SearchBarSettingsViewModel.recordLocationTelemetry(for: isBottomSearchBar ? .bottom : .top)
         TabsTelemetry.trackTabsQuantity(tabManager: tabManager)
     }
 
