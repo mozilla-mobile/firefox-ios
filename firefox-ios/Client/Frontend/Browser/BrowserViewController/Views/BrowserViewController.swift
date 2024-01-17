533a38829e8d70f8e84bc96d9672bee5df9211b1
Cyndi Chin <cgc95@cornell.edu>
2024-01-17 15:03:04 -0500

    Add FXIOS-8175 [v122.1] Refresh button to URL bar and keep toolbar button consistent on iPad (#18197)
    * remove home button from iPad
    
    * remove stop and reload from multistatebutton
    
    * add reload button to url bar

--- firefox-ios/Client/Frontend/Browser/BrowserViewController/Views/BrowserViewController.swift
+++ firefox-ios/Client/Frontend/Browser/BrowserViewController/Views/BrowserViewController.swift
@@ -327,9 +327,8 @@ class BrowserViewController: UIViewController,
         let showToolbar = shouldShowToolbarForTraitCollection(newCollection)
         let showTopTabs = shouldShowTopTabsForTraitCollection(newCollection)
 
-        let hideReloadButton = shouldUseiPadSetup(traitCollection: newCollection)
         urlBar.topTabsIsShowing = showTopTabs
-        urlBar.setShowToolbar(!showToolbar, hideReloadButton: hideReloadButton)
+        urlBar.setShowToolbar(!showToolbar)
         toolbar.addNewTabButton.isHidden = showToolbar
 
         if showToolbar {
@@ -1093,7 +1092,7 @@ class BrowserViewController: UIViewController,
             }
         } else if !url.absoluteString.hasPrefix("\(InternalURL.baseUrl)/\(SessionRestoreHandler.path)") {
             showEmbeddedWebview()
-            urlBar.shouldHideReloadButton(shouldUseiPadSetup())
+            urlBar.locationView.reloadButton.isHidden = false
         }
 
         if UIDevice.current.userInterfaceIdiom == .pad {
@@ -1305,16 +1304,8 @@ class BrowserViewController: UIViewController,
             return
         }
 
-        if traitCollection.horizontalSizeClass == .compact {
-            state = .home
-        } else {
-            state = isLoading ? .stop : .reload
-        }
-
-        handleMiddleButtonState(state)
-        if !toolbar.isHidden {
-            urlBar.locationView.reloadButton.reloadButtonState = isLoading ? .stop : .reload
-        }
+        handleMiddleButtonState(.home)
+        urlBar.locationView.reloadButton.reloadButtonState = isLoading ? .stop : .reload
         currentMiddleButtonState = state
     }
 
@@ -2430,14 +2421,14 @@ extension BrowserViewController: TabManagerDelegate {
         }
 
         if let readerMode = selected?.getContentScript(name: ReaderMode.name()) as? ReaderMode {
-            urlBar.updateReaderModeState(readerMode.state, hideReloadButton: shouldUseiPadSetup())
+            urlBar.updateReaderModeState(readerMode.state)
             if readerMode.state == .active {
                 showReaderModeBar(animated: false)
             } else {
                 hideReaderModeBar(animated: false)
             }
         } else {
-            urlBar.updateReaderModeState(ReaderModeState.unavailable, hideReloadButton: shouldUseiPadSetup())
+            urlBar.updateReaderModeState(ReaderModeState.unavailable)
         }
 
         if topTabsVisible {
