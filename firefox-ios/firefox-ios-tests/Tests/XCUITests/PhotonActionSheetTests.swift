5b461ddd107e07e5474dbd058643e370002b18c6
Clare So <cso@mozilla.com>
2024-01-20 00:49:20 -0500

    Remove whitespace

--- firefox-ios/firefox-ios-tests/Tests/XCUITests/PhotonActionSheetTests.swift
+++ firefox-ios/firefox-ios-tests/Tests/XCUITests/PhotonActionSheetTests.swift
@@ -76,7 +76,6 @@ class PhotonActionSheetTests: BaseTestCase {
 
         // This is not ideal but only way to get the element on iPhone 8
         // for iPhone 11, that would be boundBy: 2
-
         mozWaitForElementToExist(app.otherElements["ActivityListView"].otherElements["Example Domain"])
         mozWaitForElementToExist(app.otherElements["ActivityListView"].otherElements["example.com"])
         mozWaitForElementToExist(app.collectionViews.cells["Copy"], timeout: TIMEOUT)
