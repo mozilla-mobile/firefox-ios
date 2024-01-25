4cfa078c50a720cce904c0993a31182d0149be97
Winnie Teichmann <4530+thatswinnie@users.noreply.github.com>
2024-01-25 12:50:41 +0100

    Replace external link icon

--- firefox-ios/WidgetKit/OpenTabs/OpenTabsWidget.swift
+++ firefox-ios/WidgetKit/OpenTabs/OpenTabsWidget.swift
@@ -58,7 +58,7 @@ struct OpenTabsView: View {
 
     var openFirefoxButton: some View {
         HStack(alignment: .center, spacing: 15) {
-            Image("openFirefox").foregroundColor(Color.white)
+            Image("externalLinkSmall").foregroundColor(Color.white)
             Text("Open Firefox")
                 .foregroundColor(Color.white).lineLimit(1)
                 .font(.system(size: 13, weight: .semibold, design: .default))
@@ -81,7 +81,7 @@ struct OpenTabsView: View {
                     Text(String.NoOpenTabsLabel)
                     HStack {
                         Spacer()
-                        Image("openFirefox")
+                        Image("externalLinkSmall")
                         Text(String.OpenFirefoxLabel)
                             .foregroundColor(Color.white).lineLimit(1)
                             .font(.system(size: 13, weight: .semibold, design: .default))
@@ -96,7 +96,7 @@ struct OpenTabsView: View {
 
                     if entry.tabs.count > numberOfTabsToDisplay {
                         HStack(alignment: .center, spacing: 15) {
-                            Image("openFirefox").foregroundColor(Color.white).frame(width: 16, height: 16)
+                            Image("externalLinkSmall").foregroundColor(Color.white).frame(width: 16, height: 16)
                             Text(
                                 String.localizedStringWithFormat(
                                     String.MoreTabsLabel,
