696c46fd5af87239a584eee31621acc5e17e227e
Winnie Teichmann <4530+thatswinnie@users.noreply.github.com>
2024-01-30 14:22:40 +0100

    Refactor FXIOS-8253 [v124] Fix warning in FakespotClient (#18338)
    * Fix warning
    
    * Fix tests

--- firefox-ios/Client/Frontend/Fakespot/Client/FakeSpotClient.swift
+++ firefox-ios/Client/Frontend/Fakespot/Client/FakeSpotClient.swift
@@ -57,7 +57,9 @@ protocol FakespotClientType {
     ///   - aid: A string representing identifier for the event.
     /// - Returns: An `AdEventsResponse` object containing the response data.
     /// - Throws: `FakeSpotClientError.invalidURL` if the ad recording endpoint URL is invalid or not set.
-    func reportAdEvent(eventName: FakespotAdsEvent, eventSource: String, aidvs: [String]) async throws -> AdEventsResponse
+    func reportAdEvent(eventName: FakespotAdsEvent,
+                       eventSource: String,
+                       aidvs: [String]) async throws -> AdEventsResponse?
 }
 
 /// An enumeration representing different environments for the Fakespot client.
@@ -251,7 +253,9 @@ struct FakespotClient: FakespotClientType {
     }
 
     /// Reports an advertising event with specified details to the API
-    func reportAdEvent(eventName: FakespotAdsEvent, eventSource: String, aidvs: [String]) async throws -> AdEventsResponse {
+    func reportAdEvent(eventName: FakespotAdsEvent,
+                       eventSource: String,
+                       aidvs: [String]) async throws -> AdEventsResponse? {
         // Define the API endpoint URL
         guard let endpointURL = environment.adRecordingEndpoint else {
             throw FakeSpotClientError.invalidURL
@@ -261,10 +265,11 @@ struct FakespotClient: FakespotClientType {
 
         switch eventName {
         case .trustedDealsLinkClicked:
+            guard let firstItem = aidvs.first else { return nil }
             requestBody = [
                 "event_name": eventName.rawValue,
                 "event_source": eventSource,
-                "aid": aidvs.first
+                "aid": firstItem
             ]
         case .trustedDealsImpression, .trustedDealsPlacement:
             requestBody = [
