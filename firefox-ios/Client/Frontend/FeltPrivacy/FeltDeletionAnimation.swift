7ac268fe1bdfaf37ee72ea8fa4ba80950d23b12a
Cyndi Chin <cgc95@cornell.edu>
2024-02-01 13:45:14 -0500

    Add FXIOS-8173 [v124] Lottie package to project (#18523)

---
+++ firefox-ios/Client/Frontend/FeltPrivacy/FeltDeletionAnimation.swift
@@ -0,0 +1,10 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import Foundation
+import Lottie
+
+struct FeltDeletionAnimation {
+    // TODO: Add animation https://mozilla-hub.atlassian.net/browse/FXIOS-8174
+}
