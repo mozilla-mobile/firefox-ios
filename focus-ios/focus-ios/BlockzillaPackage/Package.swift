19dcab6cd2704de09291b80ef3cd143b2f670ce8
OrlaM <omitchell@mozilla.com>
2024-01-31 19:21:56 -0500

    [focus-ios] Update deployment target (https://github.com/mozilla-mobile/focus-ios/issues/4021)

--- focus-ios/focus-ios/BlockzillaPackage/Package.swift
+++ focus-ios/focus-ios/BlockzillaPackage/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Focus",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v15)],
     products: [
         .library(
             name: "UIHelpers",
