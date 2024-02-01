42f6006a552608306a58a3d138311119525f62e6
Winnie Teichmann <4530+thatswinnie@users.noreply.github.com>
2024-02-01 15:37:29 +0100

    Refactor FXIOS-6773 [v124] Update mobile icons to new style pt3.3 (#18508)
    * Add image identifier for sync_open_tab
    
    * Clean up unused icons
    
    * Fix build error
    
    * Use image identifier
    
    * Rename fire icon to dataClearanceLarge

--- firefox-ios/firefox-ios-tests/Tests/UITests/Global.swift
+++ firefox-ios/firefox-ios-tests/Tests/UITests/Global.swift
@@ -382,7 +382,7 @@ class SimplePageServer {
         let webServer: GCDWebServer = GCDWebServer()
 
         webServer.addHandler(forMethod: "GET", path: "/image.png", request: GCDWebServerRequest.self) { (request) -> GCDWebServerResponse? in
-            let img = UIImage(named: "goBack")!.pngData()!
+            let img = UIImage(named: StandardImageIdentifiers.Large.chevronLeft)!.pngData()!
             return GCDWebServerDataResponse(data: img, contentType: "image/png")
         }
 
