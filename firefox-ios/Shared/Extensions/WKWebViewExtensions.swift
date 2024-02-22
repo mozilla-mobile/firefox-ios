879abde08392e5ed4f5329bab9df015cd48d6aae
Issam Mani <issamouu69@gmail.com>
2024-02-22 18:42:30 +0100

    Add FXIOS-8465 [v125] Run autofill in allowed contexts only (#18777)
    * feat: add protocol check
    
    * chore: update comment to mention SecurityManager
    
    * fix: lint
    
    Co-authored-by: lmarceau <lmarceau@mozilla.com>
    
    * fix: failing unit tests
    
    ---------
    
    Co-authored-by: lmarceau <lmarceau@mozilla.com>
    (cherry picked from commit e529aae1ce7c9bcaf57fa6f12c7768cfbdc810a6)

--- firefox-ios/Shared/Extensions/WKWebViewExtensions.swift
+++ firefox-ios/Shared/Extensions/WKWebViewExtensions.swift
@@ -75,3 +75,14 @@ extension WKUserScript {
         )
     }
 }
+
+extension WKFrameInfo {
+    // We check both the top and current frame protocol. This is to also check for mixed content.
+    // For most cases checking the top frame protocol.`hasOnlySecureContent` is deliberatly not used here
+    // since it will return false if any external resource on the page is loaded via
+    // an insecure connection (i.e styles, images, ...), which might be too strict for most applications.
+    // Note: We should drop this extension in favour of SecurityManager in the future.
+    public var isFrameLoadedInSecureContext: Bool {
+        return self.webView?.url?.scheme == "https" && self.securityOrigin.protocol == "https"
+    }
+}
