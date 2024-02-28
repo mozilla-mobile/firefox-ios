08a9078353ffb68145804e8636a3dd7741ed9071
dragosb01 <134391433+dragosb01@users.noreply.github.com>
2024-02-28 11:04:50 +0200

    Add MTE-2383 [v125] reading list test (#18871)

--- firefox-ios/firefox-ios-tests/Tests/XCUITests/ReadingListTests.swift
+++ firefox-ios/firefox-ios-tests/Tests/XCUITests/ReadingListTests.swift
@@ -215,6 +215,55 @@ class ReadingListTests: BaseTestCase {
         XCTAssertFalse(app.tables["ReadingTable"].cells.staticTexts["The Book of Mozilla"].exists)
     }
 
+    // https://testrail.stage.mozaws.net/index.php?/cases/view/2306893
+    // Smoketest
+    func testReadingList() {
+        navigator.nowAt(NewTabScreen)
+        navigator.goto(LibraryPanel_ReadingList)
+        // Validate empty reading list panel
+        let emptyReadingList1 = AccessibilityIdentifiers.LibraryPanels.ReadingListPanel.emptyReadingList1
+        let emptyReadingList2 = AccessibilityIdentifiers.LibraryPanels.ReadingListPanel.emptyReadingList2
+        let emptyReadingList3 = AccessibilityIdentifiers.LibraryPanels.ReadingListPanel.emptyReadingList3
+        mozWaitForElementToExist(app.staticTexts[emptyReadingList1])
+        mozWaitForElementToExist(app.staticTexts[emptyReadingList2])
+        mozWaitForElementToExist(app.staticTexts[emptyReadingList3])
+        app.buttons["Done"].tap()
+        // Add item to reading list and check that it appears
+        addContentToReaderView()
+        navigator.goto(BrowserTabMenu)
+        navigator.goto(LibraryPanel_ReadingList)
+        let savedToReadingList = app.tables["ReadingTable"].cells.staticTexts["The Book of Mozilla"]
+        mozWaitForElementToExist(savedToReadingList)
+        // Tap on an article
+        savedToReadingList.tap()
+        // The article is displayed in Reader View
+        mozWaitForElementToExist(app.buttons["Reader View"], timeout: TIMEOUT)
+        XCTAssertTrue(app.buttons["Reader View"].isSelected)
+        XCTAssertTrue(app.buttons["Reader View"].isEnabled)
+        app.buttons[AccessibilityIdentifiers.Toolbar.homeButton].tap()
+        navigator.nowAt(NewTabScreen)
+        navigator.goto(LibraryPanel_ReadingList)
+        // Swipe the article left
+        // The article has been marked as Read
+        mozWaitForElementToExist(app.tables["ReadingTable"].cells.elementContainingText("The Book of Mozilla, read"))
+        savedToReadingList.swipeLeft()
+        // Two options are revealed
+        mozWaitForElementToExist(app.tables.cells.buttons.staticTexts["Mark as  Unread"], timeout: TIMEOUT)
+        mozWaitForElementToExist(app.tables.cells.buttons.staticTexts["Remove"], timeout: TIMEOUT)
+        // Tap 'Mark as Unread'
+        app.tables.cells.buttons.staticTexts["Mark as  Unread"].tap(force: true)
+        // The article has been marked as Unread
+        mozWaitForElementToExist(app.tables["ReadingTable"].cells.elementContainingText("The Book of Mozilla, unread"))
+        // Swipe te article left and tap 'Remove'
+        savedToReadingList.swipeLeft()
+        app.tables.cells.buttons.staticTexts["Remove"].tap(force: true)
+        // The article is deleted from the Reading List
+        checkReadingListNumberOfItems(items: 0)
+        mozWaitForElementToExist(app.staticTexts[emptyReadingList1])
+        mozWaitForElementToExist(app.staticTexts[emptyReadingList2])
+        mozWaitForElementToExist(app.staticTexts[emptyReadingList3])
+    }
+
     // https://testrail.stage.mozaws.net/index.php?/cases/view/2306993
     // Smoketest
     func testAddToReaderListOptions() throws {
