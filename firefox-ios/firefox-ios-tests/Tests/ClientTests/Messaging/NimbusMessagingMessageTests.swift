eb6391bafaee39edbb9f70798355539cc9aa1ce7
Nishant Bhasin <nish.bhasin@gmail.com>
2024-02-28 14:09:55 -0500

    cleanup

--- firefox-ios/firefox-ios-tests/Tests/ClientTests/Messaging/NimbusMessagingMessageTests.swift
+++ firefox-ios/firefox-ios-tests/Tests/ClientTests/Messaging/NimbusMessagingMessageTests.swift
@@ -43,11 +43,12 @@ final class NimbusMessagingMessageTests: XCTestCase {
             do {
                 _ = try evaluationUtility.isMessageEligible(
                     message,
-                    messageHelper: helper, jexlCache: &cache)
+                    messageHelper: helper,
+                    jexlCache: &cache
+                )
             } catch {
                 XCTFail("Message \(message.id) failed with invalid JEXL triggers")
             }
         }
     }
 }
-
