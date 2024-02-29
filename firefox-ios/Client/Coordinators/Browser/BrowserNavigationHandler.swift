16a7753c0fd18cc44641bbe9e2f541203262e68b
Razvan Litianu <litianu.razvan@gmail.com>
2024-02-29 14:14:01 +0200

    Fix swiftlint warning

--- firefox-ios/Client/Coordinators/Browser/BrowserNavigationHandler.swift
+++ firefox-ios/Client/Coordinators/Browser/BrowserNavigationHandler.swift
@@ -70,7 +70,8 @@ protocol BrowserNavigationHandler: AnyObject, QRCodeNavigationHandler {
                                 frame: WKFrameInfo?,
                                 alertContainer: UIView)
 
-    /// Displays an autofill interface for saved logins, allowing the user to select from stored login credentials to autofill login forms on the specified web page.
+    /// Displays an autofill interface for saved logins, allowing the user to select from stored login credentials 
+    /// to autofill login forms on the specified web page.
     func showSavedLoginAutofill(tabURL: URL, currentRequestId: String)
 
     /// Shows an AddressAutofill view for selecting addresses in order to autofill forms.
