b6ad688c283c36a545d5700989e867eb28b0761b
Matt Reagan <mreagan@mozilla.com>
2024-02-29 19:31:47 -0800

    [8088] Early work to begin bringing over some code for Content Blocking specific to WebKit. WIP

---
+++ BrowserKit/Sources/WebEngine/WKWebview/WKEngineContentBlocking.swift
@@ -0,0 +1,61 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import Foundation
+import WebKit
+
+enum WKContentBlocklistFileName: String, CaseIterable {
+    case advertisingURLs = "disconnect-block-advertising"
+    case analyticsURLs = "disconnect-block-analytics"
+    case socialURLs = "disconnect-block-social"
+    case cryptomining = "disconnect-block-cryptomining"
+    case fingerprinting = "disconnect-block-fingerprinting"
+    case advertisingCookies = "disconnect-block-cookies-advertising"
+    case analyticsCookies = "disconnect-block-cookies-analytics"
+    case socialCookies = "disconnect-block-cookies-social"
+
+    static var standardSet: [WKContentBlocklistFileName] {
+        return [
+            .advertisingCookies,
+            .analyticsCookies,
+            .socialCookies,
+            .cryptomining,
+            .fingerprinting
+        ]
+    }
+
+    static var strictSet: [WKContentBlocklistFileName] {
+        return [
+            .advertisingURLs,
+            .analyticsURLs,
+            .socialURLs,
+            cryptomining,
+            fingerprinting
+        ]
+    }
+}
+
+struct WKNoImageModeDefaults {
+    static let Script = "[{'trigger':{'url-filter':'.*','resource-type':['image']},'action':{'type':'block'}}]"
+        .replacingOccurrences(of: "'", with: "\"")
+    static let ScriptName = "images"
+}
+
+class WKContentBlocker {
+    private let ruleStore = WKContentRuleListStore.default()
+    private var blockImagesRule: WKContentRuleList?
+
+    init(blockImagesRule: WKContentRuleList? = nil) {
+        let blockImages = WKNoImageModeDefaults.Script
+        ruleStore?.compileContentRuleList(
+            forIdentifier: WKNoImageModeDefaults.ScriptName,
+            encodedContentRuleList: blockImages) { rule, error in
+                guard error == nil, rule != nil else { return }
+                self.blockImagesRule = rule
+            }
+
+        // TODO: Read safelist during startup
+        // TODO: Clean up and remove old blocker file lists based on date and name, if newer available
+    }
+}
