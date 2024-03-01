22abc17a3c6604c22efd5d7ac191a3819e5b3f04
Matt Reagan <mreagan@mozilla.com>
2024-02-29 19:44:00 -0800

    [8088] More work to begin bringing over WebKit specific content blocking support. WIP

--- BrowserKit/Sources/WebEngine/WKWebview/WKEngineContentBlocking.swift
+++ BrowserKit/Sources/WebEngine/WKWebview/WKEngineContentBlocking.swift
@@ -42,9 +42,17 @@ struct WKNoImageModeDefaults {
     static let ScriptName = "images"
 }
 
+enum WKContentBlockerState {
+    case notReady
+    case ready
+}
+
 class WKContentBlocker {
+    private(set) var state: WKContentBlockerState = .notReady
     private let ruleStore = WKContentRuleListStore.default()
     private var blockImagesRule: WKContentRuleList?
+
+    // MARK: - Initializer
 
     init(blockImagesRule: WKContentRuleList? = nil) {
         ruleStore?.compileContentRuleList(
@@ -56,5 +64,80 @@ class WKContentBlocker {
 
         // TODO: Read safelist during startup
         // TODO: Clean up and remove old blocker file lists based on date and name, if newer available
+        
+        compileListsNotInStore {
+            self.state = .ready
+        }
+    }
+
+    // MARK: - Internal Utility
+
+    private func safelistAsJSON() -> String {
+        // TODO: Compile safelist domains
+
+        /*
+        if safelistedDomains.domainSet.isEmpty {
+            return ""
+        }
+        // Note that * is added to the front of domains, so foo.com becomes *foo.com
+        let list = "'*" + safelistedDomains.domainSet.joined(separator: "','*") + "'"
+         */
+        let list = ""
+
+        return ", {'action': { 'type': 'ignore-previous-rules' }, 'trigger': { 'url-filter': '.*', 'if-domain': [\(list)] }}"
+            .replacingOccurrences(of: "'", with: "\"")
+    }
+
+
+    private func compileListsNotInStore(completion: @escaping () -> Void) {
+        let blocklists = WKContentBlocklistFileName.allCases.map { $0.rawValue }
+        let dispatchGroup = DispatchGroup()
+        blocklists.forEach { filename in
+            dispatchGroup.enter()
+            ruleStore?.lookUpContentRuleList(forIdentifier: filename) { [weak self] contentRuleList, error in
+                if contentRuleList != nil {
+                    dispatchGroup.leave()
+                    return
+                }
+                self?.loadJsonFromBundle(forResource: filename) { jsonString in
+                    var str = jsonString
+                    guard let self,
+                          let range = str.range(of: "]", options: String.CompareOptions.backwards)
+                    else {
+                        dispatchGroup.leave()
+                        return
+                    }
+                    str = str.replacingCharacters(in: range, with: self.safelistAsJSON() + "]")
+                    self.ruleStore?.compileContentRuleList(
+                        forIdentifier: filename,
+                        encodedContentRuleList: str
+                    ) { rule, error in
+                        defer {
+                            dispatchGroup.leave()
+                        }
+                        guard error == nil, rule != nil else { return }
+                    }
+                }
+            }
+        }
+
+        dispatchGroup.notify(queue: .main) {
+            completion()
+        }
+    }
+
+    private func loadJsonFromBundle(forResource file: String, completion: @escaping (_ jsonString: String) -> Void) {
+        DispatchQueue.global().async {
+            guard let path = Bundle.main.path(forResource: file, ofType: "json"),
+                  let source = try? String(contentsOfFile: path, encoding: .utf8)
+            else {
+                assertionFailure("Error unwrapping the resource contents")
+                return
+            }
+
+            DispatchQueue.main.async {
+                completion(source)
+            }
+        }
     }
 }
