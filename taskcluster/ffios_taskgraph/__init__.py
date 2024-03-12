e3874baf8147100f7958944a6698bf611e45c05c
Andrew Halberstadt <ahal@mozilla.com>
2024-03-12 14:38:08 -0400

    Build FXIOS-8134 Convert existing bitrise tasks to use bitrisescript

--- taskcluster/ffios_taskgraph/__init__.py
+++ taskcluster/ffios_taskgraph/__init__.py
@@ -4,12 +4,24 @@
 
 from importlib import import_module
 
+from mozilla_taskgraph import register as register_mozilla_taskgraph
+from taskgraph.util import schema
+
+schema.EXCEPTED_SCHEMA_IDENTIFIERS.append(
+    lambda path: any(
+        exc in path for exc in ("['attributes']",)
+    )
+)
+
 
 def register(graph_config):
     """
     Import all modules that are siblings of this one, triggering decorators in
     the process.
     """
+    # Setup mozilla-taskgraph
+    register_mozilla_taskgraph(graph_config)
+
     _import_modules(["job", "routes", "target_tasks"])
 
 
