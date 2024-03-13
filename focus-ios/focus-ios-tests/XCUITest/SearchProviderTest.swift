e4356cb7902ea9f1e618f673bd9f88ff63501c8d
Michael Kaply <345868+mkaply@users.noreply.github.com>
2024-03-13 13:30:39 -0400

    Update search engines

--- focus-ios/focus-ios-tests/XCUITest/SearchProviderTest.swift
+++ focus-ios/focus-ios-tests/XCUITest/SearchProviderTest.swift
@@ -20,16 +20,9 @@ class SearchProviderTest: BaseTestCase {
         searchProviderTestHelper(provider: "Wikipedia")
     }
 
-    // https://testrail.stage.mozaws.net/index.php?/cases/view/2512722
-    func testAmazonSearchProvider() {
-        searchProviderTestHelper(provider: "Amazon.com")
-    }
-
     // https://testrail.stage.mozaws.net/index.php?/cases/view/2524588
     func testSearchQuery() {
         searchQuery("test", provider: "Google")
-        dismissKeyboardFocusMenuSettings()
-        searchQuery("test", provider: "Amazon.com")
         dismissKeyboardFocusMenuSettings()
         searchQuery("test", provider: "DuckDuckGo")
     }
@@ -173,10 +166,6 @@ class SearchProviderTest: BaseTestCase {
 				waitForExistence(app.otherElements["mozilla at DuckDuckGo"])
 			case "Wikipedia":
 				waitForValueContains(urlbarUrltextTextField, value: "wikipedia.org")
-            case "Amazon.com":
-				waitForValueContains(urlbarUrltextTextField, value: "amazon.com")
-                waitForValueContains(app.webViews.textFields.element(boundBy: 0),
-                    value: searchWord)
 			default:
 				XCTFail("Invalid Search Provider")
 		}
