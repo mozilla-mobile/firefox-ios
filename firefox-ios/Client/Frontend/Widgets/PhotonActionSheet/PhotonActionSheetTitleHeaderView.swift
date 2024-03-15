f0120d68bbfab5745f28ca4a753a2cc55463b746
Sanchit Mittal <sanchitmittal18@gmail.com>
2024-03-16 01:21:59 +0530

    issue-18943 | font changes

--- firefox-ios/Client/Frontend/Widgets/PhotonActionSheet/PhotonActionSheetTitleHeaderView.swift
+++ firefox-ios/Client/Frontend/Widgets/PhotonActionSheet/PhotonActionSheetTitleHeaderView.swift
@@ -13,7 +13,7 @@ class PhotonActionSheetTitleHeaderView: UITableViewHeaderFooterView, ReusableCel
     }
 
     lazy var titleLabel: UILabel = .build { label in
-        label.font = DefaultDynamicFontHelper.preferredFont(withTextStyle: .caption1, size: 12)
+        label.font = FXFontStyles.Regular.caption1.scaledFont()
         label.numberOfLines = 1
     }
 
