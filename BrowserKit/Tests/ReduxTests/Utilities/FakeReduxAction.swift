c20977cdd8595642049f280656a4ce54c17a2295
Matt Reagan <mreagan@mozilla.com>
2024-03-25 09:40:49 -0700

    [8188] Rename test and also fix import issue, will need to troubleshoot this later

--- BrowserKit/Tests/ReduxTests/Utilities/FakeReduxAction.swift
+++ BrowserKit/Tests/ReduxTests/Utilities/FakeReduxAction.swift
@@ -2,9 +2,9 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import Foundation
+import XCTest
 
-@testable import Client
+
 @testable import Redux
 
 enum FakeReduxAction: Action {
@@ -20,6 +20,7 @@ enum FakeReduxAction: Action {
     case setPrivateModeTo(Bool)
 
     var windowUUID: UUID {
-        return .XCTestDefaultUUID
+        // TODO: Update to use static consts on WindowUUID (.XCTestDefaultUUID)
+        return UUID(uuidString: "D9D9D9D9-D9D9-D9D9-D9D9-CD68A019860B")!
     }
 }
