ca8c876f5eef641f7fcb15e15a2d71d1197cfb71
Clare So <cso@mozilla.com>
2024-04-02 00:47:25 -0400

    Align testrail with XCUITest

--- focus-ios/focus-ios-tests/XCUITest/ShortcutsTest.swift
+++ focus-ios/focus-ios-tests/XCUITest/ShortcutsTest.swift
@@ -11,6 +11,7 @@ class ShortcutsTest: BaseTestCase {
         super.tearDown()
     }
 
+    // https://testrail.stage.mozaws.net/index.php?/cases/view/1857439
     func testAddRemoveShortcut() {
         addShortcut(website: "mozilla.org")
 
@@ -28,6 +29,8 @@ class ShortcutsTest: BaseTestCase {
         XCTAssertFalse(app.otherElements.staticTexts["Mozilla"].exists)
     }
 
+    // Smoketest
+    // https://testrail.stage.mozaws.net/index.php?/cases/view/1857438
     func testAddRenameShortcut() {
         addShortcut(website: "mozilla.org")
 
@@ -49,6 +52,8 @@ class ShortcutsTest: BaseTestCase {
         XCTAssertTrue(app.otherElements.staticTexts["Cheese"].exists)
     }
 
+    // Smoketest
+    // https://testrail.stage.mozaws.net/index.php?/cases/view/1857440
     func testShortcutShownWhileTypingURLBar() {
         addShortcut(website: "example.com")
         app.urlTextField.tap()
