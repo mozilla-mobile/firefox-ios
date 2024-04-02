0f640fc42ce75dbf2645a30d8923b88425904912
Razvan Litianu <litianu.razvan@gmail.com>
2024-04-02 10:14:33 +0300

    FXIOS-8544 #18980 Telemetry Probes - Address Autofill Phase 1

--- firefox-ios/firefox-ios-tests/Tests/ClientTests/Autofill/FormAutofillHelperTests.swift
+++ firefox-ios/firefox-ios-tests/Tests/ClientTests/Autofill/FormAutofillHelperTests.swift
@@ -304,6 +304,7 @@ class FormAutofillHelperTests: XCTestCase {
 
         XCTAssertTrue(handlerNames!.contains(FormAutofillHelper.HandlerName.addressFormMessageHandler.rawValue))
         XCTAssertTrue(handlerNames!.contains(FormAutofillHelper.HandlerName.creditCardFormMessageHandler.rawValue))
+        XCTAssertTrue(handlerNames!.contains(FormAutofillHelper.HandlerName.addressFormTelemetryMessageHandler.rawValue))
     }
 
     func testUserContentControllerDidReceiveScriptMessage_withCreditCardHandler() {
