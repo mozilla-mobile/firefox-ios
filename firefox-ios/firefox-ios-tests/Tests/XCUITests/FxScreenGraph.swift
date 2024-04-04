293583597006ddf71c1b14e0abd3f684b487c63f
dragosb01 <134391433+dragosb01@users.noreply.github.com>
2024-04-04 10:49:08 +0300

    Add MTE-2571 - zoom actions validation test (#19462)

--- firefox-ios/firefox-ios-tests/Tests/XCUITests/FxScreenGraph.swift
+++ firefox-ios/firefox-ios-tests/Tests/XCUITests/FxScreenGraph.swift
@@ -55,6 +55,7 @@ let EnterNewBookmarkTitleAndUrl = "EnterNewBookmarkTitleAndUrl"
 let RequestDesktopSite = "RequestDesktopSite"
 let RequestMobileSite = "RequestMobileSite"
 let CreditCardsSettings = "AutofillCreditCard"
+let PageZoom = "PageZoom"
 
 // These are in the exact order they appear in the settings
 // screen. XCUIApplication loses them on small screens.
@@ -1022,6 +1023,10 @@ func createScreenGraph(for test: XCTestCase, with app: XCUIApplication) -> MMScr
         screenState.tap(app.buttons["FindInPage.close"], to: BrowserTab)
     }
 
+    map.addScreenState(PageZoom) { screenState in
+        screenState.tap(app.buttons["FindInPage.closeButton"], to: BrowserTab)
+    }
+
     map.addScreenState(RequestDesktopSite) { _ in }
 
     map.addScreenState(RequestMobileSite) { _ in }
@@ -1127,6 +1132,10 @@ func createScreenGraph(for test: XCTestCase, with app: XCUIApplication) -> MMScr
         screenState.tap(
             app.tables.otherElements[StandardImageIdentifiers.Large.search],
             to: FindInPage
+        )
+        screenState.tap(
+            app.tables.otherElements[StandardImageIdentifiers.Large.pageZoom],
+            to: PageZoom
         )
         // TODO: Add new state
 //        screenState.tap(
