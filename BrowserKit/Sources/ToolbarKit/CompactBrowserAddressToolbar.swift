5cc2e83ce9e01ac4a0b8141343857c5475d4a540
Winnie Teichmann <4530+thatswinnie@users.noreply.github.com>
2024-04-09 10:04:16 +0200

    Add CompactBrowserAddressToolbar

---
+++ BrowserKit/Sources/ToolbarKit/CompactBrowserAddressToolbar.swift
@@ -0,0 +1,17 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import UIKit
+
+public class CompactBrowserAddressToolbar: BrowserAddressToolbar {
+
+    internal override func updateActions(state: AddressToolbarState) {
+        // In compact mode no browser actions will be displayed
+        let compactState = AddressToolbarState(url: state.url,
+                                               navigationActions: state.navigationActions,
+                                               pageActions: state.pageActions,
+                                               browserActions: [])
+        super.updateActions(state: compactState)
+    }
+}
