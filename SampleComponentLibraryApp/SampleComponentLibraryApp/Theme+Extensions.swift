4beabfe5a3cbbb075b29636722009afd7963bc3c
Cyndi Chin <cgc95@cornell.edu>
2024-04-10 16:41:37 -0400

    Refactor FXIOS-8936 Sample Component Library to incorporate UUID-based themes (#19740)

---
+++ SampleComponentLibraryApp/SampleComponentLibraryApp/Theme+Extensions.swift
@@ -0,0 +1,20 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import Foundation
+import Common
+
+let defaultSampleComponentUUID = UUID(uuidString: "44BA0B7D-097A-484D-8358-91A6E374451D")!
+
+extension ThemeManager {
+    var currentTheme: Theme {
+        return currentTheme(for: defaultSampleComponentUUID)
+    }
+}
+
+extension Themeable {
+    public var currentWindowUUID: UUID? {
+        defaultSampleComponentUUID
+    }
+}
