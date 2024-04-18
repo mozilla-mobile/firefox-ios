7b5e7442484b09dcefd3a25cbf9f97e0c744c379
Sanchit Mittal <sanchitmittal18@gmail.com>
2024-04-18 17:02:16 +0530

    issue-19526 | Swiftlint: force_try

--- focus-ios/Blockzilla/Extensions/StringExtensions.swift
+++ focus-ios/Blockzilla/Extensions/StringExtensions.swift
@@ -23,12 +23,16 @@ fileprivate extension String {
 
 extension String {
     var isUrl: Bool {
-        let detector = try! NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue)
-        guard let match = detector.firstMatch(in: self, range: NSRange(location: 0, length: self.count)), match.range.length == self.count else {
-            return false
-        }
+        do {
+            let detector = try NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue)
+            guard let match = detector.firstMatch(in: self, range: NSRange(location: 0, length: self.count)), match.range.length == self.count else {
+                return false
+            }
 
-        return true
+            return true
+        } catch {
+            fatalError("Couldn't find data detector for type link")
+        }
     }
 
     fileprivate func toValue(_ index: Int) -> Character {
