648b051b42a3938a61eb7a1a14d630fecb0272fe
Winnie Teichmann <4530+thatswinnie@users.noreply.github.com>
2024-04-22 16:02:23 +0200

    Add FXIOS-8744 Create navigation toolbar (#19880)
    * Add navigation toolbar protocol
    
    * Add navigation toolbar state
    
    * Add navigation toolbar
    
    * Create navigation toolbar container and model
    
    * Use navigation toolbar in sample project and add model for root vc
    
    * Fix button enabled
    
    * Rename method
    
    * Update icons
    
    * Cleanup
    
    * Make sure toolbar has the correct height
    
    * Add menu button to navigation toolbar
    
    * Remove old toolbar and move + rename delegate
    
    * Move address toolbar model to root vc model & cleanup
    
    * Fix swiftlint warnings
    
    * Revert deleting of Package.resolved
    
    * Remove axis
    
    * Rename delegate method to tapped instead of clicked
    
    * Rename Click > Tap

---
+++ SampleBrowser/SampleBrowser/Model/RootViewControllerModel.swift
@@ -0,0 +1,100 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import Foundation
+import Common
+import ToolbarKit
+
+class RootViewControllerModel {
+    // By default the state is set to reload. We save the state to avoid setting the toolbar
+    // button multiple times when a page load is in progress
+    private var isReloading = false
+    private var canGoBack = false
+    private var canGoForward = false
+
+    var navigationToolbarDelegate: NavigationToolbarDelegate?
+    var addressToolbarDelegate: AddressToolbarContainerDelegate?
+
+    // MARK: - Navigation toolbar
+    var navigationToolbarContainerModel: NavigationToolbarContainerModel {
+        let backButton = ToolbarElement(
+            iconName: "Back",
+            isEnabled: canGoBack,
+            a11yLabel: "Navigate Back",
+            a11yId: "backButton",
+            onSelected: {
+                self.navigationToolbarDelegate?.backButtonTapped()
+            })
+        let forwardButton = ToolbarElement(
+            iconName: "Forward",
+            isEnabled: canGoForward,
+            a11yLabel: "Navigate Forward",
+            a11yId: "forwardButton",
+            onSelected: {
+                self.navigationToolbarDelegate?.forwardButtonTapped()
+            })
+        let reloadButton = ToolbarElement(
+            iconName: isReloading ? StandardImageIdentifiers.Large.cross : StandardImageIdentifiers.Large.sync,
+            isEnabled: isReloading,
+            a11yLabel: isReloading ? "Stop loading website" : "Reload website",
+            a11yId: isReloading ? "stopButton" : "reloadButton",
+            onSelected: {
+                if self.isReloading {
+                    self.navigationToolbarDelegate?.stopButtonTapped()
+                } else {
+                    self.navigationToolbarDelegate?.reloadButtonTapped()
+                }
+            })
+        let menuButton = ToolbarElement(
+            iconName: StandardImageIdentifiers.Large.appMenu,
+            isEnabled: true,
+            a11yLabel: "Open Menu",
+            a11yId: "appMenuButton",
+            onSelected: {
+                self.navigationToolbarDelegate?.menuButtonTapped()
+            })
+        let actions = [backButton, forwardButton, reloadButton, menuButton]
+
+        return NavigationToolbarContainerModel(actions: actions)
+    }
+
+    func updateReloadStopButton(loading: Bool) {
+        guard loading != isReloading else { return }
+        self.isReloading = loading
+    }
+
+    func updateBackForwardButtons(canGoBack: Bool, canGoForward: Bool) {
+        self.canGoBack = canGoBack
+        self.canGoForward = canGoForward
+    }
+
+    // MARK: - Address toolbar
+    func addressToolbarContainerModel(url: String?) -> AddressToolbarContainerModel {
+        let pageActions = [ToolbarElement(
+            iconName: StandardImageIdentifiers.Large.qrCode,
+            isEnabled: true,
+            a11yLabel: "Read QR Code",
+            a11yId: "qrCodeButton",
+            onSelected: nil)]
+
+        let browserActions = [ToolbarElement(
+            iconName: StandardImageIdentifiers.Large.appMenu,
+            isEnabled: true,
+            a11yLabel: "Open Menu",
+            a11yId: "appMenuButton",
+            onSelected: {
+                self.addressToolbarDelegate?.didTapMenu()
+            })]
+
+        // FXIOS-8947: Use scroll position
+        return AddressToolbarContainerModel(
+            toolbarPosition: .top,
+            scrollY: 0,
+            isPrivate: false,
+            url: url,
+            navigationActions: [],
+            pageActions: pageActions,
+            browserActions: browserActions)
+    }
+}
