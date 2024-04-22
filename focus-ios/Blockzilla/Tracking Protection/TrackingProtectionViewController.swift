fa9cee825620fe56ba35c0ff3ccf54c6ca085d72
cavaevinci <112208299+cavaevinci@users.noreply.github.com>
2024-04-22 08:56:17 +0200

    Add FXIOS-8813 [SwiftLint] compiler_protocol_init (#19897)

--- focus-ios/Blockzilla/Tracking Protection/TrackingProtectionViewController.swift
+++ focus-ios/Blockzilla/Tracking Protection/TrackingProtectionViewController.swift
@@ -365,10 +365,10 @@ class TrackingProtectionViewController: UIViewController {
     }
 
     private func getNumberOfTrackersBlocked() -> String {
-        let numberOfTrackersBlocked = NSNumber(integerLiteral: UserDefaults.standard.integer(forKey: BrowserViewController.userDefaultsTrackersBlockedKey))
+        let numberOfTrackersBlocked = UserDefaults.standard.integer(forKey: BrowserViewController.userDefaultsTrackersBlockedKey)
         let formatter = NumberFormatter()
         formatter.numberStyle = .decimal
-        return formatter.string(from: numberOfTrackersBlocked) ?? "0"
+        return formatter.string(from: NSNumber(value: numberOfTrackersBlocked)) ?? "0"
     }
 
     private func toggleProtection(isOn: Bool) {
