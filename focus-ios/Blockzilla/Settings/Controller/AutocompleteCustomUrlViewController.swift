7b41f0f8d365cd405d6c72955fac219e5faf7ad9
Ione Souza Junior <junior@ionixjunior.com.br>
2024-04-23 09:53:04 -0300

    Refactor FXIOS-8883 - Enabled SwiftLint overridden_super_call (#19941)
    * Remove comment from overridden_super_call
    
    * Add super call on viewDidLoad
    
    * Add super call on viewDidLoad
    
    * Add super call on viewDidLoad
    
    * Add super call on viewDidLoad
    
    * Add super call on prepareForReuse
    
    * Add super call on prepareForReuse
    
    * Add super call on viewDidLoad
    
    * Add super call on viewDidLoad
    
    * Add super call on viewDidLoad
    
    * Add super call on viewDidLoad
    
    * Add super call on viewDidLoad
    
    * Add super call on viewDidLoad

--- focus-ios/Blockzilla/Settings/Controller/AutocompleteCustomUrlViewController.swift
+++ focus-ios/Blockzilla/Settings/Controller/AutocompleteCustomUrlViewController.swift
@@ -56,6 +56,7 @@ class AutocompleteCustomUrlViewController: UIViewController {
     }
 
     override func viewDidLoad() {
+        super.viewDidLoad()
         title = UIConstants.strings.autocompleteManageSitesLabel
 
         tableView.dataSource = self
