6f13621d545e7385d65b93fb164a87b2a4b6270f
Matt Reagan <mreagan@mozilla.com>
2024-04-25 11:31:09 -0700

    [7957] Cleanup and comments

--- firefox-ios/Client/ApplicationHelper.swift
+++ firefox-ios/Client/ApplicationHelper.swift
@@ -16,13 +16,26 @@ struct DefaultApplicationHelper: ApplicationHelper {
     func openSettings() {
         UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!, options: [:])
     }
-
+    
+    /// Convenience. Opens a URL with the application.
+    ///
+    /// On iPadOS if more than one window is open, iOS will
+    /// determine which UIScene the URL is delivered to.
+    /// - Parameter url: the URL to open.
     func open(_ url: URL) {
         UIApplication.shared.open(url)
     }
-
+    
+    /// Opens a URL within a specific target window (UIScene).
+    ///
+    /// Opens a URL within a specific window on iPadOS, identified
+    /// with the provided window UUID. If no window matching the
+    /// UUID is found, the URL is opened as usual via UIApplication.open().
+    ///
+    /// - Parameters:
+    ///   - url: the URL to open.
+    ///   - inWindow: the UUID of the window to open the URL.
     func open(_ url: URL, inWindow: WindowUUID) {
-        let windowManager: WindowManager = AppContainer.shared.resolve()
         let targetScene = UIApplication.shared.connectedScenes.contains(where: {
             guard let delegate = $0.delegate as? SceneDelegate,
                   delegate.sceneCoordinator?.windowUUID == inWindow else { return false }
