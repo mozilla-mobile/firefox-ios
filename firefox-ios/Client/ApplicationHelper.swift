4d24461ae4ecbe7c8c26ffc28b6e10681fc92256
Matt Reagan <mreagan@mozilla.com>
2024-04-25 10:02:16 -0700

    [7957] Add support for routing URLs to a specific UIScene

--- firefox-ios/Client/ApplicationHelper.swift
+++ firefox-ios/Client/ApplicationHelper.swift
@@ -3,10 +3,12 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
 import Foundation
+import Common
 
 protocol ApplicationHelper {
     func openSettings()
     func open(_ url: URL)
+    func open(_ url: URL, inWindow: WindowUUID)
 }
 
 /// UIApplication.shared wrapper
@@ -17,5 +19,18 @@ struct DefaultApplicationHelper: ApplicationHelper {
 
     func open(_ url: URL) {
         UIApplication.shared.open(url)
+    }
+
+    func open(_ url: URL, inWindow: WindowUUID) {
+        let windowManager: WindowManager = AppContainer.shared.resolve()
+        let targetScene = UIApplication.shared.connectedScenes.contains(where: {
+            guard let delegate = $0.delegate as? SceneDelegate,
+                  delegate.sceneCoordinator?.windowUUID == inWindow else { return false }
+            delegate.handleOpenURL(url)
+            return true
+        })
+        if targetScene == nil {
+            open(url)
+        }
     }
 }
