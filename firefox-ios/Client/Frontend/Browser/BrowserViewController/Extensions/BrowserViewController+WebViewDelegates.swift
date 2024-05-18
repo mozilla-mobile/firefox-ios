14c8f07e4d4a94a25a0172182287eb5847deba71
Ione Souza Junior <junior@ionixjunior.com.br>
2024-05-18 20:36:43 -0300

    Get actions using builder

--- firefox-ios/Client/Frontend/Browser/BrowserViewController/Extensions/BrowserViewController+WebViewDelegates.swift
+++ firefox-ios/Client/Frontend/Browser/BrowserViewController/Extensions/BrowserViewController+WebViewDelegates.swift
@@ -418,7 +418,6 @@ extension BrowserViewController: WKUIDelegate {
                     }
 
                     let actionBuilder = ActionProviderBuilder()
-                    var actions = [UIAction]()
 
                     if !isPrivate {
                         actionBuilder.addOpenInNewTab(url: url, addTab: addTab)
@@ -461,6 +460,7 @@ extension BrowserViewController: WKUIDelegate {
                         actionBuilder.addCopyImageLink(url: url)
                     }
 
+                    let actions = actionBuilder.build()
                     return UIMenu(title: url.absoluteString, children: actions)
                 })
         )
