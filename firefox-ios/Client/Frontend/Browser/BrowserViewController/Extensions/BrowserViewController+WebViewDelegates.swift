8e7e5fc9277df00fed889e35e8cb00deb4b7824c
Ione Souza Junior <junior@ionixjunior.com.br>
2024-05-18 20:58:45 -0300

    Revert "Extract the verification if can handle actions to a new method"
    This reverts commit 1fb97a127ae065dd6c8526a3501cd91ee47dd0ea.

--- firefox-ios/Client/Frontend/Browser/BrowserViewController/Extensions/BrowserViewController+WebViewDelegates.swift
+++ firefox-ios/Client/Frontend/Browser/BrowserViewController/Extensions/BrowserViewController+WebViewDelegates.swift
@@ -429,19 +429,6 @@ extension BrowserViewController: WKUIDelegate {
         )
     }
 
-    func canHandleActions(elementInfo: WKContextMenuElementInfo) -> Bool {
-        if let url = elementInfo.linkURL,
-           let currentTab = self.tabManager.selectedTab,
-           let contextHelper = currentTab.getContentScript(
-               name: ContextMenuHelper.name()
-           ) as? ContextMenuHelper,
-           let elements = contextHelper.elements {
-            return true
-        }
-
-        return false
-    }
-
     func canTrackAds(tab: Tab, adUrl: String) -> Bool {
         return tab == self.tabManager.selectedTab &&
                !tab.adsTelemetryUrlList.isEmpty &&
