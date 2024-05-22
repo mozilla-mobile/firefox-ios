22a9f669f2bd5a6f91fe27949da354f761b43c5b
Alexandru Farcasanu <alex.farcasanu@yahoo.com>
2024-05-22 10:36:14 +0300

    FXIOS-9064 #20076 ⁃ Investigate logic of "Open link in external app""
    Updated the keys for strings

--- firefox-ios/Client/Frontend/Strings.swift
+++ firefox-ios/Client/Frontend/Strings.swift
@@ -2967,7 +2967,7 @@ extension String {
         value: "Offer to Open Copied Links",
         comment: "Title of setting to enable the Go to Copied URL feature. See https://bug1223660.bmoattachments.org/attachment.cgi?id=8898349")
     public static let SettingsOfferClipboardBarStatus = MZLocalizedString(
-        key: "Settings.OfferClipboardBar.Status",
+        key: "Settings.OfferClipboardBar.StatusV2",
         tableName: nil,
         value: "When opening Firefox",
         comment: "Description displayed under the ”Offer to Open Copied Link” option. See https://bug1223660.bmoattachments.org/attachment.cgi?id=8898349")
@@ -2981,7 +2981,7 @@ extension String {
         value: "Show Link Previews",
         comment: "Title of setting to enable link previews when long-pressing links.")
     public static let SettingsShowLinkPreviewsStatus = MZLocalizedString(
-        key: "Settings.ShowLinkPreviews.Status",
+        key: "Settings.ShowLinkPreviews.StatusV2",
         tableName: nil,
         value: "When long-pressing links",
         comment: "Description displayed under the ”Show Link Previews” option")
