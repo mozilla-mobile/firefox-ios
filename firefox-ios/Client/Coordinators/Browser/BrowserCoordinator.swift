37ce262b120f6ff3c1483b7ef1636e8a988fcf2e
cyndichin <cgc95@cornell.edu>
2024-05-31 14:07:27 -0400

    Add FXIOS-9255 [Microsurvey] UI refinements

--- firefox-ios/Client/Coordinators/Browser/BrowserCoordinator.swift
+++ firefox-ios/Client/Coordinators/Browser/BrowserCoordinator.swift
@@ -681,6 +681,8 @@ class BrowserCoordinator: BaseCoordinator,
         }
 
         let navigationController = DismissableNavigationViewController()
+        navigationController.sheetPresentationController?.detents = [.medium(), .large()]
+        navigationController.sheetPresentationController?.prefersGrabberVisible = true
         let coordinator = MicrosurveyCoordinator(
             model: model,
             router: DefaultRouter(
