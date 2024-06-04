de2583639737e41cf75d99b88b66d5a7b6d80e33
Cyndi Chin <cgc95@cornell.edu>
2024-06-04 15:19:59 -0400

    Add FXIOS-9058 [Microsurvey] Target feature for behavioral targeting (#20543)

---
+++ firefox-ios/Client/Experiments/BehavioralTargetingEvent.swift
@@ -0,0 +1,11 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import Foundation
+
+struct BehavioralTargetingEvent {
+    static let syncLoginCompletion = "sync.login_completed_view"
+    static let homepageViewed = "homepage_viewed"
+    static let appForeground = "app_cycle.foreground"
+}
