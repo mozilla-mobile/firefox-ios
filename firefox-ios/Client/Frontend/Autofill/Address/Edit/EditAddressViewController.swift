bbc8a366b82ef76f1cdb50750665d42086b61ab9
Ione Souza Junior <junior@ionixjunior.com.br>
2024-06-11 21:21:36 -0300

    Extract log error block to a new function

--- firefox-ios/Client/Frontend/Autofill/Address/Edit/EditAddressViewController.swift
+++ firefox-ios/Client/Frontend/Autofill/Address/Edit/EditAddressViewController.swift
@@ -86,6 +86,15 @@ class EditAddressViewController: UIViewController, WKNavigationDelegate, WKScrip
         }
     }
 
+    private func logError(message: String, description: String) {
+        logger.log(
+            message,
+            level: .warning,
+            category: .autofill,
+            description: description
+        )
+    }
+
     func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {}
 
     func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
