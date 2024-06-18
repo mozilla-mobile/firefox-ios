3624aa6b0f89265297ef2c15ac22e4e6eb6b4abc
Winnie Teichmann <4530+thatswinnie@users.noreply.github.com>
2024-06-18 11:44:49 +0200

    Call new reducers

--- firefox-ios/Client/Frontend/Browser/Toolbars/Redux/ToolbarState.swift
+++ firefox-ios/Client/Frontend/Browser/Toolbars/Redux/ToolbarState.swift
@@ -59,17 +59,11 @@ struct ToolbarState: ScreenState, Equatable {
     }
 
     init(windowUUID: WindowUUID) {
-        let addressToolbar = AddressState(navigationActions: [],
-                                          pageActions: [],
-                                          browserActions: [],
-                                          displayTopBorder: false,
-                                          displayBottomBorder: false,
-                                          url: nil)
         self.init(
             windowUUID: windowUUID,
             toolbarPosition: .top,
-            addressToolbar: addressToolbar,
-            navigationToolbar: NavigationState(actions: [], displayBorder: false)
+            addressToolbar: AddressState(windowUUID: windowUUID),
+            navigationToolbar: NavigationState(windowUUID: windowUUID)
         )
     }
 
@@ -92,72 +86,23 @@ struct ToolbarState: ScreenState, Equatable {
         else { return state }
 
         switch action.actionType {
-        case ToolbarActionType.didLoadToolbars:
-            guard let navToolbarModel = action.navigationToolbarModel,
-                    let addressToolbarModel = action.addressToolbarModel
-            else { return state }
-
-            var state = state
-            state.addressToolbar.navigationActions = addressToolbarModel.navigationActions
-            state.addressToolbar.pageActions = addressToolbarModel.pageActions
-            state.addressToolbar.browserActions = addressToolbarModel.browserActions
-            state.addressToolbar.displayTopBorder = addressToolbarModel.displayTopBorder
-            state.addressToolbar.displayBottomBorder = addressToolbarModel.displayBottomBorder
-            state.addressToolbar.url = addressToolbarModel.url
-
-            state.navigationToolbar.actions = navToolbarModel.actions
-            state.navigationToolbar.displayBorder = navToolbarModel.displayBorder
-            return state
-
-        case ToolbarActionType.numberOfTabsChanged:
-            guard let numberOfTabs = action.numberOfTabs else { return state }
-            var state = state
-
-            if let index = state.navigationToolbar.actions.firstIndex(where: { $0.actionType == .tabs }) {
-                state.navigationToolbar.actions[index].numberOfTabs = numberOfTabs
-            }
-
-            if let index = state.addressToolbar.browserActions.firstIndex(where: { $0.actionType == .tabs }) {
-                state.addressToolbar.browserActions[index].numberOfTabs = numberOfTabs
-            }
-
-            return state
-
-        case ToolbarActionType.urlDidChange:
-            var state = state
-            state.addressToolbar.url = action.url
-            return state
-
-        case ToolbarActionType.backButtonStateChanged:
-            guard let isEnabled = action.isButtonEnabled else { return state }
-            var state = state
-
-            if let index = state.navigationToolbar.actions.firstIndex(where: { $0.actionType == .back }) {
-                state.navigationToolbar.actions[index].isEnabled = isEnabled
-            }
-
-            if let index = state.addressToolbar.browserActions.firstIndex(where: { $0.actionType == .back }) {
-                state.addressToolbar.navigationActions[index].isEnabled = isEnabled
-            }
-
-            return state
-
-        case ToolbarActionType.forwardButtonStateChanged:
-            guard let isEnabled = action.isButtonEnabled else { return state }
-            var state = state
-
-            if let index = state.navigationToolbar.actions.firstIndex(where: { $0.actionType == .forward }) {
-                state.navigationToolbar.actions[index].isEnabled = isEnabled
-            }
-
-            if let index = state.addressToolbar.browserActions.firstIndex(where: { $0.actionType == .forward }) {
-                state.addressToolbar.navigationActions[index].isEnabled = isEnabled
-            }
-
-            return state
+        case ToolbarActionType.didLoadToolbars,
+            ToolbarActionType.numberOfTabsChanged,
+            ToolbarActionType.urlDidChange,
+            ToolbarActionType.backButtonStateChanged,
+            ToolbarActionType.forwardButtonStateChanged:
+            return ToolbarState(
+                windowUUID: state.windowUUID,
+                toolbarPosition: state.toolbarPosition,
+                addressToolbar: AddressState.reducer(state.addressToolbar, action),
+                navigationToolbar: NavigationState.reducer(state.navigationToolbar, action))
 
         default:
-            return state
+            return ToolbarState(
+                windowUUID: state.windowUUID,
+                toolbarPosition: state.toolbarPosition,
+                addressToolbar: state.addressToolbar,
+                navigationToolbar: NavigationState.reducer(state.navigationToolbar, action))
         }
     }
 }
