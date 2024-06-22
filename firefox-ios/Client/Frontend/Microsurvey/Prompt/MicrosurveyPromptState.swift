fcd140b9615dbd0066a11211c2d0eb0bf59612b8
Ione Souza Junior <junior@ionixjunior.com.br>
2024-06-22 13:32:30 -0300

    Use the new function to handle initialize action

--- firefox-ios/Client/Frontend/Microsurvey/Prompt/MicrosurveyPromptState.swift
+++ firefox-ios/Client/Frontend/Microsurvey/Prompt/MicrosurveyPromptState.swift
@@ -38,13 +38,7 @@ struct MicrosurveyPromptState: StateType, Equatable {
 
         switch action.actionType {
         case MicrosurveyPromptMiddlewareActionType.initialize:
-            let model = (action as? MicrosurveyPromptMiddlewareAction)?.microsurveyModel
-            return MicrosurveyPromptState(
-                windowUUID: state.windowUUID,
-                showPrompt: true,
-                showSurvey: false,
-                model: model
-            )
+            return handleInitialize(state: state, action: action)
         case MicrosurveyPromptActionType.closePrompt:
             return MicrosurveyPromptState(
                 windowUUID: state.windowUUID,
