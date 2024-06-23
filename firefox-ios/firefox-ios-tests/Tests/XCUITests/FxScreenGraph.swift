aad1dfada023c984383bc035bcc030ccb47e895f
Ione Souza Junior <junior@ionixjunior.com.br>
2024-06-22 21:30:12 -0300

    Extract tap device mobile image to a new function

--- firefox-ios/firefox-ios-tests/Tests/XCUITests/FxScreenGraph.swift
+++ firefox-ios/firefox-ios-tests/Tests/XCUITests/FxScreenGraph.swift
@@ -1252,6 +1252,13 @@ func createScreenGraph(for test: XCTestCase, with app: XCUIApplication) -> MMScr
         )
     }
 
+    func tapDeviceMobileImage(_ screenState: MMScreenStateNode<FxUserState>) {
+        screenState.tap(
+            app.tables.otherElements[StandardImageIdentifiers.Large.deviceMobile],
+            to: RequestMobileSite
+        )
+    }
+
     map.addScreenState(TabsSettings) { screenState in
         screenState.tap(app.switches.element(boundBy: 0), forAction: Action.ToggleInactiveTabs)
         screenState.tap(app.switches.element(boundBy: 1), forAction: Action.ToggleTabGroups)
