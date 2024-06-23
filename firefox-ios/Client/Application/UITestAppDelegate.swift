464d792a9323f4c363caa4a368a89850b0af05e2
Ione Souza Junior <junior@ionixjunior.com.br>
2024-06-22 23:49:54 -0300

    Change access modifier

--- firefox-ios/Client/Application/UITestAppDelegate.swift
+++ firefox-ios/Client/Application/UITestAppDelegate.swift
@@ -157,7 +157,7 @@ class UITestAppDelegate: AppDelegate, FeatureFlaggable {
         UserDefaults.standard.setValue(false, forKey: PrefsKeys.PlacesHistoryMigrationSucceeded)
     }
 
-    fileprivate func configureTabs(_ arg: String, launchArguments: [String], dirForTestProfile: String) {
+    private func configureTabs(_ arg: String, launchArguments: [String], dirForTestProfile: String) {
         let tabDirectory = "\(self.appRootDir())/profile.profile"
         if launchArguments.contains(LaunchArguments.ClearProfile) {
             fatalError("Clearing profile and loading tabs, not a supported combination.")
