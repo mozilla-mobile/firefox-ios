4ebf9ffa81148f8b86c15a9366f7acdfdf2cae97
Ione Souza Junior <junior@ionixjunior.com.br>
2024-06-23 00:03:12 -0300

    Change access modifier

--- firefox-ios/Client/Frontend/Fakespot/FakespotState.swift
+++ firefox-ios/Client/Frontend/Fakespot/FakespotState.swift
@@ -168,7 +168,7 @@ struct FakespotState: ScreenState, Equatable {
         return state
     }
 
-    fileprivate static func handlePressedShopping(state: FakespotState) -> FakespotState {
+    private static func handlePressedShopping(state: FakespotState) -> FakespotState {
         var state = state
         state.isOpen = !state.isOpen
         state.sidebarOpenForiPadLandscape = state.isOpen
