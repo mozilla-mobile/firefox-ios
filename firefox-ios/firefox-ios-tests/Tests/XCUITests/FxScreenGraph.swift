71afe0def6c00915662ec16b41d2a613697a6b86
Ione Souza Junior <junior@ionixjunior.com.br>
2024-06-22 21:35:08 -0300

    Extract tap link image to a new function

--- firefox-ios/firefox-ios-tests/Tests/XCUITests/FxScreenGraph.swift
+++ firefox-ios/firefox-ios-tests/Tests/XCUITests/FxScreenGraph.swift
@@ -1268,6 +1268,13 @@ func createScreenGraph(for test: XCTestCase, with app: XCUIApplication) -> MMScr
         )
     }
 
+    func tapLinkImage(_ screenState: MMScreenStateNode<FxUserState>) {
+        screenState.tap(
+            app.tables.otherElements[StandardImageIdentifiers.Large.link],
+            forAction: Action.CopyAddressPAM
+        )
+    }
+
     map.addScreenState(TabsSettings) { screenState in
         screenState.tap(app.switches.element(boundBy: 0), forAction: Action.ToggleInactiveTabs)
         screenState.tap(app.switches.element(boundBy: 1), forAction: Action.ToggleTabGroups)
