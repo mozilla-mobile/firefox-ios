225554808055de0c3cd85aa83dcb7e3371cb353c
Ione Souza Junior <junior@ionixjunior.com.br>
2024-06-22 23:09:18 -0300

    Use the new function to handle expected key

--- firefox-ios/Storage/Rust/RustAutofill.swift
+++ firefox-ios/Storage/Rust/RustAutofill.swift
@@ -448,30 +448,7 @@ public class RustAutofill {
         getKeychainData(rustKeys: rustKeys) { (key, encryptedCanaryPhrase) in
             switch (key, encryptedCanaryPhrase) {
             case (.some(key), .some(encryptedCanaryPhrase)):
-                // We expected the key to be present, and it is.
-                var canaryIsValid = false
-                do {
-                    canaryIsValid = try rustKeys.checkCanary(
-                        canary: encryptedCanaryPhrase!,
-                        text: rustKeys.canaryPhrase,
-                        key: key!
-                    )
-                } catch let error as NSError {
-                    self.logger.log("Error validating autofill encryption key",
-                                    level: .warning,
-                                    category: .storage,
-                                    description: error.localizedDescription)
-                    completion(.failure(error))
-                    return
-                }
-                if canaryIsValid {
-                    completion(.success(key!))
-                } else {
-                    self.logger.log("Autofill key was corrupted, new one generated",
-                                    level: .warning,
-                                    category: .storage)
-                    self.resetCreditCardsAndKey(rustKeys: rustKeys, completion: completion)
-                }
+                self.handleExpectedKey(rustKeys: rustKeys, encryptedCanaryPhrase: encryptedCanaryPhrase, key: key, completion: completion)
             case (.some(key), .none), (.none, .some(encryptedCanaryPhrase)):
                 // The key is present, but we didn't expect it to be there.
                 // or
