b8ed4e24b13edd0a072d9edf0758fde0a2d97283
mattlichtenstein <lichtensteinmp@gmail.com>
2024-07-02 10:44:02 -0400

    make spacer conditional

--- firefox-ios/Client/Frontend/Autofill/Address/AddressAutofillSettingsView.swift
+++ firefox-ios/Client/Frontend/Autofill/Address/AddressAutofillSettingsView.swift
@@ -37,10 +37,12 @@ struct AddressAutofillSettingsView: View {
                     .padding(.top, 25)
                     .frame(maxWidth: .infinity)
 
-                // Address list view
-                AddressListView(windowUUID: windowUUID, viewModel: addressListViewModel)
-
-                Spacer()
+                if addressListViewModel.showSection || addressListViewModel.isEditingFeatureEnabled {
+                    // Address list view
+                    AddressListView(windowUUID: windowUUID, viewModel: addressListViewModel)
+                } else {
+                    Spacer()
+                }
             }
             .background(viewBackground)
         }
