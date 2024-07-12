8ddee3d7b4cdb3b6489ed4d415306fbf0e5a29b0
Sophie <5545720+dataports@users.noreply.github.com>
2024-07-12 07:55:57 -0700

    Bugfix FXIOS-9103 Fix favicons flickering when tab closes (#20979)
    * Dismiss tab tray outside of Task
    
    * Remove delay in removeTab
    
    * cleanup
    
    ---------
    
    Co-authored-by: Sophie Amin <samin@KPQ47DG99L.lan>
    Co-authored-by: Sophie Amin <samin@KPQ47DG99L.local>

--- firefox-ios/Client/TabManagement/Legacy/LegacyTabManager.swift
+++ firefox-ios/Client/TabManagement/Legacy/LegacyTabManager.swift
@@ -571,9 +571,6 @@ class LegacyTabManager: NSObject, FeatureFlaggable, TabManager, TabEventHandler 
         self.removeTab(tab, flushToDisk: true)
         self.updateIndexAfterRemovalOf(tab, deletedIndex: index, viableTabsIndex: viableTabsIndex)
 
-        // TODO: FXIOS-9084 This is not ideal, follow up in this ticket to make tab selection reasonably synchronous
-        try? await Task.sleep(nanoseconds: NSEC_PER_SEC/10)
-
         TelemetryWrapper.recordEvent(
             category: .action,
             method: .close,
