a428e08543cf874edac9537d3d13b8260469109d
Ione Souza Junior <junior@ionixjunior.com.br>
2024-07-22 19:58:15 -0300

    Implement setUp and tearDown functions

--- firefox-ios/firefox-ios-tests/Tests/ClientTests/Frontend/Browser/Tabs/Legacy/LegacyTabPeekPreviewActionBuilderTests.swift
+++ firefox-ios/firefox-ios-tests/Tests/ClientTests/Frontend/Browser/Tabs/Legacy/LegacyTabPeekPreviewActionBuilderTests.swift
@@ -3,15 +3,18 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
 import XCTest
+@testable import Client
 
 final class LegacyTabPeekPreviewActionBuilderTests: XCTestCase {
+    var builder: LegacyTabPeekPreviewActionBuilder!
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    override func setUp() {
+        super.setUp()
+        builder = LegacyTabPeekPreviewActionBuilder()
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    override func tearDown() {
+        super.tearDown()
+        builder = nil
     }
-
 }
