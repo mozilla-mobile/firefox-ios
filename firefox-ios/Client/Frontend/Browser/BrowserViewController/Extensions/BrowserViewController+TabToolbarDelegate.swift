ea0092c6f13f9eee986dcc2f8713f59c515bc0ce
mattlichtenstein <lichtensteinmp@gmail.com>
2024-07-31 16:03:40 -0400

    Show data clearance button + handle action

--- firefox-ios/Client/Frontend/Browser/BrowserViewController/Extensions/BrowserViewController+TabToolbarDelegate.swift
+++ firefox-ios/Client/Frontend/Browser/BrowserViewController/Extensions/BrowserViewController+TabToolbarDelegate.swift
@@ -43,6 +43,10 @@ extension BrowserViewController: TabToolbarDelegate, PhotonActionSheetProtocol {
 
     // Presents alert to clear users private session data
     func tabToolbarDidPressFire(_ tabToolbar: TabToolbarProtocol, button: UIButton) {
+        didTapOnFire()
+    }
+
+    func didTapOnFire() {
         let alert = UIAlertController(
             title: .Alerts.FeltDeletion.Title,
             message: .Alerts.FeltDeletion.Body,
