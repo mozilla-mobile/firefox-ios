cc9a004daa996170a3976c2df9ffe111d46f611c
PARAIPAN SORIN <51127880+PARAIPAN9@users.noreply.github.com>
2024-08-13 19:21:25 +0300

    Add overlay mode for paste a11y action (#21520)

--- firefox-ios/Client/Frontend/Browser/BrowserViewController/Views/BrowserViewController.swift
+++ firefox-ios/Client/Frontend/Browser/BrowserViewController/Views/BrowserViewController.swift
@@ -751,7 +751,11 @@ class BrowserViewController: UIViewController,
         pasteAction = AccessibleAction(name: .PasteTitle, handler: {  [weak self] () -> Bool in
             guard let self, let pasteboardContents = UIPasteboard.general.string else { return false }
             // Enter overlay mode and make the search controller appear.
-            overlayManager.openSearch(with: pasteboardContents)
+            if isToolbarRefactorEnabled {
+                addressToolbarContainer.enterOverlayMode(pasteboardContents, pasted: true, search: true)
+            } else {
+                overlayManager.openSearch(with: pasteboardContents)
+            }
             searchController?.searchTelemetry?.interactionType = .pasted
             return true
         })
