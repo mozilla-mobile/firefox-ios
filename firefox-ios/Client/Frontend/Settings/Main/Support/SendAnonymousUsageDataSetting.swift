c07069f92d130a5e5cd6584d7e4f71cf5fa451f1
roux g. buciu <11182210+adudenamedruby@users.noreply.github.com>
2024-08-15 04:28:57 -0400

    Refactor - Comment out unused code (#21524)
    * First comments
    
    * comment out tests
    
    * Comment out on Focus
    
    * Fix swiftlint issues
    
    * comment out mocks
    
    * Swiftlints

--- firefox-ios/Client/Frontend/Settings/Main/Support/SendAnonymousUsageDataSetting.swift
+++ firefox-ios/Client/Frontend/Settings/Main/Support/SendAnonymousUsageDataSetting.swift
@@ -40,7 +40,7 @@ class SendAnonymousUsageDataSetting: BoolSetting {
             attributedTitleText: NSAttributedString(string: .SendUsageSettingTitle),
             attributedStatusText: statusText,
             settingDidChange: {
-                AdjustHelper.setEnabled($0)
+//                AdjustHelper.setEnabled($0)
                 DefaultGleanWrapper.shared.setUpload(isEnabled: $0)
                 Experiments.setTelemetrySetting($0)
             }
