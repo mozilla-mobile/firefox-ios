072b582e8cfb5ea78c912e0be67d361f052a5867
Litianu Razvan <litianu.razvan@gmail.com>
2024-08-19 14:43:43 +0300

    Add FXIOS-9655 [Address autofill phase 2] "Manage addresses" link is missing (#21457)
    * FXIOS-9655 #21258 [Address autofill phase 2] "Manage addresses" link is missing
    
    * Remove uuid from view model

--- firefox-ios/Client/Frontend/Onboarding/Protocols/OnboardingCardDelegate.swift
+++ firefox-ios/Client/Frontend/Onboarding/Protocols/OnboardingCardDelegate.swift
@@ -110,7 +110,8 @@ extension OnboardingCardDelegate where Self: OnboardingViewControllerProtocol,
         var bottomSheetViewModel = BottomSheetViewModel(
             closeButtonA11yLabel: .CloseButtonTitle,
             closeButtonA11yIdentifier:
-                AccessibilityIdentifiers.Onboarding.bottomSheetCloseButton)
+                AccessibilityIdentifiers.Onboarding.bottomSheetCloseButton
+        )
         bottomSheetViewModel.shouldDismissForTapOutside = true
         let bottomSheetVC = BottomSheetViewController(
             viewModel: bottomSheetViewModel,
