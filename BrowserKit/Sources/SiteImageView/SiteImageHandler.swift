6a69dd9ea2634af8253d072d08f126f2fcf41760
ih-codes <ihugel@mozilla.com>
2024-08-23 15:40:38 -0600

    Revert SiteImageHandler from actor to class. Add @MainActor annotation on getFaviconImage.

--- BrowserKit/Sources/SiteImageView/SiteImageHandler.swift
+++ BrowserKit/Sources/SiteImageView/SiteImageHandler.swift
@@ -11,14 +11,14 @@ public protocol SiteImageHandler {
     func clearAllCaches()
 }
 
-public actor DefaultSiteImageHandler: SiteImageHandler {
+public class DefaultSiteImageHandler: SiteImageHandler {
     private let urlHandler: FaviconURLHandler
     private let imageHandler: ImageHandler
 
-    /// Right now, multiple `SiteImageView`s each have their own `DefaultSiteImageHandler`. Ideally they'd all share a reference to the same
-    /// `DefaultSiteImageHandler` so we could properly queue and throttle requests to get favicon URLs, download images, etc. Since that's a
-    /// large architectural change, for now lets use a static queue so we can prevent too many duplicate calls to `getFaviconImage`.
-    /// (FXIOS-9830, revised FXIOS-9427 bugfix)
+    /// Right now, multiple `SiteImageView`s each have their own `DefaultSiteImageHandler`. Ideally they'd all share a reference to
+    /// the same `DefaultSiteImageHandler` so we could properly queue and throttle requests to get favicon URLs, download images,
+    /// etc. Since that's a large architectural change, for now lets use a static queue so we can prevent too many duplicate calls to remotely fetching
+    /// URLs and images. (FXIOS-9830, revised FXIOS-9427 bugfix)
     private(set) static var requestQueue: [String: Task<UIImage, Never>] = [:]
 
     public static func factory() -> DefaultSiteImageHandler {
@@ -60,7 +60,7 @@ public actor DefaultSiteImageHandler: SiteImageHandler {
         return imageModel
     }
 
-    nonisolated public func cacheFaviconURL(siteURL: URL?, faviconURL: URL?) {
+    public func cacheFaviconURL(siteURL: URL?, faviconURL: URL?) {
         guard let siteURL = siteURL,
               let faviconURL = faviconURL else {
             return
@@ -71,16 +71,16 @@ public actor DefaultSiteImageHandler: SiteImageHandler {
         urlHandler.cacheFaviconURL(cacheKey: cacheKey, faviconURL: faviconURL)
     }
 
-    public nonisolated func clearAllCaches() {
+    public func clearAllCaches() {
         urlHandler.clearCache()
         imageHandler.clearCache()
     }
 
     // MARK: - Private
 
-    private nonisolated func generateCacheKey(siteURL: URL?,
-                                              faviconURL: URL? = nil,
-                                              type: SiteImageType) -> String {
+    private func generateCacheKey(siteURL: URL?,
+                                  faviconURL: URL? = nil,
+                                  type: SiteImageType) -> String {
         // If we already have a favicon url use the url as the cache key
         if let faviconURL = faviconURL {
             return faviconURL.absoluteString
@@ -106,7 +106,8 @@ public actor DefaultSiteImageHandler: SiteImageHandler {
         }
     }
 
-    // Note: Must be on an actor to avoid duplicating queued requests.
+    // Note: Must be synchronized on an actor to avoid crashes (managing the activeRequest queue).
+    @MainActor
     private func getFaviconImage(imageModel: SiteImageModel) async -> UIImage {
         // Check if we're already awaiting a request to get the favicon image for this cacheKey.
         // If so, simply await that request rather than begin a new one.
@@ -141,7 +142,7 @@ public actor DefaultSiteImageHandler: SiteImageHandler {
         return image
     }
 
-    private nonisolated func generateDomainURL(siteURL: URL) -> ImageDomain {
+    private func generateDomainURL(siteURL: URL) -> ImageDomain {
         let bundleDomains = BundleDomainBuilder().buildDomains(for: siteURL)
         return ImageDomain(bundleDomains: bundleDomains)
     }
