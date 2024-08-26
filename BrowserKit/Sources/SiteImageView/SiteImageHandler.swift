d5043eeb25979110cb501a8f0fe593b8b7e506de
ih-codes <ihugel@mozilla.com>
2024-08-26 13:06:23 -0600

    swiftlint fixes.

--- BrowserKit/Sources/SiteImageView/SiteImageHandler.swift
+++ BrowserKit/Sources/SiteImageView/SiteImageHandler.swift
@@ -15,10 +15,10 @@ public class DefaultSiteImageHandler: SiteImageHandler {
     private let urlHandler: FaviconURLHandler
     private let imageHandler: ImageHandler
 
-    /// Right now, multiple `SiteImageView`s each have their own `DefaultSiteImageHandler`. Ideally they'd all share a reference to
-    /// the same `DefaultSiteImageHandler` so we could properly queue and throttle requests to get favicon URLs, download images,
-    /// etc. Since that's a large architectural change, for now lets use a static queue so we can prevent too many duplicate calls to remotely fetching
-    /// URLs and images. (FXIOS-9830, revised FXIOS-9427 bugfix)
+    /// Right now, multiple `SiteImageView`s each have their own `DefaultSiteImageHandler`. Ideally they'd all share a 
+    /// reference to the same `DefaultSiteImageHandler` so we could properly queue and throttle requests to get favicon
+    /// URLs, download images, etc. Since that's a large architectural change, for now lets use a static queue so we can prevent
+    /// too many duplicate calls to remotely fetching URLs and images. (FXIOS-9830, revised FXIOS-9427 bugfix)
     private(set) static var requestQueue: [String: Task<UIImage, Never>] = [:]
 
     public static func factory() -> DefaultSiteImageHandler {
