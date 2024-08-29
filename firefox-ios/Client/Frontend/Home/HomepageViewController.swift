81965bc37533470530daec0b2eeabcf47060a56d
Winnie Teichmann <4530+thatswinnie@users.noreply.github.com>
2024-08-29 14:59:45 +0100

    Bugfix FXIOS-9842 [Toolbar redesign] Tapping outside the URL bar is not completely closing the keyboard (#21639)
    * End edit mode when keyboard gets dismissed
    
    * Remove unused delegates
    
    * Cancel edit mode if user scrolls homepage

--- firefox-ios/Client/Frontend/Home/HomepageViewController.swift
+++ firefox-ios/Client/Frontend/Home/HomepageViewController.swift
@@ -365,6 +365,10 @@ class HomepageViewController:
     private func dismissKeyboard() {
         if currentTab?.lastKnownUrl?.absoluteString.hasPrefix("internal://") ?? false {
             overlayManager.cancelEditing(shouldCancelLoading: false)
+
+            let action = ToolbarMiddlewareAction(windowUUID: windowUUID,
+                                                 actionType: ToolbarMiddlewareActionType.cancelEdit)
+            store.dispatch(action)
         }
     }
 
@@ -379,6 +383,15 @@ class HomepageViewController:
             statusBarScrollDelegate?.scrollViewDidScroll(scrollView,
                                                          statusBarFrame: statusBarFrame,
                                                          theme: theme)
+        }
+
+        // Only dispatch action when user is in edit mode to avoid having the toolbar re-displayed
+        if featureFlags.isFeatureEnabled(.toolbarRefactor, checking: .buildOnly),
+           let toolbarState = store.state.screenState(ToolbarState.self, for: .toolbar, window: windowUUID),
+           toolbarState.addressToolbar.isEditing {
+            let action = ToolbarMiddlewareAction(windowUUID: windowUUID,
+                                                 actionType: ToolbarMiddlewareActionType.cancelEdit)
+            store.dispatch(action)
         }
 
         let scrolledToTop = lastContentOffsetY > 0 && scrollView.contentOffset.y <= 0
