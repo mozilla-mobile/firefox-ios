129594e793fb4d6ddbdbfcc1479db62231eaecfa
roux g. buciu <github@adudenamedruby.com>
2024-08-29 09:09:27 -0400

    Add dew file

---
+++ BrowserKit/Sources/MenuKit/MenuView.swift
@@ -0,0 +1,20 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import UIKit
+
+final class MainMenuView: UIView {
+    // MARK: - UI Elements
+
+    // MARK: - Properties
+
+    // MARK: - Initializers
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
