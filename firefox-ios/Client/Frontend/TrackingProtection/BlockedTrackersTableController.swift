c1d53a53b19c88488bb4a130238d071582d183d0
Stefan Vladut <svladut@mozilla.com>
2024-09-10 14:15:10 +0300

    removed boilerplate code line

--- firefox-ios/Client/Frontend/TrackingProtection/BlockedTrackersTableController.swift
+++ firefox-ios/Client/Frontend/TrackingProtection/BlockedTrackersTableController.swift
@@ -53,7 +53,6 @@ class BlockedTrackersTableViewController: UIViewController,
         super.init(nibName: nil, bundle: nil)
     }
 
-    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
