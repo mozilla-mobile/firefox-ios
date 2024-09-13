dc4e48e4fbfa2e77520c48fd5adaad1bee9d448e
Razvan Litianu <litianu.razvan@gmail.com>
2024-09-13 15:28:25 +0300

    FXIOS-9975 #21898 [Address autofill phase 2] [iPad] Incorrect display of the location pin

--- firefox-ios/Client/Frontend/Autofill/Address/AddressCellView.swift
+++ firefox-ios/Client/Frontend/Autofill/Address/AddressCellView.swift
@@ -9,6 +9,15 @@ import Storage
 
 import struct MozillaAppServices.Address
 
+extension VerticalAlignment {
+    enum MidAddressIconAndName: AlignmentID {
+        static func defaultValue (in context: ViewDimensions) -> CGFloat {
+            context[.top]
+        }
+    }
+    static let midAddressIconAndName = VerticalAlignment(MidAddressIconAndName.self)
+}
+
 // MARK: - AddressCellView
 
 /// A view representing a cell displaying address information.
@@ -31,17 +40,18 @@ struct AddressCellView: View {
     var body: some View {
         Button(action: onTap) {
             VStack(alignment: .leading, spacing: 0) {
-                HStack(spacing: 24) {
+                HStack(alignment: .midAccountAndName, spacing: 24) {
                     Image(StandardImageIdentifiers.Large.location)
                         .renderingMode(.template)
                         .padding(.leading, 16)
                         .foregroundColor(iconPrimary)
-                        .offset(y: -14)
+                        .alignmentGuide(.midAddressIconAndName) { $0[VerticalAlignment.center] }
                     VStack(alignment: .leading) {
                         if !address.name.isEmpty {
                             Text(address.name)
                                 .font(.body)
                                 .foregroundColor(textColor)
+                                .alignmentGuide(.midAddressIconAndName) { $0[VerticalAlignment.center] }
                         }
                         if !address.streetAddress.isEmpty {
                             Text(address.streetAddress)
