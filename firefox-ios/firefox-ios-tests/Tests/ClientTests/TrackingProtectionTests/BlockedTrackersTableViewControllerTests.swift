6fdec62a67106dd3b298c991f1436ef40ad23aea
petruSt24 <126153378+petruSt24@users.noreply.github.com>
2024-09-19 18:37:13 +0300

    Add FXIOS-9200 enhanced tracking protection blocked trackers controller (#21809)
    * Add FXIOS-9200 enhancedTrackingProtectionBlockedTrackersController (#21607)
    
    * addressed PR comments
    
    * removed boilerplate code line
    
    * addressed PR comments
    
    * removed the TrackingProtection struct and used its contents in the Large struct
    
    * updated old refs
    
    * replaced image PNGs with PDFs, preserved vector data, removed subfolder, split images into corresponding structs
    
    * replaced the Figma PDFs with Acorn PDFs, renamed standard images
    
    * removed forced unwrapping
    
    * fix swiftlint warning

---
+++ firefox-ios/firefox-ios-tests/Tests/ClientTests/TrackingProtectionTests/BlockedTrackersTableViewControllerTests.swift
@@ -0,0 +1,36 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import XCTest
+import Common
+@testable import Client
+
+class BlockedTrackersTableViewControllerTests: XCTestCase {
+    let windowUUID: WindowUUID = .XCTestDefaultUUID
+    let blockedTrackersMockModel = BlockedTrackersTableModel(
+        topLevelDomain: "test.com",
+        title: "test.com",
+        URL: "test.com",
+        contentBlockerStats: TPPageStats(),
+        connectionSecure: true
+    )
+
+    override func setUp() {
+        super.setUp()
+        DependencyHelperMock().bootstrapDependencies()
+    }
+
+    override func tearDown() {
+        DependencyHelperMock().reset()
+        super.tearDown()
+    }
+
+    func testBlockedTrackersViewController_simpleCreation_hasNoLeaks() {
+        let blockedTrackersViewController = BlockedTrackersTableViewController(
+            with: blockedTrackersMockModel,
+            windowUUID: windowUUID
+        )
+        trackForMemoryLeaks(blockedTrackersViewController)
+    }
+}
