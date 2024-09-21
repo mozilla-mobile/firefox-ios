5b759ac170d2e5aad383575e9aac4f7626a080da
Nishant Bhasin <nish.bhasin@gmail.com>
2024-09-20 20:48:48 -0400

    Add FXIOS-10111 Local remote settings records fetching for password generator

---
+++ firefox-ios/Client/Application/RemoteSettings/RemoteDataType.swift
@@ -0,0 +1,68 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import Foundation
+
+enum RemoteDataTypeError: Error, LocalizedError {
+    case fileNotFound(fileName: String)
+    case decodingError(fileName: String, error: Error)
+
+    var errorDescription: String? {
+        switch self {
+        case .fileNotFound(let fileName):
+            return "The file '\(fileName)' was not found in the app bundle."
+        case .decodingError(let fileName, let error):
+            return "Failed to decode '\(fileName)' with \(error.localizedDescription)"
+        }
+    }
+}
+
+enum RemoteDataType: String, Codable {
+    case passwordRules
+
+    var type: RemoteDataTypeRecord.Type {
+        switch self {
+        case .passwordRules:
+            return PasswordRuleRecord.self
+        }
+    }
+
+    var fileName: String {
+        switch self {
+        case .passwordRules:
+            return "RemotePasswordRules"
+        }
+    }
+
+    var name: String {
+        switch self {
+        case .passwordRules:
+            return "Password Rules"
+        }
+    }
+
+    func loadLocalSettingsFromJSON<T: RemoteDataTypeRecord>() async throws -> [T] {
+        let fileName = self.fileName
+
+        guard let path = Bundle.main.path(forResource: fileName, ofType: "json") else {
+            throw RemoteDataTypeError.fileNotFound(fileName: fileName)
+        }
+
+        let url = URL(fileURLWithPath: path)
+
+        do {
+            let data = try Data(contentsOf: url)
+
+            if let decodedArray = try? JSONDecoder().decode([T].self, from: data) {
+                return decodedArray
+            }
+
+            let singleObject = try JSONDecoder().decode(T.self, from: data)
+            return [singleObject]
+        } catch {
+            print("Error: Failed to decode '\(fileName).json'. Error: \(error.localizedDescription)")
+            throw RemoteDataTypeError.decodingError(fileName: fileName, error: error)
+        }
+    }
+}
