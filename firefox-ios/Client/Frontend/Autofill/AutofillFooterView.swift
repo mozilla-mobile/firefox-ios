bb7b5b11ae52204f20fe9cc74a2c826c80320ffa
PARAIPAN SORIN <paraipan.sorin1993@gmail.com>
2024-09-23 11:56:53 +0300

    Compile previews based on iOS version

--- firefox-ios/Client/Frontend/Autofill/AutofillFooterView.swift
+++ firefox-ios/Client/Frontend/Autofill/AutofillFooterView.swift
@@ -2,7 +2,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-#if canImport(SwiftUI)
 import SwiftUI
 import Common
 
@@ -73,7 +72,7 @@ struct AutofillFooterView: View {
     .padding()
 }
 
-#else
+@available(iOS, deprecated: 17)
 struct AutoFillFooterView_Previews: PreviewProvider {
     static var previews: some View {
         AutofillFooterView(
@@ -85,5 +84,3 @@ struct AutoFillFooterView_Previews: PreviewProvider {
         .padding()
     }
 }
-
-#endif
--- firefox-ios/Client/Frontend/Autofill/AutofillHeaderView.swift
+++ firefox-ios/Client/Frontend/Autofill/AutofillHeaderView.swift
@@ -2,7 +2,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-#if canImport(SwiftUI)
 import SwiftUI
 import Common
 
@@ -88,7 +87,7 @@ struct AutofillHeaderView: View {
     .padding()
 }
 
-#else
+@available(iOS, deprecated: 17)
 struct AutoFillHeaderView_Previews: PreviewProvider {
     static var previews: some View {
         AutofillHeaderView(
@@ -100,5 +99,3 @@ struct AutoFillHeaderView_Previews: PreviewProvider {
         .padding()
     }
 }
-
-#endif
