e81df412b235ca6de7f210f6fc857636b4680e35
Cyndi Chin <cgc95@cornell.edu>
2024-09-26 16:41:21 -0400

    Add FXIOS-10094 [Unit Tests] Add store utility for tests (#22172)

---
+++ firefox-ios/firefox-ios-tests/Tests/ClientTests/Utils/StoreTestUtility.swift
@@ -0,0 +1,34 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import Foundation
+import Redux
+@testable import Client
+
+protocol StoreTestUtility {
+    func setupAppState() -> AppState
+    func setupTestingStore()
+    func resetTestingStore()
+}
+
+/// Utility class used when replacing the global store for testing purposes
+class StoreTestUtilityHelper {
+    func setupTestingStore(with appState: AppState, middlewares: [Middleware<AppState>]) {
+        store = Store(
+            state: appState,
+            reducer: AppState.reducer,
+            middlewares: middlewares
+        )
+    }
+
+    /// In order to avoid flaky tests, we should reset the store
+    /// similar to production
+    func resetTestingStore() {
+        store = Store(
+            state: AppState(),
+            reducer: AppState.reducer,
+            middlewares: middlewares
+        )
+    }
+}
