aded8cb85902ec0f8b0004a604195c1859197adc
Daniel Dervishi <ddervishi@CC7H24PK6P.local>
2024-09-27 15:29:52 -0400

    Made requested changes

--- firefox-ios/Client/Frontend/PasswordGenerator/PasswordGeneratorViewController.swift
+++ firefox-ios/Client/Frontend/PasswordGenerator/PasswordGeneratorViewController.swift
@@ -12,18 +12,7 @@ class PasswordGeneratorViewController: UIViewController, StoreSubscriber, Themea
     private enum UX {
         static let containerPadding: CGFloat = 20
         static let containerElementsVerticalPadding: CGFloat = 16
-
-        static let passwordFieldBorderWidth: CGFloat = 1
-        static let passwordFieldCornerRadius: CGFloat = 4
-        static let passwordFieldHorizontalPadding: CGFloat = 16
-        static let passwordFieldVerticalPadding: CGFloat = 10
-        static let passwordLabelAndButtonSpacing: CGFloat = 10
-        static let passwordRefreshButtonHeight: CGFloat = 18
-
-        static let headerIconLabelSpacing: CGFloat = 10
-        static let headerVerticalPadding: CGFloat = 8
         static let headerTrailingPadding: CGFloat = 45
-        static let headerImageHeight: CGFloat = 24
     }
 
     // MARK: - Redux
@@ -36,26 +25,13 @@ class PasswordGeneratorViewController: UIViewController, StoreSubscriber, Themea
     var notificationCenter: NotificationProtocol
     let windowUUID: WindowUUID
     var currentWindowUUID: UUID? { windowUUID }
-    private let scaledRefreshButtonSize = UIFontMetrics.default.scaledValue(for: UX.passwordRefreshButtonHeight)
-    private let scaledHeaderImageSize = UIFontMetrics.default.scaledValue(for: UX.headerImageHeight)
 
     // MARK: - Views
 
     private lazy var contentView: UIView = .build { view in
         view.accessibilityIdentifier = AccessibilityIdentifiers.PasswordGenerator.content
     }
-    private lazy var headerLabel: UILabel = .build { label in
-        label.numberOfLines = 0
-        label.font = FXFontStyles.Bold.body.scaledFont()
-        label.text = .PasswordGenerator.Title
-        label.isAccessibilityElement = true
-        label.accessibilityIdentifier = AccessibilityIdentifiers.PasswordGenerator.headerLabel
-        label.accessibilityTraits = .header
-    }
-    private lazy var headerImageView: UIImageView = .build { imageView in
-        imageView.image = UIImage(named: StandardImageIdentifiers.Large.login)?.withRenderingMode(.alwaysTemplate)
-        imageView.accessibilityIdentifier = AccessibilityIdentifiers.PasswordGenerator.headerImage
-    }
+
     private lazy var descriptionLabel: UILabel = .build { label in
         label.numberOfLines = 0
         label.font = FXFontStyles.Regular.subheadline.scaledFont()
@@ -63,28 +39,9 @@ class PasswordGeneratorViewController: UIViewController, StoreSubscriber, Themea
         label.accessibilityIdentifier = AccessibilityIdentifiers.PasswordGenerator.descriptionLabel
     }
 
-    private lazy var header: UIView = .build { view in
-        view.accessibilityIdentifier = AccessibilityIdentifiers.PasswordGenerator.header
-    }
+    private lazy var header: PasswordGeneratorHeaderView = .build()
 
-    private lazy var passwordField: UIView = .build { field in
-        field.layer.borderWidth = UX.passwordFieldBorderWidth
-        field.layer.cornerRadius = UX.passwordFieldCornerRadius
-        field.accessibilityIdentifier = AccessibilityIdentifiers.PasswordGenerator.passwordField
-    }
-
-    private lazy var passwordLabel: UILabel = .build { label in
-        label.accessibilityIdentifier = AccessibilityIdentifiers.PasswordGenerator.passwordlabel
-    }
-
-    private lazy var passwordRefreshButton: UIButton = .build { button in
-        button.setImage(
-            UIImage(named: StandardImageIdentifiers.Large.arrowClockwise)?.withRenderingMode(.alwaysTemplate), for: .normal)
-        button.contentHorizontalAlignment = .fill
-        button.contentVerticalAlignment = .fill
-        button.accessibilityLabel = .PasswordGenerator.RefreshPasswordButtonA11yLabel
-        button.accessibilityIdentifier = AccessibilityIdentifiers.PasswordGenerator.passwordRefreshButton
-    }
+    private lazy var passwordField: PasswordGeneratorPasswordFieldView = .build()
 
     private lazy var usePasswordButton: PrimaryRoundedButton = .build()
 
@@ -114,26 +71,23 @@ class PasswordGeneratorViewController: UIViewController, StoreSubscriber, Themea
     override func viewDidLoad() {
         super.viewDidLoad()
         listenForThemeChange(view)
-        buildPasswordLabel()
         buildUsePasswordButton()
         setupView()
         applyTheme()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+            UIAccessibility.post(notification: .screenChanged, argument: self.header)
+        }
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         applyTheme()
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
-            UIAccessibility.post(notification: .screenChanged, argument: self.headerLabel)
-        }
     }
 
     private func setupView() {
         // Adding Subviews
         view.addSubview(contentView)
         contentView.addSubviews(header, descriptionLabel, passwordField, usePasswordButton)
-        header.addSubviews(headerImageView, headerLabel)
-        passwordField.addSubviews(passwordLabel, passwordRefreshButton)
 
         // Content View Constraints
         NSLayoutConstraint.activate([
@@ -176,66 +130,16 @@ class PasswordGeneratorViewController: UIViewController, StoreSubscriber, Themea
                 constant: UX.containerElementsVerticalPadding),
             usePasswordButton.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
         ])
-
-        // Header elements layout
-        NSLayoutConstraint.activate([
-            headerImageView.leadingAnchor.constraint(equalTo: header.leadingAnchor),
-            headerImageView.centerYAnchor.constraint(equalTo: headerLabel.centerYAnchor),
-            headerImageView.widthAnchor.constraint(equalToConstant: scaledHeaderImageSize),
-            headerImageView.heightAnchor.constraint(equalToConstant: scaledHeaderImageSize),
-            headerLabel.leadingAnchor.constraint(
-                equalTo: headerImageView.trailingAnchor,
-                constant: UX.headerIconLabelSpacing),
-            headerLabel.trailingAnchor.constraint(equalTo: header.trailingAnchor),
-            headerLabel.topAnchor.constraint(
-                equalTo: header.topAnchor,
-                constant: UX.headerVerticalPadding),
-            headerLabel.bottomAnchor.constraint(equalTo: header.bottomAnchor)
-        ])
-
-        // Password Field Elements Layout
-        NSLayoutConstraint.activate([
-            passwordLabel.leadingAnchor.constraint(
-                equalTo: passwordField.leadingAnchor,
-                constant: UX.passwordFieldHorizontalPadding),
-            passwordLabel.topAnchor.constraint(
-                equalTo: passwordField.topAnchor,
-                constant: UX.passwordFieldVerticalPadding),
-            passwordLabel.bottomAnchor.constraint(
-                equalTo: passwordField.bottomAnchor,
-                constant: -UX.passwordFieldVerticalPadding),
-            passwordRefreshButton.leadingAnchor.constraint(
-                greaterThanOrEqualTo: passwordLabel.trailingAnchor,
-                constant: UX.passwordLabelAndButtonSpacing),
-            passwordRefreshButton.trailingAnchor.constraint(
-                equalTo: passwordField.trailingAnchor,
-                constant: -UX.passwordFieldHorizontalPadding),
-            passwordRefreshButton.centerYAnchor.constraint(
-                equalTo: passwordField.centerYAnchor),
-            passwordRefreshButton.widthAnchor.constraint(equalToConstant: scaledRefreshButtonSize),
-            passwordRefreshButton.heightAnchor.constraint(equalToConstant: scaledRefreshButtonSize),
-        ])
     }
 
     // MARK: - Themable
     func applyTheme() {
         let theme = themeManager.getCurrentTheme(for: windowUUID)
         view.backgroundColor = theme.colors.layer1
-        headerImageView.tintColor = theme.colors.iconPrimary
-        headerLabel.textColor = theme.colors.textPrimary
         descriptionLabel.textColor = theme.colors.textSecondary
-        passwordField.backgroundColor = theme.colors.layer2
-        passwordField.layer.borderColor = theme.colors.borderPrimary.cgColor
-        passwordLabel.textColor = theme.colors.textPrimary
-        passwordRefreshButton.tintColor = theme.colors.iconPrimary
         usePasswordButton.applyTheme(theme: theme)
-    }
-
-    private func buildPasswordLabel() {
-        passwordLabel.numberOfLines = 0
-        passwordLabel.font = FXFontStyles.Regular.body.scaledFont()
-        passwordLabel.text = passwordGeneratorState.password
-        passwordLabel.accessibilityAttributedLabel = generateAccessibilityAttributedLabel()
+        passwordField.applyTheme(theme: theme)
+        header.applyTheme(theme: theme)
     }
 
     private func buildUsePasswordButton() {
@@ -275,16 +179,7 @@ class PasswordGeneratorViewController: UIViewController, StoreSubscriber, Themea
 
     func newState(state: PasswordGeneratorState) {
         passwordGeneratorState = state
-        passwordLabel.text = passwordGeneratorState.password
-        passwordLabel.accessibilityAttributedLabel = generateAccessibilityAttributedLabel()
-    }
-
-    private func generateAccessibilityAttributedLabel() -> NSMutableAttributedString {
-        let fullString = String(format: .PasswordGenerator.PasswordReadoutPrefaceA11y, passwordGeneratorState.password)
-        let attributedString = NSMutableAttributedString(string: fullString)
-        let rangeOfPassword = (attributedString.string as NSString).range(of: passwordGeneratorState.password)
-        attributedString.addAttributes([.accessibilitySpeechSpellOut: true], range: rangeOfPassword)
-        return attributedString
+        passwordField.configure(password: passwordGeneratorState.password)
     }
 }
 
---
+++ firefox-ios/Client/Frontend/PasswordGenerator/View/PasswordGeneratorHeaderView.swift
@@ -0,0 +1,65 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+ import Foundation
+ import Common
+
+final class PasswordGeneratorHeaderView: UIView, ThemeApplicable {
+    private enum UX {
+        static let headerIconLabelSpacing: CGFloat = 10
+        static let headerVerticalPadding: CGFloat = 8
+        static let headerImageHeight: CGFloat = 24
+    }
+
+    private let scaledHeaderImageSize = UIFontMetrics.default.scaledValue(for: UX.headerImageHeight)
+
+    private lazy var headerLabel: UILabel = .build { label in
+        label.numberOfLines = 0
+        label.font = FXFontStyles.Bold.body.scaledFont()
+        label.text = .PasswordGenerator.Title
+        label.isAccessibilityElement = true
+        label.accessibilityIdentifier = AccessibilityIdentifiers.PasswordGenerator.headerLabel
+        label.accessibilityTraits = .header
+    }
+
+    private lazy var headerImageView: UIImageView = .build { imageView in
+        imageView.image = UIImage(named: StandardImageIdentifiers.Large.login)?.withRenderingMode(.alwaysTemplate)
+        imageView.accessibilityIdentifier = AccessibilityIdentifiers.PasswordGenerator.headerImage
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.accessibilityIdentifier = AccessibilityIdentifiers.PasswordGenerator.header
+        setupLayout()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupLayout() {
+        self.addSubviews(headerImageView, headerLabel)
+
+        // Header elements layout
+        NSLayoutConstraint.activate([
+            headerImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            headerImageView.centerYAnchor.constraint(equalTo: headerLabel.centerYAnchor),
+            headerImageView.widthAnchor.constraint(equalToConstant: scaledHeaderImageSize),
+            headerImageView.heightAnchor.constraint(equalToConstant: scaledHeaderImageSize),
+            headerLabel.leadingAnchor.constraint(
+                equalTo: headerImageView.trailingAnchor,
+                constant: UX.headerIconLabelSpacing),
+            headerLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            headerLabel.topAnchor.constraint(
+                equalTo: self.topAnchor,
+                constant: UX.headerVerticalPadding),
+            headerLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+    }
+
+    func applyTheme(theme: any Common.Theme) {
+        headerImageView.tintColor = theme.colors.iconPrimary
+        headerLabel.textColor = theme.colors.textPrimary
+    }
+}
