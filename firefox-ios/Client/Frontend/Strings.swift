c5fdff841130ae050fb4ab3760fa3ab78f8f16a2
Matt Lichtenstein <lichtensteinmp@gmail.com>
2024-09-27 15:30:32 -0400

    Bugfix FXIOS-8948 Fix toolbar a11y labels (#22234)

--- firefox-ios/Client/Frontend/Strings.swift
+++ firefox-ios/Client/Frontend/Strings.swift
@@ -6371,11 +6371,6 @@ extension String {
         tableName: "PrivateBrowsing",
         value: "Private browsing",
         comment: "Accessibility label for toggling on/off private mode")
-    public static let TabTrayToggleAccessibilityHint = MZLocalizedString(
-        key: "Turns private mode on or off",
-        tableName: "PrivateBrowsing",
-        value: nil,
-        comment: "Accessiblity hint for toggling on/off private mode")
     public static let TabTrayToggleAccessibilityValueOn = MZLocalizedString(
         key: "On",
         tableName: "PrivateBrowsing",
@@ -7393,6 +7388,11 @@ extension String {
                 tableName: "PrivateBrowsing",
                 value: nil,
                 comment: "Accessibility label for toggling on/off private mode")
+            public static let TabTrayToggleAccessibilityHint = MZLocalizedString(
+                key: "Turns private mode on or off",
+                tableName: "PrivateBrowsing",
+                value: nil,
+                comment: "Accessiblity hint for toggling on/off private mode")
         }
     }
 }
