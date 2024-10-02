e2aa1b5402543f281a89c84ca351df69bc11588d
ih-codes <ihugel@mozilla.com>
2024-10-02 16:20:23 -0600

    Fix call to apply theme in searchEngineImageView.

--- BrowserKit/Sources/ToolbarKit/AddressToolbar/LocationView/LocationView.swift
+++ BrowserKit/Sources/ToolbarKit/AddressToolbar/LocationView/LocationView.swift
@@ -397,6 +397,7 @@ final class LocationView: UIView, LocationTextFieldDelegate, ThemeApplicable, Ac
         urlTextFieldColor = colors.textPrimary
         urlTextFieldSubdomainColor = colors.textSecondary
         gradientLayer.colors = colors.layerGradientURL.cgColors.reversed()
+        searchEngineContentView.applyTheme(theme: theme)
         iconContainerBackgroundView.backgroundColor = colors.layerSearch
         lockIconButton.tintColor = colors.iconPrimary
         lockIconButton.backgroundColor = colors.layerSearch
