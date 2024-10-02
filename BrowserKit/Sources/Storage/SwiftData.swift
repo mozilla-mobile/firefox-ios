b5add604a00ce54c644838250e46284bbfb872fb
Filippo <fzazzeroni@mozilla.com>
2024-10-02 16:08:38 +0200

    refactor swift data header

--- BrowserKit/Sources/Storage/SwiftData.swift
+++ BrowserKit/Sources/Storage/SwiftData.swift
@@ -1,36 +1,6 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
-
-/*
- * This is a heavily modified version of SwiftData.swift by Ryan Fowler
- * This has been enhanced to support custom files, correct binding, versioning,
- * and a streaming results via Cursors. The API has also been changed to use NSError, Cursors, and
- * to force callers to request a connection before executing commands. Database creation helpers, savepoint
- * helpers, image support, and other features have been removed.
- */
-
-// SwiftData.swift
-//
-// Copyright (c) 2014 Ryan Fowler
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import Foundation
 import Common
