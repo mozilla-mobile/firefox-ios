94d69c2530872333f55adc282ecdd7530702df19
Filippo <fzazzeroni@mozilla.com>
2024-10-02 16:55:53 +0200

    Removed translation

--- firefox-ios/Shared/Shared-Bridging-Header.h
+++
@@ -1,8 +0,0 @@
-#ifndef Client_Shared_Bridging_Header_h
-#define Client_Shared_Bridging_Header_h
-
-#import <Foundation/Foundation.h>
-#import <CommonCrypto/CommonCrypto.h>
-#import "FSUtils.h"
-
-#endif
