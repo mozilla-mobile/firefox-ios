bfbfd0a4fe5a2f137225dc35103c6aee49d92391
Daniel Dervishi <58835213+DanielDervishi@users.noreply.github.com>
2024-10-07 16:33:55 -0400

    Addressed Issam's comment

--- firefox-ios/Client/Frontend/UserContent/UserScripts/AllFrames/AtDocumentStart/LoginsHelper.js
+++ firefox-ios/Client/Frontend/UserContent/UserScripts/AllFrames/AtDocumentStart/LoginsHelper.js
@@ -460,7 +460,7 @@ window.__firefox__.includeOnce("LoginsHelper", function() {
 
     const generatedPassword = PasswordGenerator.generatePassword({
       inputMaxLength: LoginManagerContent.activeField.maxLength,
-      rules: mapOfRules ?? undefined,
+        ...(mapOfRules && { rules: mapOfRules }),
     });
 
     return generatedPassword;
