39c281aabff58deacb807d6dcb2cca30bfd45dc0
Ione Souza Junior <junior@ionixjunior.com.br>
2024-10-09 21:32:37 -0300

    Use the new function

--- firefox-ios/Client/Frontend/Browser/Toolbars/Redux/ToolbarState.swift
+++ firefox-ios/Client/Frontend/Browser/Toolbars/Redux/ToolbarState.swift
@@ -137,22 +137,7 @@ struct ToolbarState: ScreenState, Equatable {
             return handleTraitCollectionDidChange(state: state, action: action)
 
         case ToolbarActionType.navigationButtonDoubleTapped:
-            guard let toolbarAction = action as? ToolbarAction else { return state }
-            return ToolbarState(
-                windowUUID: state.windowUUID,
-                toolbarPosition: state.toolbarPosition,
-                isPrivateMode: state.isPrivateMode,
-                addressToolbar: AddressBarState.reducer(state.addressToolbar, toolbarAction),
-                navigationToolbar: NavigationBarState.reducer(state.navigationToolbar, toolbarAction),
-                isShowingNavigationToolbar: state.isShowingNavigationToolbar,
-                isShowingTopTabs: state.isShowingTopTabs,
-                canGoBack: state.canGoBack,
-                canGoForward: state.canGoForward,
-                numberOfTabs: state.numberOfTabs,
-                showMenuWarningBadge: state.showMenuWarningBadge,
-                isNewTabFeatureEnabled: state.isNewTabFeatureEnabled,
-                canShowDataClearanceAction: state.canShowDataClearanceAction,
-                canShowNavigationHint: true)
+            return handleNavigationButtonDoubleTapped(state: state, action: action)
 
         case ToolbarActionType.navigationHintFinishedPresenting:
             guard let toolbarAction = action as? ToolbarAction else { return state }
