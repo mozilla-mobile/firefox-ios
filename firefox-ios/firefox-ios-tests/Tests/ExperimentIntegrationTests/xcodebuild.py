fd2cf6738ce5d2925a4b82e7ac23f610aca6730c
Benjamin Forehand Jr <bforehand@mozilla.com>
2024-10-10 09:10:00 -0500

    Added xcode command files and removed changes from sync tests.

--- firefox-ios/firefox-ios-tests/Tests/ExperimentIntegrationTests/xcodebuild.py
+++ firefox-ios/firefox-ios-tests/Tests/ExperimentIntegrationTests/xcodebuild.py
@@ -1,44 +1,47 @@
 import logging
-import os
 import subprocess
+from pathlib import Path
 
 from .xcrun import XCRun
 
-here = os.path.dirname(__file__)
+here = Path(__file__).resolve()
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 
 class XCodeBuild(object):
-    binary = 'xcodebuild'
-    destination = 'platform=iOS Simulator,name=iPhone 15,OS=17.4'
+    binary = "xcodebuild"
+    destination = "platform=iOS Simulator,name=iPhone 15,OS=17.5"
     logger = logging.getLogger()
-    scheme = 'Fennec'
-    testPlan = 'SyncIntegrationTestPlan'
+    scheme = "Fennec"
+    testPlan = "SyncIntegrationTestPlan"
     xcrun = XCRun()
 
     def __init__(self, log, **kwargs):
         self.scheme = kwargs.get("scheme", self.scheme)
         self.testPlan = kwargs.get("test_plan", self.testPlan)
         self.log = log
+        self.firefox_app_path = next(
+            Path("/Users").glob(
+                "**/Library/Developer/Xcode/DerivedData/Client-*/Build/Products/Fennec_Testing-*/Client.app"  # noqa
+            )
+        )
 
     def install(self, boot=True):
-        command = "find ~/Library/Developer/Xcode/DerivedData/Client-*/Build/Products/Fennec-* -type d -iname 'Client.app'"
-        path = subprocess.check_output(command, shell=True, universal_newlines=True)
         if boot:
             self.xcrun.boot()
         try:
             out = subprocess.check_output(
-                f"xcrun simctl install booted {path}",
-                cwd=os.pardir,
+                f"xcrun simctl install booted {self.firefox_app_path}",
+                cwd=f"{here.parent}",
                 stderr=subprocess.STDOUT,
                 universal_newlines=True,
-                shell=True
+                shell=True,
             )
         except subprocess.CalledProcessError as e:
             out = e.output
             raise
         finally:
-            with open(self.log, 'w') as f:
+            with open(self.log, "w") as f:
                 f.write(out)
 
     def test(self, identifier, build=True, erase=True):
@@ -49,23 +52,23 @@ class XCodeBuild(object):
             run_args = "test-without-building"
         args = [
             self.binary,
-            f'{run_args}',
-            '-scheme', self.scheme,
-            '-destination', self.destination,
-            '-only-testing:{}'.format(identifier),
-            '-testPlan', self.testPlan]
-        self.logger.info('Running: {}'.format(' '.join(args)))
+            f"{run_args}",
+            "-scheme",
+            self.scheme,
+            "-destination",
+            self.destination,
+            "-only-testing:{}".format(identifier),
+            "-testPlan",
+            self.testPlan,
+        ]
+        self.logger.info("Running: {}".format(" ".join(args)))
         try:
             out = subprocess.check_output(
-                args,
-                cwd=os.chdir("../../.."),
-                stderr=subprocess.STDOUT,
-                universal_newlines=True)
+                args, cwd=f"{here.parents[3]}", stderr=subprocess.STDOUT, universal_newlines=True
+            )
         except subprocess.CalledProcessError as e:
             out = e.output
             raise
         finally:
-            with open(self.log, 'w') as f:
+            with open(self.log, "w") as f:
                 f.write(out)
-            os.chdir("firefox-ios-tests/Tests/SyncIntegrationTests")
-
