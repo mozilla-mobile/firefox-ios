9b599c0c2f2d038f7d8dd50ac4be649d5967b37f
Daniel Dervishi <58835213+DanielDervishi@users.noreply.github.com>
2024-10-10 10:18:51 -0400

    Add FXIOS-9660 & FXIOS-9659 - Password Generator: Generating a strong password (#22183)
    * Base functionality added
    
    * Removed password generator from debug settings
    
    * Fixed password generation logic
    
    * Addressed Issam's comment
    
    * Removed print statements and replaced with logs
    
    * Disabled on private mode
    
    ---------
    
    Co-authored-by: Daniel Dervishi <ddervishi@CC7H24PK6P.local>

--- firefox-ios/Client/Frontend/PasswordGenerator/PasswordGeneratorMiddleware.swift
+++ firefox-ios/Client/Frontend/PasswordGenerator/PasswordGeneratorMiddleware.swift
@@ -7,25 +7,41 @@ import Redux
 import Shared
 import Common
 
-final class PasswordGeneratorMiddleware: FeatureFlaggable {
+final class PasswordGeneratorMiddleware {
+    private let logger: Logger = DefaultLogger.shared
     lazy var passwordGeneratorProvider: Middleware<AppState> = { state, action in
         let windowUUID = action.windowUUID
         guard let currentTab = (action as? PasswordGeneratorAction)?.currentTab else { return }
         switch action.actionType {
         case PasswordGeneratorActionType.showPasswordGenerator:
-                self.showPasswordGenerator(tab: currentTab, windowUUID: windowUUID)
+            self.showPasswordGenerator(tab: currentTab, windowUUID: windowUUID)
         default:
             break
         }
     }
 
     private func showPasswordGenerator(tab: Tab, windowUUID: WindowUUID) {
-        // TODO - FXIOS-9660 Business Logic to be added (tab is a necessary part of future business logic)
-        let newAction = PasswordGeneratorAction(
-            windowUUID: windowUUID,
-            actionType: PasswordGeneratorActionType.updateGeneratedPassword,
-            password: "fjdisapfjio32iojds" // to be replaced
-        )
-        store.dispatch(newAction)
+       generateNewPassword(with: tab, completion: { generatedPassword in
+           let newAction = PasswordGeneratorAction(
+               windowUUID: windowUUID,
+               actionType: PasswordGeneratorActionType.updateGeneratedPassword,
+               password: generatedPassword
+           )
+           store.dispatch(newAction)
+       })
     }
+
+    private func generateNewPassword(with tab: Tab, completion: @escaping (String) -> Void) {
+            let jsFunctionCall = "window.__firefox__.logins.generatePassword()"
+            tab.webView?.evaluateJavascriptInDefaultContentWorld(jsFunctionCall) { (result, error) in
+                if let error = error {
+                    self.logger.log("JavaScript evaluation error",
+                                    level: .warning,
+                                    category: .webview,
+                                    description: "\(error.localizedDescription)")
+                } else if let result = result as? String {
+                    completion(result)
+                }
+            }
+        }
 }
