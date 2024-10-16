f387704352915d6cd896b326ebdcaaddbf3658c4
mattreaganmozilla <145381717+mattreaganmozilla@users.noreply.github.com>
2024-10-16 14:37:19 -0700

    Bugfix FXIOS-10315 Fix WebKit exceptions from JS alert completion handlers (#22602)
    * [FXIOS-10315] Fix redundant completion handler call.
    
    * [FXIOS-10315] Fix crash occurring in some scenarios where we might attempt to dequeue and present a JS alert when there was already a presented VC on screen

--- firefox-ios/Client/Frontend/Browser/BrowserViewController/Views/BrowserViewController.swift
+++ firefox-ios/Client/Frontend/Browser/BrowserViewController/Views/BrowserViewController.swift
@@ -1298,11 +1298,12 @@ class BrowserViewController: UIViewController,
     }
 
     fileprivate func showQueuedAlertIfAvailable() {
-        if let queuedAlertInfo = tabManager.selectedTab?.dequeueJavascriptAlertPrompt() {
-            let alertController = queuedAlertInfo.alertController()
-            alertController.delegate = self
-            present(alertController, animated: true, completion: nil)
-        }
+        // See also: similar safety checks in `shouldDisplayJSAlertForWebView`
+        guard presentedViewController == nil else { return }
+        guard let queuedAlertInfo = tabManager.selectedTab?.dequeueJavascriptAlertPrompt() else { return }
+        let alertController = queuedAlertInfo.alertController()
+        alertController.delegate = self
+        present(alertController, animated: true, completion: nil)
     }
 
     func resetBrowserChrome() {
@@ -4099,6 +4100,9 @@ extension BrowserViewController: TabTrayDelegate {
 
 extension BrowserViewController: JSPromptAlertControllerDelegate {
     func promptAlertControllerDidDismiss(_ alertController: JSPromptAlertController) {
+        logger.log("JS prompt was dismissed. Will dequeue next alert.",
+                   level: .info,
+                   category: .webview)
         showQueuedAlertIfAvailable()
     }
 }
