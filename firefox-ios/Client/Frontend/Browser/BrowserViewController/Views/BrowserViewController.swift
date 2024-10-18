30bc06235d5151451e964887836e310ad32c97c5
OrlaM <omitchell@mozilla.com>
2024-10-18 11:29:42 -0400

    Revert "Bugfix FXIOS-10315 Fix WebKit exceptions from JS alert completion handlers" (#22638)
    Revert "Bugfix FXIOS-10315 Fix WebKit exceptions from JS alert completion han…"
    
    This reverts commit f387704352915d6cd896b326ebdcaaddbf3658c4.
    
    (cherry picked from commit 00c05a584a404e590f76376982febf5e776acd01)

--- firefox-ios/Client/Frontend/Browser/BrowserViewController/Views/BrowserViewController.swift
+++ firefox-ios/Client/Frontend/Browser/BrowserViewController/Views/BrowserViewController.swift
@@ -1225,12 +1225,11 @@ class BrowserViewController: UIViewController,
     }
 
     fileprivate func showQueuedAlertIfAvailable() {
-        // See also: similar safety checks in `shouldDisplayJSAlertForWebView`
-        guard presentedViewController == nil else { return }
-        guard let queuedAlertInfo = tabManager.selectedTab?.dequeueJavascriptAlertPrompt() else { return }
-        let alertController = queuedAlertInfo.alertController()
-        alertController.delegate = self
-        present(alertController, animated: true, completion: nil)
+        if let queuedAlertInfo = tabManager.selectedTab?.dequeueJavascriptAlertPrompt() {
+            let alertController = queuedAlertInfo.alertController()
+            alertController.delegate = self
+            present(alertController, animated: true, completion: nil)
+        }
     }
 
     func resetBrowserChrome() {
@@ -3938,9 +3937,6 @@ extension BrowserViewController: TabTrayDelegate {
 
 extension BrowserViewController: JSPromptAlertControllerDelegate {
     func promptAlertControllerDidDismiss(_ alertController: JSPromptAlertController) {
-        logger.log("JS prompt was dismissed. Will dequeue next alert.",
-                   level: .info,
-                   category: .webview)
         showQueuedAlertIfAvailable()
     }
 }
