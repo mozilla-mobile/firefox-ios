6124260c4c003a7fe84354f4b189b4bcbdeae6c8
Ione Souza Junior <junior@ionixjunior.com.br>
2024-10-22 18:52:37 -0300

    Extract the toolbar reader mode state changed to a new function

--- firefox-ios/Client/Frontend/Browser/Toolbars/Redux/AddressBarState.swift
+++ firefox-ios/Client/Frontend/Browser/Toolbars/Redux/AddressBarState.swift
@@ -575,6 +575,32 @@ struct AddressBarState: StateType, Equatable {
         )
     }
 
+    private static func handleToolbarReaderModeStateChanged(state: Self, action: Action) -> Self {
+        guard let toolbarAction = action as? ToolbarAction else { return state }
+
+        return AddressBarState(
+            windowUUID: state.windowUUID,
+            navigationActions: state.navigationActions,
+            pageActions: pageActions(action: toolbarAction,
+                                     addressBarState: state,
+                                     isEditing: state.isEditing,
+                                     showQRPageAction: state.showQRPageAction),
+            browserActions: state.browserActions,
+            borderPosition: state.borderPosition,
+            url: state.url,
+            searchTerm: state.searchTerm,
+            lockIconImageName: state.lockIconImageName,
+            safeListedURLImageName: state.safeListedURLImageName,
+            isEditing: state.isEditing,
+            isScrollingDuringEdit: state.isScrollingDuringEdit,
+            shouldSelectSearchTerm: state.shouldSelectSearchTerm,
+            isLoading: state.isLoading,
+            readerModeState: toolbarAction.readerModeState,
+            didStartTyping: state.didStartTyping,
+            showQRPageAction: state.showQRPageAction
+        )
+    }
+
     // MARK: - Address Toolbar Actions
     private static func navigationActions(
         action: ToolbarAction,
