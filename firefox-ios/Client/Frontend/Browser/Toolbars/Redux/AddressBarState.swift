ccf0f438a96de89c49ba3bb9ed8a187cb338386f
Ione Souza Junior <junior@ionixjunior.com.br>
2024-10-22 19:05:57 -0300

    Extract the toolbar trait collection did change to a new function

--- firefox-ios/Client/Frontend/Browser/Toolbars/Redux/AddressBarState.swift
+++ firefox-ios/Client/Frontend/Browser/Toolbars/Redux/AddressBarState.swift
@@ -591,6 +591,31 @@ struct AddressBarState: StateType, Equatable {
         )
     }
 
+    private static func handleToolbarTraitCollectionDidChange(state: Self, action: Action) -> Self {
+        guard let toolbarAction = action as? ToolbarAction else { return state }
+
+        return AddressBarState(
+            windowUUID: state.windowUUID,
+            navigationActions: navigationActions(action: toolbarAction,
+                                                 addressBarState: state,
+                                                 isEditing: state.isEditing),
+            pageActions: pageActions(action: toolbarAction, addressBarState: state, isEditing: state.isEditing),
+            browserActions: browserActions(action: toolbarAction, addressBarState: state),
+            borderPosition: state.borderPosition,
+            url: state.url,
+            searchTerm: nil,
+            lockIconImageName: state.lockIconImageName,
+            safeListedURLImageName: state.safeListedURLImageName,
+            isEditing: state.isEditing,
+            isScrollingDuringEdit: state.isScrollingDuringEdit,
+            shouldSelectSearchTerm: state.shouldSelectSearchTerm,
+            isLoading: state.isLoading,
+            readerModeState: state.readerModeState,
+            didStartTyping: state.didStartTyping,
+            showQRPageAction: state.showQRPageAction
+        )
+    }
+
     // MARK: - Address Toolbar Actions
     private static func navigationActions(
         action: ToolbarAction,
