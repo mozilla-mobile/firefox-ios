6c8d0212c95ee42aa44e4cf0dfdc6d0707737bcd
Ione Souza Junior <junior@ionixjunior.com.br>
2024-10-22 19:00:49 -0300

    Extract the toolbar back forward button state changed to a new function

--- firefox-ios/Client/Frontend/Browser/Toolbars/Redux/AddressBarState.swift
+++ firefox-ios/Client/Frontend/Browser/Toolbars/Redux/AddressBarState.swift
@@ -587,6 +587,31 @@ struct AddressBarState: StateType, Equatable {
         )
     }
 
+    private static func handleToolbarBackForwardButtonStateChanged(state: Self, action: Action) -> Self {
+        guard let toolbarAction = action as? ToolbarAction else { return state }
+
+        return AddressBarState(
+            windowUUID: state.windowUUID,
+            navigationActions: navigationActions(action: toolbarAction,
+                                                 addressBarState: state,
+                                                 isEditing: state.isEditing),
+            pageActions: pageActions(action: toolbarAction, addressBarState: state, isEditing: state.isEditing),
+            browserActions: state.browserActions,
+            borderPosition: state.borderPosition,
+            url: state.url,
+            searchTerm: nil,
+            lockIconImageName: state.lockIconImageName,
+            safeListedURLImageName: state.safeListedURLImageName,
+            isEditing: state.isEditing,
+            isScrollingDuringEdit: state.isScrollingDuringEdit,
+            shouldSelectSearchTerm: state.shouldSelectSearchTerm,
+            isLoading: state.isLoading,
+            readerModeState: state.readerModeState,
+            didStartTyping: state.didStartTyping,
+            showQRPageAction: state.showQRPageAction
+        )
+    }
+
     // MARK: - Address Toolbar Actions
     private static func navigationActions(
         action: ToolbarAction,
