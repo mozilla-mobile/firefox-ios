bca37081804a87d9ba2dbc7bca5aaeccdfec2649
Ione Souza Junior <junior@ionixjunior.com.br>
2024-10-22 19:18:37 -0300

    Use the new function to handle did scroll during edit

--- firefox-ios/Client/Frontend/Browser/Toolbars/Redux/AddressBarState.swift
+++ firefox-ios/Client/Frontend/Browser/Toolbars/Redux/AddressBarState.swift
@@ -164,24 +164,7 @@ struct AddressBarState: StateType, Equatable {
             return handleToolbarDidSetTextInLocationView(state: state, action: action)
 
         case ToolbarActionType.didScrollDuringEdit:
-            return AddressBarState(
-                windowUUID: state.windowUUID,
-                navigationActions: state.navigationActions,
-                pageActions: state.pageActions,
-                browserActions: state.browserActions,
-                borderPosition: state.borderPosition,
-                url: state.url,
-                searchTerm: state.searchTerm,
-                lockIconImageName: state.lockIconImageName,
-                safeListedURLImageName: state.safeListedURLImageName,
-                isEditing: state.isEditing,
-                isScrollingDuringEdit: true,
-                shouldSelectSearchTerm: state.shouldSelectSearchTerm,
-                isLoading: state.isLoading,
-                readerModeState: state.readerModeState,
-                didStartTyping: state.didStartTyping,
-                showQRPageAction: state.showQRPageAction
-            )
+            return handleToolbarDidScrollDuringEdit(state: state)
 
         case ToolbarActionType.clearSearch:
             guard let toolbarAction = action as? ToolbarAction else { return state }
