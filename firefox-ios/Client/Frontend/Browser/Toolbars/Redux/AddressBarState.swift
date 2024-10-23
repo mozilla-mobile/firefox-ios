c7215633009ded630c7ba2c5fae79f83e615e16f
Ione Souza Junior <junior@ionixjunior.com.br>
2024-10-23 20:11:31 -0300

    Rename function

--- firefox-ios/Client/Frontend/Browser/Toolbars/Redux/AddressBarState.swift
+++ firefox-ios/Client/Frontend/Browser/Toolbars/Redux/AddressBarState.swift
@@ -142,7 +142,7 @@ struct AddressBarState: StateType, Equatable {
             return handleTraitCollectionDidChangeAction(state: state, action: action)
 
         case ToolbarActionType.showMenuWarningBadge:
-            return handleToolbarShowMenuWarningBadge(state: state, action: action)
+            return handleShowMenuWarningBadgeAction(state: state, action: action)
 
         case ToolbarActionType.borderPositionChanged,
             ToolbarActionType.toolbarPositionChanged:
@@ -334,7 +334,7 @@ struct AddressBarState: StateType, Equatable {
         )
     }
 
-    private static func handleToolbarShowMenuWarningBadge(state: Self, action: Action) -> Self {
+    private static func handleShowMenuWarningBadgeAction(state: Self, action: Action) -> Self {
         guard let toolbarAction = action as? ToolbarAction else { return state }
 
         return AddressBarState(
