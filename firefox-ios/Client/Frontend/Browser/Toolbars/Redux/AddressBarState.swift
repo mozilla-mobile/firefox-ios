5a3cafa97c49c906cd5baf9f08cd5d14003faca0
Ione Souza Junior <junior@ionixjunior.com.br>
2024-10-23 20:09:40 -0300

    Rename function

--- firefox-ios/Client/Frontend/Browser/Toolbars/Redux/AddressBarState.swift
+++ firefox-ios/Client/Frontend/Browser/Toolbars/Redux/AddressBarState.swift
@@ -130,7 +130,7 @@ struct AddressBarState: StateType, Equatable {
             return handleReaderModeStateChangedAction(state: state, action: action)
 
         case ToolbarActionType.websiteLoadingStateDidChange:
-            return handleToolbarWebsiteLoadingStateDidChange(state: state, action: action)
+            return handleWebsiteLoadingStateDidChangeAction(state: state, action: action)
 
         case ToolbarActionType.urlDidChange:
             return handleToolbarUrlDidChange(state: state, action: action)
@@ -240,7 +240,7 @@ struct AddressBarState: StateType, Equatable {
         )
     }
 
-    private static func handleToolbarWebsiteLoadingStateDidChange(state: Self, action: Action) -> Self {
+    private static func handleWebsiteLoadingStateDidChangeAction(state: Self, action: Action) -> Self {
         guard let toolbarAction = action as? ToolbarAction else { return state }
 
         return AddressBarState(
