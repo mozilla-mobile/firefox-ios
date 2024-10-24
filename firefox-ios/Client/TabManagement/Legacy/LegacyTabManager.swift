c529d9f1b71c43efb1aa7c777741090e43fed437
Nishant Bhasin <nish.bhasin@gmail.com>
2024-10-24 19:35:11 -0400

    Bugfix FXIOS-9483 Bugzilla 1905749  (#22519)
    * Bugfix FXIOS-9483 Bugzilla 1905749
    
    * Cleanup

--- firefox-ios/Client/TabManagement/Legacy/LegacyTabManager.swift
+++ firefox-ios/Client/TabManagement/Legacy/LegacyTabManager.swift
@@ -408,7 +408,6 @@ class LegacyTabManager: NSObject, FeatureFlaggable, TabManager, TabEventHandler 
         let popup = Tab(profile: profile,
                         isPrivate: parentTab.isPrivate,
                         windowUUID: windowUUID)
-
         // Configure the tab for the child popup webview. In this scenario we need to be sure to pass along
         // the specific `configuration` that we are given by the WKUIDelegate callback, since if we do not
         // use this configuration WebKit will throw an exception.
