ee15a4c9b813bc596b94e515273f4d7bebb4c6e2
Ione Souza Junior <junior@ionixjunior.com.br>
2024-10-29 18:28:22 -0300

    Extract the default action to a new function

--- firefox-ios/Client/Frontend/Browser/Toolbars/Redux/AddressBarState.swift
+++ firefox-ios/Client/Frontend/Browser/Toolbars/Redux/AddressBarState.swift
@@ -642,6 +642,27 @@ struct AddressBarState: StateType, Equatable {
         )
     }
 
+    private static func handleDefaultAction(state: Self) -> Self {
+        return AddressBarState(
+            windowUUID: state.windowUUID,
+            navigationActions: state.navigationActions,
+            pageActions: state.pageActions,
+            browserActions: state.browserActions,
+            borderPosition: state.borderPosition,
+            url: state.url,
+            searchTerm: state.searchTerm,
+            lockIconImageName: state.lockIconImageName,
+            safeListedURLImageName: state.safeListedURLImageName,
+            isEditing: state.isEditing,
+            isScrollingDuringEdit: state.isScrollingDuringEdit,
+            shouldSelectSearchTerm: state.shouldSelectSearchTerm,
+            isLoading: state.isLoading,
+            readerModeState: state.readerModeState,
+            didStartTyping: state.didStartTyping,
+            showQRPageAction: state.showQRPageAction
+        )
+    }
+
     // MARK: - Address Toolbar Actions
     private static func navigationActions(
         action: ToolbarAction,
