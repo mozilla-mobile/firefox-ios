2143f58a3c60d17b0462b253af53b2aa95877857
Matt Reagan <mreagan@mozilla.com>
2024-10-29 08:32:39 -0700

    [FXIOS-10404] Fix lock icon showing incorrectly on homepage

--- firefox-ios/Client/Frontend/Toolbar+URLBar/TabLocationView.swift
+++ firefox-ios/Client/Frontend/Toolbar+URLBar/TabLocationView.swift
@@ -370,11 +370,10 @@ class TabLocationView: UIView, FeatureFlaggable {
     }
 
     func showTrackingProtectionButton(for url: URL?) {
-        guard let url else { return }
         ensureMainThread {
             let isValidHttpUrlProtocol = self.isValidHttpUrlProtocol(url)
-            let isReaderModeURL = url.isReaderModeURL
-            let isFxHomeUrl = url.isFxHomeUrl
+            let isReaderModeURL = url?.isReaderModeURL ?? false
+            let isFxHomeUrl = url?.isFxHomeUrl ?? false
             if !isFxHomeUrl, !isReaderModeURL, isValidHttpUrlProtocol, self.trackingProtectionButton.isHidden {
                 self.trackingProtectionButton.transform = UX.trackingProtectionxOffset
                 self.trackingProtectionButton.alpha = 0
