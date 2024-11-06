f5319d6591c9f92231dfa5c165e5f4d5c51c4b56
Alexandru Farcasanu <alex.farcasanu@yahoo.com>
2024-11-06 17:10:34 +0200

    FXIOS-10473 #22931 ⁃ Fix the Menu cells for Accessibility dynamic text

--- BrowserKit/Sources/MenuKit/MenuCell.swift
+++ BrowserKit/Sources/MenuKit/MenuCell.swift
@@ -22,11 +22,12 @@ public class MenuCell: UITableViewCell, ReusableCell, ThemeApplicable {
     // MARK: - UI Elements
     private var titleLabel: UILabel = .build { label in
         label.font = FXFontStyles.Regular.body.scaledFont()
-        label.numberOfLines = 2
+        label.numberOfLines = 0
     }
 
     private var descriptionLabel: UILabel = .build { label in
         label.font = FXFontStyles.Regular.caption1.scaledFont()
+        label.numberOfLines = 0
     }
 
     private var icon: UIImageView = .build()
