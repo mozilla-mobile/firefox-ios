db68e037b535b58b5fa4c0ca8aa65e10c29d23da
Ione Souza Junior <junior@ionixjunior.com.br>
2024-11-11 19:14:07 -0300

    Remove force unwrap

--- firefox-ios/firefox-ios-tests/Tests/StorageTests/RustAutofillTests.swift
+++ firefox-ios/firefox-ios-tests/Tests/StorageTests/RustAutofillTests.swift
@@ -195,7 +195,7 @@ class RustAutofillTests: XCTestCase {
                     expectationGetCard.fulfill()
 
                     let expectedCcExpYear = Int64(2028)
-                    let updatedCreditCard = self.createUnencryptedCreditCardFields(creditCard: creditCard!,
+                    let updatedCreditCard = self.createUnencryptedCreditCardFields(creditCard: creditCard,
                                                                                    expectedCcExpYear: expectedCcExpYear)
                     self.autofill.updateCreditCard(id: creditCard!.guid,
                                                    creditCard: updatedCreditCard) { success, err in
