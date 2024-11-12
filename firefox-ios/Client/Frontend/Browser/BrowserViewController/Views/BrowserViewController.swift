10786914ef045decda915902b76f1f52473086d1
PARAIPAN SORIN <51127880+PARAIPAN9@users.noreply.github.com>
2024-11-12 13:28:15 +0200

    Reset progress to 0 when a tab is closed

--- firefox-ios/Client/Frontend/Browser/BrowserViewController/Views/BrowserViewController.swift
+++ firefox-ios/Client/Frontend/Browser/BrowserViewController/Views/BrowserViewController.swift
@@ -3910,6 +3910,7 @@ extension BrowserViewController: TabManagerDelegate {
                                               title: tab.lastTitle,
                                               lastExecutedTime: tab.lastExecutedTime)
         }
+        urlBar.updateProgressBar(Float(0))
         updateTabCountUsingTabManager(tabManager)
     }
 
