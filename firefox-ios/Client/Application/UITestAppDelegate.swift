6688ee4a4fedab999b1c04052c73a4fa31308cab
bmihai23 <bolojanm23@gmail.com>
2024-11-15 12:26:16 +0200

    Use optional binding instead of force casting

--- firefox-ios/Client/Application/UITestAppDelegate.swift
+++ firefox-ios/Client/Application/UITestAppDelegate.swift
@@ -146,7 +146,9 @@ class UITestAppDelegate: AppDelegate, FeatureFlaggable {
         let output = URL(fileURLWithPath: "\(dirForTestProfile)/places.db")
 
         let enumerator = FileManager.default.enumerator(atPath: dirForTestProfile)
-        let filePaths = enumerator?.allObjects as! [String]
+        guard let filePaths = enumerator?.allObjects as? [String] else {
+            fatalError("Failed to cast enumerator.allObjects to [String] during filePaths extraction")
+        }
         filePaths.filter { $0.contains(".db") }.forEach { item in
             try? FileManager.default.removeItem(
                 at: URL(fileURLWithPath: "\(dirForTestProfile)/\(item)")
