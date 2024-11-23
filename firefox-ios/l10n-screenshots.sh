0109a68a6f05865c997513c437f832f836d3af1f
Clare So <cso@mozilla.com>
2024-11-22 23:24:46 -0500

    Try L10nBuild with macos 15 (edge version of stack)

--- firefox-ios/l10n-screenshots.sh
+++ firefox-ios/l10n-screenshots.sh
@@ -38,7 +38,7 @@ for lang in $LOCALES; do
         --skip_open_summary \
         --xcargs "-maximum-parallel-testing-workers 2" \
         --derived_data_path l10n-screenshots-dd \
-        --ios_version "18.0" \
+        --ios_version "18.1" \
         --erase_simulator --localize_simulator \
         --devices "iPhone 16" --languages "$lang" \
         --output_directory "l10n-screenshots/$lang" \
