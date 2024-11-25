a8e8b1f0275612e33ac5cd9f90eae767604ba9ff
Clare So <cso@mozilla.com>
2024-11-25 11:00:00 -0500

    Use xcode 15 for L10n tests due to issue on fastlane & Xcode 16

--- firefox-ios/l10n-screenshots.sh
+++ firefox-ios/l10n-screenshots.sh
@@ -38,9 +38,9 @@ for lang in $LOCALES; do
         --skip_open_summary \
         --xcargs "-maximum-parallel-testing-workers 2" \
         --derived_data_path l10n-screenshots-dd \
-        --ios_version "18.1" \
+        --ios_version "17.5" \
         --erase_simulator --localize_simulator \
-        --devices "iPhone 16 Pro" --languages "$lang" \
+        --devices "iPhone 15" --languages "$lang" \
         --output_directory "l10n-screenshots/$lang" \
         $EXTRA_FAST_LANE_ARGS
     echo "Fastlane exited with code: $?"
