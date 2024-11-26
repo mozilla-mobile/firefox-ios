04bfca42ed1ae7d419e94e69d4b7c8acfb270a8a
Orla <omitchell@mozilla.com>
2024-11-26 16:52:48 -0500

    Fix linting

--- firefox-ios/Shared/Prefs.swift
+++ firefox-ios/Shared/Prefs.swift
@@ -40,7 +40,7 @@ public struct PrefsKeys {
     public static let KeySecondRun = "SecondRun"
     public static let KeyAutofillCreditCardStatus = "KeyAutofillCreditCardStatus"
     public static let KeyAutofillAddressStatus = "KeyAutofillAddressStatus"
-    
+
     // Only set if we get an actual response, no assumptions, nil otherwise
     public static let AppleConfirmedUserIsDefaultBrowser = "AppleConfirmedUserIsDefaultBrowser"
 
