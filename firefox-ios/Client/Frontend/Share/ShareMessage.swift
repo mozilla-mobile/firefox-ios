fbc02a0124a7438630f043a17570e1b17d95c4d1
ih-codes <ihugel@mozilla.com>
2024-11-26 16:28:09 -0600

    Stub in types for the new share manager interface which will make sharing behaviour much more explicit and less conditional on random things like the weather last Tuesday.

---
+++ firefox-ios/Client/Frontend/Share/ShareMessage.swift
@@ -0,0 +1,12 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import Foundation
+
+/// Additional text to share alongside a `ShareType`. Some apps, like Mail, support an additional subject line (called a
+/// subtitle).
+struct ShareMessage: Equatable {
+    let message: String
+    let subtitle: String?
+}
