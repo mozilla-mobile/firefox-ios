b6c2a1c41816ab8a29c934044030d0ed76ec4905
Isabella <ihugel@mozilla.com>
2024-11-27 10:18:39 -0600

    Add FXIOS-10669 [Sent from Firefox] Add new share manager types to make sharing behaviour more explicit (#23428)
    * Stub in types for the new share manager interface which will make sharing behaviour much more explicit and less conditional on random things like the weather last Tuesday.
    
    * Add ShareTab conformance to Tab. This will make the sharing manager more testable and allow for the use of a MockShareTab.

---
+++ firefox-ios/Client/Frontend/Share/ShareType.swift
@@ -0,0 +1,38 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import Foundation
+
+/// Preconfigured sharing schemes which the share manager knows how to handle.
+///     file: Include a file URL (file://). Best used for sharing downloaded files.
+///     site: Include a website URL (http(s)://). Best used for sharing library/bookmarks, etc. without an active tab.
+///           Shares configured using .site will not append a title to Messages but will have a subtitle in Mail.
+///     tab:  Include a website URL and a tab to share. If sharing a tab with an active webView, then additional sharing
+///           options will be configured, including printing the page and adding the webpage to your iOS home screen.
+enum ShareType: Equatable {
+    case file(url: URL)
+    case site(url: URL)
+    case tab(url: URL, tab: any ShareTab)
+
+    static func == (lhs: Self, rhs: Self) -> Bool {
+        switch (lhs, rhs) {
+        case let (.file(lhsURL), .file(rhsURL)):
+            return lhsURL == rhsURL
+        case let (.site(lhsURL), .site(rhsURL)):
+            return lhsURL == rhsURL
+        case let (.tab(lhsURL, lhsTab), .tab(rhsURL, rhsTab)):
+            return lhsURL == rhsURL && lhsTab.isEqual(to: rhsTab)
+        default:
+            return false
+        }
+    }
+}
+
+extension ShareTab {
+    func isEqual(to otherShareTab: some ShareTab) -> Bool {
+        return self.displayTitle == otherShareTab.displayTitle
+        && self.url == otherShareTab.url
+        && self.webView == otherShareTab.webView
+    }
+}
