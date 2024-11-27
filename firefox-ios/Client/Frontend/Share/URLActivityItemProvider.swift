e32646c6a7a28766e0e292ed87dbbe241d63dcd7
Isabella <ihugel@mozilla.com>
2024-11-27 11:34:07 -0600

    Add FXIOS-10669 [Sent from Firefox] Add new UIActivityItemProviders so ShareExtensionHelper isn't doing unrelated activity item processing (#23427)
    * Stub in new activity providers which will take some logic burden off the old ShareExtensionHelper. The ShareExtensionHelper should really not be serving as an item provider as well as organizing other item providers.

---
+++ firefox-ios/Client/Frontend/Share/URLActivityItemProvider.swift
@@ -0,0 +1,39 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import Foundation
+import UniformTypeIdentifiers
+
+class URLActivityItemProvider: UIActivityItemProvider, @unchecked Sendable {
+    private let url: URL
+
+    static let excludedActivities = [
+        UIActivity.ActivityType.addToReadingList,
+    ]
+
+    init(url: URL) {
+        self.url = url
+
+        super.init(placeholderItem: url)
+    }
+
+    override var placeholderItem: Any? {
+        return url
+    }
+
+    override var item: Any {
+        if let activityType = activityType, URLActivityItemProvider.excludedActivities.contains(activityType) {
+            return NSNull()
+        }
+
+        return url
+    }
+
+    override func activityViewController(
+        _ activityViewController: UIActivityViewController,
+        dataTypeIdentifierForActivityType activityType: UIActivity.ActivityType?
+    ) -> String {
+        return url.isFileURL ? UTType.fileURL.identifier : UTType.url.identifier
+    }
+}
