8dffdf78b2bbe9707a84b1f9385803c159554672
Filippo <fzazzeroni@mozilla.com>
2024-11-29 09:57:17 +0100

    First implementation OptimizedContentContainer

--- firefox-ios/Client/Frontend/Components/ContentContainer.swift
+++ firefox-ios/Client/Frontend/Components/ContentContainer.swift
@@ -10,6 +10,15 @@ enum ContentType {
     case privateHomepage
     case nativeErrorPage
     case webview
+
+    var shouldContentBeCached: Bool {
+        return switch self {
+            case .nativeErrorPage, .homepage:
+                false
+            default:
+                true
+        }
+    }
 }
 
 protocol ContentContainable: UIViewController {
@@ -90,6 +99,80 @@ class ContentContainer: UIView {
         // We're not removing the webview controller for now since if it's not loaded, the
         // webview doesn't layout it's WKCompositingView which result in black screen
         guard !hasWebView else { return }
+        contentController?.willMove(toParent: nil)
+        contentController?.view.removeFromSuperview()
+        contentController?.removeFromParent()
+    }
+
+    private func saveContentType(content: ContentContainable) {
+        type = content.contentType
+        contentController = content
+    }
+
+    private func addToView(content: ContentContainable) {
+        addSubview(content.view)
+        content.view.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            content.view.topAnchor.constraint(equalTo: topAnchor),
+            content.view.leadingAnchor.constraint(equalTo: leadingAnchor),
+            content.view.bottomAnchor.constraint(equalTo: bottomAnchor),
+            content.view.trailingAnchor.constraint(equalTo: trailingAnchor)
+        ])
+    }
+}
+
+/// A container for view controllers, currently used to embed content in BrowserViewController
+class OptimizedContentContainer: ContentContainer {
+    private var type: ContentType?
+    private var contentController: ContentContainable?
+
+    /// Determine if the content can be added, making sure we only add once
+    /// - Parameters:
+    ///   - viewController: The view controller to add to the container
+    /// - Returns: True when we can add the view controller to the container
+    override func canAdd(content: ContentContainable) -> Bool {
+        switch type {
+        case .legacyHomepage:
+            return !(content is LegacyHomepageViewController)
+        case .nativeErrorPage:
+            return !(content is NativeErrorPageViewController)
+        case .homepage:
+            return !(content is HomepageViewController)
+        case .privateHomepage:
+            return !(content is PrivateHomepageViewController)
+        case .webview:
+            return !(content is WebviewViewController)
+        case .none:
+            return true
+        }
+    }
+
+    /// Add content view controller to the container, we remove the previous content if present before adding new one
+    /// - Parameter content: The view controller to add
+    override func add(content: ContentContainable) {
+        removePreviousContentIfNeeded()
+        saveContentType(content: content)
+        if content.view.superview == nil {
+            addToView(content: content)
+        } else {
+            bringSubviewToFront(content.view)
+        }
+    }
+
+    /// Update content in the container. This is used in the case of the webview since
+    /// it's not removed, we don't need to add it back again.
+    ///
+    /// - Parameter content: The content to update
+    override func update(content: ContentContainable) {
+        removePreviousContentIfNeeded()
+        bringSubviewToFront(content.view)
+        saveContentType(content: content)
+    }
+
+    // MARK: - Private
+
+    private func removePreviousContentIfNeeded() {
+        guard let type, !type.shouldContentBeCached else { return }
         contentController?.willMove(toParent: nil)
         contentController?.view.removeFromSuperview()
         contentController?.removeFromParent()
