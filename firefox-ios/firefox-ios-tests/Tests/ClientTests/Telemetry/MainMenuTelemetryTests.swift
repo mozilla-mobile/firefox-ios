436ea74d095b2e06c3a06341deaed9ea2e5a13ec
Travis Long <tlong@mozilla.com>
2024-12-06 16:33:03 -0600

    Ensure custom pings are registered before Glean.init/reset to unbreak tests

--- firefox-ios/firefox-ios-tests/Tests/ClientTests/Telemetry/MainMenuTelemetryTests.swift
+++ firefox-ios/firefox-ios-tests/Tests/ClientTests/Telemetry/MainMenuTelemetryTests.swift
@@ -14,6 +14,10 @@ final class MainMenuTelemetryTests: XCTestCase {
 
     override func setUp() {
         super.setUp()
+        // Due to changes allow certain custom pings to implement their own opt-out
+        // independent of Glean, custom pings may need to be registered manually in
+        // tests in order to puth them in a state in which they can collect data.
+        Glean.shared.registerPings(GleanMetrics.Pings.shared)
         Glean.shared.resetGlean(clearStores: true)
         subject = MainMenuTelemetry()
     }
