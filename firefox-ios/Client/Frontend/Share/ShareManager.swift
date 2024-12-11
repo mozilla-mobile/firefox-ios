af091c91652b4101850a4f3d24b3ffd9916fbc03
ih-codes <ihugel@mozilla.com>
2024-12-11 17:30:43 -0600

    Only override WhatsApp shares when Sent from Firefox is enabled.

--- firefox-ios/Client/Frontend/Share/ShareManager.swift
+++ firefox-ios/Client/Frontend/Share/ShareManager.swift
@@ -89,9 +89,19 @@ class ShareManager: NSObject, FeatureFlaggable {
             }
 
         case .tab(let siteURL, let tab):
+            let isSentFromFirefoxEnabled = LegacyFeatureFlagsManager.shared.isFeatureEnabled(
+                .sentFromFirefoxTreatmentA,
+                checking: .buildOnly
+            )
+            activityItems.append(
+                URLActivityItemProvider(
+                    url: siteURL,
+                    allowSentFromFirefoxTreatment: isSentFromFirefoxEnabled
+                )
+            )
+
             // For websites, we also want to offer a few additional activity items besides the URL, like printing the
             // webpage or adding a website to the iOS home screen
-            activityItems.append(URLActivityItemProvider(url: siteURL, allowSentFromFirefoxTreatment: true))
 
             // Only show the print activity if the tab's webview is loaded
             if tab.webView != nil {
