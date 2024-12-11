95e14943c58554feeeff55dbce1ac2615fa2de40
Matt Reagan <mreagan@mozilla.com>
2024-12-11 12:38:13 -0800

    [10711] Additional cleanup and also fix invalidation to be UUID specific for iPad users

--- firefox-ios/Client/Telemetry/TabErrorTelemetryHelper.swift
+++ firefox-ios/Client/Telemetry/TabErrorTelemetryHelper.swift
@@ -41,21 +41,21 @@ final class TabErrorTelemetryHelper {
 
     private func recordTabCount(_ window: WindowUUID) {
         guard self.tabManagerAvailable(for: window) else { return }
-        var tabCounts = self.defaults.object(forKey: self.defaultsKey) as? [String: Int] ?? [String: Int]()
-        let tabCount = self.getTabCount(window: window)
+        var tabCounts = defaults.object(forKey: defaultsKey) as? [String: Int] ?? [String: Int]()
+        let tabCount = getTabCount(window: window)
         tabCounts[window.uuidString] = tabCount
-        self.defaults.set(tabCounts, forKey: self.defaultsKey)
+        defaults.set(tabCounts, forKey: defaultsKey)
     }
 
     private func validateTabCount(_ window: WindowUUID) {
-        guard self.tabManagerAvailable(for: window) else { return }
-        guard let tabCounts = self.defaults.object(forKey: self.defaultsKey) as? [String: Int],
+        guard tabManagerAvailable(for: window) else { return }
+        guard let tabCounts = defaults.object(forKey: defaultsKey) as? [String: Int],
               let expectedTabCount = tabCounts[window.uuidString] else { return }
-        let currentTabCount = self.getTabCount(window: window)
+        let currentTabCount = getTabCount(window: window)
 
         if expectedTabCount > 1 && (expectedTabCount - currentTabCount) > 1 {
             // Potential tab loss bug detected. Log a MetricKit error.
-            self.sendTelemetryTabLossDetectedEvent()
+            sendTelemetryTabLossDetectedEvent()
         }
 
         // After validating the tab count, we make sure to remove the count
@@ -64,11 +64,13 @@ final class TabErrorTelemetryHelper {
         // is still in preferences and the app crashes. If the user removed
         // any tabs during this time, it means the next launch there will be
         // fewer tabs than recorded and we'll send the event erroneously.
-        invalidateTabCount()
+        invalidateTabCount(for: window)
     }
 
-    private func invalidateTabCount() {
-        defaults.removeObject(forKey: defaultsKey)
+    private func invalidateTabCount(for window: WindowUUID) {
+        guard var tabCounts = defaults.object(forKey: defaultsKey) as? [String: Int] else { return }
+        tabCounts.removeValue(forKey: window.uuidString)
+        defaults.set(tabCounts, forKey: defaultsKey)
     }
 
     /// It's possible for this telemetry helper to be called during onboarding flow before
