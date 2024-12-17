5ddfc4d01572f3bda0bdbf7a2837624550ddfe43
Laurie Marceau <lmarceau@mozilla.com>
2024-12-17 10:36:23 -0500

    Revert unit test change

--- firefox-ios/firefox-ios-tests/Tests/ClientTests/Frontend/Home/LegacyHomepageViewControllerTests.swift
+++ firefox-ios/firefox-ios-tests/Tests/ClientTests/Frontend/Home/LegacyHomepageViewControllerTests.swift
@@ -60,7 +60,7 @@ class LegacyHomepageViewControllerTests: XCTestCase {
                 expression: "'homepage_viewed'|eventSum('Days', 1, 0) > 0"
             )
         )
-        subject.viewDidAppear(false)
+        subject.viewWillAppear(false)
         let expectation = self.expectation(description: "Record event called")
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
             expectation.fulfill()
