28344041b0b24c2876fa080292e383557c1be3a7
Marceau Tonelli <marceau.tonelli@cerema.fr>
2024-12-17 11:41:51 +0100

    Resolve warnings in CollapsibleCardView

--- BrowserKit/Sources/ComponentLibrary/CollapsibleCardView.swift
+++ BrowserKit/Sources/ComponentLibrary/CollapsibleCardView.swift
@@ -104,7 +104,7 @@ public class CollapsibleCardView: ShadowCardView, UIGestureRecognizerDelegate {
 
     private lazy var headerView: UIView = .build { _ in }
     private lazy var containerView: UIView = .build { _ in }
-    private var tapRecognizer: UITapGestureRecognizer!
+    private var tapRecognizer: UITapGestureRecognizer?
 
     lazy var titleLabel: UILabel = .build { label in
         label.adjustsFontForContentSizeCategory = true
@@ -124,9 +124,11 @@ public class CollapsibleCardView: ShadowCardView, UIGestureRecognizerDelegate {
 
         setupLayout()
 
-        tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapHeader))
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapHeader))
         tapRecognizer.delegate = self
         headerView.addGestureRecognizer(tapRecognizer)
+
+        self.tapRecognizer = tapRecognizer
     }
 
     required init?(coder aDecoder: NSCoder) {
