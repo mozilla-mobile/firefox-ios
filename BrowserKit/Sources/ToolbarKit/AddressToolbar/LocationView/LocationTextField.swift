5035f0a1f2b4bb9c496764d930207b64274e5c64
FilippoZazzeroni <55580351+FilippoZazzeroni@users.noreply.github.com>
2024-12-18 17:58:50 +0100

    Bugfix FXIOS-10880 URLBarView enterOverlay mode is slow when opening first time keyboard (#23781)
    * Changed TextContentType to URL
    
    * Added better description to textContentType
    
    ---------
    
    Co-authored-by: Filippo <fzazzeroni@mozilla.com>

--- BrowserKit/Sources/ToolbarKit/AddressToolbar/LocationView/LocationTextField.swift
+++ BrowserKit/Sources/ToolbarKit/AddressToolbar/LocationView/LocationTextField.swift
@@ -50,6 +50,11 @@ class LocationTextField: UITextField, UITextFieldDelegate, ThemeApplicable {
         autocapitalizationType = .none
         returnKeyType = .go
         tintAdjustmentMode = .normal
+
+        // Setting the content type to a field that is not related to AutoFill functionality
+        // like email and password, should disable the Operating system to load those content,
+        // hence having a faster keyboard start up the first time
+        textContentType = .URL
         delegate = self
 
         // Disable dragging urls on iPhones because it conflicts with editing the text
