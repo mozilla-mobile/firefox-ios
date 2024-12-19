3cab8a6cf3950c134965b94c3846aa0d415694c2
bmihai23 <bolojanm23@gmail.com>
2024-12-19 13:46:32 +0200

    Refactor shouldStartAtHome function for improved readability

--- firefox-ios/Client/Frontend/Browser/StartAtHomeHelper.swift
+++ firefox-ios/Client/Frontend/Browser/StartAtHomeHelper.swift
@@ -44,16 +44,14 @@ class StartAtHomeHelper: FeatureFlaggable {
     /// the user's last activity and whether, based on their settings, Start at Home feature
     /// should perform its function.
     public func shouldStartAtHome() -> Bool {
-        let setting = startAtHomeSetting
-
-        let lastActiveTimestamp = UserDefaults.standard.object(forKey: "LastActiveTimestamp") as? Date ?? Date()
-        let dateComponents = Calendar.current.dateComponents([.hour, .second],
-                                                             from: lastActiveTimestamp,
-                                                             to: Date())
-
-        switch setting {
+        switch startAtHomeSetting {
         case .afterFourHours:
-            return dateComponents.hour ?? 0 >= 4
+            let lastActiveTimestamp = UserDefaults.standard.object(forKey: "LastActiveTimestamp") as? Date ?? Date()
+            let dateComponents = Calendar.current.dateComponents([.hour],
+                                                                 from: lastActiveTimestamp,
+                                                                 to: Date())
+            let hoursSinceLastActive = dateComponents.hour ?? 0
+            return hoursSinceLastActive >= 4
         case .always:
             return true
         case .disabled:
