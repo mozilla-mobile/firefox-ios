4510650c3b454693153f96a089c1bbf311c37f9e
Marceau Tonelli <marceau.tonelli@gmail.com>
2024-12-19 16:56:46 +0100

    Resolve subsequent build errors in HistoryPanel

--- firefox-ios/Client/Frontend/Library/HistoryPanel/HistoryPanel.swift
+++ firefox-ios/Client/Frontend/Library/HistoryPanel/HistoryPanel.swift
@@ -854,7 +854,7 @@ extension HistoryPanel {
     }
 
     private func resyncHistory() {
-        profile.syncManager.syncHistory().uponQueue(.main) { syncResult in
+        profile.syncManager?.syncHistory().uponQueue(.main) { syncResult in
             self.endRefreshing()
 
             if syncResult.isSuccess {
