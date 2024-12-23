04eb2ecd6c56ccfcf41b5d40d00e0dc34a1294ef
lmarceau <lmarceau@mozilla.com>
2024-12-23 16:16:42 -0500

    Add FXIOS-10895 [Bookmarks evolution] Add save button + fix issue with saving an update to bookmark (#23942)
    Add save button + fix issue with saving an update to bookmark
    
    + adjust tests

--- firefox-ios/Client/Frontend/Library/Bookmarks/Edit Folder/EditFolderViewController.swift
+++ firefox-ios/Client/Frontend/Library/Bookmarks/Edit Folder/EditFolderViewController.swift
@@ -44,6 +44,16 @@ class EditFolderViewController: UIViewController,
         view.tableHeaderView = headerSpacerView
     }
 
+    private lazy var saveBarButton: UIBarButtonItem =  {
+        let button = UIBarButtonItem(
+            title: String.Bookmarks.Menu.EditBookmarkSave,
+            style: .done,
+            target: self,
+            action: #selector(saveButtonAction)
+        )
+        return button
+    }()
+
     init(viewModel: EditFolderViewModel,
          windowUUID: WindowUUID,
          themeManager: any ThemeManager = AppContainer.shared.resolve(),
@@ -64,6 +74,7 @@ class EditFolderViewController: UIViewController,
     override func viewDidLoad() {
         super.viewDidLoad()
         title = viewModel.controllerTitle
+        navigationItem.rightBarButtonItem = saveBarButton
         viewModel.onFolderStatusUpdate = { [weak self] in
             self?.tableView.reloadSections(IndexSet(integer: Section.parentFolder.rawValue), with: .automatic)
         }
@@ -107,6 +118,14 @@ class EditFolderViewController: UIViewController,
             tableView.topAnchor.constraint(equalTo: view.topAnchor),
             tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
+    }
+
+    // MARK: - Actions
+
+    @objc
+    func saveButtonAction() {
+        // Save will happen in viewWillDisappear
+        navigationController?.popViewController(animated: true)
     }
 
     // MARK: - Themeable
