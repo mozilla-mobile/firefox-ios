0bc1615ceb298edf780d51d51d5303a27e81d545
dragosb01 <134391433+dragosb01@users.noreply.github.com>
2025-01-17 12:20:07 +0200

    Fixes MTE-4140 - for multiple ipad failures (#24200)
    (cherry picked from commit 250df85ed0b2f20f5ad4502be9e8c6e12f303935)
    
    # Conflicts:
    #	firefox-ios/firefox-ios-tests/Tests/XCUITests/TopTabsTest.swift

--- firefox-ios/firefox-ios-tests/Tests/XCUITests/TopTabsTest.swift
+++ firefox-ios/firefox-ios-tests/Tests/XCUITests/TopTabsTest.swift
@@ -350,8 +350,13 @@ class TopTabsTest: BaseTestCase {
         if !iPad() {
             tabsTrayCell.staticTexts.element(boundBy: 3).tap()
         } else {
+<<<<<<< HEAD
             XCTAssertEqual(tabsTrayCell.count, Int(numTab!))
             tabsTrayCell.staticTexts.element(boundBy: 6).tap()
+=======
+            XCTAssertTrue(Int(numTab!) == 11)
+            tabsTrayCell.staticTexts.element(boundBy: 6).waitAndTap()
+>>>>>>> 250df85ed (Fixes MTE-4140 - for multiple ipad failures (#24200))
         }
         // The current tab’s thumbnail is focused in the “Open Tabs” view
         navigator.nowAt(NewTabScreen)
