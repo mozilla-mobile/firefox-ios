3c01db1db59c3cdc81fdbf1d60132f4d5ebd3c6c
yoanarios <yriosdiaz@mozilla.com>
2025-01-17 09:40:59 -0500

    Create BookmarksTelemetry struct

---
+++ firefox-ios/Client/Frontend/Library/Bookmarks/BookmarksTelemetry.swift
@@ -0,0 +1,27 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import Foundation
+import Glean
+
+struct BookmarksTelemetry {
+    private let gleanWrapper: GleanWrapper
+    private let label = "bookmarks-panel"
+
+    init(gleanWrapper: GleanWrapper = DefaultGleanWrapper()) {
+        self.gleanWrapper = gleanWrapper
+    }
+
+    func addBookmarkFolder() {
+        gleanWrapper.recordEvent(for: GleanMetrics.Bookmarks.folderAdd)
+    }
+
+    func deleteBookmark() {
+        gleanWrapper.recordLabel(for: GleanMetrics.Bookmarks.delete, label: label)
+    }
+
+    func openBookmarksPanel(isSwipeGesture: Bool) {
+        gleanWrapper.recordLabel(for: GleanMetrics.Bookmarks.open, label: label)
+    }
+}
