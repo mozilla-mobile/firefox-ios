a09a7b381bd921287dfa011730ab09590368a636
mattlichtenstein <lichtensteinmp@gmail.com>
2025-01-17 15:45:26 -0500

    drag to dismiss keyboard

--- firefox-ios/Client/Frontend/Library/Bookmarks/Edit Folder/EditFolderViewController.swift
+++ firefox-ios/Client/Frontend/Library/Bookmarks/Edit Folder/EditFolderViewController.swift
@@ -42,6 +42,7 @@ class EditFolderViewController: UIViewController,
         let headerSpacerView = UIView(frame: CGRect(origin: .zero,
                                                     size: CGSize(width: 0, height: UX.editFolderCellTopPadding)))
         view.tableHeaderView = headerSpacerView
+        view.keyboardDismissMode = .onDrag
     }
 
     private lazy var saveBarButton: UIBarButtonItem =  {
