01943c8de90b7516fc03842089a72c60d0f4de79
Isabella <ihugel@mozilla.com>
2025-01-20 16:06:28 -0600

    Bugfix FXIOS-7301 Fix swiftlint build errors on main (#24259)
    * Fix closure length violation in Focus.
    
    * Fix to properly swiftlint ignore nimbus generated files in Focus.

--- focus-ios/BlockzillaPackage/Sources/Onboarding/SwiftUI Onboarding/ShowMeHowOnboardingView.swift
+++ focus-ios/BlockzillaPackage/Sources/Onboarding/SwiftUI Onboarding/ShowMeHowOnboardingView.swift
@@ -18,25 +18,9 @@ public struct ShowMeHowOnboardingView: View {
         NavigationView {
             ScrollView {
                 VStack(alignment: .leading, spacing: .verticalSpacing) {
-                    HStack(alignment: .top, spacing: .horizontalSpacing) {
-                        Image.stepOneImage
-                            .resizable()
-                            .frame(width: .iconSize, height: .iconSize)
-                            .foregroundColor(.gray)
-                        Text(config.subtitleStep1)
-                            .font(.body16)
-                            .multilineTextAlignment(.leading)
-                    }
+                    OnboardingInstructionLabel(image: Image.stepOneImage, label: config.subtitleStep1)
                     VStack(alignment: .leading, spacing: .horizontalSpacing) {
-                        HStack(alignment: .top, spacing: .horizontalSpacing) {
-                            Image.stepTwoImage
-                                .resizable()
-                                .frame(width: .iconSize, height: .iconSize)
-                                .foregroundColor(.gray)
-                            Text(config.subtitleStep2)
-                                .font(.body16)
-                                .multilineTextAlignment(.leading)
-                        }
+                        OnboardingInstructionLabel(image: Image.stepTwoImage, label: config.subtitleStep2)
                         HStack {
                             Spacer()
                             Image.jiggleModeImage
@@ -44,15 +28,7 @@ public struct ShowMeHowOnboardingView: View {
                         }
                     }
                     VStack(alignment: .leading, spacing: .horizontalSpacing) {
-                        HStack(alignment: .top, spacing: .horizontalSpacing) {
-                            Image.stepThreeImage
-                                .resizable()
-                                .frame(width: .iconSize, height: .iconSize)
-                                .foregroundColor(.gray)
-                            Text(config.subtitleStep3)
-                                .font(.body16)
-                                .multilineTextAlignment(.leading)
-                        }
+                        OnboardingInstructionLabel(image: Image.stepThreeImage, label: config.subtitleStep3)
                         HStack {
                             Spacer()
                             OnboardingSearchWidgetView(title: config.widgetText, padding: true, background: true)
@@ -73,6 +49,28 @@ public struct ShowMeHowOnboardingView: View {
                     }
             }
             .navigationBarBackButtonHidden(true)
+        }
+    }
+}
+
+public struct OnboardingInstructionLabel: View {
+    private let image: Image
+    private let label: String
+
+    public init(image: Image, label: String) {
+        self.image = image
+        self.label = label
+    }
+
+    public var body: some View {
+        HStack(alignment: .top, spacing: .horizontalSpacing) {
+            image
+                .resizable()
+                .frame(width: .iconSize, height: .iconSize)
+                .foregroundColor(.gray)
+            Text(label)
+                .font(.body16)
+                .multilineTextAlignment(.leading)
         }
     }
 }
