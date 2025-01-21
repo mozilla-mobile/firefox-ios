25f668ded77dbe8d5416cee0da2a1a47312d47b7
Carson Ramsden <carsonramsden@gmail.com>
2025-01-21 06:51:33 -0700

    Remove [FXIOS-10299] remove universal link logic from Firefox and FirefoxBeta (#24196)
    Remove universal links logic

--- firefox-ios/Client/Coordinators/Router/RouteBuilder.swift
+++ firefox-ios/Client/Coordinators/Router/RouteBuilder.swift
@@ -158,8 +158,7 @@ final class RouteBuilder: FeatureFlaggable {
 
         // If the user activity has a webpageURL, it's a deep link or an old history item.
         // Use the URL to create a new search tab.
-        if let url = userActivity.webpageURL,
-           isBrowsingActivity(userActivity) {
+        if let url = userActivity.webpageURL {
             return .search(url: url, isPrivate: false)
         }
 
@@ -204,15 +203,6 @@ final class RouteBuilder: FeatureFlaggable {
             }
         case .qrCode:
             return .action(action: .showQRCode)
-        }
-    }
-
-    private func isBrowsingActivity(_ userActivity: NSUserActivity) -> Bool {
-        if featureFlags.isFeatureEnabled(.universalLinks, checking: .buildOnly) {
-            return userActivity.activityType == NSUserActivityTypeBrowsingWeb ||
-            userActivity.activityType == browsingActivityType
-        } else {
-            return true
         }
     }
 
