ce34e553e1281c01824240505edfe94a787c6107
Clare So <cso@mozilla.com>
2025-01-29 10:47:43 -0500

    Only do testToS

--- firefox-ios/l10n-screenshots.sh
+++ firefox-ios/l10n-screenshots.sh
@@ -35,7 +35,7 @@ for lang in $LOCALES; do
     mkdir "l10n-screenshots/$lang"
     fastlane snapshot --project firefox-ios/Client.xcodeproj --scheme L10nSnapshotTests \
         --testPlan L10nSnapshotTests \
-        --only-testing L10nSuite1SnapshotTests/testToS
+        --only-testing L10nSuite1SnapshotTests/testToS \
         --number_of_retries 0 \
         --skip_open_summary \
         --xcargs "-maximum-parallel-testing-workers 2" \
