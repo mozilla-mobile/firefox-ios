81304de45c8d774935c5c199423b7cfe881431cf
Jan Brasna <1784648+janbrasna@users.noreply.github.com>
2025-01-30 00:45:04 +0100

    Update desktop-like UAstring to 17.7

--- BrowserKit/Sources/Shared/UserAgent.swift
+++ BrowserKit/Sources/Shared/UserAgent.swift
@@ -48,7 +48,7 @@ open class UserAgent {
 
     public static func desktopUserAgent() -> String {
         // swiftlint:disable line_length
-        return "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.0 Safari/605.1.15"
+        return "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.7 Safari/605.1.15"
         // swiftlint:enable line_length
     }
 
