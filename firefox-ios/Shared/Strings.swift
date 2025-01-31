de223b2db93e396389a6acec40d6790f91a78cf0
ih-codes <ihugel@mozilla.com>
2025-01-31 15:57:46 -0600

    Revert fix partially (just fix English).

--- firefox-ios/Shared/Strings.swift
+++ firefox-ios/Shared/Strings.swift
@@ -7674,6 +7674,11 @@ extension String {
                 tableName: "Onboarding",
                 value: "%@ Terms of Service.",
                 comment: "Title for the Terms of Service button link, in the Terms of Service screen for redirecting the user to the Terms of Service page. Placeholder is for the app name.")
+            public static let SendCrashReportsSettingMessage = MZLocalizedString(
+                key: "Settings.CrashReports.Message.v135",
+                tableName: "Settings",
+                value: "Crash reports allow us to diagnose and fix issues with the browser.",
+                comment: "On the Settings screen, this is the subtitle text for a toggle which controls automatically sending crash reports.")
             public static let AgreementButtonTitle = MZLocalizedString(
                 key: "Onboarding.TermsOfService.AgreementButtonTitle.v135",
                 tableName: "Onboarding",
