bd2e082b10eee9c69f16765f1a56c8bf9b82b965
Jimoh Yusuf <yusuphjoluwaseun@gmail.com>
2025-02-03 11:11:23 +0000

    testing that swiftlint catches redundant type annptation

--- firefox-ios/Client/Frontend/Toolbar+URLBar/TabLocationView.swift
+++ firefox-ios/Client/Frontend/Toolbar+URLBar/TabLocationView.swift
@@ -56,7 +56,7 @@ class TabLocationView: UIView, FeatureFlaggable {
     let windowUUID: WindowUUID
     let logger: Logger
 
-    var isTrackingProtectionDisplayed = false
+    var isTrackingProtectionDisplayed:Bool = false
 
     /// Tracking protection button, gets updated from tabDidChangeContentBlocking
     var blockerStatus: BlockerStatus = .noBlockedURLs {
