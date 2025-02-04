540fbfaedc608de44c4ef0d18148d47b6fe6c432
Laurie Marceau <lmarceau@mozilla.com>
2025-02-04 10:17:04 -0500

    Create crash tracker and related tests

--- firefox-ios/Client/Frontend/Browser/BrowserViewController/Views/BrowserViewController.swift
+++ firefox-ios/Client/Frontend/Browser/BrowserViewController/Views/BrowserViewController.swift
@@ -128,6 +128,7 @@ class BrowserViewController: UIViewController,
 
     let profile: Profile
     let tabManager: TabManager
+    let crashTracker: CrashTracker
     let ratingPromptManager: RatingPromptManager
     var isToolbarRefactorEnabled: Bool {
         return featureFlags.isFeatureEnabled(.toolbarRefactor, checking: .buildOnly)
@@ -279,7 +280,8 @@ class BrowserViewController: UIViewController,
         self.tabManager = tabManager
         self.themeManager = themeManager
         self.notificationCenter = notificationCenter
-        self.ratingPromptManager = RatingPromptManager(prefs: profile.prefs)
+        self.crashTracker = DefaultCrashTracker()
+        self.ratingPromptManager = RatingPromptManager(prefs: profile.prefs, crashTracker: crashTracker)
         self.readerModeCache = DiskReaderModeCache.sharedInstance
         self.downloadQueue = downloadQueue
         self.logger = logger
@@ -344,7 +346,7 @@ class BrowserViewController: UIViewController,
             self?.browserDidBecomeActive()
         }
 
-        ratingPromptManager.updateData()
+        crashTracker.updateData()
         if featureFlags.isFeatureEnabled(.ratingPromptFeature, checking: .buildOnly) {
             ratingPromptManager.showRatingPromptIfNeeded()
         }
