e824747cbd18a7f1852bd1696089ac7fc1938f9e
Laurie Marceau <lmarceau@mozilla.com>
2025-02-07 10:54:09 -0500

    Add internal pages management in engine

---
+++ BrowserKit/Sources/WebEngine/WKWebview/Internal/WKErrorPageHandler.swift
@@ -0,0 +1,47 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import WebKit
+
+class WKErrorPageHandler: WKInternalSchemeResponse {
+    static let path = WKInternalURL.Path.errorpage.rawValue
+
+    func response(forRequest request: URLRequest) -> (URLResponse, Data)? {
+        guard let url = request.url,
+              let components = URLComponents(url: url, resolvingAgainstBaseURL: false),
+              let code = components.valueForQuery("code"),
+              let errCode = Int(code) else {
+            return nil
+        }
+
+        /// Used for checking if current error code is for no internet connection
+        let noInternetErrorCode = Int(
+            CFNetworkErrors.cfurlErrorNotConnectedToInternet.rawValue
+        )
+
+        return responseForNativeErrorPage(request: request)
+    }
+
+    func responseForNativeErrorPage(request: URLRequest) -> (URLResponse, Data)? {
+        guard let url = request.url else { return nil }
+        let response = WKInternalSchemeHandler.response(forUrl: url)
+        // Blank page with a color matching the background of the panels which
+        // is displayed for a split-second until the panel shows.
+        let html = """
+            <!DOCTYPE html>
+            <html>
+              <body></body>
+            </html>
+        """
+        guard let data = html.data(using: .utf8) else { return nil }
+        return (response, data)
+    }
+}
+
+fileprivate extension URLComponents {
+    // Return the first query parameter that matches
+    func valueForQuery(_ param: String) -> String? {
+        return self.queryItems?.first { $0.name == param }?.value
+    }
+}
