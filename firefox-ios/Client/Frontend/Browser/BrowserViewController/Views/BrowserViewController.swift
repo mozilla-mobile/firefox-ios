c77b0a238d50aaab27cd00b9edf5fc1db4143e80
cyndichin <cgc95@cornell.edu>
2025-02-14 10:01:55 -0500

    with redux

--- firefox-ios/Client/Frontend/Browser/BrowserViewController/Views/BrowserViewController.swift
+++ firefox-ios/Client/Frontend/Browser/BrowserViewController/Views/BrowserViewController.swift
@@ -1164,7 +1164,7 @@ class BrowserViewController: UIViewController,
 
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
         super.viewWillTransition(to: size, with: coordinator)
-
+        dispatchViewAction(for: size)
         dismissVisibleMenus()
 
         var fakespotNeedsUpdate = false
@@ -1201,6 +1201,16 @@ class BrowserViewController: UIViewController,
             self.scrollController.setMinimumZoom()
         })
         microsurvey?.setNeedsUpdateConstraints()
+    }
+
+    private func dispatchViewAction(for size: CGSize) {
+        store.dispatch(
+            ViewLifecycleAction(
+                size: size,
+                windowUUID: windowUUID,
+                actionType: ViewLifecycleActionType.viewWillTransition
+            )
+        )
     }
 
     override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
