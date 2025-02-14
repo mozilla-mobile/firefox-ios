7dee81820c2062cb3508669327e7e16cfb3e3c13
Laurie Marceau <lmarceau@mozilla.com>
2025-02-14 15:31:59 -0500

    Fix todo

--- firefox-ios/Client/Frontend/Settings/Main/AppSettingsTableViewController.swift
+++ firefox-ios/Client/Frontend/Settings/Main/AppSettingsTableViewController.swift
@@ -315,7 +315,7 @@ class AppSettingsTableViewController: SettingsTableViewController,
             HomeSetting(settings: self, settingsDelegate: parentCoordinator),
             ThemeSetting(settings: self, settingsDelegate: parentCoordinator),
             SiriPageSetting(settings: self, settingsDelegate: parentCoordinator),
-            AutoplaySetting(settings: self), // TODO: Coordinator
+            AutoplaySetting(settings: self, settingsDelegate: parentCoordinator),
         ]
 
         if isSearchBarLocationFeatureEnabled, let profile {
