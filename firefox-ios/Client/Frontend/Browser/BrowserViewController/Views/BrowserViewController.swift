3b078b39acc8bc192500e099aba1737710e77996
yoanarios <yriosdiaz@mozilla.com>
2025-02-18 11:20:07 -0500

    Bugfix FXIOS-11306 [Address Bar Animation] Loading bar color is incorrect in private mode after force-closing and resuming Firefox. (#24760)
    * Use applyUIMode to update theme for progressBar
    
    (cherry picked from commit c6e9eea612d13bb5e621f2011b4f0c735c57eeea)

--- firefox-ios/Client/Frontend/Browser/BrowserViewController/Views/BrowserViewController.swift
+++ firefox-ios/Client/Frontend/Browser/BrowserViewController/Views/BrowserViewController.swift
@@ -3221,9 +3221,11 @@ class BrowserViewController: UIViewController,
 
         tabManager.selectedTab?.applyTheme(theme: currentTheme)
 
+        let isPrivate = tabManager.selectedTab?.isPrivate ?? false
         if !isToolbarRefactorEnabled {
-            let isPrivate = tabManager.selectedTab?.isPrivate ?? false
             legacyUrlBar?.applyUIMode(isPrivate: isPrivate, theme: currentTheme)
+        } else {
+            addressToolbarContainer.applyUIMode(isPrivate: isPrivate, theme: currentTheme)
         }
 
         toolbar.applyTheme(theme: currentTheme)
