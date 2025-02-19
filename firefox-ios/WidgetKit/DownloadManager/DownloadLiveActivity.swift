5bed8855edbbc3ebe88865ad42972cb702b54174
Ryan Li <ryan.lichangrui@gmail.com>
2025-02-19 12:15:07 -0500

    initial impl

--- firefox-ios/WidgetKit/DownloadManager/DownloadLiveActivity.swift
+++ firefox-ios/WidgetKit/DownloadManager/DownloadLiveActivity.swift
@@ -5,9 +5,52 @@
 import WidgetKit
 import ActivityKit
 import SwiftUI
+import Foundation
 
 struct DownloadLiveActivityAttributes: ActivityAttributes {
     struct ContentState: Codable, Hashable {
+        struct Download: Codable, Hashable {
+            var id: UUID
+            var fileName: String
+            var mimeType: String
+            var hasContentEncoding: Bool?
+            var downloadPath: URL
+
+            var totalBytesExpected: Int64?
+            var bytesDownloaded: UInt64
+            var isComplete: Bool
+        }
+        var downloads: [Download]
+        
+        func getCompletedDownloads() -> Int {
+            return downloads.filter { $0.isComplete }.count
+        }
+        
+        func getTotalDownloads() -> Int {
+            return downloads.count
+        }
+        
+        func getTotalProgress() -> Double {
+            var totalBytesExpected: UInt64 = 0
+            var totalBytesDownloaded: UInt64 = 0
+            
+            for download in downloads {
+                // downloads with content encoding cannot
+                // be estimated accurately and should be
+                // skipped entirely in the calculation of progress
+                if download.hasContentEncoding == true || download.totalBytesExpected == nil{
+                    continue
+                }
+                totalBytesExpected += UInt64(download.totalBytesExpected!)
+                totalBytesDownloaded += download.bytesDownloaded
+            }
+            
+            if totalBytesExpected == 0 {
+                return 0
+            }
+            
+            return Double(totalBytesDownloaded) / Double(totalBytesExpected)
+        }
     }
 }
 
---
+++ firefox-ios/WidgetKitTests/WidgetKitTests.swift
@@ -0,0 +1,13 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import Testing
+
+struct WidgetKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
