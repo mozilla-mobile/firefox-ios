ffc8885a5cf666d27a284c27b06b3b9b42b97f5e
Laurie Marceau <lmarceau@mozilla.com>
2025-02-24 14:32:24 -0500

    Write tests

--- BrowserKit/Tests/WebEngineTests/WKEngineTests.swift
+++ BrowserKit/Tests/WebEngineTests/WKEngineTests.swift
@@ -2,20 +2,27 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
+import Common
 import XCTest
 @testable import WebEngine
 
 final class WKEngineTests: XCTestCase {
     private var userScriptManager: MockWKUserScriptManager!
+    private var webServerUtil: MockWKWebServerUtil!
+    private var sourceTimerFactory: MockDispatchSourceTimerFactory!
 
     override func setUp() {
         super.setUp()
         userScriptManager = MockWKUserScriptManager()
+        webServerUtil = MockWKWebServerUtil()
+        sourceTimerFactory = MockDispatchSourceTimerFactory()
     }
 
     override func tearDown() {
-        super.tearDown()
         userScriptManager = nil
+        webServerUtil = nil
+        sourceTimerFactory = nil
+        super.tearDown()
     }
 
     func testCreateViewThenCreatesView() {
@@ -32,11 +39,41 @@ final class WKEngineTests: XCTestCase {
         XCTAssertNotNil(session)
     }
 
+    func testWarmEngineCallsSetupEngine() {
+        let subject = createSubject()
+        subject.warmEngine()
+
+        XCTAssertEqual(sourceTimerFactory.dispatchSource.cancelCalled, 0)
+        XCTAssertEqual(webServerUtil.setUpWebServerCalled, 1)
+    }
+
+    func testWarmEngineAfterIdle() {
+        let subject = createSubject()
+        subject.idleEngine()
+        subject.warmEngine()
+
+        XCTAssertEqual(sourceTimerFactory.dispatchSource.cancelCalled, 1)
+        XCTAssertEqual(webServerUtil.setUpWebServerCalled, 1)
+    }
+
+    func testIdleEngineCallsStopEngine() {
+        let subject = createSubject()
+        subject.idleEngine()
+
+        XCTAssertEqual(sourceTimerFactory.dispatchSource.scheduleCalled, 1)
+        XCTAssertEqual(sourceTimerFactory.dispatchSource.setEventHandlerCalled, 1)
+        XCTAssertEqual(sourceTimerFactory.dispatchSource.resumeCalled, 1)
+        XCTAssertEqual(webServerUtil.stopWebServerCalled, 1)
+    }
+
     // MARK: Helper
 
-    func createSubject() -> WKEngine {
-        let subject = WKEngine(userScriptManager: userScriptManager)
-        trackForMemoryLeaks(subject)
+    func createSubject(file: StaticString = #file,
+                       line: UInt = #line) -> WKEngine {
+        let subject = WKEngine(userScriptManager: userScriptManager,
+                               webServerUtil: webServerUtil,
+                               sourceTimerFactory: sourceTimerFactory)
+        trackForMemoryLeaks(subject, file: file, line: line)
         return subject
     }
 }
