c55c9ac2730d079c6819e6d8e3c2626de1bdfc66
ih-codes <ihugel@mozilla.com>
2025-02-28 11:10:37 -0600

    Restructure if let instead of guard to fix upload error.

--- firefox-ios/Client/Frontend/Settings/AppIconSelection/AppIconView.swift
+++ firefox-ios/Client/Frontend/Settings/AppIconSelection/AppIconView.swift
@@ -47,46 +47,46 @@ struct AppIconView: View, ThemeApplicable {
     }
 
     var body: some View {
-        guard let image = UIImage(named: appIcon.imageSetAssetName) else {
-            return EmptyView()
-        }
+        if let image = UIImage(named: appIcon.imageSetAssetName) {
+            Group {
+                Button(action: { setAppIcon(appIcon) }) {
+                    HStack {
+                        Image(systemName: selectionImageIdentifier)
+                            .padding(.trailing, UX.itemPadding)
+                            .tint(themeColors.actionPrimary.color)
+                            .accessibilityLabel(selectionImageAccessibilityLabel)
 
-        return Group {
-            Button(action: { setAppIcon(appIcon) }) {
-                HStack {
-                    Image(systemName: selectionImageIdentifier)
-                        .padding(.trailing, UX.itemPadding)
-                        .tint(themeColors.actionPrimary.color)
-                        .accessibilityLabel(selectionImageAccessibilityLabel)
+                        // swiftlint:disable:next accessibility_label_for_image
+                        Image(uiImage: image)
+                            .resizable()
+                            .frame(width: UX.appIconSize, height: UX.appIconSize)
+                            .cornerRadius(UX.cornerRadius)
+                            .overlay(
+                                // Add rounded border
+                                RoundedRectangle(cornerRadius: UX.cornerRadius)
+                                    .stroke(themeColors.borderPrimary.color, lineWidth: UX.appIconBorderWidth)
+                            )
+                            .padding(.trailing, UX.itemPadding)
 
-                    // swiftlint:disable:next accessibility_label_for_image
-                    Image(uiImage: image)
-                        .resizable()
-                        .frame(width: UX.appIconSize, height: UX.appIconSize)
-                        .cornerRadius(UX.cornerRadius)
-                        .overlay(
-                            // Add rounded border
-                            RoundedRectangle(cornerRadius: UX.cornerRadius)
-                                .stroke(themeColors.borderPrimary.color, lineWidth: UX.appIconBorderWidth)
-                        )
-                        .padding(.trailing, UX.itemPadding)
+                        Text(appIcon.displayName)
+                            .tint(themeColors.textPrimary.color)
 
-                    Text(appIcon.displayName)
-                        .tint(themeColors.textPrimary.color)
-
-                    Spacer()
+                        Spacer()
+                    }
+                    .padding(.all, UX.itemPadding)
                 }
-                .padding(.all, UX.itemPadding)
+                .background(Color.clear)
+                .accessibilityHint(selectionAccessibilityHint)
             }
-            .background(Color.clear)
-            .accessibilityHint(selectionAccessibilityHint)
-        }
-        .onAppear {
-            applyTheme(theme: themeManager.getCurrentTheme(for: windowUUID))
-        }
-        .onReceive(NotificationCenter.default.publisher(for: .ThemeDidChange)) { notification in
-            guard let uuid = notification.windowUUID, uuid == windowUUID else { return }
-            applyTheme(theme: themeManager.getCurrentTheme(for: windowUUID))
+            .onAppear {
+                applyTheme(theme: themeManager.getCurrentTheme(for: windowUUID))
+            }
+            .onReceive(NotificationCenter.default.publisher(for: .ThemeDidChange)) { notification in
+                guard let uuid = notification.windowUUID, uuid == windowUUID else { return }
+                applyTheme(theme: themeManager.getCurrentTheme(for: windowUUID))
+            }
+        } else {
+            EmptyView()
         }
     }
 
