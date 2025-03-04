ed1aad3536105680005d76b6f067466172c488d9
Daniel Dervishi <58835213+DanielDervishi@users.noreply.github.com>
2025-03-04 16:00:58 -0500

    initial draft

--- firefox-ios/Client/Frontend/Browser/BrowserViewController/Views/BrowserViewController.swift
+++ firefox-ios/Client/Frontend/Browser/BrowserViewController/Views/BrowserViewController.swift
@@ -14,6 +14,8 @@ import MobileCoreServices
 import Common
 import Redux
 import WebEngine
+import WidgetKit
+import ActivityKit
 
 import class MozillaAppServices.BookmarkFolderData
 import class MozillaAppServices.BookmarkItemData
@@ -110,6 +112,13 @@ class BrowserViewController: UIViewController,
     var keyboardBackdrop: UIView?
     var pendingToast: Toast? // A toast that might be waiting for BVC to appear before displaying
     var downloadToast: DownloadToast? // A toast that is showing the combined download progress
+    var downloadProgressManager: DownloadProgressManager?
+
+    var _downloadLiveActivityWrapper: Any?
+    @available(iOS 16.2, *)
+    var downloadLiveActivityWrapper: DownloadLiveActivityWrapper? {
+        return _downloadLiveActivityWrapper as? DownloadLiveActivityWrapper
+    }
 
     // popover rotation handling
     var displayedPopoverController: UIViewController?
