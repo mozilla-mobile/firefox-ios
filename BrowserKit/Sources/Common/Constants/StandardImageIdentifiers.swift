85bfd82edc09de51b37ea9d05428e7d5fdc4c243
Winnie Teichmann <4530+thatswinnie@users.noreply.github.com>
2025-03-10 17:56:16 +0100

    Add images

--- BrowserKit/Sources/Common/Constants/StandardImageIdentifiers.swift
+++ BrowserKit/Sources/Common/Constants/StandardImageIdentifiers.swift
@@ -24,11 +24,13 @@ public struct StandardImageIdentifiers {
 
     // Icon size 20x20
     public struct Medium {
+        public static let arrowClockwise = "arrowClockwiseMedium"
         public static let bookmarkBadgeFillBlue50 = "bookmarkBadgeFillMediumBlue50"
         public static let cross = "crossMedium"
         public static let crossCircleFill = "crossCircleFillMedium"
         public static let nightMode = "nightModeMedium"
         public static let privateModeCircleFillPurple = "privateModeCircleFillMediumPurple"
+        public static let shareiOS = "shareiOSMedium"
         public static let starFill = "starFillMedium"
         public static let starOneHalfFill = "starOneHalfFillMedium"
         public static let sun = "sunMedium"
