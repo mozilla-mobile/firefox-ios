8ea7f2fb6bf6b86b8eddb218fb9c2ad383ca882c
Isabella <173110554+ih-codes@users.noreply.github.com>
2025-03-10 15:22:42 -0600

    Add FXIOS-11520 [App Icon 2025] Add several new alternative app icon variants (#25088)
    * Add all the new alternative light/dark App Icon and Image Set assets. Update the build settings to include new alternative app assets.
    
    * Temporarily remove hand-drawn App Icons and add FXIOS-11579 tag while we wait for approval.
    
    ---------
    
    Co-authored-by: Carson Ramsden <carsonramsden@gmail.com>

--- firefox-ios/Client/Frontend/Settings/AppIconSelection/AppIcon.swift
+++ firefox-ios/Client/Frontend/Settings/AppIconSelection/AppIcon.swift
@@ -5,14 +5,49 @@
 import Foundation
 
 enum AppIcon: String, CaseIterable {
+    // MARK: Default
     /// The default system app icon.
     case regular
 
+    // MARK: Alternative app icons that do NOT adjust to the iOS 18+ light/dark/tinted modes
     /// The old app icon before the iOS 18 light/dark/tinted update. Has a purple dark background in all modes.
     case darkPurple
 
+    // MARK: Alternative app icons which do adjust to the iOS 18+ light/dark/tinted modes. Add alphabetically.
+    /// A blue version of the icon.
+    case blue
+
+    /// A blue version of the icon with a gradient background.
+    case blueGradient
+
+    /// A green version of the icon.
+    case green
+
+    // TODO: FXIOS-11579 We are waiting to see if we can use hand-drawn icon alternatives.
+    // swiftlint:disable orphaned_doc_comment
     /// An artsy app icon of a person hugging the Firefox logo. Has a light orange background in all modes.
-    case hug
+//    case hug
+
+    /// An artsy app icon which has a fox sleeping on the globe.
+//    case lazy
+
+    /// An orange version of the icon with a gradient background.
+    case orangeGradient
+
+    /// A pink version of the icon.
+    case pink
+
+    /// A pixelated version of the regular app icon.
+//    case pixelated
+
+    /// A pride fox logo.
+//    case pride
+
+    /// A red version of the icon with a gradient background.
+    case redGradient
+
+    /// The retro Firefox app icon.
+//    case retro
 
     /// The name of the asset to display in the app selection.
     var displayName: String {
@@ -21,8 +56,28 @@ enum AppIcon: String, CaseIterable {
             return .Settings.AppIconSelection.AppIconNames.Regular
         case .darkPurple:
             return .Settings.AppIconSelection.AppIconNames.DarkPurple
-        case .hug:
-            return .Settings.AppIconSelection.AppIconNames.Hug
+        case .blue:
+            return .Settings.AppIconSelection.AppIconNames.Blue
+        case .blueGradient:
+            return .Settings.AppIconSelection.AppIconNames.BlueGradient
+        case .green:
+            return .Settings.AppIconSelection.AppIconNames.Green
+//        case .hug:
+//            return .Settings.AppIconSelection.AppIconNames.Hug
+//        case .lazy:
+//            return .Settings.AppIconSelection.AppIconNames.Lazy
+        case .orangeGradient:
+            return .Settings.AppIconSelection.AppIconNames.OrangeGradient
+        case .pink:
+            return .Settings.AppIconSelection.AppIconNames.Pink
+//        case .pixelated:
+//            return .Settings.AppIconSelection.AppIconNames.Pixelated
+//        case .pride:
+//            return .Settings.AppIconSelection.AppIconNames.Pride
+        case .redGradient:
+            return .Settings.AppIconSelection.AppIconNames.RedGradient
+//        case .retro:
+//            return .Settings.AppIconSelection.AppIconNames.Retro
         }
     }
 
@@ -37,8 +92,28 @@ enum AppIcon: String, CaseIterable {
             return "appIconAlternate_default"
         case .darkPurple:
             return "appIconAlternate_darkPurple"
-        case .hug:
-            return "appIconAlternate_hug"
+        case .blue:
+            return "appIconAlternate_blue"
+        case .blueGradient:
+            return "appIconAlternate_blueGradient"
+        case .green:
+            return "appIconAlternate_green"
+//        case .hug:
+//            return "appIconAlternate_hug"
+//        case .lazy:
+//            return "appIconAlternate_lazy"
+        case .orangeGradient:
+            return "appIconAlternate_orangeGradient"
+        case .pink:
+            return "appIconAlternate_pink"
+//        case .pixelated:
+//            return "appIconAlternate_pixelated"
+//        case .pride:
+//            return "appIconAlternate_pride"
+        case .redGradient:
+            return "appIconAlternate_redGradient"
+//        case .retro:
+//            return "appIconAlternate_retro"
         }
     }
 
@@ -49,8 +124,28 @@ enum AppIcon: String, CaseIterable {
             return nil // Setting the alternative app icon to nil will restore the default app icon asset
         case .darkPurple:
             return "AppIcon_Alt_DarkPurple"
-        case .hug:
-            return "AppIcon_Alt_Hug"
+        case .blue:
+            return "AppIcon_Alt_Blue"
+        case .blueGradient:
+            return "AppIcon_Alt_BlueGradient"
+        case .green:
+            return "AppIcon_Alt_Green"
+//        case .hug:
+//            return "AppIcon_Alt_Hug"
+//        case .lazy:
+//            return "AppIcon_Alt_Lazy"
+        case .orangeGradient:
+            return "AppIcon_Alt_OrangeGradient"
+        case .pink:
+            return "AppIcon_Alt_Pink"
+//        case .pixelated:
+//            return "AppIcon_Alt_Pixelated"
+//        case .pride:
+//            return "AppIcon_Alt_Pride"
+        case .redGradient:
+            return "AppIcon_Alt_RedGradient"
+//        case .retro:
+//            return "AppIcon_Alt_Retro"
         }
     }
 
@@ -69,3 +164,4 @@ enum AppIcon: String, CaseIterable {
         }
     }
 }
+// swiftlint:enable orphaned_doc_comment
