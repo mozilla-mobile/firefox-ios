85c282f6cdc4215386c00e29118b223a4a4cb7a1
Litianu Razvan <litianu.razvan@gmail.com>
2025-03-11 14:25:16 +0200

    Add FXIOS-10930 Focus iOS ToS Card in Onboarding (#25124)
    * FXIOS-10930 Focus iOS ToS Card in Onboarding
    
    * Delete spaces
    
    * Swiftlint fix
    
    * Update with terms of use
    
    * Disable test
    
    * Add separate legal urls
    
    * Update focus-ios/Blockzilla/Onboarding/OnboardingFactory.swift
    
    Co-authored-by: Francesco Lodolo <flod@lodolo.net>
    
    * Update focus-ios/Blockzilla/Onboarding/OnboardingFactory.swift
    
    Co-authored-by: Francesco Lodolo <flod@lodolo.net>
    
    * Update focus-ios/Blockzilla/Onboarding/OnboardingFactory.swift
    
    Co-authored-by: Francesco Lodolo <flod@lodolo.net>
    
    * Update focus-ios/Blockzilla/Onboarding/OnboardingFactory.swift
    
    Co-authored-by: Francesco Lodolo <flod@lodolo.net>
    
    * Update focus-ios/Blockzilla/Onboarding/OnboardingFactory.swift
    
    Co-authored-by: Francesco Lodolo <flod@lodolo.net>
    
    * Update focus-ios/Blockzilla/Onboarding/OnboardingFactory.swift
    
    Co-authored-by: Francesco Lodolo <flod@lodolo.net>
    
    * Update focus-ios/Blockzilla/Onboarding/OnboardingFactory.swift
    
    Co-authored-by: Francesco Lodolo <flod@lodolo.net>
    
    * Update focus-ios/Blockzilla/Onboarding/OnboardingFactory.swift
    
    Co-authored-by: Francesco Lodolo <flod@lodolo.net>
    
    * Add terms links
    
    ---------
    
    Co-authored-by: Francesco Lodolo <flod@lodolo.net>
    (cherry picked from commit 837e0b612a44ecb2cb685329e2bb1939cd617631)

--- focus-ios/Blockzilla/Onboarding/OnboardingFactory.swift
+++ focus-ios/Blockzilla/Onboarding/OnboardingFactory.swift
@@ -10,6 +10,7 @@ class TestOnboarding: OnboardingEventsHandling {
     @Published var route: Onboarding.ToolTipRoute?
     var routePublisher: Published<Onboarding.ToolTipRoute?>.Publisher { $route }
     func send(_ action: Onboarding.Action) {}
+    func dismissTooltip(route: Onboarding.ToolTipRoute) {}
 }
 
 class OnboardingFactory {
@@ -52,6 +53,19 @@ class OnboardingFactory {
                     secondSubtitle: .defaultBrowserOnboardingViewSecondSubtitleV2,
                     topButtonTitle: .defaultBrowserOnboardingViewTopButtonTitleV2,
                     bottomButtonTitle: .defaultBrowserOnboardingViewBottomButtonTitleV2),
+                tosConfig: .init(
+                    title: .termsOfUseTitle,
+                    subtitle: .termsOfUseSubtitle,
+                    termsText: String(format: .termsOfUseText, String.termsLinkText),
+                    privacyText: String(format: .privacyNoticeText, String.privacyLinkText),
+                    termsLinkText: .termsLinkText,
+                    privacyLinkText: .privacyLinkText,
+                    buttonText: .termsOfUseButtonText,
+                    doneButton: .doneButtonText
+                ),
+                isTosEnabled: true,
+                termsURL: termsURL,
+                privacyURL: privacyURL,
                 dismissAction: dismissAction,
                 telemetry: { action in
                     switch action {
@@ -69,6 +83,13 @@ class OnboardingFactory {
                         telemetry(.defaultBrowserSkip)
                     case .defaultBrowserAppeared:
                         telemetry(.defaultBrowserAppeared)
+                    // Add telemetry
+                    case .onAcceptAndContinueTapped:
+                        break
+                    case .openTermsOfUse:
+                        break
+                    case .openPrivacyNotice:
+                        break
                     }
                 })
             let controller = PortraitHostingController(rootView: OnboardingView(viewModel: onboardingViewModel))
@@ -95,6 +116,14 @@ class OnboardingFactory {
             return controller
         }
     }
+
+    static var termsURL: URL {
+        URL(string: "https://www.mozilla.org/about/legal/terms/firefox-focus/")!
+    }
+
+    static var privacyURL: URL {
+        URL(string: "https://www.mozilla.org/privacy/firefox-focus/")!
+    }
 }
 
 fileprivate extension String {
@@ -116,4 +145,44 @@ fileprivate extension String {
     static let defaultBrowserOnboardingViewSecondSubtitleV2 = String(format: NSLocalizedString("Onboarding.DefaultBrowser.SecondSubtitle.V2", value: "Make %@ your default to protect your data with every link you open.", comment: "Text for a label that indicates the second subtitle for the default browser onboarding screen version 2. %@ is the name of the app (Focus/Klar)"), AppInfo.shortProductName)
     static let defaultBrowserOnboardingViewTopButtonTitleV2 = NSLocalizedString("Onboarding.DefaultBrowser.TopButtonTitle.V2", value: "Set as Default Browser", comment: "Text for a label that indicates the title of the top button from the default browser onboarding screen version 2.")
     static let defaultBrowserOnboardingViewBottomButtonTitleV2 = NSLocalizedString("Onboarding.DefaultBrowser.BottomButtonTitle.V2", value: "Skip", comment: "Text for a label that indicates the title of the bottom button from the default browser onboarding screen version 2.")
+    static let termsOfUseTitle = NSLocalizedString(
+        "Onboarding.TermsOfUse.Title",
+        value: String(format: "Welcome to %@", AppInfo.productName),
+        comment: "Title for the Terms of Use screen during onboarding. %@ is the app name (for example “Firefox Focus” or ”Firefox Klar”)."
+    )
+    static let termsOfUseSubtitle = NSLocalizedString(
+        "Onboarding.TermsOfUse.Subtitle",
+        value: "Fast. Private. No distractions.",
+        comment: "Subtitle for the Terms of Use screen during onboarding."
+    )
+    static let termsOfUseText = NSLocalizedString(
+        "Onboarding.TermsOfUse.Text",
+        value: "By continuing, you agree to %@.",
+        comment: "Text that describes the agreement to the Firefox Terms of Use. %@ is a link using Onboarding.TermsOfUse.LinkText as text."
+    )
+    static let termsLinkText = NSLocalizedString(
+        "Onboarding.TermsOfUse.LinkText",
+        value: "Firefox Terms of Use",
+        comment: "Used as a text for the link in Onboarding.TermsOfUse.Text."
+    )
+    static let privacyNoticeText = NSLocalizedString(
+        "Onboarding.PrivacyNotice.Text",
+        value: "Firefox cares about your privacy. Read more in %@.",
+        comment: "Text that describes Firefox's commitment to privacy. %@ is a link using Onboarding.PrivacyNotice.LinkText as text."
+    )
+    static let privacyLinkText = NSLocalizedString(
+        "Onboarding.PrivacyNotice.LinkText",
+        value: "Privacy Notice",
+        comment: "Used as a text for the link in Onboarding.PrivacyNotice.Text."
+    )
+    static let termsOfUseButtonText = NSLocalizedString(
+        "Onboarding.TermsOfUse.ButtonText",
+        value: "Agree and Continue",
+        comment: "Label for the agreement button in the Terms of Use screen."
+    )
+    static let doneButtonText = NSLocalizedString(
+        "Onboarding.TermsOfUse.Done.ButtonText",
+        value: "Done",
+        comment: "Button label to dismiss the Privacy Policy and Terms of Use screen."
+    )
 }
