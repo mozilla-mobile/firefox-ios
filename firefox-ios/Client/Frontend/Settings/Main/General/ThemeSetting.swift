8be13413519c13f2e5acd7bce1f030759f2a91fa
Issam Mani <issamouu69@gmail.com>
2025-03-11 03:48:31 +0100

    chore: cleanup + change string for settings menu entry

--- firefox-ios/Client/Frontend/Settings/Main/General/ThemeSetting.swift
+++ firefox-ios/Client/Frontend/Settings/Main/General/ThemeSetting.swift
@@ -19,18 +19,6 @@ class ThemeSetting: Setting {
         return AccessibilityIdentifiers.Settings.Theme.title
     }
 
-    override var status: NSAttributedString {
-        if themeManager.systemThemeIsOn {
-            return NSAttributedString(string: .SystemThemeSectionHeader)
-        } else if !themeManager.automaticBrightnessIsOn {
-            return NSAttributedString(string: .DisplayThemeManualStatusLabel)
-        } else if themeManager.automaticBrightnessIsOn {
-            return NSAttributedString(string: .DisplayThemeAutomaticStatusLabel)
-        }
-
-        return NSAttributedString(string: "")
-    }
-
     init(settings: SettingsTableViewController,
          settingsDelegate: GeneralSettingsDelegate?,
          themeManager: ThemeManager = AppContainer.shared.resolve()
@@ -41,7 +29,7 @@ class ThemeSetting: Setting {
         let theme = settings.currentTheme()
         super.init(
             title: NSAttributedString(
-                string: .SettingsDisplayThemeTitle,
+                string: .SettingsAppearanceTitle,
                 attributes: [
                     NSAttributedString.Key.foregroundColor: theme.colors.textPrimary
                 ]
