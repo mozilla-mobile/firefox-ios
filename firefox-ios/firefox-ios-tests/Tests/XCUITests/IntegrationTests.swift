c911f31f8634a62126f3e8d4c3f91ff5a1f2cc79
Clare So <1740517+clarmso@users.noreply.github.com>
2025-03-13 04:33:14 -0400

    Bugfix MTE-4389 Remove extra swipe from test-sync-disconnect-connect-fxa (#25292)
    * swipe() exits from the settings screen. We don't need it.
    
    * don't need this step

--- firefox-ios/firefox-ios-tests/Tests/XCUITests/IntegrationTests.swift
+++ firefox-ios/firefox-ios-tests/Tests/XCUITests/IntegrationTests.swift
@@ -291,7 +291,6 @@ class IntegrationTests: BaseTestCase {
 
         navigator.nowAt(SettingsScreen)
         mozWaitForElementToExist(app.staticTexts["GENERAL"])
-        app.swipeDown()
         mozWaitForElementToExist(app.staticTexts["ACCOUNT"])
         mozWaitForElementToExist(app.tables.staticTexts["Sync Now"], timeout: TIMEOUT_LONG)
 
