ff7b4b6e818152eaadeab6b76b65b9073abfa8e6
Aarif Sumra <aarif.sumra@live.com>
2025-03-13 19:14:07 +0100

    Localize FXIOS-9612 #21206 [FirefoxHomepage][shortcut] Localized Pinned Tiles for voice over accessibility  (#25267)
    Co-authored-by: Winnie Teichmann <4530+thatswinnie@users.noreply.github.com>

--- firefox-ios/Shared/Strings.swift
+++ firefox-ios/Shared/Strings.swift
@@ -1172,6 +1172,13 @@ extension String {
                 tableName: nil,
                 value: "Sponsored",
                 comment: "This string will show under a shortcuts tile on the firefox home page, indicating that the tile is a sponsored tile. Space is limited, please keep as short as possible.")
+
+            public static let PinnedAccessibilityLabel = MZLocalizedString(
+                key: "FirefoxHomepage.Shortcuts.Pinned.AccessibilityLabel.v139",
+                tableName: "FirefoxHomepage",
+                value: "Pinned: %@",
+                comment: "Accessibility label for shortcuts tile on the Firefox home page, indicating that the tile is a pinned tile. %@ is the title of the website."
+            )
         }
 
         public struct YourLibrary { }
