a7f6d73d86d9d5ff10444dd5b11cfbf6c0878b19
Sophie Amin <samin@Mac.localdomain>
2025-03-18 16:15:51 -0700

    Add Animation file

---
+++ firefox-ios/Client/Frontend/Browser/Tabs/Views/TabAnimation.swift
@@ -0,0 +1,15 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import Foundation
+
+extension TabDisplayView: UIViewControllerTransitioningDelegate {
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return nil
+    }
+
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return nil
+    }
+}
