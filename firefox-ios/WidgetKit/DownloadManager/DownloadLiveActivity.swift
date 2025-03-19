ba8dbcc421281cc8069a5a00c3b3fb4d0b642e37
Lily <lilyy.phan@mail.utoronto.ca>
2025-03-19 18:58:38 -0400

    Updated to a struct

--- firefox-ios/WidgetKit/DownloadManager/DownloadLiveActivity.swift
+++ firefox-ios/WidgetKit/DownloadManager/DownloadLiveActivity.swift
@@ -50,6 +50,29 @@ struct DownloadLiveActivityAttributes: ActivityAttributes {
     }
 }
 
+struct expandedContentSettings {
+    var iconFrameSize: CGFloat = 50
+    var firefoxIconSize: CGFloat = 44
+    var iconEdgeRounding: CGFloat = 15
+    var iconTopPadding: CGFloat = 10
+    var iconLeftPadding: CGFloat = 0
+    var iconRightPadding: CGFloat = 0
+    var iconBottomPadding: CGFloat = 0
+    var inProgessOpacity: CGFloat = 0.5
+    var progressWidth: CGFloat = 4
+    var stateIconSize: CGFloat = 24
+    var downloadingFontSize: CGFloat = 17
+    var progressFontSize: CGFloat = 15
+    var wordsTopPadding: CGFloat = 0
+    var wordsLeftPadding: CGFloat = 5
+    var wordsRightPadding: CGFloat = 5
+    var wordsBottomPadding: CGFloat = 0
+    var checkmarkIcon = "checkmarkLarge"
+    var mediaStopIcon = "mediaStop"
+    var firefoxIcon = "faviconFox"
+    var widgetColours = Color.white
+}
+
 @available(iOS 16.2, *)
 struct DownloadLiveActivity: Widget {
     var body: some WidgetConfiguration {
@@ -61,7 +84,8 @@ struct DownloadLiveActivity: Widget {
                 .widgetURL(URL(string: URL.mozInternalScheme + "://deep-link?url=/homepanel/downloads"))
         } dynamicIsland: { liveDownload in
             DynamicIsland {
-                expandedContent(liveDownload: liveDownload)
+                let settings = expandedContentSettings()
+                expandedContent(liveDownload: liveDownload, settings: settings)
             } compactLeading: {
                 EmptyView()
             } compactTrailing: {
@@ -76,48 +100,67 @@ struct DownloadLiveActivity: Widget {
 @available(iOS 16.2, *)
 @DynamicIslandExpandedContentBuilder
 private func expandedContent
-(liveDownload: ActivityViewContext<DownloadLiveActivityAttributes>) -> DynamicIslandExpandedContent<some View> {
+(liveDownload: ActivityViewContext<DownloadLiveActivityAttributes>, settings: expandedContentSettings) ->
+DynamicIslandExpandedContent<some View> {
         DynamicIslandExpandedRegion(.leading) {
             ZStack {
-                RoundedRectangle(cornerRadius: 15)
-                    .fill(Color.white)
-                    .frame(width: 50, height: 50)
-                Image("faviconFox")
+                RoundedRectangle(cornerRadius: settings.iconEdgeRounding)
+                    .fill(settings.widgetColours)
+                    .frame(width: settings.iconFrameSize,
+                           height: settings.iconFrameSize)
+                Image(settings.firefoxIcon)
                     .resizable()
                     .scaledToFit()
-                    .frame(width: 44, height: 44)
-            }.padding(EdgeInsets(top: 10, leading: 0, bottom: 0, trailing: 0))
+                    .frame(width: settings.firefoxIconSize,
+                           height: settings.firefoxIconSize)
+            }.padding(EdgeInsets(top: settings.iconTopPadding,
+                                 leading: settings.iconLeftPadding,
+                                 bottom: settings.iconBottomPadding,
+                                 trailing: settings.iconRightPadding))
         }
         DynamicIslandExpandedRegion(.trailing) {
             ZStack {
                 Circle()
-                    .stroke(Color.white.opacity(0.5), lineWidth: 5)
-                    .frame(width: 50, height: 50)
+                    .stroke(settings.widgetColours.opacity(settings.inProgessOpacity),
+                            lineWidth: settings.progressWidth)
+                    .frame(width: settings.iconFrameSize,
+                           height: settings.iconFrameSize)
                 Circle()
-                    .trim(from: 0.0, to: min(liveDownload.state.totalProgress, 1.0))
-                    .stroke(style: StrokeStyle(lineWidth: 5))
+                    .trim(from: 0.0,
+                          to: min(liveDownload.state.totalProgress, 1.0))
+                    .stroke(style: StrokeStyle(lineWidth: settings.progressWidth))
                     .rotationEffect(.degrees(270.0))
                     .animation(.linear, value: 0.5)
                 if liveDownload.state.totalProgress == 1.0 {
-                    Image("checkmarkLarge")
+                    Image(settings.checkmarkIcon)
                     .renderingMode(.template)
-                    .frame(width: 20, height: 20)
-                    .foregroundStyle(Color("widgetLabelColors"))
+                    .frame(width: settings.stateIconSize,
+                           height: settings.stateIconSize)
+                    .foregroundStyle(settings.widgetColours)
                 } else {
-                    Image("mediaStop")
+                    Image(settings.mediaStopIcon)
                         .resizable()
                         .scaledToFit()
-                        .frame(width: 24, height: 24)
+                        .frame(width: settings.stateIconSize,
+                               height: settings.stateIconSize)
                 }
-            }.frame(width: 50, height: 50)
-                .padding(EdgeInsets(top: 10, leading: 0, bottom: 0, trailing: 0))
+            }.frame(width: settings.iconFrameSize,
+                    height: settings.iconFrameSize)
+                .padding(EdgeInsets(top: settings.iconTopPadding,
+                                    leading: settings.iconLeftPadding,
+                                    bottom: settings.iconBottomPadding,
+                                    trailing: settings.iconRightPadding))
         }
         DynamicIslandExpandedRegion(.center) {
             Text("Downloading \(liveDownload.state.downloads[0].fileName)")
-                .font(.system(size: 17,
+                .font(.system(size: settings.downloadingFontSize,
                               weight: .bold))
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(EdgeInsets(top: 0, leading: 5, bottom: 0, trailing: 5))
+                .frame(maxWidth: .infinity,
+                       alignment: .leading)
+                .padding(EdgeInsets(top: settings.wordsTopPadding,
+                                    leading: settings.wordsLeftPadding,
+                                    bottom: settings.wordsBottomPadding,
+                                    trailing: settings.wordsRightPadding))
             let bytesDownloaded = ByteCountFormatter.string(
                 fromByteCount: liveDownload.state.totalBytesDownloaded,
                 countStyle: .file
@@ -127,10 +170,13 @@ private func expandedContent
                 countStyle: .file
                 )
             Text("\(bytesDownloaded) of \(bytesExpected)")
-                                        .font(.system(size: 15))
-                                        .opacity(0.8)
-                                        .foregroundColor(Color.white)
-                                        .frame(maxWidth: .infinity, alignment: .leading)
-                                        .padding(EdgeInsets(top: 0, leading: 5, bottom: 0, trailing: 5))
+                .font(.system(size: settings.progressFontSize))
+                .foregroundColor(settings.widgetColours)
+                .frame(maxWidth: .infinity,
+                       alignment: .leading)
+                .padding(EdgeInsets(top: settings.wordsTopPadding,
+                                    leading: settings.wordsLeftPadding,
+                                    bottom: settings.wordsBottomPadding,
+                                    trailing: settings.wordsRightPadding))
         }
 }
