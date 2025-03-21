2acc2d86319f4e6c5539e209ff5e9c3935ef9795
Clare So <1740517+clarmso@users.noreply.github.com>
2025-03-21 09:07:12 -0400

    Add MTE-3745 Add testCloseOneTabUndo() (#25483)
    * Add testCloseOneTab()
    
    * Remove syntax error
    
    * Move new test to TopTabsTest

--- firefox-ios/firefox-ios-tests/Tests/XCUITests/TopTabsTest.swift
+++ firefox-ios/firefox-ios-tests/Tests/XCUITests/TopTabsTest.swift
@@ -410,6 +410,59 @@ class TopTabsTest: BaseTestCase {
 //        mozWaitForElementToExist(app.otherElements.cells.staticTexts[urlLabelExample])
     }
 
+    // https://mozilla.testrail.io/index.php?/cases/view/2306867
+    func testCloseOneTabUndo() {
+        // Open a few tabs
+        waitForTabsButton()
+        navigator.openURL("http://localhost:\(serverPort)/test-fixture/find-in-page-test.html")
+        waitUntilPageLoad()
+        navigator.createNewTab()
+        navigator.openURL("http://localhost:\(serverPort)/test-fixture/test-example.html")
+        waitUntilPageLoad()
+        navigator.createNewTab()
+        navigator.openURL("localhost:\(serverPort)/test-fixture/test-mozilla-org.html")
+        waitUntilPageLoad()
+        navigator.goto(TabTray)
+
+        // Experiment from #25337: "Undo" button no longer available on iPhone.
+        if iPad() {
+            // Tap "x"
+            app.cells[AccessibilityIdentifiers.TabTray.tabCell+"_1_2"].buttons[StandardImageIdentifiers.Large.cross].tap()
+            mozWaitForElementToNotExist(app.cells[AccessibilityIdentifiers.TabTray.tabCell+"_1_2"])
+            app.buttons["Undo"].waitAndTap()
+            mozWaitForElementToExist(app.cells[AccessibilityIdentifiers.TabTray.tabCell+"_1_2"])
+
+            // Long press tab. Tap "Close Tab" from the context menu
+            app.cells[AccessibilityIdentifiers.TabTray.tabCell+"_1_2"].press(forDuration: 2)
+            mozWaitForElementToExist(app.collectionViews.buttons["Close Tab"])
+            app.collectionViews.buttons["Close Tab"].waitAndTap()
+            mozWaitForElementToNotExist(app.cells[AccessibilityIdentifiers.TabTray.tabCell+"_1_2"])
+            app.buttons["Undo"].waitAndTap()
+            mozWaitForElementToExist(app.cells[AccessibilityIdentifiers.TabTray.tabCell+"_1_2"])
+
+            // Swipe tab
+            app.cells[AccessibilityIdentifiers.TabTray.tabCell+"_1_2"].swipeLeft()
+            mozWaitForElementToNotExist(app.cells[AccessibilityIdentifiers.TabTray.tabCell+"_1_2"])
+            app.buttons["Undo"].waitAndTap()
+            mozWaitForElementToExist(app.cells[AccessibilityIdentifiers.TabTray.tabCell+"_1_2"])
+        } else {
+            // Tap "x"
+            app.cells[AccessibilityIdentifiers.TabTray.tabCell+"_1_2"]
+                .buttons[AccessibilityIdentifiers.TabTray.closeButton].waitAndTap()
+            mozWaitForElementToNotExist(app.cells[AccessibilityIdentifiers.TabTray.tabCell+"_1_2"])
+
+            // Long press tab. Tap "Close Tab" from the context menu
+            app.cells[AccessibilityIdentifiers.TabTray.tabCell+"_1_1"].press(forDuration: 2)
+            mozWaitForElementToExist(app.collectionViews.buttons["Close Tab"])
+            app.collectionViews.buttons["Close Tab"].waitAndTap()
+            mozWaitForElementToNotExist(app.cells[AccessibilityIdentifiers.TabTray.tabCell+"_1_2"])
+
+            // Swipe tab
+            app.cells[AccessibilityIdentifiers.TabTray.tabCell+"_1_0"].swipeLeft()
+            mozWaitForElementToNotExist(app.cells[AccessibilityIdentifiers.TabTray.tabCell+"_1_0"])
+        }
+    }
+
     private func validateToastWhenClosingMultipleTabs() {
         // Have multiple tabs opened in the tab tray
         navigator.openURL(urlExample)
