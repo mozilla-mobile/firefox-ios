e519de0b971e9db20e7e48aedea6f23e64ed5ff6
Sophie Amin <samin@Mac.localdomain>
2025-03-21 00:32:00 -0700

    WIP

--- firefox-ios/Client/Frontend/Browser/Tabs/Views/TabAnimation.swift
+++ firefox-ios/Client/Frontend/Browser/Tabs/Views/TabAnimation.swift
@@ -3,6 +3,8 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
 import Foundation
+import UIKit
+import Shared
 
 extension TabTrayViewController: UIViewControllerTransitioningDelegate {
     func animationController(
@@ -10,7 +12,7 @@ extension TabTrayViewController: UIViewControllerTransitioningDelegate {
       presenting: UIViewController,
       source: UIViewController
     ) -> UIViewControllerAnimatedTransitioning? {
-      return nil
+        return BasicAnimationController(delegate: self, direction: .presenting)
     }
 
     func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
@@ -37,7 +39,9 @@ extension TabTrayViewController: BasicAnimationControllerDelegate {
       return
     }
 
-    guard let selectedTab = tabManager.selectedTab else {
+      guard let selectedTab = bvc.tabManager.selectedTab,
+            let webView = selectedTab.webView
+      else {
       logger.log("Attempted to present the tab tray without having a selected tab",
                  level: .warning,
                  category: .tabs)
@@ -52,11 +56,11 @@ extension TabTrayViewController: BasicAnimationControllerDelegate {
     tabSnapshot.layer.cornerCurve = .continuous
     tabSnapshot.clipsToBounds = true
     tabSnapshot.contentMode = .scaleAspectFill
-    tabSnapshot.frame = bvc.webViewContainer.frame
+    tabSnapshot.frame = webView.frame
 
     // Allow the UI to render to make the snapshotting code more performant
     // swiftlint:disable closure_body_length
-    DispatchQueue.main.async { [self] in
+    Task { [self] in
       // BVC snapshot animates to the cell
       let bvcSnapshot = UIImageView(image: bvc.view.snapshot)
       bvcSnapshot.layer.cornerCurve = .continuous
@@ -76,12 +80,17 @@ extension TabTrayViewController: BasicAnimationControllerDelegate {
       destinationController.view.setNeedsLayout()
       destinationController.view.layoutIfNeeded()
 
-      let cv = tabTrayView.collectionView
+     guard let panel = currentPanel as? ThemedNavigationController,
+           let panelViewController = panel.viewControllers.first as? TabDisplayPanelViewController,
+           let cv = panelViewController.tabDisplayView.collectionView
+        else { return }
+
+     // let cv = tabTrayView.collectionView
       cv.reloadData()
       var tabCell: TabCell?
       var cellFrame: CGRect?
       var cellTitleSnapshot: UIView?
-      if let indexPath = dataSource.indexPath(for: selectedTab) {
+        if let indexPath = cv.indexPath(for: selectedTab) {
         // This is needed for some reason otherwise the collection views content offset is
         // incorrect.
         cv.scrollToItem(at: indexPath, at: .centeredVertically, animated: false)
@@ -152,138 +161,148 @@ extension TabTrayViewController: BasicAnimationControllerDelegate {
     // swiftlint:enable closure_body_length
   }
 
-  func animateDismissal(context: UIViewControllerContextTransitioning) {
-    guard let toViewController = context.viewController(forKey: .to),
-      let toView = context.view(forKey: .to)
-    else {
-      logger.log(
-        """
-            Attempted to dismiss the tab tray without a view to dismiss from.
-
-            Likely the `modalPresentationStyle` was changed away from `fullScreen` and should be changed
-            back if using this custom animation.
-        """,
-        level: .warning,
-        category: .tabs)
-      context.completeTransition(true)
-      return
-    }
-
-    guard let containerController = toViewController as? UINavigationController,
-      let bvc = containerController.topViewController as? BrowserViewController
-    else {
-        logger.log(
-        """
-            Attempted to dismiss the tab tray from something that is not a BrowserViewController which is
-            currently unsupported.
-        """,
-        level: .warning,
-        category: .tabs)
-      context.completeTransition(true)
-      return
-    }
-
-    // Tab screenshot will animate from the cell to the bvc web container
-    let tabSnapshot = UIImageView(image: tabManager.selectedTab?.screenshot ?? .init())
-    tabSnapshot.layer.cornerCurve = .continuous
-    tabSnapshot.clipsToBounds = true
-    tabSnapshot.contentMode = .scaleAspectFill
-    tabSnapshot.layer.cornerRadius = TabCell.UX.cornerRadius
-    tabSnapshot.isHidden = true
-
-    let finalFrame = context.finalFrame(for: toViewController)
-
-    toView.frame = finalFrame
-
-    // Allow the UI to render to make the snapshotting code more performant
-    // swiftlint:disable closure_body_length
-    DispatchQueue.main.async { [self] in
-      // Just a small background view for animation sake between the tab tray and the bvc
-      let backgroundView = UIView()
-      backgroundView.backgroundColor = .init(white: 0.0, alpha: 0.3)
-      backgroundView.alpha = 0
-      backgroundView.frame = finalFrame
-
-      context.containerView.addSubview(toView)
-      context.containerView.addSubview(backgroundView)
-
-      toView.setNeedsLayout()
-      toView.layoutIfNeeded()
-
-      // BVC snapshot animates from the cell to its final resting spot
-      let toVCSnapshot: UIView =
-        toView.snapshotView(afterScreenUpdates: true) ?? UIImageView(image: toView.snapshot)
-      toVCSnapshot.layer.cornerCurve = .continuous
-      toVCSnapshot.layer.cornerRadius = TabCell.UX.cornerRadius
-      toVCSnapshot.clipsToBounds = true
-
-      context.containerView.addSubview(toVCSnapshot)
-      context.containerView.addSubview(tabSnapshot)
-
-      // Hide the destination as we're animating a snapshot into place
-      toView.isHidden = true
-
-      let cv = tabTrayView.collectionView
-      cv.reloadData()
-
-      var tabCell: TabCell?
-      var cellTitleSnapshot: UIView?
-      if let tab = tabManager.selectedTab, let indexPath = dataSource.indexPath(for: tab) {
-        // This is needed for some reason otherwise the collection views content offset is
-        // incorrect.
-        cv.scrollToItem(at: indexPath, at: .centeredVertically, animated: false)
-        cv.layoutIfNeeded()
-
-        if let cell = cv.cellForItem(at: indexPath) as? TabCell {
-          tabCell = cell
-
-          tabSnapshot.frame = cv.convert(cell.frame, to: view)
-          toVCSnapshot.frame = tabSnapshot.frame
-
-          let titleSnapshot = UIImageView(image: cell.titleBackgroundView.snapshot)
-          titleSnapshot.contentMode = .scaleToFill
-          titleSnapshot.clipsToBounds = true
-          tabSnapshot.addSubview(titleSnapshot)
-          titleSnapshot.snp.makeConstraints {
-            $0.top.leading.trailing.equalToSuperview()
-          }
-          cellTitleSnapshot = titleSnapshot
-          tabSnapshot.setNeedsLayout()
-          tabSnapshot.layoutIfNeeded()
-
-          cell.isHidden = true
-        }
-      }
-      tabSnapshot.isHidden = false
-      let animator = UIViewPropertyAnimator(duration: 0.3, dampingRatio: 1.0) {
-        // For some flourish
-        cv.transform = .init(scaleX: 1.2, y: 1.2)
-        cv.alpha = 0.5
-
-        tabSnapshot.frame = bvc.webViewContainer.frame
-        tabSnapshot.layer.cornerRadius = 0
-        toVCSnapshot.frame = finalFrame
-        toVCSnapshot.layer.cornerRadius = 0
-        backgroundView.alpha = 1
-      }
-      if let titleSnapshot = cellTitleSnapshot {
-        // Need a quicker animation for this one
-        UIViewPropertyAnimator(duration: 0.1, curve: .linear) {
-          titleSnapshot.alpha = 0
-        }
-        .startAnimation()
-      }
-      animator.addCompletion { _ in
-        tabCell?.isHidden = false
-        toView.isHidden = false
-        self.view.removeFromSuperview()
-        tabSnapshot.removeFromSuperview()
-        toVCSnapshot.removeFromSuperview()
-        backgroundView.removeFromSuperview()
-        context.completeTransition(true)
-      }
-      animator.startAnimation()
-    }
-    // swiftlint:enable closure_body_length
-  }
+//  func animateDismissal(context: UIViewControllerContextTransitioning) {
+//    guard let toViewController = context.viewController(forKey: .to),
+//      let toView = context.view(forKey: .to)
+//    else {
+//      logger.log(
+//        """
+//            Attempted to dismiss the tab tray without a view to dismiss from.
+//
+//            Likely the `modalPresentationStyle` was changed away from `fullScreen` and should be changed
+//            back if using this custom animation.
+//        """,
+//        level: .warning,
+//        category: .tabs)
+//      context.completeTransition(true)
+//      return
+//    }
+//
+//      guard let containerController = toViewController as? UINavigationController,
+//            let bvc = containerController.topViewController as? BrowserViewController
+//      else {
+//          logger.log(
+//        """
+//            Attempted to dismiss the tab tray from something that is not a BrowserViewController which is
+//            currently unsupported.
+//        """,
+//        level: .warning,
+//        category: .tabs)
+//          context.completeTransition(true)
+//          return
+//      }
+//
+//      guard let selectedTab = bvc.tabManager.selectedTab,
+//            let webView = selectedTab.webView
+//      else {
+//          logger.log("Attempted to dismiss the tab tray without having a selected tab",
+//                     level: .warning,
+//                     category: .tabs)
+//          context.completeTransition(true)
+//          return
+//      }
+//
+//    // Tab screenshot will animate from the cell to the bvc web container
+//    let tabSnapshot = UIImageView(image: selectedTab.screenshot ?? .init())
+//    tabSnapshot.layer.cornerCurve = .continuous
+//    tabSnapshot.clipsToBounds = true
+//    tabSnapshot.contentMode = .scaleAspectFill
+//    tabSnapshot.layer.cornerRadius = TabCell.UX.cornerRadius
+//    tabSnapshot.isHidden = true
+//
+//    let finalFrame = context.finalFrame(for: toViewController)
+//
+//    toView.frame = finalFrame
+//
+//    // Allow the UI to render to make the snapshotting code more performant
+//    // swiftlint:disable closure_body_length
+//    DispatchQueue.main.async { [self] in
+//      // Just a small background view for animation sake between the tab tray and the bvc
+//      let backgroundView = UIView()
+//      backgroundView.backgroundColor = .init(white: 0.0, alpha: 0.3)
+//      backgroundView.alpha = 0
+//      backgroundView.frame = finalFrame
+//
+//      context.containerView.addSubview(toView)
+//      context.containerView.addSubview(backgroundView)
+//
+//      toView.setNeedsLayout()
+//      toView.layoutIfNeeded()
+//
+//      // BVC snapshot animates from the cell to its final resting spot
+//      let toVCSnapshot: UIView =
+//        toView.snapshotView(afterScreenUpdates: true) ?? UIImageView(image: toView.snapshot)
+//      toVCSnapshot.layer.cornerCurve = .continuous
+//      toVCSnapshot.layer.cornerRadius = TabCell.UX.cornerRadius
+//      toVCSnapshot.clipsToBounds = true
+//
+//      context.containerView.addSubview(toVCSnapshot)
+//      context.containerView.addSubview(tabSnapshot)
+//
+//      // Hide the destination as we're animating a snapshot into place
+//      toView.isHidden = true
+//
+//      let cv = tabTrayView.collectionView
+//      cv.reloadData()
+//
+//      var tabCell: TabCell?
+//      var cellTitleSnapshot: UIView?
+//      if let tab = tabManager.selectedTab, let indexPath = dataSource.indexPath(for: tab) {
+//        // This is needed for some reason otherwise the collection views content offset is
+//        // incorrect.
+//        cv.scrollToItem(at: indexPath, at: .centeredVertically, animated: false)
+//        cv.layoutIfNeeded()
+//
+//        if let cell = cv.cellForItem(at: indexPath) as? TabCell {
+//          tabCell = cell
+//
+//          tabSnapshot.frame = cv.convert(cell.frame, to: view)
+//          toVCSnapshot.frame = tabSnapshot.frame
+//
+//          let titleSnapshot = UIImageView(image: cell.titleBackgroundView.snapshot)
+//          titleSnapshot.contentMode = .scaleToFill
+//          titleSnapshot.clipsToBounds = true
+//          tabSnapshot.addSubview(titleSnapshot)
+//          titleSnapshot.snp.makeConstraints {
+//            $0.top.leading.trailing.equalToSuperview()
+//          }
+//          cellTitleSnapshot = titleSnapshot
+//          tabSnapshot.setNeedsLayout()
+//          tabSnapshot.layoutIfNeeded()
+//
+//          cell.isHidden = true
+//        }
+//      }
+//      tabSnapshot.isHidden = false
+//      let animator = UIViewPropertyAnimator(duration: 0.3, dampingRatio: 1.0) {
+//        // For some flourish
+//        cv.transform = .init(scaleX: 1.2, y: 1.2)
+//        cv.alpha = 0.5
+//
+//        tabSnapshot.frame = bvc.webViewContainer.frame
+//        tabSnapshot.layer.cornerRadius = 0
+//        toVCSnapshot.frame = finalFrame
+//        toVCSnapshot.layer.cornerRadius = 0
+//        backgroundView.alpha = 1
+//      }
+//      if let titleSnapshot = cellTitleSnapshot {
+//        // Need a quicker animation for this one
+//        UIViewPropertyAnimator(duration: 0.1, curve: .linear) {
+//          titleSnapshot.alpha = 0
+//        }
+//        .startAnimation()
+//      }
+//      animator.addCompletion { _ in
+//        tabCell?.isHidden = false
+//        toView.isHidden = false
+//        self.view.removeFromSuperview()
+//        tabSnapshot.removeFromSuperview()
+//        toVCSnapshot.removeFromSuperview()
+//        backgroundView.removeFromSuperview()
+//        context.completeTransition(true)
+//      }
+//      animator.startAnimation()
+//    }
+//    // swiftlint:enable closure_body_length
+//  }
 }
