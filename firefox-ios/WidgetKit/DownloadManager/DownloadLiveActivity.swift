f3e056dabe522b1e21345b0f8658b77d910b3397
Enaya <enaya@outlook.com>
2025-03-22 18:26:49 -0400

    pr changes

--- firefox-ios/WidgetKit/DownloadManager/DownloadLiveActivity.swift
+++ firefox-ios/WidgetKit/DownloadManager/DownloadLiveActivity.swift
@@ -54,58 +54,75 @@ struct DownloadLiveActivityAttributes: ActivityAttributes {
 
 @available(iOS 16.2, *)
 struct DownloadLiveActivity: Widget {
+    private struct UX {
+        static let hSpacing: CGFloat = 16
+        static let vSpacing: CGFloat = 4
+        static let iconSize: CGFloat = 40
+        static let titleFont: CGFloat = 17
+        static let subtitleFont: CGFloat = 15
+        static let subtitleOpacity: CGFloat = 0.8
+        static let circleRadius: CGFloat = 44
+        static let circleWidth: CGFloat = 4
+        static let circleOpacity: CGFloat = 0.3
+        static let circleRotation: CGFloat = 270
+        static let circleAnimation: CGFloat = 0.5
+        static let progressIconSize: CGFloat = 20
+        static let appIcon = "faviconFox"
+        static let stopIcon = "mediaStop"
+        static let checkmarkIcon = StandardImageIdentifiers.Large.checkmark
+        static let gradient1 =  Color("searchButtonColorTwo")
+        static let gradient2 = Color("searchButtonColorOne")
+        static let labelColor = Color("widgetLabelColors")
+    }
     var body: some WidgetConfiguration {
-        ActivityConfiguration(for: DownloadLiveActivityAttributes.self) { LiveDownload in
-            // Using Rectangle instead of EmptyView because the hitbox
-            // of the empty view is too small (likely non existent),
-            // meaning we'd never be redirected to the downloads panel
+        ActivityConfiguration(for: DownloadLiveActivityAttributes.self) { liveDownload in
+            let bytesCompleted = liveDownload.state.totalBytesDownloaded
+            let bytesExpected = liveDownload.state.totalBytesExpected
+            let mbCompleted = ByteCountFormatter.string(fromByteCount: bytesCompleted, countStyle: .file)
+            let mbExpected = ByteCountFormatter.string(fromByteCount: bytesExpected, countStyle: .file)
+            let subtitle = String(format: .LiveActivity.Downloads.FileProgressText, mbCompleted, mbExpected)
+            let totalCompletion = liveDownload.state.completedDownloads == liveDownload.state.downloads.count
             ZStack {
                 Rectangle()
                     .widgetURL(URL(string: URL.mozInternalScheme + "://deep-link?url=/homepanel/downloads"))
                     .foregroundStyle(LinearGradient(
-                        gradient: Gradient(colors: [Color("searchButtonColorTwo"), Color("searchButtonColorOne")]),
+                        gradient: Gradient(colors: [UX.gradient1, UX.gradient2]),
                         startPoint: .topLeading,
-                        endPoint: .bottomTrailing
-                    ))
-                HStack(spacing: 16) {
+                        endPoint: .bottomTrailing))
+                HStack(spacing: UX.hSpacing) {
                     ZStack {
-                        Image("faviconFox")
+                        Image(UX.appIcon)
                             .resizable()
                             .scaledToFit()
-                            .frame(width: 40, height: 40)
+                            .frame(width: UX.iconSize, height: UX.iconSize)
                     }
-                    VStack(alignment: .leading, spacing: 4) {
-                        Text("Downloading \(LiveDownload.state.downloads[0].fileName)")
-                            .font(.system(size: 17, weight: .bold))
-                            .foregroundColor(Color("widgetLabelColors"))
-                        Text("\(String(format: "%.1f", Double(LiveDownload.state.totalBytesDownloaded) / 1000000)) MB of \(String(format: "%.1f", Double(LiveDownload.state.totalBytesExpected) / 1000000)) MB")
-                            .font(.system(size: 15))
-                            .opacity(0.8)
-                            .foregroundColor(Color("widgetLabelColors"))
+                    VStack(alignment: .leading, spacing: UX.vSpacing) {
+                        Text(liveDownload.state.downloads.count == 1 ?
+                             String(format: .LiveActivity.Downloads.FileNameText, liveDownload.state.downloads[0].fileName) :
+                                String(format: .LiveActivity.Downloads.FileNameText, liveDownload.state.downloads.count))
+                            .font(.system(size: UX.titleFont, weight: .bold))
+                            .foregroundColor(UX.labelColor)
+                        Text(subtitle)
+                            .font(.system(size: UX.subtitleFont))
+                            .opacity(UX.subtitleOpacity)
+                            .foregroundColor(UX.labelColor)
                     }
                     Spacer()
                     ZStack {
-                        Circle()
-                            .stroke(lineWidth: 4)
-                            .foregroundColor(Color("widgetLabelColors"))
-                            .opacity(0.3)
-                        Circle()
-                            .trim(from: 0.0, to: min(LiveDownload.state.totalProgress, 1.0))
-                            .stroke(style: StrokeStyle(lineWidth: 4))
-                            .rotationEffect(.degrees(270.0))
-                            .animation(.linear, value: 0.5)
-                            .foregroundColor(Color("widgetLabelColors"))
-                        if LiveDownload.state.totalProgress == 1.0 {
-                            Image(StandardImageIdentifiers.Large.checkmark)
-                                .renderingMode(.template)
-                                .frame(width: 20, height: 20)
-                                .foregroundStyle(Color("widgetLabelColors"))
-                        } else {
-                            Image("mediaStop")
-                                .frame(width: 20, height: 20)
-                        }
+                        Circle().stroke(lineWidth: UX.circleWidth)
+                            .foregroundColor(UX.labelColor)
+                            .opacity(UX.circleOpacity)
+                        Circle().trim(from: 0.0, to: min(liveDownload.state.totalProgress, 1.0))
+                            .stroke(style: StrokeStyle(lineWidth: UX.circleWidth))
+                            .rotationEffect(.degrees(UX.circleRotation))
+                            .animation(.linear, value: UX.circleAnimation)
+                            .foregroundColor(UX.labelColor)
+                        Image(totalCompletion ? UX.checkmarkIcon : UX.stopIcon)
+                            .renderingMode(.template)
+                            .frame(width: UX.progressIconSize, height: UX.progressIconSize)
+                            .foregroundStyle(UX.labelColor)
                     }
-                    .frame(width: 44, height: 44)
+                    .frame(width: UX.circleRadius, height: UX.circleRadius)
                 }
                 .padding()
             }
