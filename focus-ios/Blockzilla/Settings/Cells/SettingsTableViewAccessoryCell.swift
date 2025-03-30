323068b765297d6c74bce22b41c4400867a53ce2
k.gusev <myaumuraa@gmail.com>
2025-03-30 19:54:57 +0300

    Fix long label

--- focus-ios/Blockzilla/Settings/Cells/SettingsTableViewAccessoryCell.swift
+++ focus-ios/Blockzilla/Settings/Cells/SettingsTableViewAccessoryCell.swift
@@ -5,20 +5,6 @@
 import UIKit
 
 class SettingsTableViewAccessoryCell: SettingsTableViewCell {
-    var labelText: String? {
-        get { return textLabel?.text }
-        set {
-            textLabel?.text = newValue
-        }
-    }
-
-    var accessoryLabelText: String? {
-        get { return detailTextLabel?.text }
-        set {
-            detailTextLabel?.text = newValue
-        }
-    }
-
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         accessoryView = UIImageView(image: UIImage(systemName: "chevron.right"))
@@ -27,5 +13,31 @@ class SettingsTableViewAccessoryCell: SettingsTableViewCell {
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setConfiguration(text: String, secondaryText: String? = nil) {
+        var configuration = defaultContentConfiguration()
+        configuration.text = text
+        configuration.textProperties.color = .primaryText
+        configuration.textProperties.allowsDefaultTighteningForTruncation = true
+        configuration.textProperties.adjustsFontForContentSizeCategory = true
+        
+        configuration.textProperties.alignment = .natural
+        configuration.textProperties.numberOfLines = 0
+        configuration.textProperties.lineBreakMode = .byWordWrapping
+        configuration.axesPreservingSuperviewLayoutMargins = .horizontal
+        configuration.prefersSideBySideTextAndSecondaryText = true
+        
+        if let secondaryText {
+            configuration.secondaryText = secondaryText
+            configuration.secondaryTextProperties.alignment = .natural
+            configuration.secondaryTextProperties.adjustsFontForContentSizeCategory = true
+            configuration.secondaryTextProperties.adjustsFontSizeToFitWidth = true
+        }
+        
+        let margins = configuration.directionalLayoutMargins
+        configuration.directionalLayoutMargins = NSDirectionalEdgeInsets(top: margins.top, leading: UIConstants.layout.settingsCellLeftInset, bottom: margins.bottom, trailing: margins.trailing)
+        configuration.prefersSideBySideTextAndSecondaryText = true
+        contentConfiguration = configuration
     }
 }
