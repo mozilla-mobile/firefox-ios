96ca933f756e195a55627f7695b7c223c0b4aad8
Sophie <5545720+dataports@users.noreply.github.com>
2025-04-02 16:42:26 -0700

    Bugfix FIXIOS-11813 [Tab Tray Experiments] Fix tab selector starting on tabs every time (#25761)

--- firefox-ios/Client/Frontend/Browser/Tabs/Views/CustomSelectorView/TabTraySelectorView.swift
+++ firefox-ios/Client/Frontend/Browser/Tabs/Views/CustomSelectorView/TabTraySelectorView.swift
@@ -6,7 +6,7 @@ import UIKit
 import Common
 
 protocol TabTraySelectorDelegate: AnyObject {
-    func didSelectSection(section: Int)
+    func didSelectSection(panelType: TabTrayPanelType)
 }
 
 // MARK: - UX Constants
@@ -31,7 +31,7 @@ final class TabTraySelectorView: UIView,
     weak var delegate: TabTraySelectorDelegate?
 
     private let windowUUID: WindowUUID
-    private var selectedIndex = 1
+    private var selectedIndex: Int
 
     var items: [String] = [] {
         didSet {
@@ -61,9 +61,11 @@ final class TabTraySelectorView: UIView,
     }()
 
     // MARK: - Init
-    init(windowUUID: WindowUUID,
+    init(selectedIndex: Int,
+         windowUUID: WindowUUID,
          themeManager: ThemeManager = AppContainer.shared.resolve(),
          notificationCenter: NotificationCenter = NotificationCenter.default) {
+        self.selectedIndex = selectedIndex
         self.windowUUID = windowUUID
         self.themeManager = themeManager
         self.notificationCenter = notificationCenter
@@ -152,10 +154,19 @@ final class TabTraySelectorView: UIView,
         selectedIndex = newIndex
         collectionView.reloadData()
         scrollToItem(at: selectedIndex, animated: true)
-        delegate?.didSelectSection(section: newIndex)
+
+        var panelType: TabTrayPanelType = .tabs
+        if selectedIndex == 0 {
+            panelType = .privateTabs
+        } else if selectedIndex == 1 {
+            panelType = .tabs
+        } else if selectedIndex == 2 {
+            panelType = .syncedTabs
+        }
+        delegate?.didSelectSection(panelType: panelType)
     }
 
-    // MARK: - Theamable
+    // MARK: - Themeable
 
     func applyTheme() {
         let theme = themeManager.getCurrentTheme(for: windowUUID)
