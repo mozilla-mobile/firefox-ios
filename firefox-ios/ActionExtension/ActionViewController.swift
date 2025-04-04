14631b6ca470e6e9f6e1686e2df445f797245ee1
Razvan Litianu <litianu.razvan@gmail.com>
2025-04-04 14:50:06 +0300

    Refactor

--- firefox-ios/ActionExtension/ActionViewController.swift
+++ firefox-ios/ActionExtension/ActionViewController.swift
@@ -3,47 +3,41 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
 import UIKit
-import MobileCoreServices
-import UniformTypeIdentifiers
-import Shared
 import Storage
-import Account
-import Common
 
-class ActionViewController: UIViewController {
-
-    var embedController: EmbeddedNavController?
-    var shareViewController: ShareViewController?
+class ActionViewController: UIViewController, ShareControllerDelegate {
+    private var embedController: EmbeddedNavController?
+    private var shareViewController: ShareViewController?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         UIDevice.current.beginGeneratingDeviceOrientationNotifications()
         view.backgroundColor = .clear
-        self.view.alpha = 0
+        view.alpha = 0
 
-        // Extract the shared item using the same helper as the share extension.
-        self.getShareItem { shareItem in
+        getShareItem { [weak self] shareItem in
+            guard let self = self else { return }
             DispatchQueue.main.async {
                 guard let shareItem = shareItem else {
                     let alert = UIAlertController(
-                        title: "Error",
-                        message: "There was an error processing the shared item.",
+                        title: .SendToErrorTitle,
+                        message: .SendToErrorMessage,
                         preferredStyle: .alert
                     )
-                    alert.addAction(UIAlertAction(title: "OK", style: .default) { _ in
-                        self.finish(afterDelay: 0)
+                    alert.addAction(UIAlertAction(
+                        title: .SendToErrorOKButton,
+                        style: .default
+                    ) { _ in self.finish(afterDelay: 0)
                     })
                     self.present(alert, animated: true, completion: nil)
                     return
                 }
 
-                // Instantiate the ShareViewController (which handles all Firefox actions)
                 let shareController = ShareViewController()
                 shareController.delegate = self
                 shareController.shareItem = shareItem
                 self.shareViewController = shareController
 
-                // Embed the ShareViewController using the EmbeddedNavController from the share extension
                 self.embedController = EmbeddedNavController(
                     isSearchMode: !shareItem.isUrlType(),
                     parent: self,
@@ -55,41 +49,38 @@ class ActionViewController: UIViewController {
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        // Fade in the UI to match the share extension’s animation
         UIView.animate(withDuration: 0.2) {
             self.view.alpha = 1
         }
     }
 
-    // Extract shared item similar to the share extension's implementation.
-    func getShareItem(completion: @escaping (ExtensionUtils.ExtractedShareItem?) -> Void) {
-        ExtensionUtils.extractSharedItem(fromExtensionContext: extensionContext) { item, error in
+    /// Extracts the shared item using the extension’s helper.
+    private func getShareItem(completion: @escaping (ExtensionUtils.ExtractedShareItem?) -> Void) {
+        ExtensionUtils.extractSharedItem(fromExtensionContext: extensionContext) { [weak self] item, error in
             if let item = item, error == nil {
                 completion(item)
             } else {
                 completion(nil)
-                self.extensionContext?.cancelRequest(withError: CocoaError(.keyValueValidation))
+                self?.extensionContext?.cancelRequest(withError: CocoaError(.keyValueValidation))
             }
         }
     }
 
-    // Called by the ShareViewController when an action is completed.
-    func finish(afterDelay: TimeInterval) {
-        UIView.animate(withDuration: 0.2, delay: afterDelay, options: [], animations: {
+    /// Fades out the UI and completes the extension request.
+    func finish(afterDelay delay: TimeInterval) {
+        UIView.animate(withDuration: 0.2, delay: delay, options: [], animations: {
             self.view.alpha = 0
-        }, completion: { _ in
+        }) { _ in
             self.extensionContext?.completeRequest(returningItems: [], completionHandler: nil)
-        })
+        }
     }
-}
 
-extension ActionViewController: ShareControllerDelegate {
     func getValidExtensionContext() -> NSExtensionContext? {
-        return self.extensionContext
+        extensionContext
     }
     
+    /// Hides the popup UI when an alert is presented.
     func hidePopupWhenShowingAlert() {
-        // Hide the popup UI, similar to the share extension behavior.
-        self.embedController?.navigationController.view.alpha = 0
+        embedController?.navigationController.view.alpha = 0
     }
 }
