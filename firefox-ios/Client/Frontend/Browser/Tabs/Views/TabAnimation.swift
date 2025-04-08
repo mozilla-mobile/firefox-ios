92d6094b9b30aa3c86970889950da189ca75e1bc
Sophie Amin <samin@Mac.localdomain>
2025-04-08 12:40:05 -0700

    Remove print statements

--- firefox-ios/Client/Frontend/Browser/Tabs/Views/TabAnimation.swift
+++ firefox-ios/Client/Frontend/Browser/Tabs/Views/TabAnimation.swift
@@ -159,9 +159,7 @@ extension TabTrayViewController: BasicAnimationControllerDelegate {
         context.containerView.addSubview(backgroundView)
         context.containerView.addSubview(bvcSnapshot)
         context.containerView.addSubview(tabSnapshot)
-        print(finalFrame)
-        print(destinationController.view.frame)
-        print("hi")
+
         destinationController.view.frame = finalFrame
         destinationController.view.setNeedsLayout()
         destinationController.view.layoutIfNeeded()
@@ -285,9 +283,6 @@ extension TabTrayViewController: BasicAnimationControllerDelegate {
             if let cell = cv.cellForItem(at: indexPath) as? ExperimentTabCell {
                 tabCell = cell
                 tabSnapshot.frame = cv.convert(cell.frame, to: view)
-                print(tabSnapshot.frame)
-                print(toVCSnapshot.frame)
-                print("hi")
                 toVCSnapshot.frame = tabSnapshot.frame
 
                 tabSnapshot.setNeedsLayout()
