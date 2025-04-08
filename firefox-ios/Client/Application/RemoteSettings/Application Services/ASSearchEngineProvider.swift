7b2ba54a9474242d818c155d40de2c14c1fb65c3
Matt Reagan <mreagan@mozilla.com>
2025-04-07 19:23:38 -0700

    [FXIOS-11745] Offload AS engine fetch, icon fetch, and ordering to background queue. For now using GCD to follow existing completion block patterns though will be revisiting to explore async await soon

--- firefox-ios/Client/Application/RemoteSettings/Application Services/ASSearchEngineProvider.swift
+++ firefox-ios/Client/Application/RemoteSettings/Application Services/ASSearchEngineProvider.swift
@@ -38,6 +38,22 @@ final class ASSearchEngineProvider: SearchEngineProvider {
         // Note: this currently duplicates the logic from DefaultSearchEngineProvider.
         // Eventually that class will be removed once we switch fully to consolidated search.
 
+        DispatchQueue.global().async { [weak self] in
+            self?.fetchUnorderedEnginesAndApplyOrdering(
+                customEngines: customEngines,
+                engineOrderingPrefs: engineOrderingPrefs,
+                prefsMigrator: prefsMigrator,
+                completion: completion
+            )
+        }
+    }
+
+    // MARK: - Private Utilities
+
+    private func fetchUnorderedEnginesAndApplyOrdering(customEngines: [OpenSearchEngine],
+                                                       engineOrderingPrefs: SearchEnginePrefs,
+                                                       prefsMigrator: SearchEnginePreferencesMigrator,
+                                                       completion: @escaping SearchEngineCompletion) {
         let locale = Locale(identifier: Locale.preferredLanguages.first ?? Locale.current.identifier)
         let prefsVersion = preferencesVersion
 
@@ -82,8 +98,6 @@ final class ASSearchEngineProvider: SearchEngineProvider {
             ensureMainThread { completion(finalEngineOrderingPrefs, orderedEngines) }
         })
     }
-
-    // MARK: - Private Utilities
 
     private func getUnorderedBundledEnginesFor(locale: Locale,
                                                possibleLanguageIdentifier: [String],
