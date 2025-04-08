a4170c3842444958c37f500fce6757685a47f356
Issam Mani <issamouu69@gmail.com>
2025-04-08 13:47:14 +0200

    fix: update reader mode csp to allow dark reader script and styles to run

--- firefox-ios/Client/Frontend/Reader/ReaderModeHandlers.swift
+++ firefox-ios/Client/Frontend/Reader/ReaderModeHandlers.swift
@@ -116,8 +116,14 @@ struct ReaderModeHandlers: ReaderModeHandlersProtocol {
               let response = GCDWebServerDataResponse(html: html) else { return nil }
         // Apply a Content Security Policy that disallows everything except images from
         // anywhere and fonts and css from our internal server
-        response.setValue("default-src 'none'; img-src *; style-src http://localhost:* '\(ReaderModeStyleHash)'; font-src http://localhost:*",
-                          forAdditionalHeader: "Content-Security-Policy")
+        let csp = """
+            default-src 'none';
+            img-src *;
+            style-src 'unsafe-inline' http://localhost:*;
+            font-src http://localhost:*;
+            script-src 'unsafe-inline' http://localhost:*;
+        """
+        response.setValue(scp, forAdditionalHeader: "Content-Security-Policy")
         return response
     }
 
