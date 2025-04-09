e6de9745849c3c5dd565b453f102fe71d475735d
Winnie Teichmann <4530+thatswinnie@users.noreply.github.com>
2025-04-09 14:06:11 +0200

    Use button configuration

--- firefox-ios/Client/Frontend/Browser/TopTabCell.swift
+++ firefox-ios/Client/Frontend/Browser/TopTabCell.swift
@@ -13,6 +13,7 @@ class TopTabCell: UICollectionViewCell, ThemeApplicable, ReusableCell, FeatureFl
         static let faviconSize: CGFloat = 20
         static let faviconCornerRadius: CGFloat = 2
         static let tabTitlePadding: CGFloat = 10
+        static let tabTitlePaddingVersion1: CGFloat = 14
         static let tabNudge: CGFloat = 1 // Nudge the favicon and close button by 1px
 
         // MARK: - Tab Appearance Constants
@@ -57,6 +58,7 @@ class TopTabCell: UICollectionViewCell, ThemeApplicable, ReusableCell, FeatureFl
     let favicon: FaviconImageView = .build { _ in }
 
     let closeButton: UIButton = .build { button in
+        button.configuration = .plain()
         button.configuration?.contentInsets = NSDirectionalEdgeInsets(top: UX.verticalPadding,
                                                                       leading: UX.tabTitlePadding,
                                                                       bottom: UX.verticalPadding,
@@ -89,9 +91,13 @@ class TopTabCell: UICollectionViewCell, ThemeApplicable, ReusableCell, FeatureFl
 
         let toolbarState = store.state.screenState(ToolbarState.self, for: .toolbar, window: tab.windowUUID)
         if let toolbarState, toolbarState.toolbarLayout == .version1 {
-            closeButton.setImage(UIImage.templateImageNamed(StandardImageIdentifiers.Medium.cross), for: [])
+            closeButton.configuration?.image = UIImage.templateImageNamed(StandardImageIdentifiers.Medium.cross)
+            closeButton.configuration?.contentInsets = NSDirectionalEdgeInsets(top: UX.verticalPadding,
+                                                                               leading: UX.tabTitlePaddingVersion1,
+                                                                               bottom: UX.verticalPadding,
+                                                                               trailing: UX.tabTitlePaddingVersion1)
         } else {
-            closeButton.setImage(UIImage.templateImageNamed(StandardImageIdentifiers.Large.cross), for: [])
+            closeButton.configuration?.image = UIImage.templateImageNamed(StandardImageIdentifiers.Large.cross)
         }
 
         closeButton.scalesLargeContentImage = true
