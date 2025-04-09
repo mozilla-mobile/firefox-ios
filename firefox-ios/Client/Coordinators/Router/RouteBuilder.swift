e63004370efdc9b0cc535585943395a65c9d1a0b
Cyndi Chin <cgc95@cornell.edu>
2025-04-09 07:48:37 -0400

    Bugfix FXIOS-11795 Bugzilla #1951558 (#25886)

--- firefox-ios/Client/Coordinators/Router/RouteBuilder.swift
+++ firefox-ios/Client/Coordinators/Router/RouteBuilder.swift
@@ -34,6 +34,7 @@ final class RouteBuilder: FeatureFlaggable {
 
         if let host = parseURLHost(url) {
             let urlQuery = urlScanner.fullURLQueryItem()?.asURL
+            guard host.isValidURL(urlQuery: urlQuery) else { return nil }
             // Unless the `open-url` URL specifies a `private` parameter,
             // use the last browsing mode the user was in.
             let isPrivate = Bool(urlScanner.value(query: "private") ?? "") ?? isPrivate
@@ -69,12 +70,7 @@ final class RouteBuilder: FeatureFlaggable {
                 )
 
             case .openUrl:
-                // If we have a URL query, then make sure to check its a webpage
-                if urlQuery == nil || urlQuery?.isWebPage() ?? false {
-                    return .search(url: urlQuery, isPrivate: isPrivate)
-                } else {
-                    return nil
-                }
+                return .search(url: urlQuery, isPrivate: isPrivate)
 
             case .openText:
                 return .searchQuery(query: urlScanner.value(query: "text") ?? "", isPrivate: isPrivate)
@@ -101,6 +97,7 @@ final class RouteBuilder: FeatureFlaggable {
                     return .searchQuery(query: searchText, isPrivate: isPrivate)
                 } else {
                     let url = UIPasteboard.general.url
+                    guard host.isValidURL(urlQuery: url) else { return nil }
                     return .search(url: url, isPrivate: isPrivate)
                 }
 
