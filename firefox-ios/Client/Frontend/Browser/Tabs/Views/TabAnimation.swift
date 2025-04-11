e1461a12e76fbb781825f7e8a78dbef206472d13
Sophie Amin <samin@Mac.localdomain>
2025-04-10 17:03:32 -0700

    Use container views to animate and create a temp screenshot for cases where none exists

--- firefox-ios/Client/Frontend/Browser/Tabs/Views/TabAnimation.swift
+++ firefox-ios/Client/Frontend/Browser/Tabs/Views/TabAnimation.swift
@@ -39,8 +39,7 @@ extension TabTrayViewController: BasicAnimationControllerDelegate {
             return
         }
 
-        guard let selectedTab = bvc.tabManager.selectedTab,
-              let webView = selectedTab.webView
+        guard let selectedTab = bvc.tabManager.selectedTab
         else {
             logger.log("Attempted to present the tab tray without having a selected tab",
                        level: .warning,
@@ -52,13 +51,21 @@ extension TabTrayViewController: BasicAnimationControllerDelegate {
         let finalFrame = context.finalFrame(for: destinationController)
 
         // Tab snapshot animates from web view container on BVC to the cell frame
-        let tabSnapshot = UIImageView(image: selectedTab.screenshot ?? .init())
+        var tempScreenshot = UIImage()
+        if selectedTab.screenshot == nil {
+            // When we first open a tab we do not have a screenshot before this animation runs
+            // We can fix this to run in a sequence where the tab has a snapshot by the time we get here
+            // if we roll out the .tabTrayUIExperiments fully
+            let contentView = bvc.contentContainer.contentView
+            tempScreenshot = contentView?.screenshot(quality: UIConstants.ActiveScreenshotQuality) ?? UIImage()
+        }
+        let tabSnapshot = UIImageView(image: selectedTab.screenshot ?? tempScreenshot)
         tabSnapshot.layer.cornerCurve = .continuous
         tabSnapshot.clipsToBounds = true
         tabSnapshot.contentMode = .scaleAspectFill
-        tabSnapshot.frame = webView.convert(webView.bounds, to: bvc.view)
+        let contentContainer = bvc.contentContainer
+        tabSnapshot.frame = contentContainer.convert(contentContainer.bounds, to: bvc.view)
 
-        // Allow the UI to render to make the snapshotting code more performant
         DispatchQueue.main.async { [self] in
             runPresentationAnimation(
                 context: context,
@@ -230,8 +237,7 @@ extension TabTrayViewController: BasicAnimationControllerDelegate {
         selectedTab: Tab
     ) {
         guard let panel = currentPanel as? ThemedNavigationController,
-              let panelViewController = panel.viewControllers.first as? TabDisplayPanelViewController,
-              let webView = selectedTab.webView
+              let panelViewController = panel.viewControllers.first as? TabDisplayPanelViewController
         else {
             context.completeTransition(true)
             return
@@ -295,8 +301,8 @@ extension TabTrayViewController: BasicAnimationControllerDelegate {
             cv.transform = .init(scaleX: 1.2, y: 1.2)
             cv.alpha = 0.5
 
-            // SOPHIE - Fix for webview frame presentation
-            tabSnapshot.frame = webView.convert(webView.bounds, to: browserVC.view)
+            let contentContainer = browserVC.contentContainer
+            tabSnapshot.frame = contentContainer.convert(contentContainer.bounds, to: browserVC.view)
             print("✨Tab snapshot frame✨")
             print(tabSnapshot.frame)
             tabSnapshot.layer.cornerRadius = 0
