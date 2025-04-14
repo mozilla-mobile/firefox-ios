04c414a46f008190eed2647c06140d47edd07790
ih-codes <ihugel@mozilla.com>
2025-04-14 15:40:56 -0600

    Added feature flag and check to revert unsafe continuations workaround for iOS 18.0 (beta?) users.

---
+++ firefox-ios/Client/Utils/ContinuationWrapper.swift
@@ -0,0 +1,25 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+/// In the future, we may wish to flesh out a wrapper for continuations. This will allow us to check for usage violations
+/// to log misuse rather than crash the app (FXIOS-11895).
+struct ContinuationWrapper {
+    // FXIOS-11895 Temporary test for reverting continuation workaround we put in place for iOS 18.0 (beta?) users
+    static var shouldUseCheckedContinuation: Bool {
+        let systemVersion = UIDevice.current.systemVersion
+        let isRevertUnsafeContinuationsRefactorEnabled = LegacyFeatureFlagsManager.shared.isFeatureEnabled(
+            .revertUnsafeContinuationsRefactor,
+            checking: .buildOnly
+        )
+
+        // iOS 18.0 saw crashes on checked versions of continuations (possibly only on beta iOS 18.0). We are experimenting
+        // here to see if crashes also occur for our official release iOS 18.0 users when we revert the workaround of using
+        // unsafe variants of continuations.
+        guard systemVersion != "18.0" else {
+            return isRevertUnsafeContinuationsRefactorEnabled
+        }
+
+        return true
+    }
+}
