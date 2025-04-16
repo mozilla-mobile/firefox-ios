769f01c6edab227f5d803de1865b9feeb149a643
Laurie Marceau <lmarceau@mozilla.com>
2025-04-16 11:03:17 -0400

    Revert changes

--- BrowserKit/Sources/WebEngine/Engine.swift
+++ BrowserKit/Sources/WebEngine/Engine.swift
@@ -14,7 +14,8 @@ public protocol Engine {
     /// Creates a new engine session.
     /// - Parameter dependencies: Pass in the required session dependencies on creation
     /// - Returns: The created `EngineSession`
-    func createSession(dependencies: EngineSessionDependencies) async throws -> EngineSession
+    @MainActor
+    func createSession(dependencies: EngineSessionDependencies) throws -> EngineSession
 
     /// Warm the `Engine` whenever we move the application to foreground
     func warmEngine()
