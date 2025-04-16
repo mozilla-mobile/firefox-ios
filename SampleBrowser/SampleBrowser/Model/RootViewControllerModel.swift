d1ca4f547ed3c37a0b06a7923cf792e82f93ac8a
Filippo <fzazzeroni@mozilla.com>
2025-04-16 09:43:54 +0200

    Refactor UI Sample Browser

--- SampleBrowser/SampleBrowser/Model/RootViewControllerModel.swift
+++ SampleBrowser/SampleBrowser/Model/RootViewControllerModel.swift
@@ -80,17 +80,6 @@ class RootViewControllerModel {
 
     // MARK: - Address toolbar
     func addressToolbarContainerModel(url: URL?) -> AddressToolbarContainerModel {
-        let browserActions = [ToolbarElement(
-            iconName: StandardImageIdentifiers.Large.appMenu,
-            isEnabled: true,
-            a11yLabel: "Open Menu",
-            a11yHint: nil,
-            a11yId: "appMenuButton",
-            hasLongPressAction: false,
-            onSelected: { _ in
-                self.addressToolbarDelegate?.didTapMenu()
-            })]
-
         let locationViewConfig = LocationViewConfiguration(
             searchEngineImageViewA11yId: "searchEngine",
             searchEngineImageViewA11yLabel: "Search engine icon",
@@ -98,7 +87,7 @@ class RootViewControllerModel {
             lockIconButtonA11yLabel: "Tracking Protection",
             urlTextFieldPlaceholder: "Search or enter address",
             urlTextFieldA11yId: "urlTestField",
-            searchEngineImage: UIImage(named: "bingSearchEngine"),
+            searchEngineImage: UIImage(named: "duckduckgoSearchEngine"),
             lockIconImageName: StandardImageIdentifiers.Large.lock,
             lockIconNeedsTheming: false,
             safeListedURLImageName: StandardImageIdentifiers.Small.notificationDotFill,
@@ -119,6 +108,6 @@ class RootViewControllerModel {
             locationViewConfiguration: locationViewConfig,
             navigationActions: [],
             pageActions: [],
-            browserActions: browserActions)
+            browserActions: [])
     }
 }
