e96689ac87e3c3b96fa64bf2fe1e1111861bd135
umbralOptimatum <=>
2025-04-17 15:06:23 -0400

    Improve Focus URL encoding

--- focus-ios/Blockzilla/Utilities/URIFixup.swift
+++ focus-ios/Blockzilla/Utilities/URIFixup.swift
@@ -11,7 +11,7 @@ class URIFixup {
             return nil
         }
 
-        guard !trimmed.lowercased().hasPrefix("javascript:") else {
+        guard !escaped.lowercased().hasPrefix("javascript:") else {
             return nil
         }
 
@@ -19,7 +19,7 @@ class URIFixup {
         // If it starts with "file://", return nil, indicating that this function
         // does not handle local file URLs, resulting in a search
 
-        guard !trimmed.lowercased().hasPrefix("file:") else {
+        guard !escaped.lowercased().hasPrefix("file:") else {
             return nil
         }
 
@@ -27,12 +27,12 @@ class URIFixup {
         // all valid requests starting with "http://", "about:", etc.
         // Also check with a regular expression if there is a port in the url
         // this will be handle later in this function adding http prefix
-        if let url = URL(string: trimmed, invalidCharacters: false),
+        if let url = URL(string: escaped, invalidCharacters: false),
             url.scheme != nil,
-            trimmed.range(of: "\\b:[0-9]{1,5}", options: .regularExpression) == nil {
+            escaped.range(of: "\\b:[0-9]{1,5}", options: .regularExpression) == nil {
             // check for top-level domain if scheme is "http://" or "https://"
-            if trimmed.hasPrefix("http://") || trimmed.hasPrefix("https://") {
-                if trimmed.contains(".") == false {
+            if escaped.hasPrefix("http://") || escaped.hasPrefix("https://") {
+                if escaped.contains(".") == false {
                     return nil
                 }
             }
@@ -43,19 +43,14 @@ class URIFixup {
         // make sure there's at least one "." in the host. This means
         // we'll allow single-word searches (e.g., "foo") at the expense
         // of breaking single-word hosts without a scheme (e.g., "localhost").
-        if trimmed.contains(".") == false {
+        if escaped.contains(".") == false {
             return nil
         }
 
-        if trimmed.contains(" ") == true {
-            return nil
-        }
-
-        // If the input url has a prefix http or https, don't append again
+        // If there is a "." but no http or https prefix, prepend "http://" and try again.
         let finalurl = escaped.hasPrefix("http://") || escaped.hasPrefix("https://") ? escaped : "http://\(escaped)"
 
-        // If there is a ".", prepend "http://" and try again. Since this
-        // is strictly an "http://" URL, we also require a host.
+        // Since this is strictly an "http://" URL, we also require a host.
         if let url = URL(string: finalurl, invalidCharacters: false), url.host != nil {
             return url
         }
