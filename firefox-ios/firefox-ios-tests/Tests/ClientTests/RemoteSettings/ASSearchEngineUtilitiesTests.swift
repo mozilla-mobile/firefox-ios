905d32990883dc67041fb9bc801906a7d5592a06
mattreaganmozilla <145381717+mattreaganmozilla@users.noreply.github.com>
2025-04-24 11:06:12 -0700

    Bugfix FXIOS-12033 [SEC] Fix check for `{searchTerms}` in base URL (#26223)
    * [FXIOS-12033] Turns out it was a typo after all
    
    * [FXIOS-12033] More unit tests
    
    * [FXIOS-12033] More unit tests

--- firefox-ios/firefox-ios-tests/Tests/ClientTests/RemoteSettings/ASSearchEngineUtilitiesTests.swift
+++ firefox-ios/firefox-ios-tests/Tests/ClientTests/RemoteSettings/ASSearchEngineUtilitiesTests.swift
@@ -10,6 +10,59 @@ import WebKit
 import MozillaAppServices
 
 class ASSearchEngineUtilitiesTests: XCTestCase {
+    private let leo_eng_deu_engine_no_search_params =
+    SearchEngineDefinition(
+        aliases: [],
+        charset: "UTF-8",
+        classification: .unknown,
+        identifier: "leo_ende_de",
+        name: "LEO Eng-Deu",
+        optional: false,
+        partnerCode: "",
+        telemetrySuffix: "",
+        urls: SearchEngineUrls(
+            search: SearchEngineUrl(
+                base: "https://dict.leo.org/englisch-deutsch/{searchTerms}",
+                method: "GET",
+                params: [],
+                searchTermParamName: nil
+            ),
+            suggestions: nil,
+            trending: nil,
+            searchForm: nil
+        ),
+        orderHint: nil,
+        clickUrl: nil
+    )
+    private let leo_eng_deu_engine =
+    SearchEngineDefinition(
+        aliases: [],
+        charset: "UTF-8",
+        classification: .unknown,
+        identifier: "leo_ende_de",
+        name: "LEO Eng-Deu",
+        optional: false,
+        partnerCode: "",
+        telemetrySuffix: "",
+        urls: SearchEngineUrls(
+            search: SearchEngineUrl(
+                base: "https://dict.leo.org/englisch-deutsch/{searchTerms}",
+                method: "GET",
+                params: [SearchUrlParam(
+                    name: "foo",
+                    value: "bar",
+                    enterpriseValue: nil,
+                    experimentConfig: nil
+                )],
+                searchTermParamName: nil
+            ),
+            suggestions: nil,
+            trending: nil,
+            searchForm: nil
+        ),
+        orderHint: nil,
+        clickUrl: nil
+    )
     private let google_US_testEngine =
     SearchEngineDefinition(
         aliases: ["google"],
@@ -87,6 +140,36 @@ class ASSearchEngineUtilitiesTests: XCTestCase {
         let result = ASSearchEngineUtilities.convertASSearchURLToOpenSearchURL(engine.urls.suggestions,
                                                                                for: engine)
         let expected = "https://www.google.com/complete/search?client=firefox&q={searchTerms}"
+        XCTAssertEqual(result, expected)
+    }
+
+    func testEmptyPartnerCodeSearchURL() {
+        var engine = google_US_testEngine
+
+        // Force an empty partnerCode string:
+        engine.partnerCode = ""
+
+        let result = ASSearchEngineUtilities.convertASSearchURLToOpenSearchURL(engine.urls.search,
+                                                                               for: engine)
+        let expected = "https://www.google.com/search?client=&q={searchTerms}"
+        XCTAssertEqual(result, expected)
+    }
+
+    func testSearchTermIncludedInBaseURL() {
+        let engine = leo_eng_deu_engine_no_search_params
+
+        let result = ASSearchEngineUtilities.convertASSearchURLToOpenSearchURL(engine.urls.search,
+                                                                               for: engine)
+        let expected = "https://dict.leo.org/englisch-deutsch/{searchTerms}"
+        XCTAssertEqual(result, expected)
+    }
+
+    func testSearchTermIncludedInBaseURLAndParams() {
+        let engine = leo_eng_deu_engine
+
+        let result = ASSearchEngineUtilities.convertASSearchURLToOpenSearchURL(engine.urls.search,
+                                                                               for: engine)
+        let expected = "https://dict.leo.org/englisch-deutsch/{searchTerms}?foo=bar"
         XCTAssertEqual(result, expected)
     }
 }
