2c7792653a3211e21de7cf49c791674e27e081f8
Bastien Orivel <borivel@mozilla.com>
2025-04-24 14:06:02 +0200

    Add missing license headers

--- taskcluster/ffios_taskgraph/worker_types.py
+++ taskcluster/ffios_taskgraph/worker_types.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 from taskgraph.transforms.task import payload_builder
 from taskgraph.util.schema import taskref_or_string
 from voluptuous import Optional, Required
