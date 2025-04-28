8c00320a40ce6ccced4cb1cb3d548288c5907ab1
Winnie Teichmann <4530+thatswinnie@users.noreply.github.com>
2025-04-28 14:41:12 +0200

    Fix spelling

--- firefox-ios/Client/Frontend/Browser/TabScrollController.swift
+++ firefox-ios/Client/Frontend/Browser/TabScrollController.swift
@@ -441,14 +441,14 @@ private extension TabScrollController {
     /// - `.visible`: Toolbars are currently showing (`toolbarsShowing == true`).
     /// - `.animating`: In transition or partially visible state.
     func updateToolbarState() {
-        // bottom containers are collapsedd
+        // bottom containers are collapsed
         let bottomContainerCollapsed = bottomContainerOffset == bottomContainerScrollHeight
         let overKeyboardContainerCollapsed = overKeyboardContainerOffset == overKeyboardScrollHeight
 
         // top container
-        let headerContainerIsCollapesed = headerTopOffset == -headerHeight
+        let headerContainerIsCollapsed = headerTopOffset == -headerHeight
 
-        if headerContainerIsCollapesed && (bottomContainerCollapsed && overKeyboardContainerCollapsed) {
+        if headerContainerIsCollapsed && (bottomContainerCollapsed && overKeyboardContainerCollapsed) {
             setToolbarState(state: .collapsed)
         } else if toolbarsShowing {
             setToolbarState(state: .visible)
