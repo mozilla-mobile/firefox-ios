ec2731a6f14f8c80edc6fbb57a6847dc320509fd
Filippo <fzazzeroni@mozilla.com>
2025-05-06 14:28:34 +0200

    lint

--- firefox-ios/Client/Frontend/Browser/BrowserViewController/Extensions/BrowserViewController+WebViewDelegates.swift
+++ firefox-ios/Client/Frontend/Browser/BrowserViewController/Extensions/BrowserViewController+WebViewDelegates.swift
@@ -776,9 +776,9 @@ extension BrowserViewController: WKNavigationDelegate {
             tempPDF.onDownloadError = { error in
                 self?.navigationHandler?.removeDocumentLoading()
                 self?.logger.log("Failed to download Document",
-                                level: .warning,
-                                category: .webview,
-                                extra: ["url": request.url?.absoluteString ?? "Unknown URL"])
+                                 level: .warning,
+                                 category: .webview,
+                                 extra: ["url": request.url?.absoluteString ?? "Unknown URL"])
                 guard let error, let webView = tab?.webView else { return }
                 self?.showErrorPage(webView: webView, error: error)
             }
