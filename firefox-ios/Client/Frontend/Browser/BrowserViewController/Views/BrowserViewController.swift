32acc4a407ad85b2ebc7dc1f76fec4250035461d
PARAIPAN SORIN <paraipan.sorin1993@gmail.com>
2025-05-07 15:58:27 +0300

    Disable animation when tapping the tab tray button.

--- firefox-ios/Client/Frontend/Browser/BrowserViewController/Views/BrowserViewController.swift
+++ firefox-ios/Client/Frontend/Browser/BrowserViewController/Views/BrowserViewController.swift
@@ -1979,8 +1979,7 @@ class BrowserViewController: UIViewController,
     }
 
     private func updateToolbarAnimationStateIfNeeded() {
-        guard isSwipingTabsEnabled,
-              isToolbarRefactorEnabled,
+        guard isToolbarRefactorEnabled,
         store.state.screenState(
             ToolbarState.self,
             for: .toolbar,
@@ -2387,6 +2386,13 @@ class BrowserViewController: UIViewController,
             // TODO: FXIOS-11248 Use NavigationBrowserAction instead of GeneralBrowserAction to open tab tray
             updateZoomPageBarVisibility(visible: false)
             focusOnTabSegment()
+            store.dispatch(
+                ToolbarAction(
+                    shouldAnimate: false,
+                    windowUUID: windowUUID,
+                    actionType: ToolbarActionType.animationStateChanged
+                )
+            )
         case .share:
             // User tapped the Share button in the toolbar
             guard let button = state.buttonTapped else { return }
