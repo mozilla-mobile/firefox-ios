31ee026660588fe5d9bd99b01c48124f4cc91e28
Bastien Orivel <eijebong@bananium.fr>
2025-05-19 20:46:22 +0200

    Promote and push beta/release builds for prod releases

--- taskcluster/ffios_taskgraph/transforms/relpro_promote.py
+++ taskcluster/ffios_taskgraph/transforms/relpro_promote.py
@@ -8,29 +8,9 @@ from taskgraph.util.schema import resolve_keyed_by
 transforms = TransformSequence()
 
 @transforms.add
-def replace_bitrise_scheme_for_release_task(config, tasks):
-    """
-    This replaces `<release-type-scheme>` in all bitrise inputs to:
-
-    - "FirefoxBeta" if the current release-type is beta
-    - "Firefox" otherwise
-    """
-
-    # TODO: Replace this with `resolve_keyed_by` when https://github.com/taskcluster/taskgraph/pull/608 is merged
-
-    scheme = "Firefox"
-    if config.params.get("release_type") == "beta":
-        scheme = "FirefoxBeta"
-
+def add_release_type_attribute(config, tasks):
     for task in tasks:
         task.setdefault("attributes", {})["release-type"] = config.params.get("release_type")
-        for workflow in task.get('bitrise', {}).get('workflows', {}):
-            if isinstance(workflow, str):
-                continue
-            for name, params in workflow.items():
-                for param in params:
-                    for param_name, param_value in param.items():
-                        param_value = param_value.replace("<release-type-scheme>", scheme)
-                        param[param_name] = param_value
+        resolve_keyed_by(task, 'treeherder.symbol', task["name"], **{'build-type': task['attributes']['build-type']})
 
         yield task
