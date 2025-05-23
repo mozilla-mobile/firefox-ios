0e999a6db91c2e85ebd959dafdba44fd112767e8
Laurie Marceau <lmarceau@mozilla.com>
2025-05-23 11:12:26 -0400

    Fix some of the changes

--- firefox-ios/Client/Frontend/Browser/Tabs/Views/TabDisplayDiffableDataSource.swift
+++ firefox-ios/Client/Frontend/Browser/Tabs/Views/TabDisplayDiffableDataSource.swift
@@ -10,26 +10,25 @@ final class TabDisplayDiffableDataSource: UICollectionViewDiffableDataSource<Tab
         // Adding a UUID to the section allows us to trigger a reload on the header by updating the UUID
         // (which creates a diff) while updating the snapshot. This avoids calling reloadSections on inactiveTabs
         // when there are no inactiveTabs in the section, which triggers a crash on iOS 15.
-//        case inactiveTabs(UUID)
+        case inactiveTabs(UUID)
         case tabs
     }
 
     enum TabItem: Hashable {
-//        case inactiveTab(InactiveTabsModel)
+        case inactiveTab(InactiveTabsModel)
         case tab(TabModel)
     }
 
     func updateSnapshot(state: TabsPanelState) {
         var snapshot = NSDiffableDataSourceSnapshot<TabDisplayViewSection, TabDisplayViewItem>()
 
-//        let inactiveTabsUUID = UUID()
-//        snapshot.appendSections([.inactiveTabs(inactiveTabsUUID), .tabs])
-        snapshot.appendSections([.tabs])
+        let inactiveTabsUUID = UUID()
+        snapshot.appendSections([.inactiveTabs(inactiveTabsUUID), .tabs])
 
-//        if state.isInactiveTabsExpanded {
-//            let inactiveTabs = state.inactiveTabs.map { TabDisplayViewItem.inactiveTab($0) }
-//            snapshot.appendItems(inactiveTabs, toSection: .inactiveTabs(inactiveTabsUUID))
-//        }
+        if state.isInactiveTabsExpanded {
+            let inactiveTabs = state.inactiveTabs.map { TabDisplayViewItem.inactiveTab($0) }
+            snapshot.appendItems(inactiveTabs, toSection: .inactiveTabs(inactiveTabsUUID))
+        }
 
         let tabs = state.tabs.map { TabDisplayViewItem.tab($0) }
         snapshot.appendItems(tabs, toSection: .tabs)
