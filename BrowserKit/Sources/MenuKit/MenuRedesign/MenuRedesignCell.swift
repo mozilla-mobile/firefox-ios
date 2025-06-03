51dc31e39c2bb55a9a2b2859e2cc28cde12e02a3
dicarobinho <61138287+dicarobinho@users.noreply.github.com>
2025-06-03 17:17:10 +0300

    Add FXIOS-12356 ⁃ [Menu Redesign] Implement the logic to handle different active states for menu options (#26925)
    FXIOS-12356 #26917 ⁃ [Menu Redesign] Implement the logic to handle different active states for menu options

--- BrowserKit/Sources/MenuKit/MenuRedesign/MenuRedesignCell.swift
+++ BrowserKit/Sources/MenuKit/MenuRedesign/MenuRedesignCell.swift
@@ -8,15 +8,14 @@ import UIKit
 
 final class MenuRedesignCell: UITableViewCell, ReusableCell, ThemeApplicable {
     private struct UX {
-        static let contentMargin: CGFloat = 12
+        static let contentMargin: CGFloat = 16
+        static let infoLabelHorizontalMargin: CGFloat = 8
+        static let infoLabelVerticalPadding: CGFloat = 7
+        static let infoLabelHorizontalPadding: CGFloat = 14
         static let iconSize: CGFloat = 24
         static let largeIconSize: CGFloat = 48
         static let contentSpacing: CGFloat = 3
         static let noDescriptionContentSpacing: CGFloat = 0
-    }
-
-    private var separatorInsetSize: CGFloat {
-        return UX.contentMargin * 2 + UX.iconSize
     }
 
     // MARK: - UI Elements
@@ -36,6 +35,18 @@ final class MenuRedesignCell: UITableViewCell, ReusableCell, ThemeApplicable {
 
     private var iconImageView: UIImageView = .build()
 
+    private var infoLabelView: MenuPaddedLabel = .build { label in
+        label.textAlignment = .center
+        label.layer.masksToBounds = true
+        label.font = FXFontStyles.Regular.subheadline.scaledFont()
+        label.textInsets = UIEdgeInsets(
+            top: UX.infoLabelVerticalPadding,
+            left: UX.infoLabelHorizontalPadding,
+            bottom: UX.infoLabelVerticalPadding,
+            right: UX.infoLabelHorizontalPadding
+        )
+    }
+
     // MARK: - Properties
     var model: MenuElement?
 
@@ -48,12 +59,27 @@ final class MenuRedesignCell: UITableViewCell, ReusableCell, ThemeApplicable {
         fatalError("init(coder:) has not been implemented")
     }
 
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        infoLabelView.text = nil
+        iconImageView.image = nil
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        infoLabelView.layer.cornerRadius = infoLabelView.frame.height / 2
+    }
+
     func configureCellWith(model: MenuElement) {
         self.model = model
         self.titleLabel.text = model.title
         self.descriptionLabel.text = model.description
         self.contentStackView.spacing = model.description != nil ? UX.contentSpacing : UX.noDescriptionContentSpacing
-        self.iconImageView.image = UIImage(named: model.iconName)?.withRenderingMode(.alwaysTemplate)
+        if let infoTitle = model.infoTitle {
+            infoLabelView.text = infoTitle
+        } else {
+            self.iconImageView.image = UIImage(named: model.iconName)?.withRenderingMode(.alwaysTemplate)
+        }
         self.isAccessibilityElement = true
         self.isUserInteractionEnabled = !model.isEnabled ? false : true
         self.accessibilityIdentifier = model.a11yId
@@ -61,26 +87,44 @@ final class MenuRedesignCell: UITableViewCell, ReusableCell, ThemeApplicable {
         self.accessibilityHint = model.a11yHint
         self.accessibilityTraits = .button
         self.separatorInset = .zero
-        setupView()
+        setupView(hasInfoTitle: model.infoTitle != nil)
     }
 
-    private func setupView() {
+    private func setupView(hasInfoTitle: Bool) {
         self.addSubview(contentStackView)
-        self.addSubview(iconImageView)
+        self.addSubview(hasInfoTitle ? infoLabelView : iconImageView)
         contentStackView.addArrangedSubview(titleLabel)
         contentStackView.addArrangedSubview(descriptionLabel)
         NSLayoutConstraint.activate([
             contentStackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: UX.contentMargin),
             contentStackView.topAnchor.constraint(equalTo: topAnchor, constant: UX.contentMargin),
-            contentStackView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -UX.contentMargin),
-
-            iconImageView.leadingAnchor.constraint(equalTo: contentStackView.trailingAnchor,
-                                                   constant: UX.contentMargin),
-            iconImageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -UX.contentMargin),
-            iconImageView.centerYAnchor.constraint(equalTo: centerYAnchor),
-            iconImageView.widthAnchor.constraint(equalToConstant: UX.iconSize),
-            iconImageView.heightAnchor.constraint(equalToConstant: UX.iconSize)
+            contentStackView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -UX.contentMargin)
         ])
+
+        if hasInfoTitle {
+            NSLayoutConstraint.activate([
+                contentStackView.trailingAnchor.constraint(
+                    equalTo: infoLabelView.leadingAnchor,
+                    constant: -UX.infoLabelHorizontalMargin
+                ),
+                infoLabelView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -UX.infoLabelHorizontalMargin),
+                infoLabelView.centerYAnchor.constraint(equalTo: centerYAnchor)
+            ])
+            infoLabelView.setContentCompressionResistancePriority(.required, for: .horizontal)
+            infoLabelView.setContentHuggingPriority(.required, for: .horizontal)
+
+            contentStackView.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+            contentStackView.setContentHuggingPriority(.defaultLow, for: .horizontal)
+        } else {
+            NSLayoutConstraint.activate([
+                iconImageView.leadingAnchor.constraint(equalTo: contentStackView.trailingAnchor,
+                                                       constant: UX.contentMargin),
+                iconImageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -UX.contentMargin),
+                iconImageView.centerYAnchor.constraint(equalTo: centerYAnchor),
+                iconImageView.widthAnchor.constraint(equalToConstant: UX.iconSize),
+                iconImageView.heightAnchor.constraint(equalToConstant: UX.iconSize)
+            ])
+        }
         adjustLayout(isAccessibilityCategory: UIApplication.shared.preferredContentSizeCategory.isAccessibilityCategory)
     }
 
@@ -94,7 +138,11 @@ final class MenuRedesignCell: UITableViewCell, ReusableCell, ThemeApplicable {
     func applyTheme(theme: Theme) {
         guard let model else { return }
         backgroundColor = theme.colors.layer2
-        if model.isActive {
+        if model.infoTitle != nil {
+            infoLabelView.textColor = theme.colors.textPrimary
+            infoLabelView.backgroundColor = model.isActive ? theme.colors.layerInformation : theme.colors.layer3
+            titleLabel.textColor = model.isActive ? theme.colors.textAccent : theme.colors.textPrimary
+        } else if model.isActive {
             titleLabel.textColor = theme.colors.textAccent
             descriptionLabel.textColor = theme.colors.textSecondary
             iconImageView.tintColor = theme.colors.iconAccentBlue
