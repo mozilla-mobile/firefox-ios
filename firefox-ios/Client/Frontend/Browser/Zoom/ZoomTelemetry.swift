6af5c2fa7db73c99927aaa821a5ebaa078fe9564
Yoana Rios <yrrios13@gmail.com>
2025-06-05 13:46:35 -0400

    Add FXIOS-12371 #26968 ⁃ [Default Zoom] telemetry for Page Zoom Settings (#27006)
    Co-authored-by: Isabella <173110554+ih-codes@users.noreply.github.com>
    (cherry picked from commit 472c33b2d9d821ac0101d5d00a07b07198fcb57c)

--- firefox-ios/Client/Frontend/Browser/Zoom/ZoomTelemetry.swift
+++ firefox-ios/Client/Frontend/Browser/Zoom/ZoomTelemetry.swift
@@ -7,11 +7,13 @@ import Glean
 
 struct ZoomTelemetry {
     private let gleanWrapper: GleanWrapper
+    static let defaultZoomExtraKey = "settings.zoom_bar.default_zoom"
 
     init(gleanWrapper: GleanWrapper = DefaultGleanWrapper()) {
         self.gleanWrapper = gleanWrapper
     }
 
+    // Zoom bar actions
     func zoomIn(zoomLevel: ZoomLevel) {
         let extra = GleanMetrics.ZoomBar.ZoomInButtonTappedExtra(level: zoomLevel.telemetryQuantity)
         gleanWrapper.recordEvent(for: GleanMetrics.ZoomBar.zoomInButtonTapped, extras: extra)
@@ -28,5 +30,24 @@ struct ZoomTelemetry {
 
     func closeZoomBar() {
         gleanWrapper.recordEvent(for: GleanMetrics.ZoomBar.closeButtonTapped)
+    }
+
+    // Page Zoom Settings actions
+    func updateDefaultZoomLevel(zoomLevel: ZoomLevel) {
+        let changedTo = zoomLevel.displayName
+        let preference = ZoomTelemetry.defaultZoomExtraKey
+
+        let extra = GleanMetrics.Preferences.ChangedExtra(changedTo: changedTo,
+                                                          preference: preference)
+        gleanWrapper.recordEvent(for: GleanMetrics.Preferences.changed, extras: extra)
+    }
+
+    func deleteZoomDomainLevel(value: Int32) {
+        let extra = GleanMetrics.SettingsZoomBar.DomainListItemSwipedToDeleteExtra(index: value)
+        gleanWrapper.recordEvent(for: GleanMetrics.SettingsZoomBar.domainListItemSwipedToDelete, extras: extra)
+    }
+
+    func resetDomainZoomLevel() {
+        gleanWrapper.recordEvent(for: GleanMetrics.SettingsZoomBar.domainListResetButtonTapped)
     }
 }
