100905aedfe04950238f7b2243d1f00bde89d1b2
PARAIPAN SORIN <paraipan.sorin1993@gmail.com>
2025-06-05 15:38:56 +0300

    Revert constraint changes.

--- BrowserKit/Sources/ToolbarKit/AddressToolbar/LocationView/LocationView.swift
+++ BrowserKit/Sources/ToolbarKit/AddressToolbar/LocationView/LocationView.swift
@@ -176,11 +176,11 @@ final class LocationView: UIView,
                 containerView.leadingAnchor.constraint(equalTo: leadingAnchor),
                 containerView.trailingAnchor.constraint(equalTo: trailingAnchor),
             ]
-        } else if let superview, !doesURLTextFieldExceedViewWidth {
+        } else if !doesURLTextFieldExceedViewWidth {
             newConstraints = [
-                containerView.leadingAnchor.constraint(greaterThanOrEqualTo: superview.leadingAnchor),
-                containerView.trailingAnchor.constraint(lessThanOrEqualTo: superview.trailingAnchor),
-                containerView.centerXAnchor.constraint(equalTo: superview.centerXAnchor)
+                containerView.leadingAnchor.constraint(greaterThanOrEqualTo: leadingAnchor),
+                containerView.trailingAnchor.constraint(lessThanOrEqualTo: trailingAnchor),
+                containerView.centerXAnchor.constraint(equalTo: centerXAnchor)
             ]
         }
 
