d0d27d9f5ca721266dae11e062f32bb920588e0d
Carson Ramsden <carsonramsden@gmail.com>
2025-06-06 08:20:58 -0600

    Add [SkadNetwork] Update conversion metrics for Skan 3 platforms (#27126)
    * Update skan conversion metrics
    
    * update conversion values
    
    * update new tab and search conversion values
    
    * don't have a great way to test ConversionValueUtil right now so removing these tests

--- firefox-ios/Client/Application/UserConversionMetrics.swift
+++ firefox-ios/Client/Application/UserConversionMetrics.swift
@@ -7,84 +7,27 @@ import Foundation
 import Shared
 import StoreKit
 
-class UserConversionMetrics {
-    private var appOpenTimestamps: [Date] {
-        get { return userDefaults.array(forKey: PrefsKeys.Session.firstWeekAppOpenTimestamps) as? [Date] ?? [Date]() }
-        set { userDefaults.set(newValue, forKey: PrefsKeys.Session.firstWeekAppOpenTimestamps) }
+struct UserConversionMetrics {
+    private enum ConversionValue: Int {
+        /// These conversion values just need to be larger than the onboarding events
+        /// that can be as low as 1 or as high as 7.
+        case newTab = 10
+        case search = 11
     }
 
-    private var searchesTimestamps: [Date] {
-        get { return userDefaults.array(forKey: PrefsKeys.Session.firstWeekSearchesTimestamps) as? [Date] ?? [Date]() }
-        set { userDefaults.set(newValue, forKey: PrefsKeys.Session.firstWeekSearchesTimestamps) }
-    }
-    var userDefaults: UserDefaultsInterface = UserDefaults.standard
-
-    init(userDefaults: UserDefaults = .standard) {
-        self.userDefaults = userDefaults
-    }
-
-    func didStartNewSession() {
-        guard shouldRecordMetric() else { return }
-        appOpenTimestamps.append(Date())
-        if shouldActivateProfile() {
-            sendActivationEvent()
-        }
+    func didOpenNewTab() {
+        sendActivationEvent(conversionValue: .newTab)
     }
 
     func didPerformSearch() {
-        guard shouldRecordMetric() else { return }
-        searchesTimestamps.append(Date())
-        if shouldActivateProfile() {
-            sendActivationEvent()
-        }
+        sendActivationEvent(conversionValue: .search)
     }
 
-    /// there shouldn't be more records added to UserDefaults once this event happens
-    /// and it should not affect existing users
-    func shouldRecordMetric() -> Bool {
-        guard let firstAppUse = userDefaults.object(forKey: PrefsKeys.Session.FirstAppUse) as? UInt64,
-              !userDefaults.bool(forKey: PrefsKeys.Session.didUpdateConversionValue) else { return false }
-
-        let firstAppUseDate = Date.fromTimestamp(firstAppUse)
-        guard let oneWeekSinceFirstUse = Calendar.current.date(
-            byAdding: .day,
-            value: 7,
-            to: firstAppUseDate
-        ) else { return false }
-        if Date() > oneWeekSinceFirstUse {
-            return false
-        }
-        return true
-    }
-
-///    The criteria for this event is:
-///    * The user needs to open the app on at least 3 distinct days during their first week (days 1-7)
-///    * Perform at least one search between days 4-7
-    func shouldActivateProfile() -> Bool {
-        guard let firstAppUse = userDefaults.object(forKey: PrefsKeys.Session.FirstAppUse) as? UInt64 else {
-            return false
-        }
-        let firstAppUseDate = Date.fromTimestamp(firstAppUse)
-        if let oneWeekSinceFirstUse = Calendar.current.date(byAdding: .day, value: 7, to: firstAppUseDate),
-            let lastThreeDaysOfFirstWeek = Calendar.current.date(byAdding: .day, value: -3, to: oneWeekSinceFirstUse) {
-            let appOpensInWeek = appOpenTimestamps.filter { $0 < oneWeekSinceFirstUse }
-            let distinctDaysOpened = Set(appOpensInWeek.map { Calendar.current.startOfDay(for: $0) }).count
-
-            // last three days of the first week of usage
-            let searchInLastThreeDays = searchesTimestamps.filter {
-                $0 > lastThreeDaysOfFirstWeek && $0 < oneWeekSinceFirstUse
-            }
-            let shouldActivateProfile = distinctDaysOpened >= 3 && !searchInLastThreeDays.isEmpty
-            return shouldActivateProfile
-        } else {
-            return false
-        }
-    }
-
-    private func sendActivationEvent() {
+    private func sendActivationEvent(conversionValue: ConversionValue) {
         let logger: Logger = DefaultLogger.shared
-        let conversionValue = ConversionValueUtil(fineValue: 0, coarseValue: .high, logger: logger)
-        conversionValue.adNetworkAttributionUpdateConversionEvent()
-        userDefaults.set(true, forKey: PrefsKeys.Session.didUpdateConversionValue)
+        /// Google ads only supports SKAN 3 so it will only interpret fine values.
+        /// Setting the coarse value to low for now.
+        let conversionValueUtil = ConversionValueUtil(fineValue: conversionValue.rawValue, coarseValue: .low, logger: logger)
+        conversionValueUtil.adNetworkAttributionUpdateConversionEvent()
     }
 }
