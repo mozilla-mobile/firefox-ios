c98e65716d1414f5f6359301de6e2a9b4e89d81c
Sophie <samin@mozilla.com>
2025-06-26 15:18:20 -0700

    Add comment

--- firefox-ios/Client/Frontend/Browser/Tabs/Views/Animation/TabAnimation.swift
+++ firefox-ios/Client/Frontend/Browser/Tabs/Views/Animation/TabAnimation.swift
@@ -230,8 +230,9 @@ extension TabTrayViewController: BasicAnimationControllerDelegate {
             self?.unhideCellBorder(tabCell: tabCell, isPrivate: selectedTab.isPrivate, theme: theme)
         }
 
-        // NOTE FOR SIMULATOR TESTING - Debug > Slow Animations setting on the simulator does not render this border animation correctly
-        // to see changes, it's best to make a recording of the simulator animation and play it back at a reduced speed or go frame-by-frame.
+        // IMPORTANT NOTE FOR SIMULATOR TESTING
+        // The Debug > Slow Animations setting on the simulator does not render this border animation correctly on XCode 16.2
+        // Alternative: Make recording of the simulator animation and play it back at a reduced speed or go frame-by-frame.
         let lineWidthAnimation = CABasicAnimation(keyPath: UX.lineWidthKeyPath)
         lineWidthAnimation.fromValue = UX.initialBorderWidth
         lineWidthAnimation.toValue = borderWidth
