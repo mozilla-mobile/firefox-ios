ccd3d6ae7c92a1afe7b790fa602969598ccc17a1
Ione Souza Junior <junior@ionixjunior.com.br>
2025-06-26 20:06:29 -0300

    Extract the active branch section into a new property

--- focus-ios/Blockzilla/InternalSettings/InternalExperimentDetailView.swift
+++ focus-ios/Blockzilla/InternalSettings/InternalExperimentDetailView.swift
@@ -68,6 +68,32 @@ extension InternalExperimentDetailView: View {
             }
         }
     }
+
+    private var activeBranchSection: some View {
+        SwiftUI.Section {
+            Picker(selection: $selectedBranchSlug, label: Text(verbatim: "Active Branch")) {
+                ForEach(pickerBranches, id: \.self) { branch in
+                    if branch == notEnrolledBranchSlug {
+                        Text(verbatim: "Not Enrolled")
+                    } else {
+                        Text(verbatim: branch)
+                    }
+                }
+            }.onReceive(Just(selectedBranchSlug)) { newValue in
+                if newValue != notEnrolledBranchSlug {
+                    if NimbusWrapper.shared.getEnrolledBranchSlug(forExperiment: experiment) != newValue {
+                        if let branch = experiment.branches.first(where: { $0.slug == newValue }) {
+                            NimbusWrapper.shared.optIn(toExperiment: experiment, withBranch: branch)
+                        }
+                    }
+                } else {
+                    if NimbusWrapper.shared.getEnrolledBranchSlug(forExperiment: experiment) != nil {
+                        NimbusWrapper.shared.optOut(ofExperiment: experiment)
+                    }
+                }
+            }
+        }
+    }
 }
 
 struct InternalExperimentDetailView_Previews: PreviewProvider {
