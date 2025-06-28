80f844c026b22931b49945bd60986f92409b740a
Ione Souza Junior <junior@ionixjunior.com.br>
2025-06-27 23:10:16 -0300

    Refactor FXIOS-7301 - Remove 2 closure_body_length violations from MainMenuState.swift, and InternalExperimentDetailView.swift (#27605)
    * Decrease warning and error threshold
    
    * Extract the entire reducer into a new function
    
    * Use the new function to handle reducer
    
    * Fix trailing whitespace violation
    
    * Extract the description section into a new property
    
    * Use the new property to show the description section
    
    * Extract the available branches section into a new property
    
    * Use the new property to show the available branches section
    
    * Extract the active branch section into a new property
    
    * Use the new property to show the active branch section

--- focus-ios/Blockzilla/InternalSettings/InternalExperimentDetailView.swift
+++ focus-ios/Blockzilla/InternalSettings/InternalExperimentDetailView.swift
@@ -23,42 +23,54 @@ struct InternalExperimentDetailView {
 extension InternalExperimentDetailView: View {
     var body: some View {
         Form {
-            SwiftUI.Section {
-                Text(verbatim: experiment.userFacingDescription)
-            }
-            SwiftUI.Section(header: Text(verbatim: "Available Branches")) {
-                ForEach(experiment.branches, id: \.slug) { branch in
-                    HStack {
-                        Text(verbatim: branch.slug)
-                        Spacer()
-                        Text(verbatim: "\(branch.ratio)")
-                    }
-                }
-            }
-            SwiftUI.Section {
-                Picker(selection: $selectedBranchSlug, label: Text(verbatim: "Active Branch")) {
-                    ForEach(pickerBranches, id: \.self) { branch in
-                        if branch == notEnrolledBranchSlug {
-                            Text(verbatim: "Not Enrolled")
-                        } else {
-                            Text(verbatim: branch)
-                        }
-                    }
-                }.onReceive(Just(selectedBranchSlug)) { newValue in
-                    if newValue != notEnrolledBranchSlug {
-                        if NimbusWrapper.shared.getEnrolledBranchSlug(forExperiment: experiment) != newValue {
-                            if let branch = experiment.branches.first(where: { $0.slug == newValue }) {
-                                NimbusWrapper.shared.optIn(toExperiment: experiment, withBranch: branch)
-                            }
-                        }
-                    } else {
-                        if NimbusWrapper.shared.getEnrolledBranchSlug(forExperiment: experiment) != nil {
-                            NimbusWrapper.shared.optOut(ofExperiment: experiment)
-                        }
-                    }
-                }
-            }
+            descriptionSection
+            availableBranchesSection
+            activeBranchSection
         }.navigationBarTitle(Text(verbatim: experiment.userFacingName))
+    }
+
+    private var descriptionSection: some View {
+        SwiftUI.Section {
+            Text(verbatim: experiment.userFacingDescription)
+        }
+    }
+
+    private var availableBranchesSection: some View {
+        SwiftUI.Section(header: Text(verbatim: "Available Branches")) {
+            ForEach(experiment.branches, id: \.slug) { branch in
+                HStack {
+                    Text(verbatim: branch.slug)
+                    Spacer()
+                    Text(verbatim: "\(branch.ratio)")
+                }
+            }
+        }
+    }
+
+    private var activeBranchSection: some View {
+        SwiftUI.Section {
+            Picker(selection: $selectedBranchSlug, label: Text(verbatim: "Active Branch")) {
+                ForEach(pickerBranches, id: \.self) { branch in
+                    if branch == notEnrolledBranchSlug {
+                        Text(verbatim: "Not Enrolled")
+                    } else {
+                        Text(verbatim: branch)
+                    }
+                }
+            }.onReceive(Just(selectedBranchSlug)) { newValue in
+                if newValue != notEnrolledBranchSlug {
+                    if NimbusWrapper.shared.getEnrolledBranchSlug(forExperiment: experiment) != newValue {
+                        if let branch = experiment.branches.first(where: { $0.slug == newValue }) {
+                            NimbusWrapper.shared.optIn(toExperiment: experiment, withBranch: branch)
+                        }
+                    }
+                } else {
+                    if NimbusWrapper.shared.getEnrolledBranchSlug(forExperiment: experiment) != nil {
+                        NimbusWrapper.shared.optOut(ofExperiment: experiment)
+                    }
+                }
+            }
+        }
     }
 }
 
