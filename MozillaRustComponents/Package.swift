e51af6a59d3318f95580351cc5f3be2fff3a121e
Sammy Khamis <skhamis@mozilla.com>
2025-07-01 14:31:28 -1000

    Add automation to update nightly app-servcies

--- MozillaRustComponents/Package.swift
+++ MozillaRustComponents/Package.swift
@@ -8,6 +8,7 @@ let url = "https://firefox-ci-tc.services.mozilla.com/api/index/v1/task/project.
 // Focus xcframework
 let focusChecksum = "6393d15bbb764a553add2c27e55f661dbe6f4763cfbf0f7b891db0803bd8d1c6"
 let focusUrl = "https://firefox-ci-tc.services.mozilla.com/api/index/v1/task/project.application-services.v2.swift.142.20250625050341/artifacts/public/build/FocusRustComponents.xcframework.zip"
+
 let package = Package(
     name: "MozillaRustComponentsSwift",
     platforms: [.iOS(.v15)],
@@ -24,6 +25,13 @@ let package = Package(
             dependencies: ["MozillaRustComponents", .product(name: "Glean", package: "glean-swift")],
             path: "Sources/MozillaRustComponentsWrapper"
         ),
+        .target(
+            name: "FocusMozillaAppServices",
+            dependencies: [
+                .target(name: "FocusRustComponents", condition: .when(platforms: [.iOS]))
+            ],
+            path: "Sources/FocusRustComponentsWrapper"
+        ),
         .binaryTarget(
             name: "MozillaRustComponents",
             //
@@ -37,6 +45,20 @@ let package = Package(
             // Note that you have to actually check it in and make a tag for it to work correctly.
             //
             //path: "./MozillaRustComponents.xcframework"
+        ),
+        .binaryTarget(
+            name: "FocusRustComponents",
+            //
+            // For release artifacts, reference the MozillaRustComponents as a URL with checksum.
+            // IMPORTANT: The checksum has to be on the line directly after the `url`
+            // this is important for our release script so that all values are updated correctly
+            url: focusUrl,
+            checksum: focusChecksum
+
+            // For local testing, you can point at an (unzipped) XCFramework that's part of the repo.
+            // Note that you have to actually check it in and make a tag for it to work correctly.
+            //
+            //path: "./FocusRustComponents.xcframework"
         ),
         // Tests
         .testTarget(
