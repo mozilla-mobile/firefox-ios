4051ef680a6eea02c3f37478624df5a15158e9e8
Winnie Teichmann <4530+thatswinnie@users.noreply.github.com>
2025-07-11 16:14:15 +0200

    Bugfix FXIOS-12499 [Toolbar] Shadow around address bar is cut off at the top and bottom (#27920)
    Update shadow even if toolbar translucency is off
    
    (cherry picked from commit 66eeaab65bfdb5d910b446bd5fe24b5c554c9695)

--- firefox-ios/Client/Frontend/Browser/BrowserViewController/Views/BrowserViewController.swift
+++ firefox-ios/Client/Frontend/Browser/BrowserViewController/Views/BrowserViewController.swift
@@ -512,7 +512,7 @@ class BrowserViewController: UIViewController,
         toolbar.setNeedsDisplay()
         searchBarView.updateConstraints()
         updateMicrosurveyConstraints()
-        updateBlurViews()
+        updateToolbarDisplay()
 
         let action = GeneralBrowserMiddlewareAction(
             scrollOffset: scrollController.contentOffset,
@@ -520,6 +520,21 @@ class BrowserViewController: UIViewController,
             windowUUID: windowUUID,
             actionType: GeneralBrowserMiddlewareActionType.toolbarPositionChanged)
         store.dispatchLegacy(action)
+    }
+
+    private func updateToolbarDisplay(scrollOffset: CGFloat? = nil) {
+        guard isToolbarRefactorEnabled else { return }
+
+        // move views to the front so the address toolbar shadow doesn't get clipped
+        if isBottomSearchBar {
+            overKeyboardContainer.bringSubviewToFront(addressToolbarContainer)
+            view.bringSubviewToFront(overKeyboardContainer)
+        } else {
+            header.bringSubviewToFront(addressToolbarContainer)
+            view.bringSubviewToFront(header)
+        }
+
+        updateBlurViews(scrollOffset: scrollOffset)
     }
 
     private func updateBlurViews(scrollOffset: CGFloat? = nil) {
@@ -547,18 +562,10 @@ class BrowserViewController: UIViewController,
             let isFxHomeTab = tabManager.selectedTab?.isFxHomeTab ?? false
             let offset = scrollOffset ?? statusBarOverlay.scrollOffset
             topBlurView.alpha = isFxHomeTab ? offset : 1
-
-            // move views to the front so the address toolbar shadow doesn't get clipped
-            overKeyboardContainer.bringSubviewToFront(addressToolbarContainer)
-            view.bringSubviewToFront(overKeyboardContainer)
         } else {
             header.isClearBackground = enableBlur
             overKeyboardContainer.isClearBackground = false
             topBlurView.alpha = 1
-
-            // move views to the front so the address toolbar shadow doesn't get clipped
-            header.bringSubviewToFront(addressToolbarContainer)
-            view.bringSubviewToFront(header)
         }
 
         bottomContainer.isClearBackground = showNavToolbar && enableBlur
@@ -675,7 +682,8 @@ class BrowserViewController: UIViewController,
 
         header.setNeedsLayout()
         view.layoutSubviews()
-        updateBlurViews()
+
+        updateToolbarDisplay()
 
         if let tab = tabManager.selectedTab,
            let webView = tab.webView,
@@ -1120,7 +1128,7 @@ class BrowserViewController: UIViewController,
 
     @objc
     private func onReduceTransparencyStatusDidChange(_ notification: Notification) {
-        updateBlurViews()
+        updateToolbarDisplay()
 
         store.dispatchLegacy(
             ToolbarAction(
@@ -1373,7 +1381,7 @@ class BrowserViewController: UIViewController,
 
         // when toolbars are hidden/shown the mask on the content view that is used for
         // toolbar translucency needs to be updated
-        updateBlurViews()
+        updateToolbarDisplay()
     }
 
     func checkForJSAlerts() {
@@ -1753,7 +1761,7 @@ class BrowserViewController: UIViewController,
 
         if isPrivate && featureFlags.isFeatureEnabled(.feltPrivacySimplifiedUI, checking: .buildOnly) {
             browserDelegate?.showPrivateHomepage(overlayManager: overlayManager)
-            updateBlurViews()
+            updateToolbarDisplay()
             return
         }
 
@@ -1786,7 +1794,8 @@ class BrowserViewController: UIViewController,
             // to overlay the homepage.
             browserDelegate?.setHomepageVisibility(isVisible: true)
         }
-        updateBlurViews()
+
+        updateToolbarDisplay()
     }
 
     func showEmbeddedWebview() {
@@ -1811,7 +1820,7 @@ class BrowserViewController: UIViewController,
         }
 
         browserDelegate?.show(webView: webView)
-        updateBlurViews()
+        updateToolbarDisplay()
     }
 
     // MARK: - Document Loading
@@ -4200,7 +4209,7 @@ extension BrowserViewController: HomePanelDelegate {
 
     // MARK: - BrowserStatusBarScrollDelegate
     func homepageScrollViewDidScroll(scrollOffset: CGFloat) {
-        updateBlurViews(scrollOffset: scrollOffset)
+        updateToolbarDisplay(scrollOffset: scrollOffset)
     }
 }
 
@@ -4683,7 +4692,7 @@ extension BrowserViewController: KeyboardHelperDelegate {
                 self.bottomContentStackView.layoutIfNeeded()
             })
 
-        updateBlurViews()
+        updateToolbarDisplay()
     }
 
     func keyboardHelper(_ keyboardHelper: KeyboardHelper, keyboardWillHideWithState state: KeyboardState) {
@@ -4699,7 +4708,7 @@ extension BrowserViewController: KeyboardHelperDelegate {
             })
 
         cancelEditingMode()
-        updateBlurViews()
+        updateToolbarDisplay()
     }
 
     func keyboardHelper(_ keyboardHelper: KeyboardHelper, keyboardDidHideWithState state: KeyboardState) {
