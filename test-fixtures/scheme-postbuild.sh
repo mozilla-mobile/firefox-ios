bd7d27107959ebf7d7355953ef87d3707a72b073
isabelrios <isabelrios@gmail.com>
2025-07-15 10:37:05 +0200

    debug

--- test-fixtures/scheme-postbuild.sh
+++ test-fixtures/scheme-postbuild.sh
@@ -8,34 +8,32 @@
 #
 
 #!/bin/bash
-exec 1>&2  # redirect stdout to stderr so Bitrise shows it
-
-echo "🏗️ SCRIPT STARTING"
-echo "🔍 Script path: $0"
-echo "📂 Current dir: $(pwd)"
+echo "SCRIPT STARTING"
+echo "Script path: $0"
+echo "Current dir: $(pwd)"
 
 echo "••• Preparing test-fixtures directory for UI tests •••"
 
-SCRIPT_PATH="${BASH_SOURCE[0]:-${(%):-%N}}"
-echo $SCRIPT_PATH
-SCRIPT_DIR="$(cd "$(dirname "$SCRIPT_PATH")" && pwd)"
-echo $SCRIPT_DIR
-SRCROOT="$(cd "$SCRIPT_DIR/.." && pwd)"
-echo $SRCROOT
+# SCRIPT_PATH="${BASH_SOURCE[0]:-${(%):-%N}}"
+# echo $SCRIPT_PATH
+# SCRIPT_DIR="$(cd "$(dirname "$SCRIPT_PATH")" && pwd)"
+# echo $SCRIPT_DIR
+# SRCROOT="$(cd "$SCRIPT_DIR/.." && pwd)"
+# echo $SRCROOT
 
-fixtures="$SRCROOT/test-fixtures"
-echo $fixtures
-[[ -d "$fixtures" ]] || {
-  echo "Fixtures directory not found at: $fixtures"
-  exit 1
-}
+# fixtures="$SRCR/test-fixtures"
+# echo $fixtures
+# [[ -d "$fixtures" ]] || {
+#   echo "Fixtures directory not found at: $fixtures"
+#   exit 1
+# }
 
-# Prepare a temp location inside TMPDIR for the app to read at runtime
-runtime_fixtures_path="$TMPDIR/test-fixtures"
-echo "Copying test fixtures to runtime-accessible path: $runtime_fixtures_path"
-mkdir -p "$runtime_fixtures_path"
+# # Prepare a temp location inside TMPDIR for the app to read at runtime
+# runtime_fixtures_path="$TMPD/test-fixtures"
+# echo "Copying test fixtures to runtime-accessible path: $runtime_fixtures_path"
+# mkdir -p "$runtime_fixtures_path"
 
-# Perform the copy
-rsync -zvrt --delete --update "$fixtures/" "$runtime_fixtures_path"
+# # Perform the copy
+# rsync -zvrt --delete --update "$fixtures/" "$runtime_fixtures_path"
 
-echo "Fixtures prepared successfully"
+# echo "Fixtures prepared successfully"
