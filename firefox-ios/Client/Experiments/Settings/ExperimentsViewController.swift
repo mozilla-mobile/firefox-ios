29a24b1a36a90a54158b6516cc31e3c92dc2b36f
ih-codes <ihugel@mozilla.com>
2025-07-18 09:58:38 -0600

    Update observer in Experiments VC.

--- firefox-ios/Client/Experiments/Settings/ExperimentsViewController.swift
+++ firefox-ios/Client/Experiments/Settings/ExperimentsViewController.swift
@@ -23,8 +23,19 @@ class ExperimentsViewController: UIViewController {
             self.showSettings()
         }
 
-        NotificationCenter.default.addObserver(forName: .nimbusExperimentsApplied, object: nil, queue: .main) { _ in
-            self.onExperimentsApplied()
+        NotificationCenter.default.addObserver(
+            forName: .nimbusExperimentsApplied,
+            object: nil,
+            queue: .main
+        ) { _ in
+            guard Thread.isMainThread else {
+                assertionFailure("This must be called main thread")
+                return
+            }
+
+            MainActor.assumeIsolated {
+                self.onExperimentsApplied()
+            }
         }
     }
 
