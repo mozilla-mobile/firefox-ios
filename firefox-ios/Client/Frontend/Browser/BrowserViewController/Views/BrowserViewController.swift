cd3005db28f87c583977ebfeff885a4aeca43236
Raluca Iordan <ralucaiordan@icloud.com>
2025-07-18 09:59:11 +0300

    solved presenting issue

--- firefox-ios/Client/Frontend/Browser/BrowserViewController/Views/BrowserViewController.swift
+++ firefox-ios/Client/Frontend/Browser/BrowserViewController/Views/BrowserViewController.swift
@@ -1353,7 +1353,6 @@ class BrowserViewController: UIViewController,
             browserDelegate?.browserHasLoaded()
         }
         AppEventQueue.signal(event: .browserIsReady)
-        presentToUBottomSheet()
     }
 
     func willNavigateAway(from tab: Tab?, completion: (() -> Void)? = nil) {
@@ -1935,25 +1934,6 @@ class BrowserViewController: UIViewController,
         self.microsurvey = nil
         updateBarBordersForMicrosurvey()
         updateViewConstraints()
-    }
-    
-    private func presentToUBottomSheet() {
-        guard ToUManager.shared.shouldShow() else { return }
-
-        var viewModel = ToUBottomSheetViewModel()
-        viewModel.onAccept = {
-            ToUManager.shared.markAccepted()
-        }
-        viewModel.onNotNow = {
-            ToUManager.shared.markDismissed()
-        }
-
-        let touVC = ToUBottomSheetViewController(
-            viewModel: viewModel,
-            windowUUID: self.windowUUID
-        )
-
-        present(touVC, animated: true)
     }
     
     
