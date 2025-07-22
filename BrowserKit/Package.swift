b72d178bf341a8d95d8f7614e510e7aa2cf107e4
Issam Mani <issamouu69@gmail.com>
2025-07-23 01:22:15 +0200

    chore: bump macOS to v12

--- BrowserKit/Package.swift
+++ BrowserKit/Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     name: "BrowserKit",
     platforms: [
         .iOS(.v15),
-        .macOS(.v10_15)
+        .macOS(.v12)
     ],
     products: [
         .library(name: "Shared",
