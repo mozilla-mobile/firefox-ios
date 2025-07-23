f438e6380c4679fb6f6e8ab70fdee377a81fcb41
Carson Ramsden <carsonramsden@gmail.com>
2025-07-23 07:54:19 -0600

    clean up 16.2 warnings

--- BrowserKit/Sources/Common/Utilities/Notifiable.swift
+++ BrowserKit/Sources/Common/Utilities/Notifiable.swift
@@ -43,6 +43,7 @@ extension NotificationCenter: NotificationProtocol {
 @objc
 public protocol Notifiable {
     var notificationCenter: NotificationProtocol { get }
+    @MainActor
     func handleNotifications(_ notification: Notification)
 }
 
