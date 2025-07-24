6809e05841dcb668fec1798534826c4b6bf2ae06
Laurie Marceau <lmarceau@mozilla.com>
2025-07-24 10:39:00 -0400

    Adjust microsurvey and message card middlewares

--- firefox-ios/Client/Frontend/Home/Homepage Rebuild/MessageCard/MessageCardMiddleware.swift
+++ firefox-ios/Client/Frontend/Home/Homepage Rebuild/MessageCard/MessageCardMiddleware.swift
@@ -12,6 +12,7 @@ struct MessageCardConfiguration: Hashable {
     let buttonLabel: String?
 }
 
+@MainActor
 final class MessageCardMiddleware {
     private var message: GleanPlumbMessage?
     private let messagingManager: GleanPlumbMessageManagerProtocol
@@ -20,7 +21,6 @@ final class MessageCardMiddleware {
         self.messagingManager = messagingManager
     }
 
-    // TODO: FXIOS-12831 We need this middleware isolated to the main actor (due to `onMessagePressed` call)
     lazy var messageCardProvider: Middleware<AppState> = { state, action in
         let windowUUID = action.windowUUID
 
@@ -60,6 +60,6 @@ final class MessageCardMiddleware {
             windowUUID: windowUUID,
             actionType: MessageCardMiddlewareActionType.initialize
         )
-        store.dispatchLegacy(newAction)
+        store.dispatch(newAction)
     }
 }
