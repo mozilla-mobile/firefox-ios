71bb6b7707bd25a8edda421dd8eb56df3a05721b
Winnie Teichmann <4530+thatswinnie@users.noreply.github.com>
2025-07-25 15:24:19 -0400

    Bugfix FXIOS-12895 [Toolbar] Keyboard Closes Unintentionally When Using Search Suggestions in Address Bar (#28237)
    * Keep keyboard up when user taps append on search term
    
    * Update test
    
    (cherry picked from commit 6046a417bcc90e7f06045a3a49dc9e2987d9f83f)

--- firefox-ios/Client/Frontend/Browser/Toolbars/Redux/AddressBarState.swift
+++ firefox-ios/Client/Frontend/Browser/Toolbars/Redux/AddressBarState.swift
@@ -619,7 +619,7 @@ struct AddressBarState: StateType, Equatable {
             lockIconNeedsTheming: state.lockIconNeedsTheming,
             safeListedURLImageName: state.safeListedURLImageName,
             isEditing: true,
-            shouldShowKeyboard: false,
+            shouldShowKeyboard: true,
             shouldSelectSearchTerm: false,
             isLoading: state.isLoading,
             readerModeState: state.readerModeState,
