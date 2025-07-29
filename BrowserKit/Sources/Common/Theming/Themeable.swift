5e3d34acb13858021867fabbc400204e16b6f76d
ih-codes <ihugel@mozilla.com>
2025-07-29 16:33:19 -0600

    Update our Themeable, ThemeApplicable, and Notifiable APIs one more time to use a Combine solution for theme notification observation.

--- BrowserKit/Sources/Common/Theming/Themeable.swift
+++ BrowserKit/Sources/Common/Theming/Themeable.swift
@@ -3,21 +3,43 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
 import UIKit
+import Combine
 
-public protocol Themeable: ThemeUUIDIdentifiable, AnyObject {
-    var shouldUsePrivateOverride: Bool { get }
+public protocol Themeable: ThemeUUIDIdentifiable {
+    /// Whether we should override private theme whether we want to force the theme to be private or not private. Going
+    /// against the basic theme set up.
+    nonisolated var shouldUsePrivateOverride: Bool { get }
+
+    /// Determines if we want views to be in private theme or not.
+    @MainActor
     var shouldBeInPrivateTheme: Bool { get }
-    var themeManager: ThemeManager { get }
-    var themeObserver: NSObjectProtocol? { get set }
-    var notificationCenter: NotificationProtocol { get set }
 
-    func listenForThemeChange(_ subview: UIView)
+    @MainActor
+    var themeManager: ThemeManager { get }
+
+    // MARK: - Theme observation
+
+    /// Holds a reference to the theme changes publisher. Once deallocated, the NotificationCenter observer is removed.
+    /// We use an `Any?` type here because we don't want to force a Combine import to access `Cancellable` in other files.
+    @MainActor
+    var themeListenerCancellable: Any? { get set }
+
+    // FIXME: change name of param? / Use UIViewController's parent view instead?? - no, bc of TabScrollController not
+    /// Start listening for theme changes. Should only be called once in the view lifecycle (e.g. in `viewDidLoad()`).
+    @MainActor
+    func listenForThemeChanges(_ subview: UIView, withNotificationCenter notificationCenter: NotificationProtocol)
+
+    /// Applies the current theme to the current
+    /// Called when the theme updates for the registered view and all its subviews.
+    @MainActor
     func applyTheme()
+    // FIXME: Pass in theme?
 }
 
 /// Protocol for views to identify which iPad window (UUID) the view is associated with.
 /// By default, all UIViews conform to this automatically. See: UIView+ThemeUUIDIdentifiable.swift.
 public protocol ThemeUUIDIdentifiable: AnyObject {
+    @MainActor
     var currentWindowUUID: WindowUUID? { get }
 }
 
@@ -25,33 +47,43 @@ public protocol ThemeUUIDIdentifiable: AnyObject {
 /// window UUID. This is used by our convenience extensions to allow UIViews to automatically detect their
 /// associated iPad window UUID, even if the view is not immediately installed in a window or view hierarchy.
 public protocol InjectedThemeUUIDIdentifiable: AnyObject {
+    @MainActor
     var windowUUID: WindowUUID { get }
 }
 
 extension Themeable {
-    // Whether we should override private theme whether we want to
-    // force the theme to be private or not private. Going against the basic theme set up
     public var shouldUsePrivateOverride: Bool { return false }
-
-    // Determines if we want views to be in private theme or not
     public var shouldBeInPrivateTheme: Bool { return false }
 
-    public func listenForThemeChange(_ subview: UIView) {
-        let mainQueue = OperationQueue.main
-        themeObserver = notificationCenter.addObserver(name: .ThemeDidChange,
-                                                       queue: mainQueue) { [weak self] _ in
-            self?.applyTheme()
-            self?.updateThemeApplicableSubviews(subview, for: self?.currentWindowUUID)
-        }
+    @MainActor // FIXME: Do we need subview here or can we conform to UIViewController?
+    public func listenForThemeChanges(_ subview: UIView, withNotificationCenter notificationCenter: NotificationProtocol) {
+        themeListenerCancellable = notificationCenter
+            .publisher(for: .ThemeDidChange, object: nil)
+            // NOTE: In case theme updates are ever posted on a background thread, we **MUST** ensure the main queue here.
+            // The `@MainActor` isolation is not enough to prevent crashes at the call site where a notification is posted.
+            .receive(on: DispatchQueue.main)
+            .sink(receiveValue: { [weak self, weak subview] notification in
+                guard let self, let subview else  { return }
+
+                print("Theme update for \(String(describing: self))")
+                self.applyTheme()
+
+            // FIXME: Why do we have to call this on subviews? Doesn't applyTheme do the entire UIViewController hierarchy?
+                self.updateThemeApplicableSubviews(subview, for: self.currentWindowUUID)
+            })
     }
 
+    // FIXME: Why and where do we need this?
     /// Updates subviews of the `Themeable` view, which can specify whether it wants to use the
     /// base theme via `getCurrentTheme` or override the private mode theme via `resolvedTheme`
-    public func updateThemeApplicableSubviews(_ view: UIView, for window: WindowUUID?) {
+    @MainActor
+    private func updateThemeApplicableSubviews(_ view: UIView, for window: WindowUUID?) {
+        assert(Thread.isMainThread)
+
         guard let uuid = (view as? ThemeUUIDIdentifiable)?.currentWindowUUID ?? window else { return }
         assert(uuid != .unavailable, "Theme applicable view has `unavailable` window UUID. Unexpected.")
-        let theme: Theme
 
+        let theme: Theme
         if shouldUsePrivateOverride {
             theme = themeManager.resolvedTheme(with: shouldBeInPrivateTheme)
         } else {
@@ -62,6 +94,7 @@ extension Themeable {
         themeViews.forEach { $0.applyTheme(theme: theme) }
     }
 
+    @MainActor
     public func getAllSubviews<T>(for view: UIView, ofType type: T.Type) -> [T] {
         var secondLevelSubviews = [T]()
         let firstLevelSubviews: [T] = view.subviews.compactMap { childView in
