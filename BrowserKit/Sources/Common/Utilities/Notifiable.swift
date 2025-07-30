b5a1b2a595d59f1b0ce65369e2e1e5fe038c780b
ih-codes <ihugel@mozilla.com>
2025-07-30 12:47:14 -0600

    Update our Themeable and Notifiable APIs documentation.

--- BrowserKit/Sources/Common/Utilities/Notifiable.swift
+++ BrowserKit/Sources/Common/Utilities/Notifiable.swift
@@ -9,16 +9,27 @@ public protocol NotificationProtocol: Sendable {
     nonisolated func post(name: NSNotification.Name, withObject object: Any?, withUserInfo info: Any?)
 
     /// Adds an observer.
-    /// **NOTE**: Do not call this method directly. Use the `Notifiable` helper methods instead.
+    /// **NOTE**: Do not call this method directly. Use the `Notifiable` helper method `stopObservingNotifications` instead.
     ///
     /// Our `Notifiable` protocol relies on this method to provide default implementations.
     nonisolated func addObserver(_ observer: Any, selector aSelector: Selector, name aName: NSNotification.Name?, object anObject: Any?)
 
     /// Removes an observer.
-    /// **NOTE**: Do not call this method directly. Use the `Notifiable` helper methods instead.
+    /// **NOTE**: Do not call this method directly. Use the `Notifiable` helper method `stopObservingNotifications` instead.
     ///
     /// Our `Notifiable` protocol relies on this method to provide default implementations.
     nonisolated func removeObserver(_ observer: Any, name aName: NSNotification.Name?, object anObject: Any?)
+    
+    /// Removes an observer.
+    ///
+    /// Our `Notifiable` protocol relies on this method to provide default implementations.
+    nonisolated func removeObserver(_ observer: Any)
+    
+    /// Combine-based variant for listening to posted notifications from `NSNotificationCenter`.
+    ///
+    /// Our `Themeable` protocol relies on this method to provide default implementations that automatically clean up
+    /// observers without having to remove them in the `deinit`, or worry about `nonisolated` handler methods.
+    func publisher(for name: Notification.Name, object: AnyObject?) -> NotificationCenter.Publisher
 }
 
 /// Provides default implementation for `NotificationCenter` conformance to `NotificationProtocol`.
