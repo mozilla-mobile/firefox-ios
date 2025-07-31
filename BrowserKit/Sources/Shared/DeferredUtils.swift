affb7fa5b0986fe159ec29390bf955e139739fbd
lmarceau <lmarceau@mozilla.com>
2025-07-31 09:40:39 -0400

    Remove FXIOS-12961 [Deferred] Clean up Deferred code that isn't used (#28275)
    Clean up, that code isn't used

--- BrowserKit/Sources/Shared/DeferredUtils.swift
+++ BrowserKit/Sources/Shared/DeferredUtils.swift
@@ -54,13 +54,6 @@ public func >>> <T>(x: Deferred<Maybe<T>>, f: @escaping () -> Void) {
     }
 }
 
-/**
-* Returns a thunk that return a Deferred that resolves to the provided value.
-*/
-public func always<T>(_ t: T) -> () -> Deferred<Maybe<T>> {
-    return { deferMaybe(t) }
-}
-
 public func deferMaybe<T>(_ s: T) -> Deferred<Maybe<T>> {
     return Deferred(value: Maybe(success: s))
 }
@@ -80,16 +73,6 @@ public typealias Success = Deferred<Maybe<Void>>
 @discardableResult
 public func succeed() -> Success {
     return deferMaybe(())
-}
-
-/**
- * Return a single Deferred that represents the sequential chaining
- * of f over the provided items.
- */
-public func walk<T>(_ items: [T], f: @escaping (T) -> Success) -> Success {
-    return items.reduce(succeed()) { success, item -> Success in
-        success >>> { f(item) }
-    }
 }
 
 /**
@@ -137,43 +120,6 @@ public func accumulate<T>(_ thunks: [() -> Deferred<Maybe<T>>]) -> Deferred<Mayb
 }
 
 /**
- * Take a function and turn it into a side-effect that can appear
- * in a chain of async operations without producing its own value.
- */
-public func effect<T, U>(_ f: @escaping (T) -> U) -> (T) -> Deferred<Maybe<T>> {
-    return { t in
-        _ = f(t)
-        return deferMaybe(t)
-    }
-}
-// Prevents "Cannot convert call result type '(_) -> Deferred<Maybe<_>>' to expected type '() -> Deferred<Maybe<Void>>"
-// SE-0029 introduced this behaviour
-// https://github.com/apple/swift-evolution/blob/master/proposals/0029-remove-implicit-tuple-splat.md
-public func effect(_ f: @escaping (Swift.Void) -> Void) -> () -> Success {
-    return {
-        f(())
-        return succeed()
-    }
-}
-
-/**
- * Return a single Deferred that represents the sequential chaining of
- * f over the provided items, with the return value chained through.
- */
-public func walk<T, U, S: Sequence>(
-    _ items: S,
-    start: Deferred<Maybe<U>>,
-    f: @escaping (T, U) -> Deferred<Maybe<U>>
-) -> Deferred<Maybe<U>> where S.Iterator.Element == T {
-    let fs = items.map { item in
-        return { val in
-            f(item, val)
-        }
-    }
-    return fs.reduce(start, >>==)
-}
-
-/**
  * Like `all`, but doesn't accrue individual values.
  */
 extension Array where Element: Success {
@@ -195,29 +141,4 @@ public func chainDeferred<T, U>(_ a: Deferred<Maybe<T>>, f: @escaping (T) -> Def
         }
         return Deferred(value: Maybe<U>(failure: res.failureValue!))
     }
-}
-
-public func chainResult<T, U>(_ a: Deferred<Maybe<T>>, f: @escaping (T) -> Maybe<U>) -> Deferred<Maybe<U>> {
-    return a.map { res in
-        if let v = res.successValue {
-            return f(v)
-        }
-        return Maybe<U>(failure: res.failureValue!)
-    }
-}
-
-public func chain<T, U>(_ a: Deferred<Maybe<T>>, f: @escaping (T) -> U) -> Deferred<Maybe<U>> {
-    return chainResult(a, f: { Maybe<U>(success: f($0)) })
-}
-
-/// Defer-ifies a block to an async dispatch queue.
-public func deferDispatchAsync<T>(_ queue: DispatchQueue, f: @escaping () -> Deferred<Maybe<T>>) -> Deferred<Maybe<T>> {
-    let deferred = Deferred<Maybe<T>>()
-    queue.async(execute: {
-        f().upon { result in
-            deferred.fill(result)
-        }
-    })
-
-    return deferred
 }
