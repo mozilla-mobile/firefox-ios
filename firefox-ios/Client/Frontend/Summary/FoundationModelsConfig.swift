73dca0b8c491d87b5e49e871b7031ab0f7844c7d
Issam Mani <issamouu69@gmail.com>
2025-07-31 17:11:22 +0200

    feat: add config wrapper to fetch config from vars

---
+++ firefox-ios/Client/Frontend/Summary/FoundationModelsConfig.swift
@@ -0,0 +1,13 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import Foundation
+
+struct FoundationModelsConfig {
+    /// `maxWords` limits the number of words in the input text.
+    /// 3000 words was computed by assuming an average of 1.3 tokens per word.
+    /// Given the foundation model’s 4,096 token context window, this brings us to approx. 3000 words.
+    static let maxWords = 3_000
+    static let prompt = SummarizerPrompts.appleSummarizerPrompt
+}
