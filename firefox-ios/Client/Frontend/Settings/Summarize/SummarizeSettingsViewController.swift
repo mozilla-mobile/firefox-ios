0a1a9bc52e88361723ad321be55a9780e06a0524
Cyndi Chin <cgc95@cornell.edu>
2025-08-01 10:07:49 -0400

    Add FXIOS-12993 [Shake to Summarize] shake gesture setting

--- firefox-ios/Client/Frontend/Settings/Summarize/SummarizeSettingsViewController.swift
+++ firefox-ios/Client/Frontend/Settings/Summarize/SummarizeSettingsViewController.swift
@@ -23,10 +23,18 @@ final class SummarizeSettingsViewController: SettingsTableViewController, Featur
     }
 
     override func generateSettings() -> [SettingSection] {
-        return [summarizeContentSettingSection]
+        let summarizeContentEnabled = prefs.boolForKey(PrefsKeys.Summarizer.summarizeContentFeature) ?? true
+        let shakeFeatureFlag = SummarizerNimbusUtils.shared.isShakeGestureFeatureFlagEnabled()
+
+        // Shows and hides the gesture section
+        // based on the summarize feature being enabled
+        // and shake gesture feature flag is true
+        guard summarizeContentEnabled && shakeFeatureFlag else { return [summarizeContentSection] }
+
+        return [summarizeContentSection, gesturesSection]
     }
 
-    private var summarizeContentSettingSection: SettingSection {
+    private var summarizeContentSection: SettingSection {
         // TODO: FXIOS-12992 - Add Strings when ready
         let titleText = "Enable Summarize Content"
         let summarizeContentSetting = BoolSetting(
@@ -35,7 +43,30 @@ final class SummarizeSettingsViewController: SettingsTableViewController, Featur
             prefKey: PrefsKeys.Summarizer.summarizeContentFeature,
             defaultValue: true,
             titleText: titleText
-        )
+        ) { [weak self] isOn in
+            guard let self else { return }
+            // Reload sections to hide and show gesture section
+            // depending if summarize content setting toggle is On or Off
+            self.settings = self.generateSettings()
+            self.tableView.reloadData()
+        }
         return SettingSection(title: nil, children: [summarizeContentSetting])
+    }
+
+    private var gesturesSection: SettingSection {
+        // TODO: FXIOS-12992 - Add Strings when ready
+        let titleText = "Enable Shake Gesture"
+        let sectionTitle = "Gestures"
+        let shakeGestureSetting = BoolSetting(
+            prefs: prefs,
+            theme: theme,
+            prefKey: PrefsKeys.Summarizer.shakeGestureEnabled,
+            defaultValue: true,
+            titleText: titleText
+        )
+        return SettingSection(
+            title: NSAttributedString(string: sectionTitle),
+            children: [shakeGestureSetting]
+        )
     }
 }
