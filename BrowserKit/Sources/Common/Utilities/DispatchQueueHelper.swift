f7a50f4e2d2a48aa9b628204ccb79649e63c264c
ih-codes <ihugel@mozilla.com>
2025-08-07 11:54:19 -0600

    Migrate `ensureMainThread` utility methods to Common package and fixing `Sendable` warnings.

--- BrowserKit/Sources/Common/Utilities/DispatchQueueHelper.swift
+++ BrowserKit/Sources/Common/Utilities/DispatchQueueHelper.swift
@@ -7,7 +7,7 @@ import Foundation
 // Only push the task async if we are not already on the main thread.
 // Unless you want another event to fire before your work happens.
 // This is better than using DispatchQueue.main.async to ensure main thread
-func ensureMainThread(execute work: @escaping @MainActor @convention(block) () -> Swift.Void) {
+public func ensureMainThread(execute work: @escaping @MainActor @Sendable @convention(block) () -> Swift.Void) {
     if Thread.isMainThread {
         MainActor.assumeIsolated {
             work()
@@ -19,7 +19,7 @@ func ensureMainThread(execute work: @escaping @MainActor @convention(block) () -
     }
 }
 
-func ensureMainThread<T>(execute work: @escaping @MainActor () -> T) {
+public func ensureMainThread<T>(execute work: @escaping @MainActor @Sendable () -> T) {
     if Thread.isMainThread {
         MainActor.assumeIsolated {
             _ = work()
