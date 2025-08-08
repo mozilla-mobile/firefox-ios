2417450f883a4193f5c196e7e9805c3de484fc93
Cyndi Chin <cgc95@cornell.edu>
2025-08-08 12:02:54 -0400

    Bugfix FXIOS-13092 [StartAtHome] tab manager not found

--- firefox-ios/Client/Frontend/Browser/BrowserViewController/Views/BrowserViewController.swift
+++ firefox-ios/Client/Frontend/Browser/BrowserViewController/Views/BrowserViewController.swift
@@ -857,6 +857,7 @@ class BrowserViewController: UIViewController,
     // MARK: - Start At Home
     private func dispatchStartAtHomeAction() {
         let startAtHomeAction = StartAtHomeAction(
+            tabManager: tabManager,
             windowUUID: windowUUID,
             actionType: StartAtHomeActionType.didBrowserBecomeActive
         )
