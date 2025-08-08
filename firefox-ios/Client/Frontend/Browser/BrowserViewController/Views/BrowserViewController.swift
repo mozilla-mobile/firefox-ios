6def305d13f60dd5dfeea31477c3ce2308eedd70
Matt Reagan <mreagan@mozilla.com>
2025-08-08 10:39:41 -0700

    Additional logs for missing tab manager issues

--- firefox-ios/Client/Frontend/Browser/BrowserViewController/Views/BrowserViewController.swift
+++ firefox-ios/Client/Frontend/Browser/BrowserViewController/Views/BrowserViewController.swift
@@ -463,7 +463,7 @@ class BrowserViewController: UIViewController,
     }
 
     deinit {
-        logger.log("BVC deallocating", level: .info, category: .lifecycle)
+        logger.log("BVC deallocating (window: \(windowUUID))", level: .info, category: .lifecycle)
         unsubscribeFromRedux()
         // TODO: FXIOS-13097 This is a work around until we can leverage isolated deinits
         // KVO observation removal directly requires self so we can not use the apple
