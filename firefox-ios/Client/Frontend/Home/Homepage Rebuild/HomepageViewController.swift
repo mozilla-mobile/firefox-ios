e9015854b7632e63ccdd85b6f0beca36b82b2dd8
Carson Ramsden <carsonramsden@gmail.com>
2025-08-11 13:15:38 -0600

    Refactor FXIOS-12796 [Swift 6] Resolve remaining deinit warnings (#28545)
    * resolve remaining deinit warnings
    
    * resolve merge conflicts

--- firefox-ios/Client/Frontend/Home/Homepage Rebuild/HomepageViewController.swift
+++ firefox-ios/Client/Frontend/Home/Homepage Rebuild/HomepageViewController.swift
@@ -322,7 +322,7 @@ final class HomepageViewController: UIViewController,
         }
     }
 
-    func unsubscribeFromRedux() {
+    nonisolated func unsubscribeFromRedux() {
         let action = ScreenAction(
             windowUUID: windowUUID,
             actionType: ScreenActionType.closeScreen,
