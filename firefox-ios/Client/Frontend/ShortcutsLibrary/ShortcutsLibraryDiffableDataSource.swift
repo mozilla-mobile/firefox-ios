d5161f394b01f91464f023d32b021f8f9904f648
Foxbolts <Foxbolts3@gmail.com>
2025-08-12 12:00:55 -0400

    configure collection view cell

--- firefox-ios/Client/Frontend/ShortcutsLibrary/ShortcutsLibraryDiffableDataSource.swift
+++ firefox-ios/Client/Frontend/ShortcutsLibrary/ShortcutsLibraryDiffableDataSource.swift
@@ -2,7 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import Foundation
+import Common
 
 typealias ShortcutsLibrarySection = ShortcutsLibraryDiffableDataSource.Section
 typealias ShortcutsLibraryItem = ShortcutsLibraryDiffableDataSource.Item
@@ -15,6 +15,12 @@ final class ShortcutsLibraryDiffableDataSource:
 
     enum Item: Hashable {
         case shortcut(TopSiteConfiguration)
+
+        static var cellTypes: [ReusableCell.Type] {
+            return [
+                TopSiteCell.self,
+            ]
+        }
     }
 
     func updateSnapshot(state: ShortcutsLibraryState) {
