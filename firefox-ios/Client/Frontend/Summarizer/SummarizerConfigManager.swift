878eef15e8b827ae15d1134e4f89bd26dda9aa1a
Issam Mani <issamouu69@gmail.com>
2025-08-14 01:33:26 +0200

    feat: add config manager

---
+++ firefox-ios/Client/Frontend/Summarizer/SummarizerConfigManager.swift
@@ -0,0 +1,35 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import SummarizeKit
+
+/// A wrapper to Manage the configuration sources for the summarizer. 
+/// This class is responsible for loading and merging configurations from multiple sources.
+/// We should never access the sources directly.
+class SummarizerConfigManager {
+    /// The order of sources determines their priority.
+    /// User overrides take precedence, followed by remote config, and then defaults.
+    static let defaultSources: [SummarizerConfigSourceProtocol] = [
+        UserSummarizerConfigSource(),
+        RemoteSummarizerConfigSource(),
+        DefaultSummarizerConfigSource()
+    ]
+
+    private let sources: [SummarizerConfigSourceProtocol]
+
+    init(sources: [SummarizerConfigSourceProtocol] = SummarizerConfigManager.defaultSources) {
+        self.sources = sources
+    }
+
+    func getConfig(_ summarizer: SummarizerModel, contentType: SummarizationContentType) async -> SummarizerConfig {
+        var mergedConfig = SummarizerConfig(instructions: "", options: [:])
+        // Merge configs in reverse order (so higher priority overrides lower)
+        for source in sources.reversed() {
+            if let config = await source.load(summarizer, contentType: contentType) {
+                mergedConfig = config.merging(with: mergedConfig)
+            }
+        }
+        return mergedConfig
+    }
+}
