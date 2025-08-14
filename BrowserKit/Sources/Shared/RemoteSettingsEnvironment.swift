bcf9f647f19f3290e9ed4988f66ae905435fa09a
Issam Mani <issamouu69@gmail.com>
2025-08-14 15:23:42 +0200

    refactor: add RemoteSettingsEnvironment enum

---
+++ BrowserKit/Sources/Shared/RemoteSettingsEnvironment.swift
@@ -0,0 +1,21 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import enum MozillaAppServices.RemoteSettingsServer
+
+/// NOTE: It would much cleaner to use RemoteSettingsServer if it had a public initializer.
+/// TODO(FXIOS-xxxx): Add public initializer from rawValue to RemoteSettingsServer.
+public enum RemoteSettingsEnvironment: String {
+    case prod
+    case stage
+    case dev
+
+    public func toRemoteSettingsServer() -> RemoteSettingsServer {
+        switch self {
+        case .prod: return .prod
+        case .stage: return .stage
+        case .dev: return .dev
+        }
+    }
+}
