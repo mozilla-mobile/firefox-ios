6b83e64da92cc2c083f6b981785e10f5b71c7252
Ione Souza Junior <junior@ionixjunior.com.br>
2025-08-14 20:31:42 -0300

    Extract the creation of view menu into a new function

--- firefox-ios/Client/Helpers/MenuBuilderHelper.swift
+++ firefox-ios/Client/Helpers/MenuBuilderHelper.swift
@@ -276,4 +276,55 @@ class MenuBuilderHelper {
 
         return findMenu
     }
+
+    private func makeViewMenu() -> UIMenu {
+        var viewMenuChildren: [UIMenuElement] = [
+            UIKeyCommand(
+                title: .KeyboardShortcuts.ZoomIn,
+                action: #selector(BrowserViewController.zoomIn),
+                input: "+",
+                modifierFlags: .command,
+                discoverabilityTitle: .KeyboardShortcuts.ZoomIn
+            ),
+            UIKeyCommand(
+                title: .KeyboardShortcuts.ZoomOut,
+                action: #selector(BrowserViewController.zoomOut),
+                input: "-",
+                modifierFlags: .command,
+                discoverabilityTitle: .KeyboardShortcuts.ZoomOut
+            ),
+            UIKeyCommand(
+                title: .KeyboardShortcuts.ActualSize,
+                action: #selector(BrowserViewController.resetZoom),
+                input: "0",
+                modifierFlags: .command,
+                discoverabilityTitle: .KeyboardShortcuts.ActualSize
+            ),
+            UIKeyCommand(
+                title: .KeyboardShortcuts.ReloadPage,
+                action: #selector(BrowserViewController.reloadTabKeyCommand),
+                input: "r",
+                modifierFlags: .command,
+                discoverabilityTitle: .KeyboardShortcuts.ReloadPage
+            )
+        ]
+
+        // UIKeyCommand.f5 is only available since iOS 13.4 - Shortcut will only work from this version
+        viewMenuChildren.append(
+            UIKeyCommand(
+                title: .KeyboardShortcuts.ReloadWithoutCache,
+                action: #selector(BrowserViewController.reloadTabIgnoringCacheKeyCommand),
+                input: UIKeyCommand.f5,
+                modifierFlags: [.control],
+                discoverabilityTitle: .KeyboardShortcuts.ReloadWithoutCache
+            )
+        )
+
+        let viewMenu = UIMenu(options: .displayInline, children: viewMenuChildren)
+        viewMenu.children.forEach {
+            ($0 as? UIKeyCommand)?.wantsPriorityOverSystemBehavior = true
+        }
+
+        return viewMenu
+    }
 }
