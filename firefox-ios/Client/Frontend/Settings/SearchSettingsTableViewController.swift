6ebd7da081c8566c72fa8dcfe8522f4ce43fd530
Nishant Bhasin <nish.bhasin@gmail.com>
2025-08-14 14:24:32 -0400

    Apply suggestion from @nbhasin2

--- firefox-ios/Client/Frontend/Settings/SearchSettingsTableViewController.swift
+++ firefox-ios/Client/Frontend/Settings/SearchSettingsTableViewController.swift
@@ -337,7 +337,6 @@ final class SearchSettingsTableViewController: ThemedTableViewController, Featur
         }
 
         if #available(iOS 26.0, *) {
-            // So that the separator line goes all the way to the left edge.
             cell.separatorInset = UIEdgeInsets(top: 0, left: UX.horizontalMargin, bottom: 0, right: UX.horizontalMargin)
         } else {
             // So that the separator line goes all the way to the left edge.
