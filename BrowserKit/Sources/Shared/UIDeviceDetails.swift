13b6aea2b5bcb473b85819cd491f1b890fd075c6
ih-codes <ihugel@mozilla.com>
2025-08-15 14:22:33 -0600

    Update documentation.

--- BrowserKit/Sources/Shared/UIDeviceDetails.swift
+++ BrowserKit/Sources/Shared/UIDeviceDetails.swift
@@ -5,8 +5,8 @@
 import UIKit
 
 /// Contains static, unchanging information about the current `UIDevice`, like the model and system version.
-/// This makes it easier for code that runs on a background thread without an asynchronous context to use these values
-/// (e.g. to log additional information during background operations like networking).
+/// This makes it easier for code that runs on a background thread (without an asynchronous context) to use these values
+/// (e.g. to log additional information during background networking).
 ///
 /// **Never put values in here that might change during runtime.**
 struct UIDeviceDetails {
@@ -30,15 +30,13 @@ struct UIDeviceDetails {
 
     // MARK: Helper method
 
-    /// This nonisolated function will execute the `work` closure on the main thread return a value outside an asynchronous
-    /// or main actor context. If not called on the main thread, it will use `DispatchQueue.main.sync` to synchronously wait
-    /// for the `main` thread to be available, get the value on the main thread, and then return the value to the current
-    /// context without any suspension.
+    /// This nonisolated function will execute the `work` closure on the main thread to return a value outside an
+    /// asynchronous or main actor context. If called off the main thread, this method will use `DispatchQueue.main.sync`
+    /// to synchronously return a value without a suspension point.
     ///
-    /// This method should only be used to get unchanging (but main actor isolated) values like `UIDevice.current.model` on
-    /// first launch of the app. This is a workaround for code that Apple will probably mark `nonisolated` in the future.
+    /// This is a workaround to access unchanging values that Apple will probably mark `nonisolated` in the future.
     ///
-    /// Do not use this method to get the value of main actor isolated state which can change during runtime, such as the
+    /// **Do not** use this method to get the value of main actor-isolated state which can change during runtime, such as the
     /// device orientation or current system theme.
     /// - Parameter work: Work to execute to return a value for variables normally isolated to the main thread.
     /// - Returns: The value from `work`.
