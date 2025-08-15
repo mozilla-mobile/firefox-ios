6ef83a7ed2c8e731fa3a497de22cfebd0e350ec2
ih-codes <ihugel@mozilla.com>
2025-08-15 10:46:00 -0600

    Enable strict concurrency checking in the Redux BrowserKit package.

--- BrowserKit/Package.swift
+++ BrowserKit/Package.swift
@@ -126,7 +126,10 @@ let package = Package(
         .target(
             name: "Redux",
             dependencies: ["Common"],
-            swiftSettings: [.unsafeFlags(["-enable-testing"])]),
+            swiftSettings: [
+                .unsafeFlags(["-enable-testing"]),
+                .enableExperimentalFeature("StrictConcurrency")
+            ]),
         .testTarget(
             name: "ReduxTests",
             dependencies: ["Redux"]),
