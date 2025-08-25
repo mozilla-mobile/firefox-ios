3761df37b9a6eedb00705cbb6c399059cbd10c61
Raluca Iordan <ralucaiordan@icloud.com>
2025-08-25 12:02:02 +0300

    Add FXIOS-12151 [Terms of Use] Add telemetry for impression, accept, date and version

--- firefox-ios/Client/Frontend/Browser/TermsOfUse/TermsOfUseMiddleware.swift
+++ firefox-ios/Client/Frontend/Browser/TermsOfUse/TermsOfUseMiddleware.swift
@@ -4,10 +4,14 @@
 import Common
 import Redux
 import Shared
+import Glean
 
 // TODO: FXIOS-12947 - Add tests for TermsOfUse Feature
 @MainActor
 class TermsOfUseMiddleware {
+    private let termsOfUseVersion: Int64 = 4
+    private let termsOfUseSurface = "bottom_sheet"
+
     private let prefs: Prefs
     private let logger: Logger
 
@@ -18,6 +22,10 @@ class TermsOfUseMiddleware {
     }
 
     lazy var termsOfUseProvider: Middleware<AppState> = { _, action in
+        self.handleTermsOfUseAction(action)
+    }
+
+    private func handleTermsOfUseAction(_ action: Action) {
         // TODO: FXIOS-12557 We assume that we are isolated to the Main Actor
         // because we dispatch to the main thread in the store. We will want
         // to also isolate that to the @MainActor to remove this.
@@ -36,14 +44,38 @@ class TermsOfUseMiddleware {
 
             switch type {
             case TermsOfUseActionType.markAccepted:
-                self.prefs.setBool(true, forKey: PrefsKeys.TermsOfUseAccepted)
-
+                self.recordAcceptance()
             case TermsOfUseActionType.markDismissed:
                 self.prefs.setTimestamp(Date.now(), forKey: PrefsKeys.TermsOfUseDismissedDate)
-
             case TermsOfUseActionType.markShown:
-                self.prefs.setBool(true, forKey: PrefsKeys.TermsOfUseFirstShown)
+                self.recordImpression()
             }
         }
+    }
+
+    private func recordAcceptance() {
+        self.prefs.setBool(true, forKey: PrefsKeys.TermsOfUseAccepted)
+        self.prefs.setString(String(termsOfUseVersion), forKey: PrefsKeys.TermsOfUseAcceptedVersion)
+        self.prefs.setTimestamp(Date.now(), forKey: PrefsKeys.TermsOfUseAcceptedDate)
+
+        // Record telemetry for ToU acceptance
+        let acceptedExtra = GleanMetrics.Termsofuse.AcceptedExtra(
+            surface: termsOfUseSurface,
+            touVersion: String(termsOfUseVersion)
+        )
+        GleanMetrics.Termsofuse.accepted.record(acceptedExtra)
+        GleanMetrics.Termsofuse.version.set(termsOfUseVersion)
+        GleanMetrics.Termsofuse.date.set(Date())
+    }
+
+    private func recordImpression() {
+        self.prefs.setBool(true, forKey: PrefsKeys.TermsOfUseFirstShown)
+
+        // Record telemetry for ToU impression
+        let impressionExtra = GleanMetrics.Termsofuse.ImpressionExtra(
+            surface: termsOfUseSurface,
+            touVersion: String(termsOfUseVersion)
+        )
+        GleanMetrics.Termsofuse.impression.record(impressionExtra)
     }
 }
