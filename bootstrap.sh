4d65897ff2f8b11285690c8296bf13c6dfe95663
Matt Reagan <mreagan@mozilla.com>
2025-08-26 11:34:50 -0700

    Typo

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 #
-# Pass either 'firefox' (defualt) or 'focus' to specify which product
+# Pass either 'firefox' (default) or 'focus' to specify which product
 # Use the --force option to force a re-build locales.
 
 # Default argument is "firefox"
