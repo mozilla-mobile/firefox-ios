74f1c0e05c0f16e498dc3401472aac5b183cb926
Issam Mani <issamouu69@gmail.com>
2025-09-01 19:11:32 +0200

    Bugfix FXIOS-13381 Add menu background blur for iOS 26 when app built with < 26 (#29072)
    * feat: add conditional blur effect setup for iOS versions
    
    * fix: also conditionally add zero alpha for iOS 26
    
    (cherry picked from commit a8256b37d2a22c4057377e3bd5db29c7d7459c4a)

--- firefox-ios/Client/Frontend/Browser/MainMenu/Views/MainMenuViewController.swift
+++ firefox-ios/Client/Frontend/Browser/MainMenu/Views/MainMenuViewController.swift
@@ -255,9 +255,13 @@ class MainMenuViewController: UIViewController,
     }
 
     private func setupView() {
+        #if canImport(FoundationModels)
         if #unavailable(iOS 26.0) {
             view.addBlurEffectWithClearBackgroundAndClipping(using: .regular)
         }
+        #else
+            view.addBlurEffectWithClearBackgroundAndClipping(using: .regular)
+        #endif
         view.addSubview(menuContent)
 
         NSLayoutConstraint.activate([
