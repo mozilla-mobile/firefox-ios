4578df94151b3bf95509709a6f5798f35facf947
Laurie Marceau <lmarceau@mozilla.com>
2025-09-02 19:24:09 -0400

    Adding InferSendableFromCaptures

--- BrowserKit/Package.swift
+++ BrowserKit/Package.swift
@@ -155,7 +155,9 @@ let package = Package(
         .target(
             name: "ToolbarKit",
             dependencies: ["Common"],
-            swiftSettings: [.unsafeFlags(["-enable-testing"])]),
+            swiftSettings: [.unsafeFlags(["-enable-testing"]),
+                            .enableExperimentalFeature("StrictConcurrency"),
+                            .enableUpcomingFeature("InferSendableFromCaptures")]),
         .testTarget(
             name: "ToolbarKitTests",
             dependencies: ["ToolbarKit"]),
