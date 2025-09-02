1ed63f7b40f3aadfa294466ae3545d5ce6580b19
Ione Souza Junior <junior@ionixjunior.com.br>
2025-09-02 19:48:48 -0300

    Extract handle show new tab long press action into a new function

--- firefox-ios/Client/Frontend/Browser/BrowserViewController/State/BrowserViewControllerState.swift
+++ firefox-ios/Client/Frontend/Browser/BrowserViewController/State/BrowserViewControllerState.swift
@@ -459,6 +459,17 @@ struct BrowserViewControllerState: ScreenState, Equatable {
         }
     }
 
+    private static func handleShowNewTabLongPressAction(state: BrowserViewControllerState,
+                                                        action: GeneralBrowserAction) -> BrowserViewControllerState {
+        return BrowserViewControllerState(
+            searchScreenState: state.searchScreenState,
+            toast: state.toast,
+            windowUUID: state.windowUUID,
+            browserViewType: state.browserViewType,
+            displayView: .newTabLongPressActions,
+            microsurveyState: MicrosurveyPromptState.reducer(state.microsurveyState, action))
+    }
+
     private static func handleAddToReadingListLongPressAction(state: BrowserViewControllerState,
                                                               action: GeneralBrowserAction) -> BrowserViewControllerState {
         return BrowserViewControllerState(
