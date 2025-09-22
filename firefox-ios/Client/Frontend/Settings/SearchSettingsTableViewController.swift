41425993de3ef7e1c5ee2898181bbc137a143f25
Ione Souza Junior <junior@ionixjunior.com.br>
2025-09-22 18:56:40 -0300

    Remove the redundant type annotation

--- firefox-ios/Client/Frontend/Settings/SearchSettingsTableViewController.swift
+++ firefox-ios/Client/Frontend/Settings/SearchSettingsTableViewController.swift
@@ -19,7 +19,7 @@ final class SearchSettingsTableViewController: ThemedTableViewController, Featur
     private struct UX {
         static let imageViewCornerRadius: CGFloat = 4
         static let textLabelMinimumScaleFactor: CGFloat = 0.5
-        static let textLabelLinesLimit: Int = 0
+        static let textLabelLinesLimit = 0
     }
 
     // MARK: - Properties
