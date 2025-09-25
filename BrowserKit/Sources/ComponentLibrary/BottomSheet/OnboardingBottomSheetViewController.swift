522f00f316bdb743004f171b6e90bbde824412ee
Razvan Litianu <litianu.razvan@gmail.com>
2025-09-25 16:33:08 +0300

    FXIOS-13606 #29553 Remove delay while clicking "Set as Default Browser"

---
+++ BrowserKit/Sources/ComponentLibrary/BottomSheet/OnboardingBottomSheetViewController.swift
@@ -0,0 +1,30 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import Common
+import Foundation
+import UIKit
+
+/// A specialized bottom sheet controller for onboarding flows that uses viewWillAppear for animations
+@MainActor
+public class OnboardingBottomSheetViewController: BottomSheetViewController {
+    
+    // MARK: - View lifecycle
+    
+    override public func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // Move animation from viewDidAppear to viewWillAppear for onboarding
+        // We need to access the parent's properties, so we'll call the parent's viewDidAppear logic here
+        contentViewBottomConstraint?.constant = 0
+        UIView.animate(withDuration: viewModel.animationTransitionDuration) {
+            self.view.backgroundColor = self.viewModel.backgroundColor
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    override public func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        // No animation here - it's handled in viewWillAppear
+    }
+}
