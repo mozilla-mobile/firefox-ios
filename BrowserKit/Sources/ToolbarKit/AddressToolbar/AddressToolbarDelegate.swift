d1209f31b36eb2e36e23e865541e75ab5e87c2c6
ih-codes <ihugel@mozilla.com>
2025-09-26 08:59:58 -0600

    Fix under-specified protocols: AddressToolbarDelegate, EmptyPrivateTabView, InsetUpdatable, PhotonActionSheetContainerCellDelegate, NavigationController.

--- BrowserKit/Sources/ToolbarKit/AddressToolbar/AddressToolbarDelegate.swift
+++ BrowserKit/Sources/ToolbarKit/AddressToolbar/AddressToolbarDelegate.swift
@@ -6,16 +6,26 @@ import Foundation
 import UIKit
 
 public protocol AddressToolbarDelegate: AnyObject {
+    @MainActor
     func searchSuggestions(searchTerm: String)
+    @MainActor
     func didClearSearch()
+    @MainActor
     func openBrowser(searchTerm: String)
+    @MainActor
     func addressToolbarDidBeginEditing(searchTerm: String, shouldShowSuggestions: Bool)
+    @MainActor
     func addressToolbarAccessibilityActions() -> [UIAccessibilityCustomAction]?
+    @MainActor
     func configureContextualHint(_ addressToolbar: BrowserAddressToolbar,
                                  for button: UIButton,
                                  with contextualHintType: String)
+    @MainActor
     func addressToolbarDidBeginDragInteraction()
+    @MainActor
     func addressToolbarDidProvideItemsForDragInteraction()
+    @MainActor
     func addressToolbarDidTapSearchEngine(_ searchEngineView: UIView)
+    @MainActor
     func addressToolbarNeedsSearchReset()
 }
