fe05a50ddbf9eefee1d79ba7bedceccfbe140625
Sophie <samin@mozilla.com>
2025-09-29 14:38:09 -0700

    Turn on concurrency for summarizerTests

--- BrowserKit/Package.swift
+++ BrowserKit/Package.swift
@@ -220,7 +220,11 @@ let package = Package(
             ]
         ),
         .testTarget(name: "SummarizeKitTests",
-                    dependencies: ["SummarizeKit"]),
+                    dependencies: ["SummarizeKit"],
+                    swiftSettings: [
+                        .unsafeFlags(["-enable-testing"]),
+                        .enableExperimentalFeature("StrictConcurrency")
+                    ]),
         .target(
             name: "JWTKit",
             dependencies: ["Common", "Shared"],
