6580c838e3c06d21af83c065a08da35419a95a79
Foxbolts <foxbolts3@gmail.com>
2025-10-01 16:41:45 -0400

    Add FXIOS-13674 [Stories Feed] Redux boilerplate (#29659)

--- firefox-ios/Client/Frontend/StoriesFeed/StoriesFeedViewController.swift
+++ firefox-ios/Client/Frontend/StoriesFeed/StoriesFeedViewController.swift
@@ -4,9 +4,14 @@
 
 import Common
 import Foundation
+import Redux
 
 class StoriesFeedViewController: UIViewController,
+                                 StoreSubscriber,
                                  Themeable {
+    // MARK: - Private variables
+    private var storiesFeedState: StoriesFeedState
+
     // MARK: - Themeable Properties
     let windowUUID: WindowUUID
     var currentWindowUUID: UUID? { windowUUID }
@@ -14,31 +19,84 @@ class StoriesFeedViewController: UIViewController,
     var themeListenerCancellable: Any?
     var notificationCenter: NotificationProtocol
 
+    // MARK: - Private constants
+    private let logger: Logger
+
     // MARK: Initializers
     init(windowUUID: WindowUUID,
          themeManager: ThemeManager = AppContainer.shared.resolve(),
-         notificationCenter: NotificationProtocol = NotificationCenter.default
+         notificationCenter: NotificationProtocol = NotificationCenter.default,
+         logger: Logger = DefaultLogger.shared
     ) {
         self.windowUUID = windowUUID
         self.themeManager = themeManager
         self.notificationCenter = notificationCenter
+        self.logger = logger
+        self.storiesFeedState = StoriesFeedState(windowUUID: windowUUID)
 
         super.init(nibName: nil, bundle: nil)
+
+        subscribeToRedux()
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
+    deinit {
+        unsubscribeFromRedux()
+    }
+
     // MARK: View lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        store.dispatchLegacy(
+            StoriesFeedAction(
+                windowUUID: windowUUID,
+                actionType: StoriesFeedActionType.initialize
+            )
+        )
+
         applyTheme()
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         setupNavigationBar(animated: animated)
+    }
+
+    // MARK: - Redux
+    func subscribeToRedux() {
+        let action = ScreenAction(
+            windowUUID: windowUUID,
+            actionType: ScreenActionType.showScreen,
+            screen: .storiesFeed
+        )
+        store.dispatchLegacy(action)
+
+        let uuid = windowUUID
+        store.subscribe(self, transform: {
+            return $0.select({ appState in
+                return StoriesFeedState(
+                    appState: appState,
+                    uuid: uuid
+                )
+            })
+        })
+    }
+
+    func newState(state: StoriesFeedState) {
+        self.storiesFeedState = state
+    }
+
+    func unsubscribeFromRedux() {
+        let action = ScreenAction(
+            windowUUID: windowUUID,
+            actionType: ScreenActionType.closeScreen,
+            screen: .storiesFeed
+        )
+        store.dispatchLegacy(action)
     }
 
     // MARK: Helper functions
