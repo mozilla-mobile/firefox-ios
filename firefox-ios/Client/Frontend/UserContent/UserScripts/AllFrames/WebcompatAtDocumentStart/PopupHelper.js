3f6db763d2b520d8ce91732440525a25f016f7a1
Issam Mani <issamouu69@gmail.com>
2025-10-01 21:50:07 +0200

    fix: inject styles to hide popup

---
+++ firefox-ios/Client/Frontend/UserContent/UserScripts/AllFrames/WebcompatAtDocumentStart/PopupHelper.js
@@ -0,0 +1,24 @@
+// // This Source Code Form is subject to the terms of the Mozilla Public
+// // License, v. 2.0. If a copy of the MPL was not distributed with this
+// // file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+// NOTE: We only want to run this on Google search result pages.
+// There is no need to modify other pages.
+if (location.hostname === "www.google.com" || location.hostname === "google.com") {
+  // CSS to hide specific popup elements on Google search result pages.
+  // This targets known popup structures as of the time of writing.
+  // Future changes to the markup may require updates to this CSS.
+  // This is a temporary workaround until a more robust solution is implemented.
+  const css = `
+  div[role="dialog"]:has(basic-promo-image),
+  div[role="dialog"][jsname="XKSfm"],
+  div[role="dialog"][aria-describedby="promo_desc_id"], 
+  div[role="dialog"] div#stUuGf {
+    display: none !important;
+    visibility: hidden !important;
+  }
+  `;
+  const style = document.createElement('style');
+  style.textContent = css;
+  document.documentElement.appendChild(style);
+}
