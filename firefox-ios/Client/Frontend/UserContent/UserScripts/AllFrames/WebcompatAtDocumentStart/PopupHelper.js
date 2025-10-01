17840fd17daf9ee03d27c69e0eaba6852823e677
Issam Mani <issamouu69@gmail.com>
2025-10-01 22:09:14 +0200

    fix: use regex instead

--- firefox-ios/Client/Frontend/UserContent/UserScripts/AllFrames/WebcompatAtDocumentStart/PopupHelper.js
+++ firefox-ios/Client/Frontend/UserContent/UserScripts/AllFrames/WebcompatAtDocumentStart/PopupHelper.js
@@ -2,18 +2,19 @@
 // // License, v. 2.0. If a copy of the MPL was not distributed with this
 // // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-const HOSTNAMES = [
-  "www.google.com",
-  "google.com"
-];
+/// NOTE: This regex will match real google (sub)domains + some invalid ones.
+/// Examples of valid matches: `google.com`, `www.google.com`, `google.co.uk`.
+/// False positive like `google.something-weird` isn't a practical concern for our use case. 
+/// It's fine to match that too.
+const DOMAIN_PATTERN = /(\.|^)google\./;
 
-// NOTE: We only want to run this on Google search result pages.
-// There is no need to modify other pages.
-if (HOSTNAMES.includes(location.hostname)) {
-  // CSS to hide specific popup elements on Google search result pages.
-  // This targets known popup structures as of the time of writing.
-  // Future changes to the markup may require updates to this CSS.
-  // This is a temporary workaround until a more robust solution is implemented.
+/// NOTE: We only want to run this on Google search result pages.
+/// There is no need to modify other pages.
+if (DOMAIN_PATTERN.test(location.hostname)) {
+  /// NOTE: CSS to hide specific popup elements on Google search result pages.
+  /// This targets known popup structures as of the time of writing.
+  /// Future changes to the markup may require updates to this CSS.
+  /// This is a temporary workaround until a more robust solution is implemented.
   const css = `
   div[role="dialog"]:has(basic-promo-image),
   div[role="dialog"][jsname="XKSfm"],
