7fb2a32f2748a54e3364fb2f96bb21cc7863f0b6
Filippo Zazzeroni <fzazzeroni@mozilla.com>
2025-10-02 10:57:47 +0200

    Port javascript alert interface in WebEngine

--- BrowserKit/Sources/WebEngine/Utilities/PopupThrottler.swift
+++ BrowserKit/Sources/WebEngine/Utilities/PopupThrottler.swift
@@ -2,48 +2,56 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-/// Utility for tracking various types of popups that may be presented
-/// over a short time. Used to prevent Javascript abuse or DOS attacks.
-final class PopupThrottler {
-    enum PopupType {
-        /// Javascript alert()
-        case alert
-        /// Popup window (window.open)
-        case popupWindow
+import Foundation
 
-        /// Max consecutive popups allowed within a given time period (below)
-        /// after which throttling begins.
-        var maxPopupThreshold: Int {
-            switch self {
-            case .alert: return 5
-            case .popupWindow: return 2
-            }
+public protocol PopupThrottler {
+    func canShowAlert(type: PopupType) -> Bool
+
+    func willShowAlert(type: PopupType)
+}
+
+public enum PopupType: Sendable {
+    /// Javascript alert()
+    case alert
+    /// Popup window (window.open)
+    case popupWindow
+
+    /// Max consecutive popups allowed within a given time period (below)
+    /// after which throttling begins.
+    var maxPopupThreshold: Int {
+        switch self {
+        case .alert: return 5
+        case .popupWindow: return 2
         }
-
-        /// The time within which the popup threshold may be reached. After this
-        /// many seconds since the first alert, the time and alert count are reset.
-        var resetTime: TimeInterval {
-            switch self {
-            case .alert: return 20
-            case .popupWindow: return 2
-            }
-        }
-
-        static let defaultResetTimes = [PopupType.alert: PopupType.alert.resetTime,
-                                       PopupType.popupWindow: PopupType.popupWindow.resetTime]
     }
 
+    /// The time within which the popup threshold may be reached. After this
+    /// many seconds since the first alert, the time and alert count are reset.
+    var resetTime: TimeInterval {
+        switch self {
+        case .alert: return 20
+        case .popupWindow: return 2
+        }
+    }
+
+    public static let defaultResetTimes = [PopupType.alert: PopupType.alert.resetTime,
+                                           PopupType.popupWindow: PopupType.popupWindow.resetTime]
+}
+
+/// Utility for tracking various types of popups that may be presented
+/// over a short time. Used to prevent Javascript abuse or DOS attacks.
+public final class DefaultPopupThrottler: PopupThrottler {
     var alertCount = [PopupType.alert: 0, PopupType.popupWindow: 0]
     var lastAlertDate = [PopupType.alert: Date.distantPast, PopupType.popupWindow: Date.distantPast]
     private let resetTime: [PopupType: TimeInterval]
 
-    init(resetTime: [PopupType: TimeInterval] = PopupType.defaultResetTimes) {
+    public init(resetTime: [PopupType: TimeInterval] = PopupType.defaultResetTimes) {
         self.resetTime = resetTime
     }
 
     // MARK: - Public API
 
-    func canShowAlert(type: PopupThrottler.PopupType) -> Bool {
+    public func canShowAlert(type: PopupType) -> Bool {
         guard let count = alertCount[type] else { return true }
         guard let date = lastAlertDate[type] else { return true }
         guard let time = resetTime[type] else { return true }
@@ -53,7 +61,7 @@ final class PopupThrottler {
         return alertCountOK || timeOK
     }
 
-    func willShowAlert(type: PopupThrottler.PopupType) {
+    public func willShowAlert(type: PopupType) {
         guard let count = alertCount[type] else { return }
         guard let date = lastAlertDate[type] else { return }
         guard let time = resetTime[type] else { return }
