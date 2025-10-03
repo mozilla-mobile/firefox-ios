bf422d76c59bbada75528ba7717c8ce82759a4a2
Sophie <samin@mozilla.com>
2025-10-02 21:06:13 -0700

    Turn on OnboardingKitTests Strict Concurrency

--- BrowserKit/Package.swift
+++ BrowserKit/Package.swift
@@ -228,7 +228,10 @@ let package = Package(
             ]),
         .testTarget(
             name: "OnboardingKitTests",
-            dependencies: ["OnboardingKit"]),
+            dependencies: ["OnboardingKit"],
+            swiftSettings: [
+                .enableExperimentalFeature("StrictConcurrency")
+            ]),
         .executableTarget(
             name: "ExecutableContentBlockingGenerator",
             dependencies: ["ContentBlockingGenerator"]),
