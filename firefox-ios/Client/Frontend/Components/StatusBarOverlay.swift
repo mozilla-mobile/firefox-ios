df5dd4433584c20542e0dc225a97dfb1cb9bee53
Winnie Teichmann <4530+thatswinnie@users.noreply.github.com>
2025-10-06 19:24:37 +0200

    Fix unit tests

--- firefox-ios/Client/Frontend/Components/StatusBarOverlay.swift
+++ firefox-ios/Client/Frontend/Components/StatusBarOverlay.swift
@@ -142,7 +142,7 @@ final class StatusBarOverlay: UIView,
     // MARK: - ThemeApplicable
 
     func applyTheme(theme: Theme) {
-        savedBackgroundColor = hasTopTabs ? theme.colors.layerSurfaceLow : theme.colors.layer1
+        savedBackgroundColor = theme.colors.layerSurfaceLow
         let isHomepage: Bool = savedIsHomepage ?? false
         let needsNoStatusBar = needsNoStatusBar(isHomepage: isHomepage)
         updateStatusBarAlpha(isHomepage: isHomepage, needsNoStatusBar: needsNoStatusBar)
