ca2fa886ce3760c736c80a10f215db62d590320c
Foxbolts <foxbolts3@gmail.com>
2025-10-07 17:29:42 -0400

    Add [Stories Feed] FXIOS-13084 / FXIOS-13095 Stories feed  (#29845)

--- firefox-ios/Client/Application/AccessibilityIdentifiers.swift
+++ firefox-ios/Client/Application/AccessibilityIdentifiers.swift
@@ -236,6 +236,10 @@ struct AccessibilityIdentifiers {
             static let favIconImage = "SyncedTabFavIconImage"
             static let descriptionLabel = "SyncedTabDescriptionLabel"
         }
+
+        struct StoriesFeed {
+            static let storiesFeedCell = "StoriesFeedCell"
+        }
     }
 
     struct GeneralizedIdentifiers {
