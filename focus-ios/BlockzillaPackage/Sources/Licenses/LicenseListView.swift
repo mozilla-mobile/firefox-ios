b3f4caa7580d7a3bc406595e354bdde2074d595d
Nicolas Combe <42418337+NicolasCombe5555@users.noreply.github.com>
2025-10-15 05:05:22 -0500

    Refactor FXIOS-10468 [SwiftLint] Fix force_unwrapping in /Licenses (#29674)
    * Refactor FXIOS-10468 [SwiftLint] Fix force_unwrapping in /Licenses
    
    * Address comment
    
    ---------
    
    Co-authored-by: Nicolas Combe <nicolas.combe@uber.com>

--- focus-ios/BlockzillaPackage/Sources/Licenses/LicenseListView.swift
+++ focus-ios/BlockzillaPackage/Sources/Licenses/LicenseListView.swift
@@ -2,7 +2,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import UIKit
 import SwiftUI
 
 public struct LicenseListView: View {
@@ -27,15 +26,13 @@ public struct LicenseListView: View {
 
 public extension LicenseListView {
     init() {
-        let licenseListURL = Bundle.module.url(forResource: "license-list", withExtension: "plist")!
-        let focusURL = Bundle.module.url(forResource: "focus-ios", withExtension: "plist")!
-
-        let focusLicense = (try? decodePropertyList(LicenseList.self, from: focusURL).libraries) ?? []
-        let libraries = (try? decodePropertyList(LicenseList.self, from: licenseListURL).libraries) ?? []
+        let focusLicense = LicenseManager.licenseList(for: "focus-ios")
+        let libraries = LicenseManager.licenseList(for: "license-list")
         self.libraries = focusLicense + libraries
     }
 }
 
+// MARK: - Preview
 struct LicenseListView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
@@ -44,8 +41,16 @@ struct LicenseListView_Previews: PreviewProvider {
     }
 }
 
-func decodePropertyList<T: Decodable>(_ type: T.Type, from url: URL) throws -> T {
-    let data = try Data(contentsOf: url)
-    let decoder = PropertyListDecoder()
-    return try decoder.decode(type.self, from: data)
+// MARK: - Private
+private enum LicenseManager {
+    static func licenseList(for resource: String) -> [Library] {
+        guard let url = Bundle.module.url(forResource: resource, withExtension: "plist") else { return [] }
+        return (try? decodePropertyList(LicenseList.self, from: url).libraries) ?? []
+    }
+
+    private static func decodePropertyList<T: Decodable>(_ type: T.Type, from url: URL) throws -> T {
+        let data = try Data(contentsOf: url)
+        let decoder = PropertyListDecoder()
+        return try decoder.decode(type.self, from: data)
+    }
 }
