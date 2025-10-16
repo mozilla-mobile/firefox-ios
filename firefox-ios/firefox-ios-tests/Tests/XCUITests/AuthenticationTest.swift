bfe6b19767245e68c3c1ae1438f300c2f0c510ac
Clare So <1740517+clarmso@users.noreply.github.com>
2025-10-16 10:57:55 -0400

    Revert changes manually

--- firefox-ios/firefox-ios-tests/Tests/XCUITests/AuthenticationTest.swift
+++ firefox-ios/firefox-ios-tests/Tests/XCUITests/AuthenticationTest.swift
@@ -42,6 +42,7 @@ class AuthenticationTest: BaseTestCase {
         )
         app.alerts.textFields["Username"].typeText("guest")
         app.alerts.secureTextFields["Password"].tapAndTypeText("guest")
+        mozWaitElementEnabled(element: app.alerts.buttons["Log in"], timeout: TIMEOUT)
         app.alerts.buttons["Log in"].waitAndTap()
         /* There is no other way to verify basic auth is successful as the webview is
          inaccessible after sign in to verify the success text. */
