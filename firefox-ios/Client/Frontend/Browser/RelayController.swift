11af2eeff688ed085b475dde36cc8ea244d8353c
Matt Reagan <mreagan@mozilla.com>
2025-10-17 15:54:22 -0700

    [FXIOS-13621] Stub out early Relay controller code. WIP

---
+++ firefox-ios/Client/Frontend/Browser/RelayController.swift
@@ -0,0 +1,49 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import Common
+
+/// Describes public protocol for Relay component to track state and facilitate
+/// messaging between the BVC, keyboard accessory, and A~S Relay APIs.
+protocol RelayControllerProtocol {
+    /// Whether to present the UI for a Relay mask after focusing on an email field.
+    /// This should account for all logic necessary for Relay display, which includes:
+    ///    - User account status (signed into Mozilla / Relay active)
+    ///    - Allow and Block lists
+    /// - Parameter String: The website URL.
+    /// - Returns: `true` if the website is valid for Relay, after checking block/allow lists.
+    func emailFocusShouldDisplayRelayPrompt(url: String) -> Bool
+}
+
+final class RelayController: RelayControllerProtocol {
+    // MARK: - Properties
+
+    static let shared = RelayController()
+
+    static let isFeatureEnabled = {
+        LegacyFeatureFlagsManager.shared.isFeatureEnabled(.relayIntegration, checking: .buildOnly)
+    }()
+
+    private let logger: Logger
+    private let profile: Profile
+
+    // MARK: - Init
+
+    private init(logger: Logger = DefaultLogger.shared, profile: Profile = AppContainer.shared.resolve()) {
+        self.logger = logger
+        self.profile = profile
+    }
+
+    // MARK: - RelayControllerProtocol
+
+    func emailFocusShouldDisplayRelayPrompt(url: String) -> Bool {
+        guard Self.isFeatureEnabled else { return false }
+
+        // TODO: Check for Relay OAuth attached client. Forthcoming.
+        guard profile.hasAccount() else { return false }
+
+        // TODO: [FXIOS-13625] Forthcoming.
+        return true
+    }
+}
