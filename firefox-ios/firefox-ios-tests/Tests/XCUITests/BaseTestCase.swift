fededbf839622708856e27baed008300b0a3ba43
Clare So <1740517+clarmso@users.noreply.github.com>
2025-10-20 10:17:20 -0400

    Proof my theory about ensuring links or buttons enabled before tapping

--- firefox-ios/firefox-ios-tests/Tests/XCUITests/BaseTestCase.swift
+++ firefox-ios/firefox-ios-tests/Tests/XCUITests/BaseTestCase.swift
@@ -610,13 +610,17 @@ extension XCUIElement {
     }
     /// Waits for the UI element and then taps if it exists.
     func waitAndTap(timeout: TimeInterval? = TIMEOUT) {
-        BaseTestCase().mozWaitForElementToExist(self, timeout: timeout)
+        BaseTestCase().mozWaitForElementToExist(self, timeout: timeout ?? TIMEOUT)
+        if self.elementType == .button || self.elementType == .link {
+            BaseTestCase().mozWaitElementHittable(element: self, timeout: timeout ?? TIMEOUT)
+        }
         self.tap()
     }
     /// Waits for the UI element and then taps and types the provided text if it exists.
     func tapAndTypeText(_ text: String, timeout: TimeInterval? = TIMEOUT) {
         BaseTestCase().mozWaitForElementToExist(self, timeout: timeout)
         self.tap()
+        BaseTestCase().mozWaitElementEnabled(element: self, timeout: timeout ?? TIMEOUT)
         self.typeText(text)
     }
 
