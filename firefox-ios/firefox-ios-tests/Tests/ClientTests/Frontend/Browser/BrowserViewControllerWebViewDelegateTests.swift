ad40ba6fefa4e63e923f2dc7d83c8e405f048d6a
Yoana Rios <yrrios13@gmail.com>
2025-10-20 09:12:43 -0400

    Add FXIOS-13819 #29942 ⁃ unit test for MarketPlace Navigation handling (#29959)
    * Test if test pass in bitrise without mock changes
    
    * Add changes to mock
    
    * Create Mock webview delegates file and clean up
    
    * Use mock file manager and update 3 Mocks, why so many??

--- firefox-ios/firefox-ios-tests/Tests/ClientTests/Frontend/Browser/BrowserViewControllerWebViewDelegateTests.swift
+++ firefox-ios/firefox-ios-tests/Tests/ClientTests/Frontend/Browser/BrowserViewControllerWebViewDelegateTests.swift
@@ -240,6 +240,48 @@ class BrowserViewControllerWebViewDelegateTests: XCTestCase {
         }
     }
 
+    @MainActor
+    func testWebViewDecidePolicyForNavigationAction_allowMarketPlaceScheme_whenUserAction() {
+        let subject = createSubject()
+        let url = URL(string: "marketplace-kit://install?exampleApp.com")!
+        let tab = createTab()
+        tabManager.tabs = [tab]
+        let navigationAction = MockNavigationAction(url: url, type: .linkActivated)
+
+        subject.webView(tab.webView!,
+                        decidePolicyFor: navigationAction) { policy in
+            XCTAssertEqual(policy, .allow)
+        }
+    }
+
+    @MainActor
+    func testWebViewDecidePolicyForNavigationAction_cancelMarketPlaceScheme_whenNotMainFrame() {
+        let subject = createSubject()
+        let url = URL(string: "marketplace-kit://install?exampleApp.com")!
+        let tab = createTab()
+        tabManager.tabs = [tab]
+        let navigationAction = MockNavigationAction(url: url, type: .linkActivated, isMainFrame: false)
+
+        subject.webView(tab.webView!,
+                        decidePolicyFor: navigationAction) { policy in
+            XCTAssertEqual(policy, .cancel)
+        }
+    }
+
+    @MainActor
+    func testWebViewDecidePolicyForNavigationAction_cancelMarketPlaceScheme_whenReloadAction() {
+        let subject = createSubject()
+        let url = URL(string: "marketplace-kit://install?exampleApp.com")!
+        let tab = createTab()
+        tabManager.tabs = [tab]
+        let navigationAction = MockNavigationAction(url: url, type: .reload)
+
+        subject.webView(tab.webView!,
+                        decidePolicyFor: navigationAction) { policy in
+            XCTAssertEqual(policy, .cancel)
+        }
+    }
+
     // MARK: - Authentication
 
     @MainActor
@@ -365,113 +407,5 @@ class BrowserViewControllerWebViewDelegateTests: XCTestCase {
         subject.webView(tab.webView!, didFinish: nil)
 
         XCTAssertTrue(screenshotHelper.takeScreenshotCalled)
-    }
-}
-
-class MockNavigationAction: WKNavigationAction {
-    private var type: WKNavigationType?
-    private var urlRequest: URLRequest
-
-    override var navigationType: WKNavigationType {
-        return type ?? .other
-    }
-
-    override var request: URLRequest {
-        return urlRequest
-    }
-
-    init(url: URL, type: WKNavigationType? = nil) {
-        self.type = type
-        self.urlRequest = URLRequest(url: url)
-    }
-}
-
-class MockURLAuthenticationChallengeSender: NSObject, URLAuthenticationChallengeSender {
-    func use(_ credential: URLCredential, for challenge: URLAuthenticationChallenge) {}
-
-    func continueWithoutCredential(for challenge: URLAuthenticationChallenge) {}
-
-    func cancel(_ challenge: URLAuthenticationChallenge) {}
-}
-
-final class MockFileManager: FileManagerProtocol, @unchecked Sendable {
-    var fileExistsCalled = 0
-    var fileExists = false
-    var urlsForDirectoryCalled = 0
-    var contentOfDirectoryCalled = 0
-    var removeItemAtPathCalled = 0
-    var removeItemAtURLCalled = 0
-    var copyItemCalled = 0
-    var createDirectoryCalled = 0
-    var contentOfDirectoryAtPathCalled = 0
-
-    /// Fires every time `removeItem(at: URL)` is called. This is useful for tests that fire this on a background thread
-    /// (e.g. in a deinit) and we want to wait for an expectation of a file removal to be fulfilled.
-    /// Closure contains the updated value of `removeItemAtURLCalled`.
-    var removeItemAtURLDispatch: ((Int) -> Void)?
-
-    func fileExists(atPath path: String) -> Bool {
-        fileExistsCalled += 1
-        return fileExists
-    }
-
-    func urls(for directory: FileManager.SearchPathDirectory,
-              in domainMask: FileManager.SearchPathDomainMask) -> [URL] {
-        urlsForDirectoryCalled += 1
-        return []
-    }
-
-    func contentsOfDirectory(atPath path: String) throws -> [String] {
-        contentOfDirectoryCalled += 1
-        return []
-    }
-
-    func contentsOfDirectoryAtPath(
-        _ path: String,
-        withFilenamePrefix prefix: String
-    ) throws -> [String] {
-        contentOfDirectoryAtPathCalled += 1
-        return []
-    }
-
-    func removeItem(atPath path: String) throws {
-        removeItemAtPathCalled += 1
-    }
-
-    func removeItem(at url: URL) throws {
-        removeItemAtURLCalled += 1
-        removeItemAtURLDispatch?(removeItemAtURLCalled)
-    }
-
-    func copyItem(at srcURL: URL, to dstURL: URL) throws {
-        copyItemCalled += 1
-    }
-
-    func createDirectory(
-        atPath path: String,
-        withIntermediateDirectories createIntermediates: Bool,
-        attributes: [FileAttributeKey: Any]?
-    ) throws {
-        createDirectoryCalled += 1
-    }
-
-    func contents(atPath path: String) -> Data? {
-        return nil
-    }
-
-    func contentsOfDirectory(
-        at url: URL,
-        includingPropertiesForKeys keys: [URLResourceKey]?,
-        options mask: FileManager.DirectoryEnumerationOptions
-    ) throws -> [URL] {
-        return []
-    }
-
-    func createFile(
-        atPath path: String,
-        contents data: Data?,
-        attributes attr: [FileAttributeKey: Any]?
-    ) -> Bool {
-        return true
     }
 }
