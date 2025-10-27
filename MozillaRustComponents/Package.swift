e90aaa750b5b13219a78f898de5556e36a3977d1
Jan-Erik Rediger <jrediger@mozilla.com>
2025-10-27 15:58:41 +0100

    Update to Glean v66.0.1

--- MozillaRustComponents/Package.swift
+++ MozillaRustComponents/Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .library(name: "FocusRustComponents", targets: ["FocusAppServices"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/mozilla/glean-swift", from: "65.1.1"),
+        .package(url: "https://github.com/mozilla/glean-swift", from: "66.0.1"),
     ],
     targets: [
         // A wrapper around our binary target that combines + any swift files we want to expose to the user
