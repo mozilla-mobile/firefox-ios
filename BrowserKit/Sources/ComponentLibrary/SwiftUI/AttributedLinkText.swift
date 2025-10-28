98df97a35f763e1276a25514377774a131fa9456
Razvan Litianu <litianu.razvan@gmail.com>
2025-10-28 13:25:06 +0200

    Remove trailing space

--- BrowserKit/Sources/ComponentLibrary/SwiftUI/AttributedLinkText.swift
+++ BrowserKit/Sources/ComponentLibrary/SwiftUI/AttributedLinkText.swift
@@ -48,7 +48,7 @@ public struct AttributedLinkText<Action: RawRepresentable>: View where Action.Ra
                 return .systemAction
             })
     }
-    
+
     private var attributedString: AttributedString {
         var attrString = AttributedString(fullText)
         attrString.foregroundColor = Color(uiColor: theme.colors.textSecondary)
