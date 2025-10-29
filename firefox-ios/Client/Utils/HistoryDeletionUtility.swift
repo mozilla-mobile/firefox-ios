7a2a9f610faa4f36d1607c9056d1b88b3de7b42e
ih-codes <ihugel@mozilla.com>
2025-10-29 15:21:13 -0600

    Fix strict concurrency warnings in HistoryDeletionUtility.

--- firefox-ios/Client/Utils/HistoryDeletionUtility.swift
+++ firefox-ios/Client/Utils/HistoryDeletionUtility.swift
@@ -16,14 +16,16 @@ enum HistoryDeletionUtilityDateOptions: String, CaseIterable {
 }
 
 protocol HistoryDeletionProtocol {
-    func delete(_ sites: [String], completion: @escaping (Bool) -> Void)
+    func delete(_ sites: [String], completion: @Sendable @escaping (Bool) -> Void)
+    @MainActor
     func deleteHistoryFrom(_ dateOption: HistoryDeletionUtilityDateOptions,
-                           completion: @escaping (HistoryDeletionUtilityDateOptions) -> Void)
+                           completion: @Sendable @escaping (HistoryDeletionUtilityDateOptions) -> Void)
 }
 
-class HistoryDeletionUtility: HistoryDeletionProtocol {
-    private var profile: Profile
-    private let gleanWrapper: GleanWrapper
+final class HistoryDeletionUtility: HistoryDeletionProtocol, Sendable {
+    private let profile: Profile
+    // FIXME: FXIOS-13957 All properties should be truly Sendable
+    private nonisolated(unsafe) let gleanWrapper: GleanWrapper
 
     init(with profile: Profile, gleanWrapper: GleanWrapper = DefaultGleanWrapper()) {
         self.profile = profile
@@ -33,7 +35,7 @@ class HistoryDeletionUtility: HistoryDeletionProtocol {
     // MARK: Interface
     func delete(
         _ sites: [String],
-        completion: @escaping (Bool) -> Void
+        completion: @Sendable @escaping (Bool) -> Void
     ) {
         deleteFromHistory(sites)
         deleteMetadata(sites) { result in
@@ -41,9 +43,10 @@ class HistoryDeletionUtility: HistoryDeletionProtocol {
         }
     }
 
+    @MainActor
     func deleteHistoryFrom(
         _ dateOption: HistoryDeletionUtilityDateOptions,
-        completion: @escaping (HistoryDeletionUtilityDateOptions) -> Void
+        completion: @Sendable @escaping (HistoryDeletionUtilityDateOptions) -> Void
     ) {
         deleteWKWebsiteDataSince(dateOption, for: WKWebsiteDataStore.allWebsiteDataTypes())
         // For efficiency, we'll delete data in parallel, which is why closures are
@@ -65,7 +68,7 @@ class HistoryDeletionUtility: HistoryDeletionProtocol {
 
     private func deleteMetadata(
         _ sites: [String],
-        completion: @escaping (Bool) -> Void
+        completion: @Sendable @escaping (Bool) -> Void
     ) {
         sites.forEach { currentSite in
             profile.places
@@ -81,6 +84,7 @@ class HistoryDeletionUtility: HistoryDeletionProtocol {
     }
 
     // MARK: - Date based deletion functions
+    @MainActor
     private func deleteWKWebsiteDataSince(
         _ dateOption: HistoryDeletionUtilityDateOptions,
         for types: Set<String>
@@ -96,7 +100,7 @@ class HistoryDeletionUtility: HistoryDeletionProtocol {
 
     private func deleteProfileHistorySince(
         _ dateOption: HistoryDeletionUtilityDateOptions,
-        completion: @escaping (Bool?) -> Void
+        completion: @Sendable @escaping (Bool?) -> Void
     ) {
         switch dateOption {
         case .allTime:
