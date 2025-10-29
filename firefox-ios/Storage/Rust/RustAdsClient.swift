3fae3efd2d056e5ab8a28d2e4bb189c8a68b4893
ahanot <ahanot@mozilla.com>
2025-10-29 17:51:02 -0400

    feat(ads-client): update constructor with env and cache config

--- firefox-ios/Storage/Rust/RustAdsClient.swift
+++ firefox-ios/Storage/Rust/RustAdsClient.swift
@@ -3,10 +3,14 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
 import Foundation
-import class MozillaAppServices.MozAdsClient
+import MozillaAppServices
 
 public final class RustAdsClient {
-    public static let shared = MozAdsClient(dbPath: getDatabaseURL())
+    public static let shared = {
+        let cacheConfig = MozAdsCacheConfig(dbPath: getDatabaseURL(), defaultCacheTtlSeconds: nil, maxSizeMib: nil)
+        let config = MozAdsClientConfig(environment: .prod, cacheConfig: cacheConfig)
+        return MozAdsClient(clientConfig: config)
+    }()
     private init() {}
 }
 
