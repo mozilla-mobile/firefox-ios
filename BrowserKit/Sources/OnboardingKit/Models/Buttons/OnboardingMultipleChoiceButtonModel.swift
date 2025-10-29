65d004a8536768918f9c92d0cfdd46bff69cb57f
Razvan Litianu <litianu.razvan@gmail.com>
2025-10-29 13:30:48 +0200

    Swiftlint fix

--- BrowserKit/Sources/OnboardingKit/Models/Buttons/OnboardingMultipleChoiceButtonModel.swift
+++ BrowserKit/Sources/OnboardingKit/Models/Buttons/OnboardingMultipleChoiceButtonModel.swift
@@ -21,7 +21,7 @@ public struct OnboardingMultipleChoiceButtonModel<OnboardingMultipleChoiceAction
         if let image = UIImage(named: finalImageID, in: Bundle.main, compatibleWith: nil) {
             return image
         }
-        
+
         // If selected version not found, fallback to non-selected version
         if isSelected {
             if let image = UIImage(named: imageID, in: Bundle.module, compatibleWith: nil) {
@@ -29,7 +29,7 @@ public struct OnboardingMultipleChoiceButtonModel<OnboardingMultipleChoiceAction
             }
             return UIImage(named: imageID, in: Bundle.main, compatibleWith: nil)
         }
-        
+
         return nil
     }
 
