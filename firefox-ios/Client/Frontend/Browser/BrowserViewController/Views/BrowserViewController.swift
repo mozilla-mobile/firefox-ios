c67ae5a2682f4ec3479aaa0e74a6e387146f2084
Alexandru Farcasanu <alex.farcasanu@yahoo.com>
2025-10-30 15:26:20 +0200

    FXIOS-13597 #29536 ⁃ [Shake to summarize] [Intermittent] - Incorrect display of the CFR on homepage

--- firefox-ios/Client/Frontend/Browser/BrowserViewController/Views/BrowserViewController.swift
+++ firefox-ios/Client/Frontend/Browser/BrowserViewController/Views/BrowserViewController.swift
@@ -1768,6 +1768,7 @@ class BrowserViewController: UIViewController,
     /// it's the zero search page, aka when the home page is shown by clicking the url bar from a loaded web page.
     func showEmbeddedHomepage(inline: Bool, isPrivate: Bool) {
         resetDataClearanceCFRTimer()
+        summarizeToolbarEntryContextHintVC.dismiss(animated: false)
 
         if isPrivate && featureFlags.isFeatureEnabled(.feltPrivacySimplifiedUI, checking: .buildOnly) {
             browserDelegate?.showPrivateHomepage(overlayManager: overlayManager)
