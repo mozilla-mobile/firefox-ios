8d6e20ee32d31fdbb2e171f5c169df6d2d3b5d20
Clare So <1740517+clarmso@users.noreply.github.com>
2025-11-03 15:42:40 -0500

    Remove flag

--- firefox-ios/Client/Frontend/Browser/SearchEngines/SearchEngineFlagManager.swift
+++ firefox-ios/Client/Frontend/Browser/SearchEngines/SearchEngineFlagManager.swift
@@ -8,9 +8,6 @@ struct SearchEngineFlagManager {
     /// Whether Search Engine Consolidation is enabled.
     /// If enabled, search engines are fetched from Remote Settings rather than our pre-bundled XML files.
     static var isSECEnabled: Bool {
-        // TODO: [FXIOS-13834 & 11403] Remove hardcoded override once UI & unit tests are updated (or deleted)
-        guard !AppConstants.isRunningUnitTest && !AppConstants.isRunningUITests else { return false }
-
         // SEC enabled for all users. Related clean-up forthcoming in [FXIOS-11403].
         return true
     }
