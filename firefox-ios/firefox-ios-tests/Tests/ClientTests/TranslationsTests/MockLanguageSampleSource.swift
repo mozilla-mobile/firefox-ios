8105445a048f53309f765bacc02844dd0a189298
Issam Mani <issamouu69@gmail.com>
2025-11-04 14:46:22 +0100

    feat: add tests

---
+++ firefox-ios/firefox-ios-tests/Tests/ClientTests/TranslationsTests/MockLanguageSampleSource.swift
@@ -0,0 +1,18 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import Foundation
+@testable import Client
+
+/// Test helper that simulates JS evaluation for language sample extraction.
+@MainActor
+final class MockLanguageSampleSource: LanguageSampleSource {
+    var mockResult: Any?
+    var mockError: Error?
+
+    func getLanguageSample(scriptEvalExpression: String) async throws -> String? {
+        if let error = mockError { throw error }
+        return mockResult as? String
+    }
+}
