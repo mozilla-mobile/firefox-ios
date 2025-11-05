6f87cda53d6eb3feb72b1fbca5ce3e762711f214
Clare So <1740517+clarmso@users.noreply.github.com>
2025-11-05 15:51:03 -0500

    oops syntax error

--- firefox-ios/Client/Frontend/Browser/SearchEngines/SearchEngineFlagManager.swift
+++ firefox-ios/Client/Frontend/Browser/SearchEngines/SearchEngineFlagManager.swift
@@ -8,7 +8,7 @@ struct SearchEngineFlagManager {
     /// Whether Search Engine Consolidation is enabled.
     /// If enabled, search engines are fetched from Remote Settings rather than our pre-bundled XML files.
     /// TODO: [FXIOS-13834 & 11403] Remove hardcoded override once UI & unit tests are updated (or deleted)
-    guard !AppConstants.isRunningUnitTest else { return false }
+    guard !AppConstants.isRunningUnitTest { return false }
     static var isSECEnabled: Bool {
         // SEC enabled for all users. Related clean-up forthcoming in [FXIOS-11403].
         return true
