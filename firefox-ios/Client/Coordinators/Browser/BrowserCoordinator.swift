4c61aba08504276f24636e5ca71db9a28d2f0ce6
Matt <foxbolts3@gmail.com>
2025-11-05 11:50:02 -0500

    Add FXIOS-14002 [Stories Feed] Handle new-tab link targets in the current tab (#30383)

--- firefox-ios/Client/Coordinators/Browser/BrowserCoordinator.swift
+++ firefox-ios/Client/Coordinators/Browser/BrowserCoordinator.swift
@@ -1156,21 +1156,8 @@ class BrowserCoordinator: BaseCoordinator,
 
     func showStoriesWebView(url: URL?) {
         guard let url else { return }
-
-        // Creates an unmanaged tab that is destroyed once the stories webview view controller is dismissed
-        // Used to prevent persisting tab in the tab tray during and across app sessions
-        let tab = Tab(profile: profile, isPrivate: false, windowUUID: windowUUID)
-        let tabConfigurationProvider = TabConfigurationProvider(prefs: profile.prefs)
-        let tabConfiguration = tabConfigurationProvider.configuration(isPrivate: tab.isPrivate).webViewConfiguration
-        tab.url = url
-        tab.navigationDelegate = browserViewController
-        tab.tabDelegate = browserViewController
-        tab.createWebview(configuration: tabConfiguration)
-        tab.loadRequest(URLRequest(url: url))
-
-        // Present the stories web view with the tab's webview
-        guard let webView = tab.webView else { return }
-        let webviewViewController = StoriesWebviewViewController(windowUUID: windowUUID, webView: webView)
+        let webviewViewController = StoriesWebviewViewController(profile: profile, windowUUID: windowUUID)
+        webviewViewController.configure(url: url)
         router.push(webviewViewController)
     }
 
