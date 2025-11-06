f229ed05c4bd85e3c49554768008aed3a7f9451d
FilippoZazzeroni <55580351+FilippoZazzeroni@users.noreply.github.com>
2025-11-06 14:23:12 +0100

    BugFix [Onboarding] FXIOS-13911 Firefox loader view being distorted when passing from portrait to landscape on iPad (#30395)
    * Refactor Launch screen loader view with UIKit
    
    * lint
    
    * Remove transitions delegate for modern onboarding
    
    * Added workoround for landscape to portrait layout glitch
    
    * Add tests to SceneCoordinator
    
    ---------
    
    Co-authored-by: Filippo Zazzeroni <fzazzeroni@mozilla.com>

--- firefox-ios/Client/Coordinators/Launch/LaunchCoordinator.swift
+++ firefox-ios/Client/Coordinators/Launch/LaunchCoordinator.swift
@@ -28,7 +28,6 @@ final class LaunchCoordinator: BaseCoordinator,
     private let isIphone: Bool
     let windowUUID: WindowUUID
     let themeManager: ThemeManager = AppContainer.shared.resolve()
-    private let modernTransitionDelegate = ModernLaunchTransitionDelegate()
     weak var parentCoordinator: LaunchCoordinatorDelegate?
 
     init(router: Router,
@@ -177,9 +176,11 @@ final class LaunchCoordinator: BaseCoordinator,
         )
 
         let viewController = PortraitOnlyHostingController(rootView: view)
-        viewController.modalPresentationStyle = .fullScreen
+        // `.overFullScreen` is required to display the underlying view controller beneath the presented one,
+        // and to prevent a white strip glitch caused by synchronization issues between SwiftUI and UIKit.
+        viewController.modalPresentationStyle = .overFullScreen
         viewController.modalTransitionStyle = .crossDissolve
-        viewController.transitioningDelegate = modernTransitionDelegate
+        viewController.view.backgroundColor = .clear
 
         router.present(viewController, animated: true)
     }
@@ -300,9 +301,11 @@ final class LaunchCoordinator: BaseCoordinator,
         )
 
         let hostingController = PortraitOnlyHostingController(rootView: view)
-        hostingController.modalPresentationStyle = .fullScreen
+        // `.overFullScreen` is required to display the underlying view controller beneath the presented one,
+        // and to prevent a white strip glitch caused by synchronization issues between SwiftUI and UIKit.
+        hostingController.modalPresentationStyle = .overFullScreen
         hostingController.modalTransitionStyle = .crossDissolve
-        hostingController.transitioningDelegate = modernTransitionDelegate
+        hostingController.view.backgroundColor = .clear
 
         router.present(hostingController, animated: true)
     }
