769ec0ab1c1bea51ea5a6feecabd2b9644457922
Carson Ramsden <carsonramsden@gmail.com>
2025-11-11 09:58:29 -0700

    revert changes to skeleton tab index calculation

--- firefox-ios/Client/TabManagement/TabManager.swift
+++ firefox-ios/Client/TabManagement/TabManager.swift
@@ -25,7 +25,6 @@ protocol TabManager: AnyObject {
     var count: Int { get }
 
     var selectedTab: Tab? { get }
-    var selectedIndex: Int { get }
     var backupCloseTab: BackupCloseTab? { get set }
 
     var tabs: [Tab] { get }
