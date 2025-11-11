951ed600b7fcff259acf3f2932eb95cab81e8308
Foxbolts <Foxbolts3@gmail.com>
2025-11-10 23:44:22 -0500

    fix calculation

--- firefox-ios/Client/Frontend/StoriesFeed/StoriesFeedDimensionCalculator.swift
+++ firefox-ios/Client/Frontend/StoriesFeed/StoriesFeedDimensionCalculator.swift
@@ -8,12 +8,12 @@ struct StoriesFeedDimensionCalculator {
         // Portion of the container not occupied by insets
         let availableContainerWidth = containerWidth * StoriesFeedSectionLayoutProvider.UX.groupWidthRatio
         // # of cells that would fit in container
-        let cellsPerRow = availableContainerWidth / StoriesFeedSectionLayoutProvider.UX.minimumCellWidth
+        let cellsPerRow = floor(availableContainerWidth / StoriesFeedSectionLayoutProvider.UX.minimumCellWidth)
         // Amount of space used by inter-item spacing
         let spacingAdjustment = (cellsPerRow > 1 ?
                                  (cellsPerRow - 1) * StoriesFeedSectionLayoutProvider.UX.interItemSpacing : 0)
         // Available container width for cells
-        let adjustedContainerWidth = containerWidth - spacingAdjustment
+        let adjustedContainerWidth = availableContainerWidth - spacingAdjustment
         // Number of cells that will fit in a row, considering inter-item spacing
         let adjustedCellsPerRow = Int(adjustedContainerWidth / StoriesFeedSectionLayoutProvider.UX.minimumCellWidth)
         return max(StoriesFeedSectionLayoutProvider.UX.minimumCellsPerRow, adjustedCellsPerRow)
