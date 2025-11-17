9c508bf3d466dce8e8d40c34e9568db7e20b91e0
Ione Souza Junior <junior@ionixjunior.com.br>
2025-11-17 20:37:13 -0300

    Rename function

--- firefox-ios/Client/Frontend/Settings/PasswordDetailViewController.swift
+++ firefox-ios/Client/Frontend/Settings/PasswordDetailViewController.swift
@@ -157,7 +157,7 @@ extension PasswordDetailViewController: UITableViewDataSource {
             return passwordCell(tableView: tableView, indexPath: indexPath)
 
         case .website:
-            return cell(tableView: tableView, forWebsite: indexPath)
+            return websiteCell(tableView: tableView, forWebsite: indexPath)
 
         case .lastModifiedSeparator:
             return cell(tableView: tableView, forLastModifiedSeparator: indexPath)
@@ -243,7 +243,7 @@ extension PasswordDetailViewController: UITableViewDataSource {
         return loginCell
     }
 
-    private func cell(tableView: UITableView, forWebsite indexPath: IndexPath) -> UITableViewCell {
+    private func websiteCell(tableView: UITableView, forWebsite indexPath: IndexPath) -> UITableViewCell {
         guard let loginCell = cell(tableView: tableView, forIndexPath: indexPath) else {
             return UITableViewCell()
         }
