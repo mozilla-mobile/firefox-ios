f5f4c6397514855d4b4dd49ad31377c8aec3da2c
Tif Tran <ttran@mozilla.com>
2025-11-17 15:02:30 -0800

    Refactor DISCO-3580 [Experiments] Switch to use new app service RS api (#29433)

--- firefox-ios/Client/Experiments/Experiments.swift
+++ firefox-ios/Client/Experiments/Experiments.swift
@@ -250,8 +250,10 @@ enum Experiments {
             cannotUseAppleIntelligence: cannotUseAppleIntelligence()
         )
 
+        let profile: Profile = AppContainer.shared.resolve()
+        let remoteSettingsService = profile.remoteSettingsService
+
         return NimbusBuilder(dbPath: dbPath)
-            .with(url: remoteSettingsURL)
             .using(previewCollection: usePreviewCollection())
             .with(errorReporter: errorReporter)
             .with(initialExperiments: initialExperiments)
@@ -281,7 +283,10 @@ enum Experiments {
                     category: .experiments
                 )
             })
-            .build(appInfo: getAppSettings(isFirstRun: isFirstRun))
+            .build(
+                appInfo: getAppSettings(isFirstRun: isFirstRun),
+                remoteSettingsService: remoteSettingsService
+            )
     }
 
     /// A convenience method to initialize the `NimbusApi` object at startup.
