6cff921a18ad3abba07168c58122fbae0079b4b6
Issam Mani <issamouu69@gmail.com>
2025-11-19 13:37:14 +0100

    feat: add tests

---
+++ firefox-ios/firefox-ios-tests/Tests/ClientTests/TranslationsTests/MockTranslationsSchemeHandler.swift
@@ -0,0 +1,31 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import WebKit
+
+final class MockSchemeHandler: NSObject, WKURLSchemeHandler {
+    private(set) var startedURLs: [URL] = []
+
+    func webView(_ webView: WKWebView, start urlSchemeTask: WKURLSchemeTask) {
+        if let url = urlSchemeTask.request.url {
+            startedURLs.append(url)
+        }
+
+        // Optionally pretend the request succeeded so WebKit is satisfied.
+        guard let url = urlSchemeTask.request.url else { return }
+
+        let response = HTTPURLResponse(
+            url: url,
+            statusCode: 200,
+            httpVersion: "HTTP/1.1",
+            headerFields: ["Content-Type": "text/html"]
+        )!
+
+        urlSchemeTask.didReceive(response)
+        urlSchemeTask.didReceive(Data()) // empty body
+        urlSchemeTask.didFinish()
+    }
+
+    func webView(_ webView: WKWebView, stop urlSchemeTask: WKURLSchemeTask) { }
+}
---
+++ firefox-ios/firefox-ios-tests/Tests/ClientTests/TranslationsTests/TranslationsEngineTests.swift
@@ -0,0 +1,80 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import XCTest
+import WebKit
+@testable import Client
+
+@MainActor
+final class TranslationsEngineTests: XCTestCase {
+    func test_bridgeTo_reusesBridgeForSameWebView() {
+        let subject = createSubject()
+        let pageWebView = WKWebView()
+
+        let firstBridge = subject.bridge(to: pageWebView)
+        let secondBridge = subject.bridge(to: pageWebView)
+
+        XCTAssertTrue(firstBridge === secondBridge)
+    }
+
+    func test_bridgeTo_createsDifferentBridgesForDifferentWebViews() {
+        let subject = createSubject()
+        let firstWebView = WKWebView()
+        let secondWebView = WKWebView()
+
+        let firstBridge = subject.bridge(to: firstWebView)
+        let secondBridge = subject.bridge(to: secondWebView)
+
+        XCTAssertFalse(firstBridge === secondBridge)
+    }
+
+    func test_removeBridge_removesCachedBridge() {
+        let subject = createSubject()
+        let pageWebView = WKWebView()
+
+        let firstBridge = subject.bridge(to: pageWebView)
+
+        subject.removeBridge(for: pageWebView)
+
+        let secondBridge = subject.bridge(to: pageWebView)
+
+        XCTAssertFalse(firstBridge === secondBridge)
+    }
+
+    func test_removeBridge_isIdempotent() {
+        let subject = createSubject()
+        let pageWebView = WKWebView()
+
+        XCTAssertNoThrow {
+            subject.removeBridge(for: pageWebView)
+            subject.removeBridge(for: pageWebView)
+            subject.removeBridge(for: pageWebView)
+        }
+    }
+
+    func test_NSMapTable_dropsEntryWhenWebViewIsDeallocated() {
+        let engine = TranslationsEngine(schemeHandler: MockSchemeHandler())
+        // We need a weak reference so we can check that WKWebView actually deallocates.
+        weak var weakWebView: WKWebView?
+        
+        // Using an autoreleasepool just to mimic a context where something gets garbage collected
+        autoreleasepool {
+            var pageWebView = WKWebView()
+            weakWebView = pageWebView
+            // Adding the bridge stores the webview as a weak key in NSMapTable.
+            _ = engine.bridge(to: pageWebView)
+            XCTAssertEqual(engine.bridgeCount, 1)
+        }
+        
+        // Force the main runloop to spin. WKWebView teardown seems to happens asynchronously on the main runloop.
+        // Without this, the webview may stay alive for several cycles and NSMapTable won't clear its weak key yet.
+        // RunLoop.main.run(mode: .default, before: Date().addingTimeInterval(0.1))
+        XCTAssertNil(weakWebView, "WKWebView should have been deallocated")
+        XCTAssertEqual(engine.bridgeCount, 0)
+    }
+
+    private func createSubject() -> TranslationsEngine {
+        return TranslationsEngine(schemeHandler: MockSchemeHandler())
+    }
+}
