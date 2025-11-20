167d50856be443e72d5067ac3d308628f751ba24
cyndichin <cgc95@cornell.edu>
2025-11-20 12:03:02 -0500

    fix by adding Sendable

--- BrowserKit/Sources/ToolbarKit/LoadingConfig.swift
+++ BrowserKit/Sources/ToolbarKit/LoadingConfig.swift
@@ -2,7 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-public struct LoadingConfig: Equatable {
+public struct LoadingConfig: Equatable, Sendable {
     public let isLoading: Bool
     let a11yLabel: String
 
