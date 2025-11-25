24d79ebee44f6d6b106ddb2dab5328db7276edc1
Isabella <173110554+ih-codes@users.noreply.github.com>
2025-11-25 16:41:44 -0600

    Refactor FXIOS-12796 [Swift 6 Migration] Suppress userInfo NotificationService warning (#30898)
    * Suppress warning.
    
    * Remove unnecessary sending param.

--- firefox-ios/Extensions/NotificationService/NotificationService.swift
+++ firefox-ios/Extensions/NotificationService/NotificationService.swift
@@ -26,7 +26,8 @@ class NotificationService: UNNotificationServiceExtension {
         Viaduct.shared.useReqwestBackend()
         MozillaAppServices.initialize()
 
-        let userInfo = request.content.userInfo
+        // FIXME: FXIOS-14273 Dictionaries are non-Sendable
+        nonisolated(unsafe) let userInfo = request.content.userInfo
 
         guard let content = request.content.mutableCopy() as? UNMutableNotificationContent else {
             contentHandler(request.content)
