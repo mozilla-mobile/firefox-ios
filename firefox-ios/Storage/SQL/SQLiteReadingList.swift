fb1f1c982d83aba6b9af712c86b5a42828a1bf1f
ih-codes <ihugel@mozilla.com>
2025-11-28 15:51:48 -0600

    Refactor: Make some types in Storage Sendable and structs. Use auto created initializers.

--- firefox-ios/Storage/SQL/SQLiteReadingList.swift
+++ firefox-ios/Storage/SQL/SQLiteReadingList.swift
@@ -5,20 +5,19 @@
 import Foundation
 import Shared
 
-public final class ReadingListStorageError: MaybeErrorType {
+public struct ReadingListStorageError: MaybeErrorType, Sendable {
     let message: String
-    public init(_ message: String) {
-        self.message = message
-    }
+
     public var description: String {
         return message
     }
 }
 
-public final class SQLiteReadingList: Sendable {
-    let db: BrowserDB
+public struct SQLiteReadingList: Sendable {
+    private let db: BrowserDB
+    private let notificationCenter: NotificationCenter
 
-    let allColumns = [
+    private let allColumns = [
         "client_id",
         "client_last_modified",
         "id",
@@ -30,13 +29,6 @@ public final class SQLiteReadingList: Sendable {
         "favorite",
         "unread"
     ].joined(separator: ",")
-    let notificationCenter: NotificationCenter
-
-    public required init(db: BrowserDB,
-                         notificationCenter: NotificationCenter = NotificationCenter.default) {
-        self.db = db
-        self.notificationCenter = notificationCenter
-    }
 }
 
 extension SQLiteReadingList: ReadingList {
@@ -83,7 +75,7 @@ extension SQLiteReadingList: ReadingList {
             try connection.executeChange(insertSQL, withArgs: insertArgs)
 
             if connection.lastInsertedRowID == lastInsertedRowID {
-                throw ReadingListStorageError("Unable to insert ReadingListItem")
+                throw ReadingListStorageError(message: "Unable to insert ReadingListItem")
             }
 
             let querySQL = "SELECT \(self.allColumns) FROM items WHERE client_id = ? LIMIT 1"
@@ -100,7 +92,7 @@ extension SQLiteReadingList: ReadingList {
                 self.notificationCenter.post(name: .ReadingListUpdated, object: self)
                 return item
             } else {
-                throw ReadingListStorageError("Unable to get inserted ReadingListItem")
+                throw ReadingListStorageError(message: "Unable to get inserted ReadingListItem")
             }
         }
     }
@@ -113,7 +105,7 @@ extension SQLiteReadingList: ReadingList {
             if let item = items.first {
                 return Maybe(success: item)
             } else {
-                return Maybe(failure: ReadingListStorageError("Can't create RLCR from row"))
+                return Maybe(failure: ReadingListStorageError(message: "Can't create RLCR from row"))
             }
         }
         }
@@ -140,12 +132,12 @@ extension SQLiteReadingList: ReadingList {
                 self.notificationCenter.post(name: .ReadingListUpdated, object: self)
                 return item
             } else {
-                throw ReadingListStorageError("Unable to get updated ReadingListItem")
+                throw ReadingListStorageError(message: "Unable to get updated ReadingListItem")
             }
         }
     }
 
-    fileprivate class func ReadingListItemFactory(_ row: SDRow) -> ReadingListItem {
+    fileprivate static func ReadingListItemFactory(_ row: SDRow) -> ReadingListItem {
         guard let id = row["client_id"] as? Int,
               let url = row["url"] as? String,
               let title = row["title"] as? String,
