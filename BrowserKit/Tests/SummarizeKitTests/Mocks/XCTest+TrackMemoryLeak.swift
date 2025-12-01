a3363713b65936a026d5724dcacc1113e9525625
cyndichin <cgc95@cornell.edu>
2025-12-01 13:11:38 -0500

    Refactor FXIOS-14176 [Unit Tests] testing with errors in async context

--- BrowserKit/Tests/SummarizeKitTests/Mocks/XCTest+TrackMemoryLeak.swift
+++ BrowserKit/Tests/SummarizeKitTests/Mocks/XCTest+TrackMemoryLeak.swift
@@ -12,4 +12,21 @@ public extension XCTestCase {
             XCTAssertNil(object, "Memory leak detected in \(file):\(line)")
         }
     }
+
+    func assertAsyncThrows<E: Error, T>(
+        ofType expectedType: E.Type,
+        _ expression: () async throws -> T,
+        file: StaticString = #filePath,
+        line: UInt = #line,
+        verify: ((E) -> Void)? = nil
+    ) async {
+        do {
+            _ = try await expression()
+            XCTFail("Expected error \(expectedType), but no error thrown.", file: file, line: line)
+        } catch let error as E {
+            verify?(error)
+        } catch {
+            XCTFail("Expected error \(expectedType), but got \(error)", file: file, line: line)
+        }
+    }
 }
