b9945b377e2f7f3b54c9d5451a312ab08170550a
Isabella <173110554+ih-codes@users.noreply.github.com>
2025-12-02 11:05:57 -0600

    Refactor FXIOS-12796 [Swift 6 Migration] Fix Swift 6 errors in Client unit tests for `setUp` and `tearDown` - Batch 1 (#31010)
    Fix some easy setup / teardown isolation warnings that are strict swift 6 errors.

--- firefox-ios/firefox-ios-tests/Tests/ClientTests/BrowserViewControllerStateTests.swift
+++ firefox-ios/firefox-ios-tests/Tests/ClientTests/BrowserViewControllerStateTests.swift
@@ -11,16 +11,16 @@ import SummarizeKit
 final class BrowserViewControllerStateTests: XCTestCase, StoreTestUtility {
     let storeUtilityHelper = StoreTestUtilityHelper()
 
-    override func setUp() {
-        super.setUp()
+    override func setUp() async throws {
+        try await super.setUp()
         DependencyHelperMock().bootstrapDependencies()
         setupStore()
     }
 
-    override func tearDown() {
+    override func tearDown() async throws {
         DependencyHelperMock().reset()
         resetStore()
-        super.tearDown()
+        try await super.tearDown()
     }
 
     func testAddNewTabAction() {
