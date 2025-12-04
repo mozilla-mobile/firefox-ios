c874db9255c7b88f94eaf54987a18d78a0fb5c74
Sophie <samin@mozilla.com>
2025-12-03 17:39:48 -0800

    Update tests

--- BrowserKit/Tests/TabDataStoreTests/XCTestCaseExtension.swift
+++ BrowserKit/Tests/TabDataStoreTests/XCTestCaseExtension.swift
@@ -6,7 +6,7 @@ import Foundation
 import XCTest
 
 extension XCTestCase {
-    // TODO: Needs group discussion on solution for warnings
+    @MainActor
     func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
         addTeardownBlock { [weak instance] in
             XCTAssertNil(
