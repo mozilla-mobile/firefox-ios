e7016835d9ed1940cecf0e6f6668bda19e35a407
Clare So <1740517+clarmso@users.noreply.github.com>
2025-12-10 17:41:38 -0500

    iOS 26 for SearchTests

--- firefox-ios/firefox-ios-tests/Tests/XCUITests/SearchTest.swift
+++ firefox-ios/firefox-ios-tests/Tests/XCUITests/SearchTest.swift
@@ -180,7 +180,6 @@ class SearchTests: FeatureFlaggedTestBase {
         )
         navigator.goto(URLBarOpen)
         app.textFields[AccessibilityIdentifiers.Browser.AddressToolbar.searchTextField].waitAndTap()
-        urlBarAddress.waitAndTap()
 
         app.menuItems["Paste"].waitAndTap()
 
@@ -292,7 +291,17 @@ class SearchTests: FeatureFlaggedTestBase {
         // Select some text and long press to find the option
         app.webViews.staticTexts["cloud"].press(forDuration: 1)
         // Click on the > button to get to that option only on iPhone
-        if #available(iOS 16, *) {
+        if #available(iOS 26, *) {
+            while !app.collectionViews.buttons["Search with Firefox"].exists {
+                app.buttons["Forward"].firstMatch.waitAndTap()
+                waitForElementsToExist(
+                    [
+                        app.buttons["Forward"].firstMatch,
+                        app.collectionViews.buttons.firstMatch
+                    ]
+                )
+            }
+        } else if #available(iOS 16, *) {
             while !app.collectionViews.menuItems["Search with Firefox"].exists {
                 app.buttons["Forward"].firstMatch.waitAndTap()
                 waitForElementsToExist(
@@ -313,8 +322,11 @@ class SearchTests: FeatureFlaggedTestBase {
                 )
             }
         }
-
-        app.menuItems["Search with Firefox"].waitAndTap()
+        if #available(iOS 26, *) {
+            app.buttons["Search with Firefox"].waitAndTap()
+        } else {
+            app.menuItems["Search with Firefox"].waitAndTap()
+        }
         waitUntilPageLoad()
         let url = app.textFields[AccessibilityIdentifiers.Browser.AddressToolbar.searchTextField]
         mozWaitForValueContains(url, value: "google")
