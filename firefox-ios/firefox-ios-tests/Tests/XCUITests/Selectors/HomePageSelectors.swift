24a708261c2e3f84d1a8e0fcf382f1c610717fff
dragosb01 <134391433+dragosb01@users.noreply.github.com>
2026-01-29 21:56:18 +0200

    [MTE-5185] - fix testMultipleTabsPrivateBrowsingCloseEnabled smoke test (#31871)

--- firefox-ios/firefox-ios-tests/Tests/XCUITests/Selectors/HomePageSelectors.swift
+++ firefox-ios/firefox-ios-tests/Tests/XCUITests/Selectors/HomePageSelectors.swift
@@ -4,18 +4,26 @@
 
 protocol HomePageSelectorsSet {
     var COLLECTION_VIEW: Selector { get }
+    var TABS_BUTTON: Selector { get }
     var all: [Selector] { get }
 }
 
 struct HomePageSelectors: HomePageSelectorsSet {
     private enum IDs {
-        static let collectionView      = "FxCollectionView"
+        static let collectionView = "FxCollectionView"
+        static let tabsButton = AccessibilityIdentifiers.Toolbar.tabsButton
     }
 
     let COLLECTION_VIEW = Selector.collectionViewIdOrLabel(
         IDs.collectionView,
         description: "Firefox Home main collection view",
         groups: ["homepage"]
+    )
+
+    let TABS_BUTTON = Selector.buttonId(
+        IDs.tabsButton,
+        description: "Tabs button on Firefox Home",
+        groups: ["homepage", "toolbar"]
     )
 
     var all: [Selector] { [COLLECTION_VIEW] }
