60dc55c589bbf80766496c1d2389831a409f4977
lmarceau <lmarceau@mozilla.com>
2026-02-09 10:46:13 -0500

    Apply suggestion from @lmarceau

--- firefox-ios/firefox-ios-tests/Tests/ClientTests/Mocks/MockMozAdsClientFactory.swift
+++ firefox-ios/firefox-ios-tests/Tests/ClientTests/Mocks/MockMozAdsClientFactory.swift
@@ -3,6 +3,8 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
 import Foundation
+import MozillaAppServices
+
 @testable import Client
 
 final class MockMozAdsClientFactory: MozAdsClientFactory {
