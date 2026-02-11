dd22b2d5c521d3ec13cc7233b2e50f3f523e481d
Firefox Sync Engineering <sync-team@mozilla.com>
2026-02-11 10:33:16 +0000

    (Local AS flow) Nightly auto-update (149.0.20260210050259)

--- MozillaRustComponents/Sources/MozillaRustComponentsWrapper/Generated/Metrics/Metrics.swift
+++ MozillaRustComponents/Sources/MozillaRustComponentsWrapper/Generated/Metrics/Metrics.swift
@@ -22,6 +22,7 @@ extension GleanMetrics {
         private init() {
             // Intentionally left private, no external user can instantiate a new global object.
         }
+
         public static let info = BuildInfo(buildDate: DateComponents(calendar: Calendar.current, timeZone: TimeZone(abbreviation: "UTC"), year: 2026, month: 2, day: 10, hour: 5, minute: 18, second: 18))
     }
 
