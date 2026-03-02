e93666cbc7f997bfa20325cbc801f8a96991f328
Carson Ramsden <carsonramsden@gmail.com>
2026-03-02 14:47:21 -0700

    Delay keyboard work to the next run loop to improve performance

--- firefox-ios/Client/Frontend/Browser/BrowserViewController/Views/BrowserViewController.swift
+++ firefox-ios/Client/Frontend/Browser/BrowserViewController/Views/BrowserViewController.swift
@@ -4302,7 +4302,10 @@ class BrowserViewController: UIViewController,
             // we handle that in `showZeroSearchView()` and avoid embedding homepage here.
             let isPrivateMode = tabManager.selectedTab?.isPrivate ?? false
             if !isRecentOrTrendingSearchEnabled || isPrivateMode {
-                showEmbeddedHomepage(inline: false, isPrivate: isPrivateMode)
+                // Wait for the next run loop to embed homepage so that we give time for the keyboard to layout
+                DispatchQueue.main.async {
+                    self.showEmbeddedHomepage(inline: false, isPrivate: isPrivateMode)
+                }
             }
         }
 
@@ -5185,35 +5188,38 @@ extension BrowserViewController {
 
 extension BrowserViewController: KeyboardHelperDelegate {
     func keyboardHelper(_ keyboardHelper: KeyboardHelper, keyboardWillShowWithState state: KeyboardState) {
-        keyboardState = state
+        // Defer observer work to the next run loop to not compete with keyboard showing
+        DispatchQueue.main.async { [self] in
+            keyboardState = state
 
-        if !isSnapKitRemovalEnabled {
-            updateViewConstraints()
-        } else {
-            updateOverKeyboardContainerConstraints()
-            updateConstraintsForKeyboard()
-        }
+            if !isSnapKitRemovalEnabled {
+                updateViewConstraints()
+            } else {
+                updateOverKeyboardContainerConstraints()
+                updateConstraintsForKeyboard()
+            }
 
-        if isSwipingTabsEnabled {
-            addressToolbarContainer.hideSkeletonBars()
-        }
+            if isSwipingTabsEnabled {
+                addressToolbarContainer.hideSkeletonBars()
+            }
 
-        UIView.animate(
-            withDuration: state.animationDuration,
-            delay: 0,
-            options: [UIView.AnimationOptions(rawValue: UInt(state.animationCurve.rawValue << 16))],
-            animations: {
-                self.bottomContentStackView.layoutIfNeeded()
-            })
+            UIView.animate(
+                withDuration: state.animationDuration,
+                delay: 0,
+                options: [UIView.AnimationOptions(rawValue: UInt(state.animationCurve.rawValue << 16))],
+                animations: {
+                    self.bottomContentStackView.layoutIfNeeded()
+                })
 
-        if isToolbarTranslucencyRefactorEnabled {
-            // When animation duration is zero the keyboard is already showing and we don't need
-            // to update the toolbar again. This is the case when we are moving between fields in a form.
-            if state.animationDuration > 0 {
+            if isToolbarTranslucencyRefactorEnabled {
+                // When animation duration is zero the keyboard is already showing and we don't need
+                // to update the toolbar again. This is the case when we are moving between fields in a form.
+                if state.animationDuration > 0 {
+                    updateToolbarDisplay()
+                }
+            } else {
                 updateToolbarDisplay()
             }
-        } else {
-            updateToolbarDisplay()
         }
     }
 
