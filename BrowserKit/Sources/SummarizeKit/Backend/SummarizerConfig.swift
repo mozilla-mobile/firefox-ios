c8f71dc7c10270e685f53ae38c0e267926206ff4
Filippo Zazzeroni <fzazzeroni@mozilla.com>
2026-03-09 11:01:36 +0100

    Removed unused imports

--- BrowserKit/Sources/SummarizeKit/Backend/SummarizerConfig.swift
+++ BrowserKit/Sources/SummarizeKit/Backend/SummarizerConfig.swift
@@ -2,8 +2,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import Foundation
-
 /// A configuration container for a summarizer.
 public struct SummarizerConfig: Equatable, Sendable {
     public let instructions: String
