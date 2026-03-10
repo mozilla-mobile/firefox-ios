6f166b4a24742410feef4bce782b4cdf67f852b1
Firefox Sync Engineering <sync-team@mozilla.com>
2026-03-10 08:06:23 +0000

    (Local AS flow) Nightly auto-update (150.0.20260310050304)

--- MozillaRustComponents/Package.swift
+++ MozillaRustComponents/Package.swift
@@ -1,13 +1,13 @@
 // swift-tools-version: 5.10
 import PackageDescription
 
-let checksum = "cb92f7d9a2555fef7b048eabc00400c4e27f32f9503bc646ef0981648cae903c"
-let version = "150.0.20260306050311"
-let url = "https://firefox-ci-tc.services.mozilla.com/api/index/v1/task/project.application-services.v2.swift.150.20260306050311/artifacts/public/build/MozillaRustComponents.xcframework.zip"
+let checksum = "4f490e4a4ddb9ec3d22b5a1ec7d5aa6a9bdb553bbae3199f98319307db985a47"
+let version = "150.0.20260310050304"
+let url = "https://firefox-ci-tc.services.mozilla.com/api/index/v1/task/project.application-services.v2.swift.150.20260310050304/artifacts/public/build/MozillaRustComponents.xcframework.zip"
 
 // Focus xcframework
-let focusChecksum = "52429fd75518d47ee0fe50d2aa50cc3b4e4999d8878ae3deaaf96f9f7cb7a77b"
-let focusUrl = "https://firefox-ci-tc.services.mozilla.com/api/index/v1/task/project.application-services.v2.swift.150.20260306050311/artifacts/public/build/FocusRustComponents.xcframework.zip"
+let focusChecksum = "2c0fc32fb3b837e1f1e26173e657fdc0a01d660c8e85019d2b9146813ce5a812"
+let focusUrl = "https://firefox-ci-tc.services.mozilla.com/api/index/v1/task/project.application-services.v2.swift.150.20260310050304/artifacts/public/build/FocusRustComponents.xcframework.zip"
 
 let package = Package(
     name: "MozillaRustComponentsSwift",
--- MozillaRustComponents/Sources/MozillaRustComponentsWrapper/Generated/Metrics/Metrics.swift
+++ MozillaRustComponents/Sources/MozillaRustComponentsWrapper/Generated/Metrics/Metrics.swift
@@ -23,7 +23,7 @@ extension GleanMetrics {
             // Intentionally left private, no external user can instantiate a new global object.
         }
 
-        public static let info = BuildInfo(buildDate: DateComponents(calendar: Calendar.current, timeZone: TimeZone(abbreviation: "UTC"), year: 2026, month: 3, day: 6, hour: 5, minute: 24, second: 12))
+        public static let info = BuildInfo(buildDate: DateComponents(calendar: Calendar.current, timeZone: TimeZone(abbreviation: "UTC"), year: 2026, month: 3, day: 10, hour: 5, minute: 24, second: 45))
     }
 
     enum AdsClient {
