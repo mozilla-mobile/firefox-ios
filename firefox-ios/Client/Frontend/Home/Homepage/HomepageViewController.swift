d196affb8b4876dfd73a45d9a1393dac0cdecddb
Foxbolts <Foxbolts3@gmail.com>
2026-03-11 12:59:30 -0400

    update transition header on data source changes

--- firefox-ios/Client/Frontend/Home/Homepage/HomepageViewController.swift
+++ firefox-ios/Client/Frontend/Home/Homepage/HomepageViewController.swift
@@ -385,6 +385,7 @@ final class HomepageViewController: UIViewController,
                 jumpBackInDisplayConfig: getJumpBackInDisplayConfig()
             )
             updateWallpaperConstraints(availableWallpaperHeight: state.availableWallpaperHeight)
+            updateNewsTransitionHeaderProgress()
         }
 
         // FXIOS-11523 - Trigger impression when user opens homepage view new tab + scroll to top
