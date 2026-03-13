75509524cc53226ec2cca1202c9c58d74197766f
Foxbolts <Foxbolts3@gmail.com>
2026-03-13 09:29:20 -0400

    remove obsolete homepage test
    - Now that we have the firefox logo header, the homepage data source will always be non-empty and trigger sectionSeen telemetry
    - This test has been "broken" since we re-introduced the firefox logo header because we weren't triggering a layout pass, so even though the data source had data, the collection view wasn't updated to shown the firefox logo header section at the time of the impression telemetry check

--- firefox-ios/firefox-ios-tests/Tests/ClientTests/Frontend/Homepage/HomepageViewControllerTests.swift
+++ firefox-ios/firefox-ios-tests/Tests/ClientTests/Frontend/Homepage/HomepageViewControllerTests.swift
@@ -277,7 +277,7 @@ final class HomepageViewControllerTests: XCTestCase, StoreTestUtility {
         XCTAssertEqual(actionCalled.windowUUID, .XCTestDefaultUUID)
     }
 
-    func test_newState_forTriggeringImpression_triggersHomepageAction() async throws {
+    func test_newState_forTriggeringImpression_withPopulatedDataSource_triggersHomepageAction() async throws {
         let subject = createSubject()
         let initialState = HomepageState(windowUUID: .XCTestDefaultUUID)
 
@@ -307,30 +307,6 @@ final class HomepageViewControllerTests: XCTestCase, StoreTestUtility {
         let actionType = try XCTUnwrap(actionCalled.actionType as? HomepageActionType)
         XCTAssertEqual(actionType, HomepageActionType.sectionSeen)
         XCTAssertEqual(actionCalled.windowUUID, .XCTestDefaultUUID)
-    }
-
-    func test_newState_forTriggeringImpression_withNoVisibleSections_doesNotTriggersHomepageAction() throws {
-        let subject = createSubject()
-        let initialState = HomepageState(windowUUID: .XCTestDefaultUUID)
-
-        subject.loadViewIfNeeded()
-
-        let newState = HomepageState.reducer(
-            initialState,
-            GeneralBrowserAction(
-                windowUUID: .XCTestDefaultUUID,
-                actionType: GeneralBrowserActionType.didSelectedTabChangeToHomepage
-            )
-        )
-        subject.newState(state: newState)
-
-        XCTAssertTrue(newState.shouldTriggerImpression)
-        XCTAssertTrue(mockThrottler.didCallThrottle)
-        let homepageActions = mockStore.dispatchedActions.compactMap { $0 as? HomepageAction }
-        let sectionSeenAction = homepageActions.first(where: {
-            ($0.actionType as? HomepageActionType) == .sectionSeen
-        })
-        XCTAssertNil(sectionSeenAction)
     }
 
     func test_newState_didSelectedTabChangeToHomepageAction_forScrollToTop_setsCollectionViewOffsetToZero() {
