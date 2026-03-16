0694dba08c30807cc3ffef2f8c0c401802e4ecbe
Razvan Litianu <litianu.razvan@gmail.com>
2026-03-16 14:38:23 +0200

    Add TranslationPickerSettingsViewController unit tests

---
+++ firefox-ios/firefox-ios-tests/Tests/ClientTests/Settings/TranslationPickerSettingsViewControllerTests.swift
@@ -0,0 +1,134 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import XCTest
+
+@testable import Client
+
+@MainActor
+final class TranslationPickerSettingsViewControllerTests: XCTestCase, StoreTestUtility {
+    var mockStore: MockStoreForMiddleware<AppState>!
+
+    override func setUp() async throws {
+        try await super.setUp()
+        DependencyHelperMock().bootstrapDependencies()
+        setupStore()
+    }
+
+    override func tearDown() async throws {
+        DependencyHelperMock().reset()
+        resetStore()
+        try await super.tearDown()
+    }
+
+    // MARK: - Init
+
+    func test_init_noMemoryLeak() {
+        let subject = createSubject()
+        trackForMemoryLeaks(subject)
+    }
+
+    func test_init_setsTitle() {
+        let subject = createSubject()
+        XCTAssertEqual(subject.title, .Settings.Translation.Title)
+    }
+
+    // MARK: - newState
+
+    func test_newState_withTranslationsEnabled_doesNotCrash() {
+        let subject = createSubject()
+        subject.loadViewIfNeeded()
+        let state = TranslationSettingsState(
+            windowUUID: .XCTestDefaultUUID,
+            isTranslationsEnabled: true,
+            preferredLanguages: ["en", "fr"],
+            supportedLanguages: ["en", "fr", "de"]
+        )
+        subject.newState(state: state)
+    }
+
+    func test_newState_withTranslationsDisabled_doesNotCrash() {
+        let subject = createSubject()
+        subject.loadViewIfNeeded()
+        let state = TranslationSettingsState(
+            windowUUID: .XCTestDefaultUUID,
+            isTranslationsEnabled: false,
+            preferredLanguages: [],
+            supportedLanguages: []
+        )
+        subject.newState(state: state)
+    }
+
+    func test_newState_calledTwice_doesNotCrash() {
+        let subject = createSubject()
+        subject.loadViewIfNeeded()
+        subject.newState(state: TranslationSettingsState(
+            windowUUID: .XCTestDefaultUUID,
+            isTranslationsEnabled: true,
+            preferredLanguages: ["en"],
+            supportedLanguages: ["en", "fr"]
+        ))
+        subject.newState(state: TranslationSettingsState(
+            windowUUID: .XCTestDefaultUUID,
+            isTranslationsEnabled: false,
+            preferredLanguages: [],
+            supportedLanguages: []
+        ))
+    }
+
+    // MARK: - nativeName
+
+    func test_nativeName_returnsNonEmptyString() {
+        let name = TranslationPickerSettingsViewController.nativeName(for: "en")
+        XCTAssertFalse(name.isEmpty)
+    }
+
+    func test_nativeName_forUnknownCode_returnsFallback() {
+        let name = TranslationPickerSettingsViewController.nativeName(for: "xyz")
+        XCTAssertEqual(name, "xyz")
+    }
+
+    // MARK: - localizedName
+
+    func test_localizedName_returnsNonEmptyString() {
+        let name = TranslationPickerSettingsViewController.localizedName(for: "fr")
+        XCTAssertFalse(name.isEmpty)
+    }
+
+    func test_localizedName_forUnknownCode_returnsFallback() {
+        let name = TranslationPickerSettingsViewController.localizedName(for: "xyz")
+        XCTAssertEqual(name, "xyz")
+    }
+
+    // MARK: - StoreTestUtility
+
+    func setupAppState() -> AppState {
+        return AppState(
+            activeScreens: ActiveScreensState(
+                screens: [
+                    .translationSettings(
+                        TranslationSettingsState(windowUUID: .XCTestDefaultUUID)
+                    )
+                ]
+            )
+        )
+    }
+
+    func setupStore() {
+        mockStore = MockStoreForMiddleware(state: setupAppState())
+        StoreTestUtilityHelper.setupStore(with: mockStore)
+    }
+
+    func resetStore() {
+        StoreTestUtilityHelper.resetStore()
+    }
+
+    // MARK: - Helpers
+
+    private func createSubject() -> TranslationPickerSettingsViewController {
+        let subject = TranslationPickerSettingsViewController(windowUUID: .XCTestDefaultUUID)
+        trackForMemoryLeaks(subject)
+        return subject
+    }
+}
