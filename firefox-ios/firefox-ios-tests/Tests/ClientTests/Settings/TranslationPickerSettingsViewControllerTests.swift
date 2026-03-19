2ad45f499996c1b6339c93bf598409a6f9dd2c06
Razvan Litianu <litianu.razvan@gmail.com>
2026-03-19 10:20:58 +0200

    Remove low-value doesNotCrash tests from TranslationPickerSettingsViewControllerTests

--- firefox-ios/firefox-ios-tests/Tests/ClientTests/Settings/TranslationPickerSettingsViewControllerTests.swift
+++ firefox-ios/firefox-ios-tests/Tests/ClientTests/Settings/TranslationPickerSettingsViewControllerTests.swift
@@ -59,22 +59,6 @@ final class TranslationPickerSettingsViewControllerTests: XCTestCase, StoreTestU
         ))
     }
 
-    // MARK: - viewDidDisappear
-
-    func test_viewDidDisappear_doesNotCrash() {
-        let subject = createSubject()
-        subject.loadViewIfNeeded()
-        subject.viewDidDisappear(false)
-    }
-
-    // MARK: - applyTheme
-
-    func test_applyTheme_doesNotCrash() {
-        let subject = createSubject()
-        subject.loadViewIfNeeded()
-        subject.applyTheme()
-    }
-
     // MARK: - collectionView delegate
 
     func test_shouldSelectItem_returnsFalse() {
