9ea1ec2d57adaac26304dd845d47e7540cc65a3e
Razvan Litianu <litianu.razvan@gmail.com>
2026-03-19 10:02:21 +0200

    Tidy translation settings picker and tests
    Make picker settings controller initialization more explicit, simplify middleware task dispatch, and annotate translation settings reducer tests to run on the main actor.
    
    # Conflicts:
    #	firefox-ios/Client/Frontend/Settings/Translation/TranslationPickerSettingsViewController.swift

--- firefox-ios/firefox-ios-tests/Tests/ClientTests/Settings/TranslationSettingsStateTests.swift
+++ firefox-ios/firefox-ios-tests/Tests/ClientTests/Settings/TranslationSettingsStateTests.swift
@@ -37,7 +37,6 @@ final class TranslationSettingsStateTests: XCTestCase {
 
     // MARK: - Reducer Tests - didLoadSettings
 
-    @MainActor
     func test_didLoadSettings_updatesAllValues() {
         let initialState = createSubject()
         let reducer = translationSettingsReducer()
@@ -57,7 +56,6 @@ final class TranslationSettingsStateTests: XCTestCase {
         XCTAssertEqual(newState.supportedLanguages, ["en", "fr", "de"])
     }
 
-    @MainActor
     func test_didLoadSettings_withNilValues_preservesExistingState() {
         let initialState = TranslationSettingsState(
             windowUUID: .XCTestDefaultUUID,
@@ -81,7 +79,6 @@ final class TranslationSettingsStateTests: XCTestCase {
 
     // MARK: - Reducer Tests - didUpdateSettings
 
-    @MainActor
     func test_didUpdateSettings_updatesTranslationsEnabled() {
         let initialState = createSubject()
         let reducer = translationSettingsReducer()
@@ -99,7 +96,6 @@ final class TranslationSettingsStateTests: XCTestCase {
         XCTAssertEqual(newState.supportedLanguages, initialState.supportedLanguages)
     }
 
-    @MainActor
     func test_didUpdateSettings_preservesLanguagesWhenNil() {
         let initialState = TranslationSettingsState(
             windowUUID: .XCTestDefaultUUID,
@@ -179,7 +175,6 @@ final class TranslationSettingsStateTests: XCTestCase {
 
     // MARK: - Edge Cases
 
-    @MainActor
     func test_unknownAction_returnsDefaultState() {
         let initialState = createSubject()
         let reducer = translationSettingsReducer()
@@ -199,7 +194,6 @@ final class TranslationSettingsStateTests: XCTestCase {
         XCTAssertEqual(newState, initialState)
     }
 
-    @MainActor
     func test_actionWithDifferentWindowUUID_returnsDefaultState() {
         let initialState = createSubject()
         let reducer = translationSettingsReducer()
