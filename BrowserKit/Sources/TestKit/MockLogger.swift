7ecd5685c4c77688e6572c578ab4fb880400fe14
cyndichin <cgc95@cornell.edu>
2026-03-24 06:53:56 -0400

    Refactor FXIOS-15122 [Quick Answers] LiteLLM code to its own package

---
+++ BrowserKit/Sources/TestKit/MockLogger.swift
@@ -0,0 +1,36 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import Common
+
+public final class MockLogger: Logger, @unchecked Sendable {
+    public var crashedLastLaunch = false
+    var savedMessage: String?
+    var savedLevel: LoggerLevel?
+    var savedCategory: LoggerCategory?
+    var savedExtra: [String: String]?
+
+    public init() { }
+
+    public func setup(sendCrashReports: Bool) {}
+    public func copyLogsToDocuments() {}
+    public func logCustomError(error: Error) {}
+    public func deleteCachedLogFiles() {}
+
+    public func log(
+        _ message: String,
+        level: LoggerLevel,
+        category: LoggerCategory,
+        extra: [String: String]? = nil,
+        description: String? = nil,
+        file: String = #filePath,
+        function: String = #function,
+        line: Int = #line
+    ) {
+        savedMessage = message
+        savedLevel = level
+        savedCategory = category
+        savedExtra = extra
+    }
+}
