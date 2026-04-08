101de2240cf045ee59664de282c9f71af5c1655a
Razvan Litianu <litianu.razvan@gmail.com>
2026-04-08 17:35:36 +0300

    Fix SwiftLint vertical whitespace in Strings.swift

--- firefox-ios/Shared/Strings.swift
+++ firefox-ios/Shared/Strings.swift
@@ -3388,7 +3388,6 @@ extension String {
                     value: "Add Language…",
                     comment: "Row label in the preferred languages list that opens the language picker to add a new preferred language for translation."
                 )
-
             }
 
             public struct AutoTranslate {
