94599717bdb3b6fa1d4d33337c62d9a08da3a063
kartikayy007 <kartikayy6969@gmail.com>
2026-04-15 20:49:20 +0530

    Fix #33095: Convert TabCounterTests to new framework

--- firefox-ios/firefox-ios-tests/Tests/XCUITests/TabCounterTests.swift
+++ firefox-ios/firefox-ios-tests/Tests/XCUITests/TabCounterTests.swift
@@ -2,56 +2,49 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import Common
 import XCTest
 
 class TabCounterTests: BaseTestCase {
     // https://mozilla.testrail.io/index.php?/cases/view/2359077
     func testTabIncrement() {
+        let toolbar = ToolbarScreen(app: app)
+
         navigator.nowAt(NewTabScreen)
         waitForTabsButton()
 
-        var tabsOpen = app.buttons[AccessibilityIdentifiers.Toolbar.tabsButton].value
-        XCTAssertEqual("1", tabsOpen as? String)
+        toolbar.assertTabsButtonValue(expectedCount: "1")
 
         navigator.createNewTab()
         navigator.nowAt(NewTabScreen)
         waitForTabsButton()
 
-        tabsOpen = app.buttons[AccessibilityIdentifiers.Toolbar.tabsButton].value
-        XCTAssertEqual("2", tabsOpen as? String)
+        toolbar.assertTabsButtonValue(expectedCount: "2")
     }
 
     // https://mozilla.testrail.io/index.php?/cases/view/2359078
     func testTabDecrement() {
+        let toolbar = ToolbarScreen(app: app)
+        let tabTray = TabTrayScreen(app: app)
+
         navigator.nowAt(NewTabScreen)
         waitForTabsButton()
 
-        var tabsOpen = app.buttons[AccessibilityIdentifiers.Toolbar.tabsButton].value
-        XCTAssertEqual("1", tabsOpen as? String)
+        toolbar.assertTabsButtonValue(expectedCount: "1")
 
         navigator.createNewTab()
         navigator.nowAt(NewTabScreen)
         waitForTabsButton()
 
-        tabsOpen = app.buttons[AccessibilityIdentifiers.Toolbar.tabsButton].value
-        XCTAssertEqual("2", tabsOpen as? String)
+        toolbar.assertTabsButtonValue(expectedCount: "2")
 
         navigator.goto(TabTray)
-        if iPad() {
-            app.cells.buttons[StandardImageIdentifiers.Large.cross].firstMatch.waitAndTap()
-        } else {
-            app.otherElements[tabsTray]
-                .collectionViews.cells.element(boundBy: 0)
-                .buttons[AccessibilityIdentifiers.TabTray.closeButton].waitAndTap()
-        }
+        tabTray.closeFirstTab()
 
         app.otherElements[tabsTray].cells.element(boundBy: 0).waitAndTap()
         navigator.nowAt(NewTabScreen)
         waitForTabsButton()
 
-        tabsOpen = app.buttons[AccessibilityIdentifiers.Toolbar.tabsButton].value
-        XCTAssertEqual("1", tabsOpen as? String)
+        toolbar.assertTabsButtonValue(expectedCount: "1")
 
         navigator.goto(TabTray)
         XCTAssertEqual(app.cells.count, 1, "There should be only one tab in the tab tray")
