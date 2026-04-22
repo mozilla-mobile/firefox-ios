7e0312dd7d77117aa01dd4918fb56cb9a4e5868d
cyndichin <cgc95@cornell.edu>
2026-04-22 08:54:10 -0400

    Add FXIOS-15146 [Quick Answers] creating llm client in results service

--- BrowserKit/Sources/QuickAnswersKit/Backend/ResultsService/ResultsServiceFactory.swift
+++ BrowserKit/Sources/QuickAnswersKit/Backend/ResultsService/ResultsServiceFactory.swift
@@ -5,27 +5,42 @@
 import Foundation
 import MLPAKit
 import LLMKit
+import Shared
 
 // MARK: - Protocol
 /// Creates a ResultsService with using MLPA (App Attest) authentication and LiteLLM.
 protocol ResultsServiceFactory {
-    func make() -> ResultsService
+    func make(prefs: Prefs, config: QuickAnswersConfig) -> ResultsService?
 }
 
 // MARK: - Default Implementation
 public struct DefaultResultsServiceFactory: ResultsServiceFactory {
     let config: QuickAnswersConfig
+    let liteLLMCreator: LiteLLMCreating
 
-    func make() -> ResultsService {
-        // TODO: FXIOS-15196 - Create Results Service from the LiteLLMClient, remove optional when implementing appropriate
-        // LiteLLMClient
-        let client = makeLiteLLMClient()
+    public init(
+        config: QuickAnswersConfig,
+        liteLLMCreator: LiteLLMCreating
+    ) {
+        self.config = config
+        self.liteLLMCreator = liteLLMCreator
+    }
+
+    func make(
+        prefs: Prefs,
+        config: QuickAnswersConfig = QuickAnswersConfig()
+    ) -> ResultsService? {
+        guard let model = config.options["model"] as? String, !model.isEmpty,
+              let client = makeLiteLLMClient(prefs: prefs)
+        else {
+            return nil
+        }
+
         return DefaultResultsService(client: client, config: config)
     }
 
     // MARK: - Private Helpers
-    private func makeLiteLLMClient() -> LiteLLMClient? {
-        // TODO: FXIOS-15196 - Create LiteLLMClient with proper configurations and authenticator
-        return nil
+    private func makeLiteLLMClient(prefs: Prefs) -> LiteLLMClientProtocol? {
+        return liteLLMCreator.createAppAttestLiteLLM(using: prefs)
     }
 }
