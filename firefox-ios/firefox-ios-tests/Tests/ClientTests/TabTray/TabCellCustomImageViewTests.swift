6a84bf1a6dcd46b4408616893afb42be0a6f93d2
Alexandru Farcasanu <afarcasanu@mozilla.com>
2026-04-24 13:22:02 +0300

    Added tests for TabCellCustomImage

---
+++ firefox-ios/firefox-ios-tests/Tests/ClientTests/TabTray/TabCellCustomImageViewTests.swift
@@ -0,0 +1,25 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import XCTest
+
+@testable import Client
+
+final class TabCellCustomImageTests: XCTestCase {
+    override func setUp() async throws {
+        try await super.setUp()
+        await DependencyHelperMock().bootstrapDependencies()
+    }
+
+    override func tearDown() async throws {
+        DependencyHelperMock().reset()
+        try await super.tearDown()
+    }
+
+    @MainActor
+    private func testForMemoryLeaks() {
+        let subject = TabCellCustomImage()
+        trackForMemoryLeaks(subject)
+    }
+}
