35d705e480ba3b3b20948c431133ce3c9f0ea451
Alexandru Farcasanu <afarcasanu@mozilla.com>
2026-04-27 10:51:40 +0300

    Added test for TabCellCustomImage

--- firefox-ios/firefox-ios-tests/Tests/ClientTests/TabTray/TabCellCustomImageViewTests.swift
+++ firefox-ios/firefox-ios-tests/Tests/ClientTests/TabTray/TabCellCustomImageViewTests.swift
@@ -3,7 +3,6 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
 import XCTest
-
 @testable import Client
 
 final class TabCellCustomImageTests: XCTestCase {
@@ -18,8 +17,31 @@ final class TabCellCustomImageTests: XCTestCase {
     }
 
     @MainActor
-    private func testForMemoryLeaks() {
+    func testUpdateContentsRect_SquareImageInSquareView() {
+        let size = CGSize(width: 100, height: 100)
+        let subject = createSubject()
+        subject.frame = CGRect(origin: .zero, size: size)
+        subject.image = createTestImage(size: size)
+
+        subject.layoutSubviews()
+
+        XCTAssertEqual(subject.layer.contentsRect.height, 1.0, accuracy: 0.001)
+        XCTAssertEqual(subject.layer.contentsRect.origin.y, 0, "Image should be alligned from top")
+    }
+
+    // MARK: - Helper
+    private func createTestImage(size: CGSize) -> UIImage {
+        UIGraphicsBeginImageContext(size)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image ?? UIImage()
+    }
+
+    // MARK: - Subject
+    @MainActor
+    private func createSubject() -> TabCellCustomImage {
         let subject = TabCellCustomImage()
         trackForMemoryLeaks(subject)
+        return subject
     }
 }
