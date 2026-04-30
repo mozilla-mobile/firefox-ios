76ef6bd11dbfd0c47a280b95ec1378cb7fd21694
Filippo Zazzeroni <fzazzeroni@mozilla.com>
2026-04-30 18:35:35 +0200

    lint

--- firefox-ios/Client/Frontend/Home/Homepage/HomepageDiffableDataSource.swift
+++ firefox-ios/Client/Frontend/Home/Homepage/HomepageDiffableDataSource.swift
@@ -9,7 +9,7 @@ typealias HomepageSection = HomepageDiffableDataSource.HomeSection
 typealias HomepageItem = HomepageDiffableDataSource.HomeItem
 
 /// Holds the data source configuration for the new homepage as part of the rebuild project
-final class HomepageDiffableDataSource: UICollectionViewDiffableDataSource<HomepageSection, HomepageItem>,                                          FeatureFlaggable {
+final class HomepageDiffableDataSource: UICollectionViewDiffableDataSource<HomepageSection, HomepageItem>, FeatureFlaggable {
     typealias TextColor = UIColor
     typealias NumberOfTilesPerRow = Int
     typealias ShouldShowSectionHeader = Bool
