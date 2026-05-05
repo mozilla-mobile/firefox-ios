d31ae61347cfd113e26e7909068601dfa371d0fa
Hasik Choi <hotcs6@naver.com>
2026-05-05 16:43:03 +0900

    Refactor FXIOS-15630 [UI Tests] Migrate toolbarmenu tests to TAE (#33506)
    * Add MainMenu sign-in selector
    
    * Migrate ToolbarMenuTests to TAE
    
    - add ToolbarScreen.assertForwardButtonExists
    - add MainMenuScreen.assertMenuOptionsExist, dismissMenu, assertMenuIsDismissed
    - add getter for tabsButton, forwardButton in ToolbarScreen
    
    * Add Passwords button in MainMenu Options assertion

--- firefox-ios/firefox-ios-tests/Tests/XCUITests/PageScreens/ToolbarScreen.swift
+++ firefox-ios/firefox-ios-tests/Tests/XCUITests/PageScreens/ToolbarScreen.swift
@@ -16,6 +16,7 @@ final class ToolbarScreen {
 
     private var tabsButton: XCUIElement { sel.TABS_BUTTON.element(in: app) }
     private var newTabButton: XCUIElement { sel.NEW_TAB_BUTTON.element(in: app)}
+    private var forwardButton: XCUIElement { sel.FORWARD_BUTTON.element(in: app)}
     private var backButton: XCUIElement { sel.BACK_BUTTON.element(in: app)}
     private var tabToolbarMenuButton: XCUIElement { sel.TABTOOLBAR_MENUBUTTON.element(in: app) }
     private var shareButton: XCUIElement { sel.SHARE_BUTTON.element(in: app) }
@@ -92,6 +93,10 @@ final class ToolbarScreen {
         BaseTestCase().mozWaitForElementToExist(backButton)
     }
 
+	func assertForwardButtonExists() {
+		BaseTestCase().mozWaitForElementToExist(forwardButton)
+	}
+
     func tapBackButton() {
         backButton.waitAndTap()
     }
@@ -130,6 +135,16 @@ final class ToolbarScreen {
         let settingMenuButton = sel.SETTINGS_MENU_BUTTON.element(in: app)
         BaseTestCase().mozWaitForElementToExist(settingMenuButton, timeout: TIMEOUT)
         return settingMenuButton
+    }
+
+    func getTabsButtonElement() -> XCUIElement {
+        BaseTestCase().mozWaitForElementToExist(tabsButton)
+        return tabsButton
+    }
+
+    func getForwardButtonElement() -> XCUIElement {
+        BaseTestCase().mozWaitForElementToExist(forwardButton)
+        return forwardButton
     }
 
     func tapShareButton() {
