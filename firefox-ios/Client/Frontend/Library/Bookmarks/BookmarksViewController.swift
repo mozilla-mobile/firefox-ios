f05e04c463085b5f19f69c2e236863d32ffe2de8
Winnie Teichmann <4530+thatswinnie@users.noreply.github.com>
2026-05-08 10:26:07 +0200

    Optimizing code

--- firefox-ios/Client/Frontend/Library/Bookmarks/BookmarksViewController.swift
+++ firefox-ios/Client/Frontend/Library/Bookmarks/BookmarksViewController.swift
@@ -53,7 +53,10 @@ final class BookmarksViewController: SiteTableViewController,
               viewModel.bookmarkFolderGUID != LocalDesktopFolder.localDesktopFolderGuid
         else { return [] }
 
-        return toolbarButtonItems
+        let items = toolbarButtonItems
+        applyThemeToButtons()
+
+        return items
     }
 
     private var isBookmarksSearchEnabled: Bool {
@@ -70,30 +73,18 @@ final class BookmarksViewController: SiteTableViewController,
         case .bookmarks(state: .mainView),
              .bookmarks(state: .inFolder):
             bottomRightButton.title = .BookmarksEdit
-            if #available(iOS 26.0, *) {
-                bottomRightButton.tintColor = currentTheme().colors.textPrimary
-            }
             return searchItems + [flexibleSpace, bottomRightButton]
         case .bookmarks(state: .search):
             return searchItems + [flexibleSpace]
         case .bookmarks(state: .inFolderEditMode):
             bottomRightButton.title = String.AppSettingsDone
-            if #available(iOS 26.0, *) {
-                bottomRightButton.tintColor = currentTheme().colors.textAccent
-            }
             return [bottomLeftButton, flexibleSpace, bottomRightButton]
         case .bookmarks(state: .itemEditMode):
             bottomRightButton.title = String.AppSettingsDone
-            if #available(iOS 26.0, *) {
-                bottomRightButton.tintColor = currentTheme().colors.textAccent
-            }
             bottomRightButton.isEnabled = true
             return [flexibleSpace, bottomRightButton]
         case .bookmarks(state: .itemEditModeInvalidField):
             bottomRightButton.title = String.AppSettingsDone
-            if #available(iOS 26.0, *) {
-                bottomRightButton.tintColor = currentTheme().colors.textAccent
-            }
             bottomRightButton.isEnabled = false
             return [flexibleSpace, bottomRightButton]
         default:
