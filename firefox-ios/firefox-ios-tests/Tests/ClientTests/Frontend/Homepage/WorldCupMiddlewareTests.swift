6966d56832a35afed4013f5161b7e1f21f29aad7
Issam Mani <issamouu69@gmail.com>
2026-05-18 23:00:32 +0200

    Bugfix FXIOS-15834 [WC] TBD shouldn't show in group stages (#33872)

--- firefox-ios/firefox-ios-tests/Tests/ClientTests/Frontend/Homepage/WorldCupMiddlewareTests.swift
+++ firefox-ios/firefox-ios-tests/Tests/ClientTests/Frontend/Homepage/WorldCupMiddlewareTests.swift
@@ -720,7 +720,8 @@ final class WorldCupMiddlewareTests: XCTestCase, StoreTestUtility {
                            home: String,
                            away: String,
                            date: String = "2026-06-12T18:00:00+00:00",
-                           statusType: String = "scheduled") -> WorldCupMatchesResponse.Match {
+                           statusType: String = "scheduled",
+                           stage: String? = "Group Stage") -> WorldCupMatchesResponse.Match {
         let homeTeam = WorldCupMatchesResponse.Team(
             key: home, name: home, iconUrl: nil, group: "Group A", eliminated: false
         )
@@ -740,7 +741,8 @@ final class WorldCupMiddlewareTests: XCTestCase, StoreTestUtility {
             homePenalty: nil,
             awayPenalty: nil,
             clock: nil,
-            statusType: statusType
+            statusType: statusType,
+            stage: stage
         )
     }
 
