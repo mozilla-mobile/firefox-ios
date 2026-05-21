1b021dd0a6b9d7aba135d68c2dcc230a5745846a
Filippo Zazzeroni <fzazzeroni@mozilla.com>
2026-05-21 12:10:24 +0200

    fix: remove default values

--- firefox-ios/Client/Frontend/Home/Homepage/WorldCup/WorldCupStore.swift
+++ firefox-ios/Client/Frontend/Home/Homepage/WorldCup/WorldCupStore.swift
@@ -70,13 +70,11 @@ struct WorldCupStore: WorldCupStoreProtocol, FeatureFlaggable {
     }
 
     var isMilestone2: Bool {
-        return true
         guard let enableDate = milestone2EnableDate else { return false }
         return dateProvider.now() >= enableDate
     }
 
     var hasWorldCupStarted: Bool {
-        return true
         guard let startDate = worldCupStartDate else { return false }
         return dateProvider.now() >= startDate
     }
