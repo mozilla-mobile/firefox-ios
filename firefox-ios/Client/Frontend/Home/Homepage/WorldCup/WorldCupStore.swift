83fac91f18f8dd979ac6f4a0e748d86addba6954
Filippo Zazzeroni <fzazzeroni@mozilla.com>
2026-05-27 14:33:07 +0200

    fix: remove world cup store debug change

--- firefox-ios/Client/Frontend/Home/Homepage/WorldCup/WorldCupStore.swift
+++ firefox-ios/Client/Frontend/Home/Homepage/WorldCup/WorldCupStore.swift
@@ -79,7 +79,6 @@ struct WorldCupStore: WorldCupStoreProtocol, FeatureFlaggable {
     }
 
     var isMilestone2: Bool {
-        return true
         guard let enableDate = milestone2EnableDate else { return false }
         return dateProvider.now() >= enableDate
     }
