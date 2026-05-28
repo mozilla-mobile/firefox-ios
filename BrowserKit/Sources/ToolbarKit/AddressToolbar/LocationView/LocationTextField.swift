b599e03f85ea3bd3106164816c007cbf6c16d506
Alexandru Farcasanu <afarcasanu@mozilla.com>
2026-05-28 10:55:16 +0300

    FXIOS-15776 #33742 ⁃ Dual Keyboard Not Functioning Correctly on iPhone Firefox

--- BrowserKit/Sources/ToolbarKit/AddressToolbar/LocationView/LocationTextField.swift
+++ BrowserKit/Sources/ToolbarKit/AddressToolbar/LocationView/LocationTextField.swift
@@ -69,7 +69,8 @@ final class LocationTextField: UITextField, UITextFieldDelegate, ThemeApplicable
         // Setting the content type to a field that is not related to AutoFill functionality
         // like email and password, should disable the Operating system to load those content,
         // hence having a faster keyboard start up the first time
-        textContentType = .URL
+        // Avoid using '.URL' which disrupts bilingual keyboards (e.g., Korean) with aggressive web-autocorrect.
+        textContentType = UITextContentType(rawValue: "")
         delegate = self
 
         // Disable dragging urls on iPhones because it conflicts with editing the text
