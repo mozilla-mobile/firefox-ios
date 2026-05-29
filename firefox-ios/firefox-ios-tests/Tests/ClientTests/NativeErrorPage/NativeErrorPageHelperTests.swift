39a1a96c43196fdd9d3eec15c6eacdf68e1a88c8
anannyashreesharma <ananyashreesharma19@gmail.com>
2026-05-29 17:24:22 +0530

    Updated NativeErrorPageHelperTests coverage

--- firefox-ios/firefox-ios-tests/Tests/ClientTests/NativeErrorPage/NativeErrorPageHelperTests.swift
+++ firefox-ios/firefox-ios-tests/Tests/ClientTests/NativeErrorPage/NativeErrorPageHelperTests.swift
@@ -109,4 +109,153 @@ final class NativeErrorPageHelperTests: XCTestCase {
         let url = URL(string: "http://example.com/errorpage?certerror=SSL_ERROR_BAD_CERT_DOMAIN")!
         XCTAssertFalse(NativeErrorPageHelper.isBadCertDomainErrorURL(url))
     }
+
+    // MARK: - buildErrorPageQueryItems
+
+    func testBuildErrorPageQueryItems_nonCertError_returnsURLAndCodeOnly() {
+        let url = URL(string: "https://example.com")!
+        let noInternetCode = Int(CFNetworkErrors.cfurlErrorNotConnectedToInternet.rawValue)
+        let error = NSError(domain: NSURLErrorDomain, code: noInternetCode, userInfo: [:])
+
+        let items = NativeErrorPageHelper.buildErrorPageQueryItems(for: error, url: url)
+
+        XCTAssertEqual(items.count, 2)
+        XCTAssertTrue(items.contains(where: { $0.name == "url" && $0.value == url.absoluteString }))
+        XCTAssertTrue(items.contains(where: { $0.name == "code" && $0.value == String(noInternetCode) }))
+    }
+
+    func testBuildErrorPageQueryItems_certErrorWithCFStreamCode_includesCertErrorQueryItem() {
+        let url = URL(string: "https://example.com")!
+        let error = makeBadCertDomainError()
+
+        let items = NativeErrorPageHelper.buildErrorPageQueryItems(for: error, url: url)
+
+        XCTAssertTrue(items.contains(where: { $0.name == "certerror" && $0.value == "SSL_ERROR_BAD_CERT_DOMAIN" }))
+        XCTAssertTrue(items.contains(where: { $0.name == "code" }))
+    }
+
+    func testBuildErrorPageQueryItems_certErrorWithoutCFStreamCode_doesNotIncludeCertErrorItem() {
+        let url = URL(string: "https://example.com")!
+        let error = NSError(
+            domain: NSURLErrorDomain,
+            code: NSURLErrorServerCertificateUntrusted,
+            userInfo: [:]
+        )
+
+        let items = NativeErrorPageHelper.buildErrorPageQueryItems(for: error, url: url)
+
+        XCTAssertFalse(items.contains(where: { $0.name == "certerror" }))
+    }
+
+    // MARK: - parseErrorDetails
+
+    func testParseErrorDetails_noInternetError_withURL_returnsNoInternetModel() {
+        let url = URL(string: "https://example.com")!
+        let noInternetCode = Int(CFNetworkErrors.cfurlErrorNotConnectedToInternet.rawValue)
+        let error = NSError(domain: NSURLErrorDomain, code: noInternetCode, userInfo: [
+            NSURLErrorFailingURLErrorKey: url
+        ])
+        let helper = NativeErrorPageHelper(error: error)
+
+        let model = helper.parseErrorDetails()
+
+        XCTAssertEqual(model.foxImageName, ImageIdentifiers.NativeErrorPage.noInternetConnection)
+        XCTAssertNil(model.url)
+    }
+
+    func testParseErrorDetails_noFailingURL_returnsNoInternetModel() {
+        let noInternetCode = Int(CFNetworkErrors.cfurlErrorNotConnectedToInternet.rawValue)
+        let error = NSError(domain: NSURLErrorDomain, code: noInternetCode, userInfo: [:])
+        let helper = NativeErrorPageHelper(error: error)
+
+        let model = helper.parseErrorDetails()
+
+        XCTAssertEqual(model.foxImageName, ImageIdentifiers.NativeErrorPage.noInternetConnection)
+        XCTAssertNil(model.url)
+    }
+
+    func testParseErrorDetails_certError_withURL_returnsSecurityModel() {
+        let url = URL(string: "https://example.com")!
+        let error = NSError(
+            domain: NSURLErrorDomain,
+            code: NSURLErrorServerCertificateUntrusted,
+            userInfo: [NSURLErrorFailingURLErrorKey: url]
+        )
+        let helper = NativeErrorPageHelper(error: error)
+
+        let model = helper.parseErrorDetails()
+
+        XCTAssertEqual(model.foxImageName, ImageIdentifiers.NativeErrorPage.securityError)
+    }
+
+    func testParseErrorDetails_certErrorBadCertDomain_returnsAdvancedSection() {
+        let url = URL(string: "https://example.com")!
+        let error = NSError(
+            domain: NSURLErrorDomain,
+            code: NSURLErrorServerCertificateUntrusted,
+            userInfo: [
+                NSURLErrorFailingURLErrorKey: url,
+                NSUnderlyingErrorKey: NSError(
+                    domain: "NSURLErrorDomain",
+                    code: NSURLErrorServerCertificateUntrusted,
+                    userInfo: ["_kCFStreamErrorCodeKey": -9843]
+                )
+            ]
+        )
+        let helper = NativeErrorPageHelper(error: error)
+
+        let model = helper.parseErrorDetails()
+
+        XCTAssertNotNil(model.advancedSection)
+        XCTAssertTrue(model.showGoBackButton)
+    }
+
+    func testParseErrorDetails_genericError_withURL_returnsGenericModel() {
+        let url = URL(string: "https://example.com")!
+        let error = NSError(domain: NSURLErrorDomain, code: -1, userInfo: [
+            NSURLErrorFailingURLErrorKey: url
+        ])
+        let helper = NativeErrorPageHelper(error: error)
+
+        let model = helper.parseErrorDetails()
+
+        XCTAssertEqual(model.foxImageName, ImageIdentifiers.NativeErrorPage.securityError)
+        XCTAssertNil(model.advancedSection)
+    }
+
+    // MARK: - getCertDetails
+
+    func testGetCertDetails_missingFailingURL_returnsNil() {
+        let error = NSError(domain: NSURLErrorDomain, code: NSURLErrorServerCertificateUntrusted, userInfo: [:])
+        let helper = NativeErrorPageHelper(error: error)
+
+        XCTAssertNil(helper.getCertDetails())
+    }
+
+    func testGetCertDetails_missingCertChain_returnsNil() {
+        let url = URL(string: "https://example.com")!
+        let error = NSError(
+            domain: NSURLErrorDomain,
+            code: NSURLErrorServerCertificateUntrusted,
+            userInfo: [NSURLErrorFailingURLErrorKey: url]
+        )
+        let helper = NativeErrorPageHelper(error: error)
+
+        XCTAssertNil(helper.getCertDetails())
+    }
+
+    func testParseErrorDetails_certCodeWithNonURLErrorDomain_returnsGenericModel() {
+        let url = URL(string: "https://example.com")!
+        let error = NSError(
+            domain: "SomeOtherDomain",
+            code: NSURLErrorServerCertificateUntrusted,
+            userInfo: [NSURLErrorFailingURLErrorKey: url]
+        )
+        let helper = NativeErrorPageHelper(error: error)
+
+        let model = helper.parseErrorDetails()
+
+        XCTAssertEqual(model.foxImageName, ImageIdentifiers.NativeErrorPage.securityError)
+        XCTAssertNil(model.advancedSection)
+    }
 }
