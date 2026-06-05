0efb30533fba1ca70b127dc078e43acfeebaaede
Quang <qha23@outlook.com>
2026-06-05 16:39:35 -0700

    probably not needed

--- firefox-ios/Client/Frontend/ContextualHint/ContextualHintViewController.swift
+++ firefox-ios/Client/Frontend/ContextualHint/ContextualHintViewController.swift
@@ -201,12 +201,6 @@ class ContextualHintViewController: UIViewController,
 
     func applyTheme() {
         let theme = themeManager.getCurrentTheme(for: windowUUID)
-
-        view.isOpaque = false
-        view.backgroundColor = .clear
-        hintView.isOpaque = false
-        hintView.backgroundColor = .clear
-
         hintView.applyTheme(theme: theme)
         popoverPresentationController?.backgroundColor = theme.colors.layerGradient.colors.last
     }
