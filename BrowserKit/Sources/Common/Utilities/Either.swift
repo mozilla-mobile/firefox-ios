36193844f5d6551f0b68b768e93e41cb205e6206
ih-codes <ihugel@mozilla.com>
2026-06-09 16:03:33 -0600

    Add Either utility.

---
+++ BrowserKit/Sources/Common/Utilities/Either.swift
@@ -0,0 +1,11 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+
+/// A code migration helper; can be used to wrap different values in an array, method arguments, etc. while migrating from
+/// one type to another.
+public enum Either<Left, Right> {
+    case left(Left)
+    case right(Right)
+}
