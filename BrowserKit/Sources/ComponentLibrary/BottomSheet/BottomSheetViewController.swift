68cf1a734e961151df7d360a6219dd9dec0646d5
Sahil Satralkar <62758655+sahilsatralkar@users.noreply.github.com>
2026-06-10 20:12:20 +0530

    Bugfix FXIOS-15166 Remove redundant bottom sheet layout pass

--- BrowserKit/Sources/ComponentLibrary/BottomSheet/BottomSheetViewController.swift
+++ BrowserKit/Sources/ComponentLibrary/BottomSheet/BottomSheetViewController.swift
@@ -127,7 +127,6 @@ public class BottomSheetViewController: UIViewController,
             }
         } else {
             view.backgroundColor = viewModel.backgroundColor
-            view.layoutIfNeeded()
         }
     }
 
