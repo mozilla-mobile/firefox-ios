a67b22c0ab9b91f60e481fe27331a15d173558a4
Alex Bangu <abangu@Alexs-MacBook-Pro.local>
2026-06-11 18:53:25 -0400

    simple demo working

--- firefox-ios/Client/Frontend/Browser/Toolbars/AddressBarPanGestureHandler.swift
+++ firefox-ios/Client/Frontend/Browser/Toolbars/AddressBarPanGestureHandler.swift
@@ -47,6 +47,8 @@ final class AddressBarPanGestureHandler: NSObject, StoreSubscriber {
     private let addressToolbarContainer: AddressToolbarContainer
     private let statusBarOverlay: StatusBarOverlay
     private var panGestureRecognizer: UIPanGestureRecognizer?
+    private var swipeUpGestureRecognizer: UISwipeGestureRecognizer?
+    private var swipeDownGestureRecognizer: UISwipeGestureRecognizer?
 
     // MARK: - Properties
     private let tabManager: TabManager
@@ -101,9 +103,21 @@ final class AddressBarPanGestureHandler: NSObject, StoreSubscriber {
     }
 
     private func setupGesture() {
-        let gesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
-        addressToolbarContainer.addGestureRecognizer(gesture)
-        panGestureRecognizer = gesture
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
+        addressToolbarContainer.addGestureRecognizer(panGesture)
+        panGestureRecognizer = panGesture
+
+        let swipeUpGesture = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe))
+        swipeUpGesture.direction = .up
+        addressToolbarContainer.addGestureRecognizer(swipeUpGesture)
+        swipeUpGestureRecognizer = swipeUpGesture
+
+        let swipeDownGesture = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe))
+        swipeDownGesture.direction = .down
+        addressToolbarContainer.addGestureRecognizer(swipeDownGesture)
+        swipeDownGestureRecognizer = swipeDownGesture
+
+        panGesture.require(toFail: swipeUpGesture)
     }
 
     // MARK: - Redux
@@ -196,6 +210,22 @@ final class AddressBarPanGestureHandler: NSObject, StoreSubscriber {
             handleGestureEndedState(translation: translation, velocity: velocity, nextTab: nextTab)
         default: break
         }
+    }
+
+    @objc
+    @MainActor
+    private func handleSwipe(_ gesture: UISwipeGestureRecognizer) {
+        let direction = gesture.direction
+
+        if direction == .up && toolbarState?.toolbarPosition == .top {
+            return
+        } else if direction == .down && toolbarState?.toolbarPosition == .bottom {
+            return
+        }
+
+        let action = GeneralBrowserAction(windowUUID: windowUUID,
+                                          actionType: GeneralBrowserActionType.showTabTray)
+        store.dispatch(action)
     }
 
     private func handleGestureChangedState(translation: CGPoint, nextTab: Tab?) {
