071d9fb82823c234ea0c994c055555f44fc3f3fb
Laurie Marceau <lmarceau@mozilla.com>
2026-06-17 16:29:56 -0400

    Remove myself from concurrency reviewers

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -543,7 +543,7 @@ class CodeUsageDetector {
             case .swiftUIText:
                 return "### ⚠️ SwiftUI `Text(\"\")` usage detected\nSwiftUI 'Text(\"\"' needs to be avoided, use Strings.swift localization instead."
             case .task:
-                let contacts = "@Cramsden @ih-codes @lmarceau"
+                let contacts = "@Cramsden @ih-codes"
                 return "### 🧑‍💻 New `Task {}` detected\nNew `Task {}` added. Please add a concurrency reviewer on your PR: \(contacts)"
             case .notifiable:
                 return "### ⚠️ `NotificationCenter.default.addObserver` detected\nPlease prefer Notifiable over `NotificationCenter` unless specific circumstances."
