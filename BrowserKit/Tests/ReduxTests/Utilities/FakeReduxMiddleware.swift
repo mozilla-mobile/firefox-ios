dc473cfd4cfaec1d78342c272ce23ca756e524f8
ih-codes <ihugel@mozilla.com>
2026-06-26 16:24:18 -0600

    Migrate Redux Store and Middleware to support ModernActions inside the Redux package.

--- BrowserKit/Tests/ReduxTests/Utilities/FakeReduxMiddleware.swift
+++ BrowserKit/Tests/ReduxTests/Utilities/FakeReduxMiddleware.swift
@@ -10,16 +10,54 @@ import Foundation
 class FakeReduxMiddleware {
     var generateInitialCountValue: (() -> Int)?
 
-    lazy var fakeProvider: Middleware<FakeReduxState> = { state, action in
-        switch action.actionType {
-        case FakeReduxActionType.requestInitialValue:
+    lazy var fakeProvider: Middleware<FakeReduxState> = (legacyFakeProvider, modernFakeProvider)
+
+    lazy var modernFakeProvider: MiddlewareMethod<FakeReduxState> = { [self] state, action, windowUUID in
+        // Handles one type of action
+        guard let action = action as? FakeReduxModernAction else { return }
+
+        switch action {
+        case .requestInitialValue:
+            let initialValue = self.generateInitialCountValue?() ?? 0
+            store.dispatch(
+                FakeReduxModernAction.initialValueLoaded(initialValue: initialValue),
+                forWindowUUID: windowUUID
+            )
+
+        case .increaseCounter:
+            let existingValue = state.counter
+            let newValue = self.increaseCounter(currentValue: existingValue)
+            store.dispatch(
+                FakeReduxModernAction.counterIncreased(counterValue: newValue),
+                forWindowUUID: windowUUID
+            )
+
+        case .decreaseCounter:
+            let existingValue = state.counter
+            let newValue = self.decreaseCounter(currentValue: existingValue)
+            store.dispatch(
+                FakeReduxModernAction.counterDecreased(counterValue: newValue),
+                forWindowUUID: windowUUID
+            )
+
+        default:
+            break
+        }
+    }
+
+    lazy var legacyFakeProvider: LegacyMiddlewareMethod<FakeReduxState> = { [self] state, action in
+        // Handles one type of action
+        guard let actionType = action.actionType as? FakeReduxActionType else { return }
+
+        switch actionType {
+        case .requestInitialValue:
             let initialValue = self.generateInitialCountValue?() ?? 0
             let action = FakeReduxAction(counterValue: initialValue,
                                          windowUUID: windowUUID,
                                          actionType: FakeReduxActionType.initialValueLoaded)
             store.dispatch(action)
 
-        case FakeReduxActionType.increaseCounter:
+        case .increaseCounter:
             let existingValue = state.counter
             let newValue = self.increaseCounter(currentValue: existingValue)
             let action = FakeReduxAction(counterValue: newValue,
@@ -27,7 +65,7 @@ class FakeReduxMiddleware {
                                          actionType: FakeReduxActionType.counterIncreased)
             store.dispatch(action)
 
-        case FakeReduxActionType.decreaseCounter:
+        case .decreaseCounter:
             let existingValue = state.counter
             let newValue = self.decreaseCounter(currentValue: existingValue)
             let action = FakeReduxAction(counterValue: newValue,
