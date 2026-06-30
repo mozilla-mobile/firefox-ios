4f6121961c401f1517d7b589b58ea6e9595811d5
Filippo Zazzeroni <fzazzeroni@mozilla.com>
2026-06-30 11:13:30 +0200

    fix: remove errors from merging

--- firefox-ios/Client/FeatureFlags/FeatureFlagID.swift
+++ firefox-ios/Client/FeatureFlags/FeatureFlagID.swift
@@ -67,8 +67,6 @@ enum FeatureFlagID: String, CaseIterable {
     case unifiedSearch
     case videoIntroOnboarding
     case worldCupWidget
-    case toolbarTabTrayGestureEnableSwipe
-    case toolbarTabTrayGestureEnableInteractive
 
     /// The user preferences key for features that support user-togglable settings.
     /// Returns `nil` for features that are not user-configurable.
