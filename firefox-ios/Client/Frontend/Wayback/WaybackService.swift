a80163b525230b2984affeaa833eacb4e86e4ab8
yxm4nsy <accounts@yxmansy.dev>
2026-07-03 09:52:04 -0400

    Add Wayback utils and tests, fix issue with setting data in MockURLProtocol

---
+++ firefox-ios/Client/Frontend/Wayback/WaybackService.swift
@@ -0,0 +1,31 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import Foundation
+
+public class WaybackService {
+    public struct Snapshot: Decodable {
+        public let available: Bool
+        public let url: String
+        public let timestamp: String
+        public let status: String
+    }
+
+    private struct Response: Decodable {
+        let archived_snapshots: [String: Snapshot]
+    }
+
+    /// Returns the archived snapshot for a URL, or nil if none exists.
+    public static func fetchSnapshot(
+        for urlString: String,
+        session: URLSession = .shared
+    ) async throws -> Snapshot? {
+        var components = URLComponents(string: "https://archive.org/wayback/available")!
+        components.queryItems = [URLQueryItem(name: "url", value: urlString)]
+
+        let (data, _) = try await session.data(from: components.url!)
+        let response = try JSONDecoder().decode(Response.self, from: data)
+        return response.archived_snapshots["closest"]
+    }
+}
