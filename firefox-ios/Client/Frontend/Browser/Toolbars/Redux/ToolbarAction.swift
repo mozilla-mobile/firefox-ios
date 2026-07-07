93a09fca195513be0654d29811567d289d894d76
Foxbolts <Foxbolts3@gmail.com>
2026-07-07 15:46:36 -0400

    hide lens in pbm
    comment

--- firefox-ios/Client/Frontend/Browser/Toolbars/Redux/ToolbarAction.swift
+++ firefox-ios/Client/Frontend/Browser/Toolbars/Redux/ToolbarAction.swift
@@ -36,7 +36,6 @@ struct ToolbarAction: Action {
     let shouldAnimate: Bool?
     let middleButton: NavigationBarMiddleButtonType?
     let isTranslationsEnabled: Bool?
-    let isGoogleLensEnabled: Bool?
     let translationConfiguration: TranslationConfiguration?
     let previousTabScreenshot: UIImage?
     let nextTabScreenshot: UIImage?
@@ -68,7 +67,6 @@ struct ToolbarAction: Action {
          shouldAnimate: Bool? = nil,
          middleButton: NavigationBarMiddleButtonType? = nil,
          isTranslationsEnabled: Bool? = nil,
-         isGoogleLensEnabled: Bool? = nil,
          translationConfiguration: TranslationConfiguration? = nil,
          previousTabScreenshot: UIImage? = nil,
          nextTabScreenshot: UIImage? = nil,
@@ -103,7 +101,6 @@ struct ToolbarAction: Action {
         self.canSummarize = canSummarize
         self.middleButton = middleButton
         self.isTranslationsEnabled = isTranslationsEnabled
-        self.isGoogleLensEnabled = isGoogleLensEnabled
         self.translationConfiguration = translationConfiguration
         self.previousTabScreenshot = previousTabScreenshot
         self.nextTabScreenshot = nextTabScreenshot
@@ -179,7 +176,7 @@ enum ToolbarMiddlewareActionType: ActionType {
     case didTapButton
     case customA11yAction
     case urlDidChange
-    case didUpdateDefaultSearchEngine
+    case googleLensAvailabilityDidChange
     case didClearSearch
     case didStartDragInteraction
     case didSwipeToOpenTabTray
