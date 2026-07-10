001f60275a34b7e9dc85ae41935f3b595d06be05
Firefox Sync Engineering <sync-team@mozilla.com>
2026-07-10 09:00:11 +0000

    (Local AS flow) Nightly auto-update (154.0.20260710050225)

--- MozillaRustComponents/Package.swift
+++ MozillaRustComponents/Package.swift
@@ -1,13 +1,13 @@
 // swift-tools-version: 5.10
 import PackageDescription
 
-let checksum = "bf60e1bbfab081964931f11340db24a1a720cd1181957fa32101fbeafbbdc532"
-let version = "154.0.20260709050232"
-let url = "https://firefox-ci-tc.services.mozilla.com/api/index/v1/task/project.application-services.v2.swift.154.20260709050232/artifacts/public/build/MozillaRustComponents.xcframework.zip"
+let checksum = "fd7386377b06fdc46d92da5e76c530a8ba1a7711f57f079a0869b4f3f0fb69bf"
+let version = "154.0.20260710050225"
+let url = "https://firefox-ci-tc.services.mozilla.com/api/index/v1/task/project.application-services.v2.swift.154.20260710050225/artifacts/public/build/MozillaRustComponents.xcframework.zip"
 
 // Focus xcframework
-let focusChecksum = "724bfa6b22be6ba45fbd56b78f8d9f7cefc433626e5b019eec6e023c956e26a2"
-let focusUrl = "https://firefox-ci-tc.services.mozilla.com/api/index/v1/task/project.application-services.v2.swift.154.20260709050232/artifacts/public/build/FocusRustComponents.xcframework.zip"
+let focusChecksum = "caaf6478b54aa97368bfa0b396e7307a9329a38f66118a2f5555df196aeb8cd7"
+let focusUrl = "https://firefox-ci-tc.services.mozilla.com/api/index/v1/task/project.application-services.v2.swift.154.20260710050225/artifacts/public/build/FocusRustComponents.xcframework.zip"
 
 let package = Package(
     name: "MozillaRustComponentsSwift",
