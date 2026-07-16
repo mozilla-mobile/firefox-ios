498593b99a54850374520273156d15c6237bce26
Sophie <samin@mozilla.com>
2026-07-15 18:03:54 -0700

    Move availableContentHeight and availableWallpaperHeight into wallpaperState
    # Conflicts:
    #	firefox-ios/Client/Frontend/Home/Homepage/Redux/HomepageState.swift

--- firefox-ios/firefox-ios-tests/Tests/ClientTests/Frontend/Homepage/Wallpaper/WallpaperStateTests.swift
+++ firefox-ios/firefox-ios-tests/Tests/ClientTests/Frontend/Homepage/Wallpaper/WallpaperStateTests.swift
@@ -19,6 +19,56 @@ final class WallpaperStateTests: XCTestCase {
         XCTAssertNil(initialState.wallpaperConfiguration.textColor)
         XCTAssertNil(initialState.wallpaperConfiguration.cardColor)
         XCTAssertNil(initialState.wallpaperConfiguration.logoTextColor)
+        XCTAssertEqual(initialState.availableContentHeight, 0)
+        XCTAssertEqual(initialState.availableWallpaperHeight, 0)
+    }
+
+    @MainActor
+    func test_availableContentHeightDidChange_returnsExpectedState() {
+        let initialState = createSubject()
+        let reducer = headerReducer()
+
+        let newState = reducer(
+            initialState,
+            HomepageAction(
+                availableContentHeight: 500,
+                availableWallpaperHeight: 525,
+                windowUUID: .XCTestDefaultUUID,
+                actionType: HomepageActionType.availableContentHeightDidChange
+            )
+        )
+
+        XCTAssertEqual(newState.windowUUID, .XCTestDefaultUUID)
+        XCTAssertEqual(newState.availableContentHeight, 500)
+        XCTAssertEqual(newState.availableWallpaperHeight, 525)
+    }
+
+    @MainActor
+    func test_availableContentHeightDidChange_withPartialUpdate_keepsOtherValueStable() {
+        let initialState = createSubject()
+        let reducer = headerReducer()
+
+        let stateWithHeights = reducer(
+            initialState,
+            HomepageAction(
+                availableContentHeight: 500,
+                availableWallpaperHeight: 525,
+                windowUUID: .XCTestDefaultUUID,
+                actionType: HomepageActionType.availableContentHeightDidChange
+            )
+        )
+
+        let newState = reducer(
+            stateWithHeights,
+            HomepageAction(
+                availableContentHeight: 600,
+                windowUUID: .XCTestDefaultUUID,
+                actionType: HomepageActionType.availableContentHeightDidChange
+            )
+        )
+
+        XCTAssertEqual(newState.availableContentHeight, 600)
+        XCTAssertEqual(newState.availableWallpaperHeight, 525)
     }
 
     @MainActor
