4978f2556419b110adb3fe7d5c94d8b94aa892e8
Sushant Tiwari <149157016+sushanttiwari-14@users.noreply.github.com>
2026-07-30 03:59:40 +0530

    Remove FXIOS-16364 [Periphery] Remove dead OnboardingTelemetry protoc… (#34919)

--- firefox-ios/Client/Frontend/Onboarding/Protocols/OnboardingTelemetryProtocol.swift
+++ firefox-ios/Client/Frontend/Onboarding/Protocols/OnboardingTelemetryProtocol.swift
@@ -3,7 +3,6 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
 import Foundation
-import OnboardingKit
 
 enum OnboardingReason: String {
     case newUser = "new_user"
@@ -11,25 +10,9 @@ enum OnboardingReason: String {
 }
 
 protocol OnboardingTelemetryProtocol: AnyObject {
-    func sendCardViewTelemetry(from cardName: String)
-    func sendButtonActionTelemetry(from cardName: String,
-                                   with action: OnboardingActions,
-                                   and primaryButton: Bool)
-    func sendMultipleChoiceButtonActionTelemetry(
-        from cardName: String,
-        with action: OnboardingMultipleChoiceAction
-    )
-    func sendDismissOnboardingTelemetry(from cardName: String)
     func sendGoToSettingsButtonTappedTelemetry()
     func sendDismissButtonTappedTelemetry()
-    /// Records `onboarding.shown` and submits the `onboarding` ping.
-    func sendOnboardingShownTelemetry()
-    /// Records `onboarding.dismissed` with the given method and submits the `onboarding` ping.
-    func sendOnboardingDismissedTelemetry(outcome: OnboardingFlowOutcome)
     func sendWallpaperSelectorViewTelemetry()
     func sendWallpaperSelectorCloseTelemetry()
     func sendWallpaperSelectorSelectedTelemetry(wallpaperName: String, wallpaperType: String)
-    func sendWallpaperSelectedTelemetry(wallpaperName: String, wallpaperType: String)
-    func sendEngagementNotificationTappedTelemetry()
-    func sendEngagementNotificationCancelTelemetry()
 }
