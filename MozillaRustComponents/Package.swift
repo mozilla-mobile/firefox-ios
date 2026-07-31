09fe83955c434c5af36cdf31597a59b2349200d3
Firefox Sync Engineering <sync-team@mozilla.com>
2026-07-31 08:58:08 +0000

    (Local AS flow) Nightly auto-update (155.0.20260731050244)

--- MozillaRustComponents/Package.swift
+++ MozillaRustComponents/Package.swift
@@ -1,13 +1,13 @@
 // swift-tools-version: 5.10
 import PackageDescription
 
-let checksum = "fdb5f9678066e2b0ea2970224c6bf9c9a1d25b07c2e370d419b1f33dc04918d0"
-let version = "155.0.20260729050217"
-let url = "https://firefox-ci-tc.services.mozilla.com/api/index/v1/task/project.application-services.v2.swift.155.20260729050217/artifacts/public/build/MozillaRustComponents.xcframework.zip"
+let checksum = "9c2c458a20dc4b06fc3b5e7bdb42ac734708f28d6804d2a58ef3cfbb189e8ad9"
+let version = "155.0.20260731050244"
+let url = "https://firefox-ci-tc.services.mozilla.com/api/index/v1/task/project.application-services.v2.swift.155.20260731050244/artifacts/public/build/MozillaRustComponents.xcframework.zip"
 
 // Focus xcframework
-let focusChecksum = "639e0aa6e57adcff2d0126d0f747497db8947dda0a35979b4a9fa61ceae4744e"
-let focusUrl = "https://firefox-ci-tc.services.mozilla.com/api/index/v1/task/project.application-services.v2.swift.155.20260729050217/artifacts/public/build/FocusRustComponents.xcframework.zip"
+let focusChecksum = "3e8a91e72a3904e8998baf82290df0ab97d242435b09386bbaab55ec194a7d03"
+let focusUrl = "https://firefox-ci-tc.services.mozilla.com/api/index/v1/task/project.application-services.v2.swift.155.20260731050244/artifacts/public/build/FocusRustComponents.xcframework.zip"
 
 let package = Package(
     name: "MozillaRustComponentsSwift",
--- MozillaRustComponents/Sources/FocusRustComponentsWrapper/Generated/Metrics/Metrics.swift
+++ MozillaRustComponents/Sources/FocusRustComponentsWrapper/Generated/Metrics/Metrics.swift
@@ -23,7 +23,7 @@ extension GleanMetrics {
             // Intentionally left private, no external user can instantiate a new global object.
         }
 
-        public static let info = BuildInfo(buildDate: DateComponents(calendar: Calendar.current, timeZone: TimeZone(abbreviation: "UTC"), year: 2026, month: 7, day: 29, hour: 5, minute: 28, second: 51))
+        public static let info = BuildInfo(buildDate: DateComponents(calendar: Calendar.current, timeZone: TimeZone(abbreviation: "UTC"), year: 2026, month: 7, day: 31, hour: 5, minute: 24, second: 5))
     }
 
     enum NimbusEvents {
