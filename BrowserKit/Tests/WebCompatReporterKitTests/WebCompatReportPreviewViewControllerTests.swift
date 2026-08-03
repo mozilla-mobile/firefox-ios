f1a6e926270076bd76da62eb455513a7214b01ab
Razvan Litianu <litianu.razvan@gmail.com>
2026-08-03 16:32:58 +0300

    Bugfix FXIOS-16186 [WebCompat] Fix the close button test
    The close button is a plain bar button item now, not a CloseButton custom view,
    so drive its target and action instead of sending control events.

--- BrowserKit/Tests/WebCompatReporterKitTests/WebCompatReportPreviewViewControllerTests.swift
+++ BrowserKit/Tests/WebCompatReporterKitTests/WebCompatReportPreviewViewControllerTests.swift
@@ -3,7 +3,6 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
 import Common
-import ComponentLibrary
 import XCTest
 @testable import WebCompatReporterKit
 
@@ -123,11 +122,9 @@ final class WebCompatReportPreviewViewControllerTests: XCTestCase {
         subject.delegate = delegate
         subject.loadViewIfNeeded()
 
-        let closeButton = try XCTUnwrap(
-            subject.navigationItem.rightBarButtonItem?.customView as? CloseButton,
-            "The close button should be the bar button item's custom view"
-        )
-        fireActions(on: closeButton, for: .touchUpInside)
+        let closeButton = try XCTUnwrap(subject.navigationItem.rightBarButtonItem)
+        let target = try XCTUnwrap(closeButton.target as? NSObject)
+        target.perform(try XCTUnwrap(closeButton.action))
 
         XCTAssertEqual(delegate.didRequestDismissCallCount, 1)
     }
