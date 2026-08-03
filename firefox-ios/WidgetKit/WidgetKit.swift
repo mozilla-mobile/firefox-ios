bd0dac2c3440af94b7b55250504af0b664284795
Winnie Teichmann <4530+thatswinnie@users.noreply.github.com>
2026-08-03 20:39:59 +0200

    Add FXIOS-16422 [Theming] Add theming to WidgetKit (#35008)
    * Add theming to WidgetKit
    
    * Add test
    
    * Address PR comments
    
    ---------
    
    Co-authored-by: Winnie Teichmann <>

--- firefox-ios/WidgetKit/WidgetKit.swift
+++ firefox-ios/WidgetKit/WidgetKit.swift
@@ -3,8 +3,8 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
 #if canImport(WidgetKit)
-import WidgetKit
 import SwiftUI
+import WidgetKit
 
 @main
 struct FirefoxWidgets: WidgetBundle {
