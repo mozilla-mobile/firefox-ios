4da8c96e26fe3bd68b9528a6d38c30edd9fa5ec3
Yahya Sherif El Mansy <yxm4n5y@gmail.com>
2026-08-06 15:18:15 -0400

    Add TODO comment
    Co-authored-by: Jan Brasna <1784648+janbrasna@users.noreply.github.com>

--- firefox-ios/Client/Frontend/Wayback/WaybackService.swift
+++ firefox-ios/Client/Frontend/Wayback/WaybackService.swift
@@ -24,6 +24,7 @@ class WaybackService {
     private static let session: URLSession = {
         let version = AppInfo.appVersion
         let config = URLSessionConfiguration.default
+        // TODO: FXIOS-16529 Move to shared UserAgent configuration
         config.httpAdditionalHeaders = [
             "User-Agent": "firefox-ios-neterr/\(version) (+https://mzl.la/3RNfZFB)"
         ]
