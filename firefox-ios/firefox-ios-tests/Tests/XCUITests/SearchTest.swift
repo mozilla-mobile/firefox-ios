28249f9e2413bc052d7f553e9192ddedf89ce8a0
Clare So <1740517+clarmso@users.noreply.github.com>
2026-08-11 00:34:45 -0400

    Working solution

--- firefox-ios/firefox-ios-tests/Tests/XCUITests/SearchTest.swift
+++ firefox-ios/firefox-ios-tests/Tests/XCUITests/SearchTest.swift
@@ -551,6 +551,46 @@ class SearchTests: FeatureFlaggedTestBase {
         }
     }
 
+    func testFirefoxSuggestionsLandscapePrivate() {
+        app.launch()
+
+        // Precondition: "Ingest New Suggestions Now" from the
+        // secret settings
+        navigator.goto(SettingsScreen)
+        navigator.performAction(Action.OpenSecretSettings)
+        navigator.performAction(Action.IngestNewSuggestionsNow)
+        navigator.goto(HomePanelsScreen)
+
+        // Step 1: Type a keyword that trigers a sponsored result
+        browserScreen.tapOnAddressBar()
+        browserScreen.tapClearButtonIfExists()
+        browserScreen.typeOnSearchBar(text: "Amazon")
+
+        // Step 2: Sponsored result should be specified
+        mozWaitForElementToExist(app.scrollViews.buttons["Search Settings"])
+        mozWaitForElementToExist(app.staticTexts["Amazon.com - Official Site"])
+        mozWaitForElementToExist(app.staticTexts["Sponsored"])
+
+        // Step 3: Turn the device to landscape and observe the sponsored result
+        XCUIDevice.shared.orientation = UIDeviceOrientation.landscapeLeft
+        mozWaitForElementToExist(app.scrollViews.buttons["Search Settings"])
+        mozWaitForElementToExist(app.staticTexts["Amazon.com - Official Site"])
+        mozWaitForElementToExist(app.staticTexts["Sponsored"])
+        navigator.performAction(Action.CloseURLBarOpen)
+
+        // Step 4: Trigger a sponsored result in private mode
+        XCUIDevice.shared.orientation = UIDeviceOrientation.portrait
+        navigator.goto(TabTray)
+        navigator.toggleOn(userState.isPrivate, withAction: Action.ToggleExperimentPrivateMode)
+        navigator.goto(NewTabScreen)
+        browserScreen.tapOnAddressBar()
+        browserScreen.tapClearButtonIfExists()
+        browserScreen.typeOnSearchBar(text: "Amazon")
+        mozWaitForElementToNotExist(app.scrollViews.buttons["Search Settings"])
+        mozWaitForElementToNotExist(app.staticTexts["Amazon.com - Official Site"])
+        mozWaitForElementToNotExist(app.staticTexts["Sponsored"])
+    }
+
     private func turnOnOffSearchSuggestions(turnOnSwitch: Bool) {
         let showSearchSuggestions = app.switches[AccessibilityIdentifiers.Settings.Search.showSearchSuggestions]
         mozWaitForElementToExist(showSearchSuggestions)
