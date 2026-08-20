69dc518b88a1c9ef7121ada88030a493c9cd461c
Clare So <1740517+clarmso@users.noreply.github.com>
2026-08-20 14:44:15 -0400

    Created pytest.mark.regression

--- firefox-ios/firefox-ios-tests/Tests/SyncIntegrationTests/test_integration.py
+++ firefox-ios/firefox-ios-tests/Tests/SyncIntegrationTests/test_integration.py
@@ -49,6 +49,6 @@ def test_sync_tabs_firefox_suggest(tps, xcodebuild):
     tps.run('test_tabs_desktop.js')
     xcodebuild.test('XCUITests/IntegrationTests/testFxATabsFirefoxSuggest')
 
-# Regression
+@pytest.mark.regression
 def test_modern_kit_onboarding_start_syncing_use_email_instead(xcodebuild):
     xcodebuild.test('XCUITests/IntegrationTests/testModernKitOnboardingStartSyncingUseEmailInstead')
