2ffb4a7dace626757b367723ff02144bcf94bc92
Razvan Litianu <litianu.razvan@gmail.com>
2026-08-20 11:29:41 +0300

    Refactor FXIOS-16487 [WebCompat] Say up front that the preview file never ships

--- BrowserKit/Sources/WebCompatReporterKit/Preview/WebCompatReportSheetPreview.swift
+++ BrowserKit/Sources/WebCompatReporterKit/Preview/WebCompatReportSheetPreview.swift
@@ -2,6 +2,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
+/// Scaffolding for the Xcode previews below. None of it ships: the view models here stand in for
+/// what the Client-side Redux layer builds at runtime.
 #if DEBUG
 import Common
 import SwiftUI
