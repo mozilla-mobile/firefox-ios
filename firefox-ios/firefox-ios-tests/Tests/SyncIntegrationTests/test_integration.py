327da02df2b129f60e7f82b5d089a41c8e0012f1
Clare So <1740517+clarmso@users.noreply.github.com>
2026-08-20 15:23:30 -0400

    MTE-5692 Add onboarding sync sign-in test (#34985)

--- firefox-ios/firefox-ios-tests/Tests/SyncIntegrationTests/test_integration.py
+++ firefox-ios/firefox-ios-tests/Tests/SyncIntegrationTests/test_integration.py
@@ -49,6 +49,10 @@ def test_sync_tabs_firefox_suggest(tps, xcodebuild):
     tps.run('test_tabs_desktop.js')
     xcodebuild.test('XCUITests/IntegrationTests/testFxATabsFirefoxSuggest')
 
+@pytest.mark.regression
+def test_modern_kit_onboarding_start_syncing_use_email_instead(xcodebuild):
+    xcodebuild.test('XCUITests/IntegrationTests/testModernKitOnboardingStartSyncingUseEmailInstead')
+
 @pytest.mark.smoketest
 def test_add_tab_from_tab_tray(tps, xcodebuild):
     tps.run('test_tabs_desktop.js')
