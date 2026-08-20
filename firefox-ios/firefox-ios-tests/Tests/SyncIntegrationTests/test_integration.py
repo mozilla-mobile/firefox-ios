03b4ff3c51deea80ea4ffbf99d3f85e2ec97be2c
Clare So <1740517+clarmso@users.noreply.github.com>
2026-08-20 01:58:53 -0400

    Add smoke test decorator

--- firefox-ios/firefox-ios-tests/Tests/SyncIntegrationTests/test_integration.py
+++ firefox-ios/firefox-ios-tests/Tests/SyncIntegrationTests/test_integration.py
@@ -49,6 +49,7 @@ def test_sync_tabs_firefox_suggest(tps, xcodebuild):
     tps.run('test_tabs_desktop.js')
     xcodebuild.test('XCUITests/IntegrationTests/testFxATabsFirefoxSuggest')
 
+@pytest.mark.smoketest
 def test_add_tab_from_tab_tray(tps, xcodebuild):
     tps.run('test_tabs_desktop.js')
     xcodebuild.test('XCUITests/IntegrationTests/testAddTabFromTabTray')
