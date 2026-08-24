ebd22c9795f96a59612942c971b46b432b6dfcb4
Kio Godsfavour <71834021+Kiyo99@users.noreply.github.com>
2026-08-24 09:03:43 -0400

    Refactor FXIOS-14968 [Homepage] Move section header configuration out of Redux state (#35335)
    * Moved homepage-related state constants out of the redux stack
    
    * Moved section header tests to its dedicated file

--- firefox-ios/Client/Frontend/Home/Homepage/SectionHeader/SectionHeaderConfiguration.swift
+++ firefox-ios/Client/Frontend/Home/Homepage/SectionHeader/SectionHeaderConfiguration.swift
@@ -17,3 +17,37 @@ struct SectionHeaderConfiguration: Equatable, Hashable {
     var buttonTitle: String?
     var style: Style = .sectionTitle
 }
+
+extension SectionHeaderConfiguration {
+    static let bookmarks = SectionHeaderConfiguration(
+        title: .BookmarksSectionTitle,
+        a11yIdentifier: AccessibilityIdentifiers.FirefoxHomepage.SectionTitles.bookmarks,
+        isButtonHidden: false,
+        buttonA11yIdentifier: AccessibilityIdentifiers.FirefoxHomepage.MoreButtons.bookmarks,
+        buttonTitle: .BookmarksSavedShowAllText
+    )
+
+    static let jumpBackIn = SectionHeaderConfiguration(
+        title: .FirefoxHomeJumpBackInSectionTitle,
+        a11yIdentifier: AccessibilityIdentifiers.FirefoxHomepage.SectionTitles.jumpBackIn,
+        isButtonHidden: false,
+        buttonA11yIdentifier: AccessibilityIdentifiers.FirefoxHomepage.MoreButtons.jumpBackIn,
+        buttonTitle: .BookmarksSavedShowAllText
+    )
+
+    static let topSites = SectionHeaderConfiguration(
+        title: .FirefoxHomepage.Shortcuts.SectionTitle,
+        a11yIdentifier: AccessibilityIdentifiers.FirefoxHomepage.SectionTitles.topSites,
+        isButtonHidden: false,
+        buttonA11yIdentifier: AccessibilityIdentifiers.FirefoxHomepage.MoreButtons.shortcuts,
+        buttonTitle: .BookmarksSavedShowAllText
+    )
+
+    static var merino: SectionHeaderConfiguration {
+        SectionHeaderConfiguration(
+            title: .FirefoxHomepage.Pocket.NewsSectionTitle,
+            a11yIdentifier: AccessibilityIdentifiers.FirefoxHomepage.SectionTitles.merino,
+            style: .newsAffordance
+        )
+    }
+}
