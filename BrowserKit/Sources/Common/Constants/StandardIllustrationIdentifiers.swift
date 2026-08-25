5c608616804439454a81429bbaedfccbea4c8a09
Winnie Teichmann <>
2026-08-25 11:41:25 +0200

    Add StandardIllustrationIdentifiers

---
+++ BrowserKit/Sources/Common/Constants/StandardIllustrationIdentifiers.swift
@@ -0,0 +1,22 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import Foundation
+
+/// This struct defines all the standard illustration identifiers used in the app.
+/// When adding new identifiers, please respect alphabetical order.
+/// Sing the song if you must.
+public struct StandardIllustrationIdentifiers {
+    // Custom illustrations exported at feature-specific dimensions
+    public struct Exports {
+    }
+
+    // Reusable illustration kits
+    public struct Kit {
+    }
+
+    // Pictogram illustrations
+    public struct Pictograms {
+    }
+}
