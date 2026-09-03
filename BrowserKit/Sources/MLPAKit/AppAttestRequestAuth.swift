a7e4784fb033406f01d62fd3d012e00c1cc84ac9
Filippo Zazzeroni <fzazzeroni@mozilla.com>
2026-09-03 18:06:16 +0200

    chore: add correct service

--- BrowserKit/Sources/MLPAKit/AppAttestRequestAuth.swift
+++ BrowserKit/Sources/MLPAKit/AppAttestRequestAuth.swift
@@ -46,7 +46,7 @@ public struct AppAttestRequestAuth: RequestAuthProtocol {
         ).encode()
 
         request.setValue(MLPAConstants.bearerPrefix + jwt, forHTTPHeaderField: MLPAConstants.authorizationHeader)
-        request.setValue("liner-answers", forHTTPHeaderField: MLPAConstants.serviceTypeHeader)
+        request.setValue("liner-answer", forHTTPHeaderField: MLPAConstants.serviceTypeHeader)
         request.setValue("true", forHTTPHeaderField: MLPAConstants.useAppAttestHeader)
         request.httpBody = result.payload
     }
