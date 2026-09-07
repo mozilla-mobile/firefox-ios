654414920ba4393accde3272a9c0fbbd177af762
Filippo Zazzeroni <fzazzeroni@mozilla.com>
2026-09-07 17:32:12 +0200

    chore: fix favicon on quick answers

--- BrowserKit/Sources/QuickAnswersKit/UI/QuickAnswersSourceView.swift
+++ BrowserKit/Sources/QuickAnswersKit/UI/QuickAnswersSourceView.swift
@@ -78,12 +78,18 @@ final class QuickAnswersSourceCell: UICollectionViewCell, ReusableCell, ThemeApp
             faviconCornerRadius: UX.faviconCornerRadius,
             faviconBorderWidth: UX.thumbnailBorderWidth,
             heroImageSize: .zero,
-            fallbackFaviconSize: CGSize(width: UX.faviconSize, height: UX.faviconSize)
+            fallbackFaviconSize: CGSize(width: UX.faviconSize, height: UX.faviconSize),
         )
         thumbnailImageView.setHeroImage(heroImageViewModel)
+        let faviconSiteResource: SiteResource? = if let url = item.faviconURL {
+            SiteResource.remoteURL(url: url)
+        } else {
+            nil
+        }
         faviconImageView.setFavicon(
             FaviconImageViewModel(
-                siteURLString: item.faviconURL?.absoluteString ?? item.url?.absoluteString,
+                siteURLString: item.url?.absoluteString ?? "",
+                siteResource: faviconSiteResource,
                 faviconCornerRadius: UX.faviconCornerRadius
             )
         )
