cd33931ae3b3c9c903422a24efd6a2e4de86e030
yoanarios <yrrios13@gmail.com>
2026-09-11 09:45:13 -0400

    Migrate NavigationAction to builder from state

--- firefox-ios/firefox-ios-tests/Tests/ClientTests/Toolbar/AddressBarStateTests.swift
+++ firefox-ios/firefox-ios-tests/Tests/ClientTests/Toolbar/AddressBarStateTests.swift
@@ -35,7 +35,7 @@ final class AddressBarStateTests: XCTestCase, StoreTestUtility {
         let initialState = createSubject()
 
         XCTAssertEqual(initialState.windowUUID, windowUUID)
-        XCTAssertEqual(initialState.navigationActionsState, NavigationActionsState(windowUUID: windowUUID))
+        XCTAssertEqual(initialState.navigationActions, [])
         XCTAssertEqual(initialState.trailingPageActions, [])
         XCTAssertEqual(initialState.leadingPageActions, [])
         XCTAssertEqual(initialState.browserActions, [])
@@ -69,7 +69,7 @@ final class AddressBarStateTests: XCTestCase, StoreTestUtility {
         )
 
         XCTAssertEqual(newState.windowUUID, windowUUID)
-        XCTAssertEqual(newState.navigationActionsState, NavigationActionsState(windowUUID: windowUUID))
+        XCTAssertEqual(newState.navigationActions, [])
 
         XCTAssertEqual(newState.leadingPageActions.count, 0)
         XCTAssertEqual(newState.trailingPageActions.count, 0)
@@ -426,9 +426,9 @@ final class AddressBarStateTests: XCTestCase, StoreTestUtility {
         XCTAssertEqual(newState.leadingPageActions.count, 1)
         XCTAssertEqual(newState.leadingPageActions[0].actionType, .share)
 
-        XCTAssertEqual(newState.navigationActionsState.actions.count, 2)
-        XCTAssertEqual(newState.navigationActionsState.actions[0].actionType, .back)
-        XCTAssertEqual(newState.navigationActionsState.actions[1].actionType, .forward)
+        XCTAssertEqual(newState.navigationActions.count, 2)
+        XCTAssertEqual(newState.navigationActions[0].actionType, .back)
+        XCTAssertEqual(newState.navigationActions[1].actionType, .forward)
     }
 
     func test_urlDidChangeAction_withNavigationToolbar_returnsExpectedState() {
@@ -483,7 +483,7 @@ final class AddressBarStateTests: XCTestCase, StoreTestUtility {
         )
 
         XCTAssertEqual(newState.windowUUID, windowUUID)
-        XCTAssertEqual(newState.navigationActionsState.actions.count, 0)
+        XCTAssertEqual(newState.navigationActions.count, 0)
     }
 
     func test_backForwardButtonStateChangedAction_withoutNavigationToolbar_returnsExpectedState() {
@@ -503,11 +503,11 @@ final class AddressBarStateTests: XCTestCase, StoreTestUtility {
         )
 
         XCTAssertEqual(newState.windowUUID, windowUUID)
-        XCTAssertEqual(newState.navigationActionsState.actions.count, 2)
-        XCTAssertEqual(newState.navigationActionsState.actions[0].actionType, .back)
-        XCTAssertEqual(newState.navigationActionsState.actions[0].isEnabled, true)
-        XCTAssertEqual(newState.navigationActionsState.actions[1].actionType, .forward)
-        XCTAssertEqual(newState.navigationActionsState.actions[1].isEnabled, false)
+        XCTAssertEqual(newState.navigationActions.count, 2)
+        XCTAssertEqual(newState.navigationActions[0].actionType, .back)
+        XCTAssertEqual(newState.navigationActions[0].isEnabled, true)
+        XCTAssertEqual(newState.navigationActions[1].actionType, .forward)
+        XCTAssertEqual(newState.navigationActions[1].isEnabled, false)
     }
 
     // MARK: - Translation Configuration
@@ -763,9 +763,9 @@ final class AddressBarStateTests: XCTestCase, StoreTestUtility {
         )
 
         XCTAssertEqual(newState.windowUUID, windowUUID)
-        XCTAssertEqual(newState.navigationActionsState.actions.count, 2)
-        XCTAssertEqual(newState.navigationActionsState.actions[0].actionType, .back)
-        XCTAssertEqual(newState.navigationActionsState.actions[1].actionType, .forward)
+        XCTAssertEqual(newState.navigationActions.count, 2)
+        XCTAssertEqual(newState.navigationActions[0].actionType, .back)
+        XCTAssertEqual(newState.navigationActions[1].actionType, .forward)
 
         XCTAssertEqual(newState.trailingPageActions.count, 0)
         XCTAssertEqual(newState.leadingPageActions.count, 0)
@@ -816,9 +816,9 @@ final class AddressBarStateTests: XCTestCase, StoreTestUtility {
         )
 
         XCTAssertEqual(newState.windowUUID, windowUUID)
-        XCTAssertEqual(newState.navigationActionsState.actions.count, 2)
-        XCTAssertEqual(newState.navigationActionsState.actions[0].actionType, .back)
-        XCTAssertEqual(newState.navigationActionsState.actions[1].actionType, .forward)
+        XCTAssertEqual(newState.navigationActions.count, 2)
+        XCTAssertEqual(newState.navigationActions[0].actionType, .back)
+        XCTAssertEqual(newState.navigationActions[1].actionType, .forward)
 
         XCTAssertEqual(newState.trailingPageActions.count, 0)
 
@@ -907,7 +907,7 @@ final class AddressBarStateTests: XCTestCase, StoreTestUtility {
         )
 
         XCTAssertEqual(newState.windowUUID, windowUUID)
-        XCTAssertEqual(newState.navigationActionsState.actions.count, 0)
+        XCTAssertEqual(newState.navigationActions.count, 0)
 
         XCTAssertEqual(newState.leadingPageActions.count, 0)
         XCTAssertEqual(newState.trailingPageActions.count, 0)
@@ -936,7 +936,7 @@ final class AddressBarStateTests: XCTestCase, StoreTestUtility {
         )
 
         XCTAssertEqual(newState.windowUUID, windowUUID)
-        XCTAssertEqual(newState.navigationActionsState.actions.count, 0)
+        XCTAssertEqual(newState.navigationActions.count, 0)
 
         XCTAssertEqual(newState.leadingPageActions.count, 0)
         XCTAssertEqual(newState.trailingPageActions.count, 0)
@@ -967,7 +967,7 @@ final class AddressBarStateTests: XCTestCase, StoreTestUtility {
         )
 
         XCTAssertEqual(newState.windowUUID, windowUUID)
-        XCTAssertEqual(newState.navigationActionsState.actions.count, 0)
+        XCTAssertEqual(newState.navigationActions.count, 0)
 
         XCTAssertEqual(newState.leadingPageActions.count, 0)
         XCTAssertEqual(newState.trailingPageActions.count, 0)
@@ -1142,7 +1142,7 @@ final class AddressBarStateTests: XCTestCase, StoreTestUtility {
         )
 
         XCTAssertEqual(newState.windowUUID, windowUUID)
-        XCTAssertEqual(newState.navigationActionsState.actions.count, 0)
+        XCTAssertEqual(newState.navigationActions.count, 0)
 
         XCTAssertEqual(newState.trailingPageActions.count, 1)
         XCTAssertEqual(newState.trailingPageActions[0].actionType, .reload)
@@ -1176,7 +1176,7 @@ final class AddressBarStateTests: XCTestCase, StoreTestUtility {
         )
 
         XCTAssertEqual(newState.windowUUID, windowUUID)
-        XCTAssertEqual(newState.navigationActionsState.actions.count, 0)
+        XCTAssertEqual(newState.navigationActions.count, 0)
         XCTAssertEqual(newState.leadingPageActions.count, 0)
         XCTAssertEqual(newState.trailingPageActions.count, 0)
         XCTAssertEqual(newState.browserActions.count, 1)
