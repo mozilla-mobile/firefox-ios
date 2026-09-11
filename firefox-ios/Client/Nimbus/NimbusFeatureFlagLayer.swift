3261259e0155b9747f26ce5f79b68e9f97a66ba6
OrlaM <omitchell@mozilla.com>
2026-09-10 22:47:11 -0400

    Add [AdBlocker] Enable the ad blocker by default for all users (#35632)
    * Add [AdBlocker] Enable the ad blocker by default for all users
    
    * fix: also enable ad blocker badge by default
    
    ---------
    
    Co-authored-by: Issam Mani <issamouu69@gmail.com>

--- firefox-ios/Client/Nimbus/NimbusFeatureFlagLayer.swift
+++ firefox-ios/Client/Nimbus/NimbusFeatureFlagLayer.swift
@@ -443,7 +443,8 @@ final class NimbusFeatureFlagLayer: NimbusFeatureFlagLayerProviding, Sendable {
     }
 
     private func checkAdBlockerFeature() -> Bool {
-        return nimbus.features.adBlockerFeature.value().enabled
+        // Hardcoded on so the ad blocker is enabled for everyone, without risking changes to the code directly.
+        return true
     }
 
     private func checkBackgroundAudioFeature() -> Bool {
@@ -451,7 +452,8 @@ final class NimbusFeatureFlagLayer: NimbusFeatureFlagLayerProviding, Sendable {
     }
 
     private func checkAdBlockerBadgeFeature() -> Bool {
-        return nimbus.features.adBlockerFeature.value().badgeEnabled
+        // Hardcoded on so the ad blocker is enabled for everyone, without risking changes to the code directly.
+        return true
     }
 
     func checkStartAtHomeConfiguration() -> StartAtHome {
