cf35fc72f848b5c35b79cace2a6db121870f383b
Shreyans Jaiswal <shreyansj.it.23@nitj.ac.in>
2026-09-16 21:47:39 +0530

    Bugfix FXIOS-29121 [Widgets] Fix deeplink tab restoration

--- firefox-ios/Client/Application/SceneDelegate.swift
+++ firefox-ios/Client/Application/SceneDelegate.swift
@@ -71,6 +71,10 @@ class SceneDelegate: UIResponder,
         // Handle clean-up here for closing windows on iPad
         guard let sceneCoordinator = (scene.delegate as? SceneDelegate)?.sceneCoordinator else { return }
 
+        if AppEventQueue.activityIsInProgress(.pendingDeeplinkTab(sceneCoordinator.windowUUID)) {
+            AppEventQueue.completed(.pendingDeeplinkTab(sceneCoordinator.windowUUID))
+        }
+
         // For now, we explicitly cancel downloads for windows that are closed.
         // On iPhone this will happen during app termination, for iPad it will
         // occur on termination or when a window is disconnected/closed by iPadOS
@@ -236,6 +240,10 @@ class SceneDelegate: UIResponder,
 
         logger.log("Scene coordinator will handle a route", level: .info, category: .coordinator)
         sessionManager.launchSessionProvider.openedFromExternalSource = true
+
+        if route.willSelectTabOnHandling {
+            AppEventQueue.started(.pendingDeeplinkTab(sceneCoordinator.windowUUID))
+        }
 
         if isDeeplinkOptimizationRefactorEnabled {
             AppEventQueue.wait(for: [.startupFlowComplete]) {
