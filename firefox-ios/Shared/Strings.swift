1bed7fe5a5fadd684af8ea21b8ee0c01acf688cc
Alexander Bangu <alexander.bangu@gmail.com>
2026-09-22 09:19:43 -0700

    updated string descriptions

--- firefox-ios/Shared/Strings.swift
+++ firefox-ios/Shared/Strings.swift
@@ -1820,61 +1820,61 @@ extension String {
                     key: "Onboarding.MultiDay.NotificationCard.Title.v158",
                     tableName: "Onboarding",
                     value: "Stay in the Loop With %@",
-                    comment: "String that describes the title text of the notification permission onboarding card. %@ is the name of the app (e.g., Firefox")
+                    comment: "Title for the onboarding card requesting permissions to send notifications. %@ is the name of the app (e.g., Firefox).")
                 public static let BodyText = MZLocalizedString(
                     key: "Onboarding.MultiDay.NotificationCard.BodyText.v158",
                     tableName: "Onboarding",
                     value: "Get privacy feature updates, protection tips, and summaries of your privacy report.",
-                    comment: "String that describes the body text of the notification permission onboarding card. It tells the user what notifications they can expect to see if they enable notifications.")
+                    comment: "Body of the onboarding card requesting permissions to send notifications, with examples of notifications the user will receive.")
                 public static let AcceptButtonText = MZLocalizedString(
                     key: "Onboarding.MultiDay.NotificationCard.AcceptButtonText.v158",
                     tableName: "Onboarding",
                     value: "Enable Notifications",
-                    comment: "String that describes the accept button of the notification permission onboarding card. Tapping this button accepts the notification permission request.")
+                    comment: "Primary button on the onboarding card requesting permission to send notifications. Tapping this button accepts the permission request.")
                 public static let DeclineButtonText = MZLocalizedString(
                     key: "Onboarding.MultiDay.NotificationCard.DeclineButtonText.v158",
                     tableName: "Onboarding",
                     value: "Not Now",
-                    comment: "String that describes the decline button of the notification permission onboarding card. Tapping this button declines the notification permission request.")
+                    comment: "Secondary button on the onboarding card requesting permission to send notifications. Tapping this button declines the permission request.")
             }
             public struct HomeScreenNotificationsPopup {
                 public static let Title = MZLocalizedString(
                     key: "Onboarding.MultiDay.HomeScreenNotificationsPopup.Title.v158",
                     tableName: "Onboarding",
                     value: "Get Fresh Privacy Features",
-                    comment: "Describes the title of a homescreen popup that appears if the user denied notification permissions. It's used to tell the user what kinds of notifications they can expect to receive.")
+                    comment: "Title of the home screen popup requesting permission to send notifications. It emphasizes how notifications keep you updated on privacy features.")
                 public static let BodyText = MZLocalizedString(
                     key: "Onboarding.MultiDay.HomeScreenNotificationsPopup.BodyText.v158",
                     tableName: "Onboarding",
-                    value: "Stay up to date on %@'s latest privacy features.",
-                    comment: "Describes the body text of a homescreen popup that appears if the user denied notification permissions. It's used to tell the user what they'd get out from turning on notifications. %@ is a placeholder for the app name (e.g., Firefox).")
+                    value: "Stay up to date on %@’s latest privacy features.",
+                    comment: "Body of the home screen popup requesting permission to send notifications. It emphasizes how notifications keep you updated on privacy features. %@ is a placeholder for the app name (e.g., Firefox).")
                 public static let EnableButtonText = MZLocalizedString(
                     key: "Onboarding.MultiDay.HomeScreenNotificationsPopup.EnableButtonText.v158",
                     tableName: "Onboarding",
                     value: "Enable Notifications",
-                    comment: "Button text for enabling notifications in the homescreen popup that appears if the user denied notification permissions.")
+                    comment: "Button on the home screen popup requesting notification permissions. Tapping this button accepts the permission request.")
             }
             public struct SyncCard {
                 public static let Title = MZLocalizedString(
                     key: "Onboarding.MultiDay.SyncCard.Title.v158",
                     tableName: "Onboarding",
                     value: "Sync Your Stuff to Any Device",
-                    comment: "Title for the notification card that prompts users to sync their account data (e.g., history, passwords, payment information).")
+                    comment: "Title of the onboarding card that prompts users to enable syncing for their account data so they can access it across devices (e.g., history, passwords, payment information).")
                 public static let BodyText = MZLocalizedString(
                     key: "Onboarding.MultiDay.SyncCard.BodyText.v158",
                     tableName: "Onboarding",
                     value: "Instantly get passwords, history, and more everywhere you use %@.",
-                    comment: "Body text for the notification card that prompts users to sync their account data (e.g., history, passwords, payment information). %@ is a placeholder for the app name (e.g., Firefox).")
+                    comment: "Body of the onboarding card that prompts users to enable syncing for their account data so they can access it across devices (e.g., history, passwords, payment information). %@ is a placeholder for the app name (e.g., Firefox).")
                 public static let AcceptButtonText = MZLocalizedString(
                     key: "Onboarding.MultiDay.SyncCard.AcceptButtonText.v158",
                     tableName: "Onboarding",
                     value: "Sign In to Sync",
-                    comment: "Button text for the notification card that prompts users to sync their account data (e.g., history, passwords, payment information). Should be interpreted as asking the user to sign in so that they sync their information.")
+                    comment: "Primary button on the onboarding card that prompts users to enable syncing for their account data. Tapping it directs the user sign into to their Mozilla account to access the feature.")
                 public static let DeclineButtonText = MZLocalizedString(
                     key: "Onboarding.MultiDay.NotificationCard.DeclineButtonText.v158",
                     tableName: "Onboarding",
                     value: "Not Now",
-                    comment: "String that describes the decline button of the sync onboarding card. Tapping this button dismisses the request to sign in.")
+                    comment: "Secondary button on the onboarding card that prompts users to enable syncing for their account data. Tapping this button dismisses the prompt and closes the card.")
             }
         }
 
